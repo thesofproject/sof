d3ba00c6d1e50c8a343194552ba7955b67d3c63b
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2015-08-28 19:17:47 +0100

    debug: fix mailbox and debug output
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/include/reef/debug.h
+++ src/include/reef/debug.h
@@ -9,10 +9,24 @@
 #ifndef __INCLUDE_DEBUG__
 #define __INCLUDE_DEBUG__
 
-// tmp hack
-#include <platform/memory.h>
+#include <reef/mailbox.h>
+#include <stdint.h>
 
 #define dbg() \
-	*((int*)SHIM_BASE) = __LINE__;
+	do { \
+		uint32_t *m = (uint32_t*)mailbox_get_debug_base(); \
+		*(m++) = (__FILE__[0] << 24) + (__FILE__[1] << 16) + (__FILE__[2] << 8) + (__FILE__[3]); \
+		*(m++) = (__func__[0] << 24) + (__func__[1] << 16) + (__func__[2] << 8) + (__func__[3]); \
+		*m = __LINE__; \
+	} while (0)
+
+#define dump(addr, count) \
+	do { \
+		volatile uint32_t *m = (uint32_t*)mailbox_get_debug_base(); \
+		volatile uint32_t *a = (uint32_t*)addr; \
+		volatile int c = count; \
+		while (c--) \
+			*(m++) = *(a++); \
+	} while (0);
 
 #endif
