bb51e165f671fa87d91d06f838f67fee5fbea8a1
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2015-08-29 22:41:15 +0100

    debug: improve output
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/include/reef/debug.h
+++ src/include/reef/debug.h
@@ -14,11 +14,18 @@
 
 #define dbg() \
 	do { \
-		uint32_t *m = (uint32_t*)mailbox_get_debug_base(); \
+		volatile uint32_t *m = (uint32_t*)mailbox_get_debug_base(); \
 		*(m++) = (__FILE__[0] << 24) + (__FILE__[1] << 16) + (__FILE__[2] << 8) + (__FILE__[3]); \
 		*(m++) = (__func__[0] << 24) + (__func__[1] << 16) + (__func__[2] << 8) + (__func__[3]); \
 		*m = __LINE__; \
 	} while (0)
+
+#define dbg_val(val) \
+	do { \
+		volatile uint32_t *m = (uint32_t*)mailbox_get_debug_base(); \
+		*m = val; \
+	} while (0)
+
 
 #define dump(addr, count) \
 	do { \
