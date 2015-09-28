368c09abef5a4cf0254a3ff242262866a384d4b6
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2015-09-28 13:49:14 +0100

    debug: add panic macro
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/include/reef/debug.h
+++ src/include/reef/debug.h
@@ -15,16 +15,19 @@
 #define dbg() \
 	do { \
 		volatile uint32_t *m = (uint32_t*)mailbox_get_debug_base(); \
-		*(m++) = (__FILE__[0] << 24) + (__FILE__[1] << 16) + (__FILE__[2] << 8) + (__FILE__[3]); \
-		*(m++) = (__func__[0] << 24) + (__func__[1] << 16) + (__func__[2] << 8) + (__func__[3]); \
+		*(m++) = (__FILE__[0] << 24) + (__FILE__[1] << 16) +\
+			 (__FILE__[2] << 8) + (__FILE__[3]); \
+		*(m++) = (__func__[0] << 24) + (__func__[1] << 16) + \
+			(__func__[2] << 8) + (__func__[3]); \
 		*m = __LINE__; \
-	} while (0)
+	} while (0);
 
 #define dbg_val(_v) \
 	do { \
-		volatile uint32_t *_m = (volatile uint32_t*)mailbox_get_debug_base(); \
+		volatile uint32_t *_m = \
+			(volatile uint32_t*)mailbox_get_debug_base(); \
 		*_m = _v; \
-	} while (0)
+	} while (0);
 
 
 #define dump(addr, count) \
@@ -35,5 +38,10 @@
 		while (c--) \
 			*(m++) = *(a++); \
 	} while (0);
+
+#define panic(_p) \
+	dbg() \
+	dbg_val(_p) \
+	*((uint32_t*)0) = 0;
 
 #endif
