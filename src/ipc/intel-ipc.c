668f6deda46e161f75117650e8135b508ca737b8
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2015-09-30 18:41:42 +0100

    ipc: dispatch commands
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/ipc/intel-ipc.c
+++ src/ipc/intel-ipc.c
@@ -20,108 +20,82 @@
 #include <platform/shim.h>
 #include <uapi/intel-ipc.h>
 
-static int ipc = 1000;
-
-#if 0
-static int hsw_process_reply(struct sst_hsw *hsw, u32 header)
-{
-	struct ipc_message *msg;
-	u32 reply = msg_get_global_reply(header);
-
-	trace_ipc_reply("processing -->", header);
-
-	msg = sst_ipc_reply_find_msg(&hsw->ipc, header);
-	if (msg == NULL) {
-		trace_ipc_error("error: can't find message header", header);
-		return -EIO;
-	}
-
-	/* first process the header */
-	switch (reply) {
-	case IPC_INTEL_GLB_REPLY_PENDING:
-		trace_ipc_pending_reply("received", header);
-		msg->pending = true;
-		hsw->ipc.pending = true;
-		return 1;
-	case IPC_INTEL_GLB_REPLY_SUCCESS:
-		if (msg->pending) {
-			trace_ipc_pending_reply("completed", header);
-			sst_dsp_inbox_read(hsw->dsp, msg->rx_data,
-				msg->rx_size);
-			hsw->ipc.pending = false;
-		} else {
-			/* copy data from the DSP */
-			sst_dsp_outbox_read(hsw->dsp, msg->rx_data,
-				msg->rx_size);
-		}
-		break;
-	/* these will be rare - but useful for debug */
-	case IPC_INTEL_GLB_REPLY_UNKNOWN_MESSAGE_TYPE:
-		trace_ipc_error("error: unknown message type", header);
-		msg->errno = -EBADMSG;
-		break;
-	case IPC_INTEL_GLB_REPLY_OUT_OF_RESOURCES:
-		trace_ipc_error("error: out of resources", header);
-		msg->errno = -ENOMEM;
-		break;
-	case IPC_INTEL_GLB_REPLY_BUSY:
-		trace_ipc_error("error: reply busy", header);
-		msg->errno = -EBUSY;
-		break;
-	case IPC_INTEL_GLB_REPLY_FAILURE:
-		trace_ipc_error("error: reply failure", header);
-		msg->errno = -EINVAL;
-		break;
-	case IPC_INTEL_GLB_REPLY_STAGE_UNINITIALIZED:
-		trace_ipc_error("error: stage uninitialized", header);
-		msg->errno = -EINVAL;
-		break;
-	case IPC_INTEL_GLB_REPLY_NOT_FOUND:
-		trace_ipc_error("error: reply not found", header);
-		msg->errno = -EINVAL;
-		break;
-	case IPC_INTEL_GLB_REPLY_SOURCE_NOT_STARTED:
-		trace_ipc_error("error: source not started", header);
-		msg->errno = -EINVAL;
-		break;
-	case IPC_INTEL_GLB_REPLY_INVALID_REQUEST:
-		trace_ipc_error("error: invalid request", header);
-		msg->errno = -EINVAL;
-		break;
-	case IPC_INTEL_GLB_REPLY_ERROR_INVALID_PARAM:
-		trace_ipc_error("error: invalid parameter", header);
-		msg->errno = -EINVAL;
-		break;
-	default:
-		trace_ipc_error("error: unknown reply", header);
-		msg->errno = -EINVAL;
-		break;
-	}
-
-	/* update any stream states */
-	if (msg_get_global_type(header) == IPC_INTEL_GLB_STREAM_MESSAGE)
-		hsw_stream_update(hsw, msg);
-
-	/* wake up and return the error if we have waiters on this message ? */
-	list_del(&msg->list);
-	sst_ipc_tx_msg_reply_complete(&hsw->ipc, msg);
-
-	return 1;
-}
-
-#endif
-
-static void ipc_get_fw_version(void)
-{
-
-}
-
 static inline uint32_t msg_get_global_type(uint32_t msg)
 {
 	return (msg & IPC_INTEL_GLB_TYPE_MASK) >> IPC_INTEL_GLB_TYPE_SHIFT;
 }
 
-static void ipc_cmd(void)
+static uint32_t ipc_fw_version(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_fw_caps(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_stream_alloc(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_stream_free(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_stream_info(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_dump(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_device_get_formats(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_device_set_formats(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_context_save(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_context_restore(uint32_t header)
+{
+	dbg();
+
+	return IPC_INTEL_GLB_REPLY_SUCCESS;
+}
+
+static uint32_t ipc_cmd(void)
 {
 	uint32_t type, header;
 
@@ -130,49 +104,41 @@ static void ipc_cmd(void)
 
 	switch (type) {
 	case IPC_INTEL_GLB_GET_FW_VERSION:
-		ipc_get_fw_version();
-		break;
+		return ipc_fw_version(header);
 	case IPC_INTEL_GLB_ALLOCATE_STREAM:
+		return ipc_stream_alloc(header);
 	case IPC_INTEL_GLB_FREE_STREAM:
+		return ipc_stream_free(header);
 	case IPC_INTEL_GLB_GET_FW_CAPABILITIES:
+		return ipc_fw_caps(header);
 	case IPC_INTEL_GLB_REQUEST_DUMP:
+		return ipc_dump(header);
 	case IPC_INTEL_GLB_GET_DEVICE_FORMATS:
+		return ipc_device_get_formats(header);
 	case IPC_INTEL_GLB_SET_DEVICE_FORMATS:
+		return ipc_device_set_formats(header);
 	case IPC_INTEL_GLB_ENTER_DX_STATE:
+		return ipc_context_save(header);
 	case IPC_INTEL_GLB_GET_MIXER_STREAM_INFO:
-	case IPC_INTEL_GLB_MAX_IPC_MESSAGE_TYPE:
+		return ipc_stream_info(header);
 	case IPC_INTEL_GLB_RESTORE_CONTEXT:
-	case IPC_INTEL_GLB_SHORT_REPLY:
-		dbg_val(type);
-		break;
-	case IPC_INTEL_GLB_STREAM_MESSAGE:
-//		handled = hsw_stream_message(hsw, header);
-		break;
-	case IPC_INTEL_GLB_DEBUG_LOG_MESSAGE:
-//		handled = hsw_log_message(hsw, header);
-		break;
-	case IPC_INTEL_GLB_MODULE_OPERATION:
-//		handled = hsw_module_message(hsw, header);
-		break;
+		return ipc_context_restore(header);
 	default:
-		
-		break;
+		return IPC_INTEL_GLB_REPLY_UNKNOWN_MESSAGE_TYPE;
 	}
 }
 
-
-
 static void do_cmd(void)
 {
-	uint32_t val;
+	uint32_t status, ipcx;
 	
-	ipc_cmd();
+	status = ipc_cmd();
 
 	/* clear BUSY bit and set DONE bit - accept new messages */
-	val = shim_read(SHIM_IPCX);
-	val &= ~SHIM_IPCX_BUSY;
-	val |= SHIM_IPCX_DONE;
-	shim_write(SHIM_IPCX, val);
+	ipcx = shim_read(SHIM_IPCX);
+	ipcx &= ~SHIM_IPCX_BUSY;
+	ipcx |= SHIM_IPCX_DONE | status;
+	shim_write(SHIM_IPCX, ipcx);
 
 	/* unmask busy interrupt */
 	shim_write(SHIM_IMRD, shim_read(SHIM_IMRD) & ~SHIM_IMRD_BUSY);
@@ -193,9 +159,6 @@ static void do_notify(void)
 static void irq_handler(void *arg)
 {
 	uint32_t isr;
-
-	ipc++;
-	dbg_val(ipc);
 
 	/* Interrupt arrived, check src */
 	isr = shim_read(SHIM_ISRD);
