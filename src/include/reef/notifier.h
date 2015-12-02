32f3fcf58687d314b02f7a596aa621eff0e73414
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2015-12-02 18:30:02 +0000

    notfier: add initial notifier code
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/include/reef/notifier.h
+++ src/include/reef/notifier.h
@@ -12,9 +12,15 @@
 #include <reef/list.h>
 
 struct notifier {
+	int id;
 	struct list_head list;
 	void *cb_data;
 	void (*cb)(int message, void *cb);
 };
+
+void notifier_register(struct notifier *notifier);
+void notifier_unregister(struct notifier *notifier);
+
+void notifier_event(int id, int message);
 
 #endif
---
+++ src/lib/notifier.c
@@ -0,0 +1,23 @@
+/*
+ * BSD 3 Clause - See LICENCE file for details.
+ *
+ * Copyright (c) 2015, Intel Corporation
+ * All rights reserved.
+ *
+ */
+
+#include <reef/notifier.h>
+#include <reef/reef.h>
+
+void notifier_register(struct notifier *notifier)
+{
+}
+
+void notifier_unregister(struct notifier *notifier)
+{
+}
+
+void notifier_event(int id, int message)
+{
+
+}
