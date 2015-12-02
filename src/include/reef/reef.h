43f2749f41d35b49abc3c25002115436837437fe
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2015-12-02 17:40:23 +0000

    reef: Add container_of() macro
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/include/reef/reef.h
+++ src/include/reef/reef.h
@@ -10,9 +10,16 @@
 #define __INCLUDE_REEF_REEF__
 
 #include <stdint.h>
+#include <stddef.h>
 #include <arch/reef.h>
 
+#define MAX_INT		0xffffffff
+
+/* use same syntax as Linux for simplicity */
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+#define container_of(ptr, type, member) \
+	({const typeof(((type *)0)->member) *__memberptr = (ptr); \
+	(type *)((char *)__memberptr - offsetof(type, member));})
 
 /* C memcpy for arch that dont have arch_memcpy() */
 void cmemcpy(void *dest, void *src, size_t size);
