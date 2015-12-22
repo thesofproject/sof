89f2574a583b37fc3e1ff37c367abc805bb32aa1
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2015-12-22 13:50:01 +0000

    wait: add wait_for_completion()
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/include/reef/wait.h
+++ src/include/reef/wait.h
@@ -11,4 +11,14 @@
 
 void wait_for_interrupt(int level);
 
+/* simple interrupt based wait for completion */
+static inline void wait_for_completion(int *complete)
+{
+	volatile int *c = complete;
+
+	/* check for completion after every wake from IRQ */
+	while (*c == 0)
+		wait_for_interrupt(0);
+}
+
 #endif
