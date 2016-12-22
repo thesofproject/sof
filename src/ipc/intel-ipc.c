cff154ca87ae44fa2409563e950ce69091f2467a
Keyon Jie <yang.jie@linux.intel.com>
2016-12-22 17:27:58 +0800

    ssp: switch dai format form PCM B mode to normal I2S mode
    it is configured from host/codec side that using I2S mode, so
    switch it.

--- src/ipc/intel-ipc.c
+++ src/ipc/intel-ipc.c
@@ -561,7 +561,7 @@ static uint32_t ipc_device_set_formats(uint32_t header)
 
 	/* setup the DAI HW config - TODO hard coded due to IPC limitations */
 	dai_dev->dai_config.mclk = config_req.clock_frequency;
-	dai_dev->dai_config.format = DAI_FMT_DSP_B | DAI_FMT_CONT |
+	dai_dev->dai_config.format = DAI_FMT_I2S | DAI_FMT_CONT |
 		DAI_FMT_NB_NF | DAI_FMT_CBS_CFS;
 	dai_dev->dai_config.frame_size = 32;	/* TODO 16bit stereo hard coded */
 	dai_dev->dai_config.bclk_fs = 32;	/* 32 BCLKs per frame - */
