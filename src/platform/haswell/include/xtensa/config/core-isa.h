21839fc0286f9e4cb2721bc4ae961e6eaf29f211
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2018-01-21 16:48:51 +0000

    platform: hsw: Add xtensa headers for haswell and broadwell
    Add xtensa headers for Haswell and Broadwell.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

---
+++ src/platform/haswell/include/xtensa/config/core-isa.h
@@ -0,0 +1,9 @@
+#include <config.h>
+
+#ifdef CONFIG_HASWELL
+#include <xtensa/config/core-isa-hsw.h>
+#elif CONFIG_BROADWELL
+#include <xtensa/config/core-isa-bdw.h>
+#else
+#error "No ISA configuration selected"
+#endif
