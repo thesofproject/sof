05df022020d85d0a223c008ec6a3dd398160ba0b
Keyon Jie <yang.jie@linux.intel.com>
2018-01-23 14:44:28 +0000

    platform: apl: Add platform drivers for apollolake.
    Add platform initialisation and drivers for Intel Apollolake platform.
    
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

---
+++ src/platform/apollolake/dai.c
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2016, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ */
+
+#include <reef/reef.h>
+#include <reef/dai.h>
+#include <reef/ssp.h>
+#include <reef/stream.h>
+#include <reef/audio/component.h>
+#include <platform/memory.h>
+#include <platform/interrupt.h>
+#include <platform/dma.h>
+#include <stdint.h>
+#include <string.h>
+#include <config.h>
+
+static struct dai ssp[6] = {
+{
+	.type = SOF_DAI_INTEL_SSP,
+	.index = 0,
+	.plat_data = {
+		.base		= SSP_BASE(0),
+		.irq		= IRQ_EXT_SSP0_LVL5(0),
+		.fifo[SOF_IPC_STREAM_PLAYBACK] = {
+			.offset		= SSP_BASE(0) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP0_TX,
+		},
+		.fifo[SOF_IPC_STREAM_CAPTURE] = {
+			.offset		= SSP_BASE(0) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP0_RX,
+		}
+	},
+	.ops		= &ssp_ops,
+},
+{
+	.type = SOF_DAI_INTEL_SSP,
+	.index = 1,
+	.plat_data = {
+		.base		= SSP_BASE(1),
+		.irq		= IRQ_EXT_SSP1_LVL5(0),
+		.fifo[SOF_IPC_STREAM_PLAYBACK] = {
+			.offset		= SSP_BASE(1) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP1_TX,
+		},
+		.fifo[SOF_IPC_STREAM_CAPTURE] = {
+			.offset		= SSP_BASE(1) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP1_RX,
+		}
+	},
+	.ops		= &ssp_ops,
+},
+{
+	.type = SOF_DAI_INTEL_SSP,
+	.index = 2,
+	.plat_data = {
+		.base		= SSP_BASE(2),
+		.irq		= IRQ_EXT_SSP2_LVL5(0),
+		.fifo[SOF_IPC_STREAM_PLAYBACK] = {
+			.offset		= SSP_BASE(2) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP2_TX,
+		},
+		.fifo[SOF_IPC_STREAM_CAPTURE] = {
+			.offset		= SSP_BASE(2) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP2_RX,
+		}
+	},
+	.ops		= &ssp_ops,
+},
+{
+	.type = SOF_DAI_INTEL_SSP,
+	.index = 3,
+	.plat_data = {
+		.base		= SSP_BASE(3),
+		.irq		= IRQ_EXT_SSP3_LVL5(0),
+		.fifo[SOF_IPC_STREAM_PLAYBACK] = {
+			.offset		= SSP_BASE(3) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP3_TX,
+		},
+		.fifo[SOF_IPC_STREAM_CAPTURE] = {
+			.offset		= SSP_BASE(3) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP3_RX,
+		}
+	},
+	.ops		= &ssp_ops,
+},
+{
+	.type = SOF_DAI_INTEL_SSP,
+	.index = 4,
+	.plat_data = {
+		.base		= SSP_BASE(4),
+		.irq		= IRQ_EXT_SSP4_LVL5(0),
+		.fifo[SOF_IPC_STREAM_PLAYBACK] = {
+			.offset 	= SSP_BASE(4) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP4_TX,
+		},
+		.fifo[SOF_IPC_STREAM_CAPTURE] = {
+			.offset 	= SSP_BASE(4) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP4_RX,
+		}
+	},
+	.ops		= &ssp_ops,
+},
+{
+	.type = SOF_DAI_INTEL_SSP,
+	.index = 5,
+	.plat_data = {
+		.base		= SSP_BASE(5),
+		.irq		= IRQ_EXT_SSP5_LVL5(0),
+		.fifo[SOF_IPC_STREAM_PLAYBACK] = {
+			.offset 	= SSP_BASE(5) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP5_TX,
+		},
+		.fifo[SOF_IPC_STREAM_CAPTURE] = {
+			.offset 	= SSP_BASE(5) + SSDR,
+			.handshake	= DMA_HANDSHAKE_SSP5_RX,
+		}
+	},
+	.ops		= &ssp_ops,
+},};
+
+struct dai *dai_get(uint32_t type, uint32_t index)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(ssp); i++) {
+		if (ssp[i].type == type && ssp[i].index == index)
+			return &ssp[i];
+	}
+
+	return NULL;
+}
---
+++ src/platform/apollolake/timer.c
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2016, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ */
+
+#include <platform/timer.h>
+#include <platform/shim.h>
+#include <reef/debug.h>
+#include <reef/audio/component.h>
+#include <stdint.h>
+
+void platform_timer_start(struct timer *timer)
+{
+	/* run timer */
+	shim_write64(SHIM_DSPWCT0C, 0);
+	shim_write(SHIM_DSPWCTCS,
+		shim_read(SHIM_DSPWCTCS) | SHIM_DSPWCTCS_T0A);
+}
+
+void platform_timer_stop(struct timer *timer)
+{
+	/* stop timer */
+	shim_write64(SHIM_DSPWCT0C, 0);
+	shim_write(SHIM_DSPWCTCS,
+		shim_read(SHIM_DSPWCTCS) & ~SHIM_DSPWCTCS_T0A);
+}
+
+int platform_timer_set(struct timer *timer, uint64_t ticks)
+{
+	/* a tick value of 0 will not generate an IRQ */
+	if (ticks == 0)
+		ticks = 1;
+
+	/* set new value and run */
+	shim_write64(SHIM_DSPWCT0C, (uint64_t)ticks);
+	shim_write(SHIM_DSPWCTCS,
+		shim_read(SHIM_DSPWCTCS) | SHIM_DSPWCTCS_T0A);
+
+	return 0;
+}
+
+void platform_timer_clear(struct timer *timer)
+{
+	/* write 1 to clear the timer interrupt */
+	shim_write(SHIM_DSPWCTCS,
+		shim_read(SHIM_DSPWCTCS) | SHIM_DSPWCTCS_T0T);
+}
+
+uint64_t platform_timer_get(struct timer *timer)
+{
+//	return arch_timer_get_system(timer);
+	return (uint64_t)shim_read64(SHIM_DSPWC);
+}
+
+/* get timestamp for host stream DMA position */
+void platform_host_timestamp(struct comp_dev *host,
+	struct sof_ipc_stream_posn *posn)
+{
+	int err;
+
+	/* get host postion */
+	err = comp_position(host, posn);
+	if (err == 0)
+		posn->flags |= SOF_TIME_HOST_VALID;
+}
+
+/* get timestamp for DAI stream DMA position */
+void platform_dai_timestamp(struct comp_dev *dai,
+	struct sof_ipc_stream_posn *posn)
+{
+	int err;
+
+	/* get DAI postion */
+	err = comp_position(dai, posn);
+	if (err == 0)
+		posn->flags |= SOF_TIME_DAI_VALID;
+
+	/* get SSP wallclock - DAI sets this to stream start value */
+	posn->wallclock = shim_read64(SHIM_DSPWC) - posn->wallclock;
+	posn->flags |= SOF_TIME_WALL_VALID;
+}
+
+/* get current wallclock for componnent */
+void platform_dai_wallclock(struct comp_dev *dai, uint64_t *wallclock)
+{
+	*wallclock = shim_read64(SHIM_DSPWC);
+}
+
+int platform_timer_register(struct timer *timer,
+	void (*handler)(void *arg), void *arg)
+{
+	return interrupt_register(timer->irq, handler, arg);
+}
+
+int timer_register(struct timer *timer, void(*handler)(void *arg), void *arg)
+{
+	switch (timer->id) {
+	case TIMER0:
+	case TIMER1:
+	case TIMER2:
+		return arch_timer_register(timer, handler, arg);
+	case TIMER3:
+		return platform_timer_register(timer, handler, arg);
+	default:
+		return -EINVAL;
+	}
+}
