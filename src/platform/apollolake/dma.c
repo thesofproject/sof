b6ab4a18e7e709acca40ed106b1e4e06b3b82039
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2018-02-08 20:48:10 +0800

    platform: apl: update dma platform data to use HDA driver
    Add the HDA driver to the DMA platform data and clean up alignment.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/platform/apollolake/dma.c
+++ src/platform/apollolake/dma.c
@@ -31,6 +31,7 @@
 
 #include <reef/dma.h>
 #include <reef/dw-dma.h>
+#include <reef/hda-dma.h>
 #include <platform/memory.h>
 #include <platform/interrupt.h>
 #include <platform/dma.h>
@@ -113,7 +114,7 @@ static struct dma dma[] = {
 		.id		= DMA_GP_LP_DMAC0,
 		.base		= LP_GP_DMA_BASE(0),
 		.channels	= 8,
-		.irq = IRQ_EXT_LP_GPDMA0_LVL5(0, 0),
+		.irq		= IRQ_EXT_LP_GPDMA0_LVL5(0, 0),
 		.drv_plat_data	= &dmac0,
 	},
 	.ops		= &dw_dma_ops,
@@ -123,7 +124,7 @@ static struct dma dma[] = {
 		.id		= DMA_GP_LP_DMAC1,
 		.base		= LP_GP_DMA_BASE(1),
 		.channels	= 8,
-		.irq = IRQ_EXT_LP_GPDMA1_LVL5(0, 0),
+		.irq		= IRQ_EXT_LP_GPDMA1_LVL5(0, 0),
 		.drv_plat_data	= &dmac1,
 	},
 	.ops		= &dw_dma_ops,
@@ -133,32 +134,40 @@ static struct dma dma[] = {
 		.id		= DMA_HOST_IN_DMAC,
 		.base		= GTW_HOST_IN_STREAM_BASE(0),
 		.channels	= 7,
-		.irq = IRQ_EXT_HOST_DMA_IN_LVL3(0, 0),
+		.irq		= IRQ_EXT_HOST_DMA_IN_LVL3(0, 0),
+		.chan_size	= GTW_HOST_IN_STREAM_SIZE,
 	},
+	.ops		= &hda_host_dma_ops,
 },
 {	/* Host out DMAC */
 	.plat_data = {
 		.id		= DMA_HOST_OUT_DMAC,
 		.base		= GTW_HOST_OUT_STREAM_BASE(0),
 		.channels	= 6,
-		.irq = IRQ_EXT_HOST_DMA_OUT_LVL3(0, 0),
+		.irq		= IRQ_EXT_HOST_DMA_OUT_LVL3(0, 0),
+		.chan_size	= GTW_HOST_OUT_STREAM_SIZE,
 	},
+	.ops		= &hda_host_dma_ops,
 },
 {	/* Link In DMAC */
 	.plat_data = {
 		.id		= DMA_LINK_IN_DMAC,
 		.base		= GTW_LINK_IN_STREAM_BASE(0),
 		.channels	= 8,
-		.irq = IRQ_EXT_LINK_DMA_IN_LVL4(0, 0),
+		.irq		= IRQ_EXT_LINK_DMA_IN_LVL4(0, 0),
+		.chan_size	= GTW_LINK_IN_STREAM_SIZE,
 	},
+	.ops		= &hda_link_dma_ops,
 },
 {	/* Link out DMAC */
 	.plat_data = {
 		.id		= DMA_LINK_OUT_DMAC,
 		.base		= GTW_LINK_OUT_STREAM_BASE(0),
 		.channels	= 8,
-		.irq = IRQ_EXT_LINK_DMA_OUT_LVL4(0, 0),
+		.irq		= IRQ_EXT_LINK_DMA_OUT_LVL4(0, 0),
+		.chan_size	= GTW_LINK_OUT_STREAM_SIZE,
 	},
+	.ops		= &hda_link_dma_ops,
 },};
 
 struct dma *dma_get(int dmac_id)
