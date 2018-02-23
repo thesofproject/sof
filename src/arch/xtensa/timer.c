bd0100167431d76adc9dc7f77504dbd3c5c227d2
Keyon Jie <yang.jie@linux.intel.com>
2018-02-23 16:56:10 +0800

    timer: fix external timer doesn't work issue for APL/CNL
    On some platforms(e.g. APL, CNL), the external timer(wall clock)
    uses the external interrupt bits, here change the common timer
    logic to call interrupt APIs, which give a chance to handle for
    different cases.
    
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

--- src/arch/xtensa/timer.c
+++ src/arch/xtensa/timer.c
@@ -217,3 +217,19 @@ int arch_timer_set(struct timer *timer, uint64_t ticks)
 	arch_interrupt_global_enable(flags);
 	return 0;
 }
+
+void timer_unregister(struct timer *timer)
+{
+	interrupt_unregister(timer->irq);
+}
+
+void timer_enable(struct timer *timer)
+{
+	interrupt_enable(timer->irq);
+}
+
+void timer_disable(struct timer *timer)
+{
+	interrupt_disable(timer->irq);
+}
+
