b99d1de979366a071aef8f7b4aff02e7e88003f6
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2018-03-05 13:58:28 -0800

    topology: add a test hostless tone pipeline
    This patch adds changes to create a test hostless tone pipeline.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- topology/test/tplg-build.sh
+++ topology/test/tplg-build.sh
@@ -13,6 +13,8 @@ M4_FLAGS="-I ../ -I ../m4 -I ../common"
 # Simple component test cases
 # can be used on components with 1 sink and 1 source.
 SIMPLE_TESTS=(test-ssp test-capture-ssp test-playback-ssp)
+TONE_TEST=test-tone-playback-ssp
+
 
 # process m4 simple tests -
 # simple_test(name, pipe_name, be_name, format, dai_id, dai_format, dai_phy_bits, dai_data_bits dai_bclk)
@@ -23,7 +25,7 @@ SIMPLE_TESTS=(test-ssp test-capture-ssp test-playback-ssp)
 # 5) dai_id - SSP port number
 # 6) dai_format - SSP sample format
 # 7) dai_phy_bits - SSP physical number of BLKCs per slot/channel
-# 8) dai_data_bits - SSP number of valid daat bits per slot/channel
+# 8) dai_data_bits - SSP number of valid data bits per slot/channel
 # 9) dai_bclk - SSP BCLK in HZ
 # 10) dai_mclk - SSP MCLK in HZ
 #
@@ -46,6 +48,37 @@ function simple_test {
 		echo "Compiling test $i -> ${TFILE}.tplg"
 		alsatplg -v 1 -c ${TFILE}.conf -o ${TFILE}.tplg
 	done
+}
+
+# process m4 tone test -
+# tone_test(name, pipe_name, be_name, format, dai_id, dai_format, dai_phy_bits, dai_data_bits dai_bclk)
+# 1) name - test filename suffix
+# 2) pipe_name - test component pipeline filename in sof/
+# 3) be_name - BE DAI link name in machine driver, used for matching
+# 4) format - PCM sample format
+# 5) dai_id - SSP port number
+# 6) dai_format - SSP sample format
+# 7) dai_phy_bits - SSP physical number of BLKCs per slot/channel
+# 8) dai_data_bits - SSP number of valid data bits per slot/channel
+# 9) dai_bclk - SSP BCLK in HZ
+# 10) dai_mclk - SSP MCLK in HZ
+#
+function tone_test {
+	TFILE="$TONE_TEST$5-$2-$4-$6-48k-$1"
+	echo "M4 pre-processing test $TONE_TEST -> ${TFILE}"
+	m4 ${M4_FLAGS} \
+		-DTEST_PIPE_NAME="$2" \
+		-DTEST_DAI_LINK_NAME="$3" \
+		-DTEST_SSP_PORT=$5 \
+		-DTEST_SSP_FORMAT=$6 \
+		-DTEST_PIPE_FORMAT=$4 \
+		-DTEST_SSP_BCLK=$9 \
+		-DTEST_SSP_MCLK=${10} \
+		-DTEST_SSP_PHY_BITS=$7 \
+		-DTEST_SSP_DATA_BITS=$8 \
+		$TONE_TEST.m4 > ${TFILE}.conf
+	echo "Compiling test $TONE_TEST -> ${TFILE}.tplg"
+	alsatplg -v 1 -c ${TFILE}.conf -o ${TFILE}.tplg
 }
 
 # Pre-process the simple tests
@@ -73,5 +106,8 @@ simple_test baytrail src "Baytrail Audio" s24le 2 s24le 25 24 2400000 19200000
 # for APL
 simple_test nocodec volume "NoCodec" s16le 4 s16le 16 16 1536000 24576000
 simple_test codec volume "SSP4-Codec" s16le 4 s16le 16 16 1536000 24576000
+
+# Tone test: Tone component only supports s32le currently
+tone_test codec tone "SSP2-Codec" s32le 2 s16le 20 16 1920000 19200000
 
 
