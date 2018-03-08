4ed28999bd13c944654bb35f56a3f51fd00f03dd
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2018-03-08 16:22:19 +0200

    SRC: Updated 16 bit coefficients
    Update due to need for subfilter lengths those are multiple of four.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/include/reef/audio/coefficients/src/src_tiny_int16_table.h
+++ src/include/reef/audio/coefficients/src/src_tiny_int16_table.h
@@ -1,16 +1,16 @@
 /* SRC conversions */
-#include <reef/audio/coefficients/src/src_tiny_int16_1_2_3750_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_1_3_1875_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_1_3_3750_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_2_1_3750_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_2_3_3750_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_3_1_1875_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_3_1_3750_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_3_2_3750_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_7_8_3750_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_8_7_3750_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_20_21_3445_5100.h>
-#include <reef/audio/coefficients/src/src_tiny_int16_21_20_3445_5100.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_1_2_3281_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_1_3_1641_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_1_3_3281_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_2_1_3281_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_2_3_3281_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_3_1_1641_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_3_1_3281_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_3_2_3281_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_7_8_3281_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_8_7_3281_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_20_21_3015_5000.h>
+#include <reef/audio/coefficients/src/src_tiny_int16_21_20_3015_5000.h>
 
 /* SRC table */
 int16_t fir_one = 16384;
@@ -21,33 +21,33 @@ int src_out_fs[6] = { 8000, 16000, 24000, 32000, 44100, 48000};
 struct src_stage *src_table1[6][6] = {
 	{ &src_int16_1_1_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_0_0_0_0, &src_int16_1_3_1875_5100
+	 &src_int16_0_0_0_0, &src_int16_1_3_1641_5000
 	},
 	{ &src_int16_0_0_0_0, &src_int16_1_1_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_0_0_0_0, &src_int16_1_3_3750_5100
+	 &src_int16_0_0_0_0, &src_int16_1_3_3281_5000
 	},
 	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
 	 &src_int16_1_1_0_0, &src_int16_0_0_0_0,
-	 &src_int16_0_0_0_0, &src_int16_1_2_3750_5100
+	 &src_int16_0_0_0_0, &src_int16_1_2_3281_5000
 	},
 	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_1_1_0_0,
-	 &src_int16_0_0_0_0, &src_int16_2_3_3750_5100
+	 &src_int16_0_0_0_0, &src_int16_2_3_3281_5000
 	},
 	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_1_1_0_0, &src_int16_21_20_3445_5100
+	 &src_int16_1_1_0_0, &src_int16_21_20_3015_5000
 	},
-	{ &src_int16_2_1_3750_5100, &src_int16_3_1_3750_5100,
-	 &src_int16_2_1_3750_5100, &src_int16_3_2_3750_5100,
-	 &src_int16_8_7_3750_5100, &src_int16_1_1_0_0
+	{ &src_int16_2_1_3281_5000, &src_int16_3_1_3281_5000,
+	 &src_int16_2_1_3281_5000, &src_int16_3_2_3281_5000,
+	 &src_int16_8_7_3281_5000, &src_int16_1_1_0_0
 	}
 };
 struct src_stage *src_table2[6][6] = {
 	{ &src_int16_1_1_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_0_0_0_0, &src_int16_1_2_3750_5100
+	 &src_int16_0_0_0_0, &src_int16_1_2_3281_5000
 	},
 	{ &src_int16_0_0_0_0, &src_int16_1_1_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
@@ -63,10 +63,10 @@ struct src_stage *src_table2[6][6] = {
 	},
 	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_1_1_0_0, &src_int16_7_8_3750_5100
+	 &src_int16_1_1_0_0, &src_int16_7_8_3281_5000
 	},
-	{ &src_int16_3_1_1875_5100, &src_int16_1_1_0_0,
+	{ &src_int16_3_1_1641_5000, &src_int16_1_1_0_0,
 	 &src_int16_1_1_0_0, &src_int16_1_1_0_0,
-	 &src_int16_20_21_3445_5100, &src_int16_1_1_0_0
+	 &src_int16_20_21_3015_5000, &src_int16_1_1_0_0
 	}
 };
