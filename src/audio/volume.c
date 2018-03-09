thesofproject/sof

commit 43e86cab6088fe10db903c46b58a4c9b0456810a

volume: fix logic for volume mute/unmute
This patch fixes the volume mute/unmute logic in volume
to handle cases when volume is already muted/unmuted

Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

src/audio/volume.c
@@ -440,7 +440,9 @@ static inline void volume_set_chan_mute(struct comp_dev *dev, int chan)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
 
-	cd->mvolume[chan] = cd->volume[chan];
+	/* Check if not muted already */
+	if (cd->volume[chan] != 0)
+		cd->mvolume[chan] = cd->volume[chan];
 	cd->tvolume[chan] = 0;
 }
 
@@ -448,7 +450,9 @@ static inline void volume_set_chan_unmute(struct comp_dev *dev, int chan)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
 
-	cd->tvolume[chan] = cd->mvolume[chan];
+	/* Check if muted */
+	if (cd->volume[chan] == 0)
+		cd->tvolume[chan] = cd->mvolume[chan];
 }
 
 static int volume_ctrl_set_cmd(struct comp_dev *dev, struct sof_ipc_ctrl_data *cdata)
@@ -521,7 +525,7 @@ static int volume_ctrl_get_cmd(struct comp_dev *dev, struct sof_ipc_ctrl_data *c
 		return -EINVAL;
 	}
 
-	if (cdata->cmd == SOF_CTRL_CMD_VOLUME) {
+	if (cdata->cmd == SOF_CTRL_CMD_VOLUME || SOF_CTRL_CMD_SWITCH) {
 		trace_volume("vgt");
 		trace_value(cdata->comp_id);
 		for (j = 0; j < cdata->num_elems; j++) {

commit 666770a15990edb903bdf5c015960ebae27de9a7

volume: fix error in logical operations
Detected with sonarcloud: test evaluates as always true.
Make sure both volume and switch are handled correctly

Fixes: 43e86cab608 ('volume: fix logic for volume mute/unmute')
Cc: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

src/audio/volume.c
@@ -525,7 +525,8 @@ static int volume_ctrl_get_cmd(struct comp_dev *dev, struct sof_ipc_ctrl_data *c
 		return -EINVAL;
 	}
 
-	if (cdata->cmd == SOF_CTRL_CMD_VOLUME || SOF_CTRL_CMD_SWITCH) {
+	if (cdata->cmd == SOF_CTRL_CMD_VOLUME ||
+	    cdata->cmd ==  SOF_CTRL_CMD_SWITCH) {
 		trace_volume("vgt");
 		trace_value(cdata->comp_id);
 		for (j = 0; j < cdata->num_elems; j++) {