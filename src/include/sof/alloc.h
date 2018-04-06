5d7ad56d84eeca39a437cda6f850b178a88eaff7
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2018-04-06 16:23:43 +0100

    lib: string: Add rstrcmp() string compare.
    Add strcmp() equivalent.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/include/sof/alloc.h
+++ src/include/sof/alloc.h
@@ -113,6 +113,7 @@ void *rballoc(int zone, uint32_t flags, size_t bytes);
 void bzero(void *s, size_t n);
 void *memset(void *s, int c, size_t n);
 int rstrlen(const char *s);
+int rstrcmp(const char *s1, const char *s2);
 
 /* Heap save/restore contents and context for PM D0/D3 events */
 uint32_t mm_pm_context_size(void);
--- src/lib/lib.c
+++ src/lib/lib.c
@@ -106,3 +106,20 @@ int rstrlen(const char *s)
 	while(*p++ != 0);
 	return (p - s) - 1;
 }
+
+/* generic string compare */
+int rstrcmp(const char *s1, const char *s2)
+{
+	while (*s1 != 0 && *s2 != 0) {
+		if (*s1 < *s2)
+			return -1;
+		if (*s1 > *s2)
+			return 1;
+		s1++;
+		s2++;
+	}
+
+	/* match */
+	return 0;
+}
+
