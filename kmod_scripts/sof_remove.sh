b327539e98f1c84e3a131dd048460189a5de8c26
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-04-13 19:43:27 -0500

    kmod_scripts: handle nocodec machine driver
    make sure it's removed
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- kmod_scripts/sof_remove.sh
+++ kmod_scripts/sof_remove.sh
@@ -16,6 +16,7 @@ rmmod snd_soc_sst_bytcr_rt5651
 rmmod snd_soc_sst_cht_bsw_rt5645
 rmmod snd_soc_sst_cht_bsw_rt5670
 rmmod snd_soc_sst_byt_cht_da7213
+rmmod snd_sof_nocodec
 
 rmmod snd_soc_rt5670
 rmmod snd_soc_rt5645
