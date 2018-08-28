ffd10a1f04fcc6f2ce7a6633e6cf729da7828ecf
Pan Xiuli <xiuli.pan@linux.intel.com>
2018-08-28 18:19:31 +0800

    ipc: use scheduler task for IPC process
    IPC process is handled after wait_for_interrupt, but there may some IPC
    is handled outside this WFI function. Use scheduler to avoid wait for
    IRQ caused IPC timeout.
    
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

--- src/include/sof/ipc.h
+++ src/include/sof/ipc.h
@@ -106,6 +106,9 @@ struct ipc {
 	/* mmap for posn_offset */
 	struct pipeline *posn_map[PLATFORM_MAX_STREAMS];
 
+	/* processing task */
+	struct task ipc_task;
+
 	void *private;
 };
 
@@ -120,6 +123,8 @@ int platform_ipc_init(struct ipc *ipc);
 void ipc_free(struct ipc *ipc);
 
 int ipc_process_msg_queue(void);
+void ipc_process_task(void *data);
+void ipc_schedule_process(struct ipc *ipc);
 
 int ipc_stream_send_position(struct comp_dev *cdev,
 		struct sof_ipc_stream_posn *posn);
