thesofproject/sof

commit b5308ee8e6eff51b0a2fea5a39a88d0de10edcfd

EQ FIR: memory: Add trace error for alloc fail and add a 1024 size buffer
This patch adds a single 1024 bytes buffer into heap for e.g. rzalloc()
usage. The FIR EQ code didn't trace the allocation failure so it's added
to see better if this happens. The 1k allocation for PCM samples delay
line is quite typical since a 32 bit 100 tap stereo FIR will need 800
bytes.

Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

src/platform/apollolake/include/platform/memory.h
@@ -136,6 +136,7 @@
 #define HEAP_RT_COUNT128		32
 #define HEAP_RT_COUNT256		64
 #define HEAP_RT_COUNT512		32
+#define HEAP_RT_COUNT1024		1
 
 #define L2_VECTOR_SIZE			0x1000
 

src/platform/cannonlake/include/platform/memory.h
@@ -176,6 +176,7 @@
 #define HEAP_RT_COUNT128		32
 #define HEAP_RT_COUNT256		64
 #define HEAP_RT_COUNT512		32
+#define HEAP_RT_COUNT1024		1
 
 #define L2_VECTOR_SIZE		0x1000
 

src/platform/icelake/include/platform/memory.h
@@ -176,6 +176,7 @@
 #define HEAP_RT_COUNT128		32
 #define HEAP_RT_COUNT256		64
 #define HEAP_RT_COUNT512		32
+#define HEAP_RT_COUNT1024		1
 
 #define L2_VECTOR_SIZE		0x1000
 

src/platform/suecreek/include/platform/memory.h
@@ -164,6 +164,7 @@
 #define HEAP_RT_COUNT128		32
 #define HEAP_RT_COUNT256		64
 #define HEAP_RT_COUNT512		32
+#define HEAP_RT_COUNT1024		1
 
 #define L2_VECTOR_SIZE		0x2000
 

src/audio/eq_fir.c
@@ -204,8 +204,11 @@ static int eq_fir_setup(struct comp_data *cd, int nch)
 
 	/* Allocate all FIR channels data in a big chunk and clear it */
 	cd->fir_delay = rzalloc(RZONE_RUNTIME, SOF_MEM_CAPS_RAM, size_sum);
-	if (!cd->fir_delay)
+	if (!cd->fir_delay) {
+		trace_eq_error("eda");
+		trace_value(size_sum);
 		return -ENOMEM;
+	}
 
 	/* Initialize 2nd phase to set EQ delay lines pointers */
 	fir_delay = cd->fir_delay;

src/platform/intel/cavs/memory.c
@@ -36,6 +36,7 @@ static struct block_hdr mod_block64[HEAP_RT_COUNT64];
 static struct block_hdr mod_block128[HEAP_RT_COUNT128];
 static struct block_hdr mod_block256[HEAP_RT_COUNT256];
 static struct block_hdr mod_block512[HEAP_RT_COUNT512];
+static struct block_hdr mod_block1024[HEAP_RT_COUNT1024];
 
 /* Heap memory map for modules */
 static struct block_map rt_heap_map[] = {
@@ -43,6 +44,7 @@ static struct block_map rt_heap_map[] = {
 	BLOCK_DEF(128, HEAP_RT_COUNT128, mod_block128),
 	BLOCK_DEF(256, HEAP_RT_COUNT256, mod_block256),
 	BLOCK_DEF(512, HEAP_RT_COUNT512, mod_block512),
+	BLOCK_DEF(1024, HEAP_RT_COUNT1024, mod_block1024),
 };
 
 /* Heap blocks for buffers */

commit 796596d89622aac5b049f2f5573bf73e8eeb6327

Memory: Fix bug in previous commit that introduced 1k buffers
This patch fixes a regression caused by commit
b5308ee8e6eff51b0a2fea5a39a88d0de10edcfd. The HEAP_RUNTIME_SIZE
calculation equation macro missed this buffer totally.

The number of 1k buffers is also increased to four to reach size
that is multiple of 4096 like other buffer pools. Non-4096 size
seems to cause DSP panic.

Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

src/platform/apollolake/include/platform/memory.h
@@ -136,7 +136,7 @@
 #define HEAP_RT_COUNT128		32
 #define HEAP_RT_COUNT256		64
 #define HEAP_RT_COUNT512		32
-#define HEAP_RT_COUNT1024		1
+#define HEAP_RT_COUNT1024		4
 
 #define L2_VECTOR_SIZE			0x1000
 
@@ -154,7 +154,8 @@
 #define HEAP_RUNTIME_BASE	(HEAP_SYSTEM_1_BASE + HEAP_SYSTEM_1_SIZE)
 #define HEAP_RUNTIME_SIZE \
 	(HEAP_RT_COUNT64 * 64 + HEAP_RT_COUNT128 * 128 + \
-	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512)
+	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512 + \
+	HEAP_RT_COUNT1024 * 1024)
 
 #define HEAP_BUFFER_BASE	(HEAP_RUNTIME_BASE + HEAP_RUNTIME_SIZE)
 #define HEAP_BUFFER_SIZE	(SOF_STACK_END - HEAP_BUFFER_BASE)

src/platform/cannonlake/include/platform/memory.h
@@ -176,7 +176,7 @@
 #define HEAP_RT_COUNT128		32
 #define HEAP_RT_COUNT256		64
 #define HEAP_RT_COUNT512		32
-#define HEAP_RT_COUNT1024		1
+#define HEAP_RT_COUNT1024		4
 
 #define L2_VECTOR_SIZE		0x1000
 
@@ -266,7 +266,8 @@
 #define HEAP_RUNTIME_BASE	(HEAP_SYSTEM_3_BASE + HEAP_SYSTEM_3_SIZE)
 #define HEAP_RUNTIME_SIZE \
 	(HEAP_RT_COUNT64 * 64 + HEAP_RT_COUNT128 * 128 + \
-	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512)
+	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512 + \
+	HEAP_RT_COUNT1024 * 1024)
 
 /* Stack configuration */
 #define SOF_STACK_SIZE				0x2000

src/platform/icelake/include/platform/memory.h
@@ -176,7 +176,7 @@
 #define HEAP_RT_COUNT128		32
 #define HEAP_RT_COUNT256		64
 #define HEAP_RT_COUNT512		32
-#define HEAP_RT_COUNT1024		1
+#define HEAP_RT_COUNT1024		4
 
 #define L2_VECTOR_SIZE		0x1000
 
@@ -266,7 +266,8 @@
 #define HEAP_RUNTIME_BASE	(HEAP_SYSTEM_3_BASE + HEAP_SYSTEM_3_SIZE)
 #define HEAP_RUNTIME_SIZE \
 	(HEAP_RT_COUNT64 * 64 + HEAP_RT_COUNT128 * 128 + \
-	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512)
+	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512 + \
+	HEAP_RT_COUNT1024 * 1024)
 
 /* Stack configuration */
 #define SOF_STACK_SIZE				0x2000

src/platform/suecreek/include/platform/memory.h
@@ -164,7 +164,7 @@
 #define HEAP_RT_COUNT128		32
 #define HEAP_RT_COUNT256		64
 #define HEAP_RT_COUNT512		32
-#define HEAP_RT_COUNT1024		1
+#define HEAP_RT_COUNT1024		4
 
 #define L2_VECTOR_SIZE		0x2000
 
@@ -221,7 +221,8 @@
 #define HEAP_RUNTIME_BASE	(HEAP_SYSTEM_1_BASE + HEAP_SYSTEM_1_SIZE)
 #define HEAP_RUNTIME_SIZE \
 	(HEAP_RT_COUNT64 * 64 + HEAP_RT_COUNT128 * 128 + \
-	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512)
+	HEAP_RT_COUNT256 * 256 + HEAP_RT_COUNT512 * 512 + \
+	HEAP_RT_COUNT1024 * 1024)
 
 /* Stack configuration */
 #define SOF_STACK_SIZE				0x2000