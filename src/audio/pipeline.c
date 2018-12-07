e30dbd9805055fa46cbbb349178bdbe75b82e774
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2018-12-07 00:38:18 +0000

    pipeline: check pipeline init memory allocation.
    Make sure we check the return value and complain if it fails.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/audio/pipeline.c
+++ src/audio/pipeline.c
@@ -1321,5 +1321,10 @@ int pipeline_init(void)
 		sizeof(*pipe_data));
 	spinlock_init(&pipe_data->lock);
 
+	if (!pipe_data) {
+		trace_pipe_error("failed to init pipeline");
+		return -ENOMEM;
+	}
+
 	return 0;
 }
