676e72a895fc23a43fb347008b6213e1b00b76dc
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2018-12-19 15:56:04 +0200

    Topology: EQ IIR: Allow capture with all formats with pipe-eq-capture
    This patch adds to IIR EQ capability to convert from s32 source to
    s16/s24/s32 sink formats in normal PCM samples equalization. The
    feature is useful in microphone equalization where input dynamic
    range is high but where pipeline needs to be lower word length. A
    typical IIR high-pass response when combined with gain will reduce
    the dynamic range by suppressing lowest frequencies from signal to
    better fit the smaller sample word length.
    
    The PCM capability in topology macro pipe-eq-capture.m4 is changed to
    allow the s16/s24/s32 formats.
    
    The patch also includes 4096 bytes increase to SOF_TEXT_SIZE for CNL
    platform.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/eq_iir.h
+++ src/audio/eq_iir.h
@@ -33,4 +33,20 @@
 #ifndef EQ_IIR_H
 #define EQ_IIR_H
 
+/** \brief IIR EQ processing functions map item. */
+struct eq_iir_func_map {
+	uint8_t source;				/**< source frame format */
+	uint8_t sink;				/**< sink frame format */
+	void (*func)(struct comp_dev *dev,	/**< EQ processing function */
+		     struct comp_buffer *source,
+		     struct comp_buffer *sink,
+		     uint32_t frames);
+};
+
+/** \brief Type definition for processing function select return value. */
+typedef void (*eq_iir_func)(struct comp_dev *dev,
+			    struct comp_buffer *source,
+			    struct comp_buffer *sink,
+			    uint32_t frames);
+
 #endif
