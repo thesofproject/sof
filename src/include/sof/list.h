0c3c5d770bbe41c8cd1fe55e30cb875020a3ab55
Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>
2019-01-10 14:54:59 +0100

    list: removing unnecessary list_item_del_init func
    list_item_del_init function  had exactly the same
    effect as list_item_del function
    
    Signed-off-by: Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>

--- src/include/sof/list.h
+++ src/include/sof/list.h
@@ -73,21 +73,12 @@ static inline void list_item_append(struct list_item *item,
 }
 
 /* delete item from the list leaves deleted list item
- *in undefined state list_is_empty won't return true
+ *in undefined state list_is_empty will return true
  */
 static inline void list_item_del(struct list_item *item)
 {
 	item->next->prev = item->prev;
 	item->prev->next = item->next;
-	list_init(item);
-}
-
-/* delete item from the list list iteam will be reinitialised
- * and list_is_empty will return true
- */
-static inline void list_item_del_init(struct list_item *item)
-{
-	list_item_del(item);
 	list_init(item);
 }
 
