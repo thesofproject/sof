c3b26e79094b857a18b150dfdc80c0fcae195524
Pan Xiuli <xiuli.pan@linux.intel.com>
2019-01-11 13:56:34 +0800

    scripts: increase shm size for some QEMU platforms
    SHM used to be 64MB, but in newer platform a HP-SRAM is bigger than
    that.
    
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

--- scripts/docker-qemu.sh
+++ scripts/docker-qemu.sh
@@ -2,6 +2,9 @@
 
 # Runs a given script in the docker container you can generate from the
 # docker_build directory.
+# Example:
+#  ./scripts/docker-qemu.sh ../sof.git/scripts/qemu-check.sh
 
-docker run -i --privileged -v `pwd`:/home/sof/sof.git \
+
+docker run -i --shm-size=512m --privileged -v `pwd`:/home/sof/sof.git \
 	   --user `id -u` sofqemu $@
