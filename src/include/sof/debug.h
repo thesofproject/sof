41a69d5234ff180051b48631f8e3a7647c38d55a
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-01-15 23:08:06 -0600

    src/include/sof/debug.h: fix style
    Make checkpatch happy
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- src/include/sof/debug.h
+++ src/include/sof/debug.h
@@ -63,74 +63,80 @@
 /* dump file and line to start of mailbox or shared memory */
 #define dbg() \
 	do { \
-		volatile uint32_t *__m = (uint32_t*)mailbox_get_debug_base(); \
+		volatile uint32_t *__m = (uint32_t *)mailbox_get_debug_base(); \
 		*(__m++) = (__FILE__[0] << 24) + (__FILE__[1] << 16) +\
 			 (__FILE__[2] << 8) + (__FILE__[3]); \
 		*(__m++) = (__func__[0] << 24) + (__func__[1] << 16) + \
 			(__func__[2] << 8) + (__func__[3]); \
 		*__m = __LINE__; \
-	} while (0);
+	} while (0)
 
 /* dump file and line to offset in mailbox or shared memory */
 #define dbg_at(__x) \
 	do { \
-		volatile uint32_t *__m = (uint32_t*)mailbox_get_debug_base() + __x; \
+		volatile uint32_t *__m = (uint32_t *)mailbox_get_debug_base() + __x; \
 		*(__m++) = (__FILE__[0] << 24) + (__FILE__[1] << 16) +\
 			 (__FILE__[2] << 8) + (__FILE__[3]); \
 		*(__m++) = (__func__[0] << 24) + (__func__[1] << 16) + \
 			(__func__[2] << 8) + (__func__[3]); \
 		*__m = __LINE__; \
-	} while (0);
+	} while (0)
 
 /* dump value to start of mailbox or shared memory */
 #define dbg_val(__v) \
 	do { \
 		volatile uint32_t *__m = \
-			(volatile uint32_t*)mailbox_get_debug_base(); \
+			(volatile uint32_t *)mailbox_get_debug_base(); \
 		*__m = __v; \
-	} while (0);
+	} while (0)
 
 /* dump value to offset in mailbox or shared memory */
 #define dbg_val_at(__v, __x) \
 	do { \
 		volatile uint32_t *__m = \
-			(volatile uint32_t*)mailbox_get_debug_base() + __x; \
+			(volatile uint32_t *)mailbox_get_debug_base() + __x; \
 		*__m = __v; \
-	} while (0);
+	} while (0)
 
 /* dump data area at addr and size count to start of mailbox or shared memory */
 #define dump(addr, count) \
 	do { \
-		volatile uint32_t *__m = (uint32_t*)mailbox_get_debug_base(); \
-		volatile uint32_t *__a = (uint32_t*)addr; \
+		volatile uint32_t *__m = (uint32_t *)mailbox_get_debug_base(); \
+		volatile uint32_t *__a = (uint32_t *)addr; \
 		volatile int __c = count; \
 		while (__c--) \
 			*(__m++) = *(__a++); \
-	} while (0);
+	} while (0)
 
 /* dump data area at addr and size count at mailbox offset or shared memory */
 #define dump_at(addr, count, offset) \
 	do { \
-		volatile uint32_t *__m = (uint32_t*)mailbox_get_debug_base() + offset; \
-		volatile uint32_t *__a = (uint32_t*)addr; \
+		volatile uint32_t *__m = (uint32_t *)mailbox_get_debug_base() + offset; \
+		volatile uint32_t *__a = (uint32_t *)addr; \
 		volatile int __c = count; \
 		while (__c--) \
 			*(__m++) = *(__a++); \
-	} while (0);
+	} while (0)
 
 /* dump object to start of mailbox */
 #define dump_object(__o) \
-	dbg(); \
-	dump(&__o, sizeof(__o) >> 2);
+	do {						\
+		dbg();					\
+		dump(&__o, sizeof(__o) >> 2);		\
+	} while (0)
 
 /* dump object from pointer at start of mailbox */
 #define dump_object_ptr(__o) \
-	dbg(); \
-	dump(__o, sizeof(*(__o)) >> 2);
+	do {						\
+		dbg();					\
+		dump(__o, sizeof(*(__o)) >> 2);		\
+	} while (0)
 
 #define dump_object_ptr_at(__o, __at) \
-	dbg(); \
-	dump_at(__o, sizeof(*(__o)) >> 2, __at);
+	do {						\
+		dbg();					\
+		dump_at(__o, sizeof(*(__o)) >> 2, __at);\
+	} while (0)
 
 #else
 
@@ -155,7 +161,7 @@
 
 /* dump stack as part of panic */
 static inline uint32_t dump_stack(uint32_t p, void *addr, size_t offset,
-	size_t limit)
+				  size_t limit)
 {
 	extern void *_stack_sentry;
 	void *stack_limit = (void *)&_stack_sentry +
