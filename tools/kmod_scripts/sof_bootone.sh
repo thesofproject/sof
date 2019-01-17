238b9557d5914ebf1eed17e401b3d64bcacce054
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-01-16 19:12:11 -0800

    scripts: modify sof_bootone.sh to exit if sof_remove failed
    Exit if sof_remove fails. This avoids running into further issues.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- tools/kmod_scripts/sof_bootone.sh
+++ tools/kmod_scripts/sof_bootone.sh
@@ -2,6 +2,18 @@
 
 dmesg -C
 ./sof_remove.sh
+
+# exit if remove failed
+ERROR=$(dmesg | grep sof-audio | grep "error")
+if [ ! -z "$ERROR" ]
+then
+    dmesg > boot_fail.log
+    echo "sof_remove failed, see boot_fail.log for details"
+    exit 1
+fi
+
+unset ERROR
+
 ./sof_insert.sh
 sleep 1
 
@@ -11,10 +23,11 @@ FW_BOOT=$(dmesg | grep sof-audio | grep "boot complete")
 ERROR=$(dmesg | grep sof-audio | grep -v "DSP trace buffer overflow" | grep "error")
 TIMEOUT=$(dmesg | grep sof-audio | grep "ipc timed out")
 
+# exit if insert failed
 if [ ! -z "$ERROR" ] || [ -z "$FW_BOOT" ] || [ ! -z "$TIMEOUT" ]
 then
     dmesg > boot_fail.log
-    echo "boot failed, see boot_fail.log for details"
+    echo "sof_insert failed, see boot_fail.log for details"
     exit 1
 else
     echo "boot success"
