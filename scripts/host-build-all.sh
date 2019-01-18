d2c4f8f7a6ab7e1fd7ccb07dac137496cae74ff4
Janusz Jankowski <janusz.jankowski@linux.intel.com>
2019-01-18 13:26:42 +0100

    cmake: update host-build-all.sh
    Signed-off-by: Janusz Jankowski <janusz.jankowski@linux.intel.com>

--- scripts/host-build-all.sh
+++ scripts/host-build-all.sh
@@ -3,30 +3,8 @@
 # fail on any errors
 set -e
 
-# parse the args
-for args in $@
-do
-	if [[ "$args" == "-l" ]]
-		then
-		BUILD_LOCAL=1
-	fi
-done
-
-# run autogen.sh
-./autogen.sh
-
-pwd=`pwd`
-
-# Build library for host platform architecture
-if [[ "x$BUILD_LOCAL" == "x" ]]
-then
-	./configure --with-arch=host --enable-library=yes --host=x86_64-unknown-linux-gnu
-else
-	# make sure host lib is build in local folder
-	echo "BUILD in local folder!"
-	rm -rf $pwd/local/
-	./configure --with-arch=host --enable-library=yes --host=x86_64-unknown-linux-gnu --prefix=$pwd/host-lib
-fi
-
-make
+mkdir build_host
+cd build_host
+cmake -DBUILD_HOST=ON -DCMAKE_INSTALL_PREFIX=install ..
+make -j4
 make install
