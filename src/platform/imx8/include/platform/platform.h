62ae973ba54f2ef0bac9a90f8408867e0759e81d
Daniel Baluta <daniel.baluta@nxp.com>
2019-01-27 20:04:08 +0200

    imx8: Add support for NXP i.MX8 platform
    i.MX 8 family of applications processor has 3-subfamilies:
    	* i.MX 8, for advanced graphics, performance and virtualization
    	* i.MX 8X, for safety and efficent performance
    	* i.MX 8M, for advanced audio, voice and video
    
    (yes, you are right i.MX 8 is the generic SoC family but also a subfamily).
    
    All of the subfamilies have SoC designs which include a DSP for efficient
    audio processing. DSP has direct access to SDRAM and to Audio
    peripherals.
    
    https://www.nxp.com/products/processors-and-microcontrollers/arm-based-processors-and-mcus/i.mx-applications-processors/i.mx-8-processors:IMX8-SERIES
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

---
+++ src/platform/imx8/include/platform/platform.h
@@ -0,0 +1,98 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright 2019 NXP
+ *
+ * Author: Daniel Baluta <daniel.baluta@nxp.com>
+ */
+
+#ifndef __PLATFORM_PLATFORM_H__
+#define __PLATFORM_PLATFORM_H__
+
+#include <sof/platform.h>
+#include <platform/interrupt.h>
+
+struct sof;
+
+#define PLATFORM_DEFAULT_CLOCK CLK_CPU(0)
+#define PLATFORM_MASTER_CORE_ID	0
+#define PLATFORM_CORE_COUNT	1
+#define LPSRAM_SIZE 16384
+
+#define PLATFORM_WORKQ_DEFAULT_TIMEOUT	1000
+
+/* IPC Interrupt */
+#define PLATFORM_IPC_INTERRUPT	IRQ_NUM_MU
+
+/* Host page size */
+#define HOST_PAGE_SIZE		4096
+#define PLATFORM_PAGE_TABLE_SIZE	256
+
+/* pipeline IRQ */
+#define PLATFORM_SCHEDULE_IRQ	IRQ_NUM_SOFTWARE0
+
+#define PLATFORM_IRQ_TASK_HIGH	IRQ_NUM_SOFTWARE1
+#define PLATFORM_IRQ_TASK_MED	IRQ_NUM_SOFTWARE1
+#define PLATFORM_IRQ_TASK_LOW	IRQ_NUM_SOFTWARE1
+
+#define PLATFORM_SCHEDULE_COST	200
+
+/* maximum preload pipeline depth */
+#define MAX_PRELOAD_SIZE	20
+
+/* DMA treats PHY addresses as host address unless within DSP region */
+#define PLATFORM_HOST_DMA_MASK	0xFF000000
+
+/* Platform stream capabilities */
+#define PLATFORM_MAX_CHANNELS	4
+#define PLATFORM_MAX_STREAMS	5
+
+/* clock source used by scheduler for deadline calculations */
+#define PLATFORM_SCHED_CLOCK	PLATFORM_DEFAULT_CLOCK
+
+/* DMA channel drain timeout in microseconds - TODO: caclulate based on topology */
+#define PLATFORM_DMA_TIMEOUT	1333
+
+/* DMA host transfer timeouts in microseconds */
+#define PLATFORM_HOST_DMA_TIMEOUT	50
+
+/* WorkQ window size in microseconds */
+#define PLATFORM_WORKQ_WINDOW	2000
+
+/* platform WorkQ clock */
+#define PLATFORM_WORKQ_CLOCK	PLATFORM_DEFAULT_CLOCK
+
+/* local buffer size of DMA tracing */
+#define DMA_TRACE_LOCAL_SIZE	HOST_PAGE_SIZE
+
+/* trace bytes flushed during panic */
+#define DMA_FLUSH_TRACE_SIZE	(MAILBOX_TRACE_SIZE >> 2)
+
+/* the interval of DMA trace copying */
+#define DMA_TRACE_PERIOD		500000
+
+/*
+ * the interval of reschedule DMA trace copying in special case like half
+ * fullness of local DMA trace buffer
+ */
+#define DMA_TRACE_RESCHEDULE_TIME	100
+
+/* DSP should be idle in this time frame */
+#define PLATFORM_IDLE_TIME	750000
+
+/* DSP default delay in cycles */
+#define PLATFORM_DEFAULT_DELAY	12
+
+/* Platform defined panic code */
+static inline void platform_panic(uint32_t p)
+{
+}
+
+/* Platform defined trace code */
+#define platform_trace_point(__x)
+
+extern struct timer *platform_timer;
+
+extern intptr_t _module_init_start;
+extern intptr_t _module_init_end;
+
+#endif
