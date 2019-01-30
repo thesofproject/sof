c80037bbab880d7cfb63f3e411ff3f3fc61c2962
Janusz Jankowski <janusz.jankowski@linux.intel.com>
2019-01-30 06:47:11 +0100

    rimage: elf.h: use stdint

--- rimage/elf.h
+++ rimage/elf.h
@@ -50,15 +50,17 @@
  * not include the padding.
  */
 
-typedef unsigned __int64 uint64;
-typedef unsigned __int32 uint32;
-typedef unsigned __int16 uint16;
-typedef unsigned __int8 uint8;
+#include <stdint.h>
 
-typedef __int64 int64;
-typedef __int32 int32;
-typedef __int16 int16;
-typedef __int8 int8;
+typedef uint64_t uint64;
+typedef uint32_t uint32;
+typedef uint16_t uint16;
+typedef uint8_t uint8;
+
+typedef int64_t int64;
+typedef int32_t int32;
+typedef int16_t int16;
+typedef int8_t int8;
 
 
 typedef struct {
