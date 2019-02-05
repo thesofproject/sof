0e64ede5388eaba52b006201defc6b4b3376aad0
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2019-02-05 14:31:50 +0100

    GDB: Added support for insert breakpoint.
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

--- src/include/sof/gdb/gdb.h
+++ src/include/sof/gdb/gdb.h
@@ -3,7 +3,7 @@
 
 #define GDB_BUFMAX 256
 #define GDB_NUMBER_OF_REGISTERS 64
-
+#define DISABLE_LOWER_INTERRUPTS_MASK ~0x1F
 void gdb_handle_exception(void);
 void gdb_debug_info(unsigned char *str);
 void gdb_init_debug_exception(void);
