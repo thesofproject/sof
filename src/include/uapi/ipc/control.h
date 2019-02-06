c4de0bd9ac623c58f76932091adc23c1b7b298ba
Jaska Uimonen <jaska.uimonen@intel.com>
2019-02-06 09:54:19 +0200

    ipc: add msg_id and total_elems to ctrl data struct
    Add members to enable sending and receiving large messages.
    
    Also bump abi minor version.
    
    Signed-off-by: Jaska Uimonen <jaska.uimonen@intel.com>

--- src/include/uapi/ipc/control.h
+++ src/include/uapi/ipc/control.h
@@ -150,9 +150,13 @@ struct sof_ipc_ctrl_data {
 	/* control data - can either be appended or DMAed from host */
 	struct sof_ipc_host_buffer buffer;
 	uint32_t num_elems;	/**< in array elems or bytes for data type */
+	uint32_t total_elems;	/**< total size if message is sent in parts */
+
+	/* for large messages sent in parts */
+	uint32_t msg_id;
 
 	/* reserved for future use */
-	uint32_t reserved[8];
+	uint32_t reserved[6];
 
 	/* control data - add new types if needed */
 	union {
