aa172da91947dc4e3b727b722ab19a7c5dc27ac7
Pan Xiuli <xiuli.pan@linux.intel.com>
2019-02-10 22:03:35 +0800

    [skip ci]scirpts: update target install
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

--- scripts/sof-target-intsall.sh
+++ scripts/sof-target-intsall.sh
@@ -3,7 +3,9 @@
 
 for host in $@
 do
-	scp build_*_*/sof-*.ri tools/topology/*.tplg root@${host}:/lib/firmware/intel/
+	scp build_*_*/sof-*.ri root@${host}:/lib/firmware/intel/sof/
+	# TODO: need modify when tools CMAKE build system updated
+	scp tools/topology/*.tplg root@${host}:/lib/firmware/intel/sof-tplg/
 	scp tools/logger/sof-logger \
 		build_*_*/src/arch/xtensa/sof-*.ldc \
 		tools/coredumper/* \
