fa6b2c6c33249e429f1783f8beec35b2ae8a52cf
Adrian Bonislawski <adrian.bonislawski@linux.intel.com>
2019-02-11 13:01:16 +0100

    Kconfig: make Tone component a config option
    Signed-off-by: Adrian Bonislawski <adrian.bonislawski@linux.intel.com>

--- src/include/sof/audio/component.h
+++ src/include/sof/audio/component.h
@@ -510,7 +510,11 @@ void sys_comp_src_init(void);
 #else
 static inline void sys_comp_src_init(void) {}
 #endif
+#ifdef CONFIG_COMP_TONE
 void sys_comp_tone_init(void);
+#else
+static inline void sys_comp_tone_init(void) {}
+#endif
 #ifdef CONFIG_COMP_IIR
 void sys_comp_eq_iir_init(void);
 #else
