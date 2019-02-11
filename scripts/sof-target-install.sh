6432170b4c465d232b359ef60136c73802cc256d
Janusz Jankowski <janusz.jankowski@linux.intel.com>
2019-02-11 13:07:37 +0100

    scripts: sof-target-install update for cmake tools
    Signed-off-by: Janusz Jankowski <janusz.jankowski@linux.intel.com>

--- scripts/sof-target-install.sh
+++ scripts/sof-target-install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# usage: ./scripts/sof-target-intsall.sh up2-hostname ip
+# usage: ./scripts/sof-target-install.sh up2-hostname ip
 
 for host in $@
 do
 	scp build_*_*/sof-*.ri root@${host}:/lib/firmware/intel/sof/
-	# TODO: need modify when tools CMAKE build system updated
-	scp tools/topology/*.tplg root@${host}:/lib/firmware/intel/sof-tplg/
-	scp tools/logger/sof-logger \
+	scp tools/build_tools/topology/*.tplg root@${host}:/lib/firmware/intel/sof-tplg/
+	scp tools/build_tools/logger/sof-logger \
 		build_*_*/src/arch/xtensa/sof-*.ldc \
 		tools/coredumper/* \
-		tools/eqctl/sof-eqctl \
+		tools/build_tools/eqctl/sof-eqctl \
 		tools/kmod_scripts/* \
 		root@${host}:~/
 done
