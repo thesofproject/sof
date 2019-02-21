c922a866e211a3e56d1e280fa72fc0328f81c802
Marcin Maka <marcin.maka@linux.intel.com>
2019-02-21 19:19:44 +0100

    debug: stack dump code converted to uintptr_t for size arithmetic
    Signed-off-by: Marcin Maka <marcin.maka@linux.intel.com>

--- src/include/sof/debug.h
+++ src/include/sof/debug.h
@@ -164,10 +164,10 @@ static inline uint32_t dump_stack(uint32_t p, void *addr, size_t offset,
 				  size_t limit)
 {
 	extern void *_stack_sentry;
-	void *stack_limit = (void *)&_stack_sentry +
+	uintptr_t stack_limit = (uintptr_t)&_stack_sentry +
 		(cpu_get_id() * SOF_STACK_SIZE);
-	void *stack_bottom = stack_limit + SOF_STACK_SIZE - sizeof(void *);
-	void *stack_top = arch_get_stack_ptr() + offset;
+	uintptr_t stack_bottom = stack_limit + SOF_STACK_SIZE - sizeof(void *);
+	uintptr_t stack_top = (uintptr_t)arch_get_stack_ptr() + offset;
 	size_t size = stack_bottom - stack_top;
 
 	/* is stack smashed ? */
@@ -182,7 +182,7 @@ static inline uint32_t dump_stack(uint32_t p, void *addr, size_t offset,
 		size = limit;
 
 	/* copy stack contents and writeback */
-	rmemcpy(addr, stack_top, size - sizeof(void *));
+	rmemcpy(addr, (void*)stack_top, size - sizeof(void *));
 	dcache_writeback_region(addr, size - sizeof(void *));
 	return p;
 }
