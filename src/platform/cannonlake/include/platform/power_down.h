5224264069649e6f506a7ce2e3c6bec2ac0f1172
Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>
2019-02-26 15:19:49 +0100

    power_down: deduplication of power_down headers for cavs
    Signed-off-by: Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>

--- src/platform/cannonlake/include/platform/power_down.h
+++ src/platform/cannonlake/include/platform/power_down.h
@@ -27,17 +27,9 @@
  *
  * Author: Lech Betlej <lech.betlej@linux.intel.com>
  */
-#ifndef CANNONLAKE_INCLUDE_PLATFORM_POWER_DOWN_H
-#define CANNONLAKE_INCLUDE_PLATFORM_POWER_DOWN_H
+#ifndef __INCLUDE_PLATFORM_POWER_DOWN__
+#define __INCLUDE_PLATFORM_POWER_DOWN__
 
-#include <stdbool.h>
-/**
- * Power down procedure.
- * Locks its code in L1 cache and shuts down memories.
- * @param  disable_lpsram        flag if LPSRAM is to be disabled (whole)
- * @param  hpsram_pwrgating_mask pointer to memory segments power gating mask
- * (each bit corresponds to one ebb)
- * @return                       nothing returned - this function never quits
- */
-void power_down(bool disable_lpsram, uint32_t *hpsram_pwrgating_mask);
-#endif /* CANNONLAKE_INCLUDE_PLATFORM_POWER_DOWN_H */
+#include <cavs/power_down.h>
+
+#endif /* __INCLUDE_PLATFORM_POWER_DOWN__ */
--- src/platform/intel/cavs/include/cavs/power_down.h
+++ src/platform/intel/cavs/include/cavs/power_down.h
@@ -27,9 +27,18 @@
  *
  * Author: Lech Betlej <lech.betlej@linux.intel.com>
  */
-#ifndef __INCLUDE_PLATFORM_POWER_DOWN__
-#define __INCLUDE_PLATFORM_POWER_DOWN__
+#ifndef __INCLUDE_CAVS_POWER_DOWN__
+#define __INCLUDE_CAVS_POWER_DOWN__
 
-#include <cavs/power_down.h>
+#include <stdbool.h>
+/**
+ * Power down procedure.
+ * Locks its code in L1 cache and shuts down memories.
+ * @param  disable_lpsram        flag if LPSRAM is to be disabled (whole)
+ * @param  hpsram_pwrgating_mask pointer to memory segments power gating mask
+ * (each bit corresponds to one ebb)
+ * @return                       nothing returned - this function never quits
+ */
+void power_down(bool disable_lpsram, uint32_t *hpsram_pwrgating_mask);
 
 #endif /* __INCLUDE_PLATFORM_POWER_DOWN__ */
