e2b994ee03916edef0f9269f1b4c9310dfab43f9
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2019-04-16 18:31:21 +0200

    dw-dma: cleanup the whole driver
    Cleanups the whole DW-DMA driver:
    - Move platform register defines to proper headers.
    - Make driver independent of platform.
    - Use the same accepted style of trace logs.
    - Other minor tweaks.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

---
+++ src/platform/haswell/include/platform/dw-dma.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifndef __INCLUDE_PLATFORM_DW_DMA_H__
+#define __INCLUDE_PLATFORM_DW_DMA_H__
+
+#include <sof/bit.h>
+
+/* CTL_HI */
+#define DW_CTLH_DONE(x)		SET_BIT(12, x)
+#define DW_CTLH_BLOCK_TS_MASK	MASK(11, 0)
+
+/* CFG_LO */
+#define DW_CFGL_CLASS(x)	SET_BITS(7, 5, x)
+
+/* CFG_HI */
+#define DW_CFGH_DST_PER(x)	SET_BITS(14, 11, x)
+#define DW_CFGH_SRC_PER(x)	SET_BITS(10, 7, x)
+#define DW_CFGH_DST(x)		DW_CFGH_DST_PER(x)
+#define DW_CFGH_SRC(x)		DW_CFGH_SRC_PER(x)
+
+/* default initial setup register values */
+#define DW_CFG_LOW_DEF		0x0
+#define DW_CFG_HIGH_DEF		0x4
+
+#define platform_dw_dma_set_class(chan, lli, class) \
+	(chan->cfg_lo |= DW_CFGL_CLASS(class))
+
+#define platform_dw_dma_set_transfer_size(chan, lli, size) \
+	(lli->ctrl_hi |= ((size / (1 << ((lli->ctrl_lo & \
+		DW_CTLL_SRC_WIDTH_MASK) >> DW_CTLL_SRC_WIDTH_SHIFT))) & \
+		DW_CTLH_BLOCK_TS_MASK))
+
+#endif
