thesofproject/sof

commit e2b994ee03916edef0f9269f1b4c9310dfab43f9

dw-dma: cleanup the whole driver
Cleanups the whole DW-DMA driver:
- Move platform register defines to proper headers.
- Make driver independent of platform.
- Use the same accepted style of trace logs.
- Other minor tweaks.

Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

src/platform/intel/cavs/include/cavs/dw-dma.h
@@ -1,0 +1,67 @@
+/*
+ * Copyright (c) 2019, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifndef __INCLUDE_CAVS_DW_DMA_H__
+#define __INCLUDE_CAVS_DW_DMA_H__
+
+#include <sof/bit.h>
+
+/* CTL_HI */
+#define DW_CTLH_CLASS(x)	SET_BITS(31, 29, x)
+#define DW_CTLH_WEIGHT(x)	SET_BITS(28, 18, x)
+#define DW_CTLH_DONE(x)		SET_BIT(17, x)
+#define DW_CTLH_BLOCK_TS_MASK	MASK(16, 0)
+
+/* CFG_LO */
+#define DW_CFG_RELOAD_DST	BIT(31)
+#define DW_CFG_RELOAD_SRC	BIT(30)
+#define DW_CFG_CTL_HI_UPD_EN	BIT(5)
+
+/* CFG_HI */
+#define DW_CFGH_DST_PER_EXT(x)		SET_BITS(31, 30, x)
+#define DW_CFGH_SRC_PER_EXT(x)		SET_BITS(29, 28, x)
+#define DW_CFGH_DST_PER(x)		SET_BITS(7, 4, x)
+#define DW_CFGH_SRC_PER(x)		SET_BITS(3, 0, x)
+#define DW_CFGH_DST(x) \
+	(DW_CFGH_DST_PER_EXT((x) & 0x30) | DW_CFGH_DST_PER((x) & 0xF))
+#define DW_CFGH_SRC(x) \
+	(DW_CFGH_SRC_PER_EXT((x) & 0x30) | DW_CFGH_SRC_PER((x) & 0xF))
+
+/* default initial setup register values */
+#define DW_CFG_LOW_DEF		0x3
+#define DW_CFG_HIGH_DEF		0x0
+
+#define platform_dw_dma_set_class(chan, lli, class) \
+	(lli->ctrl_hi |= DW_CTLH_CLASS(class))
+
+#define platform_dw_dma_set_transfer_size(chan, lli, size) \
+	(lli->ctrl_hi |= (size & DW_CTLH_BLOCK_TS_MASK))
+
+#endif

src/include/sof/dw-dma.h
@@ -32,9 +32,126 @@
 #ifndef __SOF_DW_DMA_H__
 #define __SOF_DW_DMA_H__
 
+#include <sof/bit.h>
+#include <sof/trace.h>
 #include <stdint.h>
 
-#define DW_DMA_MAX_NR_CHANNELS	8
+/* channel registers */
+#define DW_MAX_CHAN		8
+#define DW_FIFO_SIZE		0x80
+#define DW_CHAN_SIZE		0x58
+#define DW_CHAN_OFFSET(chan)	(DW_CHAN_SIZE * (chan))
+
+#define DW_SAR(chan)		(0x00 + DW_CHAN_OFFSET(chan))
+#define DW_DAR(chan)		(0x08 + DW_CHAN_OFFSET(chan))
+#define DW_LLP(chan)		(0x10 + DW_CHAN_OFFSET(chan))
+#define DW_CTRL_LOW(chan)	(0x18 + DW_CHAN_OFFSET(chan))
+#define DW_CTRL_HIGH(chan)	(0x1C + DW_CHAN_OFFSET(chan))
+#define DW_CFG_LOW(chan)	(0x40 + DW_CHAN_OFFSET(chan))
+#define DW_CFG_HIGH(chan)	(0x44 + DW_CHAN_OFFSET(chan))
+#define DW_DSR(chan)		(0x50 + DW_CHAN_OFFSET(chan))
+
+/* common registers */
+#define DW_RAW_TFR		0x2C0
+#define DW_RAW_BLOCK		0x2C8
+#define DW_RAW_SRC_TRAN		0x2D0
+#define DW_RAW_DST_TRAN		0x2D8
+#define DW_RAW_ERR		0x2E0
+#define DW_STATUS_TFR		0x2E8
+#define DW_STATUS_BLOCK		0x2F0
+#define DW_STATUS_SRC_TRAN	0x2F8
+#define DW_STATUS_DST_TRAN	0x300
+#define DW_STATUS_ERR		0x308
+#define DW_MASK_TFR		0x310
+#define DW_MASK_BLOCK		0x318
+#define DW_MASK_SRC_TRAN	0x320
+#define DW_MASK_DST_TRAN	0x328
+#define DW_MASK_ERR		0x330
+#define DW_CLEAR_TFR		0x338
+#define DW_CLEAR_BLOCK		0x340
+#define DW_CLEAR_SRC_TRAN	0x348
+#define DW_CLEAR_DST_TRAN	0x350
+#define DW_CLEAR_ERR		0x358
+#define DW_INTR_STATUS		0x360
+#define DW_DMA_CFG		0x398
+#define DW_DMA_CHAN_EN		0x3A0
+#define DW_FIFO_PART0_LO	0x400
+#define DW_FIFO_PART0_HI	0x404
+#define DW_FIFO_PART1_LO	0x408
+#define DW_FIFO_PART1_HI	0x40C
+
+/* channel bits */
+#define DW_CHAN_WRITE_EN_ALL	MASK(2 * DW_MAX_CHAN - 1, DW_MAX_CHAN)
+#define DW_CHAN_WRITE_EN(chan)	BIT((chan) + DW_MAX_CHAN)
+#define DW_CHAN_ALL		MASK(DW_MAX_CHAN - 1, 0)
+#define DW_CHAN(chan)		BIT(chan)
+#define DW_CHAN_MASK_ALL	DW_CHAN_WRITE_EN_ALL
+#define DW_CHAN_MASK(chan)	DW_CHAN_WRITE_EN(chan)
+#define DW_CHAN_UNMASK_ALL	(DW_CHAN_WRITE_EN_ALL | DW_CHAN_ALL)
+#define DW_CHAN_UNMASK(chan)	(DW_CHAN_WRITE_EN(chan) | DW_CHAN(chan))
+
+/* CFG_LO */
+#define DW_CFGL_DRAIN		BIT(10)
+#define DW_CFGL_FIFO_EMPTY	BIT(9)
+#define DW_CFGL_SUSPEND		BIT(8)
+
+/* CTL_LO */
+#define DW_CTLL_RELOAD_DST	BIT(31)
+#define DW_CTLL_RELOAD_SRC	BIT(30)
+#define DW_CTLL_LLP_S_EN	BIT(28)
+#define DW_CTLL_LLP_D_EN	BIT(27)
+#define DW_CTLL_SMS(x)		SET_BIT(25, x)
+#define DW_CTLL_DMS(x)		SET_BIT(23, x)
+#define DW_CTLL_FC_P2P		SET_BITS(21, 20, 3)
+#define DW_CTLL_FC_P2M		SET_BITS(21, 20, 2)
+#define DW_CTLL_FC_M2P		SET_BITS(21, 20, 1)
+#define DW_CTLL_FC_M2M		SET_BITS(21, 20, 0)
+#define DW_CTLL_D_SCAT_EN	BIT(18)
+#define DW_CTLL_S_GATH_EN	BIT(17)
+#define DW_CTLL_SRC_MSIZE(x)	SET_BITS(16, 14, x)
+#define DW_CTLL_DST_MSIZE(x)	SET_BITS(13, 11, x)
+#define DW_CTLL_SRC_FIX		SET_BITS(10, 9, 2)
+#define DW_CTLL_SRC_DEC		SET_BITS(10, 9, 1)
+#define DW_CTLL_SRC_INC		SET_BITS(10, 9, 0)
+#define DW_CTLL_DST_FIX		SET_BITS(8, 7, 2)
+#define DW_CTLL_DST_DEC		SET_BITS(8, 7, 1)
+#define DW_CTLL_DST_INC		SET_BITS(8, 7, 0)
+#define DW_CTLL_SRC_WIDTH(x)	SET_BITS(6, 4, x)
+#define DW_CTLL_DST_WIDTH(x)	SET_BITS(3, 1, x)
+#define DW_CTLL_INT_EN		BIT(0)
+#define DW_CTLL_SRC_WIDTH_MASK	MASK(6, 4)
+#define DW_CTLL_SRC_WIDTH_SHIFT	4
+#define DW_CTLL_DST_WIDTH_MASK	MASK(3, 1)
+#define DW_CTLL_DST_WIDTH_SHIFT	1
+
+/* DSR */
+#define DW_DSR_DSC(x)		SET_BITS(31, 20, x)
+#define DW_DSR_DSI(x)		SET_BITS(19, 0, x)
+
+/* FIFO_PART */
+#define DW_FIFO_UPD		BIT(26)
+#define DW_FIFO_CHx(x)		SET_BITS(25, 13, x)
+#define DW_FIFO_CHy(x)		SET_BITS(12, 0, x)
+
+/* number of tries to wait for reset */
+#define DW_DMA_CFG_TRIES	10000
+
+/* min number of elems for config with irq disabled */
+#define DW_DMA_CFG_NO_IRQ_MIN_ELEMS	3
+
+/* linked list item address */
+#define DW_DMA_LLI_ADDRESS(lli, dir) \
+	(((dir) == DMA_DIR_MEM_TO_DEV) ? ((lli)->sar) : ((lli)->dar))
+
+/* tracing */
+#define trace_dwdma(__e, ...) \
+	trace_event(TRACE_CLASS_DMA, __e, ##__VA_ARGS__)
+#define trace_dwdma_atomic(__e, ...) \
+	trace_event_atomic(TRACE_CLASS_DMA, __e, ##__VA_ARGS__)
+#define tracev_dwdma(__e, ...) \
+	tracev_event(TRACE_CLASS_DMA, __e, ##__VA_ARGS__)
+#define trace_dwdma_error(__e, ...) \
+	trace_error(TRACE_CLASS_DMA, __e, ##__VA_ARGS__)
 
 /* TODO: add FIFO sizes */
 struct dw_chan_data {
@@ -43,20 +160,11 @@ struct dw_chan_data {
 };
 
 struct dw_drv_plat_data {
-	struct dw_chan_data chan[DW_DMA_MAX_NR_CHANNELS];
+	struct dw_chan_data chan[DW_MAX_CHAN];
 };
 
-/* DMA descriptor used by the HW version 1 */
-struct dw_lli1 {
-	uint32_t sar;
-	uint32_t dar;
-	uint32_t llp;
-	uint32_t ctrl_lo;
-	uint32_t ctrl_hi;
-} __attribute__ ((packed));
-
-/* DMA descriptor used by HW version 2 */
-struct dw_lli2 {
+/* DMA descriptor used by HW */
+struct dw_lli {
 	uint32_t sar;
 	uint32_t dar;
 	uint32_t llp;

src/platform/apollolake/include/platform/dw-dma.h
@@ -1,0 +1,36 @@
+/*
+ * Copyright (c) 2019, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifndef __INCLUDE_PLATFORM_DW_DMA_H__
+#define __INCLUDE_PLATFORM_DW_DMA_H__
+
+#include <cavs/dw-dma.h>
+
+#endif

src/platform/baytrail/include/platform/dw-dma.h
@@ -1,0 +1,58 @@
+/*
+ * Copyright (c) 2019, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifndef __INCLUDE_PLATFORM_DW_DMA_H__
+#define __INCLUDE_PLATFORM_DW_DMA_H__
+
+#include <sof/bit.h>
+
+/* CTL_HI */
+#define DW_CTLH_CLASS(x)	SET_BITS(31, 29, x)
+#define DW_CTLH_WEIGHT(x)	SET_BITS(28, 18, x)
+#define DW_CTLH_DONE(x)		SET_BIT(17, x)
+#define DW_CTLH_BLOCK_TS_MASK	MASK(16, 0)
+
+/* CFG_HI */
+#define DW_CFGH_DST_PER(x)	SET_BITS(7, 4, x)
+#define DW_CFGH_SRC_PER(x)	SET_BITS(3, 0, x)
+#define DW_CFGH_DST(x)		DW_CFGH_DST_PER(x)
+#define DW_CFGH_SRC(x)		DW_CFGH_SRC_PER(x)
+
+/* default initial setup register values */
+#define DW_CFG_LOW_DEF		0x3
+#define DW_CFG_HIGH_DEF		0x0
+
+#define platform_dw_dma_set_class(chan, lli, class) \
+	(lli->ctrl_hi |= DW_CTLH_CLASS(class))
+
+#define platform_dw_dma_set_transfer_size(chan, lli, size) \
+	(lli->ctrl_hi |= (size & DW_CTLH_BLOCK_TS_MASK))
+
+#endif

src/platform/cannonlake/include/platform/dw-dma.h
@@ -1,0 +1,36 @@
+/*
+ * Copyright (c) 2019, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifndef __INCLUDE_PLATFORM_DW_DMA_H__
+#define __INCLUDE_PLATFORM_DW_DMA_H__
+
+#include <cavs/dw-dma.h>
+
+#endif

src/platform/haswell/include/platform/dw-dma.h
@@ -1,0 +1,61 @@
+/*
+ * Copyright (c) 2019, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifndef __INCLUDE_PLATFORM_DW_DMA_H__
+#define __INCLUDE_PLATFORM_DW_DMA_H__
+
+#include <sof/bit.h>
+
+/* CTL_HI */
+#define DW_CTLH_DONE(x)		SET_BIT(12, x)
+#define DW_CTLH_BLOCK_TS_MASK	MASK(11, 0)
+
+/* CFG_LO */
+#define DW_CFGL_CLASS(x)	SET_BITS(7, 5, x)
+
+/* CFG_HI */
+#define DW_CFGH_DST_PER(x)	SET_BITS(14, 11, x)
+#define DW_CFGH_SRC_PER(x)	SET_BITS(10, 7, x)
+#define DW_CFGH_DST(x)		DW_CFGH_DST_PER(x)
+#define DW_CFGH_SRC(x)		DW_CFGH_SRC_PER(x)
+
+/* default initial setup register values */
+#define DW_CFG_LOW_DEF		0x0
+#define DW_CFG_HIGH_DEF		0x4
+
+#define platform_dw_dma_set_class(chan, lli, class) \
+	(chan->cfg_lo |= DW_CFGL_CLASS(class))
+
+#define platform_dw_dma_set_transfer_size(chan, lli, size) \
+	(lli->ctrl_hi |= ((size / (1 << ((lli->ctrl_lo & \
+		DW_CTLL_SRC_WIDTH_MASK) >> DW_CTLL_SRC_WIDTH_SHIFT))) & \
+		DW_CTLH_BLOCK_TS_MASK))
+
+#endif

src/platform/icelake/include/platform/dw-dma.h
@@ -1,0 +1,36 @@
+/*
+ * Copyright (c) 2019, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifndef __INCLUDE_PLATFORM_DW_DMA_H__
+#define __INCLUDE_PLATFORM_DW_DMA_H__
+
+#include <cavs/dw-dma.h>
+
+#endif

src/platform/suecreek/include/platform/dw-dma.h
@@ -1,0 +1,36 @@
+/*
+ * Copyright (c) 2019, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the Intel Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
+ */
+
+#ifndef __INCLUDE_PLATFORM_DW_DMA_H__
+#define __INCLUDE_PLATFORM_DW_DMA_H__
+
+#include <cavs/dw-dma.h>
+
+#endif

commit 22e345d56b96cb6d13a797a3c223e7280fa18f3e

dw-dma: fix regression
Commit e2b994ee0391 ("dw-dma: cleanup the whole driver") introduced
bugs in bit-field calculations. This patch fixes them.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/platform/intel/cavs/include/cavs/dw-dma.h
@@ -50,9 +50,9 @@
 #define DW_CFGH_DST_PER(x)		SET_BITS(7, 4, x)
 #define DW_CFGH_SRC_PER(x)		SET_BITS(3, 0, x)
 #define DW_CFGH_DST(x) \
-	(DW_CFGH_DST_PER_EXT((x) & 0x30) | DW_CFGH_DST_PER((x) & 0xF))
+	(DW_CFGH_DST_PER_EXT((x) >> 4) | DW_CFGH_DST_PER(x))
 #define DW_CFGH_SRC(x) \
-	(DW_CFGH_SRC_PER_EXT((x) & 0x30) | DW_CFGH_SRC_PER((x) & 0xF))
+	(DW_CFGH_SRC_PER_EXT((x) >> 4) | DW_CFGH_SRC_PER(x))
 
 /* default initial setup register values */
 #define DW_CFG_LOW_DEF		0x3