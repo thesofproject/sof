fe913ccc2c62f8e3d4f225ff460cf7f3bc4a5d36
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2019-05-07 18:36:47 +0200

    ipc: fill wallclock frequency in sof_ipc_stream_posn
    Fills wallclock frequency in sof_ipc_stream_posn structure.
    This field has been never set before.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/drivers/intel/baytrail/timer.c
+++ src/drivers/intel/baytrail/timer.c
@@ -35,6 +35,7 @@
 #include <platform/interrupt.h>
 #include <sof/debug.h>
 #include <sof/audio/component.h>
+#include <sof/clk.h>
 #include <sof/drivers/timer.h>
 #include <stdint.h>
 
@@ -189,6 +190,7 @@ void platform_dai_timestamp(struct comp_dev *dai,
 
 	/* get SSP wallclock - DAI sets this to stream start value */
 	posn->wallclock = platform_timer_get(platform_timer) - posn->wallclock;
+	posn->wallclock_hz = clock_get_freq(PLATFORM_DEFAULT_CLOCK);
 	posn->flags |= SOF_TIME_WALL_VALID | SOF_TIME_WALL_64;
 }
 
