c17ae1ca303ba091a37947e536c9221b436f2e0d
Marcin Maka <marcin.maka@linux.intel.com>
2019-05-09 12:16:18 +0200

    apollolake: clk defs cleanup, using common cavs defs
    Signed-off-by: Marcin Maka <marcin.maka@linux.intel.com>

--- src/platform/apollolake/include/platform/clk-map.h
+++ src/platform/apollolake/include/platform/clk-map.h
@@ -33,19 +33,25 @@
 
 #include <sof/clk.h>
 
+#define CLK_MAX_CPU_HZ		400000000
+
 static const struct freq_table cpu_freq[] = {
 	{100000000, 100000, 0x3},
 	{200000000, 200000, 0x1},
-	{400000000, 400000, 0x0}, /* default */
+	{CLK_MAX_CPU_HZ, 400000, 0x0}, /* the default one */
 };
+
+#define CPU_DEFAULT_IDX	2
 
 /* IMPORTANT: array should be filled in increasing order
  * (regarding to .freq field)
  */
 static const struct freq_table ssp_freq[] = {
-	{ 19200000, 19200, CLOCK_SSP_XTAL_OSCILLATOR },
+	{ 19200000, 19200, CLOCK_SSP_XTAL_OSCILLATOR }, /* the default one */
 	{ 24576000, 24576, CLOCK_SSP_AUDIO_CARDINAL },
 	{ 96000000, 96000, CLOCK_SSP_PLL_FIXED },
 };
+
+#define SSP_DEFAULT_IDX	0
 
 #endif
