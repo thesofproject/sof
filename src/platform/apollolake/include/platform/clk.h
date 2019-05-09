e8472145b3489e1849ef57f849d441ee241e87ba
Marcin Maka <marcin.maka@linux.intel.com>
2019-05-09 12:16:18 +0200

    apollolake: clk defs cleanup, using common cavs defs
    Signed-off-by: Marcin Maka <marcin.maka@linux.intel.com>

--- src/platform/apollolake/include/platform/clk.h
+++ src/platform/apollolake/include/platform/clk.h
@@ -32,35 +32,6 @@
 #ifndef __PLATFORM_CLOCK__
 #define __PLATFORM_CLOCK__
 
-#include <sof/cpu.h>
-#include <sof/io.h>
-#include <platform/shim.h>
-
-#define CLK_CPU(x)	(x)
-#define CLK_SSP		2
-
-#define CPU_DEFAULT_IDX		2
-#define SSP_DEFAULT_IDX		0
-
-#define CLK_DEFAULT_CPU_HZ	400000000
-#define CLK_MAX_CPU_HZ		400000000
-
-#define NUM_CLOCKS	3
-
-static inline int clock_platform_set_cpu_freq(uint32_t cpu_freq_enc)
-{
-	/* set CPU frequency request for CCU */
-	io_reg_update_bits(SHIM_BASE + SHIM_CLKCTL, SHIM_CLKCTL_HDCS, 0);
-	io_reg_update_bits(SHIM_BASE + SHIM_CLKCTL,
-			   SHIM_CLKCTL_DPCS_MASK(cpu_get_id()),
-			   cpu_freq_enc);
-
-	return 0;
-}
-
-static inline int clock_platform_set_ssp_freq(uint32_t ssp_freq_enc)
-{
-	return 0;
-}
+#include <cavs/clk.h>
 
 #endif
