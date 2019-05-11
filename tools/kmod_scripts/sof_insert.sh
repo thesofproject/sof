4c060fd89f1e582a13b484c8333b2f36be734f6c
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-05-11 15:23:23 -0500

    tools/kmod_scripts: fix module lists
    I just wonder how many people run these module/remove tests? Clearly
    they have been broken for a while with modules missing from the lists,
    so we must have a parallel universe where CI operates.
    
    Fix by adding all the currently supported modules in the SOF kconfig.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- tools/kmod_scripts/sof_insert.sh
+++ tools/kmod_scripts/sof_insert.sh
@@ -12,14 +12,19 @@ insert_module() {
     fi
 }
 
+insert_module snd_soc_da7213
+insert_module snd_soc_da7219
+
+insert_module snd_soc_rt274
+insert_module snd_soc_rt286
+insert_module snd_soc_rt298
 insert_module snd_soc_rt5640
 insert_module snd_soc_rt5645
 insert_module snd_soc_rt5651
 insert_module snd_soc_rt5670
+insert_module snd_soc_rt5677_spi
 insert_module snd_soc_rt5682
-insert_module snd_soc_rt274
-insert_module snd_soc_da7213
-insert_module snd_soc_da7219
+
 insert_module snd_soc_pcm512x_i2c
 insert_module snd_soc_wm8804_i2c
 insert_module snd_soc_max98357a
