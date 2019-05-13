59ad3c6865a6ddf64a4775bb908fef13cec7e6d1
Slawomir Blauciak <slawomir.blauciak@linux.intel.com>
2019-05-13 13:46:35 +0200

    ipc: replace fw ready bitfield with explicit bit ordering
    Previously the structure used bitfields,
    which do not guarantee bit ordering.
    
    This change makes sure the order is clearly defined.
    It also renames and repurposes the field for general use,
    which helps avoid the need to introduce new flag fields.
    
    Signed-off-by: Slawomir Blauciak <slawomir.blauciak@linux.intel.com>

--- src/platform/haswell/platform.c
+++ src/platform/haswell/platform.c
@@ -77,7 +77,7 @@ static const struct sof_ipc_fw_ready ready
 		.tag = SOF_TAG,
 		.abi_version = SOF_ABI_VERSION,
 	},
-	.debug = DEBUG_SET_FW_READY_FLAGS,
+	.flags = DEBUG_SET_FW_READY_FLAGS,
 };
 
 #define NUM_HSW_WINDOWS		6
