02ffbea1a1c9036d4684249ab5ccc4656afc7c8d
Janusz Jankowski <janusz.jankowski@linux.intel.com>
2019-06-01 17:17:29 +0200

    test: kpb: add license
    Signed-off-by: Janusz Jankowski <janusz.jankowski@linux.intel.com>

--- test/cmocka/src/audio/kpb/kpb_mock.h
+++ test/cmocka/src/audio/kpb/kpb_mock.h
@@ -1,1 +1,8 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2019 Intel Corporation. All rights reserved.
+ *
+ * Author: Marcin Rajwa <marcin.rajwa@linux.intel.com>
+ */
+
 int comp_set_state(struct comp_dev *dev, int cmd);
