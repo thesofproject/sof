7a44ae013bad8884a4d180cf285db50c5852678f
Janusz Jankowski <janusz.jankowski@linux.intel.com>
2019-06-01 20:01:02 +0200

    rimage: change license to bsd
    Signed-off-by: Janusz Jankowski <janusz.jankowski@linux.intel.com>

--- rimage/man_cnl.c
+++ rimage/man_cnl.c
@@ -1,15 +1,6 @@
-/*
- * Copyright (c) 2017, Intel Corporation.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- */
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2017 Intel Corporation. All rights reserved.
 
 #include "css.h"
 #include "cse.h"
--- rimage/rimage.h
+++ rimage/rimage.h
@@ -1,16 +1,6 @@
-/*
- * ELF to firmware image creator.
+/* SPDX-License-Identifier: BSD-3-Clause
  *
- * Copyright (c) 2015-2018 Intel Corporation.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * Copyright(c) 2015-2018 Intel Corporation. All rights reserved.
  */
 
 #ifndef __RIMAGE_H__
