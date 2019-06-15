c4a7fe191ae47d183659ca2c6ac5d907ea385d68
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2019-06-15 23:02:46 +0200

    sys_agent: add option to temporarily enable/disable
    This patch adds an option to temporarily disable system
    agent. This is especially needed when SOF is performing
    long last tasks like draining.
    
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

--- src/include/sof/lib/agent.h
+++ src/include/sof/lib/agent.h
@@ -9,6 +9,7 @@
 #define __SOF_LIB_AGENT_H__
 
 #include <sof/schedule/task.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 struct sof;
@@ -18,9 +19,12 @@ struct sa {
 	uint64_t last_idle;	/* time of last idle */
 	uint64_t ticks;
 	struct task work;
+	bool is_active;
 };
 
 void sa_enter_idle(struct sof *sof);
 void sa_init(struct sof *sof);
+void sa_disable(void);
+void sa_enable(void);
 
 #endif /* __SOF_LIB_AGENT_H__ */
