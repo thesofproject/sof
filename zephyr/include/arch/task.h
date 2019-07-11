6d95ae414fbefc4182bb3a7bbc52363745322a97
Calvin Ageneau <calvin.ageneau@st.com>
2019-07-11 13:47:35 +0200

    zephyr: removing host library includes
    Signed-off-by: Calvin Ageneau <calvin.ageneau@st.com>

---
+++ zephyr/include/arch/task.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2017 Intel Corporation. All rights reserved.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ */
+
+/**
+ * \file arch/xtensa/include/arch/task.h
+ * \brief Arch task header file
+ * \authors Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ */
+
+#ifndef __ARCH_TASK_H_
+#define __ARCH_TASK_H_
+
+#include <sof/schedule.h>
+
+/**
+ * \brief Allocates IRQ tasks.
+ */
+static inline int arch_allocate_tasks(void)
+{
+	return 0;
+}
+
+/**
+ * \brief Runs task.
+ * \param[in,out] task Task data.
+ */
+static inline int arch_run_task(struct task *task)
+{
+	return 0;
+}
+
+#endif
