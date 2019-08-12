8a4ec5643f4921c4415e110049cae865745154cd
Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>
2019-08-12 11:28:37 +0200

    topology: add sof_ipc_comp_smart_amp struct
    Add SOF_COMP_SMART_AMP type for smart amp component.
    Basic topology using smart amplifier component
    could be as follows:
    
                    +------------------------------------------+
        +------+    | +---+  +-------------+  +---+  +-------+ |
        | Host |----->|Buf|->|  Smart Amp  |->|Buf|->|SSP Dai|-----------+
        +------+    | +---+  +-------------+  +---+  +-------+ |         |
                    +-----------------^------------------------+         |
                                      |                                  |
                                    +---+                                |
                                    |Buf|                                |
                                    +---+                                |
                                      ^                                  |
                    +-----------------|--------------------------+       |
        +------+    | +---+  +--------|------+  +---+  +-------+ |       |
        | Host |<-----|Buf|<-|     Demux     |<-|Buf|<-|SSP Dai|<--------+
        +------+    | +---+  +---------------+  +---+  +-------+ |
                    +--------------------------------------------+
    
    Basic topology contains Smart Amp component in playback pipeline and Demux
    component in capture pipeline. There is a additional buffer (feedback)
    between them in order to process "feedback".
    
    Signed-off-by: Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>

--- src/include/ipc/topology.h
+++ src/include/ipc/topology.h
@@ -46,6 +46,7 @@ enum sof_comp_type {
 	SOF_COMP_DEMUX,
 	SOF_COMP_ASRC,		/**< Asynchronous sample rate converter */
 	SOF_COMP_DCBLOCK,
+	SOF_COMP_SMART_AMP,		/**< smart amplifier component */
 	/* keep FILEREAD/FILEWRITE as the last ones */
 	SOF_COMP_FILEREAD = 10000,	/**< host test based file IO */
 	SOF_COMP_FILEWRITE = 10001,	/**< host test based file IO */
