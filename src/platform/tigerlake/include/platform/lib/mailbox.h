4aac94b405b2a6b6479ab9fb721e294920500af1
Janusz Jankowski <janusz.jankowski@linux.intel.com>
2019-08-23 12:14:51 +0200

    platform: tigerlake
    Initial version of TGL platform - based on ICL.
    
    Signed-off-by: Janusz Jankowski <janusz.jankowski@linux.intel.com>

---
+++ src/platform/tigerlake/include/platform/lib/mailbox.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2017 Intel Corporation. All rights reserved.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ *         Rander Wang <rander.wang@intel.com>
+ */
+
+#ifdef __SOF_LIB_MAILBOX_H__
+
+#ifndef __PLATFORM_LIB_MAILBOX_H__
+#define __PLATFORM_LIB_MAILBOX_H__
+
+#include <cavs/lib/mailbox.h>
+
+#endif /* __PLATFORM_LIB_MAILBOX_H__ */
+
+#else
+
+#error "This file shouldn't be included from outside of sof/lib/mailbox.h"
+
+#endif /* __SOF_LIB_MAILBOX_H__ */
