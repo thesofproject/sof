aa231ef90ac775e8f060575e20eec54797af7464
Daniel Leung <daniel.leung@intel.com>
2019-08-23 22:37:20 -0700

    zephyr: partially implement arch_interrupt_*()
    Implement the get_level() and global_en-/dis-able() to satisfy
    SOF's use of interrupts. Other functions are still stubs to
    avoid disabling interrupts needed for Zephyr.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- zephyr/include/arch/interrupt.h
+++ zephyr/include/arch/interrupt.h
@@ -8,7 +8,8 @@
 #ifndef __ARCH_INTERRUPT_H
 #define __ARCH_INTERRUPT_H
 
-#include <sof/interrupt-map.h>
+#include <irq.h>
+
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -17,14 +18,34 @@ static inline int arch_interrupt_register(int irq,
 static inline void arch_interrupt_unregister(int irq) {}
 static inline uint32_t arch_interrupt_enable_mask(uint32_t mask) {return 0; }
 static inline uint32_t arch_interrupt_disable_mask(uint32_t mask) {return 0; }
-static inline uint32_t arch_interrupt_get_level(void) { return 0; }
+
+static inline uint32_t arch_interrupt_get_level(void)
+{
+	uint32_t level;
+
+	__asm__ __volatile__(
+		"       rsr.ps %0\n"
+		"       extui  %0, %0, 0, 4\n"
+		: "=&a" (level) :: "memory");
+
+	return level;
+}
+
 static inline void arch_interrupt_set(int irq) {}
 static inline void arch_interrupt_clear(int irq) {}
 static inline uint32_t arch_interrupt_get_enabled(void) {return 0; }
 static inline uint32_t arch_interrupt_get_status(void) {return 0; }
-static inline uint32_t arch_interrupt_global_disable(void) {return 0; }
-static inline void arch_interrupt_global_enable(uint32_t flags) {}
 static inline int arch_interrupt_init(void) {return 0; }
+
+static inline uint32_t arch_interrupt_global_disable(void)
+{
+	return irq_lock();
+}
+
+static inline void arch_interrupt_global_enable(uint32_t flags)
+{
+	irq_unlock(flags);
+}
 
 #endif
 
