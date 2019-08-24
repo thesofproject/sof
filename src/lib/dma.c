29720016b52580277509a24992383fd6e2105033
Daniel Leung <daniel.leung@intel.com>
2019-08-23 22:52:28 -0700

    lib/dma: fix passing an int to pointer argument formatted string
    The formatted string wants a pointer but an interger (pointer casted
    as integer) is passed instead. So remove the cast.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- src/lib/dma.c
+++ src/lib/dma.c
@@ -137,7 +137,7 @@ void dma_put(struct dma *dma)
 		}
 	}
 	trace_event(TRACE_CLASS_DMA, "dma_put(), dma = %p, sref = %d",
-		   (uintptr_t)dma, dma->sref);
+		    dma, dma->sref);
 	spin_unlock(&dma->lock);
 }
 
