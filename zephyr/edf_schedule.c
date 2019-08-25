04ca0b0116efd540a2a95109dc5e4fc0561f743f
Daniel Leung <daniel.leung@intel.com>
2019-08-25 00:19:08 -0700

    zephyr: implement edf/ll schedulers as work queues
    This provides a very primitive implementations of both EDF and LL
    schedulers as Zephyr work queues. This allows processing of IPC
    commands without the previous work queue modification of ipc.c.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- zephyr/edf_schedule.c
+++ zephyr/edf_schedule.c
@@ -9,31 +9,21 @@
 #include <stdint.h>
 #include <sof/edf_schedule.h>
 #include <sof/wait.h>
+
 #include <kernel.h>
 
- /* scheduler testbench definition */
+struct k_work_q edf_workq;
+K_THREAD_STACK_DEFINE(edf_workq_stack, 8192);
 
-struct edf_schedule_data {
-	spinlock_t lock; /* schedule lock */
-	struct list_item list; /* list of tasks in priority queue */
-	uint32_t clock;
-};
-
-static struct edf_schedule_data *sch;
-
-static void schedule_edf_task_complete(struct task *task);
-static void schedule_edf_task(struct task *task, uint64_t start,
-			      uint64_t deadline, uint32_t flags);
-static int schedule_edf_task_init(struct task *task, uint32_t xflags);
-static int edf_scheduler_init(void);
-static void edf_scheduler_free(void);
-static void schedule_edf(void);
-static int schedule_edf_task_cancel(struct task *task);
-static void schedule_edf_task_free(struct task *task);
-
-static void schedule_edf_task_complete(struct task *task)
+static void edf_work_handler(struct k_work *work)
 {
-	list_item_del(&task->list);
+	struct task *task = CONTAINER_OF(work, struct task, z_delayed_work);
+
+	task->state = SOF_TASK_STATE_RUNNING;
+
+	if (task->func)
+		task->func(task->data);
+
 	task->state = SOF_TASK_STATE_COMPLETED;
 }
 
@@ -41,23 +31,16 @@ static void schedule_edf_task_complete(struct task *task)
 static void schedule_edf_task(struct task *task, uint64_t start,
 			      uint64_t deadline, uint32_t flags)
 {
-	(void)deadline;
-	list_item_prepend(&task->list, &sch->list);
+	k_delayed_work_submit_to_queue_us(&edf_workq,
+					  &task->z_delayed_work,
+					  start);
+
 	task->state = SOF_TASK_STATE_QUEUED;
-
-	if (task->func)
-		task->func(task->data);
-
-	schedule_edf_task_complete(task);
 }
 
 static int schedule_edf_task_init(struct task *task, uint32_t xflags)
 {
-	struct edf_task_pdata *edf_pdata;
-	(void)xflags;
-
-	edf_pdata = malloc(sizeof(*edf_pdata));
-	edf_sch_set_pdata(task, edf_pdata);
+	k_delayed_work_init(&task->z_delayed_work, edf_work_handler);
 
 	return 0;
 }
@@ -66,32 +49,28 @@ static int schedule_edf_task_init(struct task *task, uint32_t xflags)
 static int edf_scheduler_init(void)
 {
 	trace_edf_sch("edf_scheduler_init()");
-	sch = k_malloc(sizeof(*sch));
-	list_init(&sch->list);
-	spinlock_init(&sch->lock);
+
+	k_work_q_start(&edf_workq,
+		       edf_workq_stack,
+		       K_THREAD_STACK_SIZEOF(edf_workq_stack),
+		       -1);
+	k_thread_name_set(&edf_workq.thread, "edf_workq");
 
 	return 0;
-}
-
-static void edf_scheduler_free(void)
-{
-	free(sch);
-}
-
-/* The following definitions are to satisfy libsof linker errors */
-static void schedule_edf(void)
-{
 }
 
 static int schedule_edf_task_cancel(struct task *task)
 {
+	int ret = 0;
+
 	if (task->state == SOF_TASK_STATE_QUEUED) {
+		ret = k_delayed_work_cancel(&task->z_delayed_work);
+
 		/* delete task */
 		task->state = SOF_TASK_STATE_CANCEL;
-		list_item_del(&task->list);
 	}
 
-	return 0;
+	return ret;
 }
 
 static void schedule_edf_task_free(struct task *task)
@@ -99,9 +78,6 @@ static void schedule_edf_task_free(struct task *task)
 	task->state = SOF_TASK_STATE_FREE;
 	task->func = NULL;
 	task->data = NULL;
-
-	free(edf_sch_get_pdata(task));
-	edf_sch_set_pdata(task, NULL);
 }
 
 struct scheduler_ops schedule_edf_ops = {
@@ -113,6 +89,6 @@ struct scheduler_ops schedule_edf_ops = {
 	.schedule_task_cancel	= schedule_edf_task_cancel,
 	.schedule_task_free	= schedule_edf_task_free,
 	.scheduler_init		= edf_scheduler_init,
-	.scheduler_free		= edf_scheduler_free,
-	.scheduler_run		= schedule_edf
+	.scheduler_free		= NULL,
+	.scheduler_run		= NULL,
 };
