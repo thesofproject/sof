0e454831e2424abd1fdba8bca826f1ec99fb5126
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2019-08-26 15:24:28 +0300

    zephyr: Implement panic() function
    Print trace error and go to endless loop.

--- zephyr/panic.c
+++ zephyr/panic.c
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
 // Copyright(c) 2019 Intel Corporation. All rights reserved.
-//
-// Author: Tomasz Lauda <tomasz.lauda@linux.intel.com>
 
 #include <sof/panic.h>
+#include <sof/trace.h>
 
-void __panic(uint32_t p, char *filename, uint32_t linenum) { }
+void __panic(uint32_t p, char *filename, uint32_t linenum)
+{
+	trace_error(TRACE_CLASS_MEM, "%s : %u", filename, linenum);
+
+	while (1);
+}
