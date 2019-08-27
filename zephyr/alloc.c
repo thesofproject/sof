707c24edfa628b1f2da922483b206bda56ff72e3
Daniel Leung <danielcp@gmail.com>
2019-08-27 00:07:21 -0700

    zephyr: use mem_slab for buffers
    Zephyr's k_mem_pool does not allocate buffers that are cache line
    and/or DMA transfers aligned. The manual cache line manipulations
    has the potential to disrupt the k_mem_pool's own tracking
    information, and thus causing exceptions in freeing the memory.
    So the memory allocator is modified to use mem_slab for buffers.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- zephyr/alloc.c
+++ zephyr/alloc.c
@@ -12,34 +12,110 @@
 #include <stdio.h>
 #include <sof/alloc.h>
 #include <kernel.h>
+#include <sys/util.h>
 
-/* testbench mem alloc definition */
+/* Describe one mem_slab */
+struct memslab_map {
+	uint16_t block_size;    /* size of block in bytes */
+	uint16_t count;         /* number of blocks */
+	uint32_t size;          /* size of buffer */
+	struct k_mem_slab slab;
+	void* buffer;           /* pointer to buffer */
+} __aligned(PLATFORM_DCACHE_ALIGN);
 
-void *rmalloc(int zone, uint32_t caps, size_t bytes)
+#define MEMSLAB_DEF(sz, cnt, buf) \
+	{ .block_size = sz, .count = cnt, .buffer = buf, \
+	  .size = (sz * cnt), \
+	}
+
+#define BUF_ALIGN	__aligned(WB_UP(PLATFORM_DCACHE_ALIGN))
+
+
+/*
+ * RZONE_BUFFER for DMA
+ */
+static struct k_mem_slab hp_dma_buffer;
+static struct memslab_map hp_dma_buffer_map =
+	MEMSLAB_DEF(HEAP_HP_BUFFER_BLOCK_SIZE, HEAP_HP_BUFFER_COUNT,
+		    (void *)HEAP_HP_BUFFER_BASE);
+
+/*
+ * RZONE_BUFFER for non-DMA
+ *
+ * TODO: figure out a good block size.
+ */
+#define _HEAP_BUFFER_BLOCK_SIZE		1024
+#define _HEAP_BUFFER_COUNT		32
+#define _HEAP_BUFFER_SIZE \
+	(_HEAP_BUFFER_COUNT * _HEAP_BUFFER_BLOCK_SIZE)
+
+static struct k_mem_slab heap_buffer;
+static BUF_ALIGN uint8_t _heap_buf[_HEAP_BUFFER_SIZE];
+static struct memslab_map heap_buffer_map =
+	MEMSLAB_DEF(_HEAP_BUFFER_BLOCK_SIZE,
+		    _HEAP_BUFFER_COUNT,
+		    _heap_buf);
+
+
+void malloc_init(void)
+{
+	/* Initialize RZONE_BUFFER */
+	k_mem_slab_init(&hp_dma_buffer, (void *)HEAP_HP_BUFFER_BASE,
+			HEAP_HP_BUFFER_BLOCK_SIZE, HEAP_HP_BUFFER_COUNT);
+
+	k_mem_slab_init(&heap_buffer, _heap_buf,
+			_HEAP_BUFFER_BLOCK_SIZE, _HEAP_BUFFER_COUNT);
+}
+
+
+void *rmalloc(uint32_t zone, uint32_t caps, size_t bytes)
 {
 	return k_malloc(bytes);
 }
 
-void *rzalloc(int zone, uint32_t caps, size_t bytes)
+void *rzalloc(uint32_t zone, uint32_t caps, size_t bytes)
 {
 	return k_calloc(bytes, 1);
 }
 
+void *rballoc(uint32_t zone, uint32_t caps, size_t bytes)
+{
+	void *ptr = NULL;
+
+	if (caps & SOF_MEM_CAPS_DMA) {
+		k_mem_slab_alloc(&heap_buffer, &ptr, K_NO_WAIT);
+	} else {
+		k_mem_slab_alloc(&hp_dma_buffer, &ptr, K_NO_WAIT);
+	}
+
+	return ptr;
+}
+
+static bool free_in_slab(struct memslab_map *map, void *ptr)
+{
+	uintptr_t begin = (uintptr_t)map->buffer;
+	uintptr_t end = begin + map->size;
+	uintptr_t addr = (uintptr_t)ptr;
+
+	if ((addr >= begin) && (addr < end)) {
+		k_mem_slab_free(&map->slab, &ptr);
+		return true;
+	}
+
+	return false;
+}
+
 void rfree(void *ptr)
 {
+	/* RZONE_BUFFER */
+	if (free_in_slab(&hp_dma_buffer_map, ptr)) {
+		return;
+	}
+
+	if (free_in_slab(&heap_buffer_map, ptr)) {
+		return;
+	}
+
+	/* Not in mem_slab, so assume k_mem_pool */
 	k_free(ptr);
-}
-
-void *rballoc(int zone, uint32_t caps, size_t bytes)
-{
-	return k_malloc(bytes);
-}
-
-void heap_trace(struct mm_heap *heap, int size)
-{
-}
-
-void heap_trace_all(int force)
-{
-	heap_trace(NULL, 0);
 }
