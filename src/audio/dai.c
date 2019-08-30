b2bb06379f4286aaa7dc68edee597147a70e0495
Rander Wang <rander.wang@linux.intel.com>
2019-08-30 09:47:45 +0800

    alh: fix playback & capture issue with 16/24bit pipeline
    Now the sdw HW FIFO only accept 32bit sample, so force
    dai format to 32bit to let volume component do the
    conversion.
    
    Signed-off-by: Rander Wang <rander.wang@linux.intel.com>

--- src/audio/dai.c
+++ src/audio/dai.c
@@ -676,6 +676,7 @@ static int dai_position(struct comp_dev *dev, struct sof_ipc_stream_posn *posn)
 
 static int dai_config(struct comp_dev *dev, struct sof_ipc_dai_config *config)
 {
+	struct sof_ipc_comp_config *dconfig = COMP_GET_CONFIG(dev);
 	struct dai_data *dd = comp_get_drvdata(dev);
 	int channel = 0;
 	int i;
@@ -773,6 +774,12 @@ static int dai_config(struct comp_dev *dev, struct sof_ipc_dai_config *config)
 		 * it is recalculated in dai_params() later
 		 */
 		dd->frame_bytes = 4;
+
+		/* SDW HW FIFO requires 32bit MSB aligned sample data for
+		 * all formats, such as 8/16/24/32 bits. Now set frame_fmt
+		 * to S32_LE to let volume component do the conversion.
+		 */
+		dconfig->frame_fmt = SOF_IPC_FRAME_S32_LE;
 
 		/* As with HDA, the DMA channel is assigned in runtime,
 		 * not during topology parsing.
