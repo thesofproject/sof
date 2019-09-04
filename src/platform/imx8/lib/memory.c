2036b0f09f0d9be47f6797752bd42f40554d6560
Paul Olaru <paul.olaru@nxp.com>
2019-09-04 16:12:13 +0300

    platform: imx: Add 4 2048-byte blocks in memory map
    Some drivers require allocating blocks larger than 1024 bytes. Since
    i.MX has plenty of memory free for buffers, getting 8kB and turning them
    into actual runtime memory blocks of size 2048 is fine.
    
    Signed-off-by: Paul Olaru <paul.olaru@nxp.com>

--- src/platform/imx8/lib/memory.c
+++ src/platform/imx8/lib/memory.c
@@ -30,6 +30,7 @@ static struct block_hdr mod_block128[HEAP_RT_COUNT128];
 static struct block_hdr mod_block256[HEAP_RT_COUNT256];
 static struct block_hdr mod_block512[HEAP_RT_COUNT512];
 static struct block_hdr mod_block1024[HEAP_RT_COUNT1024];
+static struct block_hdr mod_block2048[HEAP_RT_COUNT2048];
 
 /* Heap memory map for modules */
 static struct block_map rt_heap_map[] = {
@@ -40,6 +41,7 @@ static struct block_map rt_heap_map[] = {
 	BLOCK_DEF(256, HEAP_RT_COUNT256, mod_block256),
 	BLOCK_DEF(512, HEAP_RT_COUNT512, mod_block512),
 	BLOCK_DEF(1024, HEAP_RT_COUNT1024, mod_block1024),
+	BLOCK_DEF(2048, HEAP_RT_COUNT2048, mod_block2048),
 };
 
 /* Heap blocks for buffers */
