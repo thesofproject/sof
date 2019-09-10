9e60405c071547dfc8d9f2a58d3fec71b0079647
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2019-09-10 16:34:04 +0200

    platform: remove unused PLATFORM_LINK_DMA_TIMEOUT define
    Removes unused define PLATFORM_LINK_DMA_TIMEOUT.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/platform/icelake/include/platform/platform.h
+++ src/platform/icelake/include/platform/platform.h
@@ -85,12 +85,6 @@ struct timer;
 /* DMA host transfer timeouts in microseconds */
 #define PLATFORM_HOST_DMA_TIMEOUT	200
 
-/* DMA link transfer timeouts in microseconds
- * TODO: timeout should be reduced
- * (DMA might needs some further changes to do that)
- */
-#define PLATFORM_LINK_DMA_TIMEOUT	1000
-
 /* WorkQ window size in microseconds */
 #define PLATFORM_WORKQ_WINDOW	2000
 
