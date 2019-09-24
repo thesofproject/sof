6b0529e79eed731273ac615ee0408baf7244efe6
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2019-09-24 16:43:25 +0300

    ASRC: Add filter coefficients for ASRC
    This patch adds the FIR filter coefficients and filter parameters
    header files for the ASRC.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

---
+++ src/include/sof/audio/coefficients/asrc/asrc_farrow_param_24000Hz_to_16000Hz.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2012-2019 Intel Corporation. All rights reserved.
+ */
+
+/* Conversion from 24000 Hz to 16000 Hz */
+
+{
+	80,
+	6
+},
