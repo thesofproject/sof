e2c24e83fba6661f16ddc6e039deb1916d7d67cf
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2019-09-27 10:53:36 +0200

    platform: define and use CONFIG_SYSTICK_PERIOD
    Adds additional CONFIG_SYSTICK_PERIOD to Kconfig,
    which is used to drive timer based low latency scheduler
    and also will be used as a timeout check value for system agent.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/platform/imx8/platform.c
+++ src/platform/imx8/platform.c
@@ -150,7 +150,7 @@ int platform_init(struct sof *sof)
 	/* init low latency domains and schedulers */
 	platform_timer_domain =
 		timer_domain_init(platform_timer, PLATFORM_DEFAULT_CLOCK,
-				  PLATFORM_LL_DEFAULT_TIMEOUT);
+				  CONFIG_SYSTICK_PERIOD);
 	scheduler_init_ll(platform_timer_domain);
 
 	/* Init EDMA platform domain */
