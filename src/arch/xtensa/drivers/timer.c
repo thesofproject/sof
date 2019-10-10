thesofproject/sof

commit 75188e22434d58720f59624aa4eedd56afc6dd26

timer: rework timer_set() so it verifies requested ticks
This patch reworks cAVS timer_set() interface used
to write into timer's compare register.
This change takes care of ticks request which are
"past time". If such condition occur we set compare
register to current time so the latency is minimum.

Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

src/arch/xtensa/drivers/timer.c
@@ -103,7 +103,7 @@ uint64_t arch_timer_get_system(struct timer *timer)
 	return time;
 }
 
-int arch_timer_set(struct timer *timer, uint64_t ticks)
+int64_t arch_timer_set(struct timer *timer, uint64_t ticks)
 {
 	uint32_t time = 1;
 	uint32_t hitimeout = ticks >> 32;

src/drivers/intel/baytrail/timer.c
@@ -75,7 +75,7 @@ void platform_timer_stop(struct timer *timer)
 	shim_write(SHIM_EXT_TIMER_CNTLH, SHIM_EXT_TIMER_CLEAR);
 }
 
-int platform_timer_set(struct timer *timer, uint64_t ticks)
+int64_t platform_timer_set(struct timer *timer, uint64_t ticks)
 {
 	uint32_t time = 1;
 	uint32_t hitimeout = ticks >> 32;

src/drivers/imx/timer.c
@@ -22,7 +22,7 @@ void platform_timer_stop(struct timer *timer)
 	arch_timer_disable(timer);
 }
 
-int platform_timer_set(struct timer *timer, uint64_t ticks)
+int64_t platform_timer_set(struct timer *timer, uint64_t ticks)
 {
 	return arch_timer_set(timer, ticks);
 }

src/drivers/intel/cavs/timer.c
@@ -32,17 +32,22 @@ void platform_timer_stop(struct timer *timer)
 		   shim_read(SHIM_DSPWCTCS) & ~SHIM_DSPWCTCS_T0A);
 }
 
-int platform_timer_set(struct timer *timer, uint64_t ticks)
+int64_t platform_timer_set(struct timer *timer, uint64_t ticks)
 {
 	/* a tick value of 0 will not generate an IRQ */
 	if (ticks == 0)
 		ticks = 1;
 
-	/* set new value and run */
-	shim_write64(SHIM_DSPWCT0C, ticks);
+	/* Check if requested time is not past time */
+	if (ticks > shim_read64(SHIM_DSPWC))
+		shim_write64(SHIM_DSPWCT0C, ticks);
+	else
+		shim_write64(SHIM_DSPWCT0C, shim_read64(SHIM_DSPWC) + 1);
+
+	/* Enable IRQ */
 	shim_write(SHIM_DSPWCTCS, SHIM_DSPWCTCS_T0A);
 
-	return 0;
+	return shim_read64(SHIM_DSPWCT0C);
 }
 
 void platform_timer_clear(struct timer *timer)

src/drivers/intel/haswell/timer.c
@@ -23,7 +23,7 @@ void platform_timer_stop(struct timer *timer)
 	arch_timer_disable(timer);
 }
 
-int platform_timer_set(struct timer *timer, uint64_t ticks)
+int64_t platform_timer_set(struct timer *timer, uint64_t ticks)
 {
 	return arch_timer_set(timer, ticks);
 }

src/arch/host/include/arch/drivers/timer.h
@@ -21,8 +21,8 @@ static inline void arch_timer_unregister(struct timer *timer) {}
 static inline void arch_timer_enable(struct timer *timer) {}
 static inline void arch_timer_disable(struct timer *timer) {}
 static inline uint32_t arch_timer_get_system(struct timer *timer) {return 0; }
-static inline int arch_timer_set(struct timer *timer,
-	uint64_t ticks) {return 0; }
+static inline int64_t arch_timer_set(struct timer *timer,
+				     uint64_t ticks) {return 0; }
 static inline void arch_timer_clear(struct timer *timer) {}
 
 #endif /* __ARCH_DRIVERS_TIMER_H__ */

src/arch/xtensa/include/arch/drivers/timer.h
@@ -63,7 +63,7 @@ static inline void arch_timer_disable(struct timer *timer)
 
 uint64_t arch_timer_get_system(struct timer *timer);
 
-int arch_timer_set(struct timer *timer, uint64_t ticks);
+int64_t arch_timer_set(struct timer *timer, uint64_t ticks);
 
 static inline void arch_timer_clear(struct timer *timer)
 {

src/include/sof/drivers/timer.h
@@ -25,7 +25,7 @@ void timer_unregister(struct timer *timer, void *arg);
 void timer_enable(struct timer *timer, void *arg, int core);
 void timer_disable(struct timer *timer, void *arg, int core);
 
-static inline int timer_set(struct timer *timer, uint64_t ticks)
+static inline int64_t timer_set(struct timer *timer, uint64_t ticks)
 {
 	return arch_timer_set(timer, ticks);
 }
@@ -46,7 +46,7 @@ static inline uint64_t timer_get_system(struct timer *timer)
 	return arch_timer_get_system(timer);
 }
 
-int platform_timer_set(struct timer *timer, uint64_t ticks);
+int64_t platform_timer_set(struct timer *timer, uint64_t ticks);
 void platform_timer_clear(struct timer *timer);
 uint64_t platform_timer_get(struct timer *timer);
 void platform_timer_start(struct timer *timer);

commit 938652a4dde57859f492f7da054d8e775410ca92

timer: fix return values of platform_timer_set and arch_timer_set
Fixes return value of platform_timer_set and arch_timer_set functions.
Change was introduced for cAVS platforms, which accidentally broke
these functions for other platforms. The intention of this functions
is to return value of set ticks, so the timer scheduler can check
for delays.

Fixes: 75188e22434d ("timer: rework timer_set() so it verifies requested ticks")

Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

src/arch/xtensa/drivers/timer.c
@@ -133,5 +133,5 @@ int64_t arch_timer_set(struct timer *timer, uint64_t ticks)
 	xthal_set_ccompare(timer->id, time);
 
 	arch_interrupt_global_enable(flags);
-	return 0;
+	return ticks;
 }

src/drivers/intel/baytrail/timer.c
@@ -106,7 +106,7 @@ int64_t platform_timer_set(struct timer *timer, uint64_t ticks)
 
 	arch_interrupt_global_enable(flags);
 
-	return 0;
+	return ticks;
 }
 
 void platform_timer_clear(struct timer *timer)