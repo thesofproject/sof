thesofproject/sof

commit de7d4c95cfbde36183ddfb08203520dd5528eda4

pipeline: allocate pipe_task only if needed
Changes implementation of pipe_task allocation and initialization
to happen in prepare call instead of during pipeline creations.
It might be that the pipeline will never be used.

Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

src/audio/pipeline.c
@@ -44,7 +44,6 @@ struct pipeline *pipeline_new(struct sof_ipc_pipe_new *pipe_desc,
 			      struct comp_dev *cd)
 {
 	struct pipeline *p;
-	uint32_t type;
 
 	trace_pipe("pipeline_new()");
 
@@ -61,12 +60,6 @@ struct pipeline *pipeline_new(struct sof_ipc_pipe_new *pipe_desc,
 
 	assert(!memcpy_s(&p->ipc_pipe, sizeof(p->ipc_pipe),
 	   pipe_desc, sizeof(*pipe_desc)));
-
-	/* get pipeline task type */
-	type = pipeline_is_timer_driven(p) ? SOF_SCHEDULE_LL :
-		SOF_SCHEDULE_EDF;
-	schedule_task_init(&p->pipe_task, type, pipe_desc->priority,
-			   pipeline_task, NULL, p, pipe_desc->core, 0);
 
 	return p;
 }
@@ -236,7 +229,8 @@ int pipeline_free(struct pipeline *p)
 	}
 
 	/* remove from any scheduling */
-	schedule_task_free(&p->pipe_task);
+	if (p->pipe_task)
+		schedule_task_free(p->pipe_task);
 
 	/* now free the pipeline */
 	rfree(p);
@@ -390,10 +384,25 @@ static int pipeline_comp_prepare(struct comp_dev *current, void *data, int dir)
 				      &buffer_reset_pos, dir);
 }
 
+static struct task *pipeline_task_init(struct pipeline *p, uint32_t type,
+				       enum task_state (*func)(void *data))
+{
+	struct task *task = NULL;
+
+	task = rzalloc(RZONE_RUNTIME, SOF_MEM_CAPS_RAM, sizeof(*task));
+
+	if (task)
+		schedule_task_init(task, type, p->ipc_pipe.priority, func, NULL,
+				   p, p->ipc_pipe.core, 0);
+
+	return task;
+}
+
 /* prepare the pipeline for usage - preload host buffers here */
 int pipeline_prepare(struct pipeline *p, struct comp_dev *dev)
 {
 	struct pipeline_data ppl_data;
+	uint32_t type;
 	int ret = 0;
 
 	trace_pipe_with_ids(p, "pipeline_prepare()");
@@ -412,6 +421,20 @@ int pipeline_prepare(struct pipeline *p, struct comp_dev *dev)
 	 */
 	p->preload = dev->params.direction == SOF_IPC_STREAM_PLAYBACK &&
 		p->sink_comp->state != COMP_STATE_ACTIVE;
+
+	/* initialize task if necessary */
+	if (!p->pipe_task) {
+		type = pipeline_is_timer_driven(p) ? SOF_SCHEDULE_LL :
+			SOF_SCHEDULE_EDF;
+
+		p->pipe_task = pipeline_task_init(p, type, pipeline_task);
+		if (!p->pipe_task) {
+			trace_pipe_error("pipeline_prepare() error: task init "
+					 "failed");
+			return -ENOMEM;
+		}
+	}
+
 	p->status = COMP_STATE_PREPARE;
 
 	return ret;
@@ -444,8 +467,10 @@ void pipeline_cache(struct pipeline *p, struct comp_dev *dev, int cmd)
 	uint32_t flags;
 
 	/* pipeline needs to be invalidated before usage */
-	if (cmd == CACHE_INVALIDATE)
+	if (cmd == CACHE_INVALIDATE) {
 		dcache_invalidate_region(p, sizeof(*p));
+		dcache_invalidate_region(p->pipe_task, sizeof(*p->pipe_task));
+	}
 
 	trace_pipe_with_ids(p, "pipeline_cache()");
 
@@ -458,8 +483,11 @@ void pipeline_cache(struct pipeline *p, struct comp_dev *dev, int cmd)
 	pipeline_comp_cache(dev, &data, dev->params.direction);
 
 	/* pipeline needs to be flushed after usage */
-	if (cmd == CACHE_WRITEBACK_INV)
+	if (cmd == CACHE_WRITEBACK_INV) {
+		dcache_writeback_invalidate_region(p->pipe_task,
+						   sizeof(*p->pipe_task));
 		dcache_writeback_invalidate_region(p, sizeof(*p));
+	}
 
 	irq_local_enable(flags);
 }
@@ -943,12 +971,12 @@ static int pipeline_xrun_recover(struct pipeline *p)
 void pipeline_schedule_copy(struct pipeline *p, uint64_t start)
 {
 	if (p->sched_comp->state == COMP_STATE_ACTIVE)
-		schedule_task(&p->pipe_task, start, p->ipc_pipe.period);
+		schedule_task(p->pipe_task, start, p->ipc_pipe.period);
 }
 
 void pipeline_schedule_cancel(struct pipeline *p)
 {
-	schedule_task_cancel(&p->pipe_task);
+	schedule_task_cancel(p->pipe_task);
 }
 
 static enum task_state pipeline_task(void *arg)

src/drivers/intel/cavs/idc.c
@@ -192,8 +192,9 @@ static int idc_pipeline_trigger(uint32_t cmd)
 		 * Should be removed after changing memory management for
 		 * slave cores.
 		 */
-		if (pcm_dev->cd->pipeline->pipe_task.type == SOF_SCHEDULE_EDF) {
-			task = &pcm_dev->cd->pipeline->pipe_task;
+		if (pcm_dev->cd->pipeline->pipe_task->type ==
+		    SOF_SCHEDULE_EDF) {
+			task = pcm_dev->cd->pipeline->pipe_task;
 			edf_pdata = edf_sch_get_pdata(task);
 			dcache_invalidate_region(edf_pdata, sizeof(*edf_pdata));
 			task_context_cache(edf_pdata->ctx, CACHE_INVALIDATE);
@@ -213,8 +214,9 @@ static int idc_pipeline_trigger(uint32_t cmd)
 		 * Should be removed after changing memory management for
 		 * slave cores.
 		 */
-		if (pcm_dev->cd->pipeline->pipe_task.type == SOF_SCHEDULE_EDF) {
-			task = &pcm_dev->cd->pipeline->pipe_task;
+		if (pcm_dev->cd->pipeline->pipe_task->type ==
+		    SOF_SCHEDULE_EDF) {
+			task = pcm_dev->cd->pipeline->pipe_task;
 			edf_pdata = edf_sch_get_pdata(task);
 			task_context_cache(edf_pdata->ctx, CACHE_WRITEBACK_INV);
 			dcache_writeback_invalidate_region(edf_pdata,

src/include/sof/audio/pipeline.h
@@ -78,7 +78,7 @@ struct pipeline {
 	bool preload;			/* is pipeline preload needed */
 
 	/* scheduling */
-	struct task pipe_task;		/* pipeline processing task */
+	struct task *pipe_task;		/* pipeline processing task */
 
 	/* component that drives scheduling in this pipe */
 	struct comp_dev *sched_comp;

test/cmocka/src/audio/pipeline/pipeline_connection_mocks.c
@@ -37,7 +37,9 @@ struct pipeline_connect_data *get_standard_connect_objects(void)
 
 	pipe->ipc_pipe = pipe_desc;
 	pipe->status = COMP_STATE_INIT;
-	pipe->pipe_task.type = SOF_SCHEDULE_EDF;
+
+	pipe->pipe_task = calloc(sizeof(struct task), 1);
+	pipe->pipe_task->type = SOF_SCHEDULE_EDF;
 
 	schedulers = calloc(sizeof(struct schedulers), 1);
 	list_init(&schedulers->list);

test/cmocka/src/audio/pipeline/pipeline_free.c
@@ -73,9 +73,9 @@ static void test_audio_pipeline_free_sheduler_task_free(void **state)
 	/*Testing component*/
 	pipeline_free(&result);
 
-	assert_int_equal(result.pipe_task.state, SOF_TASK_STATE_FREE);
-	assert_ptr_equal(NULL, result.pipe_task.data);
-	assert_ptr_equal(NULL, result.pipe_task.run);
+	assert_int_equal(result.pipe_task->state, SOF_TASK_STATE_FREE);
+	assert_ptr_equal(NULL, result.pipe_task->data);
+	assert_ptr_equal(NULL, result.pipe_task->run);
 }
 
 static void test_audio_pipeline_free_disconnect_full(void **state)

commit 38173c3c0b0a9de6ef44ff564e92f7518359fc8d

pipeline: fix tasks initialization
Fixes tasks initialization for connected pipelines.
If we play on pipeline, which is not the owner of
the scheduling component, then the pipeline_prepare()
is not called on pipe owning that component and tasks
stay unitialized.

Fixes: de7d4c95cfbd ("pipeline: allocate pipe_task only if needed")

Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

src/audio/pipeline.c
@@ -350,6 +350,62 @@ int pipeline_params(struct pipeline *p, struct comp_dev *host,
 	return ret;
 }
 
+static struct task *pipeline_task_init(struct pipeline *p, uint32_t type,
+				       enum task_state (*func)(void *data))
+{
+	struct task *task = NULL;
+
+	task = rzalloc(RZONE_RUNTIME, SOF_MEM_CAPS_RAM, sizeof(*task));
+
+	if (task)
+		schedule_task_init(task, type, p->ipc_pipe.priority, func, NULL,
+				   p, p->ipc_pipe.core, 0);
+
+	return task;
+}
+
+static int pipeline_comp_task_init(struct pipeline *p)
+{
+	uint32_t type;
+
+	/* pipeline preload needed only for playback streams without active
+	 * sink component (it can be active for e.g. mixer pipelines)
+	 */
+	p->preload =
+		p->sink_comp->params.direction == SOF_IPC_STREAM_PLAYBACK &&
+		p->sink_comp->state != COMP_STATE_ACTIVE;
+
+	/* initialize task if necessary */
+	if (!p->pipe_task) {
+		/* right now we always consider pipeline as a low latency
+		 * component, but it may change in the future
+		 */
+		type = pipeline_is_timer_driven(p) ? SOF_SCHEDULE_LL_TIMER :
+			SOF_SCHEDULE_LL_DMA;
+
+		p->pipe_task = pipeline_task_init(p, type, pipeline_task);
+		if (!p->pipe_task) {
+			trace_pipe_error("pipeline_prepare() error: task init "
+					 "failed");
+			return -ENOMEM;
+		}
+	}
+
+	/* initialize preload task if necessary */
+	if (p->preload && !p->preload_task) {
+		/* preload task is always EDF as it guarantees scheduling */
+		p->preload_task = pipeline_task_init(p, SOF_SCHEDULE_EDF,
+						     pipeline_preload_task);
+		if (!p->preload_task) {
+			trace_pipe_error("pipeline_prepare() error: preload "
+					 "task init failed");
+			return -ENOMEM;
+		}
+	}
+
+	return 0;
+}
+
 static int pipeline_comp_prepare(struct comp_dev *current, void *data, int dir)
 {
 	int err = 0;
@@ -381,6 +437,10 @@ static int pipeline_comp_prepare(struct comp_dev *current, void *data, int dir)
 		}
 	}
 
+	err = pipeline_comp_task_init(current->pipeline);
+	if (err < 0)
+		return err;
+
 	err = comp_prepare(current);
 	if (err < 0 || err == PPL_STATUS_PATH_STOP)
 		return err;
@@ -389,25 +449,10 @@ static int pipeline_comp_prepare(struct comp_dev *current, void *data, int dir)
 				      &buffer_reset_pos, dir);
 }
 
-static struct task *pipeline_task_init(struct pipeline *p, uint32_t type,
-				       enum task_state (*func)(void *data))
-{
-	struct task *task = NULL;
-
-	task = rzalloc(RZONE_RUNTIME, SOF_MEM_CAPS_RAM, sizeof(*task));
-
-	if (task)
-		schedule_task_init(task, type, p->ipc_pipe.priority, func, NULL,
-				   p, p->ipc_pipe.core, 0);
-
-	return task;
-}
-
 /* prepare the pipeline for usage - preload host buffers here */
 int pipeline_prepare(struct pipeline *p, struct comp_dev *dev)
 {
 	struct pipeline_data ppl_data;
-	uint32_t type;
 	int ret = 0;
 
 	trace_pipe_with_ids(p, "pipeline_prepare()");
@@ -419,40 +464,6 @@ int pipeline_prepare(struct pipeline *p, struct comp_dev *dev)
 		trace_pipe_error("pipeline_prepare() error: ret = %d,"
 				 "dev->comp.id = %u", ret, dev->comp.id);
 		return ret;
-	}
-
-	/* pipeline preload needed only for playback streams without active
-	 * sink component (it can be active for e.g. mixer pipelines)
-	 */
-	p->preload = dev->params.direction == SOF_IPC_STREAM_PLAYBACK &&
-		p->sink_comp->state != COMP_STATE_ACTIVE;
-
-	/* initialize task if necessary */
-	if (!p->pipe_task) {
-		/* right now we always consider pipeline as a low latency
-		 * component, but it may change in the future
-		 */
-		type = pipeline_is_timer_driven(p) ? SOF_SCHEDULE_LL_TIMER :
-			SOF_SCHEDULE_LL_DMA;
-
-		p->pipe_task = pipeline_task_init(p, type, pipeline_task);
-		if (!p->pipe_task) {
-			trace_pipe_error("pipeline_prepare() error: task init "
-					 "failed");
-			return -ENOMEM;
-		}
-	}
-
-	/* initialize preload task if necessary */
-	if (p->preload && !p->preload_task) {
-		/* preload task is always EDF as it guarantees scheduling */
-		p->preload_task = pipeline_task_init(p, SOF_SCHEDULE_EDF,
-						     pipeline_preload_task);
-		if (!p->preload_task) {
-			trace_pipe_error("pipeline_prepare() error: preload "
-					 "task init failed");
-			return -ENOMEM;
-		}
 	}
 
 	p->status = COMP_STATE_PREPARE;