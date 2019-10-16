6d6995885c0a6db5eb691a9e99c00a7c8d3e9629
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2019-10-16 13:13:52 +0200

    ipc_msg: limit input parameters to ipc callbacks
    This patch chenges amount of parameters to ipc
    messages callback functions from two to one.
    There is no reason to keep this data separate.
    
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

--- src/include/sof/drivers/ipc.h
+++ src/include/sof/drivers/ipc.h
@@ -80,7 +80,7 @@ struct ipc_msg {
 	uint32_t rx_size;	/* payload size in bytes */
 	uint8_t rx_data[SOF_IPC_MSG_MAX_SIZE];	/* pointer to payload data */
 	struct list_item list;
-	void (*cb)(void *cb_data, void *mailbox_data);
+	void (*cb)(void *cb_data);
 	void *cb_data;
 };
 
