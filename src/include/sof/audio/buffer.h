44257a8739e63ecd8f397990c380d101d6774d7f
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2019-10-17 14:56:00 +0200

    buffer: extract allocation part to separate function
    Extracts allocation part of buffer_new to separate function.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/include/sof/audio/buffer.h
+++ src/include/sof/audio/buffer.h
@@ -116,6 +116,7 @@ struct comp_buffer {
 typedef void (*cache_buff_op)(struct comp_buffer *);
 
 /* pipeline buffer creation and destruction */
+struct comp_buffer *buffer_alloc(uint32_t size, uint32_t caps, uint32_t align);
 struct comp_buffer *buffer_new(struct sof_ipc_buffer *desc);
 int buffer_set_size(struct comp_buffer *buffer, uint32_t size);
 void buffer_free(struct comp_buffer *buffer);
