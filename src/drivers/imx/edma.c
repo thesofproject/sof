thesofproject/sof

commit 61b0b58865b30d97e329439eb377b4462381545a

drivers: imx: edma: avoid accessing register for non ready channels
avoid accessing registers for EDMA channels that are not
started to prevent crash

Signed-off-by: Guido Roncarolo <guido.roncarolo@nxp.com>
Signed-off-by: Jerome Laclavere <jerome.laclavere@nxp.com>

src/drivers/imx/edma.c
@@ -435,6 +435,9 @@ static int edma_remove(struct dma *dma)
 
 static int edma_interrupt(struct dma_chan_data *channel, enum dma_irq_cmd cmd)
 {
+	if (channel->status != COMP_STATE_INIT)
+		return 0;
+
 	switch (cmd) {
 	case DMA_IRQ_STATUS_GET:
 		return dma_chan_reg_read(channel, EDMA_CH_INT);

commit 03d8cd72d79406144dc941cff6b93563ed1ac160

drivers: imx: edma: fix accessing register logic
channel must not be accessed when status *is* INIT
Fixes: 61b0b58

Signed-off-by: Guido Roncarolo <guido.roncarolo@nxp.com>

src/drivers/imx/edma.c
@@ -442,7 +442,7 @@ static int edma_remove(struct dma *dma)
 
 static int edma_interrupt(struct dma_chan_data *channel, enum dma_irq_cmd cmd)
 {
-	if (channel->status != COMP_STATE_INIT)
+	if (channel->status == COMP_STATE_INIT)
 		return 0;
 
 	switch (cmd) {