c5d7ecb2e204c25c3db9fd0eca01eb2bfc7da438
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2019-11-07 07:47:55 +0100

    kconfig: selector: group functions and callbacks by supported data type
    It will be helpful in next step - conditional code compilation
    according to configuration
    
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

--- src/audio/selector/selector_generic.c
+++ src/audio/selector/selector_generic.c
@@ -43,30 +43,6 @@ static void sel_s16le_1ch(struct comp_dev *dev, struct comp_buffer *sink,
 }
 
 /**
- * \brief Channel selection for 32 bit, 1 channel data format.
- * \param[in,out] dev Selector base component device.
- * \param[in,out] sink Destination buffer.
- * \param[in,out] source Source buffer.
- * \param[in] frames Number of frames to process.
- */
-static void sel_s32le_1ch(struct comp_dev *dev, struct comp_buffer *sink,
-			  struct comp_buffer *source, uint32_t frames)
-{
-	struct comp_data *cd = comp_get_drvdata(dev);
-	int32_t *src;
-	int32_t *dest;
-	uint32_t i;
-	uint32_t j = 0;
-	uint32_t nch = cd->config.in_channels_count;
-
-	for (i = cd->config.sel_channel; i < frames * nch; i += nch) {
-		src = buffer_read_frag_s32(source, i);
-		dest = buffer_write_frag_s32(sink, j++);
-		*dest = *src;
-	}
-}
-
-/**
  * \brief Channel selection for 16 bit, at least 2 channels data format.
  * \param[in,out] dev Selector base component device.
  * \param[in,out] sink Destination buffer.
@@ -91,6 +67,30 @@ static void sel_s16le_nch(struct comp_dev *dev, struct comp_buffer *sink,
 			*dest = *src;
 			j++;
 		}
+	}
+}
+
+/**
+ * \brief Channel selection for 32 bit, 1 channel data format.
+ * \param[in,out] dev Selector base component device.
+ * \param[in,out] sink Destination buffer.
+ * \param[in,out] source Source buffer.
+ * \param[in] frames Number of frames to process.
+ */
+static void sel_s32le_1ch(struct comp_dev *dev, struct comp_buffer *sink,
+			  struct comp_buffer *source, uint32_t frames)
+{
+	struct comp_data *cd = comp_get_drvdata(dev);
+	int32_t *src;
+	int32_t *dest;
+	uint32_t i;
+	uint32_t j = 0;
+	uint32_t nch = cd->config.in_channels_count;
+
+	for (i = cd->config.sel_channel; i < frames * nch; i += nch) {
+		src = buffer_read_frag_s32(source, i);
+		dest = buffer_write_frag_s32(sink, j++);
+		*dest = *src;
 	}
 }
 
@@ -124,13 +124,13 @@ static void sel_s32le_nch(struct comp_dev *dev, struct comp_buffer *sink,
 
 const struct comp_func_map func_table[] = {
 	{SOF_IPC_FRAME_S16_LE, 1, sel_s16le_1ch},
-	{SOF_IPC_FRAME_S24_4LE, 1, sel_s32le_1ch},
-	{SOF_IPC_FRAME_S32_LE, 1, sel_s32le_1ch},
 	{SOF_IPC_FRAME_S16_LE, 2, sel_s16le_nch},
-	{SOF_IPC_FRAME_S24_4LE, 2, sel_s32le_nch},
-	{SOF_IPC_FRAME_S32_LE, 2, sel_s32le_nch},
 	{SOF_IPC_FRAME_S16_LE, 4, sel_s16le_nch},
+	{SOF_IPC_FRAME_S24_4LE, 1, sel_s32le_1ch},
+	{SOF_IPC_FRAME_S24_4LE, 2, sel_s32le_nch},
 	{SOF_IPC_FRAME_S24_4LE, 4, sel_s32le_nch},
+	{SOF_IPC_FRAME_S32_LE, 1, sel_s32le_1ch},
+	{SOF_IPC_FRAME_S32_LE, 2, sel_s32le_nch},
 	{SOF_IPC_FRAME_S32_LE, 4, sel_s32le_nch},
 };
 
