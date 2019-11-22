02233b443543e4401e81cf41ec9dbcadcb477307
Paul Olaru <paul.olaru@nxp.com>
2019-11-22 13:20:31 +0200

    platform: imx8: Remove all references to SSP clock
    The i.MX8 platform doesn't have an SSP clock. We had a fake define for
    this platform due to prior inflexibility in SOF itself. Now that said
    lack of flexibility is gone, this patch removes all such references.
    
    Signed-off-by: Paul Olaru <paul.olaru@nxp.com>

--- src/platform/imx8/include/platform/lib/clk.h
+++ src/platform/imx8/include/platform/lib/clk.h
@@ -13,18 +13,15 @@
 #include <stdint.h>
 
 #define CLK_CPU(x)	(x)
-#define CLK_SSP		1
 
 #define CPU_DEFAULT_IDX		0
-#define SSP_DEFAULT_IDX		1
 
 #define CLK_DEFAULT_CPU_HZ	666000000
 #define CLK_MAX_CPU_HZ		666000000
 
-#define NUM_CLOCKS	2
+#define NUM_CLOCKS	1
 
 #define NUM_CPU_FREQ	1
-#define NUM_SSP_FREQ	2
 
 void platform_clock_init(void);
 
