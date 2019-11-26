thesofproject/sof

commit ee9283c159649e4248e4188fe0b47b551e22342c

sof: lib: notifier: Do not break build on platforms without CLK_SSP
Some platforms do not have a SSP clock, so a dummy define for CLK_SSP
and related macros shouldn't be required to exist in order for the
firmware to build.

Signed-off-by: Paul Olaru <paul.olaru@nxp.com>

src/include/sof/lib/notifier.h
@@ -45,8 +45,12 @@ struct notifier {
 	void (*cb)(int message, void *cb_data, void *event_data);
 };
 
+#if defined(CLK_SSP) && defined(NOTIFIER_ID_SSP_FREQ)
 #define NOTIFIER_CLK_CHANGE_ID(clk) \
 	((clk) == CLK_SSP ? NOTIFIER_ID_SSP_FREQ : NOTIFIER_ID_CPU_FREQ)
+#else
+#define NOTIFIER_CLK_CHANGE_ID(clk) NOTIFIER_ID_CPU_FREQ
+#endif
 
 struct notify **arch_notify_get(void);
 

commit bb7c10e50d094dc60174a1c83cf6f801d7157afa

sof: clk: Remove specificity on clock notifier change ID
The previous version changes behavior on the BYT platform.

Fixes: ee9283c159 ("sof: lib: notifier: Do not break build on platforms
without CLK_SSP").
Signed-off-by: Paul Olaru <paul.olaru@nxp.com>

src/include/sof/lib/notifier.h
@@ -45,7 +45,7 @@ struct notifier {
 	void (*cb)(int message, void *cb_data, void *event_data);
 };
 
-#if defined(CLK_SSP) && defined(NOTIFIER_ID_SSP_FREQ)
+#ifdef CLK_SSP
 #define NOTIFIER_CLK_CHANGE_ID(clk) \
 	((clk) == CLK_SSP ? NOTIFIER_ID_SSP_FREQ : NOTIFIER_ID_CPU_FREQ)
 #else