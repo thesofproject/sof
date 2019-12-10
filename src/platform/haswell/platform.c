d9be34d149e3a601752c30acdfffc4e5125f02e1
Pan Xiuli <xiuli.pan@linux.intel.com>
2019-12-10 17:31:56 +0800

    platform: bdw: fix SSP clock setting
    SSP clock on BDW is 24MHz.
    
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

--- src/platform/haswell/platform.c
+++ src/platform/haswell/platform.c
@@ -211,9 +211,9 @@ int platform_init(struct sof *sof)
 	trace_point(TRACE_BOOT_PLATFORM_CPU_FREQ);
 	clock_set_freq(CLK_CPU(cpu_get_id()), CLK_MAX_CPU_HZ);
 
-	/* set SSP clock to 25M */
+	/* set SSP clock to 24M */
 	trace_point(TRACE_BOOT_PLATFORM_SSP_FREQ);
-	clock_set_freq(CLK_SSP, 25000000);
+	clock_set_freq(CLK_SSP, 24000000);
 
 	/* init DMACs */
 	trace_point(TRACE_BOOT_PLATFORM_DMA);
