93a9895606bea3ca5a175c1cebe719b8ee92e9cd
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2019-12-11 16:42:11 +0200

    Volume: Fix handling of mute and unmute control
    This patch fixes the issue of audio left muted after a cycle
    of mute and unmute commands. The additional improvements are
    applying of gain ramp for mute that helps to avoid annoying click
    sounds. Also since the driver issues both volume and switch
    commands back to back the duplicate scheduling of ramp update is
    prevented if there is already a ramp happening. The old ramp
    will operate based on new step and direction if it was changed.
    
    Some variable names were changed in function volume_set_chan()
    for better readability. The impacted trace print commands were
    also updated to not split strings unnecessarily.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/include/sof/audio/volume.h
+++ src/include/sof/audio/volume.h
@@ -110,6 +110,8 @@ struct comp_data {
 	int32_t vol_min;			/**< minimum volume */
 	int32_t vol_max;			/**< maximum volume */
 	int32_t	vol_ramp_range;			/**< max ramp transition */
+	bool muted[SOF_IPC_MAX_CHANNELS];	/**< set if channel is muted */
+	bool vol_ramp_active;			/**< set if volume is ramped */
 	/**< volume processing function */
 	void (*scale_vol)(struct comp_dev *dev, struct comp_buffer *sink,
 			  struct comp_buffer *source, uint32_t frames);
