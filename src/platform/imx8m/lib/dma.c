3724608373c8d5d11b922034cdfaca61c7d4097d
Daniel Baluta <daniel.baluta@nxp.com>
2020-01-21 12:55:05 +0200

    platform: imx8m: Add platform drivers support
    This adds minimal support for dma, dai, clk, interrupt and mailbox.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

---
+++ src/platform/imx8m/lib/dma.c
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright 2019 NXP
+//
+// Author: Daniel Baluta <daniel.baluta@nxp.com>
+
+#include <sof/common.h>
+#include <sof/drivers/interrupt.h>
+#include <sof/lib/dma.h>
+#include <sof/lib/memory.h>
+#include <sof/sof.h>
+#include <sof/spinlock.h>
+
+extern struct dma_ops dummy_dma_ops;
+
+SHARED_DATA struct dma dma[PLATFORM_NUM_DMACS] = {
+{
+	.plat_data = {
+		.id		= DMA_ID_HOST,
+		.dir		= DMA_DIR_HMEM_TO_LMEM | DMA_DIR_LMEM_TO_HMEM,
+		.devs		= DMA_DEV_HOST,
+		.channels	= 16,
+	},
+	.ops	= &dummy_dma_ops,
+},
+};
+
+const struct dma_info lib_dma = {
+	.dma_array = dma,
+	.num_dmas = ARRAY_SIZE(dma)
+};
+
+int dmac_init(struct sof *sof)
+{
+	int i;
+
+	/* early lock initialization for ref counting */
+	for (i = 0; i < ARRAY_SIZE(dma); i++)
+		spinlock_init(&dma[i].lock);
+
+	platform_shared_commit(dma, sizeof(*dma));
+
+	sof->dma_info = &lib_dma;
+
+	return 0;
+}
