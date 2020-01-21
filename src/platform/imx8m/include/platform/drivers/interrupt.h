bf66f38fada2e443c3f04b14f567dbbfac642565
Daniel Baluta <daniel.baluta@nxp.com>
2020-01-21 12:55:05 +0200

    platform: imx8m: Add platform drivers support
    This adds minimal support for dma, dai, clk, interrupt and mailbox.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

---
+++ src/platform/imx8m/include/platform/drivers/interrupt.h
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright 2020 NXP
+ *
+ * Author: Daniel Baluta <daniel.baluta@nxp.com>
+ */
+
+#ifdef __SOF_DRIVERS_INTERRUPT_H__
+
+#ifndef __PLATFORM_DRIVERS_INTERRUPT_H__
+#define __PLATFORM_DRIVERS_INTERRUPT_H__
+
+#include <sof/bit.h>
+#include <config.h>
+
+/* IRQ numbers */
+#if CONFIG_INTERRUPT_LEVEL_1
+
+#define IRQ_NUM_SOFTWARE0	8	/* Level 1 */
+
+#define IRQ_MASK_SOFTWARE0	BIT(IRQ_NUM_SOFTWARE0)
+
+#endif
+
+#if CONFIG_INTERRUPT_LEVEL_2
+
+#define IRQ_NUM_TIMER0		2	/* Level 2 */
+#define IRQ_NUM_MU		7	/* Level 2 */
+#define IRQ_NUM_SOFTWARE1	9	/* Level 2 */
+#define IRQ_NUM_IRQSTR_DSP0	19	/* Level 2 */
+#define IRQ_NUM_IRQSTR_DSP1	20	/* Level 2 */
+#define IRQ_NUM_IRQSTR_DSP2	21	/* Level 2 */
+#define IRQ_NUM_IRQSTR_DSP3	22	/* Level 2 */
+#define IRQ_NUM_IRQSTR_DSP4	23	/* Level 2 */
+#define IRQ_NUM_IRQSTR_DSP5	24	/* Level 2 */
+#define IRQ_NUM_IRQSTR_DSP6	25	/* Level 2 */
+#define IRQ_NUM_IRQSTR_DSP7	26	/* Level 2 */
+
+#define IRQ_MASK_TIMER0		BIT(IRQ_NUM_TIMER0)
+#define IRQ_MASK_MU		BIT(IRQ_NUM_MU)
+#define IRQ_MASK_SOFTWARE1	BIT(IRQ_NUM_SOFTWARE1)
+#define IRQ_MASK_IRQSTR_DSP0	BIT(IRQ_NUM_IRQSTR_DSP0)
+#define IRQ_MASK_IRQSTR_DSP1	BIT(IRQ_NUM_IRQSTR_DSP1)
+#define IRQ_MASK_IRQSTR_DSP2	BIT(IRQ_NUM_IRQSTR_DSP2)
+#define IRQ_MASK_IRQSTR_DSP3	BIT(IRQ_NUM_IRQSTR_DSP3)
+#define IRQ_MASK_IRQSTR_DSP4	BIT(IRQ_NUM_IRQSTR_DSP4)
+#define IRQ_MASK_IRQSTR_DSP5	BIT(IRQ_NUM_IRQSTR_DSP5)
+#define IRQ_MASK_IRQSTR_DSP6	BIT(IRQ_NUM_IRQSTR_DSP6)
+#define IRQ_MASK_IRQSTR_DSP7	BIT(IRQ_NUM_IRQSTR_DSP7)
+
+#endif
+
+#if CONFIG_INTERRUPT_LEVEL_3
+
+#define IRQ_NUM_TIMER1		3	/* Level 3 */
+
+#define IRQ_MASK_TIMER1		BIT(IRQ_NUM_TIMER1)
+
+#endif
+
+/* 32 HW interrupts + 8 IRQ_STEER lines each with 64 interrupts */
+#define PLATFORM_IRQ_HW_NUM	XCHAL_NUM_INTERRUPTS
+#define PLATFORM_IRQ_CHILDREN	64 /* Each cascaded struct covers 64 IRQs */
+/* IMX: Covered steer IRQs are modulo-64 aligned. */
+#define PLATFORM_IRQ_FIRST_CHILD  0
+
+/* irqstr_get_sof_int() - Convert IRQ_STEER interrupt to SOF logical
+ * interrupt
+ * @irqstr_int Shared IRQ_STEER interrupt
+ *
+ * Get the SOF interrupt number for a shared IRQ_STEER interrupt number.
+ * The IRQ_STEER number is the one specified in the hardware description
+ * manuals, while the SOF interrupt number is the one usable with the
+ * interrupt_register and interrupt_enable functions.
+ *
+ * Return: SOF interrupt number
+ */
+int irqstr_get_sof_int(int irqstr_int);
+
+#endif /* __PLATFORM_DRIVERS_INTERRUPT_H__ */
+
+#else
+
+#error "This file shouldn't be included from outside of sof/drivers/interrupt.h"
+
+#endif /* __SOF_DRIVERS_INTERRUPT_H__ */
---
+++ src/platform/imx8m/include/platform/lib/memory.h
@@ -0,0 +1,211 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright 2020 NXP
+ *
+ * Author: Daniel Baluta <daniel.baluta@nxp.com>
+ */
+
+#ifdef __SOF_LIB_MEMORY_H__
+
+#ifndef __PLATFORM_LIB_MEMORY_H__
+#define __PLATFORM_LIB_MEMORY_H__
+
+#include <sof/lib/cache.h>
+#include <config.h>
+
+/* data cache line alignment */
+#define PLATFORM_DCACHE_ALIGN	DCACHE_LINE_SIZE
+
+/* physical DSP addresses */
+
+#define IRAM_BASE	0x3b6f8000
+#define IRAM_SIZE	0x800
+
+#define DRAM0_BASE	0x3b6e8000
+#define DRAM0_SIZE	0x8000
+
+#define DRAM1_BASE	0x3b6f0000
+#define DRAM1_SIZE	0x8000
+
+#define SDRAM0_BASE	0x92400000
+#define SDRAM0_SIZE	0x800000
+
+#define SDRAM1_BASE	0x92C00000
+#define SDRAM1_SIZE	0x800000
+
+#define XSHAL_MU2_SIDEB_BYPASS_PADDR 0x30E70000
+#define MU_BASE		XSHAL_MU2_SIDEB_BYPASS_PADDR
+
+#define SDMA2_BASE	0x30e10000
+#define SDMA2_SIZE	0x10000
+
+#define SAI_3_BASE	0x30c30000
+#define SAI_3_SIZE	0x00010000
+
+#define LOG_ENTRY_ELF_BASE	0x20000000
+#define LOG_ENTRY_ELF_SIZE	0x2000000
+
+/*
+ * The Heap and Stack on i.MX8M are organised like this :-
+ *
+ * +--------------------------------------------------------------------------+
+ * | Offset              | Region         |  Size                             |
+ * +---------------------+----------------+-----------------------------------+
+ * | SDRAM_BASE          | RO Data        |  SOF_DATA_SIZE                    |
+ * |                     | Data           |                                   |
+ * |                     | BSS            |                                   |
+ * +---------------------+----------------+-----------------------------------+
+ * | HEAP_SYSTEM_BASE    | System Heap    |  HEAP_SYSTEM_SIZE                 |
+ * +---------------------+----------------+-----------------------------------+
+ * | HEAP_RUNTIME_BASE   | Runtime Heap   |  HEAP_RUNTIME_SIZE                |
+ * +---------------------+----------------+-----------------------------------+
+ * | HEAP_BUFFER_BASE    | Module Buffers |  HEAP_BUFFER_SIZE                 |
+ * +---------------------+----------------+-----------------------------------+
+ * | SOF_STACK_END       | Stack          |  SOF_STACK_SIZE                   |
+ * +---------------------+----------------+-----------------------------------+
+ * | SOF_STACK_BASE      |                |                                   |
+ * +---------------------+----------------+-----------------------------------+
+ */
+
+/* Mailbox configuration */
+#define SRAM_OUTBOX_BASE	SDRAM1_BASE
+#define SRAM_OUTBOX_SIZE	0x1000
+#define SRAM_OUTBOX_OFFSET	0
+
+#define SRAM_INBOX_BASE		(SRAM_OUTBOX_BASE + SRAM_OUTBOX_SIZE)
+#define SRAM_INBOX_SIZE		0x1000
+#define SRAM_INBOX_OFFSET	SRAM_OUTBOX_SIZE
+
+#define SRAM_DEBUG_BASE		(SRAM_INBOX_BASE + SRAM_INBOX_SIZE)
+#define SRAM_DEBUG_SIZE		0x800
+#define SRAM_DEBUG_OFFSET	(SRAM_INBOX_OFFSET + SRAM_INBOX_SIZE)
+
+#define SRAM_EXCEPT_BASE	(SRAM_DEBUG_BASE + SRAM_DEBUG_SIZE)
+#define SRAM_EXCEPT_SIZE	0x800
+#define SRAM_EXCEPT_OFFSET	(SRAM_DEBUG_OFFSET + SRAM_DEBUG_SIZE)
+
+#define SRAM_STREAM_BASE	(SRAM_EXCEPT_BASE + SRAM_EXCEPT_SIZE)
+#define SRAM_STREAM_SIZE	0x1000
+#define SRAM_STREAM_OFFSET	(SRAM_EXCEPT_OFFSET + SRAM_EXCEPT_SIZE)
+
+#define SRAM_TRACE_BASE		(SRAM_STREAM_BASE + SRAM_STREAM_SIZE)
+#define SRAM_TRACE_SIZE		0x1000
+#define SRAM_TRACE_OFFSET (SRAM_STREAM_OFFSET + SRAM_STREAM_SIZE)
+
+#define SOF_MAILBOX_SIZE	(SRAM_INBOX_SIZE + SRAM_OUTBOX_SIZE \
+				+ SRAM_DEBUG_SIZE + SRAM_EXCEPT_SIZE \
+				+ SRAM_STREAM_SIZE + SRAM_TRACE_SIZE)
+
+/* Heap section sizes for module pool */
+#define HEAP_RT_COUNT8		0
+#define HEAP_RT_COUNT16		48
+#define HEAP_RT_COUNT32		48
+#define HEAP_RT_COUNT64		32
+#define HEAP_RT_COUNT128	32
+#define HEAP_RT_COUNT256	32
+#define HEAP_RT_COUNT512	4
+#define HEAP_RT_COUNT1024	4
+#define HEAP_RT_COUNT2048	4
+
+/* Heap section sizes for system runtime heap */
+#define HEAP_SYS_RT_COUNT64	128
+#define HEAP_SYS_RT_COUNT512	16
+#define HEAP_SYS_RT_COUNT1024	8
+
+/* Heap configuration */
+
+#define HEAP_SYSTEM_BASE	SDRAM1_BASE + SOF_MAILBOX_SIZE
+#define HEAP_SYSTEM_SIZE	0xe000
+
+#define HEAP_SYSTEM_0_BASE	HEAP_SYSTEM_BASE
+
+#define HEAP_SYS_RUNTIME_BASE	(HEAP_SYSTEM_BASE + HEAP_SYSTEM_SIZE)
+#define HEAP_SYS_RUNTIME_SIZE \
+	(HEAP_SYS_RT_COUNT64 * 64 + HEAP_SYS_RT_COUNT512 * 512 + \
+	HEAP_SYS_RT_COUNT1024 * 1024)
+
+#define HEAP_RUNTIME_BASE	(HEAP_SYS_RUNTIME_BASE + HEAP_SYS_RUNTIME_SIZE)
+#define HEAP_RUNTIME_SIZE \
+	(HEAP_RT_COUNT8 * 8 + HEAP_RT_COUNT16 * 16 + \
+	HEAP_RT_COUNT32 * 32 + HEAP_RT_COUNT64 * 64 + \
+	HEAP_RT_COUNT128 * 128 + HEAP_RT_COUNT256 * 256 + \
+	HEAP_RT_COUNT512 * 512 + HEAP_RT_COUNT1024 * 1024 + \
+	HEAP_RT_COUNT2048 * 2048)
+
+#define HEAP_BUFFER_BASE	(HEAP_RUNTIME_BASE + HEAP_RUNTIME_SIZE)
+#define HEAP_BUFFER_SIZE \
+	(SDRAM1_SIZE - SOF_MAILBOX_SIZE - HEAP_RUNTIME_SIZE - SOF_STACK_TOTAL_SIZE -\
+	HEAP_SYS_RUNTIME_SIZE - HEAP_SYSTEM_SIZE)
+
+#define HEAP_BUFFER_BLOCK_SIZE		0x180
+#define HEAP_BUFFER_COUNT	(HEAP_BUFFER_SIZE / HEAP_BUFFER_BLOCK_SIZE)
+
+#define PLATFORM_HEAP_SYSTEM		1 /* one per core */
+#define PLATFORM_HEAP_SYSTEM_RUNTIME	1 /* one per core */
+#define PLATFORM_HEAP_RUNTIME		1
+#define PLATFORM_HEAP_BUFFER		1
+
+/* Stack configuration */
+#define SOF_STACK_SIZE		0x1000
+#define SOF_STACK_TOTAL_SIZE	SOF_STACK_SIZE
+#define SOF_STACK_BASE		(SDRAM1_BASE + SDRAM1_SIZE)
+#define SOF_STACK_END		(SOF_STACK_BASE - SOF_STACK_TOTAL_SIZE)
+
+/* Vector and literal sizes - not in core-isa.h */
+#define SOF_MEM_VECT_LIT_SIZE		0x4
+#define SOF_MEM_VECT_TEXT_SIZE		0x1c
+#define SOF_MEM_VECT_SIZE		(SOF_MEM_VECT_TEXT_SIZE + SOF_MEM_VECT_LIT_SIZE)
+
+#define SOF_MEM_RESET_TEXT_SIZE		0x2e0
+#define SOF_MEM_RESET_LIT_SIZE		0x120
+#define SOF_MEM_VECBASE_LIT_SIZE	0x178
+
+#define SOF_MEM_RO_SIZE			0x8
+
+#define uncache_to_cache(address)	address
+#define cache_to_uncache(address)	address
+#define is_uncached(address)		0
+
+#define HEAP_BUF_ALIGNMENT		PLATFORM_DCACHE_ALIGN
+
+#if !defined(__ASSEMBLER__) && !defined(LINKER)
+
+struct sof;
+
+/**
+ * \brief Data shared between different cores.
+ * Does nothing, since IMX doesn't support SMP.
+ */
+#define SHARED_DATA
+
+void platform_init_memmap(struct sof *sof);
+
+static inline void *platform_shared_get(void *ptr, int bytes)
+{
+	return ptr;
+}
+
+/**
+ * \brief Function for keeping shared data synchronized.
+ * It's used after usage of data shared by different cores.
+ * Such data is either statically marked with SHARED_DATA
+ * or dynamically allocated with SOF_MEM_FLAG_SHARED flag.
+ * Does nothing, since IMX doesn't support SMP.
+ */
+
+static inline void platform_shared_commit(void *ptr, int bytes) { }
+
+static inline void *platform_rfree_prepare(void *ptr)
+{
+	return ptr;
+}
+
+#endif
+
+#endif /* __PLATFORM_LIB_MEMORY_H__ */
+
+#else
+
+#error "This file shouldn't be included from outside of sof/lib/memory.h"
+
+#endif /* __SOF_LIB_MEMORY_H__ */
