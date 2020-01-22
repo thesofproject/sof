77eb159653a9e4996595f42ecf180c7da638b675
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2020-01-22 10:14:49 +0100

    library: add missing memory macros
    Adds missing memory macros to host build.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/platform/library/include/platform/lib/memory.h
+++ src/platform/library/include/platform/lib/memory.h
@@ -29,6 +29,10 @@
 
 #define SHARED_DATA
 
+#define uncache_to_cache(address)	(address)
+#define cache_to_uncache(address)	(address)
+#define is_uncached(address)		0
+
 static inline void *platform_shared_get(void *ptr, int bytes)
 {
 	return ptr;
