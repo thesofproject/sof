bd5c275a92ae70a1f4ff8817904bfd1e816d4ad3
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2020-01-24 13:12:24 +0100

    audio_stream: Fill comments to each data field
    Dispel possible doubts about content of struct fields by adding
    descriptive comment to each one.
    
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

--- src/include/sof/audio/audio_stream.h
+++ src/include/sof/audio/audio_stream.h
@@ -37,8 +37,8 @@ struct audio_stream {
 
 	/* runtime stream params */
 	enum sof_ipc_frame frame_fmt;	/**< sample data format */
-	uint32_t rate;
-	uint16_t channels;
+	uint32_t rate;		/**< number of data frames per second [Hz] */
+	uint16_t channels;	/**< number of samples in each frame */
 };
 
 #define audio_stream_read_frag(buffer, idx, size) \
