29d4e36b587dbc72d4e5199d8d7973a49c2a6eb9
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2020-01-28 13:29:02 +0200

    Tune: Fix help text for SRC generator
    This patch corrects the shown parameters orders for help text
    and updates the copyright text style.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/tune/src/src_generate.m
+++ tools/tune/src/src_generate.m
@@ -1,9 +1,8 @@
 function src_generate(fs_in, fs_out, fs_inout, cfg);
 
-
 % src_generate - export src conversions for given fs_in and fs_out
 %
-% src_generate(fs_in, fs_out <, ctype, fs_inout>>)
+% src_generate(fs_in, fs_out <, fs_inout, <cfg>>)
 %
 % fs_in     - vector of input sample rates (M)
 % fs_out    - vector of output sample rates (N)
@@ -20,34 +19,12 @@ function src_generate(fs_in, fs_out, fs_inout, cfg);
 %
 % If cfg is omitted the script will assume 'int32', 'std', 1.0, 0.
 %
-% Copyright (c) 2016, Intel Corporation
-% All rights reserved.
+
+% SPDX-License-Identifier: BSD-3-Clause
 %
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions are met:
-%   * Redistributions of source code must retain the above copyright
-%     notice, this list of conditions and the following disclaimer.
-%   * Redistributions in binary form must reproduce the above copyright
-%     notice, this list of conditions and the following disclaimer in the
-%     documentation and/or other materials provided with the distribution.
-%   * Neither the name of the Intel Corporation nor the
-%     names of its contributors may be used to endorse or promote products
-%     derived from this software without specific prior written permission.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-% POSSIBILITY OF SUCH DAMAGE.
+% Copyright (c) 2016-2020, Intel Corporation. All rights reserved.
 %
 % Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
-%
 
 if (nargin < 2) || (nargin > 4)
 	error('Incorrect arguments for function!');
