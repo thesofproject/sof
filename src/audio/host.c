thesofproject/sof

commit a87b9085058492bb2e40c93a95fa6de1fb5e7403

trace: component: use err,info,dbg macros
Both class and instance macro set switched to err,info,dbg
version.

Signed-off-by: Marcin Maka <marcin.maka@linux.intel.com>

src/audio/host.c
@@ -28,32 +28,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define trace_host(__e, ...) \
-	trace_event(TRACE_CLASS_HOST, __e, ##__VA_ARGS__)
-
-#define trace_host_with_ids(comp_ptr, format, ...)	\
-	trace_event_with_ids(TRACE_CLASS_HOST,		\
-			     comp_ptr->comp.pipeline_id,\
-			     comp_ptr->comp.id,		\
-			     format, ##__VA_ARGS__)
-
-#define tracev_host(__e, ...) \
-	tracev_event(TRACE_CLASS_HOST, __e, ##__VA_ARGS__)
-
-#define tracev_host_with_ids(comp_ptr, format, ...)	\
-	tracev_event_with_ids(TRACE_CLASS_HOST,		\
-			     comp_ptr->comp.pipeline_id,\
-			     comp_ptr->comp.id,		\
-			     format, ##__VA_ARGS__)
-
-#define trace_host_error(__e, ...) \
-	trace_error(TRACE_CLASS_HOST, __e, ##__VA_ARGS__)
-
-#define trace_host_error_with_ids(comp_ptr, format, ...)	\
-	trace_error_with_ids(TRACE_CLASS_HOST,		\
-			     comp_ptr->comp.pipeline_id,\
-			     comp_ptr->comp.id,		\
-			     format, ##__VA_ARGS__)
+static const struct comp_driver comp_host;
 
 /**
  * \brief Host buffer info.
@@ -232,7 +207,7 @@ static void host_dma_cb(void *arg, enum notify_id type, void *data)
 	struct host_data *hd = comp_get_drvdata(dev);
 	uint32_t bytes = next->elem.size;
 
-	tracev_host("host_dma_cb()");
+	comp_cl_dbg(&comp_host, "host_dma_cb() %p", &comp_host);
 
 	/* update position */
 	host_update_position(dev, bytes);
@@ -261,9 +236,7 @@ static int create_local_elems(struct comp_dev *dev, uint32_t buffer_count,
 		err = dma_sg_alloc(&hd->config.elem_array, SOF_MEM_ZONE_RUNTIME,
 				   dir, 1, 0, 0, 0);
 		if (err < 0) {
-			trace_host_error_with_ids(dev, "create_local_elems() "
-						  "error: dma_sg_alloc() "
-						  "failed");
+			comp_err(dev, "create_local_elems() error: dma_sg_alloc() failed");
 			return err;
 		}
 	} else {
@@ -274,8 +247,7 @@ static int create_local_elems(struct comp_dev *dev, uint32_t buffer_count,
 			   buffer_bytes,
 			   (uintptr_t)(hd->dma_buffer->stream.addr), 0);
 	if (err < 0) {
-		trace_host_error_with_ids(dev, "create_local_elems() error: "
-					  "dma_sg_alloc() failed");
+		comp_err(dev, "create_local_elems() error: dma_sg_alloc() failed");
 		return err;
 	}
 
@@ -297,7 +269,7 @@ static int host_trigger(struct comp_dev *dev, int cmd)
 	struct host_data *hd = comp_get_drvdata(dev);
 	int ret = 0;
 
-	trace_host_with_ids(dev, "host_trigger()");
+	comp_info(dev, "host_trigger()");
 
 	ret = comp_set_state(dev, cmd);
 	if (ret < 0)
@@ -315,8 +287,7 @@ static int host_trigger(struct comp_dev *dev, int cmd)
 		return ret;
 
 	if (!hd->chan) {
-		trace_host_error_with_ids(dev, "host_trigger() error: no dma "
-					  "channel configured");
+		comp_err(dev, "host_trigger() error: no dma channel configured");
 		return -EINVAL;
 	}
 
@@ -324,16 +295,15 @@ static int host_trigger(struct comp_dev *dev, int cmd)
 	case COMP_TRIGGER_START:
 		ret = dma_start(hd->chan);
 		if (ret < 0)
-			trace_host_error_with_ids(dev, "host_trigger() error: "
-						  "dma_start() failed, "
-						  "ret = %u", ret);
+			comp_err(dev, "host_trigger() error: dma_start() failed, ret = %u",
+				 ret);
 		break;
 	case COMP_TRIGGER_STOP:
 	case COMP_TRIGGER_XRUN:
 		ret = dma_stop(hd->chan);
 		if (ret < 0)
-			trace_host_error_with_ids(dev, "host_trigger(): dma "
-						  "stop failed: %d", ret);
+			comp_err(dev, "host_trigger(): dma stop failed: %d",
+				 ret);
 		break;
 	default:
 		break;
@@ -351,10 +321,10 @@ static struct comp_dev *host_new(struct sof_ipc_comp *comp)
 	uint32_t dir;
 	int ret;
 
-	trace_host("host_new()");
+	comp_cl_info(&comp_host, "host_new()");
 
 	if (IPC_IS_SIZE_INVALID(ipc_host->config)) {
-		IPC_SIZE_ERROR_TRACE(TRACE_CLASS_HOST, ipc_host->config);
+		IPC_SIZE_ERROR_TRACE(TRACE_CLASS_COMP, ipc_host->config);
 		return NULL;
 	}
 
@@ -382,7 +352,7 @@ static struct comp_dev *host_new(struct sof_ipc_comp *comp)
 
 	hd->dma = dma_get(dir, 0, DMA_DEV_HOST, DMA_ACCESS_SHARED);
 	if (!hd->dma) {
-		trace_host_error("host_new() error: dma_get() returned NULL");
+		comp_cl_err(&comp_host, "host_new() error: dma_get() returned NULL");
 		rfree(hd);
 		rfree(dev);
 		return NULL;
@@ -405,7 +375,7 @@ static void host_free(struct comp_dev *dev)
 {
 	struct host_data *hd = comp_get_drvdata(dev);
 
-	trace_host_with_ids(dev, "host_free()");
+	comp_info(dev, "host_free()");
 
 	dma_put(hd->dma);
 
@@ -461,7 +431,7 @@ static int host_params(struct comp_dev *dev,
 	uint32_t align;
 	int err;
 
-	trace_host_with_ids(dev, "host_params()");
+	comp_info(dev, "host_params()");
 
 	/* host params always installed by pipeline IPC */
 	hd->host_size = params->buffer.size;
@@ -473,18 +443,16 @@ static int host_params(struct comp_dev *dev,
 	err = dma_get_attribute(hd->dma, DMA_ATTR_BUFFER_ADDRESS_ALIGNMENT,
 				&addr_align);
 	if (err < 0) {
-		trace_host_error_with_ids(dev, "host_params() error: could not "
-					  "get dma buffer address alignment, "
-					  "err = %d", err);
+		comp_err(dev, "host_params() error: could not get dma buffer address alignment, err = %d",
+			 err);
 		return err;
 	}
 
 	/* retrieve DMA buffer size alignment */
 	err = dma_get_attribute(hd->dma, DMA_ATTR_BUFFER_ALIGNMENT, &align);
 	if (err < 0 || !align) {
-		trace_host_error_with_ids(dev, "host_params() error: could not "
-					  "get valid dma buffer alignment, err "
-					  "= %d, align = %u", err, align);
+		comp_err(dev, "host_params() error: could not get valid dma buffer alignment, err = %d, align = %u",
+			 err, align);
 		return -EINVAL;
 	}
 
@@ -492,10 +460,8 @@ static int host_params(struct comp_dev *dev,
 	err = dma_get_attribute(hd->dma, DMA_ATTR_BUFFER_PERIOD_COUNT,
 				&period_count);
 	if (err < 0 || !period_count) {
-		trace_host_error_with_ids(dev, "host_params() error: could not "
-					  "get valid dma buffer period count, "
-					  "err = %d, period_count = %u", err,
-					  period_count);
+		comp_err(dev, "host_params() error: could not get valid dma buffer period count, err = %d, period_count = %u",
+			 err, period_count);
 		return -EINVAL;
 	}
 
@@ -511,8 +477,7 @@ static int host_params(struct comp_dev *dev,
 	period_bytes = dev->frames *
 		       audio_stream_frame_bytes(&hd->local_buffer->stream);
 	if (!period_bytes) {
-		trace_host_error_with_ids(dev, "host_params() error: invalid "
-					  "period_bytes");
+		comp_err(dev, "host_params() error: invalid period_bytes");
 		return -EINVAL;
 	}
 
@@ -540,18 +505,15 @@ static int host_params(struct comp_dev *dev,
 	if (hd->dma_buffer) {
 		err = buffer_set_size(hd->dma_buffer, buffer_size);
 		if (err < 0) {
-			trace_host_error_with_ids(dev, "host_params() error: "
-						  "buffer_set_size() failed, "
-						  "buffer_size = %u",
-						  buffer_size);
+			comp_err(dev, "host_params() error: buffer_set_size() failed, buffer_size = %u",
+				 buffer_size);
 			return err;
 		}
 	} else {
 		hd->dma_buffer = buffer_alloc(buffer_size, SOF_MEM_CAPS_DMA,
 					      addr_align);
 		if (!hd->dma_buffer) {
-			trace_host_error_with_ids(dev, "host_params() error: "
-						  "failed to alloc dma buffer");
+			comp_err(dev, "host_params() error: failed to alloc dma buffer");
 			return -ENOMEM;
 		}
 	}
@@ -579,15 +541,13 @@ static int host_params(struct comp_dev *dev,
 	 */
 	hd->chan = dma_channel_get(hd->dma, hd->stream_tag);
 	if (!hd->chan) {
-		trace_host_error_with_ids(dev, "host_params() error: "
-					  "hd->chan is NULL");
+		comp_err(dev, "host_params() error: hd->chan is NULL");
 		return -ENODEV;
 	}
 
 	err = dma_set_config(hd->chan, &hd->config);
 	if (err < 0) {
-		trace_host_error_with_ids(dev, "host_params() error: "
-					  "dma_set_config() failed");
+		comp_err(dev, "host_params() error: dma_set_config() failed");
 		dma_channel_put(hd->chan);
 		hd->chan = NULL;
 		return err;
@@ -597,8 +557,7 @@ static int host_params(struct comp_dev *dev,
 				&hd->dma_copy_align);
 
 	if (err < 0) {
-		trace_host_error_with_ids(dev, "host_params() error: "
-					  "dma_get_attribute()");
+		comp_err(dev, "host_params() error: dma_get_attribute()");
 
 		return err;
 	}
@@ -619,7 +578,7 @@ static int host_prepare(struct comp_dev *dev)
 	struct host_data *hd = comp_get_drvdata(dev);
 	int ret;
 
-	trace_host_with_ids(dev, "host_prepare()");
+	comp_info(dev, "host_prepare()");
 
 	ret = comp_set_state(dev, COMP_TRIGGER_PREPARE);
 	if (ret < 0)
@@ -662,7 +621,7 @@ static int host_reset(struct comp_dev *dev)
 {
 	struct host_data *hd = comp_get_drvdata(dev);
 
-	trace_host_with_ids(dev, "host_reset()");
+	comp_info(dev, "host_reset()");
 
 	if (hd->chan) {
 		/* remove callback */
@@ -725,9 +684,8 @@ static uint32_t host_buffer_get_copy_bytes(struct comp_dev *dev)
 		ret = dma_get_data_size(hd->chan, &avail_bytes,
 					&free_bytes);
 		if (ret < 0) {
-			trace_host_error("host_buffer_cb() error: "
-					 "dma_get_data_size() failed, ret = %u",
-					 ret);
+			comp_cl_err(&comp_host, "host_buffer_cb() error: dma_get_data_size() failed, ret = %u",
+				    ret);
 			return 0;
 		}
 
@@ -756,7 +714,7 @@ static int host_copy(struct comp_dev *dev)
 	uint32_t flags = 0;
 	int ret = 0;
 
-	tracev_host_with_ids(dev, "host_copy()");
+	comp_dbg(dev, "host_copy()");
 
 	if (dev->state != COMP_STATE_ACTIVE)
 		return 0;
@@ -772,23 +730,22 @@ static int host_copy(struct comp_dev *dev)
 
 	copy_bytes = host_buffer_get_copy_bytes(dev);
 	if (!copy_bytes) {
-		trace_host_with_ids(dev, "host_copy(): no bytes to copy");
+		comp_info(dev, "host_copy(): no bytes to copy");
 		return ret;
 	}
 
 	/* reconfigure transfer */
 	ret = dma_set_config(hd->chan, &hd->config);
 	if (ret < 0) {
-		trace_host_error("host_copy() error: "
-				 "dma_set_config() failed, ret = %u",
-				 ret);
+		comp_cl_err(&comp_host, "host_copy() error: dma_set_config() failed, ret = %u",
+			    ret);
 		return ret;
 	}
 
 	ret = dma_copy(hd->chan, copy_bytes, flags);
 	if (ret < 0) {
-		trace_host_error("host_copy() error: dma_copy() "
-				 "failed, ret = %u", ret);
+		comp_cl_err(&comp_host, "host_copy() error: dma_copy() failed, ret = %u",
+			    ret);
 		return ret;
 	}
 

src/audio/asrc/asrc.c
@@ -37,30 +37,13 @@
 #define COEF_C1		Q_CONVERT_FLOAT(0.01, 30)
 #define COEF_C2		Q_CONVERT_FLOAT(0.99, 30)
 
-/* Tracing */
-#define trace_asrc(__e, ...)					\
-	trace_event(TRACE_CLASS_SRC, __e, ##__VA_ARGS__)
-#define trace_asrc_with_ids(comp_ptr, __e, ...)			\
-	trace_event_comp(TRACE_CLASS_SRC, comp_ptr,		\
-			 __e, ##__VA_ARGS__)
-
-#define tracev_asrc(__e, ...) \
-	tracev_event(TRACE_CLASS_SRC, __e, ##__VA_ARGS__)
-#define tracev_asrc_with_ids(comp_ptr, __e, ...)		\
-	tracev_event_comp(TRACE_CLASS_SRC, comp_ptr,		\
-			  __e, ##__VA_ARGS__)
-
-#define trace_asrc_error(__e, ...) \
-	trace_error(TRACE_CLASS_SRC, __e, ##__VA_ARGS__)
-#define trace_asrc_error_with_ids(comp_ptr, __e, ...)		\
-	trace_error_comp(TRACE_CLASS_SRC, comp_ptr,		\
-			 __e, ##__VA_ARGS__)
-
 typedef void (*asrc_proc_func)(struct comp_dev *dev,
 			       const struct audio_stream *source,
 			       struct audio_stream *sink,
 			       int *consumed,
 			       int *produced);
+
+static const struct comp_driver comp_asrc;
 
 /* asrc component private data */
 struct comp_data {
@@ -144,13 +127,15 @@ static void src_copy_s32(struct comp_dev *dev,
 
 	/* Run ASRC */
 	if (cd->mode == ASRC_OM_PUSH) {
-		ret = asrc_process_push32(cd->asrc_obj, (int32_t **)cd->ibuf,
+		ret = asrc_process_push32(dev,
+					  cd->asrc_obj, (int32_t **)cd->ibuf,
 					  cd->source_frames,
 					  (int32_t **)cd->obuf, &frames,
 					  &idx, 0);
 		n = frames * sink->channels;
 	} else {
-		ret = asrc_process_pull32(cd->asrc_obj, (int32_t **)cd->ibuf,
+		ret = asrc_process_pull32(dev, cd->asrc_obj,
+					  (int32_t **)cd->ibuf,
 					  &frames, (int32_t **)cd->obuf,
 					  cd->sink_frames, cd->source_frames,
 					  &idx);
@@ -158,7 +143,7 @@ static void src_copy_s32(struct comp_dev *dev,
 	}
 
 	if (ret)
-		trace_asrc_error_with_ids(dev, "src_copy_s32(), error %d", ret);
+		comp_err(dev, "src_copy_s32(), error %d", ret);
 
 	buf = (int32_t *)cd->obuf[0];
 	while (n > 0) {
@@ -220,13 +205,15 @@ static void src_copy_s16(struct comp_dev *dev,
 
 	/* Run ASRC */
 	if (cd->mode == ASRC_OM_PUSH) {
-		ret = asrc_process_push16(cd->asrc_obj, (int16_t **)cd->ibuf,
+		ret = asrc_process_push16(dev,
+					  cd->asrc_obj, (int16_t **)cd->ibuf,
 					  cd->source_frames,
 					  (int16_t **)cd->obuf, &frames,
 					  &idx, 0);
 		n = frames * sink->channels;
 	} else {
-		ret = asrc_process_pull16(cd->asrc_obj, (int16_t **)cd->ibuf,
+		ret = asrc_process_pull16(dev,
+					  cd->asrc_obj, (int16_t **)cd->ibuf,
 					  &frames, (int16_t **)cd->obuf,
 					  cd->sink_frames, cd->source_frames,
 					  &idx);
@@ -234,7 +221,7 @@ static void src_copy_s16(struct comp_dev *dev,
 	}
 
 	if (ret)
-		trace_asrc_error_with_ids(dev, "src_copy_s16(), error %d", ret);
+		comp_err(dev, "src_copy_s16(), error %d", ret);
 
 	buf = (int16_t *)cd->obuf[0];
 	while (n > 0) {
@@ -268,21 +255,20 @@ static struct comp_dev *asrc_new(struct sof_ipc_comp *comp)
 	struct comp_data *cd;
 	int err;
 
-	trace_asrc("asrc_new()");
+	comp_cl_info(&comp_asrc, "asrc_new()");
 
 	if (IPC_IS_SIZE_INVALID(ipc_asrc->config)) {
-		IPC_SIZE_ERROR_TRACE(TRACE_CLASS_SRC, ipc_asrc->config);
+		IPC_SIZE_ERROR_TRACE(TRACE_CLASS_COMP, ipc_asrc->config);
 		return NULL;
 	}
 
-	trace_asrc("asrc_new(), source_rate=%d, sink_rate=%d"
-		   ", asynchronous_mode=%d, operation_mode=%d",
-		   ipc_asrc->source_rate, ipc_asrc->sink_rate,
-		   ipc_asrc->asynchronous_mode, ipc_asrc->operation_mode);
+	comp_cl_info(&comp_asrc, "asrc_new(), source_rate=%d, sink_rate=%d, asynchronous_mode=%d, operation_mode=%d",
+		     ipc_asrc->source_rate, ipc_asrc->sink_rate,
+		     ipc_asrc->asynchronous_mode, ipc_asrc->operation_mode);
 
 	/* validate init data - either SRC sink or source rate must be set */
 	if (ipc_asrc->source_rate == 0 && ipc_asrc->sink_rate == 0) {
-		trace_asrc_error("asrc_new(), sink and source rates are not set");
+		comp_cl_err(&comp_asrc, "asrc_new(), sink and source rates are not set");
 		return NULL;
 	}
 
@@ -325,7 +311,7 @@ static void asrc_free(struct comp_dev *dev)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
 
-	trace_asrc_with_ids(dev, "asrc_free()");
+	comp_info(dev, "asrc_free()");
 
 	rfree(cd->buf);
 	rfree(cd->asrc_obj);
@@ -335,7 +321,7 @@ static void asrc_free(struct comp_dev *dev)
 
 static int asrc_ctrl_cmd(struct comp_dev *dev, struct sof_ipc_ctrl_data *cdata)
 {
-	trace_asrc_error_with_ids(dev, "asrc_ctrl_cmd()");
+	comp_err(dev, "asrc_ctrl_cmd()");
 	return -EINVAL;
 }
 
@@ -346,7 +332,7 @@ static int asrc_cmd(struct comp_dev *dev, int cmd, void *data,
 	struct sof_ipc_ctrl_data *cdata = data;
 	int ret = 0;
 
-	trace_asrc_with_ids(dev, "asrc_cmd()");
+	comp_info(dev, "asrc_cmd()");
 
 	if (cmd == COMP_CMD_SET_VALUE)
 		ret = asrc_ctrl_cmd(dev, cdata);
@@ -356,7 +342,7 @@ static int asrc_cmd(struct comp_dev *dev, int cmd, void *data,
 
 static int asrc_trigger(struct comp_dev *dev, int cmd)
 {
-	trace_asrc_with_ids(dev, "asrc_trigger()");
+	comp_info(dev, "asrc_trigger()");
 
 	return comp_set_state(dev, cmd);
 }
@@ -369,7 +355,7 @@ static int asrc_params(struct comp_dev *dev,
 	struct sof_ipc_comp_asrc *asrc = COMP_GET_IPC(dev, sof_ipc_comp_asrc);
 	struct comp_data *cd = comp_get_drvdata(dev);
 
-	trace_asrc_with_ids(dev, "asrc_params()");
+	comp_info(dev, "asrc_params()");
 
 	/* Calculate source and sink rates, one rate will come from IPC new
 	 * and the other from params.
@@ -404,10 +390,9 @@ static int asrc_params(struct comp_dev *dev,
 	cd->sink_frames_max = cd->sink_frames + 10;
 	cd->frames = MAX(cd->source_frames_max, cd->sink_frames_max);
 
-	trace_asrc_with_ids(dev, "asrc_params(), source_rate=%u, sink_rate=%u"
-			    ", source_frames_max=%d, sink_frames_max=%d",
-			    cd->source_rate, cd->sink_rate,
-			    cd->source_frames_max, cd->sink_frames_max);
+	comp_info(dev, "asrc_params(), source_rate=%u, sink_rate=%u, source_frames_max=%d, sink_frames_max=%d",
+		  cd->source_rate, cd->sink_rate,
+		  cd->source_frames_max, cd->sink_frames_max);
 
 	return 0;
 }
@@ -432,12 +417,12 @@ static int asrc_dai_find(struct comp_dev *dev,
 						source_list);
 			next_dev = sinkb->sink;
 			if (!next_dev) {
-				trace_asrc_error("At end, no DAI found.");
+				comp_cl_err(&comp_asrc, "At end, no DAI found.");
 				return -EINVAL;
 			}
 
 			if (next_dev->comp.pipeline_id != pid) {
-				trace_asrc_error("No DAI sink in pipeline.");
+				comp_cl_err(&comp_asrc, "No DAI sink in pipeline.");
 				return -EINVAL;
 			}
 		}
@@ -450,12 +435,12 @@ static int asrc_dai_find(struct comp_dev *dev,
 						  sink_list);
 			next_dev = sourceb->source;
 			if (!next_dev) {
-				trace_asrc_error("At beginning, no DAI found.");
+				comp_cl_err(&comp_asrc, "At beginning, no DAI found.");
 				return -EINVAL;
 			}
 
 			if (next_dev->comp.pipeline_id != pid) {
-				trace_asrc_error("No DAI source in pipeline.");
+				comp_cl_err(&comp_asrc, "No DAI source in pipeline.");
 				return -EINVAL;
 			}
 		}
@@ -527,7 +512,7 @@ static int asrc_prepare(struct comp_dev *dev)
 	int ret;
 	int i;
 
-	trace_asrc_with_ids(dev, "asrc_prepare()");
+	comp_info(dev, "asrc_prepare()");
 
 	ret = comp_set_state(dev, COMP_TRIGGER_PREPARE);
 	if (ret < 0)
@@ -553,23 +538,21 @@ static int asrc_prepare(struct comp_dev *dev)
 						      cd->sink_frames);
 
 	if (sinkb->stream.size < config->periods_sink * sink_period_bytes) {
-		trace_asrc_error_with_ids(dev,
-					  "asrc_prepare(), sink size=%d is insufficient, when periods=%d, period_bytes=%d",
-					  sinkb->stream.size,
-					  config->periods_sink,
-					  sink_period_bytes);
+		comp_err(dev, "asrc_prepare(), sink size=%d is insufficient, when periods=%d, period_bytes=%d",
+			 sinkb->stream.size, config->periods_sink,
+			 sink_period_bytes);
 		ret = -ENOMEM;
 		goto err;
 	}
 
 	/* validate */
 	if (!sink_period_bytes) {
-		trace_asrc_error_with_ids(dev, "asrc_prepare(), sink_period_bytes = 0");
+		comp_err(dev, "asrc_prepare(), sink_period_bytes = 0");
 		ret = -EINVAL;
 		goto err;
 	}
 	if (!source_period_bytes) {
-		trace_asrc_error_with_ids(dev, "asrc_prepare(), source_period_bytes = 0");
+		comp_err(dev, "asrc_prepare(), source_period_bytes = 0");
 		ret = -EINVAL;
 		goto err;
 	}
@@ -588,7 +571,7 @@ static int asrc_prepare(struct comp_dev *dev)
 		cd->asrc_func = src_copy_s32;
 		break;
 	default:
-		trace_asrc_error_with_ids(dev, "asrc_prepare(), invalid frame format");
+		comp_err(dev, "asrc_prepare(), invalid frame format");
 		return -EINVAL;
 	}
 
@@ -603,8 +586,8 @@ static int asrc_prepare(struct comp_dev *dev)
 			  cd->buf_size);
 	if (!cd->buf) {
 		cd->buf_size = 0;
-		trace_asrc_error_with_ids(dev, "asrc_prepare(), allocation fail for size %d",
-					  cd->buf_size);
+		comp_err(dev, "asrc_prepare(), allocation fail for size %d",
+			 cd->buf_size);
 		ret = -ENOMEM;
 		goto err_free_buf;
 	}
@@ -619,18 +602,19 @@ static int asrc_prepare(struct comp_dev *dev)
 	 * Get required size and allocate memory for ASRC
 	 */
 	sample_bits = sample_bytes * 8;
-	ret = asrc_get_required_size(&cd->asrc_size, sourceb->stream.channels,
+	ret = asrc_get_required_size(dev, &cd->asrc_size,
+				     sourceb->stream.channels,
 				     sample_bits);
 	if (ret) {
-		trace_asrc_error_with_ids(dev, "asrc_prepare(), get_required_size_bytes failed");
+		comp_err(dev, "asrc_prepare(), get_required_size_bytes failed");
 		goto err_free_buf;
 	}
 
 	cd->asrc_obj = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM,
 			       cd->asrc_size);
 	if (!cd->asrc_obj) {
-		trace_asrc_error_with_ids(dev, "asrc_prepare(), allocation fail for size %d",
-					  cd->asrc_size);
+		comp_err(dev, "asrc_prepare(), allocation fail for size %d",
+			 cd->asrc_size);
 		cd->asrc_size = 0;
 		ret = -ENOMEM;
 		goto err_free_buf;
@@ -647,14 +631,13 @@ static int asrc_prepare(struct comp_dev *dev)
 		fs_sec = cd->source_rate;
 	}
 
-	ret = asrc_initialise(cd->asrc_obj, sourceb->stream.channels,
+	ret = asrc_initialise(dev, cd->asrc_obj, sourceb->stream.channels,
 			      fs_prim, fs_sec,
 			      ASRC_IOF_INTERLEAVED, ASRC_IOF_INTERLEAVED,
 			      ASRC_BM_LINEAR, cd->frames, sample_bits,
 			      ASRC_CM_FEEDBACK, cd->mode);
 	if (ret) {
-		trace_asrc_error_with_ids(dev, "initialise_asrc(), error %d",
-					  ret);
+		comp_err(dev, "initialise_asrc(), error %d", ret);
 		goto err_free_asrc;
 	}
 
@@ -665,11 +648,10 @@ static int asrc_prepare(struct comp_dev *dev)
 	if (!cd->skew)
 		cd->skew = Q_CONVERT_FLOAT(1.0, 30);
 
-	trace_asrc_with_ids(dev, "asrc_prepare(), skew = %d", cd->skew);
-	ret = asrc_update_drift(cd->asrc_obj, cd->skew);
+	comp_info(dev, "asrc_prepare(), skew = %d", cd->skew);
+	ret = asrc_update_drift(dev, cd->asrc_obj, cd->skew);
 	if (ret) {
-		trace_asrc_error_with_ids(dev, "asrc_update_drift(), error %d",
-					  ret);
+		comp_err(dev, "asrc_update_drift(), error %d", ret);
 		goto err_free_asrc;
 	}
 
@@ -677,7 +659,7 @@ static int asrc_prepare(struct comp_dev *dev)
 	if (cd->track_drift) {
 		ret = asrc_dai_find(dev, cd, sinkb, sourceb);
 		if (ret) {
-			trace_asrc_error_with_ids(dev, "No DAI found to track");
+			comp_err(dev, "No DAI found to track");
 			cd->track_drift = false;
 			goto err_free_asrc;
 		}
@@ -685,7 +667,7 @@ static int asrc_prepare(struct comp_dev *dev)
 		cd->ts_count = 0;
 		ret = asrc_dai_configure_timestamp(cd);
 		if (ret) {
-			trace_asrc_error_with_ids(dev, "No timestamp capability in DAI");
+			comp_err(dev, "No timestamp capability in DAI");
 			cd->track_drift = false;
 			goto err_free_asrc;
 		}
@@ -706,7 +688,7 @@ err:
 	return ret;
 }
 
-static int asrc_control_loop(struct comp_data *cd)
+static int asrc_control_loop(struct comp_dev *dev, struct comp_data *cd)
 {
 	struct timestamp_data tsd;
 	int64_t tmp;
@@ -750,7 +732,7 @@ static int asrc_control_loop(struct comp_data *cd)
 
 	/* Prevent divide by zero */
 	if (delta_sample == 0 || tsd.walclk_rate == 0) {
-		trace_asrc_error("asrc_control_loop(), DAI timestamp failed");
+		comp_cl_err(&comp_asrc, "asrc_control_loop(), DAI timestamp failed");
 		return -EINVAL;
 	}
 
@@ -765,8 +747,8 @@ static int asrc_control_loop(struct comp_data *cd)
 	/* tmp is Q4.60, shift and round to Q2.30 */
 	tmp = ((int64_t)COEF_C1) * skew + ((int64_t)COEF_C2) * cd->skew;
 	cd->skew = sat_int32(Q_SHIFT_RND(tmp, 60, 30));
-	asrc_update_drift(cd->asrc_obj, cd->skew);
-	tracev_asrc("skew %d %d %d %d", delta_sample, delta_ts, skew, cd->skew);
+	asrc_update_drift(dev, cd->asrc_obj, cd->skew);
+	comp_cl_dbg("skew %d %d %d %d", delta_sample, delta_ts, skew, cd->skew);
 	return 0;
 }
 
@@ -782,9 +764,9 @@ static int asrc_copy(struct comp_dev *dev)
 	int frames_snk;
 	int ret;
 
-	tracev_asrc_with_ids(dev, "asrc_copy()");
+	comp_dbg(dev, "asrc_copy()");
 
-	ret = asrc_control_loop(cd);
+	ret = asrc_control_loop(dev, cd);
 	if (ret)
 		return ret;
 
@@ -819,8 +801,8 @@ static int asrc_copy(struct comp_dev *dev)
 		cd->asrc_func(dev, &source->stream, &sink->stream, &consumed,
 			      &produced);
 
-	tracev_asrc_with_ids(dev, "asrc_copy(), consumed = %u,  produced = %u",
-			     consumed, produced);
+	comp_dbg(dev, "asrc_copy(), consumed = %u,  produced = %u",
+		 consumed, produced);
 
 	/* Calc new free and available if data was processed. These
 	 * functions must not be called with 0 consumed/produced.
@@ -840,7 +822,7 @@ static int asrc_reset(struct comp_dev *dev)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
 
-	trace_asrc_with_ids(dev, "asrc_reset()");
+	comp_info(dev, "asrc_reset()");
 
 	/* If any resources feasible to stop */
 	if (cd->track_drift)

commit 01dce4c79aeaaf5d1cbe88722ec31501b2535fac

host: Fix compilation error with verbose traces enabled
src/audio/host.c: In function 'host_dma_cb':
src/audio/host.c:210:46: error: initialization of
'unsigned int' from 'const struct comp_driver *' makes integer from
pointer without a cast [-Werror=int-conversion]
  comp_cl_dbg(&comp_host, "host_dma_cb() %p", &comp_host);

This happens because our tracing system is limited to integer types
for parameters.

Fixes: a87b9085058492 ("trace: component: use err,info,dbg macros")
Suggested-by: Paul Olaru <paul.olaru@nxp.com>
Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

src/audio/host.c
@@ -207,7 +207,7 @@ static void host_dma_cb(void *arg, enum notify_id type, void *data)
 	struct host_data *hd = comp_get_drvdata(dev);
 	uint32_t bytes = next->elem.size;
 
-	comp_cl_dbg(&comp_host, "host_dma_cb() %p", &comp_host);
+	comp_cl_dbg(&comp_host, "host_dma_cb() %p", (uintptr_t)&comp_host);
 
 	/* update position */
 	host_update_position(dev, bytes);