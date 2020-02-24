5da5c9b079a3d620a8a06a1d47c752e2a5584d0f
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2020-02-24 14:23:07 +0100

    ext_manifest: Include compiler description information
    This information is known at build time so should
    be passed by extended manifest.
    Introduced structure is designed to be fully compatible
    with information provided through mailbox to make
    parsing code on host side as much reusable as possible.
    
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

--- src/init/ext_manifest.c
+++ src/init/ext_manifest.c
@@ -7,6 +7,7 @@
 
 #include <sof/bit.h>
 #include <sof/common.h>
+#include <sof/compiler_info.h>
 #include <sof/debug/debug.h>
 #include <kernel/abi.h>
 #include <kernel/ext_manifest.h>
@@ -33,4 +34,23 @@ const struct ext_man_fw_version ext_man_fw_ver
 		.abi_version = SOF_ABI_VERSION,
 	},
 	.flags = DEBUG_SET_FW_READY_FLAGS,
+};
+
+const struct ext_man_cc_version ext_man_cc_ver
+	__aligned(EXT_MAN_ALIGN) __section(".fw_metadata") = {
+	.hdr.type = EXT_MAN_ELEM_CC_VERSION,
+	.hdr.elem_size = ALIGN_UP(sizeof(struct ext_man_cc_version),
+				  EXT_MAN_ALIGN),
+	.cc_version = {
+		.ext_hdr.hdr.size = sizeof(struct sof_ipc_cc_version),
+		.ext_hdr.hdr.cmd = SOF_IPC_FW_READY,
+		.ext_hdr.type = SOF_IPC_EXT_CC_INFO,
+		.micro = CC_MICRO,
+		.minor = CC_MINOR,
+		.major = CC_MAJOR,
+		.name = CC_NAME "\0",	///< eg. "XCC", "\0" is needed when
+					///< sizeof(CC_NAME)-1 == sizeof(.name)
+		.optim = CC_OPTIMIZE_FLAGS "\0", ///< eg. "O2"
+		.desc = CC_DESC "\0", ///< eg. " RG-2017.8-linux"
+	},
 };
