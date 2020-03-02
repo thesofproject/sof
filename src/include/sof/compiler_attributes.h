ff1a9814494be63e7073c866e7f5914b04063bd1
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2020-03-02 14:58:11 +0100

    sof: Separate __packed and __aligned from sof/common.h
    This part of source code is frequently used in IPC packets
    definitions, so it shouldn't have any platform dependence.
    After change this file may be include from mentioned place
    without any doubt about ipc message dependances from used
    sof build configuration.
    
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

---
+++ src/include/sof/compiler_attributes.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2019 Intel Corporation. All rights reserved.
+ *
+ * Author: Karol Trzcinski <karolx.trzcinski@linux.intel.com>
+ */
+
+#define __packed __attribute__((packed))
+
+#define __aligned(x) __attribute__((__aligned__(x)))
