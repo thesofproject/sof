f7cdfbc7e7039a3e49c460872d054a823050b2d8
Paul Olaru <paul.olaru@nxp.com>
2020-03-04 17:22:57 +0200

    platform: imx8m: Fixup DMA domain
    Signed-off-by: Paul Olaru <paul.olaru@nxp.com>

--- src/platform/imx8m/platform.c
+++ src/platform/imx8m/platform.c
@@ -182,10 +182,10 @@ int platform_init(struct sof *sof)
 	if (ret < 0)
 		return -ENODEV;
 
-	/* Init EDMA platform domain */
+	/* Init SDMA platform domain */
 	sof->platform_dma_domain =
-		dma_multi_chan_domain_init(&sof->dma_info->dma_array[0],
-					   1, PLATFORM_DEFAULT_CLOCK, false);
+		dma_multi_chan_domain_init(&sof->dma_info->dma_array[1],
+					   1, PLATFORM_DEFAULT_CLOCK, true);
 	scheduler_init_ll(sof->platform_dma_domain);
 
 	/* initialize the host IPC mechanims */
