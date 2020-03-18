48ad1b775efa450620fcb994d6cc19dea5a7cd06
Paul Olaru <paul.olaru@nxp.com>
2020-03-18 14:58:52 +0200

    platform: imx8m: dma: Fix DMA interrupt handling
    On i.MX8M we don't use SDMA which requires individual interrupts per
    channel. Let dma_chan_irq and dma_chan_irq_name fallback to the
    controller specific interrupt.
    
    Signed-off-by: Paul Olaru <paul.olaru@nxp.com>

--- src/platform/imx8m/include/platform/lib/dma.h
+++ src/platform/imx8m/include/platform/lib/dma.h
@@ -18,8 +18,7 @@
 #define DMA_ID_SDMA2	0
 #define DMA_ID_HOST	1
 
-#define dma_chan_irq(dma, chan) \
-	irqstr_get_sof_int(((int *)dma->plat_data.drv_plat_data)[chan])
+#define dma_chan_irq(dma, chan) dma_irq(dma)
 #define dma_chan_irq_name(dma, chan) dma_irq_name(dma)
 
 #endif /* __PLATFORM_LIB_DMA_H__ */
