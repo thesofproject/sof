1e59d54d549eaab9c945f2e196b7cb840679306e
Janusz Jankowski <janusz.jankowski@linux.intel.com>
2020-03-20 14:54:23 +0100

    test: audio: remove kpb
    KPB tests don't work and it's better to remove it,
    than keeping here disabled code.
    
    Signed-off-by: Janusz Jankowski <janusz.jankowski@linux.intel.com>

--- test/cmocka/src/audio/kpb/kpb_mock.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause
- *
- * Copyright(c) 2019 Intel Corporation. All rights reserved.
- *
- * Author: Marcin Rajwa <marcin.rajwa@linux.intel.com>
- */
-
-int comp_set_state(struct comp_dev *dev, int cmd);
