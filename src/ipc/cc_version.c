beea8e56fb1c9aa582ed17b61bc0be108f679698
Pan Xiuli <xiuli.pan@linux.intel.com>
2020-03-26 11:27:30 +0800

    ipc: cc_version: use fixed length for CC_DESC
    There is a bug in the Cadence XCC compiler that give us different
    linkage section sizes for flex length struct sof_ipc_ext_data in
    ELF file compared to GCC version. And there are appending struct
    in the same ELF section, this will bring wrong offset for those
    struct binary in ELF file.
    
    Example:
    When the CC_DESC is " RG-2017.8-linux", we should have struct
    length for 0x50 in header size, but the binary length in ELF file
    is 0x4c. When the CC_DESC is " RG-2017.8-win", size are both 0x4c
    
    All existing compiler description has length less than
    32 bytes. So use a fixed length 32 in this structure.
    
    ABI version changes to 3.15.0
    
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

--- src/ipc/cc_version.c
+++ src/ipc/cc_version.c
@@ -19,12 +19,16 @@
 	field = CC_OPTIMIZE_FLAGS, \
 	field[ARRAY_SIZE(((struct sof_ipc_cc_version *)(0))->optim) - 1] = 0
 
+/* copy CC_DESC to arrays during compilation time */
+#define CC_DESC_COPY(field) \
+	field = CC_DESC, \
+	field[ARRAY_SIZE(((struct sof_ipc_cc_version *)(0))->desc) - 1] = 0
+
 const struct sof_ipc_cc_version cc_version
 	__section(".fw_ready_metadata") = {
 	.ext_hdr = {
 		.hdr.cmd = SOF_IPC_FW_READY,
-		.hdr.size = ALIGN_UP(sizeof(struct sof_ipc_cc_version)
-				     + sizeof(CC_DESC), 4),
+		.hdr.size = sizeof(struct sof_ipc_cc_version),
 		.type = SOF_IPC_EXT_CC_INFO,
 	},
 	.micro = CC_MICRO,
@@ -32,5 +36,5 @@ const struct sof_ipc_cc_version cc_version
 	.major = CC_MAJOR,
 	CC_NAME_COPY(.name),
 	CC_OPTIM_COPY(.optim),
-	.desc = CC_DESC,
+	CC_DESC_COPY(.desc),
 };
