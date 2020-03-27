407576333880b7a898f846a5d9bb86c24b53f09e
Slawomir Blauciak <slawomir.blauciak@linux.intel.com>
2020-03-27 09:50:59 +0100

    alh: obtain hw params from ipc config
    This allows the firmware to become aware of the programmed format on ALH DAIs.
    
    Signed-off-by: Slawomir Blauciak <slawomir.blauciak@linux.intel.com>

--- src/include/sof/drivers/alh.h
+++ src/include/sof/drivers/alh.h
@@ -7,8 +7,13 @@
 #ifndef __SOF_DRIVERS_ALH__
 #define __SOF_DRIVERS_ALH__
 
+#include <ipc/dai-intel.h>
 #include <platform/drivers/alh.h>
 #include <sof/lib/dai.h>
+
+struct alh_pdata {
+	struct sof_ipc_dai_alh_params params;
+};
 
 extern const struct dai_driver alh_driver;
 
