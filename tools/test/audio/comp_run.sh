14ab84e7e33bb3e2a42180c07bab7653713fce0d
Pan Xiuli <xiuli.pan@linux.intel.com>
2020-03-30 16:39:25 +0800

    sripts: update host-testbench to work with latest code
    Enable 3 simple test with host-testbench:
    volume, src and eqiir
    Usage:
    ./scripts/build-tools.sh -t
    ./scripts/host-build-all.sh
    ./scripts/host-testbench.sh
    
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

--- tools/test/audio/comp_run.sh
+++ tools/test/audio/comp_run.sh
@@ -16,7 +16,7 @@ HOST_ROOT=../../testbench/build_testbench
 HOST_EXE=$HOST_ROOT/install/bin/testbench
 HOST_LIB=$HOST_ROOT/sof_ep/install/lib
 TPLG_LIB=$HOST_ROOT/sof_parser/install/lib
-TPLG_DIR=../../test/topology
+TPLG_DIR=../../build_tools/test/topology
 
 # Use topology from component test topologies
 INFMT=s${BITS_IN}le
