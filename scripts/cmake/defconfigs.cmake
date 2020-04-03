99d03de968f8cd2feec92a543fef67644019f7ff
Janusz Jankowski <janusz.jankowski@linux.intel.com>
2020-04-03 13:05:22 +0200

    cmake: add *_overridedefconfig targets
    Add targets that are meant to be used after defconfig,
    to apply configs from <arch>/configs/override on top of defconfigs.
    
    Signed-off-by: Janusz Jankowski <janusz.jankowski@linux.intel.com>

--- scripts/cmake/defconfigs.cmake
+++ scripts/cmake/defconfigs.cmake
@@ -27,3 +27,27 @@ foreach(defconfig_path ${DEFCONFIG_PATHS})
 		USES_TERMINAL
 	)
 endforeach()
+
+set(OVERRIDE_DEFCONFIGS_DIRECTORY "${DEFCONFIGS_DIRECTORY}/override")
+file(GLOB OVERRIDE_DEFCONFIGS_PATHS "${OVERRIDE_DEFCONFIGS_DIRECTORY}/*.config")
+
+foreach(config_path ${OVERRIDE_DEFCONFIGS_PATHS})
+	get_filename_component(config_name ${config_path} NAME_WE)
+	add_custom_target(
+		"${config_name}_overridedefconfig"
+		COMMAND ${CMAKE_COMMAND} -E copy
+			${config_path}
+			${PROJECT_BINARY_DIR}/override.config
+		COMMAND ${CMAKE_COMMAND} -E env
+			srctree=${PROJECT_SOURCE_DIR}
+			CC_VERSION_TEXT=${CC_VERSION_TEXT}
+			ARCH=${ARCH}
+			${PYTHON3} ${PROJECT_SOURCE_DIR}/scripts/kconfig/overrideconfig.py
+			${PROJECT_SOURCE_DIR}/Kconfig
+			${PROJECT_BINARY_DIR}/override.config
+		WORKING_DIRECTORY ${GENERATED_DIRECTORY}
+		COMMENT "Applying overrideconfig with ${config_name}"
+		VERBATIM
+		USES_TERMINAL
+	)
+endforeach()
