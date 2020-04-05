d53778f3729799cd2f18dcd52d5e0bd52b55ef9c
Daniel Leung <daniel.leung@intel.com>
2020-04-05 02:19:44 -0700

    cmake: update git submodules when cmake is run
    This adds the necessary bits to update git submodules when
    cmake is run, with the option to turn this behavior off if
    needed. This is in preparation to use fw.h and manifest.h
    from the rimage repo to prevent having two copies of each
    file in two different repos. Obviously, the files in
    the submodules must exist before building the firmware,
    so run git submodule update to checkout the files.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

---
+++ scripts/cmake/git-submodules.cmake
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+find_package(Git)
+if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
+	# Update submodules by default
+	option(GIT_SUBMODULE "Check submodules during build" ON)
+
+	if(GIT_SUBMODULE)
+		message(STATUS "Git submodule update")
+
+		execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+				RESULT_VARIABLE GIT_SUBMOD_RESULT)
+
+		if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+			message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+		endif()
+	endif()
+endif()
+
+if(NOT EXISTS "${PROJECT_SOURCE_DIR}/rimage/CMakeLists.txt")
+	message(FATAL_ERROR "The submodules were not downloaded! GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
+endif()
