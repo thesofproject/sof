thesofproject/sof

commit 36d4c9f8f6354ec1237223da353ac434a96daaa8

kpb: add new state
This adds new state KPB_STATE_INIT_DRAINING into
KPB internal states. This way we can see failed
init draining in logs. This new state is also
needed to change KPBs regular behavior in a
situation where we have initied draining task,
therefore calculated read position but the task
doesn't start before next pipeline_copy happens.
In this case, having this new state we can
recognize it and update read position
accordingly to new data.

Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

src/include/sof/audio/kpb.h
@@ -54,6 +54,15 @@ struct comp_buffer;
 /**< Host buffer shall be at least two times bigger than history buffer. */
 #define HOST_BUFFER_MIN_SIZE(hb) (hb * 2)
 
+/** All states below as well as relations between them are documented in
+ * the sof-dosc in [kpbm-state-diagram]
+ * Therefore any addition of new states or modification of existing ones
+ * should have a corresponding update in the sof-docs.
+ * [kpbm-state-diagram]: https://thesofproject.github.io/latest/
+ * developer_guides/firmware/kd_integration/
+ * kd-integration.html#kpbm-state-diagram "Keyphrase buffer manager state
+ * diagram"
+ */
 enum kpb_state {
 	KPB_STATE_DISABLED = 0,
 	KPB_STATE_RESET_FINISHING,
@@ -61,6 +70,7 @@ enum kpb_state {
 	KPB_STATE_PREPARING,
 	KPB_STATE_RUN,
 	KPB_STATE_BUFFERING,
+	KPB_STATE_INIT_DRAINING,
 	KPB_STATE_DRAINING,
 	KPB_STATE_HOST_COPY,
 	KPB_STATE_RESETTING,

src/audio/kpb.c
@@ -674,9 +674,10 @@ static int kpb_copy(struct comp_dev *dev)
 		comp_update_buffer_consume(source, copy_bytes);
 
 		break;
+	case KPB_STATE_INIT_DRAINING:
 	case KPB_STATE_DRAINING:
-		/* In draining state we only buffer data in internal,
-		 * history buffer.
+		/* In draining and init draining we only buffer data in
+		 * the internal history buffer.
 		 */
 		if (source->stream.avail <= kpb->buffer_size) {
 			ret = kpb_buffer_data(dev, source,
@@ -734,7 +735,13 @@ static int kpb_buffer_data(struct comp_dev *dev,
 
 	tracev_kpb_with_ids(dev, "kpb_buffer_data()");
 
-	if (kpb->state != KPB_STATE_RUN && kpb->state != KPB_STATE_DRAINING) {
+	/* We are allowed to buffer data in internal history buffer
+	 * only in KPB_STATE_RUN, KPB_STATE_DRAINING or KPB_STATE_INIT_DRAINING
+	 * states.
+	 */
+	if (kpb->state != KPB_STATE_RUN &&
+	    kpb->state != KPB_STATE_DRAINING &&
+	    kpb->state != KPB_STATE_INIT_DRAINING) {
 		trace_kpb_error("kpb_buffer_data() error: wrong state! (current state %d, state log %x)",
 				kpb->state, kpb->state_log);
 		return PPL_STATUS_PATH_STOP;
@@ -964,6 +971,9 @@ static void kpb_init_draining(struct comp_dev *dev, struct kpb_client *cli)
 		 * read pointer from which we will start draining.
 		 */
 		spin_lock_irq(&kpb->lock, flags);
+
+		kpb_change_state(kpb, KPB_STATE_INIT_DRAINING);
+
 		do {
 			/* Calculate how much data we have stored in
 			 * current buffer.

commit aa994d016389790a5a00068c7f69e45697dd80e9

kpb: fix broken doxygen link to kpbm-state-diagram
As reported by PR #2741:

sof/src/include/sof/audio/kpb.h:46: warning: explicit link request to 'kpbm' could not be resolved

Fixes: 36d4c9f8f635 ("kpb: add new state")

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

src/include/sof/audio/kpb.h
@@ -41,10 +41,9 @@ struct comp_buffer;
  * the sof-dosc in [kpbm-state-diagram]
  * Therefore any addition of new states or modification of existing ones
  * should have a corresponding update in the sof-docs.
- * [kpbm-state-diagram]: https://thesofproject.github.io/latest/
- * developer_guides/firmware/kd_integration/
- * kd-integration.html#kpbm-state-diagram "Keyphrase buffer manager state
- * diagram"
+ * [kpbm-state-diagram]:
+https://thesofproject.github.io/latest/developer_guides/firmware/kd_integration/kd-integration.html#kpbm-state-diagram
+"Keyphrase buffer manager state diagram"
  */
 enum kpb_state {
 	KPB_STATE_DISABLED = 0,