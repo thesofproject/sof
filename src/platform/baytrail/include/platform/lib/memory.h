ce0c0a56b717a88917f8b5f08306e6fac63027e9
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2020-04-17 12:28:35 +0200

    fixup! sof: Add fw_metadata section
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

--- src/platform/baytrail/include/platform/lib/memory.h
+++ src/platform/baytrail/include/platform/lib/memory.h
@@ -98,8 +98,8 @@ static inline void *platform_rfree_prepare(void *ptr)
 #define LOG_ENTRY_ELF_BASE	0x20000000
 #define LOG_ENTRY_ELF_SIZE	0x2000000
 
-#define EXT_MANIFEST_ELF_BASE	(LOG_ENTRY_ELF_BASE + LOG_ENTRY_ELF_SIZE)
-#define EXT_MANIFEST_ELF_SIZE	0x2000000
+#define EXT_MANIFEST_ELF_BASE	0x22000000
+#define EXT_MANIFEST_ELF_SIZE	0x8000
 
 /*
  * The Heap and Stack on Baytrail are organised like this :-
