9b3378818940ec50713b9b0dfc362cd57f46144b
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2020-04-21 11:14:40 +0200

    agent: make panic on delay configurable
    Adds additional functionality to system agent that allows
    to enable and disable panic on delay when necessary.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/include/sof/lib/agent.h
+++ src/include/sof/lib/agent.h
@@ -8,8 +8,11 @@
 #ifndef __SOF_LIB_AGENT_H__
 #define __SOF_LIB_AGENT_H__
 
+#include <sof/atomic.h>
+#include <sof/lib/memory.h>
 #include <sof/lib/perf_cnt.h>
 #include <sof/schedule/task.h>
+#include <sof/sof.h>
 #include <config.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -25,15 +28,37 @@ struct sa {
 	struct perf_cnt_data pcd;
 #endif
 	struct task work;
+	atomic_t panic_cnt;	/**< ref counter for panic_on_delay property */
+	bool panic_on_delay;	/**< emits panic on delay if true */
 };
 
 #if CONFIG_HAVE_AGENT
+
+/**
+ * Enables or disables panic on agent delay.
+ * @param enabled True for panic enabling, false otherwise.
+ */
+static inline void sa_set_panic_on_delay(bool enabled)
+{
+	struct sa *sa = sof_get()->sa;
+
+	if (enabled)
+		atomic_add(&sa->panic_cnt, 1);
+	else
+		atomic_sub(&sa->panic_cnt, 1);
+
+	/* enable panic only if no refs */
+	sa->panic_on_delay = !atomic_read(&sa->panic_cnt);
+
+	platform_shared_commit(sa, sizeof(*sa));
+}
 
 void sa_init(struct sof *sof, uint64_t timeout);
 
 #else
 
 static inline void sa_init(struct sof *sof, uint64_t timeout) { }
+static inline void sa_set_panic_on_delay(bool enabled) { }
 
 #endif
 
