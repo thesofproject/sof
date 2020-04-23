31a6a27ece6cc2a090c7c3d14921c4d101a09bcd
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2020-04-23 20:15:46 +0300

    Tools: Test: Relax signal level pass criteria in quick chirp test
    The criteria of overall -3 dB RMS attenuation was too tight
    for sample EQ responses to pass. With new -6 dB criteria the
    used EQ loudness responses do not trigger error.
    
    The chirp test is only a quick generic component health check so this
    is not critical. The other AES17 tests will catch minor characteristic
    changes.
    
    The other changes are cosmetic and improve result print and plot
    output look.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/test/audio/test_utils/chirp_test_analyze.m
+++ tools/test/audio/test_utils/chirp_test_analyze.m
@@ -1,34 +1,11 @@
 function test = chirp_test_analyze(test)
 
-%%
-% Copyright (c) 2017, Intel Corporation
-% All rights reserved.
-%
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions are met:
-%   * Redistributions of source code must retain the above copyright
-%     notice, this list of conditions and the following disclaimer.
-%   * Redistributions in binary form must reproduce the above copyright
-%     notice, this list of conditions and the following disclaimer in the
-%     documentation and/or other materials provided with the distribution.
-%   * Neither the name of the Intel Corporation nor the
-%     names of its contributors may be used to endorse or promote products
-%     derived from this software without specific prior written permission.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-% POSSIBILITY OF SUCH DAMAGE.
-%
+% SPDX-License-Identifier: BSD-3-Clause
+% Copyright(c) 2017-2020 Intel Corporation. All rights reserved.
 % Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
-%
+
+test.ph = [];
+test.fh = [];
 
 %% Load output file
 [x, nx] = load_test_output(test);
@@ -53,7 +30,7 @@ tz = sz(1)/test.fs;
 et = abs(test.cl-tz)/test.cl;
 rms_db = 10*log10(mean(z.^2)) + 20*log10(sqrt(2));
 offs_db = 20*log10(abs(mean(z)));
-sum_rms_db = 10*log10(mean(s.^2))
+sum_rms_db = 10*log10(mean(s.^2));
 % Check for proper ratio of out/in samples, minimum level, maximum offset
 % and maximum of sum of channels. The input is such that the channels should
 % sum to zero. A phase difference in channels would cause non-zero output
@@ -62,7 +39,7 @@ sum_rms_db = 10*log10(mean(s.^2))
 if et > 0.05
 	fail = 1;
 	fprintf('Failed output chirp length, err=%f, t=%f.\n', et, tz);
-else if (min(rms_db) < -3) && (test.fs2 + 1 > test.fs1)
+else if (min(rms_db) < -6) && (test.fs2 + 1 > test.fs1)
 	     fail = 1;
 	     fprintf('Failed output chirp level.\n');
      else if max(offs_db) > -40
@@ -79,6 +56,7 @@ else if (min(rms_db) < -3) && (test.fs2 + 1 > test.fs1)
 end
 
 test.fh = figure('visible', test.plot_visible);
+test.ph = subplot(1, 1, 1);
 ns = 1024;
 no = round(0.9*ns);
 specgram(y(:,1), ns, test.fs, kaiser(ns,27), no);
