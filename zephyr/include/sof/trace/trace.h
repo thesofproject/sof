74ea3abb8a0294e6db4c30ec6266e21fc4edee15
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2020-04-24 17:37:20 +0300

    zephyr: Move more headers to zephyr/include

--- zephyr/include/sof/trace/trace.h
+++ zephyr/include/sof/trace/trace.h
@@ -12,6 +12,7 @@
  */
 
 #define trace_point(x)
+#define trace_event(...)
 
 #define tracev_event(...)
 #define tracev_event_with_ids(...)
@@ -19,5 +20,25 @@
 #define tracev_event_atomic_with_ids(...)
 #define tracev_value(x)
 #define tracev_value_atomic(x)
+
+#define trace_error(...)
+#define trace_warn(...)
+
+#define trace_on(...)
+#define trace_off(...)
+
+#define trace_error_atomic(...)
+#define trace_event_atomic(...)
+
+#define trace_error_with_ids(...)
+#define trace_event_with_ids(...)
+
+#define trace_dev_dbg(...)
+#define trace_dev_info(...)
+#define trace_dev_err(...)
+#define trace_dev_warn(...)
+
+static inline void trace_flush(void) { }
+static inline void trace_log(void) { }
 
 #endif /* __INCLUDE_TRACE__ */
