1890595ac606886cec77abacbd5c82f84b175be8
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2020-04-24 13:52:55 +0300

    zephyr: Add headers to be included for zephyr build
    These headers will be included when building for Zephyr.

---
+++ zephyr/include/sof/trace/trace.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright(c) 2020 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __INCLUDE_TRACE__
+#define __INCLUDE_TRACE__
+
+/**
+ * Placeholder for trace with Zephyr logging subsystem
+ */
+
+#define trace_point(x)
+
+#define tracev_event(...)
+#define tracev_event_with_ids(...)
+#define tracev_event_atomic(...)
+#define tracev_event_atomic_with_ids(...)
+#define tracev_value(x)
+#define tracev_value_atomic(x)
+
+#endif /* __INCLUDE_TRACE__ */
