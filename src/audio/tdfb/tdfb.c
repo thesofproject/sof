fc5d74fccf690b20eb53daa66400e6ef85935eae
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2020-05-11 19:19:11 +0300

    Audio: Add Time Domain Fixed Beamformer (TDFB) processing component
    This patch adds the multi-microphone beamforming component. It enhances
    microphone capture via spatial noise suppression. The component is a
    quite generic FIR time domain filter bank and the fixed filter band
    needs to be programmed with super directive or other beamformer criteria
    filter coefficients. The coefficients are fixed but they can be
    re-programmed during run-time.
    
    The component reuses the FIR filter core but has different inputs
    selection and outputs mixing features than FIR EQ so it is made a
    separate new processing component.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

---
+++ src/audio/tdfb/tdfb.c
@@ -0,0 +1,549 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2020 Intel Corporation. All rights reserved.
+//
+// Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
+
+#include <ipc/control.h>
+#include <ipc/stream.h>
+#include <ipc/topology.h>
+#include <user/tdfb.h>
+#include <user/trace.h>
+#include <sof/common.h>
+#include <sof/debug/panic.h>
+#include <sof/drivers/ipc.h>
+#include <sof/lib/alloc.h>
+#include <sof/lib/memory.h>
+#include <sof/lib/uuid.h>
+#include <sof/list.h>
+#include <sof/platform.h>
+#include <sof/string.h>
+#include <sof/audio/buffer.h>
+#include <sof/audio/component.h>
+#include <sof/audio/pipeline.h>
+#include <sof/audio/tdfb/tdfb_comp.h>
+#include <sof/math/fir_generic.h>
+#include <sof/math/fir_hifi2ep.h>
+#include <sof/math/fir_hifi3.h>
+#include <sof/trace/trace.h>
+#include <sof/ut.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdint.h>
+
+static const struct comp_driver comp_tdfb;
+
+/* dd511749-d9fa-455c-b3a7-13585693f1af */
+DECLARE_SOF_RT_UUID("tdfb", tdfb_uuid,  0xdd511749, 0xd9fa, 0x455c, 0xb3, 0xa7,
+		    0x13, 0x58, 0x56, 0x93, 0xf1, 0xaf);
+
+DECLARE_TR_CTX(tdfb_tr, SOF_UUID(tdfb_uuid), LOG_LEVEL_INFO);
+
+/*
+ * The optimized FIR functions variants need to be updated into function
+ * set_func.
+ */
+
+#if CONFIG_FORMAT_S16LE
+static inline void set_s16_fir(struct tdfb_comp_data *cd)
+{
+	cd->tdfb_func = tdfb_fir_s16;
+}
+#endif /* CONFIG_FORMAT_S16LE */
+#if CONFIG_FORMAT_S24LE
+static inline void set_s24_fir(struct tdfb_comp_data *cd)
+{
+	cd->tdfb_func = tdfb_fir_s24;
+}
+#endif /* CONFIG_FORMAT_S24LE */
+#if CONFIG_FORMAT_S32LE
+static inline void set_s32_fir(struct tdfb_comp_data *cd)
+{
+	cd->tdfb_func = tdfb_fir_s32;
+}
+#endif /* CONFIG_FORMAT_S32LE */
+
+static inline int set_func(struct comp_dev *dev)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+	struct comp_buffer *sourceb;
+
+	sourceb = list_first_item(&dev->bsource_list, struct comp_buffer,
+				  sink_list);
+
+	switch (sourceb->stream.frame_fmt) {
+#if CONFIG_FORMAT_S16LE
+	case SOF_IPC_FRAME_S16_LE:
+		comp_info(dev, "set_func(), SOF_IPC_FRAME_S16_LE");
+		set_s16_fir(cd);
+		break;
+#endif /* CONFIG_FORMAT_S16LE */
+#if CONFIG_FORMAT_S24LE
+	case SOF_IPC_FRAME_S24_4LE:
+		comp_info(dev, "set_func(), SOF_IPC_FRAME_S24_4LE");
+		set_s24_fir(cd);
+		break;
+#endif /* CONFIG_FORMAT_S24LE */
+#if CONFIG_FORMAT_S32LE
+	case SOF_IPC_FRAME_S32_LE:
+		comp_info(dev, "set_func(), SOF_IPC_FRAME_S32_LE");
+		set_s32_fir(cd);
+		break;
+#endif /* CONFIG_FORMAT_S32LE */
+	default:
+		comp_err(dev, "set_func(), invalid frame_fmt");
+		return -EINVAL;
+	}
+	return 0;
+}
+
+/*
+ * Control code functions next. The processing is in fir_ C modules.
+ */
+
+static void tdfb_free_delaylines(struct tdfb_comp_data *cd)
+{
+	struct fir_state_32x16 *fir = cd->fir;
+	int i = 0;
+
+	/* Free the common buffer for all EQs and point then
+	 * each FIR channel delay line to NULL.
+	 */
+	rfree(cd->fir_delay);
+	cd->fir_delay = NULL;
+	cd->fir_delay_size = 0;
+	for (i = 0; i < PLATFORM_MAX_CHANNELS; i++)
+		fir[i].delay = NULL;
+}
+
+static int tdfb_init_coef(struct tdfb_comp_data *cd, int source_nch,
+			  int sink_nch)
+{
+	struct sof_fir_coef_data *coef_data;
+	struct sof_tdfb_config *config = cd->config;
+	int16_t *coefp;
+	int size_sum = 0;
+	int max_ch;
+	int s;
+	int i;
+
+	/* Sanity checks */
+	if (config->num_output_channels > PLATFORM_MAX_CHANNELS ||
+	    !config->num_output_channels) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid num_output_channels %d",
+			    config->num_output_channels);
+		return -EINVAL;
+	}
+
+	if (config->num_output_channels != sink_nch) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), stream output channels count %d does not match configuration %d",
+			    sink_nch, config->num_output_channels);
+		return -EINVAL;
+	}
+
+	if (config->num_filters > SOF_TDFB_FIR_MAX_COUNT) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid num_filters %d",
+			    config->num_filters);
+		return -EINVAL;
+	}
+
+	coefp = ASSUME_ALIGNED(&config->data[0], 2);
+	for (i = 0; i < config->num_filters; i++) {
+		/* Get delay line size */
+		coef_data = (struct sof_fir_coef_data *)coefp;
+		s = fir_delay_size(coef_data);
+		if (s > 0) {
+			size_sum += s;
+		} else {
+			comp_cl_info(&comp_tdfb, "tdfb_init_coef(), FIR length %d is invalid",
+				     coef_data->length);
+			return -EINVAL;
+		}
+
+		/* Initialize coefficients for FIR filter and find next
+		 * filter.
+		 */
+		fir_init_coef(&cd->fir[i], coef_data);
+		coefp += SOF_FIR_COEF_NHEADER + coef_data->length;
+	}
+
+	/* Get shortcuts to input and output configuration */
+	cd->input_channel_select = coefp;
+	cd->output_channel_mix = coefp + config->num_filters;
+	cd->output_stream_mix = coefp + 2 * config->num_filters;
+
+	/* Find max used input channel */
+	max_ch = 0;
+	for (i = 0; i < config->num_filters; i++) {
+		if (cd->input_channel_select[i] > max_ch)
+			max_ch = cd->input_channel_select[i];
+	}
+
+	/* The stream must contain at least the number of channels that is
+	 * used for filters input.
+	 */
+	if (max_ch + 1 > source_nch) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), stream input channels count %d is not sufficient for configuration %d",
+			    source_nch, max_ch + 1);
+		return -EINVAL;
+	}
+
+	return size_sum;
+}
+
+static void tdfb_init_delay(struct tdfb_comp_data *cd)
+{
+	int32_t *fir_delay = cd->fir_delay;
+	int i;
+
+	/* Initialize second phase to set delay lines pointers */
+	for (i = 0; i < cd->config->num_filters; i++) {
+		if (cd->fir[i].length > 0)
+			fir_init_delay(&cd->fir[i], &fir_delay);
+	}
+}
+
+static int tdfb_setup(struct tdfb_comp_data *cd, int source_nch, int sink_nch)
+{
+	int delay_size;
+
+	/* Free existing FIR channels data if it was allocated */
+	tdfb_free_delaylines(cd);
+
+	/* Set coefficients for each channel from coefficient blob */
+	delay_size = tdfb_init_coef(cd, source_nch, sink_nch);
+	if (delay_size < 0)
+		return delay_size; /* Contains error code */
+
+	/* If all channels were set to bypass there's no need to
+	 * allocate delay. Just return with success.
+	 */
+	if (!delay_size)
+		return 0;
+
+	/* Allocate all FIR channels data in a big chunk and clear it */
+	cd->fir_delay = rballoc(0, SOF_MEM_CAPS_RAM, delay_size);
+	if (!cd->fir_delay) {
+		comp_cl_err(&comp_tdfb, "tdfb_setup(), delay allocation failed for size %d",
+			    delay_size);
+		return -ENOMEM;
+	}
+
+	memset(cd->fir_delay, 0, delay_size);
+	cd->fir_delay_size = delay_size;
+
+	/* Assign delay line to all channel filters */
+	tdfb_init_delay(cd);
+	return 0;
+}
+
+/*
+ * End of algorithm code. Next the standard component methods.
+ */
+
+static struct comp_dev *tdfb_new(const struct comp_driver *drv,
+				 struct sof_ipc_comp *comp)
+{
+	struct sof_ipc_comp_process *ipc_tdfb =
+		(struct sof_ipc_comp_process *)comp;
+	struct comp_dev *dev;
+	struct tdfb_comp_data *cd;
+	size_t bs = ipc_tdfb->size;
+	int ret;
+	int i;
+
+	comp_cl_info(&comp_tdfb, "tdfb_new()");
+
+	/* Check first that configuration blob size is sane */
+	if (bs > SOF_TDFB_MAX_SIZE) {
+		comp_cl_err(&comp_tdfb, "tdfb_new() error: configuration blob size = %u > %d",
+			    bs, SOF_TDFB_MAX_SIZE);
+		return NULL;
+	}
+
+	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_process));
+	if (!dev)
+		return NULL;
+
+	memcpy_s(COMP_GET_IPC(dev, sof_ipc_comp_process),
+		 sizeof(struct sof_ipc_comp_process), ipc_tdfb,
+		 sizeof(struct sof_ipc_comp_process));
+
+	dev->state = COMP_STATE_INIT;
+
+	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*cd));
+	if (!cd) {
+		rfree(dev);
+		return NULL;
+	}
+
+	comp_set_drvdata(dev, cd);
+
+	cd->tdfb_func = NULL;
+	cd->fir_delay = NULL;
+	cd->fir_delay_size = 0;
+
+	/* Handler for configuration data */
+	cd->model_handler = comp_data_blob_handler_new(dev);
+	if (!cd->model_handler) {
+		comp_cl_err(&comp_tdfb, "tdfb_new(): comp_data_blob_handler_new() failed.");
+		rfree(dev);
+		rfree(cd);
+		return NULL;
+	}
+
+	/* Get configuration data and reset FIR filters */
+	ret = comp_init_data_blob(cd->model_handler, bs, ipc_tdfb->data);
+	if (ret < 0) {
+		comp_cl_err(&comp_tdfb, "tdfb_new(): comp_init_data_blob() failed.");
+		rfree(dev);
+		rfree(cd);
+		return NULL;
+	}
+
+	for (i = 0; i < PLATFORM_MAX_CHANNELS; i++)
+		fir_reset(&cd->fir[i]);
+
+	dev->state = COMP_STATE_READY;
+	return dev;
+}
+
+static void tdfb_free(struct comp_dev *dev)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+
+	comp_info(dev, "tdfb_free()");
+
+	tdfb_free_delaylines(cd);
+	comp_data_blob_handler_free(cd->model_handler);
+
+	rfree(cd);
+	rfree(dev);
+}
+
+static int tdfb_cmd_get_data(struct comp_dev *dev,
+			     struct sof_ipc_ctrl_data *cdata, int max_size)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+	int ret = 0;
+
+	switch (cdata->cmd) {
+	case SOF_CTRL_CMD_BINARY:
+		comp_info(dev, "tdfb_cmd_get_data(), SOF_CTRL_CMD_BINARY");
+		ret = comp_data_blob_get_cmd(cd->model_handler, cdata, max_size);
+		break;
+	default:
+		comp_err(dev, "tdfb_cmd_get_data() error: invalid cdata->cmd");
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
+}
+
+static int tdfb_cmd_set_data(struct comp_dev *dev,
+			     struct sof_ipc_ctrl_data *cdata)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+	int ret = 0;
+
+	switch (cdata->cmd) {
+	case SOF_CTRL_CMD_BINARY:
+		comp_info(dev, "tdfb_cmd_set_data(), SOF_CTRL_CMD_BINARY");
+		ret = comp_data_blob_set_cmd(cd->model_handler, cdata);
+		break;
+	default:
+		comp_err(dev, "tdfb_cmd_set_data() error: invalid cdata->cmd");
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+
+/* used to pass standard and bespoke commands (with data) to component */
+static int tdfb_cmd(struct comp_dev *dev, int cmd, void *data,
+		    int max_data_size)
+{
+	struct sof_ipc_ctrl_data *cdata = data;
+	int ret = 0;
+
+	comp_info(dev, "tdfb_cmd()");
+
+	switch (cmd) {
+	case COMP_CMD_SET_DATA:
+		ret = tdfb_cmd_set_data(dev, cdata);
+		break;
+	case COMP_CMD_GET_DATA:
+		ret = tdfb_cmd_get_data(dev, cdata, max_data_size);
+		break;
+	default:
+		comp_err(dev, "tdfb_cmd() error: invalid command");
+		ret = -EINVAL;
+	}
+
+	return ret;
+}
+
+static void tdfb_process(struct comp_dev *dev, struct comp_buffer *source,
+			 struct comp_buffer *sink, int frames,
+			 uint32_t source_bytes, uint32_t sink_bytes)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+
+	buffer_invalidate(source, source_bytes);
+
+	cd->tdfb_func(cd, &source->stream, &sink->stream, frames);
+
+	buffer_writeback(sink, sink_bytes);
+
+	/* calc new free and available */
+	comp_update_buffer_consume(source, source_bytes);
+	comp_update_buffer_produce(sink, sink_bytes);
+}
+
+/* copy and process stream data from source to sink buffers */
+static int tdfb_copy(struct comp_dev *dev)
+{
+	struct comp_copy_limits cl;
+	struct comp_buffer *sourceb;
+	struct comp_buffer *sinkb;
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+	int ret;
+	int n;
+
+	comp_dbg(dev, "tdfb_copy()");
+
+	sourceb = list_first_item(&dev->bsource_list, struct comp_buffer,
+				  sink_list);
+	sinkb = list_first_item(&dev->bsink_list, struct comp_buffer,
+				source_list);
+
+	/* Check for changed configuration */
+	if (comp_is_new_data_blob_available(cd->model_handler)) {
+		cd->config = comp_get_data_blob(cd->model_handler, NULL, NULL);
+		ret = tdfb_setup(cd, sourceb->stream.channels, sinkb->stream.channels);
+		if (ret < 0) {
+			comp_err(dev, "tdfb_copy(), failed FIR setup");
+			return ret;
+		}
+	}
+
+	/* Get source, sink, number of frames etc. to process. */
+	comp_get_copy_limits(sourceb, sinkb, &cl);
+
+	/*
+	 * Process only even number of frames with the FIR function. The
+	 * optimized filter function loads the successive input samples from
+	 * internal delay line with a 64 bit load operation.
+	 */
+	if (cl.frames >= 2) {
+		n = (cl.frames >> 1) << 1;
+
+		/* Run the process function */
+		tdfb_process(dev, sourceb, sinkb, n,
+			     n * cl.source_frame_bytes,
+			     n * cl.sink_frame_bytes);
+	}
+
+	return 0;
+}
+
+static int tdfb_prepare(struct comp_dev *dev)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+	struct comp_buffer *sourceb;
+	struct comp_buffer *sinkb;
+	int ret;
+
+	comp_info(dev, "tdfb_prepare()");
+
+	ret = comp_set_state(dev, COMP_TRIGGER_PREPARE);
+	if (ret < 0)
+		return ret;
+
+	if (ret == COMP_STATUS_STATE_ALREADY_SET)
+		return PPL_STATUS_PATH_STOP;
+
+	/* Find source and sink buffers */
+	sourceb = list_first_item(&dev->bsource_list,
+				  struct comp_buffer, sink_list);
+	sinkb = list_first_item(&dev->bsink_list,
+				struct comp_buffer, source_list);
+
+	/* Initialize filter */
+	cd->config = comp_get_data_blob(cd->model_handler, NULL, NULL);
+	if (cd->config) {
+		ret = tdfb_setup(cd, sourceb->stream.channels, sinkb->stream.channels);
+		if (ret < 0) {
+			comp_err(dev, "tdfb_prepare() error: tdfb_setup failed.");
+			goto err;
+		}
+
+		/* Clear in/out buffers */
+		memset(cd->in, 0, TDFB_IN_BUF_LENGTH * sizeof(int32_t));
+		memset(cd->out, 0, TDFB_IN_BUF_LENGTH * sizeof(int32_t));
+
+		ret = set_func(dev);
+		return ret;
+	}
+
+err:
+	comp_set_state(dev, COMP_TRIGGER_RESET);
+	return ret;
+}
+
+static int tdfb_reset(struct comp_dev *dev)
+{
+	int i;
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+
+	comp_info(dev, "tdfb_reset()");
+
+	tdfb_free_delaylines(cd);
+
+	cd->tdfb_func = NULL;
+	for (i = 0; i < PLATFORM_MAX_CHANNELS; i++)
+		fir_reset(&cd->fir[i]);
+
+	comp_set_state(dev, COMP_TRIGGER_RESET);
+	return 0;
+}
+
+static int tdfb_trigger(struct comp_dev *dev, int cmd)
+{
+	int ret = 0;
+
+	comp_info(dev, "tdfb_trigger(), command = %u", cmd);
+
+	ret = comp_set_state(dev, cmd);
+	if (ret == COMP_STATUS_STATE_ALREADY_SET)
+		ret = PPL_STATUS_PATH_STOP;
+
+	return ret;
+}
+
+static const struct comp_driver comp_tdfb = {
+	.uid = SOF_RT_UUID(tdfb_uuid),
+	.tctx	= &tdfb_tr,
+	.ops = {
+		.create = tdfb_new,
+		.free = tdfb_free,
+		.cmd = tdfb_cmd,
+		.copy = tdfb_copy,
+		.prepare = tdfb_prepare,
+		.reset = tdfb_reset,
+		.trigger = tdfb_trigger,
+	},
+};
+
+static SHARED_DATA struct comp_driver_info comp_tdfb_info = {
+	.drv = &comp_tdfb,
+};
+
+UT_STATIC void sys_comp_tdfb_init(void)
+{
+	comp_register(platform_shared_get(&comp_tdfb_info,
+					  sizeof(comp_tdfb_info)));
+}
+
+DECLARE_MODULE(sys_comp_tdfb_init);
---
+++ src/audio/tdfb/tdfb_hifi3.c
@@ -0,0 +1,232 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2020 Intel Corporation. All rights reserved.
+//
+// Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
+
+#include <sof/common.h>
+#include <user/tdfb.h>
+#include <sof/audio/audio_stream.h>
+#include <sof/audio/tdfb/tdfb_comp.h>
+#include <user/fir.h>
+#include <user/tdfb.h>
+
+#if TDFB_HIFI3
+
+#include <sof/math/fir_hifi3.h>
+
+#if CONFIG_FORMAT_S16LE
+void tdfb_fir_s16(struct tdfb_comp_data *cd,
+		  const struct audio_stream *source,
+		  struct audio_stream *sink, int frames)
+{
+	struct sof_tdfb_config *cfg = cd->config;
+	struct fir_state_32x16 *f;
+	ae_int16x4 d;
+	ae_int32 y0;
+	ae_int32 y1;
+	ae_int16 *x = (ae_int16 *)source->r_ptr;
+	ae_int16 *y = (ae_int16 *)sink->w_ptr;
+	int shift;
+	int is2;
+	int is;
+	int om;
+	int i;
+	int j;
+	int k;
+	int in_nch = source->channels;
+	int out_nch = sink->channels;
+
+	for (j = 0; j < (frames >> 1); j++) {
+		/* Clear output mix*/
+		memset(cd->out, 0,  2 * out_nch * sizeof(int32_t));
+
+		/* Read two frames from all input channels */
+		fir_comp_setup_circular(source);
+		for (i = 0; i < 2 * in_nch; i++) {
+			AE_L16_XC(d, x, sizeof(int16_t));
+			cd->in[i] = (ae_int32)AE_CVT32X2F16_32(d);
+		}
+
+		/* Run and mix all filters to their output channel */
+		for (i = 0; i < cfg->num_filters; i++) {
+			is = cd->input_channel_select[i];
+			is2 = is + in_nch;
+			om = cd->output_channel_mix[i];
+
+			/* Get filter instance */
+			f = &cd->fir[i];
+			shift = -f->out_shift;
+
+			/* Compute FIR and mix as Q5.27*/
+			fir_core_setup_circular(f);
+			fir_32x16_2x_hifi3(f, cd->in[is], cd->in[is2], &y0, &y1,
+					   shift);
+			for (k = 0; k < out_nch; k++) {
+				if (om & 1) {
+					cd->out[k] += (int32_t)y0 >> 4;
+					cd->out[k + out_nch] +=
+						(int32_t)y1 >> 4;
+				}
+				om = om >> 1;
+			}
+		}
+
+		/* Write two frames of output. The values in out[] are shifted
+		 * left and saturated to convert to Q1.27. The the values
+		 * are then rounded to 16 bit and converted to Q1.15 for
+		 * sink buffer. TODO: Could saturate four samples with
+		 * one AE_ROUND16X4F32SSYM() instruction.
+		 */
+		fir_comp_setup_circular(sink);
+		for (i = 0; i < 2 * out_nch; i++) {
+			d = AE_ROUND16X4F32SSYM(0, AE_SLAI32S(cd->out[i], 4));
+			AE_S16_0_XC(d, y, sizeof(int16_t));
+		}
+	}
+}
+#endif
+
+#if CONFIG_FORMAT_S24LE
+void tdfb_fir_s24(struct tdfb_comp_data *cd,
+		  const struct audio_stream *source,
+		  struct audio_stream *sink, int frames)
+{
+	struct sof_tdfb_config *cfg = cd->config;
+	struct fir_state_32x16 *f;
+	ae_int32x2 d;
+	ae_int32 y0;
+	ae_int32 y1;
+	ae_int32 *x = (ae_int32 *)source->r_ptr;
+	ae_int32 *y = (ae_int32 *)sink->w_ptr;
+	int shift;
+	int is2;
+	int is;
+	int om;
+	int i;
+	int j;
+	int k;
+	int in_nch = source->channels;
+	int out_nch = sink->channels;
+
+	for (j = 0; j < (frames >> 1); j++) {
+		/* Clear output mix*/
+		memset(cd->out, 0,  2 * out_nch * sizeof(int32_t));
+
+		/* Read two frames from all input channels */
+		fir_comp_setup_circular(source);
+		for (i = 0; i < 2 * in_nch; i++) {
+			AE_L32_XC(d, x, sizeof(int32_t));
+			cd->in[i] = AE_SLAI32(d, 8);
+		}
+
+		for (i = 0; i < cfg->num_filters; i++) {
+			is = cd->input_channel_select[i];
+			is2 = is + in_nch;
+			om = cd->output_channel_mix[i];
+
+			/* Get filter instance */
+			f = &cd->fir[i];
+			shift = -f->out_shift;
+
+			/* Compute FIR and mix as Q5.27*/
+			fir_core_setup_circular(f);
+			fir_32x16_2x_hifi3(f, cd->in[is], cd->in[is2], &y0, &y1,
+					   shift);
+			for (k = 0; k < out_nch; k++) {
+				if (om & 1) {
+					cd->out[k] += (int32_t)y0 >> 4;
+					cd->out[k + out_nch] +=
+						(int32_t)y1 >> 4;
+				}
+				om = om >> 1;
+			}
+		}
+
+		/* Write two frames of output. The values from out[] are first
+		 * rounded to Q5.23 format, then saturated to Q1.23, and
+		 * shifted by 8 to LSB side of the word before storing to sink.
+		 * TODO: Could shift etc. two samples simultaneously.
+		 */
+		fir_comp_setup_circular(sink);
+		for (i = 0; i < 2 * out_nch; i++) {
+			d = AE_SRAI32(AE_SLAI32S(AE_SRAI32R(cd->out[i], 4), 8),
+				      8);
+			AE_S32_L_XC(d, y, sizeof(int32_t));
+		}
+	}
+}
+#endif
+
+#if CONFIG_FORMAT_S32LE
+void tdfb_fir_s32(struct tdfb_comp_data *cd,
+		  const struct audio_stream *source,
+		  struct audio_stream *sink, int frames)
+{
+	struct sof_tdfb_config *cfg = cd->config;
+	struct fir_state_32x16 *f;
+	ae_int32x2 d;
+	ae_int32 y0;
+	ae_int32 y1;
+	ae_int32 *x = (ae_int32 *)source->r_ptr;
+	ae_int32 *y = (ae_int32 *)sink->w_ptr;
+	int shift;
+	int is2;
+	int is;
+	int om;
+	int i;
+	int j;
+	int k;
+	int in_nch = source->channels;
+	int out_nch = sink->channels;
+
+	for (j = 0; j < (frames >> 1); j++) {
+		/* Clear output mix*/
+		memset(cd->out, 0,  2 * out_nch * sizeof(int32_t));
+
+		/* Read two frames from all input channels */
+		fir_comp_setup_circular(source);
+		for (i = 0; i < 2 * in_nch; i++) {
+			AE_L32_XC(d, x, sizeof(int32_t));
+			cd->in[i] = d;
+		}
+
+		for (i = 0; i < cfg->num_filters; i++) {
+			is = cd->input_channel_select[i];
+			is2 = is + in_nch;
+			om = cd->output_channel_mix[i];
+
+			/* Get filter instance */
+			f = &cd->fir[i];
+			shift = -f->out_shift;
+
+			/* Compute FIR and mix as Q5.27*/
+			fir_core_setup_circular(f);
+			fir_32x16_2x_hifi3(f, cd->in[is], cd->in[is2], &y0, &y1,
+					   shift);
+			for (k = 0; k < out_nch; k++) {
+				if (om & 1) {
+					cd->out[k] += (int32_t)y0 >> 4;
+					cd->out[k + out_nch] +=
+						(int32_t)y1 >> 4;
+				}
+				om = om >> 1;
+			}
+		}
+
+		/* Write two frames of output. In Q5.27 to Q1.31 conversion
+		 * rounding is not applicable so just shift left by 4 and
+		 * saturate. TODO: Could shift two samples with one
+		 * instruction.
+		 */
+		fir_comp_setup_circular(sink);
+		for (i = 0; i < 2 * out_nch; i++) {
+			d = AE_SLAI32S(cd->out[i], 4);
+			AE_S32_L_XC(d, y, sizeof(int32_t));
+		}
+	}
+}
+#endif
+
+#endif /* TDFB_HIFI3 */
+
