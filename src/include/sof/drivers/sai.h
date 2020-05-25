thesofproject/sof

commit 29414aa25e56a113c7abd2990ffe9d11ca485bbc

platform: imx8m: Fix FIFO depth for i.MX8M
On i.MX8M series, SAI FIFO is configured to hold 128 words.
(128 x 32 bits).

Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

src/include/sof/drivers/sai.h
@@ -231,7 +231,12 @@
 
 #define SAI_FLAG_PMQOS   BIT(0)
 
+#ifdef CONFIG_IMX8M
+#define SAI_FIFO_WORD_SIZE	128
+#else
 #define SAI_FIFO_WORD_SIZE	64
+#endif
+
 /* Divides down the audio master clock to generate the bit clock when
  * configured for an internal bit clock.
  * The division value is (DIV + 1) * 2.

src/platform/imx8m/lib/dai.c
@@ -20,13 +20,13 @@ static SHARED_DATA struct dai sai[] = {
 		.base = SAI_3_BASE,
 		.fifo[SOF_IPC_STREAM_PLAYBACK] = {
 			.offset = SAI_3_BASE + REG_SAI_TDR0,
-			.depth = 16,
+			.depth = 128, /* in 4 bytes words */
 			/* Handshake is SDMA hardware event */
 			.handshake = 5,
 		},
 		.fifo[SOF_IPC_STREAM_CAPTURE] = {
 			.offset = SAI_3_BASE + REG_SAI_RDR0,
-			.depth = 16,
+			.depth = 128, /* in 4 bytes words */
 			.handshake = 4,
 		},
 	},

commit 178a96c44c8b325a909ae46b1e91b1c6f635e586

drivers: sai: Fix watermark mask size
On i.MX8M watermark can be up to 128 and we must adjust
the T/RFW mask register in order for values greater than 64 to be
correctly set.

Now capture works fine!

Fixes: 29414aa25e56a1 ("platform: imx8m: Fix FIFO depth for i.MX8M")
Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

src/include/sof/drivers/sai.h
@@ -127,7 +127,11 @@
 #define REG_SAI_CSR_FRDE	BIT(0)
 
 /* SAI Transmit and Receive Configuration 1 Register */
+#ifdef CONFIG_IMX8M
+#define REG_SAI_CR1_RFW_MASK	0x7f
+#else
 #define REG_SAI_CR1_RFW_MASK	0x3f
+#endif
 
 /* SAI Transmit and Receive Configuration 2 Register */
 #define REG_SAI_CR2_SYNC	SET_BITS(31, 30, 1)