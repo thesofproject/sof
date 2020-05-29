277c8391ccf4a9f4da44a0e710b562e8305af1ad
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2020-05-29 14:55:06 +0200

    logger: Send parsed runtime log levels to FW via debugFS
    "/sys/kernel/debug/sof/filter" file is responsible for updating
    trace log levels. IPC message contain list of new trace threshold,
    it is represented as single line in file where entries are separated
    with ";". Single entry has form of:
    "<uuid_id> <pipe_id> <comp_id> <log_level>"
    
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

--- tools/logger/filter.h
+++ tools/logger/filter.h
@@ -8,6 +8,8 @@
 #ifndef __LOGGER_FILTER_H__
 #define __LOGGER_FILTER_H__
 
+#define FILTER_KERNEL_PATH "/sys/kernel/debug/sof/filter"
+
 int filter_update_firmware(const struct snd_sof_uids_header *uids_dict,
 			   char *input_str);
 
