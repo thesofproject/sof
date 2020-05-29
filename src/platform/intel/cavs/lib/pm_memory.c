8a8d55d78c69f23065f138957cc26195a0df01aa
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2020-05-29 10:38:05 +0200

    boot_loader: use existing memory power gating functions
    Removes from bootloader duplicated memory power gating functions
    and uses existing implementations from pm_memory.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/platform/intel/cavs/lib/pm_memory.c
+++ src/platform/intel/cavs/lib/pm_memory.c
@@ -7,13 +7,9 @@
 
 #include <cavs/lib/pm_memory.h>
 #include <cavs/version.h>
-#include <sof/bit.h>
-#include <sof/lib/io.h>
+#include <sof/common.h>
 #include <sof/lib/memory.h>
-#include <sof/lib/shim.h>
 #include <sof/lib/uuid.h>
-#include <sof/lib/wait.h>
-#include <sof/math/numbers.h>
 #include <sof/trace/trace.h>
 #include <user/trace.h>
 #include <config.h>
@@ -25,10 +21,6 @@ DECLARE_SOF_UUID("pm-memory", pm_mem_uuid, 0x14f25ab6, 0x3a4b, 0x4e5d,
 		 0xb3, 0x43, 0x2a, 0x14, 0x2d, 0x4e, 0x4d, 0x92);
 
 DECLARE_TR_CTX(pm_mem_tr, SOF_UUID(pm_mem_uuid), LOG_LEVEL_INFO);
-
-#if CAVS_VERSION >= CAVS_VERSION_1_8
-
-#define MEMORY_POWER_DOWN_DELAY 256
 
 /**
  * \brief Retrieves memory banks based on start and end pointer.
@@ -64,72 +56,7 @@ static void memory_banks_get(void *start, void *end, uint32_t base,
 	*end_bank = ((uintptr_t)end - base) / SRAM_BANK_SIZE - 1;
 }
 
-/**
- * \brief Retrieves register mask for given segment.
- * \param[in] start_bank Start bank id.
- * \param[in] end_bank End bank id.
- * \param[in] segment Segment id.
- * \return Register mask.
- */
-static uint32_t cavs_pm_memory_hp_sram_mask_get(uint32_t start_bank,
-						uint32_t end_bank, int segment)
-{
-	uint32_t first_in_segment;
-	uint32_t last_in_segment;
-
-	first_in_segment = segment * EBB_SEGMENT_SIZE;
-	last_in_segment = ((segment + 1) * EBB_SEGMENT_SIZE) - 1;
-
-	/* not in this segment */
-	if (start_bank > last_in_segment || end_bank < first_in_segment)
-		return 0;
-
-	if (start_bank >= first_in_segment)
-		return MASK(MIN(end_bank, last_in_segment) - first_in_segment,
-			    start_bank - first_in_segment);
-
-	return MASK(end_bank - first_in_segment, 0);
-}
-
-/**
- * \brief Sets register mask for given segment.
- * \param[in] mask Register mask to be set.
- * \param[in] segment Segment id.
- * \param[in] enabled True if banks should be enabled, false otherwise.
- */
-static void cavs_pm_memory_hp_sram_mask_set(uint32_t mask, int segment,
-					    bool enabled)
-{
-	uint32_t expected = enabled ? 0 : mask;
-
-	io_reg_update_bits(SHIM_HSPGCTL(segment), mask, enabled ? 0 : mask);
-	io_reg_update_bits(SHIM_HSRMCTL(segment), mask, enabled ? 0 : mask);
-
-	idelay(MEMORY_POWER_DOWN_DELAY);
-
-	while ((io_reg_read(SHIM_HSPGISTS(segment)) & mask) != expected)
-		idelay(MEMORY_POWER_DOWN_DELAY);
-}
-
-void cavs_pm_memory_hp_sram_banks_power_gate(uint32_t start_bank_id,
-					     uint32_t ending_bank_id,
-					     bool enabled)
-{
-	uint32_t mask;
-	int i;
-
-	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_HPSRAM_LDO_ON);
-
-	idelay(MEMORY_POWER_DOWN_DELAY);
-
-	for (i = 0; i < PLATFORM_HPSRAM_SEGMENTS; ++i) {
-		mask = cavs_pm_memory_hp_sram_mask_get(start_bank_id,
-						       ending_bank_id, i);
-		cavs_pm_memory_hp_sram_mask_set(mask, i, enabled);
-	}
-
-	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_HPSRAM_LDO_BYPASS);
-}
+#if CAVS_VERSION >= CAVS_VERSION_1_8
 
 void cavs_pm_memory_hp_sram_power_gate(void *ptr, uint32_t size, bool enabled)
 {
@@ -142,28 +69,9 @@ void cavs_pm_memory_hp_sram_power_gate(void *ptr, uint32_t size, bool enabled)
 	cavs_pm_memory_hp_sram_banks_power_gate(start_bank, end_bank, enabled);
 }
 
+#endif /* CAVS_VERSION >= CAVS_VERSION_1_8 */
+
 #if CONFIG_LP_SRAM
-
-void cavs_pm_memory_lp_sram_banks_power_gate(uint32_t start_bank_id,
-					     uint32_t ending_bank_id,
-					     bool enabled)
-{
-	uint32_t mask = MASK(ending_bank_id, start_bank_id);
-	uint32_t expected = enabled ? 0 : mask;
-
-	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_LPSRAM_LDO_ON);
-
-	idelay(MEMORY_POWER_DOWN_DELAY);
-
-	io_reg_update_bits(LSPGCTL, mask, enabled ? 0 : mask);
-
-	idelay(MEMORY_POWER_DOWN_DELAY);
-
-	while ((io_reg_read(LSPGISTS) & mask) != expected)
-		idelay(MEMORY_POWER_DOWN_DELAY);
-
-	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_HPSRAM_LDO_BYPASS);
-}
 
 void cavs_pm_memory_lp_sram_power_gate(void *ptr, uint32_t size, bool enabled)
 {
@@ -177,5 +85,3 @@ void cavs_pm_memory_lp_sram_power_gate(void *ptr, uint32_t size, bool enabled)
 }
 
 #endif /* CONFIG_LP_SRAM */
-
-#endif /* CAVS_VERSION >= CAVS_VERSION_1_8 */
