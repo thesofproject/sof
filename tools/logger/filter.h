b1fc922079dea6a3f9a67a8b40ea0ff1953733b7
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2020-05-29 14:41:30 +0200

    logger: Parse runtime filter levels
    Parse arguments given in -F flag, in form where each -F argument
    is given as separate line. Format of single -F entry is
    `<log_level>=<component_list>`. <log_level> must corresponds with
    .name field from log_level_dict. List of component are build from
    elements separated with `,` and single element may have one of the
    following forms:
    "<name> <pipe_id>.<comp_id>" - single component instance
    "<name> <pipe_id>.*" - components with given name on piepeline with pipe_id
    "<name>*" - each component instance with given name or global component
    "<name>" - as above
    "* <pipe_id>.<comp_id>" - component on selected pipe_id with given comp_id
    "* <pipe_id>.*" - each component on pipeline with pipe_id
    "*" - each component in firmware
    <name> must correspond values of uuid entries from ldc file.
    
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

---
+++ tools/logger/filter.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2020 Intel Corporation. All rights reserved.
+ *
+ * Author: Karol Trzcinski	<karolx.trzcinski@linux.intel.com>
+ */
+
+#ifndef __LOGGER_FILTER_H__
+#define __LOGGER_FILTER_H__
+
+int filter_update_firmware(const struct snd_sof_uids_header *uids_dict,
+			   char *input_str);
+
+#endif /* __LOGGER_FILTER_H__ */
