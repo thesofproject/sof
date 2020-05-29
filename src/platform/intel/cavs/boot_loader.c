4cd2af27bbb2fd989d065b3070b4b8094cc4bbd2
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2020-05-29 10:38:05 +0200

    boot_loader: use existing memory power gating functions
    Removes from bootloader duplicated memory power gating functions
    and uses existing implementations from pm_memory.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/platform/intel/cavs/boot_loader.c
+++ src/platform/intel/cavs/boot_loader.c
@@ -4,6 +4,7 @@
 //
 // Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
 
+#include <cavs/lib/pm_memory.h>
 #include <cavs/version.h>
 #include <sof/bit.h>
 #include <sof/lib/cache.h>
@@ -16,6 +17,7 @@
 #include <sof/trace/trace.h>
 #include <rimage/sof/user/manifest.h>
 #include <config.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -168,98 +170,37 @@ static void parse_manifest(void)
 #endif
 
 #if CAVS_VERSION >= CAVS_VERSION_1_8
-/* function powers up a number of memory banks provided as an argument and
- * gates remaining memory banks
- */
-static int32_t hp_sram_pm_banks(uint32_t banks)
+
+static uint32_t hp_sram_power_memory(uint32_t memory_size, bool enable)
 {
-	int delay_count = 256;
-	uint32_t status;
-	uint32_t ebb_mask0, ebb_mask1, ebb_avail_mask0, ebb_avail_mask1;
-	uint32_t total_banks_count = PLATFORM_HPSRAM_EBB_COUNT;
-
-	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_HPSRAM_LDO_ON);
-
-	/* add some delay before touch power register */
-	idelay(delay_count);
-
-	/* bit masks reflect total number of available EBB (banks) in each
-	 * segment; current implementation supports 2 segments 0,1
-	 */
-	if (total_banks_count > EBB_SEGMENT_SIZE) {
-		ebb_avail_mask0 = (uint32_t)MASK(EBB_SEGMENT_SIZE - 1, 0);
-		ebb_avail_mask1 = (uint32_t)MASK(total_banks_count -
-		EBB_SEGMENT_SIZE - 1, 0);
-	} else {
-		ebb_avail_mask0 = (uint32_t)MASK(total_banks_count - 1,
-		0);
-		ebb_avail_mask1 = 0;
-	}
-
-	/* bit masks of banks that have to be powered up in each segment */
-	if (banks > EBB_SEGMENT_SIZE) {
-		ebb_mask0 = (uint32_t)MASK(EBB_SEGMENT_SIZE - 1, 0);
-		ebb_mask1 = (uint32_t)MASK(banks - EBB_SEGMENT_SIZE - 1,
-		0);
-	} else {
-		/* assumption that ebb_in_use is > 0 */
-		ebb_mask0 = (uint32_t)MASK(banks - 1, 0);
-		ebb_mask1 = 0;
-	}
-
-	/* HSPGCTL, HSRMCTL use reverse logic - 0 means EBB is power gated */
-	io_reg_write(HSPGCTL0, (~ebb_mask0) & ebb_avail_mask0);
-	io_reg_write(HSRMCTL0, (~ebb_mask0) & ebb_avail_mask0);
-	io_reg_write(HSPGCTL1, (~ebb_mask1) & ebb_avail_mask1);
-	io_reg_write(HSRMCTL1, (~ebb_mask1) & ebb_avail_mask1);
-
-	/* query the power status of first part of HP memory */
-	/* to check whether it has been powered up. A few    */
-	/* cycles are needed for it to be powered up         */
-	status = io_reg_read(HSPGISTS0);
-	while (status != ((~ebb_mask0) & ebb_avail_mask0)) {
-		idelay(delay_count);
-		status = io_reg_read(HSPGISTS0);
-	}
-	/* query the power status of second part of HP memory */
-	/* and do as above code                               */
-
-	status = io_reg_read(HSPGISTS1);
-	while (status != ((~ebb_mask1) & ebb_avail_mask1)) {
-		idelay(delay_count);
-		status = io_reg_read(HSPGISTS1);
-	}
-	/* add some delay before touch power register */
-	idelay(delay_count);
-
-	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_HPSRAM_LDO_BYPASS);
-
-	return 0;
-}
-
-static uint32_t hp_sram_power_on_memory(uint32_t memory_size)
-{
+	uint32_t start_bank;
+	uint32_t end_bank;
 	uint32_t ebb_in_use;
 
 	/* calculate total number of used SRAM banks (EBB)
 	 * to power up only necessary banks
 	 */
 	ebb_in_use = (!(memory_size % SRAM_BANK_SIZE)) ?
-	(memory_size / SRAM_BANK_SIZE) :
-	(memory_size / SRAM_BANK_SIZE) + 1;
+		(memory_size / SRAM_BANK_SIZE) :
+		(memory_size / SRAM_BANK_SIZE) + 1;
 
-	return hp_sram_pm_banks(ebb_in_use);
+	start_bank = enable ? 0 : ebb_in_use;
+	end_bank = (enable ? ebb_in_use : PLATFORM_HPSRAM_EBB_COUNT) - 1;
+
+	cavs_pm_memory_hp_sram_banks_power_gate(start_bank, end_bank, enable);
+
+	return 0;
 }
 
 static int32_t hp_sram_power_off_unused_banks(uint32_t memory_size)
 {
 	/* keep enabled only memory banks used by FW */
-	return hp_sram_power_on_memory(memory_size);
+	return hp_sram_power_memory(memory_size, false);
 }
 
 static int32_t hp_sram_init(void)
 {
-	return hp_sram_power_on_memory(HP_SRAM_SIZE);
+	return hp_sram_power_memory(HP_SRAM_SIZE, true);
 }
 
 #else
@@ -274,45 +215,6 @@ static uint32_t hp_sram_init(void)
 	return 0;
 }
 
-#endif
-
-#if CONFIG_LP_SRAM
-static int32_t lp_sram_init(void)
-{
-	uint32_t status;
-	uint32_t lspgctl_value;
-	uint32_t timeout_counter, delay_count = 256;
-
-	timeout_counter = delay_count;
-
-	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_LPSRAM_LDO_ON);
-
-	/* add some delay before writing power registers */
-	idelay(delay_count);
-
-	lspgctl_value = io_reg_read(LSPGISTS);
-	io_reg_write(LSPGCTL, lspgctl_value & ~LPSRAM_MASK(0));
-
-	/* add some delay before checking the status */
-	idelay(delay_count);
-
-	/* query the power status of first part of LP memory */
-	/* to check whether it has been powered up. A few    */
-	/* cycles are needed for it to be powered up         */
-	status = io_reg_read(LSPGISTS);
-	while (status) {
-		if (!timeout_counter--) {
-			platform_panic(SOF_IPC_PANIC_MEM);
-			break;
-		}
-		idelay(delay_count);
-		status = io_reg_read(LSPGISTS);
-	}
-
-	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_LPSRAM_LDO_BYPASS);
-
-	return status;
-}
 #endif
 
 /* boot master core */
@@ -334,11 +236,9 @@ void boot_master_core(void)
 	/* init the LPSRAM */
 	trace_point(TRACE_BOOT_LDR_LPSRAM);
 
-	result = lp_sram_init();
-	if (result < 0) {
-		platform_panic(SOF_IPC_PANIC_MEM);
-		return;
-	}
+	cavs_pm_memory_lp_sram_banks_power_gate(0,
+						PLATFORM_LPSRAM_EBB_COUNT - 1,
+						true);
 #endif
 
 #if CONFIG_BOOT_LOADER
--- src/platform/intel/cavs/include/cavs/lib/pm_memory.h
+++ src/platform/intel/cavs/include/cavs/lib/pm_memory.h
@@ -14,19 +14,87 @@
 #ifndef __CAVS_LIB_PM_MEMORY_H__
 #define __CAVS_LIB_PM_MEMORY_H__
 
+#include <cavs/version.h>
+#include <sof/bit.h>
+#include <sof/lib/io.h>
+#include <sof/lib/memory.h>
+#include <sof/lib/shim.h>
+#include <sof/lib/wait.h>
+#include <sof/math/numbers.h>
 #include <config.h>
 #include <stdbool.h>
 #include <stdint.h>
 
+#define MEMORY_POWER_DOWN_DELAY 256
+
+#if CAVS_VERSION >= CAVS_VERSION_1_8
+
 /**
- * \brief Sets HP SRAM enabled gating hw bit mask for memory banks.
- * \param[in] start_bank_id Id of first bank to be managed (inclusive) 0 based.
- * \param[in] ending_bank_id Id of last bank to be managed (inclusive) 0 based.
- * \param[in] enabled Deciding banks desired state (true powered false gated).
+ * \brief Retrieves register mask for given segment.
+ * \param[in] start_bank Start bank id.
+ * \param[in] end_bank End bank id.
+ * \param[in] segment Segment id.
+ * \return Register mask.
  */
-void cavs_pm_memory_hp_sram_banks_power_gate(uint32_t start_bank_id,
-					     uint32_t ending_bank_id,
-					     bool enabled);
+static inline uint32_t cavs_pm_memory_hp_sram_mask_get(uint32_t start_bank,
+						       uint32_t end_bank,
+						       int segment)
+{
+	uint32_t first_in_segment;
+	uint32_t last_in_segment;
+
+	first_in_segment = segment * EBB_SEGMENT_SIZE;
+	last_in_segment = ((segment + 1) * EBB_SEGMENT_SIZE) - 1;
+
+	/* not in this segment */
+	if (start_bank > last_in_segment || end_bank < first_in_segment)
+		return 0;
+
+	if (start_bank >= first_in_segment)
+		return MASK(MIN(end_bank, last_in_segment) - first_in_segment,
+			    start_bank - first_in_segment);
+
+	return MASK(end_bank - first_in_segment, 0);
+}
+
+/**
+ * \brief Sets register mask for given segment.
+ * \param[in] mask Register mask to be set.
+ * \param[in] segment Segment id.
+ * \param[in] enabled True if banks should be enabled, false otherwise.
+ */
+static inline void cavs_pm_memory_hp_sram_mask_set(uint32_t mask, int segment,
+						   bool enabled)
+{
+	uint32_t expected = enabled ? 0 : mask;
+
+	io_reg_update_bits(SHIM_HSPGCTL(segment), mask, enabled ? 0 : mask);
+	io_reg_update_bits(SHIM_HSRMCTL(segment), mask, enabled ? 0 : mask);
+
+	idelay(MEMORY_POWER_DOWN_DELAY);
+
+	while ((io_reg_read(SHIM_HSPGISTS(segment)) & mask) != expected)
+		idelay(MEMORY_POWER_DOWN_DELAY);
+}
+
+static inline void cavs_pm_memory_hp_sram_banks_power_gate(
+	uint32_t start_bank_id, uint32_t ending_bank_id, bool enabled)
+{
+	uint32_t mask;
+	int i;
+
+	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_HPSRAM_LDO_ON);
+
+	idelay(MEMORY_POWER_DOWN_DELAY);
+
+	for (i = 0; i < PLATFORM_HPSRAM_SEGMENTS; ++i) {
+		mask = cavs_pm_memory_hp_sram_mask_get(start_bank_id,
+						       ending_bank_id, i);
+		cavs_pm_memory_hp_sram_mask_set(mask, i, enabled);
+	}
+
+	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_HPSRAM_LDO_BYPASS);
+}
 
 /**
  * \brief Sets HP SRAM power gating.
@@ -40,6 +108,8 @@ void cavs_pm_memory_hp_sram_banks_power_gate(uint32_t start_bank_id,
  */
 void cavs_pm_memory_hp_sram_power_gate(void *ptr, uint32_t size, bool enabled);
 
+#endif /* CAVS_VERSION >= CAVS_VERSION_1_8 */
+
 #if CONFIG_LP_SRAM
 
 /**
@@ -48,9 +118,25 @@ void cavs_pm_memory_hp_sram_power_gate(void *ptr, uint32_t size, bool enabled);
  * \param[in] ending_bank_id Id of last bank to be managed (inclusive) 0 based.
  * \param[in] enabled Deciding banks desired state (true powered false gated).
  */
-void cavs_pm_memory_lp_sram_banks_power_gate(uint32_t start_bank_id,
-					     uint32_t ending_bank_id,
-					     bool enabled);
+static inline void cavs_pm_memory_lp_sram_banks_power_gate(
+	uint32_t start_bank_id, uint32_t ending_bank_id, bool enabled)
+{
+	uint32_t mask = MASK(ending_bank_id, start_bank_id);
+	uint32_t expected = enabled ? 0 : mask;
+
+	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_LPSRAM_LDO_ON);
+
+	idelay(MEMORY_POWER_DOWN_DELAY);
+
+	io_reg_update_bits(LSPGCTL, mask, enabled ? 0 : mask);
+
+	idelay(MEMORY_POWER_DOWN_DELAY);
+
+	while ((io_reg_read(LSPGISTS) & mask) != expected)
+		idelay(MEMORY_POWER_DOWN_DELAY);
+
+	shim_write(SHIM_LDOCTL, SHIM_LDOCTL_HPSRAM_LDO_BYPASS);
+}
 
 /**
  * \brief Sets LP SRAM power gating.
