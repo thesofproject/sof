af540c257f3879f3a2f2bc0d3e3aff32eed14994
Tomasz Lauda <tomasz.lauda@linux.intel.com>
2020-05-29 11:54:43 +0200

    pm_memory: simplify HP SRAM power related functions
    Refactors and simplifies functions related to HP SRAM
    power gating operations.
    
    Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

--- src/platform/intel/cavs/include/cavs/lib/pm_memory.h
+++ src/platform/intel/cavs/include/cavs/lib/pm_memory.h
@@ -18,20 +18,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-/** \brief Memory banks pm masks data. */
-struct ebb_data {
-	uint32_t current_mask0;
-	uint32_t current_mask1;
-	uint32_t new_mask0;
-	uint32_t new_mask1;
-	uint32_t change_mask0;
-	uint32_t change_mask1;
-	uint32_t start_bank_id;
-	uint32_t ending_bank_id;
-	uint32_t start_bank_id_high;
-	uint32_t ending_bank_id_high;
-};
-
 /**
  * \brief Sets HP SRAM enabled gating hw bit mask for memory banks.
  * \param[in] start_bank_id Id of first bank to be managed (inclusive) 0 based.
