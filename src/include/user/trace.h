0de401f0745e0f4f636f4396f652c160c32e9c06
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2020-06-05 08:35:17 +0200

    trace: Remove unused structures
    Unused parts of code only makes maintanace harder.
    
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

--- src/include/user/trace.h
+++ src/include/user/trace.h
@@ -11,21 +11,6 @@
 
 #include <stdint.h>
 
-/*
- * Host system time.
- *
- * This property is used by the driver to pass down information about
- * current system time. It is expressed in us.
- * FW translates timestamps (in log entries, probe pockets) to this time
- * domain.
- *
- * (cavs: SystemTime).
- */
-struct system_time {
-	uint32_t val_l;  /* Lower dword of current host time value */
-	uint32_t val_u;  /* Upper dword of current host time value */
-} __attribute__((packed));
-
 /* trace event classes - high 8 bits*/
 #define TRACE_CLASS_DEPRECATED	(0)
 
@@ -38,22 +23,6 @@ struct system_time {
 #define LOG_LEVEL_INFO		3
 #define LOG_LEVEL_DEBUG		4
 #define LOG_LEVEL_VERBOSE	LOG_LEVEL_DEBUG
-
-/*
- * Layout of a log fifo.
- */
-struct log_buffer_layout {
-	uint32_t read_ptr;  /*read pointer */
-	uint32_t write_ptr; /* write pointer */
-	uint32_t buffer[0]; /* buffer */
-} __attribute__((packed));
-
-/*
- * Log buffer status reported by FW.
- */
-struct log_buffer_status {
-	uint32_t core_id;  /* ID of core that logged to other half */
-} __attribute__((packed));
 
 #define TRACE_ID_LENGTH 12
 
