8a90be118c08d9ee82b491e64137aa0d5c04cdde
Shreeya Patel <shreeya.patel23498@gmail.com>
2020-06-18 11:44:48 +0530

    scripts: build-tools.sh: Make tools individually buildable
    More control over which tools should be built, instead of building
    everything every time.
    
    Note: All the build files will be removed every time this script is ran
    since tools are not independent of each other.
    
    Signed-off-by: Shreeya Patel <shreeya.patel23498@gmail.com>

--- scripts/build-tools.sh
+++ scripts/build-tools.sh
@@ -5,69 +5,126 @@
 # fail immediately on any errors
 set -e
 
-
 print_usage()
 {
         cat <<EOFUSAGE
-usage: $0 [-t|-f]
-       [-t] Build test topologies
-       [-f] Build fuzzer"
+usage: $0 [-c|-f|-h|-l|-p|-t|-T]
+       -c Rebuild ctl
+       -f Rebuild fuzzer
+       -h Display help
+       -l Rebuild logger
+       -p Rebuild probes
+       -t Rebuild test topologies
+       -T Rebuild topologies
+       -C No build, only CMake re-configuration
 EOFUSAGE
 }
 
-build_tools()
+# generate Makefiles
+reconfigure_build()
 {
-        cd "$SOF_REPO/tools"
-        rm -rf build_tools
-        mkdir build_tools
-        cd build_tools
+        rm -rf "$BUILD_TOOLS_DIR"
+        mkdir "$BUILD_TOOLS_DIR"
+
+        cd "$BUILD_TOOLS_DIR"
         cmake ..
-        make -j "$NO_PROCESSORS"
-}
 
-build_test()
-{
-        cd "$SOF_REPO/tools/build_tools"
-        make tests -j "$NO_PROCESSORS"
-}
-
-build_fuzzer()
-{
-        cd "$SOF_REPO/tools/build_tools"
-        mkdir build_fuzzer
-        cd build_fuzzer
+        mkdir "$BUILD_TOOLS_DIR/fuzzer"
+        cd "$BUILD_TOOLS_DIR/fuzzer"
         cmake ../../fuzzer
-        make -j "$NO_PROCESSORS"
+}
+
+make_tool()
+{
+        # if no argument provided, all the tools will be built. Empty tool is
+        # okay.
+        tool=$1
+        make -C "$BUILD_TOOLS_DIR" -j "$NO_PROCESSORS" $tool
+}
+
+make_fuzzer()
+{
+        make -C "$BUILD_TOOLS_DIR/fuzzer" -j "$NO_PROCESSORS"
+}
+
+print_build_info()
+{
+       cat <<EOFUSAGE
+
+Build commands for respective tools:
+        ctl:        make -C "$BUILD_TOOLS_DIR" sof-ctl
+        logger:     make -C "$BUILD_TOOLS_DIR" sof-logger
+        probes:     make -C "$BUILD_TOOLS_DIR" sof-probes
+        tests:      make -C "$BUILD_TOOLS_DIR" tests
+        topologies: make -C "$BUILD_TOOLS_DIR" topologies
+        fuzzer:     make -C "$BUILD_TOOLS_DIR/fuzzer"
+EOFUSAGE
 }
 
 main()
 {
-        local DO_BUILD_TEST DO_BUILD_FUZZER SCRIPT_DIR SOF_REPO
+        local DO_BUILD_ctl DO_BUILD_fuzzer DO_BUILD_logger DO_BUILD_probes \
+                DO_BUILD_test DO_BUILD_topology SCRIPT_DIR SOF_REPO CMAKE_ONLY \
+                BUILD_ALL
         SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
         SOF_REPO=$(dirname "$SCRIPT_DIR")
+        BUILD_TOOLS_DIR="$SOF_REPO"/tools/build_tools
         : ${NO_PROCESSORS:=$(nproc)}
+        BUILD_ALL=false
 
-        DO_BUILD_TEST=false
-        DO_BUILD_FUZZER=false
-        while getopts "tf" OPTION; do
+        if [ $# -eq 0 ]; then
+                BUILD_ALL=true
+        fi
+
+        DO_BUILD_ctl=false
+        DO_BUILD_fuzzer=false
+        DO_BUILD_logger=false
+        DO_BUILD_probes=false
+        DO_BUILD_tests=false
+        DO_BUILD_topologies=false
+        CMAKE_ONLY=false
+
+        while getopts "cfhlptTC" OPTION; do
                 case "$OPTION" in
-                t) DO_BUILD_TEST=true ;;
-                f) DO_BUILD_FUZZER=true ;;
+                c) DO_BUILD_ctl=true ;;
+                f) DO_BUILD_fuzzer=true ;;
+                l) DO_BUILD_logger=true ;;
+                p) DO_BUILD_probes=true ;;
+                t) DO_BUILD_tests=true ;;
+                T) DO_BUILD_topologies=true ;;
+                C) CMAKE_ONLY=true ;;
+                h) print_usage; exit 1;;
                 *) print_usage; exit 1;;
                 esac
         done
         shift "$(($OPTIND - 1))"
+        reconfigure_build
 
-        build_tools
-
-        if "$DO_BUILD_TEST"
-        then
-                build_test
+        if "$CMAKE_ONLY"; then
+                print_build_info
+                exit
         fi
 
-        if "$DO_BUILD_FUZZER"
-        then
-                build_fuzzer
+        if "$BUILD_ALL"; then
+                make_tool
+                make_fuzzer
+                exit
+        fi
+
+        for tool in ctl logger probes; do
+                if eval '$DO_BUILD_'$tool; then
+                        make_tool sof-$tool
+                fi
+        done
+
+        for util in tests topologies; do
+                if eval '$DO_BUILD_'$util; then
+                        make_tool $util
+                fi
+        done
+
+        if "$DO_BUILD_fuzzer"; then
+                make_fuzzer
         fi
 }
 
