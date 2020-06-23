19cb5ee12d50cc42408e54298f7cbe33481648e3
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2020-06-23 16:43:56 +0100

    zephyr: WIP: wrapper updates that need split into upstream patches
    WIP - one working needs reset and split per feature into patches for
    upstreaming.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

---
+++ zephyr/include/sof/trace/trace.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright(c) 2020 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __SOF_TRACE_TRACE1_H__
+#define __SOF_TRACE_TRACE1_H__
+
+#ifndef ZEPHYR_INCLUDE_LOGGING_LOG_H_
+#include <logging/log.h>
+
+/* Level of SOF trace on Zephyr */
+#define SOF_ZEPHYR_TRACE_LEVEL LOG_LEVEL_INF
+
+LOG_MODULE_DECLARE(sof, SOF_ZEPHYR_TRACE_LEVEL);
+#endif
+
+/* printk supports uint64_t so use it until LOG is ready */
+#define USE_PRINTK	1
+
+/* SOF trace header */
+#include "../../../../src/include/sof/trace/trace.h"
+
+struct timer;
+uint64_t platform_timer_get(struct timer *);
+
+/*
+ * Use SOF macros, but let Zephyr take care of the physical log IO.
+ */
+#undef _log_message
+
+#if USE_PRINTK
+#define _log_message(atomic, level, comp_class, ctx, id1, id2, format, ...)				\
+	do {								        \
+		if (level >= SOF_ZEPHYR_TRACE_LEVEL)                            \
+			printk("%llu: " format "\n", platform_timer_get(NULL),	\
+				##__VA_ARGS__);					\
+	} while (0)
+#else
+#define _log_message(atomic, level, comp_class, ctx, id1, id2, format, ...)				\
+	do {								        \
+		Z_LOG(level, "%u: " format, (uint32_t)platform_timer_get(NULL),	\
+		      ##__VA_ARGS__);					        \
+	} while (0)
+#endif
+
+#endif /* __INCLUDE_TRACE__ */
