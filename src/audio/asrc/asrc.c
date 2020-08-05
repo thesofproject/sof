thesofproject/sof

commit f63adb42d93521b0bb25c670fd457122857badef

Audio: ASRC: Get consumed and produced frame counts from filter core
This patch changes the parameter input_num_frames to in/out type
for push mode 16 and 32 bit processing functions. If the processing
core needs to stop due to insufficient number of frames free in the
output buffer the amount of frames can be less than requested. The if
statement for exit from while() loop is simplified. Also the if
statement tests order for ASRC_BM_CIRCULAR is changed.

The changes for pull mode 16 and 32 bit mode are similar. The parameter
out_num_frames is changed to in/out since the frames count could be less
than requested if there is no sufficient amount of input frames
available.

The upper level 16 and 32 bit copy() functions are updated to use the
returned value for SOF buffers consumed/produced.

Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

src/audio/asrc/asrc.c
@@ -112,7 +112,8 @@ static void src_copy_s32(struct comp_dev *dev,
 	int n;
 	int ret;
 	int i;
-	int frames = 0;
+	int in_frames = 0;
+	int out_frames = 0;
 	int idx = 0;
 
 	/* TODO: Optimize buffer size by circular write to snk directly */
@@ -133,26 +134,24 @@ static void src_copy_s32(struct comp_dev *dev,
 	}
 
 	/* Run ASRC */
-	if (cd->mode == ASRC_OM_PUSH) {
-		ret = asrc_process_push32(dev,
-					  cd->asrc_obj, (int32_t **)cd->ibuf,
-					  cd->source_frames,
-					  (int32_t **)cd->obuf, &frames,
+	in_frames = cd->source_frames;
+	out_frames = cd->sink_frames;
+	if (cd->mode == ASRC_OM_PUSH)
+		ret = asrc_process_push32(dev, cd->asrc_obj,
+					  (int32_t **)cd->ibuf, &in_frames,
+					  (int32_t **)cd->obuf, &out_frames,
 					  &idx, 0);
-		n = frames * sink->channels;
-	} else {
+	else
 		ret = asrc_process_pull32(dev, cd->asrc_obj,
-					  (int32_t **)cd->ibuf,
-					  &frames, (int32_t **)cd->obuf,
-					  cd->sink_frames, cd->source_frames,
-					  &idx);
-		n = cd->sink_frames * sink->channels;
-	}
+					  (int32_t **)cd->ibuf, &in_frames,
+					  (int32_t **)cd->obuf, &out_frames,
+					  out_frames, &idx);
 
 	if (ret)
 		comp_err(dev, "src_copy_s32(), error %d", ret);
 
 	buf = (int32_t *)cd->obuf[0];
+	n = out_frames * sink->channels;
 	while (n > 0) {
 		n_wrap_snk = (int32_t *)sink->end_addr - snk;
 		n_copy = (n < n_wrap_snk) ? n : n_wrap_snk;
@@ -164,13 +163,8 @@ static void src_copy_s32(struct comp_dev *dev,
 		src_inc_wrap(&snk, sink->end_addr, sink->size);
 	}
 
-	if (cd->mode == ASRC_OM_PUSH) {
-		*n_read = cd->source_frames;
-		*n_written = frames;
-	} else {
-		*n_read = frames;
-		*n_written = cd->sink_frames;
-	}
+	*n_read = in_frames;
+	*n_written = out_frames;
 }
 
 static void src_copy_s16(struct comp_dev *dev,
@@ -188,7 +182,8 @@ static void src_copy_s16(struct comp_dev *dev,
 	int s_copy;
 	int ret;
 	int n;
-	int frames = 0;
+	int in_frames = 0;
+	int out_frames = 0;
 	int idx = 0;
 
 	/* TODO: Optimize buffer size by circular write to snk directly */
@@ -211,26 +206,25 @@ static void src_copy_s16(struct comp_dev *dev,
 	}
 
 	/* Run ASRC */
-	if (cd->mode == ASRC_OM_PUSH) {
-		ret = asrc_process_push16(dev,
-					  cd->asrc_obj, (int16_t **)cd->ibuf,
-					  cd->source_frames,
-					  (int16_t **)cd->obuf, &frames,
+	in_frames = cd->source_frames;
+	out_frames = cd->sink_frames;
+
+	if (cd->mode == ASRC_OM_PUSH)
+		ret = asrc_process_push16(dev, cd->asrc_obj,
+					  (int16_t **)cd->ibuf, &in_frames,
+					  (int16_t **)cd->obuf, &out_frames,
 					  &idx, 0);
-		n = frames * sink->channels;
-	} else {
-		ret = asrc_process_pull16(dev,
-					  cd->asrc_obj, (int16_t **)cd->ibuf,
-					  &frames, (int16_t **)cd->obuf,
-					  cd->sink_frames, cd->source_frames,
-					  &idx);
-		n = cd->sink_frames * sink->channels;
-	}
+	else
+		ret = asrc_process_pull16(dev, cd->asrc_obj,
+					  (int16_t **)cd->ibuf, &in_frames,
+					  (int16_t **)cd->obuf, &out_frames,
+					  out_frames, &idx);
 
 	if (ret)
 		comp_err(dev, "src_copy_s16(), error %d", ret);
 
 	buf = (int16_t *)cd->obuf[0];
+	n = out_frames * sink->channels;
 	while (n > 0) {
 		n_wrap_snk = (int16_t *)sink->end_addr - snk;
 		n_copy = (n < n_wrap_snk) ? n : n_wrap_snk;
@@ -245,13 +239,8 @@ static void src_copy_s16(struct comp_dev *dev,
 		src_inc_wrap_s16(&snk, sink->end_addr, sink->size);
 	}
 
-	if (cd->mode == ASRC_OM_PUSH) {
-		*n_read = cd->source_frames;
-		*n_written = frames;
-	} else {
-		*n_read = frames;
-		*n_written = cd->sink_frames;
-	}
+	*n_read = in_frames;
+	*n_written = out_frames;
 }
 
 static struct comp_dev *asrc_new(const struct comp_driver *drv,

src/audio/asrc/asrc_farrow.c
@@ -975,7 +975,7 @@ void asrc_write_to_ring_buffer32(struct asrc_farrow  *src_obj,
 enum asrc_error_code asrc_process_push16(struct comp_dev *dev,
 					 struct asrc_farrow *src_obj,
 					 int16_t **__restrict input_buffers,
-					 int input_num_frames,
+					 int *input_num_frames,
 					 int16_t **__restrict output_buffers,
 					 int *output_num_frames,
 					 int *write_index,
@@ -1013,34 +1013,30 @@ enum asrc_error_code asrc_process_push16(struct comp_dev *dev,
 	index_input_frame = 0;
 
 	/* Run the state machine until all input samples are read */
-	while (index_input_frame < input_num_frames) {
+	while (index_input_frame < *input_num_frames) {
 		if (src_obj->time_value < TIME_VALUE_ONE) {
-			if (*output_num_frames < max_num_free_frames) {
-				/* Calculate impulse response */
-				(*src_obj->calc_ir)(src_obj);
-
-				/* Filter and write one output sample for each
-				 * channel to the output_buffer
-				 */
-				asrc_fir_filter16(src_obj, output_buffers,
-						  src_obj->io_buffer_idx);
-
-				/* Update time and buffer index */
-				src_obj->time_value += src_obj->fs_ratio;
-				src_obj->io_buffer_idx++;
-				if (src_obj->io_buffer_idx >=
-				    src_obj->io_buffer_length &&
-				    src_obj->io_buffer_mode == ASRC_BM_CIRCULAR)
-					/* Wrap around */
-					src_obj->io_buffer_idx = 0;
-
-				(*output_num_frames)++;
-			} else {
-				comp_err(dev, "error onf=%d, max=%d",
-					 *output_num_frames,
-					 max_num_free_frames);
+			if (*output_num_frames == max_num_free_frames)
 				break;
-			}
+
+			/* Calculate impulse response */
+			(*src_obj->calc_ir)(src_obj);
+
+			/* Filter and write one output sample for each
+			 * channel to the output_buffer
+			 */
+			asrc_fir_filter16(src_obj, output_buffers,
+					  src_obj->io_buffer_idx);
+
+			/* Update time and buffer index */
+			src_obj->time_value += src_obj->fs_ratio;
+			src_obj->io_buffer_idx++;
+
+			/* Wrap around */
+			if (src_obj->io_buffer_mode == ASRC_BM_CIRCULAR &&
+			    src_obj->io_buffer_idx >= src_obj->io_buffer_length)
+				src_obj->io_buffer_idx = 0;
+
+			(*output_num_frames)++;
 		} else {
 			/* Consume one input sample */
 			asrc_write_to_ring_buffer16(src_obj, input_buffers,
@@ -1052,12 +1048,13 @@ enum asrc_error_code asrc_process_push16(struct comp_dev *dev,
 		}
 	}
 	*write_index = src_obj->io_buffer_idx;
+	*input_num_frames = index_input_frame;
 
 	/* if fixed control mode, update the frames counters and check
 	 * if we have reached end of control cycle.
 	 */
 	if (src_obj->control_mode == ASRC_CM_FIXED) {
-		src_obj->prim_num_frames += input_num_frames;
+		src_obj->prim_num_frames += *input_num_frames;
 		src_obj->sec_num_frames += *output_num_frames;
 		if (src_obj->prim_num_frames >= src_obj->prim_num_frames_targ &&
 		    src_obj->sec_num_frames >= src_obj->sec_num_frames_targ) {
@@ -1088,7 +1085,7 @@ enum asrc_error_code asrc_process_push16(struct comp_dev *dev,
 enum asrc_error_code asrc_process_push32(struct comp_dev *dev,
 					 struct asrc_farrow *src_obj,
 					 int32_t **__restrict input_buffers,
-					 int input_num_frames,
+					 int *input_num_frames,
 					 int32_t **__restrict output_buffers,
 					 int *output_num_frames,
 					 int *write_index,
@@ -1124,36 +1121,30 @@ enum asrc_error_code asrc_process_push32(struct comp_dev *dev,
 
 	*output_num_frames = 0;
 	index_input_frame = 0;
-	while (index_input_frame < input_num_frames) {
+	while (index_input_frame < *input_num_frames) {
 		if (src_obj->time_value < TIME_VALUE_ONE) {
-			if (*output_num_frames < max_num_free_frames) {
-				/* Calculate impulse response */
-				(*src_obj->calc_ir)(src_obj);
-
-				/* Filter and write output sample to
-				 * output_buffer
-				 */
-				asrc_fir_filter32(src_obj, output_buffers,
-						  src_obj->io_buffer_idx);
-
-				/* Update time and index */
-				src_obj->time_value += src_obj->fs_ratio;
-				src_obj->io_buffer_idx++;
-
-				/* Wrap around */
-				if (src_obj->io_buffer_mode ==
-				    ASRC_BM_CIRCULAR &&
-				    src_obj->io_buffer_idx >=
-				    src_obj->io_buffer_length)
-					src_obj->io_buffer_idx = 0;
-
-				(*output_num_frames)++;
-			} else {
-				comp_err(dev, "error onf=%d, max=%d",
-					 *output_num_frames,
-					 max_num_free_frames);
+			if (*output_num_frames == max_num_free_frames)
 				break;
-			}
+
+			/* Calculate impulse response */
+			(*src_obj->calc_ir)(src_obj);
+
+			/* Filter and write output sample to
+			 * output_buffer
+			 */
+			asrc_fir_filter32(src_obj, output_buffers,
+					  src_obj->io_buffer_idx);
+
+			/* Update time and index */
+			src_obj->time_value += src_obj->fs_ratio;
+			src_obj->io_buffer_idx++;
+
+			/* Wrap around */
+			if (src_obj->io_buffer_mode == ASRC_BM_CIRCULAR &&
+			    src_obj->io_buffer_idx >= src_obj->io_buffer_length)
+				src_obj->io_buffer_idx = 0;
+
+			(*output_num_frames)++;
 		} else {
 			/* Consume input sample */
 			asrc_write_to_ring_buffer32(src_obj, input_buffers,
@@ -1164,14 +1155,15 @@ enum asrc_error_code asrc_process_push32(struct comp_dev *dev,
 			src_obj->time_value -= TIME_VALUE_ONE;
 		}
 	}
-
 	*write_index = src_obj->io_buffer_idx;
+	*input_num_frames = index_input_frame;
+
 
 	/* if fixed control mode, update the frames counters and check if we
 	 * have reached end of control cycle.
 	 */
 	if (src_obj->control_mode == ASRC_CM_FIXED) {
-		src_obj->prim_num_frames += input_num_frames;
+		src_obj->prim_num_frames += *input_num_frames;
 		src_obj->sec_num_frames += *output_num_frames;
 		if (src_obj->prim_num_frames >= src_obj->prim_num_frames_targ &&
 		    src_obj->sec_num_frames >= src_obj->sec_num_frames_targ) {
@@ -1204,7 +1196,7 @@ enum asrc_error_code asrc_process_pull16(struct comp_dev *dev,
 					 int16_t **__restrict input_buffers,
 					 int *input_num_frames,
 					 int16_t **__restrict output_buffers,
-					 int output_num_frames,
+					 int *output_num_frames,
 					 int write_index,
 					 int *read_index)
 {
@@ -1230,23 +1222,23 @@ enum asrc_error_code asrc_process_pull16(struct comp_dev *dev,
 	*input_num_frames = 0;
 
 	/* Run state machine until number of output samples are written */
-	while (index_output_frame < output_num_frames) {
+	while (index_output_frame < *output_num_frames) {
 		if (src_obj->time_value_pull < TIME_VALUE_ONE) {
 			/* Consume one input sample */
-			if (src_obj->io_buffer_idx != write_index) {
-				asrc_write_to_ring_buffer16(src_obj,
-							    input_buffers,
-							    src_obj->io_buffer_idx);
-				src_obj->io_buffer_idx++;
+			if (src_obj->io_buffer_idx == write_index)
+				break;
 
-				/* Wrap around */
-				if (src_obj->io_buffer_idx >=
-				    src_obj->io_buffer_length &&
-				    src_obj->io_buffer_mode == ASRC_BM_CIRCULAR)
-					src_obj->io_buffer_idx = 0;
+			asrc_write_to_ring_buffer16(src_obj,
+						    input_buffers,
+						    src_obj->io_buffer_idx);
+			src_obj->io_buffer_idx++;
 
-				(*input_num_frames)++;
-			}
+			/* Wrap around */
+			if (src_obj->io_buffer_mode == ASRC_BM_CIRCULAR &&
+			    src_obj->io_buffer_idx >= src_obj->io_buffer_length)
+				src_obj->io_buffer_idx = 0;
+
+			(*input_num_frames)++;
 
 			/* Update time as Q5.27 */
 			src_obj->time_value = (((int64_t)TIME_VALUE_ONE -
@@ -1268,12 +1260,13 @@ enum asrc_error_code asrc_process_pull16(struct comp_dev *dev,
 		}
 	}
 	*read_index = src_obj->io_buffer_idx;
+	*output_num_frames = index_output_frame;
 
 	/* if fixed control mode, update the frames counters and check if we
 	 * have reached end of control cycle.
 	 */
 	if (src_obj->control_mode == ASRC_CM_FIXED) {
-		src_obj->prim_num_frames += output_num_frames;
+		src_obj->prim_num_frames += *output_num_frames;
 		src_obj->sec_num_frames += *input_num_frames;
 		if (src_obj->prim_num_frames >= src_obj->prim_num_frames_targ &&
 		    src_obj->sec_num_frames >= src_obj->sec_num_frames_targ) {
@@ -1306,7 +1299,7 @@ enum asrc_error_code asrc_process_pull32(struct comp_dev *dev,
 					 int32_t **__restrict input_buffers,
 					 int *input_num_frames,
 					 int32_t **__restrict output_buffers,
-					 int output_num_frames,
+					 int *output_num_frames,
 					 int write_index,
 					 int *read_index)
 {
@@ -1333,23 +1326,23 @@ enum asrc_error_code asrc_process_pull32(struct comp_dev *dev,
 		src_obj->io_buffer_idx = 0;
 
 	*input_num_frames = 0;
-	while (index_output_frame < output_num_frames) {
+	while (index_output_frame < *output_num_frames) {
 		if (src_obj->time_value_pull < TIME_VALUE_ONE) {
 			/* Consume input sample */
-			if (src_obj->io_buffer_idx != write_index) {
-				asrc_write_to_ring_buffer32(src_obj,
-							    input_buffers,
-							    src_obj->io_buffer_idx);
-				src_obj->io_buffer_idx++;
+			if (src_obj->io_buffer_idx == write_index)
+				break;
 
-				/* Wrap around */
-				if (src_obj->io_buffer_idx >=
-				    src_obj->io_buffer_length &&
-				    src_obj->io_buffer_mode == ASRC_BM_CIRCULAR)
-					src_obj->io_buffer_idx = 0;
+			asrc_write_to_ring_buffer32(src_obj,
+						    input_buffers,
+						    src_obj->io_buffer_idx);
+			src_obj->io_buffer_idx++;
 
-				(*input_num_frames)++;
-			}
+			/* Wrap around */
+			if (src_obj->io_buffer_mode == ASRC_BM_CIRCULAR &&
+			    src_obj->io_buffer_idx >= src_obj->io_buffer_length)
+				src_obj->io_buffer_idx = 0;
+
+			(*input_num_frames)++;
 
 			/* Update time as Q5.27 */
 			src_obj->time_value = (((int64_t)TIME_VALUE_ONE -
@@ -1371,12 +1364,13 @@ enum asrc_error_code asrc_process_pull32(struct comp_dev *dev,
 		}
 	}
 	*read_index = src_obj->io_buffer_idx;
+	*output_num_frames = index_output_frame;
 
 	/* if fixed control mode, update the frames counters and check
 	 * if we have reached end of control cycle.
 	 */
 	if (src_obj->control_mode == ASRC_CM_FIXED) {
-		src_obj->prim_num_frames += output_num_frames;
+		src_obj->prim_num_frames += *output_num_frames;
 		src_obj->sec_num_frames += *input_num_frames;
 		if (src_obj->prim_num_frames >= src_obj->prim_num_frames_targ &&
 		    src_obj->sec_num_frames >= src_obj->sec_num_frames_targ) {

src/include/sof/audio/asrc/asrc_farrow.h
@@ -310,7 +310,8 @@ enum asrc_error_code asrc_initialise(struct comp_dev *dev,
  *                               interleaved or deinterleaved format,
  *                               this should also be correctly
  *                               configured in the @p src_obj.
- * @param[in] input_num_frames   Number of samples in each channel of input
+ * @param[in, out] input_num_frames
+ *				 Number of samples in each channel of input
  *                               data (NOT the number of total samples).
  * @param[out] output_buffers    Pointer to the pointers to the 16 bit
  *                               buffers the generated samples should
@@ -342,7 +343,7 @@ enum asrc_error_code asrc_initialise(struct comp_dev *dev,
 enum asrc_error_code asrc_process_push16(struct comp_dev *dev,
 					 struct asrc_farrow *src_obj,
 					 int16_t **__restrict input_buffers,
-					 int input_num_frames,
+					 int *input_num_frames,
 					 int16_t **__restrict output_buffers,
 					 int *output_num_frames,
 					 int *write_index,
@@ -368,7 +369,8 @@ enum asrc_error_code asrc_process_push16(struct comp_dev *dev,
  *                                interleaved or deinterleaved format,
  *                                this should also be correctly
  *                                configured in the @p src_obj.
- * @param[in] input_num_frames    Number of samples in each channel of
+ * @param[in, out] input_num_frames
+ *				  Number of samples in each channel of
  *                                input data (NOT the number of total samples).
  * @param[out] output_buffers     Pointer to the pointers to the 32 bit
  *                                buffers the generated samples should
@@ -400,7 +402,7 @@ enum asrc_error_code asrc_process_push16(struct comp_dev *dev,
 enum asrc_error_code asrc_process_push32(struct comp_dev *dev,
 					 struct asrc_farrow *src_obj,
 					 int32_t **__restrict input_buffers,
-					 int input_num_frames,
+					 int *input_num_frames,
 					 int32_t **__restrict output_buffers,
 					 int *output_num_frames,
 					 int *write_index,
@@ -434,7 +436,8 @@ enum asrc_error_code asrc_process_push32(struct comp_dev *dev,
  *                              or deinterleaved format, depending on
  *                              the configuration in @p src_obj.  No
  *                              support for output ring buffers yet.
- * @param[in] output_num_frames Number of samples that shall be written
+ * @param[in, out] output_num_frames
+ *				Number of samples that shall be written
  *                              to the output buffer for each channel.
  * @param[in] write_index       Index within the input ring buffer, must
  *                              point to the next frame, which has not
@@ -467,7 +470,7 @@ enum asrc_error_code asrc_process_pull16(struct comp_dev *dev,
 					 int16_t **__restrict input_buffers,
 					 int *input_num_frames,
 					 int16_t **__restrict output_buffers,
-					 int output_num_frames,
+					 int *output_num_frames,
 					 int write_index,
 					 int *read_index);
 
@@ -498,7 +501,8 @@ enum asrc_error_code asrc_process_pull16(struct comp_dev *dev,
  *                              or deinterleaved format, depending on
  *                              the configuration in @p src_obj.  No
  *                              support for output ring buffers yet.
- * @param[in] output_num_frames Number of samples that shall be written
+ * @param[in, out] output_num_frames
+ *				Number of samples that shall be written
  *                              to the output buffer for each channel.
  * @param[in] write_index       Index within the input ring buffer, must
  *                              point to the next frame, which has not
@@ -531,7 +535,7 @@ enum asrc_error_code asrc_process_pull32(struct comp_dev *dev,
 					 int32_t **__restrict input_buffers,
 					 int *input_num_frames,
 					 int32_t **__restrict output_buffers,
-					 int output_num_frames,
+					 int *output_num_frames,
 					 int write_index,
 					 int *read_index);
 

commit 2b7283325b415d432f910df14ce3b0d2863e4249

Audio: ASRC: Fix pull mode copy frames consume limit
This patch fixes the regression that happened in a previous fix
for stress test gitches issue. Since the passed 7th argument
(write_index) was set to output frames limit the ASRC copy()
terminated too early when converting from higher to lower sample
rate. It has caused issues at least in sof-diagnostic-driver based
validation for capture direction.

Fixes: f63adb42d93521b0bb25c670fd457122857badef
Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

src/audio/asrc/asrc.c
@@ -148,7 +148,7 @@ static void src_copy_s32(struct comp_dev *dev,
 		ret = asrc_process_pull32(dev, cd->asrc_obj,
 					  (int32_t **)cd->ibuf, &in_frames,
 					  (int32_t **)cd->obuf, &out_frames,
-					  out_frames, &idx);
+					  in_frames, &idx);
 
 	if (ret)
 		comp_err(dev, "src_copy_s32(), error %d", ret);
@@ -221,7 +221,7 @@ static void src_copy_s16(struct comp_dev *dev,
 		ret = asrc_process_pull16(dev, cd->asrc_obj,
 					  (int16_t **)cd->ibuf, &in_frames,
 					  (int16_t **)cd->obuf, &out_frames,
-					  out_frames, &idx);
+					  in_frames, &idx);
 
 	if (ret)
 		comp_err(dev, "src_copy_s16(), error %d", ret);