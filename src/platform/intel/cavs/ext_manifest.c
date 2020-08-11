07775daa3183a6ac0889ccbc3090d731505072f7
Adrian Bonislawski <adrian.bonislawski@linux.intel.com>
2020-08-11 19:51:00 +0200

    ext_manifest: add platform-specific config elem
    At initial version it will add CAVS_LPRO for CAVS platforms,
    OUTBOX_SIZE and INBOX_SIZE
    
    Signed-off-by: Adrian Bonislawski <adrian.bonislawski@linux.intel.com>

---
+++ src/platform/intel/cavs/ext_manifest.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2020 Intel Corporation. All rights reserved.
+//
+// Author: Adrian Bonislawski <adrian.bonislawski@linux.intel.com>
+//
+
+#include <cavs/ext_manifest.h>
+#include <kernel/ext_manifest.h>
+#include <sof/common.h>
+#include <sof/lib/memory.h>
+
+const struct ext_man_cavs_config_data ext_man_cavs_config
+	__aligned(EXT_MAN_ALIGN) __section(".fw_metadata") = {
+	.hdr.type = EXT_MAN_ELEM_PLATFORM_CONFIG_DATA,
+	.hdr.elem_size = ALIGN_UP(sizeof(struct ext_man_cavs_config_data),
+				  EXT_MAN_ALIGN),
+	.elems = {
+#if CONFIG_CAVS_LPRO
+		{EXT_MAN_CAVS_CONFIG_LPRO,  CONFIG_CAVS_LPRO},
+#endif
+		{EXT_MAN_CAVS_CONFIG_OUTBOX_SIZE, SRAM_OUTBOX_SIZE},
+		{EXT_MAN_CAVS_CONFIG_INBOX_SIZE, SRAM_INBOX_SIZE},
+	},
+};
