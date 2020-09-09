e04f56f5b3ec1e217d519ad56ef42d7821aaa1e1
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2020-09-09 10:09:06 +0200

    make param applying log an info log for the moment

--- src/audio/codec_adapter/codec/cadence.c
+++ src/audio/codec_adapter/codec/cadence.c
@@ -86,10 +86,7 @@ static int apply_config(struct comp_dev *dev, enum codec_cfg_type type)
 
 	while (size > 0) {
 		param = data;
-
-
-
-		comp_dbg(dev, "apply_config() applying param %d value %d",
+		comp_info(dev, "apply_config() applying param %d value %d",
 			 param->id, param->data[0]);
 		API_CALL(cd, XA_API_CMD_SET_CONFIG_PARAM, param->id,
 			 param->data, ret);
