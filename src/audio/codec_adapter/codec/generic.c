68a0cbcd67fa72de16bd9cdf4949b0befc3eebb5
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2020-09-17 14:35:35 +0200

    post_processing: fix codecs memory leakage
    This patch takes care of memory allocated by codecs. We store
    every memory record and free it when no longer needed.
    
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

--- src/audio/codec_adapter/codec/generic.c
+++ src/audio/codec_adapter/codec/generic.c
@@ -12,11 +12,9 @@
 #include <sof/audio/codec_adapter/codec/generic.h>
 #include <sof/audio/codec_adapter/codec/interfaces.h>
 
-static int validate_config(struct codec_config *cfg)
-{
-	//TODO:
-	return 0;
-}
+
+static void codec_free_all_memory(struct comp_dev *dev);
+static int validate_config(struct codec_config *cfg);
 
 int
 codec_load_config(struct comp_dev *dev, void *cfg, size_t size,
@@ -41,9 +39,12 @@ codec_load_config(struct comp_dev *dev, void *cfg, size_t size,
 	dst = (type == CODEC_CFG_SETUP) ? &codec->s_cfg :
 					  &codec->r_cfg;
 
-	if (!dst->data)
+	if (!dst->data) {
 		dst->data = rballoc(0, SOF_MEM_CAPS_RAM, size);
-
+	} else if (dst->size != size) {
+		rfree(dst->data);
+		dst->data = rballoc(0, SOF_MEM_CAPS_RAM, size);
+	}
 	if (!dst->data) {
 		comp_err(dev, "codec_load_config() error: failed to allocate space for setup config.");
 		ret = -ENOMEM;
@@ -66,7 +67,7 @@ codec_load_config(struct comp_dev *dev, void *cfg, size_t size,
 
 	return ret;
 err:
-	if (dst->data)
+	if (dst->data && type == CODEC_CFG_RUNTIME)
 		rfree(dst->data);
 	dst->data = NULL;
 	return ret;
@@ -138,12 +139,17 @@ int codec_prepare(struct comp_dev *dev)
 	if (ret) {
 		comp_err(dev, "codec_prepare() error %d: codec specific prepare failed, codec_id %x",
 			 ret, codec_id);
-		goto out;
+		goto end;
 	}
+
+	codec->s_cfg.avail = false;
+	codec->r_cfg.avail = false;
+	rfree(codec->r_cfg.data);
+	codec->r_cfg.data = NULL;
 
 	comp_dbg(dev, "codec_prepare() done");
 	codec->state = CODEC_PREPARED;
-out:
+end:
 	return ret;
 }
 
@@ -195,7 +201,174 @@ int codec_apply_runtime_config(struct comp_dev *dev) {
 		goto out;
 	}
 
+	cd->codec.r_cfg.avail = false;
+	rfree(codec->r_cfg.data);
+	codec->r_cfg.data = NULL;
+
 	comp_dbg(dev, "codec_apply_config() end");
 out:
 	return ret;
+}
+
+
+void *codec_allocate_memory(struct comp_dev *dev, uint32_t size,
+			    uint32_t alignment)
+{
+	struct codec_memory *container;
+	void *ptr;
+	struct comp_data *cd = comp_get_drvdata(dev);
+
+	if (!size) {
+		comp_err(dev, "codec_allocate_memory: requested allocation of 0 bytes.");
+		return NULL;
+	}
+
+	/* Allocate memory container */
+	container = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM,
+			    sizeof(struct codec_memory));
+	if (!container) {
+		comp_err(dev, "codec_allocate_memory: failed to allocate memory container.");
+		return NULL;
+	}
+
+	/* Allocate memory for codec */
+	if (alignment)
+		ptr = rballoc_align(0, SOF_MEM_CAPS_RAM, size, alignment);
+	else
+		ptr = rballoc(0, SOF_MEM_CAPS_RAM, size);
+
+	if (!ptr) {
+		comp_err(dev, "codec_allocate_memory: failed to allocate memory for codec %x.",
+			 cd->ca_config.codec_id);
+		return NULL;
+	} else {
+		/* Store reference to allocated memory */
+		container->ptr = ptr;
+		if (!cd->codec.memory) {
+			cd->codec.memory = container;
+			container->prev = NULL;
+			container->next = NULL;
+		} else {
+			container->prev = cd->codec.memory;
+			container->next = NULL;
+			cd->codec.memory->next = container;
+			cd->codec.memory = container;
+		}
+	}
+
+	return ptr;
+}
+
+int codec_free_memory(struct comp_dev *dev, void *ptr) {
+	struct comp_data *cd = comp_get_drvdata(dev);
+	struct codec_memory *mem = cd->codec.memory;
+	struct codec_memory *_mem;
+
+	if (!ptr) {
+		comp_err(dev, "codec_free_memory: error: NULL pointer passed.");
+		return -EINVAL;
+	}
+	/* Find which container keeps this memory */
+	do {
+		if (mem->ptr == ptr ) {
+			rfree(ptr);
+			mem->ptr = NULL;
+			if (mem->prev && mem->next) {
+				mem->prev->next = mem->next;
+				mem->next->prev = mem->prev;
+				_mem = mem->prev;
+				rfree(mem);
+				mem = _mem;
+			} else if (mem->prev) {
+				mem->prev->next = NULL;
+				cd->codec.memory = mem->prev;
+				rfree(mem);
+			} else {
+				rfree(mem);
+				cd->codec.memory = NULL;
+			}
+
+			return 0;
+		}
+		mem = mem->prev;
+	} while (mem);
+
+	comp_err(dev, "codec_free_memory: error: could not find memory pointed by %p",
+		 (uint32_t)ptr);
+
+	return -EINVAL;
+}
+
+static void codec_free_all_memory(struct comp_dev *dev) {
+	struct comp_data *cd = comp_get_drvdata(dev);
+	struct codec_memory *mem = cd->codec.memory;
+	struct codec_memory *_mem;
+
+	if (!mem)
+		return;
+
+	/* Find which container keeps this memory */
+	do {
+		if (mem->prev) {
+			_mem = mem->prev;
+			rfree(mem->ptr);
+			rfree(mem);
+			mem = _mem;
+		} else {
+			cd->codec.memory = NULL;
+			rfree(mem->ptr);
+			rfree(mem);
+			mem = NULL;
+		}
+	} while (mem);
+}
+
+void codec_free(struct comp_dev *dev)
+{
+	int ret;
+	struct comp_data *cd = comp_get_drvdata(dev);
+	struct codec_data *codec = &cd->codec;
+
+	ret = codec->call->reset(dev);
+	if (ret) {
+		comp_warn(dev, "codec_apply_config() error %d: codec specific .free() failed for codec_id %x",
+			  ret, cd->ca_config.codec_id);
+	}
+	/* Free all memory requested by codec */
+	codec_free_all_memory(dev);
+	/* Free all memory shared by codec_adapter & codec */
+	codec->s_cfg.avail = false;
+	codec->s_cfg.size = 0;
+	codec->r_cfg.avail = false;
+	codec->r_cfg.size = 0;
+	rfree(codec->r_cfg.data);
+	rfree(codec->s_cfg.data);
+	if (cd->runtime_params)
+		rfree(cd->runtime_params);
+}
+
+int codec_reset(struct comp_dev *dev)
+{
+	int ret;
+	struct comp_data *cd = comp_get_drvdata(dev);
+	struct codec_data *codec = &cd->codec;
+
+	ret = codec->call->reset(dev);
+	if (ret) {
+		comp_err(dev, "codec_apply_config() error %d: codec specific .reset() failed for codec_id %x",
+			  ret, cd->ca_config.codec_id);
+		return ret;
+	}
+
+	codec->r_cfg.avail = false;
+	codec->r_cfg.size = 0;
+	rfree(codec->r_cfg.data);
+
+	return 0;
+}
+
+static int validate_config(struct codec_config *cfg)
+{
+	//TODO:
+	return 0;
 }
