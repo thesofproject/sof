thesofproject/sof

commit 337afc4f3809ed2ef0da932f6ce97919b2077968

logger: add error message when reading sof/etrace instead of sof/trace
Failing silently is not nice. Now prints instead:

  TIMESTAMP   DELTA C# COMPONENT   LOCATION   CONTENT
  error: in logger_read(), fread(..., /sys/kernel/debug/sof/etrace) \
    failed: Invalid argument(22)

Also make logger_read() and the process return [-]errno which can have
different values and some information instead of -ferror() which has
only one value (non-zero in theory, 1 in practice) hence no information.

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

tools/logger/convert.c
@@ -630,8 +630,10 @@ static int logger_read(void)
 				freopen(NULL, "r", global_config->in_fd);
 				continue;
 			}
-
-			return -ferror(global_config->in_fd);
+			log_err("in %s(), fread(..., %s) failed: %s(%d)\n",
+				 __func__, global_config->in_file,
+				strerror(errno), errno);
+			return -errno;
 		}
 
 		/* checking if received trace address is located in

commit 5dafbee0f487107b25514ca4e0c4fe7ffb89bf78

logger: Log fread error only when it happened
fread may return 0 when some error occurred or nothig has been reded.
Moreover errno may not be set on Windows plaform, use ferror
instead. See:
https://www.gnu.org/software/gnulib/manual/html_node/fread.html

Fixes: 337afc4f3809: ("logger: add error message when reading sof/etrace instead of sof/trace")

Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

tools/logger/convert.c
@@ -640,10 +640,12 @@ static int logger_read(void)
 					return -errno;
 				}
 			}
-			log_err("in %s(), fread(..., %s) failed: %s(%d)\n",
-				 __func__, global_config->in_file,
-				strerror(errno), errno);
-			return -errno;
+			ret = -ferror(global_config->in_fd);
+			if (ret)
+				log_err("in %s(), fread(..., %s) failed: %s(%d)\n",
+					__func__, global_config->in_file,
+					strerror(-ret), ret);
+			return ret;
 		}
 
 		/* checking if received trace address is located in