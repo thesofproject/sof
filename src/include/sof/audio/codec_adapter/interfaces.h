72d8e09da97f405f49d6ff4120dd502241bfb5e2
Marcin Rajwa <marcin.rajwa@linux.intel.com>
2020-10-07 19:30:26 +0200

    codec_adapter: export cadence specific functions
    This exports cadence interfaces for the middle layer.
    
    Signed-off-by: Marcin Rajwa <marcin.rajwa@linux.intel.com>

--- src/include/sof/audio/codec_adapter/interfaces.h
+++ src/include/sof/audio/codec_adapter/interfaces.h
@@ -11,9 +11,25 @@
 #ifndef __SOF_AUDIO_CODEC_INTERFACES__
 #define __SOF_AUDIO_CODEC_INTERFACES__
 
+#if CONFIG_CADENCE_CODEC
+#include <sof/audio/codec_adapter/codec/cadence.h>
+#endif
+
+#define CADENCE_ID 0xCADE01
+
 /*****************************************************************************/
 /* Linked codecs interfaces						     */
 /*****************************************************************************/
-static struct codec_interface interfaces[0];
+static struct codec_interface interfaces[] = {
+	{
+		.id = CADENCE_ID, /**< Cadence interface */
+		.init  = cadence_codec_init,
+		.prepare = cadence_codec_prepare,
+		.process = cadence_codec_process,
+		.apply_config = cadence_codec_apply_config,
+		.reset = cadence_codec_reset,
+		.free = cadence_codec_free
+	},
+};
 
 #endif /* __SOF_AUDIO_CODEC_INTERFACES__ */
