f7f5066c3fe413b126d38bb17bb8552c1eb18a92
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2020-10-26 18:22:39 +0200

    Tools: Tune: TDFB: Updates for array geometry helpers and add 3D array
    This patch adds the missing XYZ 3D array helper and does minor fixes
    for two others. The L-shape microphones orientation is made similar as
    in rectangular array by inverting the y-axis. The missing centering
    of rectangular array is added.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

---
+++ tools/tune/tdfb/bf_array_xyz.m
@@ -0,0 +1,27 @@
+% bf = bf_array_xyz(bf)
+%
+% Inputs
+% bf.mic_x ... x coordinates [m]
+% bf.mic_y ... y coordinates [m]
+% bf.mic_z ... z coordinates [m]
+%
+% Outputs
+% bf.mic_n ... Number of microphones
+% bf.mic_x ... x coordinates [m]
+% bf.mic_y ... y coordinates [m]
+% bf.mic_z ... z coordinates [m]
+
+% SPDX-License-Identifier: BSD-3-Clause
+%
+% Copyright (c) 2020, Intel Corporation. All rights reserved.
+%
+% Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
+
+function bf = bf_array_xyz(bf)
+
+bf.mic_n = length(bf.mic_x);
+bf.mic_x = bf.mic_x - mean(bf.mic_x);
+bf.mic_y = bf.mic_y - mean(bf.mic_y);
+bf.mic_z = bf.mic_z - mean(bf.mic_z);
+
+end
