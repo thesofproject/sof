68b3693de14c4ccd31fc2d02bca261855527da0f
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2020-10-26 11:22:42 +0000

    plat_auth: use cavs 2.5 plat auth offsets
    cavs2.5 has different offsets for plat auth data.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/include/rimage/plat_auth.h
+++ src/include/rimage/plat_auth.h
@@ -11,6 +11,7 @@
 struct image;
 
 #define PLAT_AUTH_SHA256_LEN		32
+#define PLAT_AUTH_SHA384_LEN		48
 #define PLAT_AUTH_NAME_LEN		12
 #define PLAT_AUTH_PADDING		48	/* pad at end of struct */
 
@@ -24,6 +25,15 @@ struct signed_pkg_info_module {
 	uint16_t hash_size;
 	uint32_t meta_size;
 	uint8_t hash[PLAT_AUTH_SHA256_LEN];
+} __attribute__((packed));
+
+struct signed_pkg_info_module_v2_5 {
+	uint8_t name[PLAT_AUTH_NAME_LEN]; /* must be padded with 0 */
+	uint8_t type;
+	uint8_t hash_algo;
+	uint16_t hash_size;
+	uint32_t meta_size;
+	uint8_t hash[PLAT_AUTH_SHA384_LEN];
 } __attribute__((packed));
 
 struct signed_pkg_info_ext {
@@ -40,6 +50,22 @@ struct signed_pkg_info_ext {
 
 	/* variable length of modules */
 	struct signed_pkg_info_module module[SIGN_PKG_NUM_MODULE];
+} __attribute__((packed));
+
+struct signed_pkg_info_ext_v2_5 {
+	uint32_t ext_type;
+	uint32_t ext_len;
+
+	uint8_t name[4];
+	uint32_t vcn; /* 0 */
+	uint8_t bitmap[16];
+	uint32_t svn;
+	uint8_t fw_type;
+	uint8_t fw_sub_type;
+	uint8_t reserved[14];	/* must be 0 */
+
+	/* variable length of modules */
+	struct signed_pkg_info_module_v2_5 module[SIGN_PKG_NUM_MODULE];
 } __attribute__((packed));
 
 
@@ -73,6 +99,17 @@ struct partition_info_ext {
 	struct partition_info_module module[PART_INFO_NUM_MODULE];
 } __attribute__((packed));
 
+/* offset 0x458 id */
+struct info_ext_0x16 {
+	uint32_t ext_type;	/* 0x16 */
+	uint32_t ext_len;	/* 0x68 */
+	uint8_t name[4];	/* ADSP */
+	uint32_t size;		/* file size */
+	uint32_t data[5];	/* 0 = 0x10000000, 0, 1, 1, 0x3003 */
+	uint8_t hash[PLAT_AUTH_SHA384_LEN];
+	uint32_t data1[5];
+} __attribute__((packed));
+
 
 #define PLAT_AUTH_SIZE \
 	(sizeof(struct partition_info_ext) + \
@@ -83,5 +120,6 @@ void ri_adsp_meta_data_create_v1_8(struct image *image, int meta_start_offset,
 void ri_adsp_meta_data_create_v2_5(struct image *image, int meta_start_offset,
 				   int meta_end_offset);
 void ri_plat_ext_data_create(struct image *image);
+void ri_plat_ext_data_create_v2_5(struct image *image);
 
 #endif
