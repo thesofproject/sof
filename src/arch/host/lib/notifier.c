88158a602477849f7fa9ea6e79b3cccdd72a58b5
Curtis Malainey <cujomalainey@chromium.org>
2020-10-27 18:08:02 -0700

    host: don't alloc notifier
    Both the fuzzer and the testbench call init_system_notifier which
    initializes the struct so there is no need for this check (which causes
    a double alloc) in the get function.
    
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>

--- src/arch/host/lib/notifier.c
+++ src/arch/host/lib/notifier.c
@@ -18,8 +18,5 @@ static struct notify *host_notify;
 
 struct notify **arch_notify_get(void)
 {
-	if (!host_notify)
-		host_notify = rzalloc(SOF_MEM_ZONE_SYS, 0, SOF_MEM_CAPS_RAM,
-				      sizeof(*host_notify));
 	return &host_notify;
 }
