d364f9790d836709ad3ad7d8bb90fabf5ed8c277
Jaska Uimonen <jaska.uimonen@intel.com>
2020-11-30 10:01:29 +0200

    scripts: docker build changes to support enum control
    Update docker build description to use alsa-lib and alsa-utils upstream
    rather than sofproject versions. Also update ulimit for docker build to
    support crosstools-ng.
    
    Signed-off-by: Jaska Uimonen <jaska.uimonen@intel.com>

--- scripts/docker_build/sof_builder/docker-build.sh
+++ scripts/docker_build/sof_builder/docker-build.sh
@@ -7,4 +7,4 @@ if [ -f "/etc/apt/apt.conf" ]; then
 else
 	touch apt.conf
 fi
-docker build --build-arg UID=$(id -u) --build-arg host_http_proxy=$http_proxy --build-arg host_https_proxy=$https_proxy -t sof .
+docker build --ulimit nofile=65536:65536 --build-arg UID=$(id -u) --build-arg host_http_proxy=$http_proxy --build-arg host_https_proxy=$https_proxy -t sof .
