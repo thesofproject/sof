thesofproject/sof

commit 647390f75e6a89d15acc7e3fe71f5d890d93e85f

schedule: dma_multi_chan: fix a DMA channel interrupt race
When streaming audio data on multiple DMA channels simultaneously cases
with 3 tasks (A, B, and C), scheduled on 2 DMA channels (X and Y), have
been observed, when one of the tasks wasn't scheduled. Usually when
scanning tasks in schedule_ll_is_pending() the following sequence would
take place:
    channel    task    interrupt status
    X          A       1
    X          B       1
    Y          C       1
where the interrupt would be cleared in lines 2 and 3 above. However,
sometimes the following sequence takes place:
    channel    task    interrupt status
    X          A       0
    X          B       1
    Y          C       1
which then leaves task A not scheduled, because similar to the first case,
the interrupt on channel X will be cleared in line 2. To fix this modify
schedule_ll_is_pending() to handle DMA channels / scheduling components
one after another.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/schedule/dma_multi_chan_domain.c
@@ -275,7 +275,7 @@ out:
  * \return True is task should be executed, false otherwise.
  */
 static bool dma_multi_chan_domain_is_pending(struct ll_schedule_domain *domain,
-					     struct task *task)
+					     struct task *task, struct comp_dev **comp)
 {
 	struct dma_domain *dma_domain = ll_sch_domain_get_pdata(domain);
 	struct pipeline_task *pipe_task = pipeline_task_get(task);
@@ -286,10 +286,16 @@ static bool dma_multi_chan_domain_is_pending(struct ll_schedule_domain *domain,
 
 	for (i = 0; i < dma_domain->num_dma; ++i) {
 		for (j = 0; j < dmas[i].plat_data.channels; ++j) {
-			status = dma_interrupt(&dmas[i].chan[j],
-					       DMA_IRQ_STATUS_GET);
-			if (!status)
+			if (!*comp) {
+				status = dma_interrupt(&dmas[i].chan[j],
+						       DMA_IRQ_STATUS_GET);
+				if (!status)
+					continue;
+
+				*comp = dma_domain->data[i][j].task->sched_comp;
+			} else if (dma_domain->data[i][j].task->sched_comp != *comp) {
 				continue;
+			}
 
 			/* not the same scheduling component */
 			if (dma_domain->data[i][j].task->sched_comp !=

src/include/sof/schedule/ll_schedule_domain.h
@@ -38,7 +38,7 @@ struct ll_schedule_domain_ops {
 	void (*domain_set)(struct ll_schedule_domain *domain, uint64_t start);
 	void (*domain_clear)(struct ll_schedule_domain *domain);
 	bool (*domain_is_pending)(struct ll_schedule_domain *domain,
-				  struct task *task);
+				  struct task *task, struct comp_dev **comp);
 };
 
 struct ll_schedule_domain {
@@ -151,13 +151,13 @@ static inline void domain_clear(struct ll_schedule_domain *domain)
 }
 
 static inline bool domain_is_pending(struct ll_schedule_domain *domain,
-				     struct task *task)
+				     struct task *task, struct comp_dev **comp)
 {
 	bool ret;
 
 	assert(domain->ops->domain_is_pending);
 
-	ret = domain->ops->domain_is_pending(domain, task);
+	ret = domain->ops->domain_is_pending(domain, task, comp);
 
 	platform_shared_commit(domain, sizeof(*domain));
 

src/schedule/dma_single_chan_domain.c
@@ -509,7 +509,7 @@ out:
  * \return True is task should be executed, false otherwise.
  */
 static bool dma_single_chan_domain_is_pending(struct ll_schedule_domain *domain,
-					      struct task *task)
+					      struct task *task, struct comp_dev **comp)
 {
 	return task->start <= platform_timer_get(timer_get());
 }

src/schedule/ll_schedule.c
@@ -59,16 +59,21 @@ static bool schedule_ll_is_pending(struct ll_schedule_data *sch)
 	struct list_item *tlist;
 	struct task *task;
 	uint32_t pending_count = 0;
+	struct comp_dev *sched_comp;
 
-	/* mark each valid task as pending */
-	list_for_item(tlist, &sch->tasks) {
-		task = container_of(tlist, struct task, list);
+	do {
+		sched_comp = NULL;
 
-		if (domain_is_pending(sch->domain, task)) {
-			task->state = SOF_TASK_STATE_PENDING;
-			pending_count++;
+		/* mark each valid task as pending */
+		list_for_item(tlist, &sch->tasks) {
+			task = container_of(tlist, struct task, list);
+
+			if (domain_is_pending(sch->domain, task, &sched_comp)) {
+				task->state = SOF_TASK_STATE_PENDING;
+				pending_count++;
+			}
 		}
-	}
+	} while (sched_comp);
 
 	return pending_count > 0;
 }

src/schedule/timer_domain.c
@@ -269,7 +269,7 @@ static void timer_domain_clear(struct ll_schedule_domain *domain)
 }
 
 static bool timer_domain_is_pending(struct ll_schedule_domain *domain,
-				    struct task *task)
+				    struct task *task, struct comp_dev **comp)
 {
 	return task->start <= platform_timer_get(timer_get());
 }

commit e54c42fa0ff77740d8f9a512954c9bb9527cbaf2

schedule: dma_multi_chan: fix null pointer when scanning for tasks
When scanning all channels for a certain task we end up comparing
with a null task.
That's because we have multiple channels (e.g. 32 for i.MX) but only
some of them are used in certain scenarios. For example, for simultaneous
playback/capture we use only 2 DMA channels.
To fix this, validate task before using it.

Fixes #3690
Fixes: 647390f75e6a ("schedule: dma_multi_chan: fix a DMA channel interrupt race")

Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>
Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

src/schedule/dma_multi_chan_domain.c
@@ -293,7 +293,8 @@ static bool dma_multi_chan_domain_is_pending(struct ll_schedule_domain *domain,
 					continue;
 
 				*comp = dma_domain->data[i][j].task->sched_comp;
-			} else if (dma_domain->data[i][j].task->sched_comp != *comp) {
+			} else if (!dma_domain->data[i][j].task ||
+				   dma_domain->data[i][j].task->sched_comp != *comp) {
 				continue;
 			}
 