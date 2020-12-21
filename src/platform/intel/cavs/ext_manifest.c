5a1078d29356425ad93040cb6c43e69b9eceaeb8
Karol Trzcinski <karolx.trzcinski@linux.intel.com>
2020-12-21 09:32:59 +0100

    ext_man: Remove dictionary elements counter from ABI for ext_man_cavs_config_data
    This value shouldn't be included in any ABI header, because it may
    vary between firmware configurations.
    This change makes header file more similar to version provided by kernel.
    
    Signed-off-by: Karol Trzcinski <karolx.trzcinski@linux.intel.com>

--- src/platform/intel/cavs/ext_manifest.c
+++ src/platform/intel/cavs/ext_manifest.c
@@ -10,10 +10,14 @@
 #include <sof/common.h>
 #include <sof/lib/memory.h>
 
+/* Describes elements counter from ext_man_cavs_config dictionary */
+#define CAVS_CONFIG_ELEM_CNT (EXT_MAN_CAVS_CONFIG_LAST_ELEM - 1)
+
 const struct ext_man_cavs_config_data ext_man_cavs_config
 	__aligned(EXT_MAN_ALIGN) __section(".fw_metadata") = {
 	.hdr.type = EXT_MAN_ELEM_PLATFORM_CONFIG_DATA,
-	.hdr.elem_size = ALIGN_UP(sizeof(struct ext_man_cavs_config_data),
+	.hdr.elem_size = ALIGN_UP(sizeof(struct ext_man_cavs_config_data) +
+				  sizeof(struct config_elem) * CAVS_CONFIG_ELEM_CNT,
 				  EXT_MAN_ALIGN),
 	.elems = {
 		{EXT_MAN_CAVS_CONFIG_LPRO,  IS_ENABLED(CONFIG_CAVS_LPRO_ONLY)},
