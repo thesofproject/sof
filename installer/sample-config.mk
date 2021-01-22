9bf1e54587360cfce92918e4316e84f87c0b579d
Marc Herbert <marc.herbert@intel.com>
2021-01-22 08:15:43 +0000

    New install/GNUmakefile to gradually replace scripts in sof-bin.git
    More specifically replacing sof-bin/go.sh and sof-bin/publish.sh and
    also sof/scripts/sof-target-install.sh eventually.
    
    "make install" code has always belonged to source repositories because
    developers need to install too and we want everyone to use the same
    installers. It's also easier to have all the information in a single
    place.
    
    Once the layout in sof-bin mirrors the /lib/firmware/intel layout
    exactly, sof-bin does not need any installation code any more.
    
    Mixing source and binaries in the same repo is also a "code smell",
    notably because it forces branching them together.
    
    Using a higher level build tool for installation instead of plain
    scripts has a few benefits:
    
    - Multiple entry points: easy to invoke (and test) any part of the
      installer individually
    - ... while invoking dependencies automatically.
    - Other features "for free" like:
      - errexit
      - error messages like "dunno how to build file x"
      - commands are logged by default
    
    - Also gets rid of most of the large code duplication in go.sh and
      publish.sh, so:
      - Enabling or disabling a platform is a 3-character change
      - Allows platform selection in local config file (even just one platform)
      - Much harder to add inconsistencies
      - Much easier to review correctness, for instance no need to
        scrutinize every line to see which platforms are aliased.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>
    (cherry picked from commit c6edc5f51e069e2c70c7cf0c50567ed916b3acd7)

---
+++ installer/sample-config.mk
@@ -0,0 +1,23 @@
+# To customize the installation, copy this file to config.mk and edit
+# it. Leave undefined to use default values.
+
+# As usual with Make, all these can also be passed as either CLI
+# arguments or environment variables.  Warning: undefined is NOT the
+# same as blank!
+
+# Everything is installed by default. To install and deploy fewer
+# patforms override the default lists like this:
+# UNSIGNED_list :=
+# SIGNED_list := apl tgl
+
+# The default FW_DESTDIR is the local /lib/firmware/intel directory
+# _remote := test-system.local
+# FW_DESTDIR     := root@${_remote}:/lib/firmware/intel
+# USER_DESTDIR   := ${_remote}:bin/
+
+# Define this empty for a plain sof/ directory and no sof -> sof-v1.2.3
+# symbolic links.
+# SOF_VERSION :=
+#
+# SOF_VERSION := $(shell git describe --tags )
+# SOF_VERSION := v1.6.14
