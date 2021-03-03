b79b7b5d729cb915e4bebd52335bfcc03fb25fec
ShriramShastry <malladi.sastry@intel.com>
2021-03-03 11:42:40 +0530

    Math cordicsin function Signed-off-by: ShriramShastry <malladi.sastry@intel.com>

--- src/audio/drc/drc_math_generic.c
+++ src/audio/drc/drc_math_generic.c
@@ -13,6 +13,24 @@
 #define q_mult(a, b, qa, qb, qy) ((int32_t)Q_MULTSR_32X32((int64_t)(a), b, qa, qb, qy))
 #define q_multq(a, b, q) ((int32_t)Q_MULTSR_32X32((int64_t)(a), b, q, q, q))
 
+/* #define PIQ20OFFSET0		Q_CONVERT_FLOAT_PRECISION(1.570796012878417968750000f, 20, 0.0)			*/
+/* #define PIQ21OFFSET0		Q_CONVERT_FLOAT_PRECISION(1.570796489715576171875000f, 21, 0.0)			*/
+/* #define PIQ30OFFSET0		Q_CONVERT_FLOAT_PRECISION(1.000000000000000000000000,	30, 0.0)			*/
+/* #define PIQ22OFFSET0		Q_CONVERT_FLOAT_PRECISION(1.570796251296997070312500f, 22, 0.0)			*/
+/* #define PIQ28OFFSET4		Q_CONVERT_FLOAT_PRECISION(1.214506641030311584472656f, 28, 4.0)			*/
+/* #define PIQ29OFFSET1		Q_CONVERT_FLOAT_PRECISION(1.999999998137354850769043f, 29, -1.0)		*/
+/* Convert fractional Qnx.ny number x to float and offset correction for precision					*/
+/* #define Q_CONVERT_FLOAT_PRECISION(f, qy, offset) \															*/
+/*		((int32_t)(((const double)f) * ((int64_t)1 << (const int)qy) + ((const double)offset)))	*/
+
+
+#define PIQ20OFFSET0		1647099						/* Q12.20 */
+#define PIQ21OFFSET0		3294199						/* Q11.21 */
+#define PIQ22OFFSET0		6588397						/* Q10.22 */
+#define PIQ28OFFSET4		326016644						/* Q4.28	 */
+#define PIQ29OFFSET1		1073741823					/* Q3.29	 */
+#define PIQ30OFFSET0		1073741824					/* Q2.30	 */
+
 /*
  * Input depends on precision_x
  * Output range [0.5, 1); regulated to Q2.30
@@ -105,16 +123,336 @@ inline int32_t drc_log_fixed(int32_t x)
 }
 
 /*
- * Input is Q2.30: (-2.0, 2.0)
- * Output range: (-1.0, 1.0); regulated to Q1.31: (-1.0, 1.0)
+ * function [cdcSinTh] = drc_sin_fixed(thRadFxp)
+ * compute the sine theta.thRadFxp contains angle values in radians in the range of [-2pi,2pi]
+ * cdcSinTh is the CORDIC-based approximation of the sine of theta
+ * rotate the vector over given angle. vector co ordinate ( x & y ) is rotated through angle
+ * and obtain new vector co ordinate.
+ * Use Q12.20-bit quantized inputs and with max error 0.
+ *	 Compare the fixed-point cordicsin function results to the
+ *	 results of the double-precision sin function.
+ *
+ * +--------------+---------+----------+----------+---------+
+ * |					|thRadFxp |cdcSinTh	|QthRadFxp |QcdcSinTh|
+ * +--------------+---------+----------+----------+---------+
+ * |		WordLength| 31		 |	 32		|12.20		| 3.29		|
+ * |FractionLength| 20		 |	 29		|				|			|
+ * +--------------+---------+----------+----------+---------+
+ *	 THD+N = 29*6 = -174 dBc
+ * Arguments	 : int32_t thRadFxp Q12.20 input
+ *						int32_t cdcSinTh Q3.29	output
+ * Return Type	 : cdcSinTh
  */
-inline int32_t drc_sin_fixed(int32_t x)
+inline int32_t drc_sin_fixed(int32_t thRadFxp)
 {
-	const int32_t PI_OVER_TWO = Q_CONVERT_FLOAT(1.57079632679489661923f, 30);
+	/* cordic_atan2_lookup_table = atan(2.^-(0:N-1)) N = 30	*/
+	/* CORDIC Gain is cordic_gain = prod(sqrt(1 + 2.^(-2*(0:30-1)))) */
+	/* Inverse CORDIC Gain,inverse_cordic_gain = 1 / cordic_gain	*/
+	static const int32_t inpLUT[30] = { 421657428, 248918915, 131521918, 66762579,
+	 33510843, 16771758, 8387925, 4194219, 2097141, 1048575, 524288, 262144,
+	 131072, 65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32,
+	 16, 8, 4, 2, 1 };
 
-	/* input range of sin_fixed() is non-negative */
-	int32_t abs_sin_val = sin_fixed(q_mult(ABS(x), PI_OVER_TWO, 30, 30, 28));
-	return SGN(x) < 0 ? -abs_sin_val : abs_sin_val;
+	int32_t b_idx;
+	int32_t b_yn;
+	int32_t idx;
+	int32_t thRadFxp;
+	int32_t xn;
+	int32_t xtmp;
+	int32_t ytmp;
+	int32_t z;
+	int32_t cdcSinTh;
+	boolean_t negate;
+
+
+	if (thRadFxp > PIQ20OFFSET0) {											/* >20bits */
+		if (((thRadFxp - PIQ21OFFSET0) & PIQ30OFFSET0) != 0) {		 /* NonZero thRadFxp - 21bits & 30bits */
+			z = (thRadFxp - PIQ21OFFSET0) | -PIQ30OFFSET0;
+		} else {
+			z = (thRadFxp - PIQ21OFFSET0) & PIQ29OFFSET1;
+		}
+
+		if (z <= PIQ20OFFSET0) {												 /*	 <20 bits*/
+			thRadFxp = z;
+			negate = true;
+		} else {
+			if (((thRadFxp - PIQ22OFFSET0) & PIQ30OFFSET0) != 0) {		 /*	 thRadFxp - 22bits & 30bits */
+			 thRadFxp = (thRadFxp - PIQ22OFFSET0) | -PIQ30OFFSET0;
+			} else {
+			 thRadFxp = (thRadFxp - PIQ22OFFSET0) & PIQ29OFFSET1;
+			}
+
+			negate = false;
+		}
+	 } else if (thRadFxp < -PIQ20OFFSET0) {									/*	 <20bit */
+		if (((thRadFxp + PIQ21OFFSET0) & PIQ30OFFSET0) != 0) {			/*	 NonZero thRadFxp - 21bits & 30bits */
+			z = (thRadFxp + PIQ21OFFSET0) | -PIQ30OFFSET0;
+		} else {
+			z = (thRadFxp + PIQ21OFFSET0) & PIQ29OFFSET1;
+		}
+
+		if (z >= -PIQ20OFFSET0) {
+			thRadFxp = z;
+			negate = true;
+		} else {
+			if (((thRadFxp + PIQ22OFFSET0) & PIQ30OFFSET0) != 0) {		 /*	 thRadFxp - 22bits & 30bits */
+			 thRadFxp = (thRadFxp + PIQ22OFFSET0) | -PIQ30OFFSET0;
+			} else {
+			 thRadFxp = (thRadFxp + PIQ22OFFSET0) & PIQ29OFFSET1;
+			}
+
+			negate = false;
+		}
+	 } else {
+		negate = false;
+	 }
+
+	 thRadFxp <<= 9;															/* thRadFxp *= 512 */
+	 if ((thRadFxp & PIQ30OFFSET0) != 0) {								/*	 NonZero 30bits */
+		z = thRadFxp | -PIQ30OFFSET0;
+	 } else {
+		z = thRadFxp & PIQ29OFFSET1;
+	 }
+
+	 b_yn = 0;
+	 xn = PIQ28OFFSET4;														/*	 28 bits */
+	 xtmp = PIQ28OFFSET4;
+	 ytmp = 0;
+	 for (b_idx = 0; b_idx < 30; b_idx++) {							/*	 Iterate data with lookup table */
+		if (z < 0) {															/*	 negative data */
+			thRadFxp = z + inpLUT[b_idx];
+			if ((thRadFxp & PIQ30OFFSET0) != 0) {						/*	 30 bits */
+			 z = thRadFxp | -PIQ30OFFSET0;								/*	 Add */
+			} else {
+			 z = thRadFxp & PIQ29OFFSET1;									/*	 shift */
+			}
+
+			thRadFxp = xn + ytmp;
+			if ((thRadFxp & PIQ30OFFSET0) != 0) {
+			 xn = thRadFxp | -PIQ30OFFSET0;
+			} else {
+			 xn = thRadFxp & PIQ29OFFSET1;
+			}
+
+			thRadFxp = b_yn - xtmp;
+			if ((thRadFxp & PIQ30OFFSET0) != 0) {						/*	30 bits */
+			 b_yn = thRadFxp | -PIQ30OFFSET0;
+			} else {
+			 b_yn = thRadFxp & PIQ29OFFSET1;
+			}
+		} else {
+			thRadFxp = z - inpLUT[b_idx];									/*	 positive LookUp table */
+			if ((thRadFxp & PIQ30OFFSET0) != 0) {
+			 z = thRadFxp | -PIQ30OFFSET0;
+			} else {
+			 z = thRadFxp & PIQ29OFFSET1;
+			}
+
+			thRadFxp = xn - ytmp;
+			if ((thRadFxp & PIQ30OFFSET0) != 0) {
+			 xn = thRadFxp | -PIQ30OFFSET0;
+			} else {
+			 xn = thRadFxp & PIQ29OFFSET1;
+			}
+
+			thRadFxp = b_yn + xtmp;
+			if ((thRadFxp & PIQ30OFFSET0) != 0) {
+			 b_yn = thRadFxp | -PIQ30OFFSET0;
+			} else {
+			 b_yn = thRadFxp & PIQ29OFFSET1;
+			}
+		}
+
+		thRadFxp = xn >> (b_idx + 1);									/*	 arithmatic right shift */
+		if ((thRadFxp & PIQ30OFFSET0) != 0) {
+			xtmp = thRadFxp | -PIQ30OFFSET0;
+		} else {
+			xtmp = thRadFxp & PIQ29OFFSET1;
+		}
+
+		thRadFxp = b_yn >> (b_idx + 1);
+		if ((thRadFxp & PIQ30OFFSET0) != 0) {
+			ytmp = thRadFxp | -PIQ30OFFSET0;
+		} else {
+			ytmp = thRadFxp & PIQ29OFFSET1;
+		}
+	 }
+
+	 if (negate) {
+		if ((-b_yn & PIQ30OFFSET0) != 0) {
+			cdcSinTh = -b_yn | -PIQ30OFFSET0;
+		} else {
+			cdcSinTh = -b_yn & PIQ29OFFSET1;
+		}
+	 } else {
+		cdcSinTh = b_yn;
+	 }
+	 return (cdcSinTh);
+}
+/*
+ * function [cdcSinTh] = drc_sin_fixed(thRadFxp)
+ * compute the sine theta.thRadFxp contains angle values in radians in the range of [-2pi,2pi]
+ * cdcSinTh is the CORDIC-based approximation of the sine of theta
+ * rotate the vector over given angle. Vector is rotated for 30 iteration
+ * vector co ordinate ( x & y ) is rotated through angle and obtain new vector
+ * co ordinate.
+ * Use Q12.20-bit quantized inputs and with max error 0.
+ *	Compare the fixed-point cordicsin function results to the
+ *	results of the double-precision sin function.
+ *
+ * +--------------+---------+----------+----------+---------+
+ * |							|thRadFxp |cdcSinTh	 |QthRadFxp |QcdcSinTh|
+ * +--------------+---------+----------+----------+---------+
+ * |		WordLength| 31			|	 32			 |12.20			| 3.29		|
+ * |FractionLength| 20			|	 29			 |					|					|
+ * +--------------+---------+----------+----------+---------+
+ *	THD+N = 29*6 = -174 dBc
+ * Arguments		: int32_t thRadFxp Q12.20
+ *								int32_t cdcSinTh Q3.29
+ * Return Type	: cdcSinTh
+ */
+inline int32_t drc_sin_fixed(int32_t thRadFxp)
+{
+	static const int32_t inpLUT[30] = { 421657428, 248918915, 131521918, 66762579,
+		33510843, 16771758, 8387925, 4194219, 2097141, 1048575, 524288, 262144,
+		131072, 65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32,
+		16, 8, 4, 2, 1 };
+
+	int32_t b_idx;
+	int32_t b_yn;
+	int32_t idx;
+	int32_t thRadFxp;
+	int32_t xn;
+	int32_t xtmp;
+	int32_t ytmp;
+	int32_t z;
+	int32_t cdcSinTh;
+	boolean_t negate;
+
+
+		if (thRadFxp > 1647099) {																// >20bits
+			if (((thRadFxp - 3294199) & 1073741824) != 0) {				// NonZero thRadFxp - 21bits & 30bits
+				z = (thRadFxp - 3294199) | -1073741824;
+			} else {
+				z = (thRadFxp - 3294199) & 1073741823;
+			}
+
+			if (z <= 1647099) {																		// <20 bits
+				thRadFxp = z;
+				negate = 1U;
+			} else {
+				if (((thRadFxp - 6588397) & 1073741824) != 0) {			// thRadFxp - 22bits & 30bits
+					thRadFxp = (thRadFxp - 6588397) | -1073741824;		//
+				} else {
+					thRadFxp = (thRadFxp - 6588397) & 1073741823;
+				}
+
+				negate = 0U;
+			}
+		} else if (thRadFxp < -1647099) {													// <20
+			if (((thRadFxp + 3294199) & 1073741824) != 0) {					// NonZero thRadFxp - 21bits & 30bits
+				z = (thRadFxp + 3294199) | -1073741824;
+			} else {
+				z = (thRadFxp + 3294199) & 1073741823;
+			}
+
+			if (z >= -1647099) {
+				thRadFxp = z;
+				negate = 1U;
+			} else {
+				if (((thRadFxp + 6588397) & 1073741824) != 0) {			// thRadFxp - 22bits & 30bits
+					thRadFxp = (thRadFxp + 6588397) | -1073741824;
+				} else {
+					thRadFxp = (thRadFxp + 6588397) & 1073741823;
+				}
+
+				negate = 0U;
+			}
+		} else {
+			negate = 0U;
+		}
+
+		thRadFxp <<= 9;			// thRadFxp *= 512
+		if ((thRadFxp & 1073741824) != 0) {											// NonZero 30bits
+			z = thRadFxp | -1073741824;
+		} else {
+			z = thRadFxp & 1073741823;
+		}
+
+		b_yn = 0;
+		xn = 326016437;																					// 28 bits
+		xtmp = 326016437;
+		ytmp = 0;
+		for (b_idx = 0; b_idx < 30; b_idx++) {									// Iterate data with lookup table
+			if (z < 0) {																					// negative data
+				thRadFxp = z + inpLUT[b_idx];
+				if ((thRadFxp & 1073741824) != 0) {									// 30 bits
+					z = thRadFxp | -1073741824;												// subtract
+				} else {
+					z = thRadFxp & 1073741823;												// multiply
+				}
+
+				thRadFxp = xn + ytmp;
+				if ((thRadFxp & 1073741824) != 0) {
+					xn = thRadFxp | -1073741824;
+				} else {
+					xn = thRadFxp & 1073741823;
+				}
+
+				thRadFxp = b_yn - xtmp;
+				if ((thRadFxp & 1073741824) != 0) {									// 30 bits
+					b_yn = thRadFxp | -1073741824;
+				} else {
+					b_yn = thRadFxp & 1073741823;
+				}
+			} else {
+				thRadFxp = z - inpLUT[b_idx];												// positive LookUp table
+				if ((thRadFxp & 1073741824) != 0) {
+					z = thRadFxp | -1073741824;
+				} else {
+					z = thRadFxp & 1073741823;
+				}
+
+				thRadFxp = xn - ytmp;
+				if ((thRadFxp & 1073741824) != 0) {
+					xn = thRadFxp | -1073741824;
+				} else {
+					xn = thRadFxp & 1073741823;
+				}
+
+				thRadFxp = b_yn + xtmp;
+				if ((thRadFxp & 1073741824) != 0) {
+					b_yn = thRadFxp | -1073741824;
+				} else {
+					b_yn = thRadFxp & 1073741823;
+				}
+			}
+
+			thRadFxp = xn >> (b_idx + 1);													// arithmatic right shift
+			if ((thRadFxp & 1073741824) != 0) {
+				xtmp = thRadFxp | -1073741824;
+			} else {
+				xtmp = thRadFxp & 1073741823;
+			}
+
+			thRadFxp = b_yn >> (b_idx + 1);
+			if ((thRadFxp & 1073741824) != 0) {
+				ytmp = thRadFxp | -1073741824;
+			} else {
+				ytmp = thRadFxp & 1073741823;
+			}
+		}
+
+		if (negate) {
+			if ((-b_yn & 1073741824) != 0) {
+				cdcSinTh = -b_yn | -1073741824;
+			} else {
+				cdcSinTh = -b_yn & 1073741823;
+			}
+		} else {
+			cdcSinTh = b_yn;
+		}
+		return (cdcSinTh);
 }
 
 /*
@@ -127,11 +465,11 @@ inline int32_t drc_asin_fixed(int32_t x)
 #define qch 26
 	/* Coefficients obtained from:
 	 * If x <= 1/sqrt(2), then
-	 *   fpminimax(asin(x), [|1,3,5,7|], [|SG...|], [-1e-30;1/sqrt(2)], absolute)
-	 *   max err ~= 1.89936e-5
+	 *		fpminimax(asin(x), [|1,3,5,7|], [|SG...|], [-1e-30;1/sqrt(2)], absolute)
+	 *		max err ~= 1.89936e-5
 	 * Else then
-	 *   fpminimax(asin(x), [|1,3,5,7|], [|SG...|], [1/sqrt(2);1], absolute)
-	 *   max err ~= 3.085226e-2
+	 *		fpminimax(asin(x), [|1,3,5,7|], [|SG...|], [1/sqrt(2);1], absolute)
+	 *		max err ~= 3.085226e-2
 	 */
 	const int32_t TWO_OVER_PI = Q_CONVERT_FLOAT(0.63661977236758134f, qcl); /* 2/pi */
 	const int32_t ONE_OVER_SQRT2 = Q_CONVERT_FLOAT(0.70710678118654752f, qcl); /* 1/sqrt(2) */
@@ -178,7 +516,7 @@ inline int32_t drc_asin_fixed(int32_t x)
 
 /*
  * Input x is Q6.26; valid range: (0.0, 32.0); x <= 0 is not supported
- *       y is Q2.30: (-2.0, 2.0)
+ *			y is Q2.30: (-2.0, 2.0)
  * Output is Q12.20: max 2048.0
  */
 inline int32_t drc_pow_fixed(int32_t x, int32_t y)
