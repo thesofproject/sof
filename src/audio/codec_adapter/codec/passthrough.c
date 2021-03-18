cea7a2136d01d448dacb8f7f45535c2789f40c5b
Curtis Malainey <cujomalainey@chromium.org>
2021-03-17 17:55:34 -0700

    codec_adapter: remove term "dummy" from adapters
    Following move of open source to remove oppressive language from open
    source, dummy is one of the terms to avoid. Also the term does a poor
    job of describing what the adapter actually does.
    
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>

--- src/audio/codec_adapter/codec/passthrough.c
+++ src/audio/codec_adapter/codec/passthrough.c
@@ -4,34 +4,34 @@
 //
 // Author: Daniel Baluta <daniel.baluta@nxp.com>
 //
-// Dummy codec implementation to demonstrate Codec Adapter API
+// Passthrough codec implementation to demonstrate Codec Adapter API
 
 #include <sof/audio/codec_adapter/codec/generic.h>
-#include <sof/audio/codec_adapter/codec/dummy.h>
+#include <sof/audio/codec_adapter/codec/passthrough.h>
 
-int dummy_codec_init(struct comp_dev *dev)
+int passthrough_codec_init(struct comp_dev *dev)
 {
-	comp_info(dev, "dummy_codec_init() start");
+	comp_info(dev, "passthrough_codec_init() start");
 	return 0;
 }
 
-int dummy_codec_prepare(struct comp_dev *dev)
+int passthrough_codec_prepare(struct comp_dev *dev)
 {
 	struct codec_data *codec = comp_get_codec(dev);
 	struct comp_data *cd = comp_get_drvdata(dev);
 
-	comp_info(dev, "dummy_codec_process()");
+	comp_info(dev, "passthrough_codec_process()");
 
 	codec->cpd.in_buff = rballoc(0, SOF_MEM_CAPS_RAM, cd->period_bytes);
 	if (!codec->cpd.in_buff) {
-		comp_err(dev, "dummy_codec_prepare(): Failed to alloc in_buff");
+		comp_err(dev, "passthrough_codec_prepare(): Failed to alloc in_buff");
 		return -ENOMEM;
 	}
 	codec->cpd.in_buff_size = cd->period_bytes;
 
 	codec->cpd.out_buff = rballoc(0, SOF_MEM_CAPS_RAM, cd->period_bytes);
 	if (!codec->cpd.out_buff) {
-		comp_err(dev, "dummy_codec_prepare(): Failed to alloc out_buff");
+		comp_err(dev, "passthrough_codec_prepare(): Failed to alloc out_buff");
 		rfree(codec->cpd.in_buff);
 		return -ENOMEM;
 	}
@@ -40,12 +40,12 @@ int dummy_codec_prepare(struct comp_dev *dev)
 	return 0;
 }
 
-int dummy_codec_process(struct comp_dev *dev)
+int passthrough_codec_process(struct comp_dev *dev)
 {
 	struct codec_data *codec = comp_get_codec(dev);
 	struct comp_data *cd = comp_get_drvdata(dev);
 
-	comp_dbg(dev, "dummy_codec_process()");
+	comp_dbg(dev, "passthrough_codec_process()");
 
 	memcpy_s(codec->cpd.out_buff, codec->cpd.out_buff_size,
 		 codec->cpd.in_buff, codec->cpd.in_buff_size);
@@ -54,27 +54,27 @@ int dummy_codec_process(struct comp_dev *dev)
 	return 0;
 }
 
-int dummy_codec_apply_config(struct comp_dev *dev)
+int passthrough_codec_apply_config(struct comp_dev *dev)
 {
-	comp_info(dev, "dummy_codec_apply_config()");
+	comp_info(dev, "passthrough_codec_apply_config()");
 
 	/* nothing to do */
 	return 0;
 }
 
-int dummy_codec_reset(struct comp_dev *dev)
+int passthrough_codec_reset(struct comp_dev *dev)
 {
-	comp_info(dev, "dummy_codec_reset()");
+	comp_info(dev, "passthrough_codec_reset()");
 
 	/* nothing to do */
 	return 0;
 }
 
-int dummy_codec_free(struct comp_dev *dev)
+int passthrough_codec_free(struct comp_dev *dev)
 {
 	struct codec_data *codec = comp_get_codec(dev);
 
-	comp_info(dev, "dummy_codec_free()");
+	comp_info(dev, "passthrough_codec_free()");
 
 	rfree(codec->cpd.in_buff);
 	rfree(codec->cpd.out_buff);
