c3f3bb76bfa47c0a03da434aa22bbf2a9d616529
Oleksandr Strelchenko <oleksandr.strelchenko@waves.com>
2021-03-20 06:51:44 -0700

    audio: codec_adapter: add Waves MaxxEffect API
    add Waves MaxxEffect API definition.
    API is required to build SOF with Waves codec.
    
    Signed-off-by: Oleksandr Strelchenko <oleksandr.strelchenko@waves.com>

---
+++ src/include/sof/audio/MaxxEffect/Process/MaxxEffect_Reset.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2021 Waves Audio Ltd. All rights reserved.
+ */
+
+#ifndef MAXX_EFFECT_RESET_H
+#define MAXX_EFFECT_RESET_H
+
+#include "MaxxEffect/MaxxEffect.h"
+#include "MaxxEffect/MaxxStatus.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*******************************************************************************
+ * Reset internal [states](@ref MaxxEffect_InternalData_States), and [meters]
+ * (@ref MaxxEffect_InternalData_Meters). Configuration remains the same.
+ * 
+ * @param[in] effect initialized effect
+ * 
+ * @return 0 on success, otherwise fail
+ ******************************************************************************/
+MaxxStatus_t    MaxxEffect_Reset(
+    MaxxEffect_t*           effect);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
