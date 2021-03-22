thesofproject/sof

commit f40222a1eaae027f2f0f651d66088db6d9cc62e5

drivers: imx: esai: use rate from topology
So far we only used topologies with rate set
to 48 000Hz. This might change in the future.
So use rate from parameters passed from kernel.

While here, align sof_ipc_dai_esai_params struct
with the one from kernel.

Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

src/drivers/imx/esai.c
@@ -41,6 +41,7 @@ struct esai_pdata {
 		uint32_t prrc;
 		uint32_t pcrc;
 	} regs;
+	struct sof_ipc_dai_esai_params params;
 };
 
 static int esai_context_store(struct dai *dai)
@@ -99,9 +100,13 @@ static inline int esai_set_config(struct dai *dai,
 				 struct sof_ipc_dai_config *config)
 {
 	uint32_t xcr = 0, xccr = 0, mask;
+	struct esai_pdata *esai = dai_get_drvdata(dai);
 
 	dai_dbg(dai, "ESAI: set_config format 0x%04x",
 		config->format);
+
+	esai->params = config->esai;
+
 	switch (config->format & SOF_DAI_FMT_FORMAT_MASK) {
 	case SOF_DAI_FMT_I2S:
 		/* Data on rising edge of bclk, frame low, 1clk before
@@ -436,11 +441,16 @@ static int esai_get_hw_params(struct dai *dai,
 			      struct sof_ipc_stream_params *params,
 			      int dir)
 {
+	struct esai_pdata *esai = dai_get_drvdata(dai);
+
 	/* ESAI only currently supports these parameters */
-	params->rate = 48000;
+	params->rate = esai->params.fsync_rate;
 	params->channels = 2;
 	params->buffer_fmt = 0;
 	params->frame_fmt = SOF_IPC_FRAME_S24_4LE;
+
+	dai_info(dai, "esai_get_hw_params(): params->rate = %d, fsync_rate = %d",
+		 params->rate, esai->params.fsync_rate);
 
 	return 0;
 }

src/include/ipc/dai-imx.h
@@ -18,8 +18,11 @@ struct sof_ipc_dai_esai_params {
 	/* MCLK */
 	uint16_t reserved1;
 	uint16_t mclk_id;
-	uint32_t mclk_rate; /* MCLK frequency in Hz */
 	uint32_t mclk_direction;
+
+	uint32_t mclk_rate; /* MCLK frequency in Hz */
+	uint32_t fsync_rate;
+	uint32_t bclk_rate;
 
 	/* TDM */
 	uint32_t tdm_slots;

commit 6492b2d2615842ffb7f3ca5856a7e021debd8dc5

drivers: imx: esai: set private data after allocation
ESAI allocates private data and retrieves this data using
dai_get_drvdata().
But this is never set using dai_set_drvdata() and sometimes
dai_get_drvdata() returns NULL.
So, after private data is allocated, set it using dai_set_drvdata().

Without this fix, on i.MX8QM, on playback we get:
root@imx8qmmek:~# aplay -Dhw:1,0 -f S16_LE -c 2 -r 48000 -d 5 -t raw /dev/urandom
Playing raw data '/dev/urandom' : Signed 16 bit Little Endian, Rate 48000 Hz, Stereo
[  197.478128] sof-audio-of 556e8000.dsp: error : DSP panic!
[  197.483547] sof-audio-of 556e8000.dsp: error: runtime exception
[  197.489473] sof-audio-of 556e8000.dsp: error: trace point 0dead006
[  197.495658] sof-audio-of 556e8000.dsp: error: panic at :0
[  197.501064] sof-audio-of 556e8000.dsp: error: DSP Firmware Oops
[...]

Fixes: f40222a1eaae ("drivers: imx: esai: use rate from topology")
Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

src/drivers/imx/esai.c
@@ -398,6 +398,8 @@ static int esai_probe(struct dai *dai)
 		dai_err(dai, "ESAI probe failure, out of memory");
 		return -ENOMEM;
 	}
+	dai_set_drvdata(dai, pdata);
+
 	/* ESAI core reset */
 	dai_write(dai, REG_ESAI_ECR, ESAI_ECR_ERST | ESAI_ECR_ESAIEN);
 	dai_write(dai, REG_ESAI_ECR, ESAI_ECR_ESAIEN);