0188af0abfbf53b7c226fdfbde9da51be3aa2fd1
Marc Herbert <marc.herbert@intel.com>
2021-03-30 22:51:42 +0000

    checkpatch: --ignore C99_COMMENT_TOLERANCE
    Let's not waste considerable time in code reviews with silly problems
    computers can solve.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- scripts/sof-post-commit-hook.sh
+++ scripts/sof-post-commit-hook.sh
@@ -3,4 +3,8 @@
 # Copyright(c) 2018 Intel Corporation. All rights reserved.
 
 set -e
-exec git show --format=email HEAD | ./scripts/checkpatch.pl --no-tree --strict --codespell
+# TODO: reduce duplication with scripts/sof-pre-commit-hook.sh
+# and with .github/workflows/ with either some .conf file
+# or some wrapper script
+exec git show --format=email HEAD |
+  ./scripts/checkpatch.pl --no-tree --strict --codespell --ignore C99_COMMENT_TOLERANCE
