be6ff59ecb3c5cd2809cb3b8fdb9fcb1e3c3d1f9
Iuliana Prodan <iuliana.prodan@nxp.com>
2021-04-07 20:53:50 +0300

    src: update coefficients for tiny profile, add 96 kHz support
    This patch adds 48 <-> 96 kHz rate conversion for tiny profile.
    
    The tiny coefficients are trimmed down in computational complexity
    to just run on some platforms as gcc compiled to allow
    debugging and validation of the most complex conversions.
    
    I believe this should also include 48 <-> 96 kHz rate conversion.
    
    The files are generated with Octave, using src_tiny_int16.m and
    the instructions from
    https://thesofproject.github.io/latest/algos/src/sample_rate_conversion.html
    
    Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

--- src/include/sof/audio/coefficients/src/src_tiny_int16_table.h
+++ src/include/sof/audio/coefficients/src/src_tiny_int16_table.h
@@ -9,8 +9,10 @@
 #define __SOF_AUDIO_COEFFICIENTS_SRC_SRC_TINY_INT16_TABLE_H__
 
 /* SRC conversions */
+#include <sof/audio/coefficients/src/src_tiny_int16_1_2_1814_5000.h>
 #include <sof/audio/coefficients/src/src_tiny_int16_1_3_1814_5000.h>
 #include <sof/audio/coefficients/src/src_tiny_int16_1_6_1814_5000.h>
+#include <sof/audio/coefficients/src/src_tiny_int16_2_1_1814_5000.h>
 #include <sof/audio/coefficients/src/src_tiny_int16_2_3_1814_5000.h>
 #include <sof/audio/coefficients/src/src_tiny_int16_3_1_1814_5000.h>
 #include <sof/audio/coefficients/src/src_tiny_int16_3_2_1814_5000.h>
@@ -28,59 +30,74 @@
 int16_t fir_one = 16384;
 struct src_stage src_int16_1_1_0_0 =  { 0, 0, 1, 1, 1, 1, 1, 0, -1, &fir_one };
 struct src_stage src_int16_0_0_0_0 =  { 0, 0, 0, 0, 0, 0, 0, 0,  0, &fir_one };
-int src_in_fs[6] = { 8000, 16000, 32000, 44100, 48000, 50000};
-int src_out_fs[6] = { 8000, 16000, 32000, 44100, 48000, 50000};
-struct src_stage *src_table1[6][6] = {
+int src_in_fs[7] = { 8000, 16000, 32000, 44100, 48000, 50000, 96000};
+int src_out_fs[7] = { 8000, 16000, 32000, 44100, 48000, 50000, 96000};
+struct src_stage *src_table1[7][7] = {
 	{ &src_int16_1_1_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_1_6_1814_5000, &src_int16_0_0_0_0
-	},
-	{ &src_int16_0_0_0_0, &src_int16_1_1_0_0,
-	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_1_3_1814_5000, &src_int16_0_0_0_0
+	 &src_int16_1_6_1814_5000, &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0},
+	{ &src_int16_0_0_0_0,
+	 &src_int16_1_1_0_0, &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0, &src_int16_1_3_1814_5000,
+	 &src_int16_0_0_0_0, &src_int16_0_0_0_0
 	},
 	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
 	 &src_int16_1_1_0_0, &src_int16_0_0_0_0,
-	 &src_int16_2_3_1814_5000, &src_int16_0_0_0_0
-	},
-	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_0_0_0_0, &src_int16_1_1_0_0,
-	 &src_int16_21_20_1667_5000, &src_int16_0_0_0_0
+	 &src_int16_2_3_1814_5000, &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0},
+	{ &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
+	 &src_int16_1_1_0_0, &src_int16_21_20_1667_5000,
+	 &src_int16_0_0_0_0, &src_int16_0_0_0_0
 	},
 	{ &src_int16_6_1_1814_5000, &src_int16_3_1_1814_5000,
 	 &src_int16_3_2_1814_5000, &src_int16_8_7_1814_5000,
-	 &src_int16_1_1_0_0, &src_int16_24_25_1814_5000
+	 &src_int16_1_1_0_0, &src_int16_24_25_1814_5000,
+	 &src_int16_1_2_1814_5000},
+	{ &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0, &src_int16_25_24_1814_5000,
+	 &src_int16_1_1_0_0, &src_int16_0_0_0_0
 	},
 	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_25_24_1814_5000, &src_int16_1_1_0_0
-	}
+	 &src_int16_2_1_1814_5000, &src_int16_0_0_0_0,
+	 &src_int16_1_1_0_0}
 };
-struct src_stage *src_table2[6][6] = {
+
+struct src_stage *src_table2[7][7] = {
 	{ &src_int16_1_1_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_1_1_0_0, &src_int16_0_0_0_0
-	},
-	{ &src_int16_0_0_0_0, &src_int16_1_1_0_0,
-	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_1_1_0_0, &src_int16_0_0_0_0
+	 &src_int16_1_1_0_0, &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0},
+	{ &src_int16_0_0_0_0,
+	 &src_int16_1_1_0_0, &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0, &src_int16_1_1_0_0,
+	 &src_int16_0_0_0_0, &src_int16_0_0_0_0
 	},
 	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
 	 &src_int16_1_1_0_0, &src_int16_0_0_0_0,
-	 &src_int16_1_1_0_0, &src_int16_0_0_0_0
-	},
-	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_0_0_0_0, &src_int16_1_1_0_0,
-	 &src_int16_7_8_1814_5000, &src_int16_0_0_0_0
+	 &src_int16_1_1_0_0, &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0},
+	{ &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
+	 &src_int16_1_1_0_0, &src_int16_7_8_1814_5000,
+	 &src_int16_0_0_0_0, &src_int16_0_0_0_0
 	},
 	{ &src_int16_1_1_0_0, &src_int16_1_1_0_0,
 	 &src_int16_1_1_0_0, &src_int16_20_21_1667_5000,
-	 &src_int16_1_1_0_0, &src_int16_1_1_0_0
+	 &src_int16_1_1_0_0, &src_int16_1_1_0_0,
+	 &src_int16_1_1_0_0},
+	{ &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
+	 &src_int16_0_0_0_0, &src_int16_1_1_0_0,
+	 &src_int16_1_1_0_0, &src_int16_0_0_0_0
 	},
 	{ &src_int16_0_0_0_0, &src_int16_0_0_0_0,
 	 &src_int16_0_0_0_0, &src_int16_0_0_0_0,
-	 &src_int16_1_1_0_0, &src_int16_1_1_0_0
-	}
+	 &src_int16_1_1_0_0, &src_int16_0_0_0_0,
+	 &src_int16_1_1_0_0}
 };
 
 #endif /* __SOF_AUDIO_COEFFICIENTS_SRC_SRC_TINY_INT16_TABLE_H__ */
