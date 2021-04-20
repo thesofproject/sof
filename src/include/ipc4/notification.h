d11ce22bcb2406b59f20eb7ee6cdc2efba707884
Rander Wang <rander.wang@intel.com>
2021-04-20 15:14:02 +0800

    ipc4: add fw ready support for ipc4
    Signed-off-by: Rander Wang <rander.wang@intel.com>

--- src/include/ipc4/notification.h
+++ src/include/ipc4/notification.h
@@ -8,10 +8,11 @@
  * \brief IPC4 notification definitions
  */
 
-#ifndef __IPC4_MODULE_H__
-#define __IPC4_MODULE_H__
+#ifndef __IPC4_NOTIFICATION_H__
+#define __IPC4_NOTIFICATION_H__
 
 #include <stdint.h>
+#include <ipc4/header.h>
 
 /* ipc4 notification msg */
 
@@ -32,15 +33,11 @@ enum sof_ipc4_notification_type {
 
 #define SOF_IPC4_GLB_NOTIFY_DIR_MASK	BIT(29)
 #define SOF_IPC4_REPLY_STATUS_MASK	0xFFFFFF
-
 #define SOF_IPC4_GLB_NOTIFY_TYPE_SHIFT	16
-#define SOF_IPC4_GLB_NOTIFY_TYPE_MASK	0xFF
-#define SOF_IPC4_GLB_NOTIFY_TYPE(x)		(((x) >> SOF_IPC4_GLB_NOTIFY_TYPE_SHIFT) \
-						& SOF_IPC4_GLB_NOTIFY_TYPE_MASK)
-
 #define SOF_IPC4_GLB_NOTIFY_MSG_TYPE_SHIFT	24
-#define SOF_IPC4_GLB_NOTIFY_MSG_TYPE_MASK	0x1F
-#define SOF_IPC4_GLB_NOTIFY_MSG_TYPE(x)	(((x) >> SOF_IPC4_GLB_NOTIFY_MSG_TYPE_SHIFT)	\
-						& SOF_IPC4_GLB_NOTIFY_MSG_TYPE_MASK)
 
+
+#define SOF_IPC4_FW_READY \
+		(((SOF_IPC4_NOTIFY_FW_READY) << (SOF_IPC4_GLB_NOTIFY_TYPE_SHIFT)) |\
+		((SOF_IPC4_GLB_NOTIFICATION) << (SOF_IPC4_GLB_NOTIFY_MSG_TYPE_SHIFT)))
 #endif
