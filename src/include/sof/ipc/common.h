c6b63cf1ec06a5d1ccfe49f0f77477ae3f600a24
Rander Wang <rander.wang@intel.com>
2021-04-26 13:05:59 +0800

    ipc: add fw ready support for ipc4
    Signed-off-by: Rander Wang <rander.wang@intel.com>

--- src/include/sof/ipc/common.h
+++ src/include/sof/ipc/common.h
@@ -140,6 +140,13 @@ int ipc_process_host_buffer(struct ipc *ipc,
 int ipc_dma_trace_send_position(void);
 
 /**
+ * \brief create a IPC boot completet message.
+ * @header[in] msg header.
+ * @data[in] msg data.
+ */
+void ipc_boot_complete_msg(ipc_cmd_hdr *header, uint32_t *data);
+
+/**
  * \brief Read a compact IPC message or return NULL for normal message.
  * @return Pointer to the compact message data.
  */
