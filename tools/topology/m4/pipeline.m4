thesofproject/sof

commit 128098799978a178040194ae4134bf12053b2d52

topology: use DAI_PERIODS in calculation of DAI buffer size
Uses DAI_PERIODS in calculation of DAI buffer size.
In case the DAI_PERIODS value is undefined, we use
DAI_DEFAULT_PERIOD value of 2.

Signed-off-by: Tomasz Lauda <tomasz.lauda@linux.intel.com>

tools/topology/m4/pipeline.m4
@@ -4,6 +4,9 @@ divert(-1)
 define(`SCHEDULE_TIME_DOMAIN_DMA', 0)
 define(`SCHEDULE_TIME_DOMAIN_TIMER', 1)
 
+# default number of DAI periods
+define(`DAI_DEFAULT_PERIODS', 2)
+
 dnl Define macro for pipeline widget
 
 dnl Pipeline name)
@@ -55,6 +58,7 @@ define(`PIPELINE_PCM_ADD',
 `undefine(`SCHEDULE_TIME_DOMAIN')'
 `undefine(`DAI_FORMAT')'
 `undefine(`SCHED_COMP')'
+`undefine(`DAI_PERIODS')'
 `define(`PIPELINE_ID', $2)'
 `define(`PCM_ID', $3)'
 `define(`PIPELINE_CHANNELS', $4)'
@@ -68,6 +72,7 @@ define(`PIPELINE_PCM_ADD',
 `define(`SCHEDULE_TIME_DOMAIN', $12)'
 `define(`DAI_FORMAT', $5)'
 `define(`SCHED_COMP', $13)'
+`define(`DAI_PERIODS', DAI_DEFAULT_PERIODS)'
 `include($1)'
 `DEBUG_PCM_ADD($1, $3)'
 ,`fatal_error(`Invalid parameters ($#) to PIPELINE_PCM_ADD')')'

tools/topology/sof/pipe-eq-capture-16khz.m4
@@ -68,8 +68,8 @@ W_PGA(0, PIPELINE_FORMAT, 2, 2,
 	 capture_pga_conf, LIST(`		',
 		"CONTROL_NAME"))
 
-# "EQ 0" has 2 sink period and 2 source periods
-W_EQ_IIR(0, PIPELINE_FORMAT, 2, 2, LIST(`		', "EQIIR_C16"))
+# "EQ 0" has 2 sink period and x source periods
+W_EQ_IIR(0, PIPELINE_FORMAT, 2, DAI_PERIODS, LIST(`		', "EQIIR_C16"))
 
 # Capture Buffers
 W_BUFFER(0, COMP_BUFFER_SIZE(2,
@@ -80,7 +80,7 @@ W_BUFFER(1, COMP_BUFFER_SIZE(2,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS,
 	eval(PCM_MAX_RATE / SCHEDULE_PERIOD)), PLATFORM_PASS_MEM_CAP)
 
-W_BUFFER(2, COMP_BUFFER_SIZE(2,
+W_BUFFER(2, COMP_BUFFER_SIZE(DAI_PERIODS,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS,
 	eval(PCM_MAX_RATE / SCHEDULE_PERIOD)), PLATFORM_PASS_MEM_CAP)
 

tools/topology/sof/pipe-eq-capture.m4
@@ -70,8 +70,8 @@ W_PGA(0, PIPELINE_FORMAT, 2, 2,
 	 capture_pga_conf, LIST(`		',
 		"CONTROL_NAME"))
 
-# "EQ 0" has 2 sink period and 2 source periods
-W_EQ_IIR(0, PIPELINE_FORMAT, 2, 2, LIST(`		', "EQIIR_C48"))
+# "EQ 0" has 2 sink period and x source periods
+W_EQ_IIR(0, PIPELINE_FORMAT, 2, DAI_PERIODS, LIST(`		', "EQIIR_C48"))
 
 # Capture Buffers
 W_BUFFER(0, COMP_BUFFER_SIZE(2,
@@ -82,7 +82,7 @@ W_BUFFER(1, COMP_BUFFER_SIZE(2,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS,
 	COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)), PLATFORM_PASS_MEM_CAP)
 
-W_BUFFER(2, COMP_BUFFER_SIZE(2,
+W_BUFFER(2, COMP_BUFFER_SIZE(DAI_PERIODS,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS,
 	COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)), PLATFORM_PASS_MEM_CAP)
 

tools/topology/sof/pipe-eq-fir-volume-playback.m4
@@ -57,8 +57,8 @@ C_CONTROLBYTES(EQFIR, PIPELINE_ID,
 # with 2 sink and 0 source periods
 W_PCM_PLAYBACK(PCM_ID, Passthrough Playback, 2, 0)
 
-# "Volume" has 2 source and 2 sink periods
-W_PGA(0, PIPELINE_FORMAT, 2, 2, playback_pga_conf, LIST(`		', "PIPELINE_ID Master Playback Volume"))
+# "Volume" has 2 source and x sink periods
+W_PGA(0, PIPELINE_FORMAT, DAI_PERIODS, 2, playback_pga_conf, LIST(`		', "PIPELINE_ID Master Playback Volume"))
 
 # "EQ 0" has 2 sink period and 2 source periods
 W_EQ_FIR(0, PIPELINE_FORMAT, 2, 2, LIST(`		', "EQFIR"))
@@ -70,7 +70,7 @@ W_BUFFER(0, COMP_BUFFER_SIZE(2,
 W_BUFFER(1, COMP_BUFFER_SIZE(2,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS, COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)),
 	PLATFORM_HOST_MEM_CAP)
-W_BUFFER(2, COMP_BUFFER_SIZE(2,
+W_BUFFER(2, COMP_BUFFER_SIZE(DAI_PERIODS,
 	COMP_SAMPLE_SIZE(DAI_FORMAT), PIPELINE_CHANNELS, COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)),
 	PLATFORM_DAI_MEM_CAP)
 

tools/topology/sof/pipe-eq-iir-volume-playback.m4
@@ -57,8 +57,8 @@ C_CONTROLBYTES(EQIIR, PIPELINE_ID,
 # with 2 sink and 0 source periods
 W_PCM_PLAYBACK(PCM_ID, Passthrough Playback, 2, 0)
 
-# "Volume" has 2 source and 2 sink periods
-W_PGA(0, PIPELINE_FORMAT, 2, 2, playback_pga_conf, LIST(`		', "PIPELINE_ID Master Playback Volume"))
+# "Volume" has 2 source and x sink periods
+W_PGA(0, PIPELINE_FORMAT, DAI_PERIODS, 2, playback_pga_conf, LIST(`		', "PIPELINE_ID Master Playback Volume"))
 
 # "EQ 0" has 2 sink period and 2 source periods
 W_EQ_IIR(0, PIPELINE_FORMAT, 2, 2, LIST(`		', "EQIIR"))
@@ -70,7 +70,7 @@ W_BUFFER(0, COMP_BUFFER_SIZE(2,
 W_BUFFER(1, COMP_BUFFER_SIZE(2,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS, COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)),
 	PLATFORM_HOST_MEM_CAP)
-W_BUFFER(2, COMP_BUFFER_SIZE(2,
+W_BUFFER(2, COMP_BUFFER_SIZE(DAI_PERIODS,
 	COMP_SAMPLE_SIZE(DAI_FORMAT), PIPELINE_CHANNELS, COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)),
 	PLATFORM_DAI_MEM_CAP)
 

tools/topology/sof/pipe-eq-volume-playback.m4
@@ -71,8 +71,8 @@ C_CONTROLBYTES(EQFIR, PIPELINE_ID,
 # with 2 sink and 0 source periods
 W_PCM_PLAYBACK(PCM_ID, Passthrough Playback, 2, 0)
 
-# "Volume" has 2 source and 2 sink periods
-W_PGA(0, PIPELINE_FORMAT, 2, 2, playback_pga_conf, LIST(`		', "PIPELINE_ID Master Playback Volume"))
+# "Volume" has 2 source and x sink periods
+W_PGA(0, PIPELINE_FORMAT, DAI_PERIODS, 2, playback_pga_conf, LIST(`		', "PIPELINE_ID Master Playback Volume"))
 
 # "EQ 0" has 2 sink period and 2 source periods
 W_EQ_IIR(0, PIPELINE_FORMAT, 2, 2, LIST(`		', "EQIIR"))
@@ -90,7 +90,7 @@ W_BUFFER(1, COMP_BUFFER_SIZE(2,
 W_BUFFER(2, COMP_BUFFER_SIZE(2,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS, COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)),
 	PLATFORM_HOST_MEM_CAP)
-W_BUFFER(3, COMP_BUFFER_SIZE(2,
+W_BUFFER(3, COMP_BUFFER_SIZE(DAI_PERIODS,
 	COMP_SAMPLE_SIZE(DAI_FORMAT), PIPELINE_CHANNELS, COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)),
 	PLATFORM_DAI_MEM_CAP)
 

tools/topology/sof/pipe-kfbm-capture.m4
@@ -71,14 +71,14 @@ W_DATA(capture_pga_conf, capture_pga_tokens)
 # with 0 sink and 2 source periods
 W_PCM_CAPTURE(PCM_ID, Sound Trigger Capture, 0, 2, 2)
 
-# "Volume" has 2 source and 2 sink periods
-W_PGA(0, PIPELINE_FORMAT, 2, 2, capture_pga_conf, LIST(`		', "PIPELINE_ID KWD Capture Volume"))
+# "Volume" has x source and 2 sink periods
+W_PGA(0, PIPELINE_FORMAT, 2, DAI_PERIODS, capture_pga_conf, LIST(`		', "PIPELINE_ID KWD Capture Volume"))
 
 # "KPBM" has 2 source and 2 sink periods
 W_KPBM(0, PIPELINE_FORMAT, 2, 2, PIPELINE_ID, LIST(`             ', "KPB"))
 
 # Capture Buffers
-W_BUFFER(0, COMP_BUFFER_SIZE(2,
+W_BUFFER(0, COMP_BUFFER_SIZE(DAI_PERIODS,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS, COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)),
 	PLATFORM_DAI_MEM_CAP)
 # Capture Buffers

tools/topology/sof/pipe-low-latency-capture.m4
@@ -41,11 +41,11 @@ W_DATA(capture_pga_conf, capture_pga_tokens)
 # with 0 sink and 2 source periods
 W_PCM_CAPTURE(PCM_ID, Low Latency Capture, 0, 2)
 
-# "Capture Volume" has 2 sink and source periods for host and DAI ping-pong
-W_PGA(0, PIPELINE_FORMAT, 2, 2, capture_pga_conf, LIST(`		', "PIPELINE_ID PCM PCM_ID Capture Volume"))
+# "Capture Volume" has 2 sink and x source periods for host and DAI ping-pong
+W_PGA(0, PIPELINE_FORMAT, 2, DAI_PERIODS, capture_pga_conf, LIST(`		', "PIPELINE_ID PCM PCM_ID Capture Volume"))
 
 # Capture Buffers
-W_BUFFER(0, COMP_BUFFER_SIZE(2,
+W_BUFFER(0, COMP_BUFFER_SIZE(DAI_PERIODS,
 	COMP_SAMPLE_SIZE(PIPELINE_FORMAT), PIPELINE_CHANNELS, COMP_PERIOD_FRAMES(PCM_MAX_RATE, SCHEDULE_PERIOD)),
 	PLATFORM_DAI_MEM_CAP)
 W_BUFFER(1, COMP_BUFFER_SIZE(2,

commit 8f28f727871036ba7e422f0239cf32e3858382f4

topology: Allow overriding DAI_PERIODS
This will allow topologies to use different size dai periods.

Fixes: 128098799978a1 ("topology: use DAI_PERIODS in calculation of DAI buffer size")
Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

tools/topology/m4/pipeline.m4
@@ -59,7 +59,6 @@ define(`PIPELINE_PCM_ADD',
 `undefine(`SCHEDULE_TIME_DOMAIN')'
 `undefine(`DAI_FORMAT')'
 `undefine(`SCHED_COMP')'
-`undefine(`DAI_PERIODS')'
 `undefine(`DYNAMIC_PIPE')'
 `define(`PIPELINE_ID', $2)'
 `define(`PCM_ID', $3)'
@@ -75,7 +74,7 @@ define(`PIPELINE_PCM_ADD',
 `define(`DAI_FORMAT', $5)'
 `define(`SCHED_COMP', $13)'
 `define(`DYNAMIC_PIPE', $14)'
-`define(`DAI_PERIODS', DAI_DEFAULT_PERIODS)'
+`ifdef(`DAI_PERIODS', `', `define(`DAI_PERIODS', DAI_DEFAULT_PERIODS)')'
 `include($1)'
 `DEBUG_PCM_ADD($1, $3)'
 ,`fatal_error(`Invalid parameters ($#) to PIPELINE_PCM_ADD')')'