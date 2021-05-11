92c676338be297924d673375c298a018213a0367
Fred Oh <fred.oh@linux.intel.com>
2021-05-11 11:14:31 -0700

    Dockerfile: remove apt.conf dependency
    There was a assumption /etc/apt/apt.conf has apt proxy settings for
    the system. This workaround is not required.
    
    Signed-off-by: Fred Oh <fred.oh@linux.intel.com>

--- scripts/docker_build/sof_qemu/docker-build.sh
+++ scripts/docker_build/sof_qemu/docker-build.sh
@@ -2,9 +2,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright(c) 2018 Intel Corporation. All rights reserved.
 
-if [ -f "/etc/apt/apt.conf" ]; then
-	cp /etc/apt/apt.conf ./
-else
-	touch apt.conf
-fi
-docker build --build-arg UID=$(id -u) --build-arg host_http_proxy=$http_proxy --build-arg host_https_proxy=$https_proxy -t sofqemu .
+docker build --build-arg UID=$(id -u) --build-arg host_http_proxy="$http_proxy" \
+	--build-arg host_https_proxy="$https_proxy" -t sofqemu .
