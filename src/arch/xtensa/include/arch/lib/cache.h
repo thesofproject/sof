thesofproject/sof

commit bfe7707f37de744d3e2a44b6debedbde6c784501

cavs: disable data cache operations on uncached addresses
Trying to write back cache on uncached addresses can overwrite data.
Invalidating cache should have no ill effects but isn't very
meaningful either. Disable both.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/arch/xtensa/include/arch/lib/cache.h
@@ -20,10 +20,15 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define SRAM_UNCACHED_ALIAS	0x20000000
+
+#define is_cached(address) (!!((uintptr_t)(address) & SRAM_UNCACHED_ALIAS))
+
 static inline void dcache_writeback_region(void *addr, size_t size)
 {
 #if XCHAL_DCACHE_SIZE > 0
-	xthal_dcache_region_writeback(addr, size);
+	if (is_cached(addr))
+		xthal_dcache_region_writeback(addr, size);
 #endif
 }
 
@@ -37,7 +42,8 @@ static inline void dcache_writeback_all(void)
 static inline void dcache_invalidate_region(void *addr, size_t size)
 {
 #if XCHAL_DCACHE_SIZE > 0
-	xthal_dcache_region_invalidate(addr, size);
+	if (is_cached(addr))
+		xthal_dcache_region_invalidate(addr, size);
 #endif
 }
 
@@ -65,7 +71,8 @@ static inline void icache_invalidate_all(void)
 static inline void dcache_writeback_invalidate_region(void *addr, size_t size)
 {
 #if XCHAL_DCACHE_SIZE > 0
-	xthal_dcache_region_writeback_inv(addr, size);
+	if (is_cached(addr))
+		xthal_dcache_region_writeback_inv(addr, size);
 #endif
 }
 

src/platform/intel/cavs/include/cavs/lib/memory.h
@@ -85,7 +85,7 @@ struct sof;
 
 #define SRAM_ALIAS_BASE		0x9E000000
 #define SRAM_ALIAS_MASK		0xFF000000
-#define SRAM_ALIAS_OFFSET	0x20000000
+#define SRAM_ALIAS_OFFSET	SRAM_UNCACHED_ALIAS
 
 #if !defined UNIT_TEST && !defined __ZEPHYR__
 #define uncache_to_cache(address) \

commit 64b1a0cce6fd5ccd07e392818470e21ad316fcb0

imx: invalidate cache for specific address range only
The _memmap_cacheattr_reset linker script variable has
dedicate cache attribute for every 512M in 4GB space
1: write through
2: cache bypass
4: write back
F: invalid access

For imx, implement is_cached() based on
_memmap_cacheattr_reset.

Without this fix, when loading a topology we get:
sof-audio-of 556e8000.dsp: error: reply expected 20 got 12 bytes
sof-audio-of 556e8000.dsp: error: ipc error for 0x30010000 size 20
sof-audio-of 556e8000.dsp: error: DSP failed to add widget id 0 type 23 name : PCM0P stream Passthrough Playback 0 reply 0
sof-audio-of 556e8000.dsp: ASoC: failed to load widget PCM0P
sof-audio-of 556e8000.dsp: ASoC: topology: could not load header: -22
sof-audio-of 556e8000.dsp: error: tplg component load failed -22
sof-audio-of 556e8000.dsp: error: failed to load DSP topology -22
sof-audio-of 556e8000.dsp: ASoC: error at snd_soc_component_probe on 556e8000.dsp: -22
sof-audio-of 556e8000.dsp: ASoC: failed to probe component -22
asoc-simple-card sof-sound-wm8960: ASoC: failed to instantiate card -22
asoc-simple-card: probe of sof-sound-wm8960 failed with error -22

That's because, on mailbox_hostbox_read() we don't invalidate cache
before reading the address.

Fixes: bfe7707f37de ("cavs: disable data cache operations on uncached addresses")
Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

src/arch/xtensa/include/arch/lib/cache.h
@@ -22,7 +22,55 @@
 
 #define SRAM_UNCACHED_ALIAS	0x20000000
 
+#ifdef CONFIG_IMX
+/*
+ * The _memmap_cacheattr_reset linker script variable has
+ * dedicate cache attribute for every 512M in 4GB space
+ * 1: write through
+ * 2: cache bypass
+ * 4: write back
+ * F: invalid access
+ */
+extern uint32_t _memmap_cacheattr_reset;
+
+/*
+ * Since each hex digit keeps the attributes for a 512MB region,
+ * we have the following address ranges:
+ * Address range       - hex digit
+ * 0        - 1FFFFFFF - 0
+ * 20000000 - 3FFFFFFF - 1
+ * 40000000 - 5FFFFFFF - 2
+ * 60000000 - 7FFFFFFF - 3
+ * 80000000 - 9FFFFFFF - 4
+ * A0000000 - BFFFFFFF - 5
+ * C0000000 - DFFFFFFF - 6
+ * E0000000 - FFFFFFFF - 7
+ */
+
+/*
+ * Based on the above information, get the address region id (0-7)
+ */
+#define _addr_range(address) (((uintptr_t)(address) >> 29) & 0x7)
+/*
+ * Get the position of the cache attribute for a certain memory region.
+ * There are 4 bits per hex digit.
+ */
+#define _addr_shift(address) ((_addr_range(address)) << 2)
+/*
+ * For the given address, get the corresponding hex digit
+ * from the linker script variable that contains the cache attributes
+ */
+#define _addr_attr(address) ((((uint32_t)(&_memmap_cacheattr_reset)) >> \
+			     (_addr_shift(address))) & 0xF)
+/*
+ * Check if the address is cacheable or not, by verifying the _addr_attr,
+ * which for cacheable addresses might be 1 or 4
+ */
+#define is_cached(address) ((_addr_attr(address) == 1) || \
+			    (_addr_attr(address) == 4))
+#else
 #define is_cached(address) (!!((uintptr_t)(address) & SRAM_UNCACHED_ALIAS))
+#endif
 
 static inline void dcache_writeback_region(void *addr, size_t size)
 {