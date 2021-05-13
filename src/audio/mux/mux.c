thesofproject/sof

commit 80666b7cc7a9c70652a55086034d2bfcd35b3b76

ipc: component: abstract the component creation API
Make sure component creation is not tightly coupled to a particular IPC
version. This is mostly a mechanical change of structures being passed
to the comp creation APIs away from IPC specific to general structures.

Highlevel changes

1) Pass a common component data object and a component
specific data object during create().

2) Mark the component IPC derived data as "ipc_config" within the
component device to help developers track the data source origin.

3) Pass component specific data during creation so that componets
can allocate and copy to thier private data.

4) Comp_dev no longer has component specific data appended to it.
Instead we can store all in the comp private data (and hence use the
compiler to access it rather than by developer access methods).

Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

src/audio/mux/mux.c
@@ -11,6 +11,7 @@
 
 #include <sof/audio/component.h>
 #include <sof/audio/mux.h>
+#include <sof/audio/ipc-config.h>
 #include <sof/common.h>
 #include <sof/ipc/msg.h>
 #include <sof/lib/alloc.h>
@@ -127,7 +128,7 @@ static int mux_set_values(struct comp_dev *dev, struct comp_data *cd,
 		}
 	}
 
-	if (dev->comp.type == SOF_COMP_MUX) {
+	if (dev->ipc_config.type == SOF_COMP_MUX) {
 		if (mux_mix_check(cfg))
 			comp_cl_err(&comp_mux, "mux_set_values(): mux component is not able to mix channels");
 	}
@@ -140,13 +141,13 @@ static int mux_set_values(struct comp_dev *dev, struct comp_data *cd,
 
 	cd->config.num_streams = cfg->num_streams;
 
-	if (dev->comp.type == SOF_COMP_MUX)
+	if (dev->ipc_config.type == SOF_COMP_MUX)
 		mux_prepare_look_up_table(dev);
 	else
 		demux_prepare_look_up_table(dev);
 
 	if (dev->state > COMP_STATE_INIT) {
-		if (dev->comp.type == SOF_COMP_MUX)
+		if (dev->ipc_config.type == SOF_COMP_MUX)
 			cd->mux = mux_get_processing_function(dev);
 		else
 			cd->demux = demux_get_processing_function(dev);
@@ -156,10 +157,10 @@ static int mux_set_values(struct comp_dev *dev, struct comp_data *cd,
 }
 
 static struct comp_dev *mux_new(const struct comp_driver *drv,
-				struct sof_ipc_comp *comp)
+				struct comp_ipc_config *config,
+				void *spec)
 {
-	struct sof_ipc_comp_process *ipc_process =
-		(struct sof_ipc_comp_process *)comp;
+	struct ipc_config_process *ipc_process = spec;
 	size_t bs = ipc_process->size;
 	struct comp_dev *dev;
 	struct comp_data *cd;
@@ -167,13 +168,10 @@ static struct comp_dev *mux_new(const struct comp_driver *drv,
 
 	comp_cl_info(&comp_mux, "mux_new()");
 
-	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_process));
+	dev = comp_alloc(drv, sizeof(*dev));
 	if (!dev)
 		return NULL;
-
-	memcpy_s(COMP_GET_IPC(dev, sof_ipc_comp_process),
-		 sizeof(struct sof_ipc_comp_process),
-		 comp, sizeof(struct sof_ipc_comp_process));
+	dev->ipc_config = *config;
 
 	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM,
 		     sizeof(*cd) + MUX_MAX_STREAMS * sizeof(struct mux_stream_data));
@@ -215,9 +213,11 @@ static uint8_t get_stream_index(struct comp_data *cd, uint32_t pipe_id)
 {
 	int i;
 
-	for (i = 0; i < MUX_MAX_STREAMS; i++)
+	for (i = 0; i < MUX_MAX_STREAMS; i++) {
+		comp_cl_err(&comp_mux, "got %u", cd->config.streams[i].pipeline_id);
 		if (cd->config.streams[i].pipeline_id == pipe_id)
 			return i;
+	}
 
 	comp_cl_err(&comp_mux, "get_stream_index(): couldn't find configuration for connected pipeline %u",
 		    pipe_id);
@@ -643,7 +643,7 @@ static int mux_prepare(struct comp_dev *dev)
 	comp_info(dev, "mux_prepare()");
 
 	if (dev->state != COMP_STATE_ACTIVE) {
-		if (dev->comp.type == SOF_COMP_MUX)
+		if (dev->ipc_config.type == SOF_COMP_MUX)
 			cd->mux = mux_get_processing_function(dev);
 		else
 			cd->demux = demux_get_processing_function(dev);

src/audio/asrc/asrc.c
@@ -7,6 +7,7 @@
 #include <sof/audio/component.h>
 #include <sof/audio/format.h>
 #include <sof/audio/pipeline.h>
+#include <sof/audio/ipc-config.h>
 #include <sof/debug/panic.h>
 #include <sof/ipc/msg.h>
 #include <sof/lib/alloc.h>
@@ -58,6 +59,7 @@ struct comp_data {
 	struct asrc_farrow *asrc_obj;	/* ASRC core data */
 	struct comp_dev *dai_dev;	/* Associated DAI component */
 	enum asrc_operation_mode mode;  /* Control for push or pull mode */
+	struct ipc_config_asrc ipc_config;
 	uint64_t ts;
 	uint32_t sink_rate;	/* Sample rate in Hz */
 	uint32_t source_rate;	/* Sample rate in Hz */
@@ -247,13 +249,12 @@ static void src_copy_s16(struct comp_dev *dev,
 }
 
 static struct comp_dev *asrc_new(const struct comp_driver *drv,
-				 struct sof_ipc_comp *comp)
+				 struct comp_ipc_config *config,
+				 void *spec)
 {
 	struct comp_dev *dev;
-	struct sof_ipc_comp_asrc *asrc;
-	struct sof_ipc_comp_asrc *ipc_asrc = (struct sof_ipc_comp_asrc *)comp;
+	struct ipc_config_asrc *ipc_asrc = spec;
 	struct comp_data *cd;
-	int err;
 
 	comp_cl_info(&comp_asrc, "asrc_new()");
 
@@ -267,14 +268,10 @@ static struct comp_dev *asrc_new(const struct comp_driver *drv,
 		return NULL;
 	}
 
-	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_asrc));
+	dev = comp_alloc(drv, sizeof(*dev));
 	if (!dev)
 		return NULL;
-
-	asrc = COMP_GET_IPC(dev, sof_ipc_comp_asrc);
-	err = memcpy_s(asrc, sizeof(*asrc), ipc_asrc,
-		       sizeof(struct sof_ipc_comp_asrc));
-	assert(!err);
+	dev->ipc_config = *config;
 
 	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*cd));
 	if (!cd) {
@@ -283,18 +280,19 @@ static struct comp_dev *asrc_new(const struct comp_driver *drv,
 	}
 
 	comp_set_drvdata(dev, cd);
+	cd->ipc_config = *ipc_asrc;
 
 	/* Get operation mode:
 	 * With OM_PUSH (0) use fixed input frames count, variable output.
 	 * With OM_PULL (1) use fixed output frames count, variable input.
 	 */
-	cd->mode = asrc->operation_mode;
+	cd->mode = ipc_asrc->operation_mode;
 
 	/* Use skew tracking for DAI if it was requested. The skew
 	 * is initialized here to zero. It is set later in prepare() to
 	 * to 1.0 if there is no filtered skew factor from previous run.
 	 */
-	cd->track_drift = asrc->asynchronous_mode;
+	cd->track_drift = ipc_asrc->asynchronous_mode;
 	cd->skew = 0;
 
 	dev->state = COMP_STATE_READY;
@@ -337,7 +335,7 @@ static int asrc_cmd(struct comp_dev *dev, int cmd, void *data,
 static int asrc_verify_params(struct comp_dev *dev,
 			      struct sof_ipc_stream_params *params)
 {
-	struct sof_ipc_comp_asrc *asrc = COMP_GET_IPC(dev, sof_ipc_comp_asrc);
+	struct comp_data *cd = comp_get_drvdata(dev);
 	int ret;
 
 	comp_dbg(dev, "asrc_verify_params()");
@@ -348,13 +346,15 @@ static int asrc_verify_params(struct comp_dev *dev,
 	 * src->source/sink_rate = 0 means that source/sink rate can vary.
 	 */
 	if (dev->direction == SOF_IPC_STREAM_PLAYBACK) {
-		if (params->rate != asrc->source_rate && asrc->source_rate) {
-			comp_err(dev, "asrc_verify_params(): runtime stream pcm rate does not match rate fetched from ipc.");
+		if (cd->ipc_config.source_rate && (params->rate != cd->ipc_config.source_rate)) {
+			comp_err(dev, "asrc_verify_params(): runtime stream pcm rate %u does not match rate %u fetched from ipc.",
+				 params->rate, cd->ipc_config.source_rate);
 			return -EINVAL;
 		}
 	} else {
-		if (params->rate != asrc->sink_rate && asrc->sink_rate) {
-			comp_err(dev, "asrc_verify_params(): runtime stream pcm rate does not match rate fetched from ipc.");
+		if (cd->ipc_config.sink_rate && (params->rate != cd->ipc_config.sink_rate)) {
+			comp_err(dev, "asrc_verify_params(): runtime stream pcm rate %u does not match rate %u fetched from ipc.",
+				 params->rate, cd->ipc_config.sink_rate);
 			return -EINVAL;
 		}
 	}
@@ -555,7 +555,6 @@ static int asrc_trigger(struct comp_dev *dev, int cmd)
 static int asrc_prepare(struct comp_dev *dev)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
-	struct sof_ipc_comp_config *config = dev_comp_config(dev);
 	struct comp_buffer *sinkb;
 	struct comp_buffer *sourceb;
 	uint32_t source_period_bytes;
@@ -593,9 +592,9 @@ static int asrc_prepare(struct comp_dev *dev)
 	sink_period_bytes = audio_stream_period_bytes(&sinkb->stream,
 						      cd->sink_frames);
 
-	if (sinkb->stream.size < config->periods_sink * sink_period_bytes) {
+	if (sinkb->stream.size < dev->ipc_config.periods_sink * sink_period_bytes) {
 		comp_err(dev, "asrc_prepare(): sink buffer size %d is insufficient < %d * %d",
-			 sinkb->stream.size, config->periods_sink, sink_period_bytes);
+			 sinkb->stream.size, dev->ipc_config.periods_sink, sink_period_bytes);
 		ret = -ENOMEM;
 		goto err;
 	}

src/audio/codec_adapter/codec_adapter.c
@@ -16,6 +16,7 @@
 
 #include <sof/audio/buffer.h>
 #include <sof/audio/component.h>
+#include <sof/audio/ipc-config.h>
 #include <sof/audio/codec_adapter/codec_adapter.h>
 #include <sof/audio/pipeline.h>
 #include <sof/common.h>
@@ -33,34 +34,30 @@ int validate_setup_config(struct ca_config *cfg);
  * \return: a pointer to newly created codec adapter component.
  */
 struct comp_dev *codec_adapter_new(const struct comp_driver *drv,
-				   struct sof_ipc_comp *comp,
-				   struct codec_interface *interface)
+				   struct comp_ipc_config *config,
+				   struct codec_interface *interface,
+				   void *spec)
 {
 	int ret;
 	struct comp_dev *dev;
 	struct comp_data *cd;
-	struct sof_ipc_comp_process *ipc_codec_adapter =
-		(struct sof_ipc_comp_process *)comp;
+	struct ipc_config_process *ipc_codec_adapter = spec;
 
 	comp_cl_dbg(drv, "codec_adapter_new() start");
 
-	if (!drv || !comp) {
-		comp_cl_err(drv, "codec_adapter_new(), wrong input params! drv = %x comp = %x",
-			    (uint32_t)drv, (uint32_t)comp);
+	if (!drv || !config) {
+		comp_cl_err(drv, "codec_adapter_new(), wrong input params! drv = %x config = %x",
+			    (uint32_t)drv, (uint32_t)config);
 		return NULL;
 	}
 
-	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_process));
+	dev = comp_alloc(drv, sizeof(*dev));
 	if (!dev) {
 		comp_cl_err(drv, "codec_adapter_new(), failed to allocate memory for comp_dev");
 		return NULL;
 	}
-
+	dev->ipc_config = *config;
 	dev->drv = drv;
-
-	ret = memcpy_s(&dev->comp, sizeof(struct sof_ipc_comp_process),
-		       comp, sizeof(struct sof_ipc_comp_process));
-	assert(!ret);
 
 	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*cd));
 	if (!cd) {
@@ -70,6 +67,7 @@ struct comp_dev *codec_adapter_new(const struct comp_driver *drv,
 	}
 
 	comp_set_drvdata(dev, cd);
+
 	/* Copy setup config */
 	ret = load_setup_config(dev, ipc_codec_adapter->data, ipc_codec_adapter->size);
 	if (ret) {

src/audio/crossover/crossover.c
@@ -8,6 +8,7 @@
 #include <sof/audio/component.h>
 #include <sof/audio/format.h>
 #include <sof/audio/pipeline.h>
+#include <sof/audio/ipc-config.h>
 #include <sof/audio/crossover/crossover.h>
 #include <sof/audio/crossover/crossover_algorithm.h>
 #include <sof/audio/eq_iir/iir.h>
@@ -310,13 +311,12 @@ static int crossover_setup(struct comp_data *cd, int nch)
  * \return Pointer to Crossover Filter component device.
  */
 static struct comp_dev *crossover_new(const struct comp_driver *drv,
-				      struct sof_ipc_comp *comp)
+				      struct comp_ipc_config *config,
+				      void *spec)
 {
 	struct comp_dev *dev;
 	struct comp_data *cd;
-	struct sof_ipc_comp_process *crossover;
-	struct sof_ipc_comp_process *ipc_crossover =
-			(struct sof_ipc_comp_process *)comp;
+	struct ipc_config_process *ipc_crossover = spec;;
 	size_t bs = ipc_crossover->size;
 	int ret;
 
@@ -329,14 +329,10 @@ static struct comp_dev *crossover_new(const struct comp_driver *drv,
 		return NULL;
 	}
 
-	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_process));
+	dev = comp_alloc(drv, sizeof(*dev));
 	if (!dev)
 		return NULL;
-
-	crossover = COMP_GET_IPC(dev, sof_ipc_comp_process);
-	ret = memcpy_s(crossover, sizeof(*crossover), ipc_crossover,
-		       sizeof(struct sof_ipc_comp_process));
-	assert(!ret);
+	dev->ipc_config = *config;
 
 	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0,
 		     SOF_MEM_CAPS_RAM, sizeof(*cd));
@@ -731,7 +727,6 @@ static int crossover_copy(struct comp_dev *dev)
 static int crossover_prepare(struct comp_dev *dev)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
-	struct sof_ipc_comp_config *config = dev_comp_config(dev);
 	struct comp_buffer *source, *sink;
 	struct list_item *sink_list;
 	int32_t sink_period_bytes;
@@ -767,7 +762,7 @@ static int crossover_prepare(struct comp_dev *dev)
 		sink_period_bytes = audio_stream_period_bytes(&sink->stream,
 							      dev->frames);
 		if (sink->stream.size <
-				config->periods_sink * sink_period_bytes) {
+				dev->ipc_config.periods_sink * sink_period_bytes) {
 			comp_err(dev, "crossover_prepare(), sink %d buffer size %d is insufficient",
 				 sink->pipeline_id, sink->stream.size);
 			ret = -ENOMEM;

src/audio/dai.c
@@ -10,6 +10,7 @@
 #include <sof/audio/format.h>
 #include <sof/audio/pcm_converter.h>
 #include <sof/audio/pipeline.h>
+#include <sof/audio/ipc-config.h>
 #include <sof/common.h>
 #include <sof/debug/panic.h>
 #include <sof/drivers/edma.h>
@@ -67,6 +68,7 @@ struct dai_data {
 	uint64_t *dai_pos;
 
 	struct sof_ipc_dai_config *dai_config;	/* dai_config from the host */
+	struct ipc_config_dai ipc_config;	/* base config at DAI new */
 
 	uint64_t wallclock;	/* wall clock at stream start */
 };
@@ -179,25 +181,20 @@ static void dai_dma_cb(void *arg, enum notify_id type, void *data)
 }
 
 static struct comp_dev *dai_new(const struct comp_driver *drv,
-				struct sof_ipc_comp *comp)
+				struct comp_ipc_config *config,
+				void *spec)
 {
 	struct comp_dev *dev;
-	struct sof_ipc_comp_dai *dai;
-	struct sof_ipc_comp_dai *ipc_dai = (struct sof_ipc_comp_dai *)comp;
+	struct ipc_config_dai *dai = spec;
 	struct dai_data *dd;
 	uint32_t dir, caps, dma_dev;
-	int ret;
 
 	comp_cl_dbg(&comp_dai, "dai_new()");
 
-	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_dai));
+	dev = comp_alloc(drv, sizeof(*dev));
 	if (!dev)
 		return NULL;
-
-	dai = COMP_GET_IPC(dev, sof_ipc_comp_dai);
-	ret = memcpy_s(dai, sizeof(*dai), ipc_dai,
-		       sizeof(struct sof_ipc_comp_dai));
-	assert(!ret);
+	dev->ipc_config = *config;
 
 	dd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*dd));
 	if (!dd) {
@@ -212,6 +209,7 @@ static struct comp_dev *dai_new(const struct comp_driver *drv,
 		comp_cl_err(&comp_dai, "dai_new(): dai_get() failed to create DAI.");
 		goto error;
 	}
+	dd->ipc_config = *dai;
 
 	/* request GP LP DMA with shared access privilege */
 	dir = dai->direction == SOF_IPC_STREAM_PLAYBACK ?
@@ -270,7 +268,6 @@ static int dai_comp_get_hw_params(struct comp_dev *dev,
 				  struct sof_ipc_stream_params *params,
 				  int dir)
 {
-	struct sof_ipc_comp_config *dconfig = dev_comp_config(dev);
 	struct dai_data *dd = comp_get_drvdata(dev);
 	int ret = 0;
 
@@ -290,7 +287,7 @@ static int dai_comp_get_hw_params(struct comp_dev *dev,
 	 * frame_fmt hardware parameter as DAI component is able to convert
 	 * stream with different frame_fmt's (using pcm converter)
 	 */
-	params->frame_fmt = dconfig->frame_fmt;
+	params->frame_fmt = dev->ipc_config.frame_fmt;
 
 	return 0;
 }
@@ -347,9 +344,8 @@ static int dai_verify_params(struct comp_dev *dev,
 
 static void dai_data_config(struct comp_dev *dev)
 {
-	struct sof_ipc_comp_config *dconfig = dev_comp_config(dev);
 	struct dai_data *dd = comp_get_drvdata(dev);
-	struct sof_ipc_comp_dai *dai = COMP_GET_IPC(dev, sof_ipc_comp_dai);
+	struct ipc_config_dai *dai = &dd->ipc_config;
 
 	assert(dd->dai_config);
 
@@ -381,8 +377,8 @@ static void dai_data_config(struct comp_dev *dev)
 		/* SDW HW FIFO always requires 32bit MSB aligned sample data for
 		 * all formats, such as 8/16/24/32 bits.
 		 */
-		dconfig->frame_fmt = SOF_IPC_FRAME_S32_LE;
-		dd->dma_buffer->stream.frame_fmt = dconfig->frame_fmt;
+		dev->ipc_config.frame_fmt = SOF_IPC_FRAME_S32_LE;
+		dd->dma_buffer->stream.frame_fmt = dev->ipc_config.frame_fmt;
 
 		dd->config.burst_elems =
 			dd->dai->plat_data.fifo[dai->direction].depth;
@@ -533,7 +529,6 @@ static int dai_capture_params(struct comp_dev *dev, uint32_t period_bytes,
 static int dai_params(struct comp_dev *dev,
 		      struct sof_ipc_stream_params *params)
 {
-	struct sof_ipc_comp_config *dconfig = dev_comp_config(dev);
 	struct sof_ipc_stream_params hw_params = *params;
 	struct dai_data *dd = comp_get_drvdata(dev);
 	uint32_t frame_size;
@@ -608,7 +603,7 @@ static int dai_params(struct comp_dev *dev,
 	}
 
 	/* calculate frame size */
-	frame_size = get_frame_bytes(dconfig->frame_fmt,
+	frame_size = get_frame_bytes(dev->ipc_config.frame_fmt,
 				     dd->local_buffer->stream.channels);
 
 	/* calculate period size */
@@ -645,7 +640,7 @@ static int dai_params(struct comp_dev *dev,
 		 * component is able to convert stream with different
 		 * frame_fmt's (using pcm converter).
 		 */
-		hw_params.frame_fmt = dconfig->frame_fmt;
+		hw_params.frame_fmt = dev->ipc_config.frame_fmt;
 		buffer_set_params(dd->dma_buffer, &hw_params,
 				  BUFFER_UPDATE_FORCE);
 	}
@@ -658,7 +653,7 @@ static int dai_params(struct comp_dev *dev,
 static int dai_config_dma_channel(struct comp_dev *dev, struct sof_ipc_dai_config *config)
 {
 	struct dai_data *dd = comp_get_drvdata(dev);
-	struct sof_ipc_comp_dai *dai = COMP_GET_IPC(dev, sof_ipc_comp_dai);
+	struct ipc_config_dai *dai = &dd->ipc_config;
 	int channel;
 	int handshake;
 
@@ -701,6 +696,14 @@ static int dai_config(struct comp_dev *dev, struct sof_ipc_dai_config *config)
 {
 	struct dai_data *dd = comp_get_drvdata(dev);
 	int ret;
+
+	comp_info(dev, "dai_config() check dai type = %d index = %d dd %p",
+			  config->type, config->dai_index, dd);
+
+	/* ignore if message not for this DAI id/type */
+	if (dd->ipc_config.dai_index != config->dai_index ||
+	    dd->ipc_config.type != config->type)
+		return 0;
 
 	comp_info(dev, "dai_config() dai type = %d index = %d dd %p",
 		  config->type, config->dai_index, dd);
@@ -1176,7 +1179,7 @@ static int dai_ts_config(struct comp_dev *dev)
 {
 	struct dai_data *dd = comp_get_drvdata(dev);
 	struct timestamp_cfg *cfg = &dd->ts_config;
-	struct sof_ipc_comp_dai *dai = COMP_GET_IPC(dev, sof_ipc_comp_dai);
+	struct ipc_config_dai *dai = &dd->ipc_config;
 
 	comp_dbg(dev, "dai_ts_config()");
 	if (!dd->chan) {

src/audio/dcblock/dcblock.c
@@ -9,6 +9,7 @@
 #include <sof/audio/format.h>
 #include <sof/audio/pipeline.h>
 #include <sof/audio/dcblock/dcblock.h>
+#include <sof/audio/ipc-config.h>
 #include <sof/common.h>
 #include <sof/debug/panic.h>
 #include <sof/ipc/msg.h>
@@ -64,26 +65,21 @@ static void dcblock_init_state(struct comp_data *cd)
  * \return Pointer to DC Blocking Filter component device.
  */
 static struct comp_dev *dcblock_new(const struct comp_driver *drv,
-				    struct sof_ipc_comp *comp)
+				    struct comp_ipc_config *config,
+				    void *spec)
 {
 	struct comp_dev *dev;
 	struct comp_data *cd;
-	struct sof_ipc_comp_process *dcblock;
-	struct sof_ipc_comp_process *ipc_dcblock =
-		(struct sof_ipc_comp_process *)comp;
+	struct ipc_config_process *ipc_dcblock = spec;
 	size_t bs = ipc_dcblock->size;
 	int ret;
 
 	comp_cl_info(&comp_dcblock, "dcblock_new()");
 
-	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_process));
+	dev = comp_alloc(drv, sizeof(*dev));
 	if (!dev)
 		return NULL;
-
-	dcblock = COMP_GET_IPC(dev, sof_ipc_comp_process);
-	ret = memcpy_s(dcblock, sizeof(*dcblock), ipc_dcblock,
-		       sizeof(struct sof_ipc_comp_process));
-	assert(!ret);
+	dev->ipc_config = *config;
 
 	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*cd));
 	if (!cd) {
@@ -320,7 +316,6 @@ static int dcblock_copy(struct comp_dev *dev)
 static int dcblock_prepare(struct comp_dev *dev)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
-	struct sof_ipc_comp_config *config = dev_comp_config(dev);
 	struct comp_buffer *sourceb;
 	struct comp_buffer *sinkb;
 	uint32_t sink_period_bytes;
@@ -349,9 +344,9 @@ static int dcblock_prepare(struct comp_dev *dev)
 	sink_period_bytes =
 			audio_stream_period_bytes(&sinkb->stream, dev->frames);
 
-	if (sinkb->stream.size < config->periods_sink * sink_period_bytes) {
+	if (sinkb->stream.size < dev->ipc_config.periods_sink * sink_period_bytes) {
 		comp_err(dev, "dcblock_prepare(): sink buffer size %d is insufficient < %d * %d",
-			 sinkb->stream.size, config->periods_sink, sink_period_bytes);
+			 sinkb->stream.size, dev->ipc_config.periods_sink, sink_period_bytes);
 		ret = -ENOMEM;
 		goto err;
 	}

src/audio/drc/drc.c
@@ -10,6 +10,7 @@
 #include <sof/audio/drc/drc_algorithm.h>
 #include <sof/audio/format.h>
 #include <sof/audio/pipeline.h>
+#include <sof/audio/ipc-config.h>
 #include <sof/common.h>
 #include <sof/debug/panic.h>
 #include <sof/ipc/msg.h>
@@ -139,12 +140,12 @@ static int drc_setup(struct drc_comp_data *cd, uint16_t channels, uint32_t rate)
  */
 
 static struct comp_dev *drc_new(const struct comp_driver *drv,
-				struct sof_ipc_comp *comp)
+				struct comp_ipc_config *config,
+				void *spec)
 {
 	struct comp_dev *dev;
 	struct drc_comp_data *cd;
-	struct sof_ipc_comp_process *ipc_drc =
-		(struct sof_ipc_comp_process *)comp;
+	struct ipc_config_process *ipc_drc = spec;
 	size_t bs = ipc_drc->size;
 	int ret;
 
@@ -159,14 +160,10 @@ static struct comp_dev *drc_new(const struct comp_driver *drv,
 		return NULL;
 	}
 
-	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_process));
+	dev = comp_alloc(drv, sizeof(*dev));
 	if (!dev)
 		return NULL;
-
-	ret = memcpy_s(COMP_GET_IPC(dev, sof_ipc_comp_process),
-		       sizeof(struct sof_ipc_comp_process), ipc_drc,
-		       sizeof(struct sof_ipc_comp_process));
-	assert(!ret);
+	dev->ipc_config = *config;
 
 	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*cd));
 	if (!cd) {
@@ -362,7 +359,6 @@ static int drc_copy(struct comp_dev *dev)
 static int drc_prepare(struct comp_dev *dev)
 {
 	struct drc_comp_data *cd = comp_get_drvdata(dev);
-	struct sof_ipc_comp_config *config = dev_comp_config(dev);
 	struct comp_buffer *sourceb;
 	struct comp_buffer *sinkb;
 	uint32_t sink_period_bytes;
@@ -424,7 +420,7 @@ static int drc_prepare(struct comp_dev *dev)
 	sink_period_bytes = audio_stream_period_bytes(&sinkb->stream,
 						      dev->frames);
 
-	if (sinkb->stream.size < config->periods_sink * sink_period_bytes) {
+	if (sinkb->stream.size < dev->ipc_config.periods_sink * sink_period_bytes) {
 		comp_err(dev, "drc_prepare(), sink buffer size %d is insufficient",
 			 sinkb->stream.size);
 		ret = -ENOMEM;

src/audio/eq_fir/eq_fir.c
@@ -10,6 +10,7 @@
 #include <sof/audio/buffer.h>
 #include <sof/audio/component.h>
 #include <sof/audio/pipeline.h>
+#include <sof/audio/ipc-config.h>
 #include <sof/common.h>
 #include <sof/debug/panic.h>
 #include <sof/ipc/msg.h>
@@ -314,13 +315,12 @@ static int eq_fir_setup(struct comp_data *cd, int nch)
  */
 
 static struct comp_dev *eq_fir_new(const struct comp_driver *drv,
-				   struct sof_ipc_comp *comp)
+				   struct comp_ipc_config *config,
+				   void *spec)
 {
 	struct comp_dev *dev;
 	struct comp_data *cd;
-	struct sof_ipc_comp_process *fir;
-	struct sof_ipc_comp_process *ipc_fir
-		= (struct sof_ipc_comp_process *)comp;
+	struct ipc_config_process *ipc_fir = spec;
 	size_t bs = ipc_fir->size;
 	int i;
 	int ret;
@@ -336,14 +336,10 @@ static struct comp_dev *eq_fir_new(const struct comp_driver *drv,
 		return NULL;
 	}
 
-	dev = comp_alloc(drv, COMP_SIZE(struct sof_ipc_comp_process));
+	dev = comp_alloc(drv, sizeof(*dev));
 	if (!dev)
 		return NULL;
-
-	fir = COMP_GET_IPC(dev, sof_ipc_comp_process);
-	ret = memcpy_s(fir, sizeof(*fir), ipc_fir,
-		       sizeof(struct sof_ipc_comp_process));
-	assert(!ret);
+	dev->ipc_config = *config;
 
 	cd = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*cd));
 	if (!cd) {
@@ -545,7 +541,6 @@ static int eq_fir_copy(struct comp_dev *dev)
 static int eq_fir_prepare(struct comp_dev *dev)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
-	struct sof_ipc_comp_config *config = dev_comp_config(dev);
 	struct comp_buffer *sourceb;
 	struct comp_buffer *sinkb;
 	uint32_t sink_period_bytes;
@@ -574,9 +569,9 @@ static int eq_fir_prepare(struct comp_dev *dev)
 	sink_period_bytes = audio_stream_period_bytes(&sinkb->stream,
 						      dev->frames);
 
-	if (sinkb->stream.size < config->periods_sink * sink_period_bytes) {
+	if (sinkb->stream.size < dev->ipc_config.periods_sink * sink_period_bytes) {
 		comp_err(dev, "eq_fir_prepare(): sink buffer size %d is insufficient < %d * %d",
-			 sinkb->stream.size, config->periods_sink, sink_period_bytes);
+			 sinkb->stream.size, dev->ipc_config.periods_sink, sink_period_bytes);
 		ret = -ENOMEM;
 		goto err;
 	}

commit 090c9cbf3250711773ea3962f686796e56d4ce57

mux: remove the wrong error message
A wrong error message was added in helper get_stream_index(), remove to
fix it.

Fixes: 80666b7cc7a9 ("ipc: component: abstract the component creation API")
Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

src/audio/mux/mux.c
@@ -213,11 +213,9 @@ static uint8_t get_stream_index(struct comp_data *cd, uint32_t pipe_id)
 {
 	int i;
 
-	for (i = 0; i < MUX_MAX_STREAMS; i++) {
-		comp_cl_err(&comp_mux, "got %u", cd->config.streams[i].pipeline_id);
+	for (i = 0; i < MUX_MAX_STREAMS; i++)
 		if (cd->config.streams[i].pipeline_id == pipe_id)
 			return i;
-	}
 
 	comp_cl_err(&comp_mux, "get_stream_index(): couldn't find configuration for connected pipeline %u",
 		    pipe_id);