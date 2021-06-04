a9837a711026e8707b6fa7b624a593ab9dafe488
Rander Wang <rander.wang@intel.com>
2021-06-04 10:23:10 +0800

    ipc4: refine pipeline reset path
    Sof FW doesn't use reset cmd in pipeline_trigger function now,
    and resource is also not released by this cmd. Call pipeline_reset to
    free resource.
    
    Signed-off-by: Rander Wang <rander.wang@intel.com>

--- src/ipc/ipc4/handler.c
+++ src/ipc/ipc4/handler.c
@@ -221,10 +221,11 @@ static int ipc4_set_pipeline_state(union ipc4_message_header *ipc4)
 		ret = pipeline_trigger(host->cd->pipeline, host->cd, COMP_TRIGGER_STOP);
 		if (ret < 0) {
 			tr_err(&ipc_tr, "ipc: comp %d trigger 0x%x failed %d", id, cmd, ret);
-			ret = IPC4_PIPELINE_STATE_NOT_SET;
+			return IPC4_PIPELINE_STATE_NOT_SET;
 		}
 
-		cmd = COMP_TRIGGER_RESET;
+		/* resource is not released by triggering reset which is used by current FW */
+		return pipeline_reset(host->cd->pipeline, host->cd);
 		break;
 	case SOF_IPC4_PIPELINE_STATE_PAUSED:
 		if (pcm_dev->pipeline->status == COMP_STATE_INIT)
