9ac63d4d4ed5afe702a8786f41a84ac46d07d2d5
Bard Liao <yung-chuan.liao@linux.intel.com>
2021-06-10 09:20:53 +0800

    ipc4: add tgl-h-ipc4 support
    add tgl-h-ipc4 default config
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- scripts/xtensa-build-all.sh
+++ scripts/xtensa-build-all.sh
@@ -6,7 +6,7 @@
 set -e
 
 SUPPORTED_PLATFORMS=(byt cht bdw hsw apl skl kbl cnl sue icl jsl \
-                    imx8 imx8x imx8m tgl tgl-h tgl-ipc4)
+                    imx8 imx8x imx8m tgl tgl-h tgl-ipc4 tgl-h-ipc4)
 BUILD_ROM=no
 BUILD_DEBUG=no
 BUILD_FORCE_UP=no
@@ -296,6 +296,19 @@ do
 			HOST="xtensa-cnl-elf"
 			XTENSA_TOOLS_VERSION="RG-2017.8-linux"
 			HAVE_ROM='yes'
+			# default key for TGL
+			if [ -z "$PRIVATE_KEY_OPTION" ]
+			then
+	PRIVATE_KEY_OPTION="-D${SIGNING_TOOL}_PRIVATE_KEY=$SOF_TOP/keys/otc_private_key_3k.pem"
+			fi
+			;;
+		tgl-h-ipc4)
+			PLATFORM="tgphipc4"
+			XTENSA_CORE="cavs2x_LX6HiFi3_2017_8"
+			HOST="xtensa-cnl-elf"
+			XTENSA_TOOLS_VERSION="RG-2017.8-linux"
+			HAVE_ROM='yes'
+			IPC4_CONFIG="${SOF_TOP}/src/arch/xtensa/configs/override/ipc4.config"
 			# default key for TGL
 			if [ -z "$PRIVATE_KEY_OPTION" ]
 			then
