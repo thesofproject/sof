8d26d43d088dc4ee4f419d3ca25c5f8d4f3e1391
Marc Herbert <marc.herbert@intel.com>
2021-06-23 02:47:15 +0000

    dma-trace.c: use new mtrace_printf() to demote FW ABI banner to INFO
    ... down from ERROR. It was never an error, this was a hack to be
    duplicated to both trace and etrace. This will make it easier to search
    for actual errors.
    
    Also make the banners slightly different between etrace and trace thanks
    to new SHM: and DMA: prefixes. This makes identification of log files
    easier.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- src/trace/dma-trace.c
+++ src/trace/dma-trace.c
@@ -370,13 +370,17 @@ int dma_trace_enable(struct dma_trace_data *d)
 		goto out;
 	}
 
-	/*
-	 * It should be the very first sent log for easily identification.
-	 * Use tr_err to have this initial message also in error logs and assert
-	 * traces works well.
-	 */
-	tr_err(&dt_tr, "FW ABI 0x%x DBG ABI 0x%x tag " SOF_GIT_TAG " src hash 0x%08x (ldc hash " META_QUOTE(SOF_SRC_HASH) ")",
-	       SOF_ABI_VERSION, SOF_ABI_DBG_VERSION, SOF_SRC_HASH);
+	/* It should be the very first sent log for easy identification. */
+	mtrace_printf(LOG_LEVEL_INFO,
+		      "SHM: FW ABI 0x%x DBG ABI 0x%x tag " SOF_GIT_TAG " src hash 0x%08x (ldc hash "
+		      META_QUOTE(SOF_SRC_HASH) ")",
+		      SOF_ABI_VERSION, SOF_ABI_DBG_VERSION, SOF_SRC_HASH);
+
+	/* Use a different, DMA: prefix to ease identification of log files */
+	tr_info(&dt_tr,
+		"DMA: FW ABI 0x%x DBG ABI 0x%x tag " SOF_GIT_TAG " src hash 0x%08x (ldc hash "
+		META_QUOTE(SOF_SRC_HASH) ")",
+		SOF_ABI_VERSION, SOF_ABI_DBG_VERSION, SOF_SRC_HASH);
 
 #if CONFIG_DMA_GW
 	/*
