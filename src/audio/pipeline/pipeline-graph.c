c1f0f7068eab8667b0731012bf7d7c25f1cf02be
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2021-06-24 13:26:01 +0100

    pipeline: add heap trace to pipeline_new()
    Balance the heap status with the existing pipeline_free() heap debug.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/audio/pipeline/pipeline-graph.c
+++ src/audio/pipeline/pipeline-graph.c
@@ -112,6 +112,9 @@ struct pipeline *pipeline_new(uint32_t pipeline_id, uint32_t priority, uint32_t 
 	pipe_cl_info("pipeline new pipe_id %d priority %d",
 		     pipeline_id, priority);
 
+	/* show heap status */
+	heap_trace_all(0);
+
 	/* allocate new pipeline */
 	p = rzalloc(SOF_MEM_ZONE_RUNTIME_SHARED, 0, SOF_MEM_CAPS_RAM, sizeof(*p));
 	if (!p) {
