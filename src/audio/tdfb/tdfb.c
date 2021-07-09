singalsu/sof-1

Audio: TDFB: Add enum control for direction and switch on/off control
This patch adds the enums set/get controls for beam direction
(azimuth). The switch control provides a beam-off bypass from the
configured microphones to channels. It is used when directional
capture is not desired.

The configuration blob is updated to pass filters settings for
multiple angles, a bypass coefficients set, and microphones (x, y, z)
coordinates for direction enum get (implemented later).

Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

src/audio/tdfb/tdfb.c
@@ -32,6 +32,13 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/* The driver assigns running numbers for control index. If there's single control of
+ * type switch, enum, binary they all have index 0.
+ */
+#define CTRL_INDEX_ONOFF		0
+#define CTRL_INDEX_DIRECTION		0
+#define CTRL_INDEX_FILTERBANK		0
+
 static const struct comp_driver comp_tdfb;
 
 /* dd511749-d9fa-455c-b3a7-13585693f1af */
@@ -117,14 +124,39 @@ static void tdfb_free_delaylines(struct tdfb_comp_data *cd)
 		fir[i].delay = NULL;
 }
 
+static void *tdfb_filter_seek(struct sof_tdfb_config *config, int num_filters)
+{
+	struct sof_fir_coef_data *coef_data;
+	int i;
+	uint8_t *coefp = (uint8_t *)ASSUME_ALIGNED(&config->data[0], 2); /* 2 for 16 bits data */
+
+	/* Note: FIR coefficients are int16_t. An uint_8 type pointer coefp
+	 * is used for jumping in the flexible array member structs coef_data.
+	 */
+	for (i = 0; i < num_filters; i++) {
+		coef_data = (struct sof_fir_coef_data *)coefp;
+		coefp += sizeof(*coef_data) + sizeof(int16_t) * coef_data->length;
+	}
+
+	return (void *)coefp;
+}
+
 static int tdfb_init_coef(struct tdfb_comp_data *cd, int source_nch,
 			  int sink_nch)
 {
 	struct sof_fir_coef_data *coef_data;
 	struct sof_tdfb_config *config = cd->config;
+	int16_t *output_channel_mix_beam_off = NULL;
 	int16_t *coefp;
+	uint8_t *dp;
 	int size_sum = 0;
+	int min_delta_idx; /* Index to beam angle with smallest delta vs. target */
+	int min_delta; /* Smallest angle difference found in degrees */
 	int max_ch;
+	int num_filters;
+	int target_az; /* Target azimuth angle in degrees */
+	int delta; /* Target minus found angle in degrees absolute value */
+	int idx;
 	int s;
 	int i;
 
@@ -148,10 +180,101 @@ static int tdfb_init_coef(struct tdfb_comp_data *cd, int source_nch,
 		return -EINVAL;
 	}
 
-	coefp = ASSUME_ALIGNED(&config->data[0], 2);
+	if (config->num_angles > SOF_TDFB_MAX_ANGLES) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid num_angles %d",
+			    config->num_angles);
+		return -EINVAL;
+	}
+
+	if (config->beam_off_defined > 1) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid beam_off_defined %d",
+			    config->beam_off_defined);
+		return -EINVAL;
+	}
+
+	if (config->num_mic_locations > SOF_TDFB_MAX_MICROPHONES) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid num_mic_locations %d",
+			    config->num_mic_locations);
+		return -EINVAL;
+	}
+
+	/* In SOF v1.6 - 1.7 based beamformer topologies the multiple angles, mic locations,
+	 * and beam on/off switch were not defined. Return error if such configuration is seen.
+	 * A most basic blob has num_angles equals 1. Mic locations data is optional.
+	 */
+	if (config->num_angles == 0 && config->num_mic_locations == 0) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), ABI version less than 3.19.1 is not supported.");
+		return -EINVAL;
+	}
+
+	/* Skip filter coefficients */
+	num_filters = config->num_filters * (config->num_angles + config->beam_off_defined);
+	coefp = tdfb_filter_seek(config, num_filters);
+
+	/* Get shortcuts to input and output configuration */
+	cd->input_channel_select = coefp;
+	coefp += config->num_filters;
+	cd->output_channel_mix = coefp;
+	coefp += config->num_filters;
+	cd->output_stream_mix = coefp;
+	coefp += config->num_filters;
+
+	/* Check if there's beam-off configured, then get pointers to beam angles data
+	 * and microphone locations. Finally check that size matches.
+	 */
+	if (config->beam_off_defined) {
+		output_channel_mix_beam_off = coefp;
+		coefp += config->num_filters;
+	}
+	cd->filter_angles = (struct sof_tdfb_angle *)coefp;
+	cd->mic_locations = (struct sof_tdfb_mic_location *)
+		(&cd->filter_angles[config->num_angles]);
+	if ((char *)&cd->mic_locations[config->num_mic_locations] !=
+	    (char *)config + config->size) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid config size");
+		return -EINVAL;
+	}
+
+	/* Skip to requested coefficient set */
+	min_delta = 360;
+	min_delta_idx = 0;
+	target_az = cd->az_value * config->angle_enum_mult + config->angle_enum_offs;
+	if (target_az > 180)
+		target_az -= 360;
+
+	if (target_az < -180)
+		target_az += 360;
+
+	for (i = 0; i < config->num_angles; i++) {
+		delta = ABS(target_az - cd->filter_angles[i].azimuth);
+		if (delta < min_delta) {
+			min_delta = delta;
+			min_delta_idx = i;
+		}
+	}
+
+	idx = cd->filter_angles[min_delta_idx].filter_index;
+	if (cd->beam_on)
+		comp_cl_info(&comp_tdfb, "tdfb_init_coef(), angle request %d, found %d, idx %d",
+			     target_az, cd->filter_angles[min_delta_idx].azimuth, idx);
+	else
+		if (config->beam_off_defined) {
+			cd->output_channel_mix = output_channel_mix_beam_off;
+			idx = config->num_filters * config->num_angles;
+			comp_cl_info(&comp_tdfb, "tdfb_init_coef(), configure beam off");
+
+		} else {
+			comp_cl_err(&comp_tdfb, "tdfb_init_coef(), no beam off defined");
+			return -EINVAL;
+		}
+
+	/* Seek to proper filter for requested angle or beam off configuration */
+	dp = tdfb_filter_seek(config, idx);
+
+	/* Initialize filter bank */
 	for (i = 0; i < config->num_filters; i++) {
 		/* Get delay line size */
-		coef_data = (struct sof_fir_coef_data *)coefp;
+		coef_data = (struct sof_fir_coef_data *)dp;
 		s = fir_delay_size(coef_data);
 		if (s > 0) {
 			size_sum += s;
@@ -165,13 +288,8 @@ static int tdfb_init_coef(struct tdfb_comp_data *cd, int source_nch,
 		 * filter.
 		 */
 		fir_init_coef(&cd->fir[i], coef_data);
-		coefp += SOF_FIR_COEF_NHEADER + coef_data->length;
+		dp += sizeof(*coef_data) + sizeof(int16_t) * coef_data->length;
 	}
-
-	/* Get shortcuts to input and output configuration */
-	cd->input_channel_select = coefp;
-	cd->output_channel_mix = coefp + config->num_filters;
-	cd->output_stream_mix = coefp + 2 * config->num_filters;
 
 	/* Find max used input channel */
 	max_ch = 0;
@@ -208,9 +326,6 @@ static int tdfb_setup(struct tdfb_comp_data *cd, int source_nch, int sink_nch)
 {
 	int delay_size;
 
-	/* Free existing FIR channels data if it was allocated */
-	tdfb_free_delaylines(cd);
-
 	/* Set coefficients for each channel from coefficient blob */
 	delay_size = tdfb_init_coef(cd, source_nch, sink_nch);
 	if (delay_size < 0)
@@ -222,19 +337,25 @@ static int tdfb_setup(struct tdfb_comp_data *cd, int source_nch, int sink_nch)
 	if (!delay_size)
 		return 0;
 
-	/* Allocate all FIR channels data in a big chunk and clear it */
-	cd->fir_delay = rballoc(0, SOF_MEM_CAPS_RAM, delay_size);
-	if (!cd->fir_delay) {
-		comp_cl_err(&comp_tdfb, "tdfb_setup(), delay allocation failed for size %d",
-			    delay_size);
-		return -ENOMEM;
-	}
+	if (delay_size > cd->fir_delay_size) {
+		/* Free existing FIR channels data if it was allocated */
+		tdfb_free_delaylines(cd);
 
-	memset(cd->fir_delay, 0, delay_size);
-	cd->fir_delay_size = delay_size;
+		/* Allocate all FIR channels data in a big chunk and clear it */
+		cd->fir_delay = rballoc(0, SOF_MEM_CAPS_RAM, delay_size);
+		if (!cd->fir_delay) {
+			comp_cl_err(&comp_tdfb, "tdfb_setup(), delay allocation failed for size %d",
+				    delay_size);
+			return -ENOMEM;
+		}
+
+		memset(cd->fir_delay, 0, delay_size);
+		cd->fir_delay_size = delay_size;
+	}
 
 	/* Assign delay line to all channel filters */
 	tdfb_init_delay(cd);
+
 	return 0;
 }
 
@@ -275,9 +396,13 @@ static struct comp_dev *tdfb_new(const struct comp_driver *drv,
 
 	comp_set_drvdata(dev, cd);
 
-	cd->tdfb_func = NULL;
-	cd->fir_delay = NULL;
-	cd->fir_delay_size = 0;
+	/* Defaults for processing function pointer tdfb_func, fir_delay
+	 * pointer, are NULL. Fir_delay_size is zero from rzalloc().
+	 */
+
+	/* Defaults for enum controls are zeros from rzalloc()
+	 * az_value is zero, beam off is false, and update is false.
+	 */
 
 	/* Handler for configuration data */
 	cd->model_handler = comp_data_blob_handler_new(dev);
@@ -325,11 +450,61 @@ static int tdfb_cmd_get_data(struct comp_dev *dev,
 
 	switch (cdata->cmd) {
 	case SOF_CTRL_CMD_BINARY:
-		comp_info(dev, "tdfb_cmd_get_data(), SOF_CTRL_CMD_BINARY");
+		comp_dbg(dev, "tdfb_cmd_get_data(), SOF_CTRL_CMD_BINARY");
 		ret = comp_data_blob_get_cmd(cd->model_handler, cdata, max_size);
 		break;
 	default:
 		comp_err(dev, "tdfb_cmd_get_data() error: invalid cdata->cmd");
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
+}
+
+static int tdfb_cmd_switch_get(struct sof_ipc_ctrl_data *cdata, struct tdfb_comp_data *cd)
+{
+	int j;
+
+	/* Fail if wrong index in control, needed if several in same type */
+	if (cdata->index != CTRL_INDEX_ONOFF)
+		return -EINVAL;
+
+	for (j = 0; j < cdata->num_elems; j++)
+		cdata->chanv[j].value = cd->beam_on;
+
+	return 0;
+}
+
+static int tdfb_cmd_enum_get(struct sof_ipc_ctrl_data *cdata, struct tdfb_comp_data *cd)
+{
+	int j;
+
+	/* Fail if wrong index in control, needed if several in same type */
+	if (cdata->index != CTRL_INDEX_DIRECTION)
+		return -EINVAL;
+
+	for (j = 0; j < cdata->num_elems; j++)
+		cdata->chanv[j].value = cd->az_value;
+
+	return 0;
+}
+
+static int tdfb_cmd_get_value(struct comp_dev *dev, struct sof_ipc_ctrl_data *cdata)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+	int ret = 0;
+
+	switch (cdata->cmd) {
+	case SOF_CTRL_CMD_ENUM:
+		comp_dbg(dev, "tdfb_cmd_get_value(), SOF_CTRL_CMD_ENUM index=%d", cdata->index);
+		ret = tdfb_cmd_enum_get(cdata, cd);
+		break;
+	case SOF_CTRL_CMD_SWITCH:
+		comp_dbg(dev, "tdfb_cmd_get_value(), SOF_CTRL_CMD_SWITCH index=%d", cdata->index);
+		ret = tdfb_cmd_switch_get(cdata, cd);
+		break;
+	default:
+		comp_err(dev, "tdfb_cmd_get_value() error: invalid cdata->cmd");
 		ret = -EINVAL;
 		break;
 	}
@@ -344,11 +519,57 @@ static int tdfb_cmd_set_data(struct comp_dev *dev,
 
 	switch (cdata->cmd) {
 	case SOF_CTRL_CMD_BINARY:
-		comp_info(dev, "tdfb_cmd_set_data(), SOF_CTRL_CMD_BINARY");
+		comp_dbg(dev, "tdfb_cmd_set_data(), SOF_CTRL_CMD_BINARY");
 		ret = comp_data_blob_set_cmd(cd->model_handler, cdata);
 		break;
 	default:
 		comp_err(dev, "tdfb_cmd_set_data() error: invalid cdata->cmd");
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+
+static int tdfb_cmd_enum_set(struct sof_ipc_ctrl_data *cdata, struct tdfb_comp_data *cd)
+{
+	if (cdata->index != CTRL_INDEX_DIRECTION || cdata->num_elems != 1)
+		return -EINVAL;
+
+	if (cdata->chanv[0].value > SOF_TDFB_MAX_ANGLES)
+		return -EINVAL;
+
+	cd->az_value = cdata->chanv[0].value;
+	cd->update = true;
+	return 0;
+}
+
+static int tdfb_cmd_switch_set(struct sof_ipc_ctrl_data *cdata, struct tdfb_comp_data *cd)
+{
+	if (cdata->index != CTRL_INDEX_ONOFF || cdata->num_elems != 1)
+		return -EINVAL;
+
+	cd->beam_on = cdata->chanv[0].value;
+	cd->update = true;
+	return 0;
+}
+
+static int tdfb_cmd_set_value(struct comp_dev *dev, struct sof_ipc_ctrl_data *cdata)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+	int ret = 0;
+
+	switch (cdata->cmd) {
+	case SOF_CTRL_CMD_ENUM:
+		comp_dbg(dev, "tdfb_cmd_set_value(), SOF_CTRL_CMD_ENUM index=%d", cdata->index);
+		ret = tdfb_cmd_enum_set(cdata, cd);
+		break;
+	case SOF_CTRL_CMD_SWITCH:
+		comp_dbg(dev, "tdfb_cmd_set_value(), SOF_CTRL_CMD_SWITCH index=%d", cdata->index);
+		ret = tdfb_cmd_switch_set(cdata, cd);
+		break;
+	default:
+		comp_err(dev, "tdfb_cmd_set_value() error: invalid cdata->cmd");
 		ret = -EINVAL;
 		break;
 	}
@@ -367,10 +588,20 @@ static int tdfb_cmd(struct comp_dev *dev, int cmd, void *data,
 
 	switch (cmd) {
 	case COMP_CMD_SET_DATA:
+		comp_dbg(dev, "tdfb_cmd(): COMP_CMD_SET_DATA");
 		ret = tdfb_cmd_set_data(dev, cdata);
 		break;
 	case COMP_CMD_GET_DATA:
+		comp_dbg(dev, "tdfb_cmd(): COMP_CMD_GET_DATA");
 		ret = tdfb_cmd_get_data(dev, cdata, max_data_size);
+		break;
+	case COMP_CMD_SET_VALUE:
+		comp_dbg(dev, "tdfb_cmd(): COMP_CMD_SET_VALUE");
+		ret = tdfb_cmd_set_value(dev, cdata);
+		break;
+	case COMP_CMD_GET_VALUE:
+		comp_dbg(dev, "tdfb_cmd(): COMP_CMD_GET_VALUE");
+		ret = tdfb_cmd_get_value(dev, cdata);
 		break;
 	default:
 		comp_err(dev, "tdfb_cmd() error: invalid command");
@@ -424,6 +655,16 @@ static int tdfb_copy(struct comp_dev *dev)
 		}
 	}
 
+	/* Handle enum controls */
+	if (cd->update) {
+		cd->update = false;
+		ret = tdfb_setup(cd, sourceb->stream.channels, sinkb->stream.channels);
+		if (ret < 0) {
+			comp_err(dev, "tdfb_copy(), failed FIR setup");
+			return ret;
+		}
+	}
+
 	/* Get source, sink, number of frames etc. to process. */
 	comp_get_copy_limits(sourceb, sinkb, &cl);
 
@@ -440,6 +681,8 @@ static int tdfb_copy(struct comp_dev *dev)
 			     n * cl.source_frame_bytes,
 			     n * cl.sink_frame_bytes);
 	}
+
+	/* TODO: Update direction of arrival estimate */
 
 	return 0;
 }

thesofproject/sof

src/audio/tdfb/tdfb.c
@@ -32,6 +32,15 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/* The driver assigns running numbers for control index. If there's single control of
+ * type switch, enum, binary they all have index 0.
+ */
+#define CTRL_INDEX_PROCESS		0	/* switch */
+#define CTRL_INDEX_DIRECTION		1	/* switch */
+#define CTRL_INDEX_AZIMUTH		0	/* enum */
+#define CTRL_INDEX_AZIMUTH_ESTIMATE	1	/* enum */
+#define CTRL_INDEX_FILTERBANK		0	/* bytes */
+
 static const struct comp_driver comp_tdfb;
 
 /* dd511749-d9fa-455c-b3a7-13585693f1af */
@@ -39,6 +48,42 @@ DECLARE_SOF_RT_UUID("tdfb", tdfb_uuid,  0xdd511749, 0xd9fa, 0x455c, 0xb3, 0xa7,
 		    0x13, 0x58, 0x56, 0x93, 0xf1, 0xaf);
 
 DECLARE_TR_CTX(tdfb_tr, SOF_UUID(tdfb_uuid), LOG_LEVEL_INFO);
+
+/* IPC */
+
+static int init_get_ctl_ipc(struct comp_dev *dev)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+	int comp_id = dev_comp_id(dev);
+
+	cd->ctrl_data = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, TDFB_GET_CTRL_DATA_SIZE);
+	if (!cd->ctrl_data)
+		return -ENOMEM;
+
+	cd->ctrl_data->rhdr.hdr.cmd = SOF_IPC_GLB_COMP_MSG | SOF_IPC_COMP_GET_VALUE | comp_id;
+	cd->ctrl_data->rhdr.hdr.size = TDFB_GET_CTRL_DATA_SIZE;
+	cd->msg = ipc_msg_init(cd->ctrl_data->rhdr.hdr.cmd, cd->ctrl_data->rhdr.hdr.size);
+
+	cd->ctrl_data->comp_id = comp_id;
+	cd->ctrl_data->type = SOF_CTRL_TYPE_VALUE_COMP_GET;
+	cd->ctrl_data->cmd = SOF_CTRL_CMD_ENUM;
+	cd->ctrl_data->index = CTRL_INDEX_AZIMUTH_ESTIMATE;
+	cd->ctrl_data->num_elems = 0;
+	return 0;
+}
+
+static void send_get_ctl_ipc(struct comp_dev *dev)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+
+#if TDFB_ADD_DIRECTION_TO_GET_CMD
+	cd->ctrl_data->chanv[0].channel = 0;
+	cd->ctrl_data->chanv[0].value = cd->az_value_estimate;
+	cd->ctrl_data->num_elems = 1;
+#endif
+
+	ipc_msg_send(cd->msg, cd->ctrl_data, false);
+}
 
 /*
  * The optimized FIR functions variants need to be updated into function
@@ -117,14 +162,38 @@ static void tdfb_free_delaylines(struct tdfb_comp_data *cd)
 		fir[i].delay = NULL;
 }
 
+static int16_t *tdfb_filter_seek(struct sof_tdfb_config *config, int num_filters)
+{
+	struct sof_fir_coef_data *coef_data;
+	int i;
+	int16_t *coefp = ASSUME_ALIGNED(&config->data[0], 2); /* 2 for 16 bits data */
+
+	/* Note: FIR coefficients are int16_t. An uint_8 type pointer coefp
+	 * is used for jumping in the flexible array member structs coef_data.
+	 */
+	for (i = 0; i < num_filters; i++) {
+		coef_data = (struct sof_fir_coef_data *)coefp;
+		coefp = coef_data->coef + coef_data->length;
+	}
+
+	return coefp;
+}
+
 static int tdfb_init_coef(struct tdfb_comp_data *cd, int source_nch,
 			  int sink_nch)
 {
 	struct sof_fir_coef_data *coef_data;
 	struct sof_tdfb_config *config = cd->config;
+	int16_t *output_channel_mix_beam_off = NULL;
 	int16_t *coefp;
 	int size_sum = 0;
+	int min_delta_idx; /* Index to beam angle with smallest delta vs. target */
+	int min_delta; /* Smallest angle difference found in degrees */
 	int max_ch;
+	int num_filters;
+	int target_az; /* Target azimuth angle in degrees */
+	int delta; /* Target minus found angle in degrees absolute value */
+	int idx;
 	int s;
 	int i;
 
@@ -148,7 +217,96 @@ static int tdfb_init_coef(struct tdfb_comp_data *cd, int source_nch,
 		return -EINVAL;
 	}
 
-	coefp = ASSUME_ALIGNED(&config->data[0], 2);
+	if (config->num_angles > SOF_TDFB_MAX_ANGLES) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid num_angles %d",
+			    config->num_angles);
+		return -EINVAL;
+	}
+
+	if (config->beam_off_defined > 1) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid beam_off_defined %d",
+			    config->beam_off_defined);
+		return -EINVAL;
+	}
+
+	if (config->num_mic_locations > SOF_TDFB_MAX_MICROPHONES) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid num_mic_locations %d",
+			    config->num_mic_locations);
+		return -EINVAL;
+	}
+
+	/* In SOF v1.6 - 1.8 based beamformer topologies the multiple angles, mic locations,
+	 * and beam on/off switch were not defined. Return error if such configuration is seen.
+	 * A most basic blob has num_angles equals 1. Mic locations data is optional.
+	 */
+	if (config->num_angles == 0 && config->num_mic_locations == 0) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), ABI version less than 3.19.1 is not supported.");
+		return -EINVAL;
+	}
+
+	/* Skip filter coefficients */
+	num_filters = config->num_filters * (config->num_angles + config->beam_off_defined);
+	coefp = tdfb_filter_seek(config, num_filters);
+
+	/* Get shortcuts to input and output configuration */
+	cd->input_channel_select = coefp;
+	coefp += config->num_filters;
+	cd->output_channel_mix = coefp;
+	coefp += config->num_filters;
+	cd->output_stream_mix = coefp;
+	coefp += config->num_filters;
+
+	/* Check if there's beam-off configured, then get pointers to beam angles data
+	 * and microphone locations. Finally check that size matches.
+	 */
+	if (config->beam_off_defined) {
+		output_channel_mix_beam_off = coefp;
+		coefp += config->num_filters;
+	}
+	cd->filter_angles = (struct sof_tdfb_angle *)coefp;
+	cd->mic_locations = (struct sof_tdfb_mic_location *)
+		(&cd->filter_angles[config->num_angles]);
+	if ((uint8_t *)&cd->mic_locations[config->num_mic_locations] !=
+	    (uint8_t *)config + config->size) {
+		comp_cl_err(&comp_tdfb, "tdfb_init_coef(), invalid config size");
+		return -EINVAL;
+	}
+
+	/* Skip to requested coefficient set */
+	min_delta = 360;
+	min_delta_idx = 0;
+	target_az = cd->az_value * config->angle_enum_mult + config->angle_enum_offs;
+	if (target_az > 180)
+		target_az -= 360;
+
+	if (target_az < -180)
+		target_az += 360;
+
+	for (i = 0; i < config->num_angles; i++) {
+		delta = ABS(target_az - cd->filter_angles[i].azimuth);
+		if (delta < min_delta) {
+			min_delta = delta;
+			min_delta_idx = i;
+		}
+	}
+
+	idx = cd->filter_angles[min_delta_idx].filter_index;
+	if (cd->beam_on) {
+		comp_cl_info(&comp_tdfb, "tdfb_init_coef(), angle request %d, found %d, idx %d",
+			     target_az, cd->filter_angles[min_delta_idx].azimuth, idx);
+	} else if (config->beam_off_defined) {
+		cd->output_channel_mix = output_channel_mix_beam_off;
+		idx = config->num_filters * config->num_angles;
+		comp_cl_info(&comp_tdfb, "tdfb_init_coef(), configure beam off");
+	} else {
+		comp_cl_info(&comp_tdfb, "tdfb_init_coef(), beam off is not defined, using filter %d, idx %d",
+			     cd->filter_angles[min_delta_idx].azimuth, idx);
+	}
+
+	/* Seek to proper filter for requested angle or beam off configuration */
+	coefp = tdfb_filter_seek(config, idx);
+
+	/* Initialize filter bank */
 	for (i = 0; i < config->num_filters; i++) {
 		/* Get delay line size */
 		coef_data = (struct sof_fir_coef_data *)coefp;
@@ -165,13 +323,8 @@ static int tdfb_init_coef(struct tdfb_comp_data *cd, int source_nch,
 		 * filter.
 		 */
 		fir_init_coef(&cd->fir[i], coef_data);
-		coefp += SOF_FIR_COEF_NHEADER + coef_data->length;
+		coefp = coef_data->coef + coef_data->length;
 	}
-
-	/* Get shortcuts to input and output configuration */
-	cd->input_channel_select = coefp;
-	cd->output_channel_mix = coefp + config->num_filters;
-	cd->output_stream_mix = coefp + 2 * config->num_filters;
 
 	/* Find max used input channel */
 	max_ch = 0;
@@ -208,9 +361,6 @@ static int tdfb_setup(struct tdfb_comp_data *cd, int source_nch, int sink_nch)
 {
 	int delay_size;
 
-	/* Free existing FIR channels data if it was allocated */
-	tdfb_free_delaylines(cd);
-
 	/* Set coefficients for each channel from coefficient blob */
 	delay_size = tdfb_init_coef(cd, source_nch, sink_nch);
 	if (delay_size < 0)
@@ -222,19 +372,25 @@ static int tdfb_setup(struct tdfb_comp_data *cd, int source_nch, int sink_nch)
 	if (!delay_size)
 		return 0;
 
-	/* Allocate all FIR channels data in a big chunk and clear it */
-	cd->fir_delay = rballoc(0, SOF_MEM_CAPS_RAM, delay_size);
-	if (!cd->fir_delay) {
-		comp_cl_err(&comp_tdfb, "tdfb_setup(), delay allocation failed for size %d",
-			    delay_size);
-		return -ENOMEM;
-	}
+	if (delay_size > cd->fir_delay_size) {
+		/* Free existing FIR channels data if it was allocated */
+		tdfb_free_delaylines(cd);
 
-	memset(cd->fir_delay, 0, delay_size);
-	cd->fir_delay_size = delay_size;
+		/* Allocate all FIR channels data in a big chunk and clear it */
+		cd->fir_delay = rballoc(0, SOF_MEM_CAPS_RAM, delay_size);
+		if (!cd->fir_delay) {
+			comp_cl_err(&comp_tdfb, "tdfb_setup(), delay allocation failed for size %d",
+				    delay_size);
+			return -ENOMEM;
+		}
+
+		memset(cd->fir_delay, 0, delay_size);
+		cd->fir_delay_size = delay_size;
+	}
 
 	/* Assign delay line to all channel filters */
 	tdfb_init_delay(cd);
+
 	return 0;
 }
 
@@ -273,9 +429,18 @@ static struct comp_dev *tdfb_new(const struct comp_driver *drv,
 
 	comp_set_drvdata(dev, cd);
 
-	cd->tdfb_func = NULL;
-	cd->fir_delay = NULL;
-	cd->fir_delay_size = 0;
+	/* Defaults for processing function pointer tdfb_func, fir_delay
+	 * pointer, are NULL. Fir_delay_size is zero from rzalloc().
+	 */
+
+	/* Defaults for enum controls are zeros from rzalloc()
+	 * az_value is zero, beam off is false, and update is false.
+	 */
+
+	/* Initialize IPC for direction of arrival estimate update */
+	ret = init_get_ctl_ipc(dev);
+	if (ret)
+		goto cd_fail;
 
 	/* Handler for configuration data */
 	cd->model_handler = comp_data_blob_handler_new(dev);
@@ -298,7 +463,7 @@ static struct comp_dev *tdfb_new(const struct comp_driver *drv,
 	return dev;
 
 cd_fail:
-	comp_data_blob_handler_free(cd->model_handler);
+	comp_data_blob_handler_free(cd->model_handler); /* works for non-initialized also */
 	rfree(cd);
 fail:
 	rfree(dev);
@@ -311,9 +476,11 @@ static void tdfb_free(struct comp_dev *dev)
 
 	comp_info(dev, "tdfb_free()");
 
+	ipc_msg_free(cd->msg);
 	tdfb_free_delaylines(cd);
 	comp_data_blob_handler_free(cd->model_handler);
 
+	rfree(cd->ctrl_data);
 	rfree(cd);
 	rfree(dev);
 }
@@ -322,39 +489,141 @@ static int tdfb_cmd_get_data(struct comp_dev *dev,
 			     struct sof_ipc_ctrl_data *cdata, int max_size)
 {
 	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
-	int ret = 0;
 
-	switch (cdata->cmd) {
-	case SOF_CTRL_CMD_BINARY:
-		comp_info(dev, "tdfb_cmd_get_data(), SOF_CTRL_CMD_BINARY");
-		ret = comp_data_blob_get_cmd(cd->model_handler, cdata, max_size);
+	if (cdata->cmd == SOF_CTRL_CMD_BINARY) {
+		comp_dbg(dev, "tdfb_cmd_get_data(), SOF_CTRL_CMD_BINARY");
+		return comp_data_blob_get_cmd(cd->model_handler, cdata, max_size);
+	}
+
+	comp_err(dev, "tdfb_cmd_get_data() error: invalid cdata->cmd");
+	return -EINVAL;
+}
+
+static int tdfb_cmd_switch_get(struct sof_ipc_ctrl_data *cdata, struct tdfb_comp_data *cd)
+{
+	int j;
+
+	/* Fail if wrong index in control, needed if several in same type */
+	if (cdata->index != CTRL_INDEX_PROCESS)
+		return -EINVAL;
+
+	for (j = 0; j < cdata->num_elems; j++)
+		cdata->chanv[j].value = cd->beam_on;
+
+	return 0;
+}
+
+static int tdfb_cmd_enum_get(struct sof_ipc_ctrl_data *cdata, struct tdfb_comp_data *cd)
+{
+	int j;
+
+	switch (cdata->index) {
+	case CTRL_INDEX_AZIMUTH:
+		for (j = 0; j < cdata->num_elems; j++)
+			cdata->chanv[j].value = cd->az_value;
+
+		break;
+	case CTRL_INDEX_AZIMUTH_ESTIMATE:
+		for (j = 0; j < cdata->num_elems; j++)
+			cdata->chanv[j].value = cd->az_value_estimate;
+
 		break;
 	default:
-		comp_err(dev, "tdfb_cmd_get_data() error: invalid cdata->cmd");
-		ret = -EINVAL;
-		break;
+		return -EINVAL;
 	}
-	return ret;
+
+	return 0;
+}
+
+static int tdfb_cmd_get_value(struct comp_dev *dev, struct sof_ipc_ctrl_data *cdata)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+
+	switch (cdata->cmd) {
+	case SOF_CTRL_CMD_ENUM:
+		comp_dbg(dev, "tdfb_cmd_get_value(), SOF_CTRL_CMD_ENUM index=%d", cdata->index);
+		return tdfb_cmd_enum_get(cdata, cd);
+	case SOF_CTRL_CMD_SWITCH:
+		comp_dbg(dev, "tdfb_cmd_get_value(), SOF_CTRL_CMD_SWITCH index=%d", cdata->index);
+		return tdfb_cmd_switch_get(cdata, cd);
+	}
+
+	comp_err(dev, "tdfb_cmd_get_value() error: invalid cdata->cmd");
+	return -EINVAL;
 }
 
 static int tdfb_cmd_set_data(struct comp_dev *dev,
 			     struct sof_ipc_ctrl_data *cdata)
 {
 	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
-	int ret = 0;
 
-	switch (cdata->cmd) {
-	case SOF_CTRL_CMD_BINARY:
-		comp_info(dev, "tdfb_cmd_set_data(), SOF_CTRL_CMD_BINARY");
-		ret = comp_data_blob_set_cmd(cd->model_handler, cdata);
-		break;
-	default:
-		comp_err(dev, "tdfb_cmd_set_data() error: invalid cdata->cmd");
-		ret = -EINVAL;
-		break;
+	if (cdata->cmd == SOF_CTRL_CMD_BINARY) {
+		comp_dbg(dev, "tdfb_cmd_set_data(), SOF_CTRL_CMD_BINARY");
+		return comp_data_blob_set_cmd(cd->model_handler, cdata);
 	}
 
-	return ret;
+	comp_err(dev, "tdfb_cmd_set_data() error: invalid cdata->cmd");
+	return -EINVAL;
+}
+
+static int tdfb_cmd_enum_set(struct sof_ipc_ctrl_data *cdata, struct tdfb_comp_data *cd)
+{
+	if (cdata->num_elems != 1)
+		return -EINVAL;
+
+	if (cdata->chanv[0].value > SOF_TDFB_MAX_ANGLES)
+		return -EINVAL;
+
+	switch (cdata->index) {
+	case CTRL_INDEX_AZIMUTH:
+		cd->az_value = cdata->chanv[0].value;
+		cd->update = true;
+		break;
+	case CTRL_INDEX_AZIMUTH_ESTIMATE:
+		cd->az_value_estimate = cdata->chanv[0].value;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int tdfb_cmd_switch_set(struct sof_ipc_ctrl_data *cdata, struct tdfb_comp_data *cd)
+{
+	if (cdata->num_elems != 1)
+		return -EINVAL;
+
+	switch (cdata->index) {
+	case CTRL_INDEX_PROCESS:
+		cd->beam_on = cdata->chanv[0].value;
+		cd->update = true;
+		break;
+	case CTRL_INDEX_DIRECTION:
+		cd->direction_updates = cdata->chanv[0].value;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int tdfb_cmd_set_value(struct comp_dev *dev, struct sof_ipc_ctrl_data *cdata)
+{
+	struct tdfb_comp_data *cd = comp_get_drvdata(dev);
+
+	switch (cdata->cmd) {
+	case SOF_CTRL_CMD_ENUM:
+		comp_dbg(dev, "tdfb_cmd_set_value(), SOF_CTRL_CMD_ENUM index=%d", cdata->index);
+		return tdfb_cmd_enum_set(cdata, cd);
+	case SOF_CTRL_CMD_SWITCH:
+		comp_dbg(dev, "tdfb_cmd_set_value(), SOF_CTRL_CMD_SWITCH index=%d", cdata->index);
+		return tdfb_cmd_switch_set(cdata, cd);
+	}
+
+	comp_err(dev, "tdfb_cmd_set_value() error: invalid cdata->cmd");
+	return -EINVAL;
 }
 
 /* used to pass standard and bespoke commands (with data) to component */
@@ -362,23 +631,31 @@ static int tdfb_cmd(struct comp_dev *dev, int cmd, void *data,
 		    int max_data_size)
 {
 	struct sof_ipc_ctrl_data *cdata = ASSUME_ALIGNED(data, 4);
-	int ret = 0;
 
 	comp_info(dev, "tdfb_cmd()");
 
 	switch (cmd) {
 	case COMP_CMD_SET_DATA:
-		ret = tdfb_cmd_set_data(dev, cdata);
-		break;
+		comp_dbg(dev, "tdfb_cmd(): COMP_CMD_SET_DATA");
+		return tdfb_cmd_set_data(dev, cdata);
 	case COMP_CMD_GET_DATA:
-		ret = tdfb_cmd_get_data(dev, cdata, max_data_size);
-		break;
-	default:
-		comp_err(dev, "tdfb_cmd() error: invalid command");
-		ret = -EINVAL;
+		comp_dbg(dev, "tdfb_cmd(): COMP_CMD_GET_DATA");
+		return tdfb_cmd_get_data(dev, cdata, max_data_size);
+	case COMP_CMD_SET_VALUE:
+		comp_dbg(dev, "tdfb_cmd(): COMP_CMD_SET_VALUE");
+		return tdfb_cmd_set_value(dev, cdata);
+	case COMP_CMD_GET_VALUE:
+		comp_dbg(dev, "tdfb_cmd(): COMP_CMD_GET_VALUE");
+		return tdfb_cmd_get_value(dev, cdata);
 	}
 
-	return ret;
+	comp_err(dev, "tdfb_cmd() error: invalid command");
+	return -EINVAL;
+}
+
+/* Placeholder function for sound direction estimate */
+static void update_direction_of_arrival(struct tdfb_comp_data *cd)
+{
 }
 
 static void tdfb_process(struct comp_dev *dev, struct comp_buffer *source,
@@ -425,6 +702,16 @@ static int tdfb_copy(struct comp_dev *dev)
 		}
 	}
 
+	/* Handle enum controls */
+	if (cd->update) {
+		cd->update = false;
+		ret = tdfb_setup(cd, sourceb->stream.channels, sinkb->stream.channels);
+		if (ret < 0) {
+			comp_err(dev, "tdfb_copy(), failed FIR setup");
+			return ret;
+		}
+	}
+
 	/* Get source, sink, number of frames etc. to process. */
 	comp_get_copy_limits(sourceb, sinkb, &cl);
 
@@ -441,6 +728,11 @@ static int tdfb_copy(struct comp_dev *dev)
 			     n * cl.source_frame_bytes,
 			     n * cl.sink_frame_bytes);
 	}
+
+	/* TODO: Update direction of arrival estimate */
+	update_direction_of_arrival(cd);
+	if (cd->direction_updates && cd->direction_change)
+		send_get_ctl_ipc(dev);
 
 	return 0;
 }