9e86fe41f2203f9a5cb91c0d2cbfccd984644650
Ming Jen Tai <mingjen_tai@realtek.com>
2021-07-14 21:00:29 +0800

    Code refinement for better integration
    1. Remove code that uses RTNR_ENABLE_PROCESSING
    2. Move function implementation to C file
    3. Handle model_handler if failed
    4. Remove logging using count
    5. Handle COMP_CMD_SET_VALUE and COMP_CMD_GET_VALUE
    6. Invalidate and writeback buffers

--- src/audio/rtnr/rtnr.c
+++ src/audio/rtnr/rtnr.c
@@ -28,14 +28,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define RTNR_ENABLE_PROCESSING  1
-
-#if RTNR_ENABLE_PROCESSING
-#include <sof/audio/rtnr/rtklib/include/Ringbuffer_new.h>
 #include <sof/audio/rtnr/rtklib/include/RTK_MA_API.h>
-#endif // RTNR_ENABLE_PROCESSING
 
-#if RTNR_ENABLE_PROCESSING
 
 #define SAMPLING_RATE		16000
 #define PROCESS_RATE		16000
@@ -51,7 +45,6 @@
 #define BLOCKLENGTH	128
 #endif // SAMPLING_RATE
 
-#endif // RTNR_ENABLE_PROCESSING
 
 #define RTNR_BLK_LENGTH 4
 #define RTNR_MAX_SOURCES 1 /* Microphone stream */
@@ -61,6 +54,42 @@
 
 static const struct comp_driver comp_rtnr;
 
+/** \brief RTNR processing functions map item. */
+struct rtnr_func_map {
+	enum sof_ipc_frame fmt; /**< source frame format */
+	rtnr_func func; /**< processing function */
+};
+
+#if CONFIG_FORMAT_S16LE
+static void rtnr_s16_default(struct comp_dev *dev, const struct audio_stream **sources,
+			    struct audio_stream *sink, int frames);
+#endif /* CONFIG_FORMAT_S16LE */
+
+#if CONFIG_FORMAT_S24LE
+static void rtnr_s24_default(struct comp_dev *dev, const struct audio_stream **sources,
+			    struct audio_stream *sink, int frames);
+#endif /* CONFIG_FORMAT_S24LE */
+
+#if CONFIG_FORMAT_S32LE
+static void rtnr_s32_default(struct comp_dev *dev, const struct audio_stream **sources,
+			    struct audio_stream *sink, int frames);
+#endif /* CONFIG_FORMAT_S32LE */
+
+/* Processing functions table */
+const struct rtnr_func_map rtnr_fnmap[] = {
+#if CONFIG_FORMAT_S16LE
+	{ SOF_IPC_FRAME_S16_LE, rtnr_s16_default },
+#endif // CONFIG_FORMAT_S16LE
+#if CONFIG_FORMAT_S24LE
+	{ SOF_IPC_FRAME_S24_4LE, rtnr_s24_default },
+#endif // CONFIG_FORMAT_S24LE
+#if CONFIG_FORMAT_S32LE
+	{ SOF_IPC_FRAME_S32_LE, rtnr_s32_default },
+#endif// CONFIG_FORMAT_S32LE
+};
+
+const size_t rtnr_fncount = ARRAY_SIZE(rtnr_fnmap);
+
 /* UUID 5c7ca334-e15d-11eb-ba80-0242ac130004 */
 DECLARE_SOF_RT_UUID("rtnr", rtnr_uuid, 0x5c7ca334, 0xe15d, 0x11eb, 0xba, 0x80,
 		    0x02, 0x42, 0xac, 0x13, 0x00, 0x04);
@@ -69,7 +98,7 @@ DECLARE_TR_CTX(rtnr_tr, SOF_UUID(rtnr_uuid), LOG_LEVEL_INFO);
 
 /* Generic processing */
 
-#if RTNR_ENABLE_PROCESSING
+
 void myPrintf(int a, int b, int c, int d, int e)
 {
 	if (a == 0xa)
@@ -98,7 +127,6 @@ void rtk_rfree(void *ptr)
 {
 	rfree(ptr);
 }
-#endif // RTNR_ENABLE_PROCESSING
 
 #if CONFIG_FORMAT_S16LE
 
@@ -107,43 +135,12 @@ static void rtnr_s16_default(struct comp_dev *dev, const struct audio_stream **s
 
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
-#if RTNR_ENABLE_PROCESSING
 	RTKMA_API_S16_Default((void *)&cd->rtk_agl, sources, sink, frames,
 	0, 0, 0,
 	0, 0);
-#else
-	int64_t mix;
-	int16_t out;
-	int16_t *x;
-	int16_t *y;
-	int in_idx;
-	int i;
-	int j;
-	int nch;
-	int out_idx = 0;
-	int in_source_index;
-
-	in_source_index = RTNR_MIC_SOURCE_INDEX;
-	nch = sources[in_source_index]->channels;
-
-	/* Input from selected input channel, same output to every channel */
-	in_idx = cd->source_channel;
-	for (i = 0; i < frames; i++) {
-		x = audio_stream_read_frag_s16(sources[in_source_index], in_idx);
-		mix = *x << 16;
-		in_idx += nch;
-
-		out = sat_int16(mix >> 16);
-		for (j = 0; j < nch; j++) {
-			y = audio_stream_write_frag_s16(sink, out_idx);
-			*y = out;
-			out_idx++;
-		}
-	}
-#endif // RTNR_ENABLE_PROCESSING
 }
 
-#endif // CONFIG_FORMAT_S16LE
+#endif /* CONFIG_FORMAT_S16LE */
 
 #if CONFIG_FORMAT_S24LE
 
@@ -152,39 +149,12 @@ static void rtnr_s24_default(struct comp_dev *dev, const struct audio_stream **s
 
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
-#if RTNR_ENABLE_PROCESSING
 	RTKMA_API_S24_Default((void *)&cd->rtk_agl, sources, sink, frames,
 	0, 0, 0,
 	0, 0);
-#else
-	int64_t mix;
-	int32_t out;
-	int32_t *x;
-	int32_t *y;
-	int in_idx;
-	int i;
-	int j;
-	int nch = sources[0]->channels;
-	int out_idx = 0;
-
-	/* Input from selected input channel, same output to every channel */
-	in_idx = cd->source_channel;
-	for (i = 0; i < frames; i++) {
-		x = audio_stream_read_frag_s32(sources[0], in_idx);
-		mix = *x << 8;
-		in_idx += nch;
-
-		out = sat_int24(mix >> 8);
-		for (j = 0; j < nch; j++) {
-			y = audio_stream_write_frag_s32(sink, out_idx);
-			*y = out;
-			out_idx++;
-		}
-	}
-#endif // RTNR_ENABLE_PROCESSING
 }
 
-#endif // CONFIG_FORMAT_S24LE
+#endif /* CONFIG_FORMAT_S24LE */
 
 #if CONFIG_FORMAT_S32LE
 
@@ -193,54 +163,31 @@ static void rtnr_s32_default(struct comp_dev *dev, const struct audio_stream **s
 
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
-#if RTNR_ENABLE_PROCESSING
 	RTKMA_API_S32_Default((void *)&cd->rtk_agl, sources, sink, frames,
 	0, 0, 0,
 	0, 0);
-#else
-	int64_t mix;
-	int32_t out;
-	int32_t *x;
-	int32_t *y;
-	int in_idx;
-	int i;
-	int j;
-	int nch = sources[0]->channels;
-	int out_idx = 0;
-
-	/* Input from selected input channel, same output to every channel */
-	in_idx = cd->source_channel;
-	for (i = 0; i < frames; i++) {
-		x = audio_stream_read_frag_s32(sources[0], in_idx);
-		mix = *x;
-		in_idx += nch;
-
-		out = sat_int32(mix);
-		for (j = 0; j < nch; j++) {
-			y = audio_stream_write_frag_s32(sink, out_idx);
-			*y = out;
-			out_idx++;
-		}
-	}
-#endif // RTNR_ENABLE_PROCESSING
 }
 
-#endif // CONFIG_FORMAT_S32LE
+#endif /* CONFIG_FORMAT_S32LE */
 
-/* Processing functions table */
-const struct rtnr_func_map rtnr_fnmap[] = {
-#if CONFIG_FORMAT_S16LE
-	{ SOF_IPC_FRAME_S16_LE, rtnr_s16_default },
-#endif // CONFIG_FORMAT_S16LE
-#if CONFIG_FORMAT_S24LE
-	{ SOF_IPC_FRAME_S24_4LE, rtnr_s24_default },
-#endif // CONFIG_FORMAT_S24LE
-#if CONFIG_FORMAT_S32LE
-	{ SOF_IPC_FRAME_S32_LE, rtnr_s32_default },
-#endif// CONFIG_FORMAT_S32LE
-};
 
-const size_t rtnr_fncount = ARRAY_SIZE(rtnr_fnmap);
+/**
+ * \brief Retrieves an RTNR processing function matching
+ *	  the source buffer's frame format.
+ * \param fmt the frames' format of the source and sink buffers
+ */
+static rtnr_func rtnr_find_func(enum sof_ipc_frame fmt)
+{
+	int i;
+
+	/* Find suitable processing function from map */
+	for (i = 0; i < rtnr_fncount; i++) {
+		if (fmt == rtnr_fnmap[i].fmt)
+			return rtnr_fnmap[i].func;
+	}
+
+	return NULL;
+}
 
 /*
  * End of RTNR setup code. Next the standard component methods.
@@ -274,31 +221,31 @@ static struct comp_dev *rtnr_new(const struct comp_driver *drv,
 	cd->model_handler = comp_data_blob_handler_new(dev);
 	if (!cd->model_handler) {
 		comp_cl_err(&comp_rtnr, "rtnr_new(): comp_data_blob_handler_new() failed.");
-		goto err;
+		goto cd_fail;
 	}
 
 	/* Get configuration data */
 	ret = comp_init_data_blob(cd->model_handler, bs, ipc_rtnr->data);
 	if (ret < 0) {
 		comp_cl_err(&comp_rtnr, "rtnr_new(): comp_init_data_blob() failed.");
-		goto err;
+		goto cd_fail;
 	}
 
 	/* Component defaults */
 	cd->source_channel = 0;
-	cd->count = 0;
 
-	#if RTNR_ENABLE_PROCESSING
 	RTKMA_API_Context_Create((void *)&cd->rtk_agl);
-	#endif // RTNR_ENABLE_PROCESSING
 
 	/* Done. */
 	dev->state = COMP_STATE_READY;
 	return dev;
 
+cd_fail:
+	comp_data_blob_handler_free(cd->model_handler);
+	rfree(cd);
+
 err:
 	rfree(dev);
-	rfree(cd);
 	return NULL;
 }
 
@@ -308,10 +255,10 @@ static void rtnr_free(struct comp_dev *dev)
 
 	comp_info(dev, "rtnr_free()");
 
-	#if RTNR_ENABLE_PROCESSING
+	comp_data_blob_handler_free(cd->model_handler);
+
 	RTKMA_API_Context_Free((void *)&cd->rtk_agl);
 	free_RtkMemoryPool();
-	#endif // RTNR_ENABLE_PROCESSING
 
 	rfree(cd);
 	rfree(dev);
@@ -343,7 +290,7 @@ static int rtnr_cmd_get_data(struct comp_dev *dev,
 		comp_info(dev, "rtnr_cmd_get_data(), SOF_CTRL_CMD_BINARY");
 		break;
 	default:
-		comp_err(dev, "rtnr_cmd_set_data() error: invalid command %d", cdata->cmd);
+		comp_err(dev, "rtnr_cmd_get_data() error: invalid command %d", cdata->cmd);
 		ret = -EINVAL;
 		break;
 	}
@@ -367,6 +314,42 @@ static int rtnr_cmd_set_data(struct comp_dev *dev,
 	return ret;
 }
 
+static int32_t rtnr_cmd_get_value(struct comp_dev *dev, struct sof_ipc_ctrl_data *cdata)
+{
+	int32_t ret = 0;
+
+	switch (cdata->cmd) {
+	case SOF_CTRL_CMD_SWITCH:
+		comp_dbg(dev, "rtnr_cmd_get_value(), SOF_CTRL_CMD_SWITCH, cdata->comp_id = %u",
+			 cdata->comp_id);
+		break;
+	default:
+		comp_err(dev, "rtnr_cmd_get_value() error: invalid cdata->cmd %d", cdata->cmd);
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
+}
+
+static int32_t rtnr_cmd_set_value(struct comp_dev *dev, struct sof_ipc_ctrl_data *cdata)
+{
+	int32_t ret = 0;
+
+	switch (cdata->cmd) {
+	case SOF_CTRL_CMD_SWITCH:
+		comp_dbg(dev, "rtnr_cmd_set_value(), SOF_CTRL_CMD_SWITCH, cdata->comp_id = %u",
+			 cdata->comp_id);
+		break;
+
+	default:
+		comp_err(dev, "rtnr_cmd_set_value() error: invalid cdata->cmd");
+		ret = -EINVAL;
+		break;
+	}
+
+	return ret;
+}
+
 /* used to pass standard and bespoke commands (with data) to component */
 static int rtnr_cmd(struct comp_dev *dev, int cmd, void *data,
 		   int max_data_size)
@@ -383,6 +366,15 @@ static int rtnr_cmd(struct comp_dev *dev, int cmd, void *data,
 	case COMP_CMD_GET_DATA:
 		ret = rtnr_cmd_get_data(dev, cdata, max_data_size);
 		break;
+	case COMP_CMD_SET_VALUE:
+		ret = rtnr_cmd_set_value(dev, cdata);
+		break;
+	case COMP_CMD_GET_VALUE:
+		ret = rtnr_cmd_get_value(dev, cdata);
+		break;
+	default:
+		comp_err(dev, "rtnr_cmd() error: invalid command");
+		return -EINVAL;
 	}
 
 	return ret;
@@ -434,13 +426,12 @@ static int rtnr_copy(struct comp_dev *dev)
 	if (num_sources == 0 || num_sources > 2)
 		return -EINVAL;
 
-	#if RTNR_ENABLE_PROCESSING
-	/* put dummy data into output queue*/
+	/* put empty data into output queue*/
 	int nch = sources_stream[0]->channels;
-	int copy_size = BLOCKLENGTH*nch*sizeof(int32_t);
+	/* RTKMA uses 32 bits internal storage */
+	int copy_size = BLOCKLENGTH * nch * sizeof(int32_t);
 
 	RTKMA_API_First_Copy((void *)&cd->rtk_agl, copy_size);
-	#endif // RTNR_ENABLE_PROCESSING
 
 	sink = list_first_item(&dev->bsink_list, struct comp_buffer, source_list);
 	buffer_lock(sink, &flags);
@@ -451,31 +442,31 @@ static int rtnr_copy(struct comp_dev *dev)
 	num_blocks = frames / RTNR_BLK_LENGTH;
 	frames = num_blocks * RTNR_BLK_LENGTH;
 
-	if (cd->count) {
-		cd->count--;
-	} else {
-		for (int i = 0; i < num_sources; ++i)
-			comp_info(dev, "rtnr_copy() sources[%d]->id: %d, frames = %d",
+	for (int i = 0; i < num_sources; ++i)
+		comp_dbg(dev, "rtnr_copy() sources[%d]->id: %d, frames = %d",
 				i, sources[i]->id, frames);
-		cd->count = 249; // about once per second when 16kHz
-	}
 
 	if (frames) {
+		source_bytes = frames * audio_stream_frame_bytes(sources_stream[0]);
+		sink_bytes = frames * audio_stream_frame_bytes(&sink->stream);
+
+		buffer_invalidate(sources[0], source_bytes);
+
 		/* Run processing function */
 		cd->rtnr_func(dev, sources_stream, &sink->stream, frames);
 
+		buffer_writeback(sink, sink_bytes);
+
+		/*real process function of rtk ma*/
+		/* RTKMA uses 32 bits internal storage */
+		copy_size = BLOCKLENGTH * MicNum * sizeof(int32_t);
+		RTKMA_API_Process((void *)&cd->rtk_agl, copy_size, 0);
+
 		/* Track consume and produce */
-		source_bytes = frames * audio_stream_frame_bytes(sources_stream[0]);
-		sink_bytes = frames * audio_stream_frame_bytes(&sink->stream);
 		comp_update_buffer_consume(sources[0], source_bytes);
 		comp_update_buffer_produce(sink, sink_bytes);
 	}
 
-	#if RTNR_ENABLE_PROCESSING
-	/*real process function of rtk ma*/
-	copy_size = BLOCKLENGTH*MicNum*sizeof(int32_t);
-	RTKMA_API_Process((void *)&cd->rtk_agl, copy_size, 0);
-	#endif // RTNR_ENABLE_PROCESSING
 
 	return 0;
 }
@@ -507,10 +498,8 @@ static int rtnr_prepare(struct comp_dev *dev)
 		goto err;
 	}
 
-#if RTNR_ENABLE_PROCESSING
 	/* Clear in/out buffers */
 	RTKMA_API_Prepare((void *)&cd->rtk_agl);
-#endif // RTNR_ENABLE_PROCESSING
 
 	/* Initialize RTNR */
 	return 0;
@@ -522,7 +511,9 @@ err:
 
 static int rtnr_reset(struct comp_dev *dev)
 {
+	struct comp_data *cd = comp_get_drvdata(dev);
 	comp_info(dev, "rtnr_reset()");
+	cd->rtnr_func = NULL;
 	comp_set_state(dev, COMP_TRIGGER_RESET);
 	return 0;
 }
@@ -550,5 +541,6 @@ UT_STATIC void sys_comp_rtnr_init(void)
 {
 	comp_register(platform_shared_get(&comp_rtnr_info, sizeof(comp_rtnr_info)));
 }
+
 
 DECLARE_MODULE(sys_comp_rtnr_init);
--- src/include/sof/audio/rtnr/rtnr.h
+++ src/include/sof/audio/rtnr/rtnr.h
@@ -29,7 +29,6 @@ struct comp_data {
 	enum sof_ipc_frame ref_format;
 	int source_channel;
 	int reference_channel;
-	int count;
 	int ref_shift;
 	bool ref_32bits;
 	bool ref_active;
@@ -37,37 +36,8 @@ struct comp_data {
 	void *rtk_agl;
 };
 
-/** \brief RTNR processing functions map item. */
-struct rtnr_func_map {
-	enum sof_ipc_frame fmt; /**< source frame format */
-	rtnr_func func; /**< processing function */
-};
-
-/** \brief Map of formats with dedicated processing functions. */
-extern const struct rtnr_func_map rtnr_fnmap[];
-
-/** \brief Number of processing functions. */
-extern const size_t rtnr_fncount;
-
-/**
- * \brief Retrieves an RTNR processing function matching
- *	  the source buffer's frame format.
- * \param fmt the frames' format of the source and sink buffers
- */
-static inline rtnr_func rtnr_find_func(enum sof_ipc_frame fmt)
-{
-	int i;
-
-	/* Find suitable processing function from map */
-	for (i = 0; i < rtnr_fncount; i++) {
-		if (fmt == rtnr_fnmap[i].fmt)
-			return rtnr_fnmap[i].func;
-	}
-
-	return NULL;
-}
-
 void myPrintf(int a, int b, int c, int d, int e);
 void *rtk_rballoc(unsigned int flags, unsigned int caps, unsigned int bytes);
 void rtk_rfree(void *ptr);
+
 #endif /* __SOF_AUDIO_RTNR_RTNR_H__ */
