c52e8dbba265ded56dd6eaed77023151b5712648
Rander Wang <rander.wang@intel.com>
2021-07-19 13:34:43 +0800

    ipc4: alh: add alh support for ipc4
    Stream id is included in ipc4 config and fw constructs
    hw_params for alh based on this information.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Rander Wang <rander.wang@intel.com>

---
+++ src/include/ipc4/alh.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2021 Intel Corporation. All rights reserved.
+ */
+
+/*
+ * This file contains structures that are exact copies of an existing ABI used
+ * by IOT middleware. They are Intel specific and will be used by one middleware.
+ *
+ * Some of the structures may contain programming implementations that makes them
+ * unsuitable for generic use and general usage.
+ *
+ * This code is mostly copied "as-is" from existing C++ interface files hence the use of
+ * different style in places. The intention is to keep the interface as close as possible to
+ * original so it's easier to track changes with IPC host code.
+ */
+
+/**
+ * \file include/ipc4/alh.h
+ * \brief IPC4 global definitions.
+ * NOTE: This ABI uses bit fields and is non portable.
+ */
+
+#ifndef __SOF_IPC4_ALH_H__
+#define __SOF_IPC4_ALH_H__
+
+#include <stdint.h>
+#include <ipc4/gateway.h>
+
+#define IPC4_ALH_MAX_NUMBER_OF_GTW 16
+#define IPC4_ALH_DAI_INDEX_OFFSET 7
+
+struct ipc4_alh_multi_gtw_cfg {
+	/* Number of single channels (valid items in mapping array). */
+	uint32_t count;
+	/* Single to multi aggregation mapping item. */
+	struct {
+		/* Vindex of a single ALH channel aggregated. */
+		uint32_t alh_id;
+		/* Channel mask */
+		uint32_t channel_mask;
+	} mapping[IPC4_ALH_MAX_NUMBER_OF_GTW]; /* < Mapping items */
+} __attribute__((packed, aligned(4)));
+
+struct sof_alh_configuration_blob {
+	union ipc4_gateway_attributes gtw_attributes;
+	struct ipc4_alh_multi_gtw_cfg alh_cfg;
+} __attribute__((packed, aligned(4)));
+
+#endif
