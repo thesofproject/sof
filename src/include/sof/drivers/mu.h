b8d35715b5bbd76523e168f1a130da0a7b1a2cba
Zhang Peng <peng.zhang_8@nxp.com>
2021-07-22 21:01:36 +0800

    drivers: imx: ipc: Make MU support imx8ulp
    Make mu implementation generic. imx8ulp platform has a new version
    of mu, then update mu for preparing to support imx8ulp.
    
    Signed-off-by: Zhang Peng <peng.zhang_8@nxp.com>

--- src/include/sof/drivers/mu.h
+++ src/include/sof/drivers/mu.h
@@ -13,27 +13,61 @@
 #include <sof/bit.h>
 #include <stdint.h>
 
+enum imx_mu_type {
+	IMX_MU_V1,
+	IMX_MU_V2,
+};
+
+#ifdef CONFIG_IMX8ULP
+#define IMX_MU_VERSION IMX_MU_V2
+#else
+#define IMX_MU_VERSION IMX_MU_V1
+#endif
+
+enum imx_mu_xcr {
+	IMX_MU_GIER = 0x110,
+	IMX_MU_GCR = 0x114,
+	IMX_MU_TCR = 0x120,
+	IMX_MU_RCR = 0x128,
+};
+
+enum imx_mu_xsr {
+	IMX_MU_SR = 0x0c,
+	IMX_MU_GSR = 0x118,
+	IMX_MU_TSR = 0x124,
+	IMX_MU_RSR = 0x12c,
+};
+
+#ifdef CONFIG_IMX8ULP
+/* Transmit Register */
+#define IMX_MU_xTRn(x)         (0x200 + 4 * (x))
+/* Receive Register */
+#define IMX_MU_xRRn(x)         (0x280 + 4 * (x))
+
+#else
 /* Transmit Register */
 #define IMX_MU_xTRn(x)		(0x00 + 4 * (x))
 /* Receive Register */
 #define IMX_MU_xRRn(x)		(0x10 + 4 * (x))
+#endif
+
 /* Status Register */
-#define IMX_MU_xSR		0x20
-#define IMX_MU_xSR_GIPn(x)	BIT(28 + (3 - (x)))
-#define IMX_MU_xSR_RFn(x)	BIT(24 + (3 - (x)))
-#define IMX_MU_xSR_TEn(x)	BIT(20 + (3 - (x)))
+#define IMX_MU_xSR(type, index)		(type == IMX_MU_V2 ? index : 0x20)
+#define IMX_MU_xSR_GIPn(type, x)	(type == IMX_MU_V2 ? BIT(x) : BIT(28 + (3 - (x))))
+#define IMX_MU_xSR_RFn(type, x)		(type == IMX_MU_V2 ? BIT(x) : BIT(24 + (3 - (x))))
+#define IMX_MU_xSR_TEn(type, x)		(type == IMX_MU_V2 ? BIT(x) : BIT(20 + (3 - (x))))
 #define IMX_MU_xSR_BRDIP	BIT(9)
 
 /* Control Register */
-#define IMX_MU_xCR		0x24
+#define IMX_MU_xCR(type, index)		(type == IMX_MU_V2 ? index : 0x24)
 /* General Purpose Interrupt Enable */
-#define IMX_MU_xCR_GIEn(x)	BIT(28 + (3 - (x)))
+#define IMX_MU_xCR_GIEn(type, x)	(type == IMX_MU_V2 ? BIT(x) : BIT(28 + (3 - (x))))
 /* Receive Interrupt Enable */
-#define IMX_MU_xCR_RIEn(x)	BIT(24 + (3 - (x)))
+#define IMX_MU_xCR_RIEn(type, x)	(type == IMX_MU_V2 ? BIT(x) : BIT(24 + (3 - (x))))
 /* Transmit Interrupt Enable */
-#define IMX_MU_xCR_TIEn(x)	BIT(20 + (3 - (x)))
+#define IMX_MU_xCR_TIEn(type, x)	(type == IMX_MU_V2 ? BIT(x) : BIT(20 + (3 - (x))))
 /* General Purpose Interrupt Request */
-#define IMX_MU_xCR_GIRn(x)	BIT(16 + (3 - (x)))
+#define IMX_MU_xCR_GIRn(type, x)	(type == IMX_MU_V2 ? BIT(x) : BIT(16 + (3 - (x))))
 
 static inline uint32_t imx_mu_read(uint32_t reg)
 {
@@ -45,26 +79,26 @@ static inline void imx_mu_write(uint32_t val, uint32_t reg)
 	*((volatile uint32_t*)(MU_BASE + reg)) = val;
 }
 
-static inline uint32_t imx_mu_xcr_rmw(uint32_t set, uint32_t clr)
+static inline uint32_t imx_mu_xcr_rmw(int type, int idx, uint32_t set, uint32_t clr)
 {
 	volatile uint32_t val;
 
-	val = imx_mu_read(IMX_MU_xCR);
+	val = imx_mu_read(IMX_MU_xCR(type, idx));
 	val &= ~clr;
 	val |= set;
-	imx_mu_write(val, IMX_MU_xCR);
+	imx_mu_write(val, IMX_MU_xCR(type, idx));
 
 	return val;
 }
 
-static inline uint32_t imx_mu_xsr_rmw(uint32_t set, uint32_t clr)
+static inline uint32_t imx_mu_xsr_rmw(int type, int idx, uint32_t set, uint32_t clr)
 {
 	volatile uint32_t val;
 
-	val = imx_mu_read(IMX_MU_xSR);
+	val = imx_mu_read(IMX_MU_xSR(type, idx));
 	val &= ~clr;
 	val |= set;
-	imx_mu_write(val, IMX_MU_xSR);
+	imx_mu_write(val, IMX_MU_xSR(type, idx));
 
 	return val;
 }
