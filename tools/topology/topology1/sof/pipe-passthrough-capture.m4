thesofproject/sof

commit 659266685b4358ded2f779b29d34166432108f71

topology: remove side effects from macro definitions
This commit fixes the issue mentioned in #4583.

A temporary definition is introduced that takes it's value from
"CHANNELS_MIN" if it is already defined, otherwise it is set to
a default value. This temporary is used instead of "CHANNELS_MIN"
for the rest of the file. This avoids having the macro defined in
files where it shouldn't be.

In order to be completely sure it avoids side effects this temporary
is undefined after it is no longer needed.

Signed-off-by: Bud Liviu-Alexandru <budliviu@gmail.com>

tools/topology/topology1/sof/pipe-passthrough-capture.m4
@@ -39,11 +39,13 @@ P_GRAPH(pipe-passthrough-capture, PIPELINE_ID,
 indir(`define', concat(`PIPELINE_SINK_', PIPELINE_ID), N_BUFFER(0))
 indir(`define', concat(`PIPELINE_PCM_', PIPELINE_ID), Passthrough Capture PCM_ID)
 
-ifdef(`CHANNELS_MIN',`',
-`define(CHANNELS_MIN, `PIPELINE_CHANNELS')')
+ifdef(`CHANNELS_MIN',`define(`LOCAL_CHANNELS_MIN', `CHANNELS_MIN')',
+`define(`LOCAL_CHANNELS_MIN' `PIPELINE_CHANNELS')')
 
 #
 # PCM Configuration
 #
 
-PCM_CAPABILITIES(Passthrough Capture PCM_ID, COMP_FORMAT_NAME(PIPELINE_FORMAT), PCM_MIN_RATE, PCM_MAX_RATE, CHANNELS_MIN, PIPELINE_CHANNELS, 2, 16, 192, 16384, 65536, 65536)
+PCM_CAPABILITIES(Passthrough Capture PCM_ID, COMP_FORMAT_NAME(PIPELINE_FORMAT), PCM_MIN_RATE, PCM_MAX_RATE, LOCAL_CHANNELS_MIN, PIPELINE_CHANNELS, 2, 16, 192, 16384, 65536, 65536)
+
+undefine(`LOCAL_CHANNELS_MIN')

tools/topology/topology1/sof/pipe-passthrough-playback.m4
@@ -39,11 +39,13 @@ P_GRAPH(pipe-passthrough-playback, PIPELINE_ID,
 indir(`define', concat(`PIPELINE_SOURCE_', PIPELINE_ID), N_BUFFER(0))
 indir(`define', concat(`PIPELINE_PCM_', PIPELINE_ID), Passthrough Playback PCM_ID)
 
-ifdef(`CHANNELS_MIN',`',
-`define(CHANNELS_MIN, `2')')
+ifdef(`CHANNELS_MIN',`define(`LOCAL_CHANNELS_MIN', `CHANNELS_MIN')',
+`define(`LOCAL_CHANNELS_MIN', `2')')
 
 #
 # PCM Configuration
 #
 
-PCM_CAPABILITIES(Passthrough Playback PCM_ID, COMP_FORMAT_NAME(PIPELINE_FORMAT), PCM_MIN_RATE, PCM_MAX_RATE, CHANNELS_MIN, PIPELINE_CHANNELS, 2, 16, 192, 16384, 65536, 65536)
+PCM_CAPABILITIES(Passthrough Playback PCM_ID, COMP_FORMAT_NAME(PIPELINE_FORMAT), PCM_MIN_RATE, PCM_MAX_RATE, LOCAL_CHANNELS_MIN, PIPELINE_CHANNELS, 2, 16, 192, 16384, 65536, 65536)
+
+undefine(`LOCAL_CHANNELS_MIN')

tools/topology/topology1/sof/pipe-volume-playback.m4
@@ -75,14 +75,15 @@ P_GRAPH(pipe-volume-playback, PIPELINE_ID,
 indir(`define', concat(`PIPELINE_SOURCE_', PIPELINE_ID), N_BUFFER(1))
 indir(`define', concat(`PIPELINE_PCM_', PIPELINE_ID), Passthrough Playback PCM_ID)
 
-ifdef(`CHANNELS_MIN',`define(TCHANNELS_MIN, `CHANNELS_MIN')',
-`define(TCHANNELS_MIN, `2')')
+ifdef(`CHANNELS_MIN',`define(`LOCAL_CHANNELS_MIN', `CHANNELS_MIN')',
+`define(`LOCAL_CHANNELS_MIN', `2')')
 
 #
 # PCM Configuration
 
 #
-PCM_CAPABILITIES(Passthrough Playback PCM_ID, CAPABILITY_FORMAT_NAME(PIPELINE_FORMAT), PCM_MIN_RATE, PCM_MAX_RATE, TCHANNELS_MIN, PIPELINE_CHANNELS, 2, 16, 192, 16384, 65536, 65536)
+PCM_CAPABILITIES(Passthrough Playback PCM_ID, CAPABILITY_FORMAT_NAME(PIPELINE_FORMAT), PCM_MIN_RATE, PCM_MAX_RATE, LOCAL_CHANNELS_MIN, PIPELINE_CHANNELS, 2, 16, 192, 16384, 65536, 65536)
 
+undefine(`LOCAL_CHANNELS_MIN')
 undefine(`DEF_PGA_TOKENS')
 undefine(`DEF_PGA_CONF')

commit 3e3fcc915fae169a45a2b8dbdca2de831ddf1c52

topology1: fix error in LOCAL_CHANNELS_MIN definition
The patch to fix CHANNELS_MIN side effects had a bug in which caused
LOCAL_CHANNELS_MIN to slip into conf output. Example from
sof-adl-sdw-max98373-rt5682:

---cut--
SectionPCMCapabilities."Passthrough Capture 14" {
»       formats "S16_LE"
»       rate_min "8000"
»       rate_max "16000"
»       channels_min "LOCAL_CHANNELS_MIN"
--cut--

Fixes: 659266685b43 ("topology: remove side effects from macro definitions")
BugLink: https://github.com/thesofproject/sof/issues/4621
Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

tools/topology/topology1/sof/pipe-passthrough-capture.m4
@@ -40,7 +40,7 @@ indir(`define', concat(`PIPELINE_SINK_', PIPELINE_ID), N_BUFFER(0))
 indir(`define', concat(`PIPELINE_PCM_', PIPELINE_ID), Passthrough Capture PCM_ID)
 
 ifdef(`CHANNELS_MIN',`define(`LOCAL_CHANNELS_MIN', `CHANNELS_MIN')',
-`define(`LOCAL_CHANNELS_MIN' `PIPELINE_CHANNELS')')
+`define(`LOCAL_CHANNELS_MIN', `PIPELINE_CHANNELS')')
 
 #
 # PCM Configuration