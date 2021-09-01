781cd439e66fdf97f8abef1525e40a0ceb37ba6e
Keyon Jie <yang.jie@linux.intel.com>
2021-09-01 17:50:49 +0800

    library: memory: align it to the latest cavs version
    After zones are refined, need an alignment to the latest cavs version.
    
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

--- src/platform/library/lib/memory.c
+++ src/platform/library/lib/memory.c
@@ -19,6 +19,13 @@
 #define uncached_block_hdr(hdr)	cache_to_uncache_init((struct block_hdr *)(hdr))
 #define uncached_block_map(map)	cache_to_uncache((struct block_map *)(map))
 
+/* Memory mock for memmap */
+#define HEAP_SYSTEM_0_BASE	0xBE100000
+#define HEAP_SYS_RUNTIME_0_BASE 0xBE120000
+#define SOF_CORE_S_START	0xBE140000
+#define HEAP_RUNTIME_BASE	0xBE180000
+#define HEAP_BUFFER_BASE	0xBE1C0000
+
 /* Heap blocks for system runtime for primary core */
 static SHARED_DATA struct block_hdr sys_rt_0_block64[HEAP_SYS_RT_0_COUNT64];
 static SHARED_DATA struct block_hdr sys_rt_0_block512[HEAP_SYS_RT_0_COUNT512];
@@ -69,50 +76,32 @@ static SHARED_DATA struct block_map sys_rt_heap_map[CONFIG_CORE_COUNT][3] = {
 };
 
 /* Heap blocks for modules */
-static SHARED_DATA struct block_hdr mod_block64[HEAP_RT_COUNT64];
-static SHARED_DATA struct block_hdr mod_block128[HEAP_RT_COUNT128];
-static SHARED_DATA struct block_hdr mod_block256[HEAP_RT_COUNT256];
-static SHARED_DATA struct block_hdr mod_block512[HEAP_RT_COUNT512];
-static SHARED_DATA struct block_hdr mod_block1024[HEAP_RT_COUNT1024];
-static SHARED_DATA struct block_hdr mod_block2048[HEAP_RT_COUNT2048];
-static SHARED_DATA struct block_hdr mod_block4096[HEAP_RT_COUNT4096];
+static SHARED_DATA struct block_hdr mod_block64[HEAP_COUNT64];
+static SHARED_DATA struct block_hdr mod_block128[HEAP_COUNT128];
+static SHARED_DATA struct block_hdr mod_block256[HEAP_COUNT256];
+static SHARED_DATA struct block_hdr mod_block512[HEAP_COUNT512];
+static SHARED_DATA struct block_hdr mod_block1024[HEAP_COUNT1024];
+static SHARED_DATA struct block_hdr mod_block2048[HEAP_COUNT2048];
+static SHARED_DATA struct block_hdr mod_block4096[HEAP_COUNT4096];
 
 /* Heap memory map for modules */
 static SHARED_DATA struct block_map rt_heap_map[] = {
-	BLOCK_DEF(64, HEAP_RT_COUNT64, uncached_block_hdr(mod_block64)),
-	BLOCK_DEF(128, HEAP_RT_COUNT128, uncached_block_hdr(mod_block128)),
-	BLOCK_DEF(256, HEAP_RT_COUNT256, uncached_block_hdr(mod_block256)),
-	BLOCK_DEF(512, HEAP_RT_COUNT512, uncached_block_hdr(mod_block512)),
-	BLOCK_DEF(1024, HEAP_RT_COUNT1024, uncached_block_hdr(mod_block1024)),
-	BLOCK_DEF(2048, HEAP_RT_COUNT2048, uncached_block_hdr(mod_block2048)),
-	BLOCK_DEF(4096, HEAP_RT_COUNT4096, uncached_block_hdr(mod_block4096)),
+	BLOCK_DEF(64, HEAP_COUNT64, uncached_block_hdr(mod_block64)),
+	BLOCK_DEF(128, HEAP_COUNT128, uncached_block_hdr(mod_block128)),
+	BLOCK_DEF(256, HEAP_COUNT256, uncached_block_hdr(mod_block256)),
+	BLOCK_DEF(512, HEAP_COUNT512, uncached_block_hdr(mod_block512)),
+	BLOCK_DEF(1024, HEAP_COUNT1024, uncached_block_hdr(mod_block1024)),
+	BLOCK_DEF(2048, HEAP_COUNT2048, uncached_block_hdr(mod_block2048)),
+	BLOCK_DEF(4096, HEAP_COUNT4096, uncached_block_hdr(mod_block4096)),
 };
-
-#if CONFIG_CORE_COUNT > 1
-/* Heap blocks for runtime shared */
-static SHARED_DATA struct block_hdr rt_shared_block64[HEAP_RUNTIME_SHARED_COUNT64];
-static SHARED_DATA struct block_hdr rt_shared_block128[HEAP_RUNTIME_SHARED_COUNT128];
-static SHARED_DATA struct block_hdr rt_shared_block256[HEAP_RUNTIME_SHARED_COUNT256];
-static SHARED_DATA struct block_hdr rt_shared_block512[HEAP_RUNTIME_SHARED_COUNT512];
-static SHARED_DATA struct block_hdr rt_shared_block1024[HEAP_RUNTIME_SHARED_COUNT1024];
-
-/* Heap memory map for runtime shared */
-static SHARED_DATA struct block_map rt_shared_heap_map[] = {
-	BLOCK_DEF(64, HEAP_RUNTIME_SHARED_COUNT64, uncached_block_hdr(rt_shared_block64)),
-	BLOCK_DEF(128, HEAP_RUNTIME_SHARED_COUNT128, uncached_block_hdr(rt_shared_block128)),
-	BLOCK_DEF(256, HEAP_RUNTIME_SHARED_COUNT256, uncached_block_hdr(rt_shared_block256)),
-	BLOCK_DEF(512, HEAP_RUNTIME_SHARED_COUNT512, uncached_block_hdr(rt_shared_block512)),
-	BLOCK_DEF(1024, HEAP_RUNTIME_SHARED_COUNT1024, uncached_block_hdr(rt_shared_block1024)),
-};
-#endif
 
 /* Heap blocks for buffers */
-static SHARED_DATA struct block_hdr buf_block[HEAP_BUFFER_COUNT];
+static SHARED_DATA struct block_hdr buf_block[HEAP_BUFFER_COUNT_MAX];
 static SHARED_DATA struct block_hdr lp_buf_block[HEAP_LP_BUFFER_COUNT];
 
 /* Heap memory map for buffers */
 static SHARED_DATA struct block_map buf_heap_map[] = {
-	BLOCK_DEF(HEAP_BUFFER_BLOCK_SIZE, HEAP_BUFFER_COUNT,
+	BLOCK_DEF(HEAP_BUFFER_BLOCK_SIZE, HEAP_BUFFER_COUNT_MAX,
 		  uncached_block_hdr(buf_block)),
 };
 
@@ -125,7 +114,25 @@ static SHARED_DATA struct mm memmap;
 
 void platform_init_memmap(struct sof *sof)
 {
+	uint32_t heap_buffer_size = SOF_FW_END - HEAP_BUFFER_BASE;
+	uint32_t buffer_count;
 	int i;
+
+	/* calculate the buffer heap size */
+	buffer_count = heap_buffer_size / HEAP_BUFFER_BLOCK_SIZE;
+	heap_buffer_size = buffer_count * HEAP_BUFFER_BLOCK_SIZE;
+
+	for (i = 0; i < ARRAY_SIZE(buf_heap_map); i++) {
+		buf_heap_map[i].count = buffer_count;
+		buf_heap_map[i].free_count = buffer_count;
+	}
+	dcache_writeback_region(buf_heap_map,
+				sizeof(struct block_map) * ARRAY_SIZE(buf_heap_map));
+
+	/*
+	 * by default, use cached address for system and runtime zones,
+	 * and uncached address for system_runtime and buffer zones.
+	 */
 
 	/* access memory map through uncached region */
 	sof->memory_map = cache_to_uncache(&memmap);
@@ -178,42 +185,21 @@ void platform_init_memmap(struct sof *sof)
 			SOF_MEM_CAPS_DMA;
 	}
 
-#if CONFIG_CORE_COUNT > 1
-	/* .runtime_shared init */
-	sof->memory_map->runtime_shared[0].blocks = ARRAY_SIZE(rt_shared_heap_map);
-	sof->memory_map->runtime_shared[0].map = uncached_block_map(rt_shared_heap_map);
-	sof->memory_map->runtime_shared[0].heap =
-			(unsigned long)cache_to_uncache(malloc(HEAP_RUNTIME_SHARED_SIZE));
-	sof->memory_map->runtime_shared[0].size = HEAP_RUNTIME_SHARED_SIZE;
-	sof->memory_map->runtime_shared[0].info.free = HEAP_RUNTIME_SHARED_SIZE;
-	sof->memory_map->runtime_shared[0].caps = SOF_MEM_CAPS_RAM | SOF_MEM_CAPS_EXT |
-		SOF_MEM_CAPS_CACHE;
-
-	/* .system_shared init */
-	sof->memory_map->system_shared[0].heap =
-		(unsigned long)cache_to_uncache(malloc(HEAP_SYSTEM_SHARED_SIZE));
-	sof->memory_map->system_shared[0].size = HEAP_SYSTEM_SHARED_SIZE;
-	sof->memory_map->system_shared[0].info.free = HEAP_SYSTEM_SHARED_SIZE;
-	sof->memory_map->system_shared[0].caps = SOF_MEM_CAPS_RAM | SOF_MEM_CAPS_EXT |
-		SOF_MEM_CAPS_CACHE;
-
-#endif
-
 	/* .runtime init*/
-	sof->memory_map->runtime[0].blocks = ARRAY_SIZE(rt_heap_map);
-	sof->memory_map->runtime[0].map = uncached_block_map(rt_heap_map);
-	sof->memory_map->runtime[0].heap = (unsigned long)malloc(HEAP_RUNTIME_SIZE);
-	sof->memory_map->runtime[0].size = HEAP_RUNTIME_SIZE;
-	sof->memory_map->runtime[0].info.free = HEAP_RUNTIME_SIZE;
-	sof->memory_map->runtime[0].caps = SOF_MEM_CAPS_RAM | SOF_MEM_CAPS_EXT |
+	sof->memory_map->runtime.blocks = ARRAY_SIZE(rt_heap_map);
+	sof->memory_map->runtime.map = uncached_block_map(rt_heap_map);
+	sof->memory_map->runtime.heap = (unsigned long)malloc(HEAP_RUNTIME_SIZE);
+	sof->memory_map->runtime.size = HEAP_RUNTIME_SIZE;
+	sof->memory_map->runtime.info.free = HEAP_RUNTIME_SIZE;
+	sof->memory_map->runtime.caps = SOF_MEM_CAPS_RAM | SOF_MEM_CAPS_EXT |
 		SOF_MEM_CAPS_CACHE;
 
 	/* heap buffer init */
 	sof->memory_map->buffer[0].blocks = ARRAY_SIZE(buf_heap_map);
 	sof->memory_map->buffer[0].map = uncached_block_map(buf_heap_map);
-	sof->memory_map->buffer[0].heap = (unsigned long)malloc(HEAP_BUFFER_SIZE);
-	sof->memory_map->buffer[0].size = HEAP_BUFFER_SIZE;
-	sof->memory_map->buffer[0].info.free = HEAP_BUFFER_SIZE;
+	sof->memory_map->buffer[0].heap = (unsigned long)malloc(heap_buffer_size);
+	sof->memory_map->buffer[0].size = heap_buffer_size;
+	sof->memory_map->buffer[0].info.free = heap_buffer_size;
 	sof->memory_map->buffer[0].caps = SOF_MEM_CAPS_RAM | SOF_MEM_CAPS_HP |
 		SOF_MEM_CAPS_CACHE | SOF_MEM_CAPS_DMA;
 
@@ -228,7 +214,7 @@ void platform_init_memmap(struct sof *sof)
 
 	/* .total init */
 	sof->memory_map->total.free = HEAP_SYSTEM_T_SIZE +
-		HEAP_SYS_RUNTIME_T_SIZE + HEAP_RUNTIME_SIZE + HEAP_BUFFER_SIZE +
+		HEAP_SYS_RUNTIME_T_SIZE + HEAP_RUNTIME_SIZE + heap_buffer_size +
 		HEAP_LP_BUFFER_SIZE;
 
 }
