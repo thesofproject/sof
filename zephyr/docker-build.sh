1b4b3a575b12ee99ee8555dce3bd3eb0d70e88d3
Marc Herbert <marc.herbert@intel.com>
2021-09-27 19:19:43 -0700

    zephyr/docker-build: unset stale ZEPHYR_SDK_INSTALL_DIR, symlink toolchains
    Currently set to obsolete 0.12.4. Better remove it here for everyone
    rather than overriding it in Github Actions only.
    
    Also symlinks the toolchains in /opt to /home to fix autodetection
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- zephyr/docker-build.sh
+++ zephyr/docker-build.sh
@@ -18,6 +18,14 @@ test -e ./scripts/xtensa-build-zephyr.sh
 sudo apt-get update
 sudo apt-get -y install tree
 
+# Unset stale ZEPHYR_SDK_INSTALL_DIR variable pointing to obsolete
+# 0.12.4 as of container 0.18.x
+unset ZEPHYR_SDK_INSTALL_DIR
+
+# Zephyr's CMake does not look in /opt but it searches $HOME
+ls -ld /opt/toolchains/zephyr-sdk-*
+ln -s  /opt/toolchains/zephyr-sdk-*  ~/
+
 if test -e zephyrproject; then
     ./scripts/xtensa-build-zephyr.sh -a "$@"
 else
