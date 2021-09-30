thesofproject/sof

commit 107b60ea6f04b0432d15cc8c98a4f468f49478d5

pipeline: simplify handling of first pipeline
src/audio/pipeline/pipeline-schedule.c:186:6: style: Assignment of
function parameter has no effect outside the
function. [uselessAssignmentArg]
     cmd = -EINVAL;
     ^

The code isn't wrong but cppcheck points at a possible confusion. It's
better and more explicit to use a dedicated boolean flag than
modifying the command code.

Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

src/audio/pipeline/pipeline-schedule.c
@@ -176,6 +176,7 @@ void pipeline_schedule_triggered(struct pipeline_walk_context *ctx,
 	struct list_item *tlist;
 	struct pipeline *p;
 	uint32_t flags;
+	bool first_pipe = false;
 
 	/*
 	 * Interrupts have to be disabled while adding tasks to or removing them
@@ -199,10 +200,10 @@ void pipeline_schedule_triggered(struct pipeline_walk_context *ctx,
 			p = container_of(tlist, struct pipeline, list);
 			if (pipeline_is_timer_driven(p)) {
 				/* Use the first of connected pipelines to trigger */
-				if (cmd >= 0) {
+				if (cmd >= 0 && !first_pipe) {
 					p->trigger.cmd = cmd;
 					p->trigger.host = ppl_data->start;
-					cmd = -EINVAL;
+					first_pipe = true;
 				}
 			} else {
 				p->xrun_bytes = 0;

commit dcd3a93c276bb4a072ac6ec33dcd113847c2d7d0

pipeline: fix handling of first pipeline
the logic was just wrong as discussed in
https://github.com/thesofproject/sof/pull/4818#discussion_r719091909

remove test on cmd and flip logic for first_pipe

Fixes: 107b60ea6 ('pipeline: simplify handling of first pipeline')
Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

src/audio/pipeline/pipeline-schedule.c
@@ -176,7 +176,7 @@ void pipeline_schedule_triggered(struct pipeline_walk_context *ctx,
 	struct list_item *tlist;
 	struct pipeline *p;
 	uint32_t flags;
-	bool first_pipe = false;
+	bool first_pipe = true;
 
 	/*
 	 * Interrupts have to be disabled while adding tasks to or removing them
@@ -200,10 +200,10 @@ void pipeline_schedule_triggered(struct pipeline_walk_context *ctx,
 			p = container_of(tlist, struct pipeline, list);
 			if (pipeline_is_timer_driven(p)) {
 				/* Use the first of connected pipelines to trigger */
-				if (cmd >= 0 && !first_pipe) {
+				if (first_pipe) {
 					p->trigger.cmd = cmd;
 					p->trigger.host = ppl_data->start;
-					first_pipe = true;
+					first_pipe = false;
 				}
 			} else {
 				p->xrun_bytes = 0;