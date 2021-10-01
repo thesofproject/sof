1632b0d212dd8738d05e96df2bd6c39a8c9824ff
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2021-10-01 17:03:55 +0100

    arch: host: Add host locking via pthread mutexes.
    Add support for real host based locking for testbench with pthread
    mutexes.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- src/arch/host/include/arch/spinlock.h
+++ src/arch/host/include/arch/spinlock.h
@@ -10,16 +10,33 @@
 #ifndef __ARCH_SPINLOCK_H__
 #define __ARCH_SPINLOCK_H__
 
+#include <pthread.h>
+
 typedef struct {
+	pthread_mutex_t mutex;
 } spinlock_t;
 
-static inline void arch_spinlock_init(spinlock_t *lock) { }
-static inline void arch_spin_lock(spinlock_t *lock) {}
+static inline void arch_spinlock_init(spinlock_t *lock)
+{
+	/* this is fatal on host and no way to tell caller */
+	assert(!pthread_mutex_init(&lock->mutex, NULL));
+}
+
+static inline void arch_spin_lock(spinlock_t *lock)
+{
+	pthread_mutex_lock(&lock->mutex);
+}
+
 static inline int arch_try_lock(spinlock_t *lock)
 {
-	return 1;
+	/* try lock logic on SOF is opposite of posix ! */
+	return !pthread_mutex_trylock(&lock->mutex);
 }
-static inline void arch_spin_unlock(spinlock_t *lock) {}
+
+static inline void arch_spin_unlock(spinlock_t *lock)
+{
+	pthread_mutex_unlock(&lock->mutex);
+}
 
 #endif /* __ARCH_SPINLOCK_H__ */
 
