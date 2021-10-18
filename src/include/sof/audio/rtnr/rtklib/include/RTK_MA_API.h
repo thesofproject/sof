d1c9a126bf17fbb41121650ad00d8eca3602123b
Ming Jen Tai <mingjen_tai@realtek.com>
2021-10-18 10:28:25 +0800

    Support RTNR on Tigerlake platform
    This PR adds RTNR Noise Reduction/Suppression(NR/NS) component by
    Realtek Semiconductor Corp. This feature links to proprietary libraries.
    Please contact antz0525@realtek.com for any question about the library.
    
    Signed-off-by: Ming Jen Tai <mingjen_tai@realtek.com>
    
    Support RTNR on Tigerlake platform
    
    This PR adds RTNR Noise Reduction/Suppression(NR/NS) component by
    Realtek Semiconductor Corp. This feature links to proprietary libraries.
    Please contact antz0525@realtek.com for any question about the library.
    
    Signed-off-by: Ming Jen Tai <mingjen_tai@realtek.com>
    
    Cherry-picked from bbc5209, with the following changes:
    Move files added under tools/topology/topology1/ to tools/topology/
    Migrate changes in tools/topology/topology1/CMakeLists.txt to tools/topology/CMakeLists.txt
    Modify rtnr_new() in rtnr.c in accordance with API changes.

---
+++ src/include/sof/audio/rtnr/rtklib/include/RTK_MA_API.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2021 Realtek Corporation. All rights reserved.
+ *
+ * Author: Antz Cheng <antz0525@realtek.com>
+ */
+#ifndef _RTK_MA_API_H_
+#define _RTK_MA_API_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void RTKMA_API_S16_Default(void *Context, const struct audio_stream **sources,
+						struct audio_stream *sink, int frames,
+						_Bool ref_active, int in_idx, int ref_idx,
+						int ref_32bits, int ref_shift);
+void RTKMA_API_S24_Default(void *Context, const struct audio_stream **sources,
+						struct audio_stream *sink, int frames,
+						_Bool ref_active, int in_idx, int ref_idx,
+						int ref_32bits, int ref_shift);
+void RTKMA_API_S32_Default(void *Context, const struct audio_stream **sources,
+						struct audio_stream *sink, int frames,
+						_Bool ref_active, int in_idx, int ref_idx,
+						int ref_32bits, int ref_shift);
+
+void RTKMA_API_First_Copy(void *Context, int SampleRate, int MicCh);
+
+void RTKMA_API_Bypass(void *Context, _Bool Bypass);
+
+void RTKMA_API_Process(void *Context, _Bool has_ref, int SampleRate, int MicCh);
+
+void RTKMA_API_Prepare(void *Context);
+
+void *RTKMA_API_Context_Create(int sample_rate);
+
+void RTKMA_API_Context_Free(void *Context);
+
+void RTKMA_API_Set_Default_Parameter(void *Context);
+
+int RTKMA_API_Parameter_Size(void *Context, unsigned int IDs);
+
+int RTKMA_API_Set(void *Context, const void *pParameters, int size, unsigned int IDs);
+
+int	RTKMA_API_Get(void *Context, void *pParameters, int size, unsigned int IDs);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTK_MA_API_H_  */
+
--- tools/topology/sof-tgl-max98357a-rt5682.m4
+++ tools/topology/sof-tgl-max98357a-rt5682.m4
@@ -100,6 +100,18 @@ define(KWD_PIPE_SCH_DEADLINE_US, 5000)
 # include the generic dmic with kwd
 include(`platform/intel/intel-generic-dmic-kwd.m4')
 
+# if RTNR is defined, define DMIC16KPROC as rtnr
+ifdef(`RTNR',
+`define(DMICPROC, rtnr)',
+`ifdef(`DMICPROC', , `define(DMICPROC, eq-iir-volume)')')
+
+ifdef(`RTNR',
+`define(DMIC16KPROC, rtnr)',
+`ifdef(`DMIC16KPROC', , `define(DMIC16KPROC, eq-iir-volume)')')
+
+# include the generic dmic if RTNR is defined, else include generic dmic with kwd
+include(ifdef(`RTNR', platform/intel/intel-generic-dmic.m4, platform/intel/intel-generic-dmic-kwd.m4))
+
 dnl PIPELINE_PCM_ADD(pipeline,
 dnl     pipe id, pcm, max channels, format,
 dnl     frames, deadline, priority, core)
