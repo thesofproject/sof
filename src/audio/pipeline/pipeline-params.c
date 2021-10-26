388cf4f9f4cb75dd726fdb8ea8a980405cd87590
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2021-10-25 20:52:14 -0700

    pipeline: use buffer acquire/release
    instead of buffer_lock/unlock() which will be deprecated.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- src/audio/pipeline/pipeline-params.c
+++ src/audio/pipeline/pipeline-params.c
@@ -25,7 +25,6 @@ static int pipeline_comp_params_neg(struct comp_dev *current,
 				    int dir)
 {
 	struct pipeline_data *ppl_data = ctx->comp_data;
-	uint32_t flags = 0;
 	int err = 0;
 
 	pipe_dbg(current->pipeline, "pipeline_comp_params_neg(), current->comp.id = %u, dir = %u",
@@ -57,11 +56,11 @@ static int pipeline_comp_params_neg(struct comp_dev *current,
 	 * should explicitly configure the channels of the branched buffers.
 	 */
 	if (calling_buf) {
-		buffer_lock(calling_buf, &flags);
+		calling_buf = buffer_acquire_irq(calling_buf);
 		err = buffer_set_params(calling_buf,
 					&ppl_data->params->params,
 					BUFFER_UPDATE_FORCE);
-		buffer_unlock(calling_buf, flags);
+		calling_buf = buffer_release_irq(calling_buf);
 	}
 
 	return err;
@@ -148,7 +147,6 @@ static int pipeline_comp_hw_params(struct comp_dev *current,
 				   struct pipeline_walk_context *ctx, int dir)
 {
 	struct pipeline_data *ppl_data = ctx->comp_data;
-	uint32_t flags = 0;
 	int ret;
 
 	pipe_dbg(current->pipeline, "pipeline_comp_hw_params(), current->comp.id = %u, dir = %u",
@@ -172,10 +170,10 @@ static int pipeline_comp_hw_params(struct comp_dev *current,
 
 	/* set buffer parameters */
 	if (calling_buf) {
-		buffer_lock(calling_buf, &flags);
+		calling_buf = buffer_acquire_irq(calling_buf);
 		ret = buffer_set_params(calling_buf, &ppl_data->params->params,
 					BUFFER_UPDATE_IF_UNSET);
-		buffer_unlock(calling_buf, flags);
+		calling_buf = buffer_release_irq(calling_buf);
 		if (ret < 0)
 			pipe_err(current->pipeline,
 				 "pipeline_comp_hw_params(): buffer_set_params(): %d", ret);
