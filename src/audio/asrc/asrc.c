d2046301a4446a1141f5b324e5dcd25c998386f3
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2021-10-25 20:48:57 -0700

    asrc: use buffer acquire/release
    instead of buffer_lock/unlock() which will be deprecated.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- src/audio/asrc/asrc.c
+++ src/audio/asrc/asrc.c
@@ -829,7 +829,6 @@ static int asrc_copy(struct comp_dev *dev)
 	int frames_src;
 	int frames_snk;
 	int ret;
-	uint32_t flags = 0;
 
 	comp_dbg(dev, "asrc_copy()");
 
@@ -843,14 +842,14 @@ static int asrc_copy(struct comp_dev *dev)
 	sink = list_first_item(&dev->bsink_list, struct comp_buffer,
 			       source_list);
 
-	buffer_lock(source, &flags);
-	buffer_lock(sink, &flags);
+	source = buffer_acquire_irq(source);
+	sink = buffer_acquire_irq(sink);
 
 	frames_src = audio_stream_get_avail_frames(&source->stream);
 	frames_snk = audio_stream_get_free_frames(&sink->stream);
 
-	buffer_unlock(sink, flags);
-	buffer_unlock(source, flags);
+	buffer_release_irq(sink);
+	buffer_release_irq(source);
 
 	if (cd->mode == ASRC_OM_PULL) {
 		/* Let ASRC access max number of source frames in pull mode.
