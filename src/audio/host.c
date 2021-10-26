6ef7ebf89a5c6f7290e1ef0b073a4d37416a0ecb
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2021-10-25 20:38:14 -0700

    host: use buffer acquire/release API
    instead of buffer_lock/unlock() which will be deprecated.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- src/audio/host.c
+++ src/audio/host.c
@@ -265,19 +265,19 @@ static uint32_t host_get_copy_bytes_one_shot(struct comp_dev *dev)
 {
 	struct host_data *hd = comp_get_drvdata(dev);
 	struct dma_sg_elem *local_elem = hd->config.elem_array.elems;
+	struct comp_buffer *buffer = hd->local_buffer;
 	uint32_t copy_bytes = 0;
 	uint32_t split_value;
-	uint32_t flags = 0;
 
-	buffer_lock(hd->local_buffer, &flags);
+	buffer = buffer_acquire_irq(buffer);
 
 	/* calculate minimum size to copy */
 	if (dev->direction == SOF_IPC_STREAM_PLAYBACK)
-		copy_bytes = audio_stream_get_free_bytes(&hd->local_buffer->stream);
+		copy_bytes = audio_stream_get_free_bytes(&buffer->stream);
 	else
-		copy_bytes = audio_stream_get_avail_bytes(&hd->local_buffer->stream);
+		copy_bytes = audio_stream_get_avail_bytes(&buffer->stream);
 
-	buffer_unlock(hd->local_buffer, flags);
+	buffer_release_irq(buffer);
 
 	/* copy_bytes should be aligned to minimum possible chunk of
 	 * data to be copied by dma.
@@ -337,10 +337,10 @@ static int host_copy_one_shot(struct comp_dev *dev)
 static uint32_t host_get_copy_bytes_normal(struct comp_dev *dev)
 {
 	struct host_data *hd = comp_get_drvdata(dev);
+	struct comp_buffer *buffer = hd->local_buffer;
 	uint32_t avail_bytes = 0;
 	uint32_t free_bytes = 0;
 	uint32_t copy_bytes = 0;
-	uint32_t flags = 0;
 	int ret;
 
 	/* get data sizes from DMA */
@@ -353,26 +353,27 @@ static uint32_t host_get_copy_bytes_normal(struct comp_dev *dev)
 		return 0;
 	}
 
-	buffer_lock(hd->local_buffer, &flags);
+	buffer = buffer_acquire_irq(buffer);
 
 	/* calculate minimum size to copy */
 	if (dev->direction == SOF_IPC_STREAM_PLAYBACK) {
 		/* limit bytes per copy to one period for the whole pipeline
 		 * in order to avoid high load spike
 		 */
-		free_bytes = audio_stream_get_free_bytes(&hd->local_buffer->stream);
+		free_bytes = audio_stream_get_free_bytes(&buffer->stream);
 		copy_bytes = MIN(hd->period_bytes, MIN(avail_bytes, free_bytes));
 		if (!copy_bytes)
 			comp_info(dev, "no bytes to copy, %d free in buffer, %d available in DMA",
 				  free_bytes, avail_bytes);
 	} else {
-		avail_bytes = audio_stream_get_avail_bytes(&hd->local_buffer->stream);
+		avail_bytes = audio_stream_get_avail_bytes(&buffer->stream);
 		copy_bytes = MIN(avail_bytes, free_bytes);
 		if (!copy_bytes)
 			comp_info(dev, "no bytes to copy, %d avail in buffer, %d free in DMA",
 				  avail_bytes, free_bytes);
 	}
-	buffer_unlock(hd->local_buffer, flags);
+
+	buffer = buffer_release_irq(buffer);
 
 	/* copy_bytes should be aligned to minimum possible chunk of
 	 * data to be copied by dma.
