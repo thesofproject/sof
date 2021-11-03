thesofproject/sof

commit 7e54f456eb07eb7bc94161b5ceab9e781158d326

drivers: Intel: hda-dma: ensure DMA channel is idle after stop
Make sure the HDA DMA channel is idle after stopping by checking the
GBUSY bit.

Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

src/drivers/intel/hda/hda-dma.c
@@ -58,6 +58,7 @@ DECLARE_TR_CTX(hdma_tr, SOF_UUID(hda_dma_uuid), LOG_LEVEL_INFO);
 #define DGCS_BF		BIT(9)  /* buffer full */
 #define DGCS_BNE	BIT(8)  /* buffer not empty */
 #define DGCS_FIFORDY	BIT(5)  /* enable FIFO */
+#define DGCS_GBUSY	BIT(15)  /* indicates if the DMA channel is idle or not */
 
 /* DGBBA */
 #define DGBBA_MASK	0xffff80
@@ -574,7 +575,7 @@ static int hda_dma_release(struct dma_chan_data *channel)
 static int hda_dma_stop_common(struct dma_chan_data *channel)
 {
 	struct hda_chan_data *hda_chan;
-	uint32_t flags;
+	uint32_t flags, dgcs;
 
 	irq_local_disable(flags);
 
@@ -593,6 +594,15 @@ static int hda_dma_stop_common(struct dma_chan_data *channel)
 		dma_chan_reg_update_bits(channel, DGCS, DGCS_FIFORDY | DGCS_GEN, 0);
 	} else {
 		dma_chan_reg_update_bits(channel, DGCS, DGCS_GEN, 0);
+	}
+
+	/* check if channel is idle. No need to wait after clearing the GEN bit */
+	dgcs = dma_chan_reg_read(channel, DGCS);
+	if (dgcs & DGCS_GBUSY) {
+		tr_err(&hdma_tr, "hda-dmac: %d channel %d not idle after stop",
+		       channel->dma->plat_data.id, channel->index);
+		irq_local_enable(flags);
+		return -EBUSY;
 	}
 	channel->status = COMP_STATE_PREPARE;
 	hda_chan = dma_chan_get_data(channel);

commit ea2fd40a7deead97b168107bbfa39a70c5a0f916

drivers: Intel: hda-dma: only check DGCS_BUSY with delayed stop
When an older kernel is used, we use the immediate stop. On some
platforms, we see an -EBUSY error that prevents the trigger stop from
working.

This patch adds a check to be 'bug-compatible' with previous releases
and older kernels. An additional fix would be to insert a poll timeout
to make sure the BUSY bit is cleared.

BugLink: https://github.com/thesofproject/sof/issues/4919
Fixes: 7e54f456e ('drivers: Intel: hda-dma: ensure DMA channel is idle after stop')
Suggested-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

src/drivers/intel/hda/hda-dma.c
@@ -645,6 +645,7 @@ static int hda_dma_release(struct dma_chan_data *channel)
 
 static int hda_dma_stop_common(struct dma_chan_data *channel)
 {
+	struct dai_data *dd = channel->dev_data;
 	struct hda_chan_data *hda_chan;
 	uint32_t flags, dgcs;
 
@@ -668,12 +669,14 @@ static int hda_dma_stop_common(struct dma_chan_data *channel)
 	}
 
 	/* check if channel is idle. No need to wait after clearing the GEN bit */
-	dgcs = dma_chan_reg_read(channel, DGCS);
-	if (dgcs & DGCS_GBUSY) {
-		tr_err(&hdma_tr, "hda-dmac: %d channel %d not idle after stop",
-		       channel->dma->plat_data.id, channel->index);
-		irq_local_enable(flags);
-		return -EBUSY;
+	if (dd && dd->delayed_dma_stop) {
+		dgcs = dma_chan_reg_read(channel, DGCS);
+		if (dgcs & DGCS_GBUSY) {
+			tr_err(&hdma_tr, "hda-dmac: %d channel %d not idle after stop",
+			       channel->dma->plat_data.id, channel->index);
+			irq_local_enable(flags);
+			return -EBUSY;
+		}
 	}
 	channel->status = COMP_STATE_PREPARE;
 	hda_chan = dma_chan_get_data(channel);