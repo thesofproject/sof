29fa4e1a9c949439f0b96ab367fb4e3211fd8784
Shriram Shastry <malladi.sastry@intel.com>
2021-11-26 08:14:42 -0800

    math : Add square root function using lookup table
    fix point math square function having positive number y as input and
    return the positive number x multiplied by itself (squared)
    
    Signed-off-by: Shriram Shastry <malladi.sastry@intel.com>

---
+++ src/include/sof/math/sqrt.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2021 Intel Corporation. All rights reserved.
+ *
+ * Author: Shriram Shastry <malladi.sastry@linux.intel.com>
+ *
+ */
+
+#ifndef __SOF_MATH__SQRTLOOKUP__H
+#define __SOF_MATH__SQRTLOOKUP__H
+
+#include <stdint.h>
+
+uint16_t sqrt_int16(uint16_t u);
+#endif
