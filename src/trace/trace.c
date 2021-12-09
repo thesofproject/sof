6a4e615d5e0879365040b3c99853a13b26d679fb
Marc Herbert <marc.herbert@intel.com>
2021-12-09 05:04:51 +0000

    Revert "zephyr: fix build after a recent trace filter PR"
    This reverts commit b31d19b6c2ad3ec51db5f7362ec4acfec86b0317.
    
    Part of the fix for #5032
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- src/trace/trace.c
+++ src/trace/trace.c
@@ -342,7 +342,7 @@ struct sof_ipc_trace_filter_elem *trace_filter_fill(struct sof_ipc_trace_filter_
 static int trace_filter_update_global(int32_t log_level, uint32_t uuid_id)
 {
 	int cnt = 0;
-#if !defined(__ZEPHYR__) && !defined(CONFIG_LIBRARY)
+#if !defined(CONFIG_LIBRARY)
 	extern void *_trace_ctx_start;
 	extern void *_trace_ctx_end;
 	struct tr_ctx *ptr = (struct tr_ctx *)&_trace_ctx_start;
