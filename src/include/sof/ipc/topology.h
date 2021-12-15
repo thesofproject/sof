2dcb5cc82751adc7bbfce144688ccd39ba8b4cb8
Rander Wang <rander.wang@intel.com>
2021-12-15 13:47:55 +0800

    ipc4: add chain dma support
    Chain dma is only support by HDA gateway. The idea is
    to copy data from host to dai directly without any process.
    It is just like a pass-through pipeline.
    
    This patch adds support by create a pass-through pipeline
    internally.
    
    Signed-off-by: Rander Wang <rander.wang@intel.com>

--- src/include/sof/ipc/topology.h
+++ src/include/sof/ipc/topology.h
@@ -50,6 +50,8 @@ const struct comp_driver *ipc4_get_comp_drv(int module_id);
 struct comp_dev *ipc4_get_comp_dev(uint32_t comp_id);
 int ipc4_add_comp_dev(struct comp_dev *dev);
 const struct comp_driver *ipc4_get_drv(uint8_t *uuid);
+int ipc4_create_chain_dma(struct ipc *ipc, struct ipc4_chain_dma *cdma);
+int ipc4_trigger_chain_dma(struct ipc *ipc, struct ipc4_chain_dma *cdma);
 #else
 #error "No or invalid IPC MAJOR version selected."
 #endif
