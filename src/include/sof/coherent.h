a53cb8837f4b653c3ea31ff77d6c1ad7607b99ab
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2022-02-03 14:41:15 +0100

    coherent: add locking variants for thread context
    When used from thread context the coherent API can use mutexes for
    locking to avoid unnecessarily disabling interrupts.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/include/sof/coherent.h
+++ src/include/sof/coherent.h
@@ -53,9 +53,18 @@
  * The shared flag is only set at coherent init and thereafter it's RO.
  */
 struct coherent {
-	struct k_spinlock lock;	/* locking mechanism */
-	k_spinlock_key_t key;	/* lock flags */
-	uint16_t shared;	/* shared on other non coherent cores */
+	union {
+		struct {
+			struct k_spinlock lock;	/* locking mechanism */
+			k_spinlock_key_t key;	/* lock flags */
+		};
+#ifdef __ZEPHYR__
+		struct k_mutex mutex;
+#endif
+	};
+	uint8_t sleep_allowed;	/* the object will never be acquired or released
+				 * in atomic context */
+	uint8_t shared;		/* shared on other non coherent cores */
 	uint16_t core;		/* owner core if not shared */
 	struct list_item list;	/* coherent list iteration */
 } __coherent;
@@ -76,6 +85,18 @@ struct coherent {
 #define CHECK_COHERENT_CORE(_c)
 #endif
 
+#ifdef __ZEPHYR__
+#define CHECK_ISR()		__ASSERT(!arch_is_in_isr(), "Attempt to sleep in ISR!")
+#define CHECK_SLEEP(_c)		__ASSERT((_c)->sleep_allowed, \
+				"This context hasn't been initialized for sleeping!")
+#define CHECK_ATOMIC(_c)	__ASSERT(!(_c)->sleep_allowed, \
+				"This context has been initialized for sleeping!")
+#else
+#define CHECK_ISR()		assert(!arch_is_in_isr())
+#define CHECK_SLEEP(_c)		assert((_c)->sleep_allowed)
+#define CHECK_ATOMIC(_c)	assert(!(_c)->sleep_allowed)
+#endif
+
 #if CONFIG_INCOHERENT
 /* When coherent_acquire() is called, we are sure not to have cache for this memory */
 __must_check static inline struct coherent *coherent_acquire(struct coherent *c,
@@ -83,6 +104,7 @@ __must_check static inline struct coherent *coherent_acquire(struct coherent *c,
 {
 	/* assert if someone passes a cache/local address in here. */
 	ADDR_IS_COHERENT(c);
+	CHECK_ATOMIC(c);
 
 	/* access the shared coherent object */
 	if (c->shared) {
@@ -102,6 +124,7 @@ static inline struct coherent *coherent_release(struct coherent *c, const size_t
 {
 	/* assert if someone passes a coherent address in here. */
 	ADDR_IS_INCOHERENT(c);
+	CHECK_ATOMIC(c);
 
 	/* access the local copy of object */
 	if (c->shared) {
@@ -117,18 +140,120 @@ static inline struct coherent *coherent_release(struct coherent *c, const size_t
 	return cache_to_uncache(c);
 }
 
-#define coherent_init(object, member)						\
-	do {									\
-		/* assert if someone passes a cache/local address in here. */	\
-		ADDR_IS_COHERENT(object);					\
-		/* TODO static assert if we are not cache aligned */		\
-		k_spinlock_init(&object->member.lock);				\
-		object->member.shared = false;					\
-		object->member.core = cpu_get_id();				\
-		list_init(&object->member.list);				\
-		/* inv local data to coherent object */				\
-		dcache_invalidate_region(uncache_to_cache(object), sizeof(*object)); \
-	} while (0)
+static inline void __coherent_init(struct coherent *c, const size_t size)
+{
+	/* assert if someone passes a cache/local address in here. */
+	ADDR_IS_COHERENT(c);
+
+	/* TODO static assert if we are not cache aligned */
+	k_spinlock_init(&c->lock);
+	c->sleep_allowed = false;
+	c->shared = false;
+	c->core = cpu_get_id();
+	list_init(&c->list);
+	/* inv local data to coherent object */
+	dcache_invalidate_region(uncache_to_cache(c), size);
+}
+
+#define coherent_init(object, member) __coherent_init(&(object)->member, \
+						      sizeof(*object))
+
+/* set the object to shared mode with coherency managed by SW */
+static inline void __coherent_shared(struct coherent *c, const size_t size)
+{
+	/* assert if someone passes a cache/local address in here. */
+	ADDR_IS_COHERENT(c);
+	CHECK_ATOMIC(c);
+
+	c->key = k_spin_lock(&c->lock);
+	c->shared = true;
+	dcache_invalidate_region(c, size);
+	k_spin_unlock(&c->lock, c->key);
+}
+
+#define coherent_shared(object, member) __coherent_shared(&(object)->member, \
+							  sizeof(*object))
+
+#ifdef __ZEPHYR__
+
+__must_check static inline struct coherent *coherent_acquire_thread(struct coherent *c,
+								    const size_t size)
+{
+	/* assert if someone passes a cache/local address in here. */
+	ADDR_IS_COHERENT(c);
+	CHECK_SLEEP(c);
+	CHECK_ISR();
+
+	/* access the shared coherent object */
+	if (c->shared) {
+		CHECK_COHERENT_CORE(c);
+
+		k_mutex_lock(&c->mutex, K_FOREVER);
+
+		/* invalidate local copy */
+		dcache_invalidate_region(uncache_to_cache(c), size);
+	}
+
+	/* client can now use cached object safely */
+	return uncache_to_cache(c);
+}
+
+static inline struct coherent *coherent_release_thread(struct coherent *c, const size_t size)
+{
+	/* assert if someone passes a coherent address in here. */
+	ADDR_IS_INCOHERENT(c);
+	CHECK_SLEEP(c);
+	CHECK_ISR();
+
+	/* access the local copy of object */
+	if (c->shared) {
+		CHECK_COHERENT_CORE(c);
+
+		/* wtb and inv local data to coherent object */
+		dcache_writeback_invalidate_region(c, size);
+
+		/* unlock on uncache alias */
+		k_mutex_unlock(&cache_to_uncache(c)->mutex);
+	}
+
+	return cache_to_uncache(c);
+}
+
+static inline void __coherent_init_thread(struct coherent *c, const size_t size)
+{
+	/* assert if someone passes a cache/local address in here. */
+	ADDR_IS_COHERENT(c);
+
+	/* TODO static assert if we are not cache aligned */
+	k_mutex_init(&c->mutex);
+	c->sleep_allowed = true;
+	c->shared = false;
+	c->core = cpu_get_id();
+	list_init(&c->list);
+	/* inv local data to coherent object */
+	dcache_invalidate_region(uncache_to_cache(c), size);
+}
+
+#define coherent_init_thread(object, member) __coherent_init_thread(&(object)->member, \
+								    sizeof(*object))
+
+static inline void __coherent_shared_thread(struct coherent *c, const size_t size)
+{
+	/* assert if someone passes a cache/local address in here. */
+	ADDR_IS_COHERENT(c);
+	CHECK_SLEEP(c);
+	CHECK_ISR();
+
+	k_mutex_lock(&c->mutex, K_FOREVER);
+	c->shared = true;
+	dcache_invalidate_region(c, size);
+	k_mutex_unlock(&c->mutex);
+}
+
+#define coherent_shared_thread(object, member) __coherent_shared_thread(&(object)->member, \
+									sizeof(*object))
+
+#endif /* __ZEPHYR__ */
 
 #define coherent_free(object, member)						\
 	do {									\
@@ -138,17 +263,7 @@ static inline struct coherent *coherent_release(struct coherent *c, const size_t
 		dcache_writeback_invalidate_region(uncache_to_cache(object), sizeof(*object)); \
 	} while (0)
 
-/* set the object to shared mode with coherency managed by SW */
-#define coherent_shared(object, member)						\
-	do {									\
-		/* assert if someone passes a cache/local address in here. */	\
-		ADDR_IS_COHERENT(object);					\
-		(object)->member.key = k_spin_lock(&(object)->member.lock);	\
-		(object)->member.shared = true;					\
-		dcache_invalidate_region(object, sizeof(*object));		\
-		k_spin_unlock(&(object)->member.lock, (object)->member.key);	\
-	} while (0)
-#else
+#else /* CONFIG_INCOHERENT */
 
 /*
  * Coherent devices only require locking to manage shared access.
@@ -177,21 +292,72 @@ static inline struct coherent *coherent_release(struct coherent *c, const size_t
 	return c;
 }
 
-#define coherent_init(object, member)						\
-	do {									\
-		/* TODO static assert if we are not cache aligned */		\
-		k_spinlock_init(&object->member.lock);				\
-		object->member.shared = 0;					\
-		object->member.core = cpu_get_id();				\
-		list_init(&object->member.list);				\
-	} while (0)
+static inline void __coherent_init(struct coherent *c, const size_t size)
+{
+	/* TODO static assert if we are not cache aligned */
+	k_spinlock_init(&c->lock);
+	c->shared = 0;
+	c->core = cpu_get_id();
+	list_init(&c->list);
+}
 
-#define coherent_free(object, member)
+#define coherent_init(object, member) __coherent_init(&(object)->member, \
+						      sizeof(*object))
+
+#define coherent_free(object, member) do {} while (0)
 
 /* no function on cache coherent architectures */
-#define coherent_shared(object, member) ((object)->member.shared = true)
+#define coherent_shared(object, member) (object)->member.shared = true
 
-#endif /* CONFIG_CAVS && CONFIG_CORE_COUNT > 1 */
+#ifdef __ZEPHYR__
+__must_check static inline struct coherent *coherent_acquire_thread(struct coherent *c,
+								    const size_t size)
+{
+	if (c->shared) {
+		k_mutex_lock(&c->mutex, K_FOREVER);
+
+		/* invalidate local copy */
+		dcache_invalidate_region(uncache_to_cache(c), size);
+	}
+
+	return c;
+}
+
+static inline struct coherent *coherent_release_thread(struct coherent *c, const size_t size)
+{
+	if (c->shared) {
+		/* wtb and inv local data to coherent object */
+		dcache_writeback_invalidate_region(uncache_to_cache(c), size);
+
+		k_mutex_unlock(&c->mutex);
+	}
+
+	return c;
+}
+
+static inline void __coherent_init_thread(struct coherent *c, const size_t size)
+{
+	/* TODO static assert if we are not cache aligned */
+	k_mutex_init(&c->mutex);
+	c->shared = 0;
+	c->core = cpu_get_id();
+	list_init(&c->list);
+}
+
+#define coherent_init_thread(object, member) __coherent_init_thread(&(object)->member, \
+								    sizeof(*object))
+#endif /* __ZEPHYR__ */
+
+#endif /* CONFIG_INCOHERENT */
+
+#ifndef __ZEPHYR__
+#define coherent_acquire_thread coherent_acquire
+#define coherent_release_thread coherent_release
+#define coherent_init_thread coherent_init
+#define coherent_shared_thread coherent_shared
+#endif
+
+#define coherent_free_thread coherent_free
 
 #define is_coherent_shared(object, member) ((object)->member.shared)
 #endif
