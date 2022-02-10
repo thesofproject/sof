thesofproject/sof

commit 0d296ce7d179b75b3fd0c2178891c51a88e6ca6f

Tools: Tune: Make example FIR response long for large IPC test
The loudness EQ is changed to 250 taps to create large IPC
message type. It also improves subjective quality of this
effect with better bass response. The earlier version was
made very short to fit the that time IPC size limit.

Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

tools/topology/topology1/m4/eq_fir_coef_loudness.m4
@@ -1,39 +1,68 @@
-# Loudness effect, created with example_fir_eq.m 30-Mar-2020
+# Loudness effect, created with example_fir_eq.m 10-Dec-2021
 CONTROLBYTES_PRIV(DEF_EQFIR_PRIV,
 `       bytes "0x53,0x4f,0x46,0x00,0x00,0x00,0x00,0x00,'
-`       0x00,0x01,0x00,0x00,0x00,0xe0,0x00,0x03,'
+`       0xe8,0x01,0x00,0x00,0x00,0x40,0x01,0x03,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
-`       0x00,0x01,0x00,0x00,0x04,0x00,0x02,0x00,'
+`       0xe8,0x01,0x00,0x00,0x04,0x00,0x02,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
 `       0x04,0x00,0xff,0xff,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x40,0x00,0x00,'
-`       0x00,0x00,0x00,0x00,0x58,0x00,0x00,0x00,'
+`       0x00,0x00,0x00,0x00,0xcc,0x00,0x04,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
-`       0x1b,0x6b,0xb9,0xd2,0xf4,0x0b,0x18,0x07,'
-`       0x15,0x00,0xfa,0x02,0x11,0x02,0xcc,0x01,'
-`       0x12,0x02,0xcf,0x01,0xdc,0x01,0xd0,0x01,'
-`       0xc9,0x01,0xc1,0x01,0xb1,0x01,0xa1,0x01,'
-`       0x91,0x01,0x84,0x01,0x77,0x01,0x6d,0x01,'
-`       0x62,0x01,0x57,0x01,0x4b,0x01,0x3f,0x01,'
-`       0x31,0x01,0x26,0x01,0x1a,0x01,0x10,0x01,'
-`       0x07,0x01,0xfe,0x00,0xf6,0x00,0xef,0x00,'
-`       0xe8,0x00,0xe1,0x00,0xd9,0x00,0xd2,0x00,'
-`       0xcb,0x00,0xc4,0x00,0xbc,0x00,0xb5,0x00,'
-`       0xad,0x00,0xa6,0x00,0x9f,0x00,0x99,0x00,'
-`       0x92,0x00,0x8c,0x00,0x86,0x00,0x80,0x00,'
-`       0x7a,0x00,0x74,0x00,0x6f,0x00,0x6a,0x00,'
-`       0x65,0x00,0x60,0x00,0x5b,0x00,0x56,0x00,'
-`       0x52,0x00,0x4d,0x00,0x49,0x00,0x45,0x00,'
-`       0x41,0x00,0x3d,0x00,0x3a,0x00,0x36,0x00,'
-`       0x33,0x00,0x30,0x00,0x2d,0x00,0x2a,0x00,'
-`       0x28,0x00,0x25,0x00,0x23,0x00,0x21,0x00,'
-`       0x1f,0x00,0x1d,0x00,0x1b,0x00,0x19,0x00,'
-`       0x17,0x00,0x16,0x00,0x14,0x00,0x13,0x00,'
-`       0x11,0x00,0x10,0x00,0x0f,0x00,0x0e,0x00,'
-`       0x0d,0x00,0x0b,0x00,0x07,0x00,0x00,0x00"'
+`       0x2b,0x00,0xee,0xff,0x05,0x00,0x03,0x00,'
+`       0x00,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
+`       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
+`       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
+`       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
+`       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
+`       0x00,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
+`       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
+`       0x01,0x00,0x02,0x00,0x02,0x00,0x03,0x00,'
+`       0x04,0x00,0x06,0x00,0x07,0x00,0x0b,0x00,'
+`       0x0c,0x00,0x14,0x00,0x16,0x00,0x22,0x00,'
+`       0x25,0x00,0x3a,0x00,0x3e,0x00,0x5d,0x00,'
+`       0x64,0x00,0x93,0x00,0x9c,0x00,0xe1,0x00,'
+`       0xee,0x00,0x51,0x01,0x61,0x01,0xea,0x01,'
+`       0xff,0x01,0xb9,0x02,0xd2,0x02,0xca,0x03,'
+`       0xe7,0x03,0x2a,0x05,0x49,0x05,0xe4,0x06,'
+`       0x02,0x07,0x05,0x09,0x1e,0x09,0x97,0x0b,'
+`       0xa5,0x0b,0x9f,0x0e,0x99,0x0e,0x22,0x12,'
+`       0xfd,0x11,0x1b,0x16,0xca,0x15,0x84,0x1a,'
+`       0xf7,0x19,0x4d,0x1f,0x73,0x1e,0x60,0x24,'
+`       0x26,0x23,0xa3,0x29,0xf3,0x27,0xf3,0x2e,'
+`       0xbb,0x2c,0x2b,0x34,0x54,0x31,0x20,0x39,'
+`       0x9a,0x35,0xa7,0x3d,0x62,0x39,0x97,0x41,'
+`       0x88,0x3c,0xc8,0x44,0xe8,0x3e,0x18,0x47,'
+`       0x67,0x40,0x6a,0x48,0xee,0x40,0xae,0x48,'
+`       0x72,0x40,0xd9,0x47,0xee,0x3e,0xee,0x45,'
+`       0x6e,0x3c,0xf7,0x42,0xff,0x38,0x0d,0x3f,'
+`       0xbe,0x34,0x4c,0x3a,0xcb,0x2f,0xdc,0x34,'
+`       0x4f,0x2a,0xe8,0x2e,0x76,0x24,0xa1,0x28,'
+`       0x6e,0x1e,0x33,0x22,0x63,0x18,0xd0,0x1b,'
+`       0x81,0x12,0xa2,0x15,0xec,0x0c,0xce,0x0f,'
+`       0xc8,0x07,0x75,0x0a,0x2c,0x03,0xae,0x05,'
+`       0x2d,0xff,0x8b,0x01,0xd3,0xfb,0x14,0xfe,'
+`       0x23,0xf9,0x4b,0xfb,0x19,0xf7,0x2b,0xf9,'
+`       0xac,0xf5,0xa8,0xf7,0xcf,0xf4,0xb4,0xf6,'
+`       0x70,0xf4,0x3d,0xf6,0x7c,0xf4,0x2f,0xf6,'
+`       0xdf,0xf4,0x75,0xf6,0x84,0xf5,0xfc,0xf6,'
+`       0x59,0xf6,0xb1,0xf7,0x4b,0xf7,0x82,0xf8,'
+`       0x4c,0xf8,0x61,0xf9,0x4e,0xf9,0x43,0xfa,'
+`       0x49,0xfa,0x1e,0xfb,0x33,0xfb,0xea,0xfb,'
+`       0x09,0xfc,0xa4,0xfc,0xc8,0xfc,0x49,0xfd,'
+`       0x6d,0xfd,0xd7,0xfd,0xfa,0xfd,0x50,0xfe,'
+`       0x6f,0xfe,0xb4,0xfe,0xcf,0xfe,0x05,0xff,'
+`       0x1b,0xff,0x45,0xff,0x58,0xff,0x78,0xff,'
+`       0x86,0xff,0x9e,0xff,0xaa,0xff,0xbb,0xff,'
+`       0xc4,0xff,0xd0,0xff,0xd7,0xff,0xe0,0xff,'
+`       0xe4,0xff,0xeb,0xff,0xee,0xff,0xf2,0xff,'
+`       0xf4,0xff,0xf7,0xff,0xf9,0xff,0xfa,0xff,'
+`       0xfb,0xff,0xfd,0xff,0xfd,0xff,0xfe,0xff,'
+`       0xfe,0xff,0xff,0xff,0xff,0xff,0xff,0xff,'
+`       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00"'
 )

tools/tune/eq/example_fir_eq.m
@@ -170,7 +170,7 @@ eq.norm_offs_db = 0;        % Offset in dB to normalize
 
 eq.enable_fir = 1;          % By default both FIR and IIR disabled, enable one
 eq.fir_beta = 4.0;          % Use with care, low value can corrupt
-eq.fir_length = 86;         % Gives just < 292 bytes
+eq.fir_length = 250;        % Long filter (test large IPC messages)
 eq.fir_autoband = 0;        % Select manually frequency limits
 eq.fmin_fir = 100;          % Equalization starts from 100 Hz
 eq.fmax_fir = 20e3;         % Equalization ends at 20 kHz

commit 9b2e22ba8671d7a446e54f2142dfa7d0d08a8f0f

Tools: Topology: Fix erratic FIR example response
The previous patch increased the FIR length but it also triggered
a numerical issue in FIR design with long minimum phase filters. The
filter became essentially a band-stop filter and causes difficulties
in test of EQ component. Also the equalizer sounds subjectively
bad. The fix is to change the design to linear phase that always
converges.

Fixes: 0d296ce7d179b75b3fd0c2178891c51a88e6ca6f

Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

tools/topology/topology1/m4/eq_fir_coef_loudness.m4
@@ -1,68 +1,80 @@
-# Loudness effect, created with example_fir_eq.m 10-Dec-2021
+# Loudness effect, created with example_fir_eq.m 09-Feb-2022
 CONTROLBYTES_PRIV(DEF_EQFIR_PRIV,
 `       bytes "0x53,0x4f,0x46,0x00,0x00,0x00,0x00,0x00,'
-`       0xe8,0x01,0x00,0x00,0x00,0x40,0x01,0x03,'
+`       0x48,0x02,0x00,0x00,0x00,0x40,0x01,0x03,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
-`       0xe8,0x01,0x00,0x00,0x04,0x00,0x02,0x00,'
+`       0x48,0x02,0x00,0x00,0x04,0x00,0x02,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
 `       0x04,0x00,0xff,0xff,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x40,0x00,0x00,'
-`       0x00,0x00,0x00,0x00,0xcc,0x00,0x04,0x00,'
+`       0x00,0x00,0x00,0x00,0xfc,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
 `       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'
-`       0x2b,0x00,0xee,0xff,0x05,0x00,0x03,0x00,'
-`       0x00,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
 `       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
 `       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
+`       0x01,0x00,0x02,0x00,0x02,0x00,0x02,0x00,'
+`       0x02,0x00,0x02,0x00,0x03,0x00,0x03,0x00,'
+`       0x03,0x00,0x03,0x00,0x03,0x00,0x04,0x00,'
+`       0x04,0x00,0x04,0x00,0x05,0x00,0x05,0x00,'
+`       0x05,0x00,0x06,0x00,0x06,0x00,0x06,0x00,'
+`       0x07,0x00,0x07,0x00,0x08,0x00,0x08,0x00,'
+`       0x09,0x00,0x09,0x00,0x0a,0x00,0x0a,0x00,'
+`       0x0b,0x00,0x0b,0x00,0x0c,0x00,0x0c,0x00,'
+`       0x0d,0x00,0x0d,0x00,0x0e,0x00,0x0f,0x00,'
+`       0x0f,0x00,0x10,0x00,0x11,0x00,0x12,0x00,'
+`       0x12,0x00,0x13,0x00,0x14,0x00,0x15,0x00,'
+`       0x16,0x00,0x17,0x00,0x18,0x00,0x19,0x00,'
+`       0x1a,0x00,0x1b,0x00,0x1d,0x00,0x1e,0x00,'
+`       0x1f,0x00,0x21,0x00,0x22,0x00,0x24,0x00,'
+`       0x26,0x00,0x28,0x00,0x29,0x00,0x2b,0x00,'
+`       0x2d,0x00,0x2f,0x00,0x32,0x00,0x34,0x00,'
+`       0x36,0x00,0x38,0x00,0x3b,0x00,0x3d,0x00,'
+`       0x40,0x00,0x42,0x00,0x45,0x00,0x48,0x00,'
+`       0x4b,0x00,0x4d,0x00,0x51,0x00,0x54,0x00,'
+`       0x57,0x00,0x5b,0x00,0x5e,0x00,0x62,0x00,'
+`       0x65,0x00,0x68,0x00,0x6c,0x00,0x6f,0x00,'
+`       0x72,0x00,0x76,0x00,0x79,0x00,0x7d,0x00,'
+`       0x81,0x00,0x86,0x00,0x8c,0x00,0x92,0x00,'
+`       0x98,0x00,0x9f,0x00,0xa5,0x00,0xac,0x00,'
+`       0xb2,0x00,0xb8,0x00,0xbe,0x00,0xc6,0x00,'
+`       0xce,0x00,0xd8,0x00,0xe1,0x00,0xea,0x00,'
+`       0xef,0x00,0xf5,0x00,0xfc,0x00,0xfc,0x00,'
+`       0x18,0x01,0xff,0x00,0x2f,0x01,0x70,0x01,'
+`       0xc3,0x00,0x39,0x04,0xd3,0x02,0xae,0xe9,'
+`       0xbf,0x6c,0xae,0xe9,0xd3,0x02,0x39,0x04,'
+`       0xc3,0x00,0x70,0x01,0x2f,0x01,0xff,0x00,'
+`       0x18,0x01,0xfc,0x00,0xfc,0x00,0xf5,0x00,'
+`       0xef,0x00,0xea,0x00,0xe1,0x00,0xd8,0x00,'
+`       0xce,0x00,0xc6,0x00,0xbe,0x00,0xb8,0x00,'
+`       0xb2,0x00,0xac,0x00,0xa5,0x00,0x9f,0x00,'
+`       0x98,0x00,0x92,0x00,0x8c,0x00,0x86,0x00,'
+`       0x81,0x00,0x7d,0x00,0x79,0x00,0x76,0x00,'
+`       0x72,0x00,0x6f,0x00,0x6c,0x00,0x68,0x00,'
+`       0x65,0x00,0x62,0x00,0x5e,0x00,0x5b,0x00,'
+`       0x57,0x00,0x54,0x00,0x51,0x00,0x4d,0x00,'
+`       0x4b,0x00,0x48,0x00,0x45,0x00,0x42,0x00,'
+`       0x40,0x00,0x3d,0x00,0x3b,0x00,0x38,0x00,'
+`       0x36,0x00,0x34,0x00,0x32,0x00,0x2f,0x00,'
+`       0x2d,0x00,0x2b,0x00,0x29,0x00,0x28,0x00,'
+`       0x26,0x00,0x24,0x00,0x22,0x00,0x21,0x00,'
+`       0x1f,0x00,0x1e,0x00,0x1d,0x00,0x1b,0x00,'
+`       0x1a,0x00,0x19,0x00,0x18,0x00,0x17,0x00,'
+`       0x16,0x00,0x15,0x00,0x14,0x00,0x13,0x00,'
+`       0x12,0x00,0x12,0x00,0x11,0x00,0x10,0x00,'
+`       0x0f,0x00,0x0f,0x00,0x0e,0x00,0x0d,0x00,'
+`       0x0d,0x00,0x0c,0x00,0x0c,0x00,0x0b,0x00,'
+`       0x0b,0x00,0x0a,0x00,0x0a,0x00,0x09,0x00,'
+`       0x09,0x00,0x08,0x00,0x08,0x00,0x07,0x00,'
+`       0x07,0x00,0x06,0x00,0x06,0x00,0x06,0x00,'
+`       0x05,0x00,0x05,0x00,0x05,0x00,0x04,0x00,'
+`       0x04,0x00,0x04,0x00,0x03,0x00,0x03,0x00,'
+`       0x03,0x00,0x03,0x00,0x03,0x00,0x02,0x00,'
+`       0x02,0x00,0x02,0x00,0x02,0x00,0x02,0x00,'
 `       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
 `       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
-`       0x00,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
-`       0x01,0x00,0x01,0x00,0x01,0x00,0x01,0x00,'
-`       0x01,0x00,0x02,0x00,0x02,0x00,0x03,0x00,'
-`       0x04,0x00,0x06,0x00,0x07,0x00,0x0b,0x00,'
-`       0x0c,0x00,0x14,0x00,0x16,0x00,0x22,0x00,'
-`       0x25,0x00,0x3a,0x00,0x3e,0x00,0x5d,0x00,'
-`       0x64,0x00,0x93,0x00,0x9c,0x00,0xe1,0x00,'
-`       0xee,0x00,0x51,0x01,0x61,0x01,0xea,0x01,'
-`       0xff,0x01,0xb9,0x02,0xd2,0x02,0xca,0x03,'
-`       0xe7,0x03,0x2a,0x05,0x49,0x05,0xe4,0x06,'
-`       0x02,0x07,0x05,0x09,0x1e,0x09,0x97,0x0b,'
-`       0xa5,0x0b,0x9f,0x0e,0x99,0x0e,0x22,0x12,'
-`       0xfd,0x11,0x1b,0x16,0xca,0x15,0x84,0x1a,'
-`       0xf7,0x19,0x4d,0x1f,0x73,0x1e,0x60,0x24,'
-`       0x26,0x23,0xa3,0x29,0xf3,0x27,0xf3,0x2e,'
-`       0xbb,0x2c,0x2b,0x34,0x54,0x31,0x20,0x39,'
-`       0x9a,0x35,0xa7,0x3d,0x62,0x39,0x97,0x41,'
-`       0x88,0x3c,0xc8,0x44,0xe8,0x3e,0x18,0x47,'
-`       0x67,0x40,0x6a,0x48,0xee,0x40,0xae,0x48,'
-`       0x72,0x40,0xd9,0x47,0xee,0x3e,0xee,0x45,'
-`       0x6e,0x3c,0xf7,0x42,0xff,0x38,0x0d,0x3f,'
-`       0xbe,0x34,0x4c,0x3a,0xcb,0x2f,0xdc,0x34,'
-`       0x4f,0x2a,0xe8,0x2e,0x76,0x24,0xa1,0x28,'
-`       0x6e,0x1e,0x33,0x22,0x63,0x18,0xd0,0x1b,'
-`       0x81,0x12,0xa2,0x15,0xec,0x0c,0xce,0x0f,'
-`       0xc8,0x07,0x75,0x0a,0x2c,0x03,0xae,0x05,'
-`       0x2d,0xff,0x8b,0x01,0xd3,0xfb,0x14,0xfe,'
-`       0x23,0xf9,0x4b,0xfb,0x19,0xf7,0x2b,0xf9,'
-`       0xac,0xf5,0xa8,0xf7,0xcf,0xf4,0xb4,0xf6,'
-`       0x70,0xf4,0x3d,0xf6,0x7c,0xf4,0x2f,0xf6,'
-`       0xdf,0xf4,0x75,0xf6,0x84,0xf5,0xfc,0xf6,'
-`       0x59,0xf6,0xb1,0xf7,0x4b,0xf7,0x82,0xf8,'
-`       0x4c,0xf8,0x61,0xf9,0x4e,0xf9,0x43,0xfa,'
-`       0x49,0xfa,0x1e,0xfb,0x33,0xfb,0xea,0xfb,'
-`       0x09,0xfc,0xa4,0xfc,0xc8,0xfc,0x49,0xfd,'
-`       0x6d,0xfd,0xd7,0xfd,0xfa,0xfd,0x50,0xfe,'
-`       0x6f,0xfe,0xb4,0xfe,0xcf,0xfe,0x05,0xff,'
-`       0x1b,0xff,0x45,0xff,0x58,0xff,0x78,0xff,'
-`       0x86,0xff,0x9e,0xff,0xaa,0xff,0xbb,0xff,'
-`       0xc4,0xff,0xd0,0xff,0xd7,0xff,0xe0,0xff,'
-`       0xe4,0xff,0xeb,0xff,0xee,0xff,0xf2,0xff,'
-`       0xf4,0xff,0xf7,0xff,0xf9,0xff,0xfa,0xff,'
-`       0xfb,0xff,0xfd,0xff,0xfd,0xff,0xfe,0xff,'
-`       0xfe,0xff,0xff,0xff,0xff,0xff,0xff,0xff,'
-`       0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00"'
+`       0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00"'
 )

tools/tune/eq/example_fir_eq.m
@@ -174,7 +174,7 @@ eq.fir_length = 250;        % Long filter (test large IPC messages)
 eq.fir_autoband = 0;        % Select manually frequency limits
 eq.fmin_fir = 100;          % Equalization starts from 100 Hz
 eq.fmax_fir = 20e3;         % Equalization ends at 20 kHz
-eq.fir_minph = 1;           % Check result carefully if 1 is used, 0 is safe
+eq.fir_minph = 0;           % Check result carefully if 1 is used, 0 is safe
 eq = eq_compute(eq);
 
 %% Plot