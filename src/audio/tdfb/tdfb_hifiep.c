b6a672c3defd4e1e73631b7f435f4cdd1a37476c
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2022-02-11 18:10:10 +0200

    Audio: TDFB: Optimize read/write frag in HiFiEP copy() function
    The change similar as in generic C version patch but with
    different core function.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/tdfb/tdfb_hifiep.c
+++ src/audio/tdfb/tdfb_hifiep.c
@@ -15,67 +15,86 @@
 
 #include <sof/math/fir_hifi2ep.h>
 
+static inline void tdfb_core(struct tdfb_comp_data *cd, int in_nch, int out_nch)
+{
+	struct fir_state_32x16 *f;
+	int32_t y0;
+	int32_t y1;
+	int lshift;
+	int rshift;
+	int is2;
+	int is;
+	int om;
+	int i;
+	int k;
+	const int num_filters = cd->config->num_filters;
+
+	/* Clear output mix*/
+	memset(cd->out, 0, 2 * out_nch * sizeof(int32_t));
+
+	/* Run and mix all filters to their output channel */
+	for (i = 0; i < num_filters; i++) {
+		is = cd->input_channel_select[i];
+		is2 = is + in_nch;
+		om = cd->output_channel_mix[i];
+		/* Prepare FIR */
+		f = &cd->fir[i];
+		fir_hifiep_setup_circular(f);
+		fir_get_lrshifts(f, &lshift, &rshift);
+		/* Process two samples */
+		fir_32x16_2x_hifiep(f, cd->in[is], cd->in[is2], &y0, &y1, lshift, rshift);
+		/* Mix as Q5.27 */
+		for (k = 0; k < out_nch; k++) {
+			if (om & 1) {
+				cd->out[k] += y0 >> 4;
+				cd->out[k + out_nch] += y1 >> 4;
+			}
+			om = om >> 1;
+		}
+	}
+}
+
 #if CONFIG_FORMAT_S16LE
 void tdfb_fir_s16(struct tdfb_comp_data *cd,
 		  const struct audio_stream *source,
 		  struct audio_stream *sink, int frames)
 {
-	struct sof_tdfb_config *cfg = cd->config;
-	struct fir_state_32x16 *f;
-	int32_t y0;
-	int32_t y1;
-	int16_t *x;
-	int16_t *y;
-	int rshift;
-	int lshift;
-	int is2;
-	int is;
-	int om;
+	int16_t *x = source->r_ptr;
+	int16_t *y = sink->w_ptr;
+	int fmax;
 	int i;
 	int j;
-	int k;
-	int in_nch = source->channels;
-	int out_nch = sink->channels;
-	int idx_in = 0;
-	int idx_out = 0;
+	int f;
+	const int in_nch = source->channels;
+	const int out_nch = sink->channels;
+	int remaining_frames = frames;
+	int emp_ch = 0;
 
-	for (j = 0; j < (frames >> 1); j++) {
-		/* Clear output mix*/
-		memset(cd->out, 0,  2 * out_nch * sizeof(int32_t));
+	while (remaining_frames) {
+		fmax = audio_stream_frames_without_wrap(source, x);
+		f = MIN(remaining_frames, fmax);
+		fmax = audio_stream_frames_without_wrap(sink, y);
+		f = MIN(f, fmax);
+		for (j = 0; j < f; j += 2) {
+			/* Read two frames from all input channels */
+			for (i = 0; i < 2 * in_nch; i++) {
+				cd->in[i] = *x << 16;
+				tdfb_direction_copy_emphasis(cd, in_nch, &emp_ch, *x << 16);
+				x++;
+			}
 
-		/* Read two frames from all input channels */
-		for (i = 0; i < 2 * in_nch; i++) {
-			x = audio_stream_read_frag_s16(source, idx_in++);
-			cd->in[i] = *x << 16;
-		}
+			/* Process */
+			tdfb_core(cd, in_nch, out_nch);
 
-		/* Run and mix all filters to their output channel */
-		for (i = 0; i < cfg->num_filters; i++) {
-			is = cd->input_channel_select[i];
-			is2 = is + in_nch;
-			om = cd->output_channel_mix[i];
-			/* Prepare FIR */
-			f = &cd->fir[i];
-			fir_hifiep_setup_circular(f);
-			fir_get_lrshifts(f, &lshift, &rshift);
-			/* Process two samples */
-			fir_32x16_2x_hifiep(f, cd->in[is], cd->in[is2],
-					    &y0, &y1, lshift, rshift);
-			/* Mix as Q5.27 */
-			for (k = 0; k < out_nch; k++) {
-				if (om & 1) {
-					cd->out[k] += y0 >> 4;
-					cd->out[k + out_nch] += y1 >> 4;
-				}
-				om = om >> 1;
+			/* Write two frames of output */
+			for (i = 0; i < 2 * out_nch; i++) {
+				*y = sat_int16(Q_SHIFT_RND(cd->out[i], 27, 15));
+				y++;
 			}
 		}
-
-		/* Write two frames of output */
-		for (i = 0; i < 2 * out_nch; i++) {
-			y = audio_stream_write_frag_s16(sink, idx_out++);
-			*y = sat_int16(Q_SHIFT_RND(cd->out[i], 27, 15));
-		}
+		remaining_frames -= f;
+		x = audio_stream_wrap(source, x);
+		y = audio_stream_wrap(sink, y);
 	}
 }
 #endif
@@ -85,129 +104,91 @@ void tdfb_fir_s24(struct tdfb_comp_data *cd,
 		  const struct audio_stream *source,
 		  struct audio_stream *sink, int frames)
 {
-	struct sof_tdfb_config *cfg = cd->config;
-	struct fir_state_32x16 *f;
-	int32_t y0;
-	int32_t y1;
-	int32_t *x;
-	int32_t *y;
-	int rshift;
-	int lshift;
-	int is2;
-	int is;
-	int om;
+	int32_t *x = source->r_ptr;
+	int32_t *y = sink->w_ptr;
+	int fmax;
 	int i;
 	int j;
-	int k;
-	int in_nch = source->channels;
-	int out_nch = sink->channels;
-	int idx_in = 0;
-	int idx_out = 0;
+	int f;
+	const int in_nch = source->channels;
+	const int out_nch = sink->channels;
+	int remaining_frames = frames;
+	int emp_ch = 0;
 
-	for (j = 0; j < (frames >> 1); j++) {
-		/* Clear output mix*/
-		memset(cd->out, 0,  2 * out_nch * sizeof(int32_t));
+	while (remaining_frames) {
+		fmax = audio_stream_frames_without_wrap(source, x);
+		f = MIN(remaining_frames, fmax);
+		fmax = audio_stream_frames_without_wrap(sink, y);
+		f = MIN(f, fmax);
+		for (j = 0; j < f; j += 2) {
+			/* Read two frames from all input channels */
+			for (i = 0; i < 2 * in_nch; i++) {
+				cd->in[i] = *x << 8;
+				tdfb_direction_copy_emphasis(cd, in_nch, &emp_ch, *x << 8);
+				x++;
+			}
 
-		/* Read two frames from all input channels */
-		for (i = 0; i < 2 * in_nch; i++) {
-			x = audio_stream_read_frag_s32(source, idx_in++);
-			cd->in[i] = *x << 8;
-		}
+			/* Process */
+			tdfb_core(cd, in_nch, out_nch);
 
-		/* Run and mix all filters to their output channel */
-		for (i = 0; i < cfg->num_filters; i++) {
-			is = cd->input_channel_select[i];
-			is2 = is + in_nch;
-			om = cd->output_channel_mix[i];
-			/* Prepare FIR */
-			f = &cd->fir[i];
-			fir_hifiep_setup_circular(f);
-			fir_get_lrshifts(f, &lshift, &rshift);
-			/* Process two samples */
-			fir_32x16_2x_hifiep(f, cd->in[is], cd->in[is2],
-					    &y0, &y1, lshift, rshift);
-			/* Mix as Q5.27 */
-			for (k = 0; k < out_nch; k++) {
-				if (om & 1) {
-					cd->out[k] += y0 >> 4;
-					cd->out[k + out_nch] += y1 >> 4;
-				}
-				om = om >> 1;
+			/* Write two frames of output */
+			for (i = 0; i < 2 * out_nch; i++) {
+				*y = sat_int24(Q_SHIFT_RND(cd->out[i], 27, 23));
+				y++;
 			}
 		}
-
-		/* Write two frames of output */
-		for (i = 0; i < 2 * out_nch; i++) {
-			y = audio_stream_write_frag_s32(sink, idx_out++);
-			*y = sat_int24(Q_SHIFT_RND(cd->out[i], 27, 23));
-		}
+		remaining_frames -= f;
+		x = audio_stream_wrap(source, x);
+		y = audio_stream_wrap(sink, y);
 	}
 }
 #endif
 
 #if CONFIG_FORMAT_S32LE
-void tdfb_fir_s32(struct tdfb_comp_data *cd,
-		  const struct audio_stream *source,
+void tdfb_fir_s32(struct tdfb_comp_data *cd, const struct audio_stream *source,
 		  struct audio_stream *sink, int frames)
 {
-	struct sof_tdfb_config *cfg = cd->config;
-	struct fir_state_32x16 *f;
-	int32_t y0;
-	int32_t y1;
-	int32_t *x;
-	int32_t *y;
-	int lshift;
-	int rshift;
-	int is2;
-	int is;
-	int om;
+	int32_t *x = source->r_ptr;
+	int32_t *y = sink->w_ptr;
+	int fmax;
 	int i;
 	int j;
-	int k;
-	int in_nch = source->channels;
-	int out_nch = sink->channels;
-	int idx_in = 0;
-	int idx_out = 0;
+	int f;
+	const int in_nch = source->channels;
+	const int out_nch = sink->channels;
+	int remaining_frames = frames;
+	int emp_ch = 0;
 
-	for (j = 0; j < (frames >> 1); j++) {
-		/* Clear output mix*/
-		memset(cd->out, 0,  2 * out_nch * sizeof(int32_t));
+	while (remaining_frames) {
+		fmax = audio_stream_frames_without_wrap(source, x);
+		f = MIN(remaining_frames, fmax);
+		fmax = audio_stream_frames_without_wrap(sink, y);
+		f = MIN(f, fmax);
+		for (j = 0; j < f; j += 2) {
+			/* Clear output mix*/
+			memset(cd->out, 0, 2 * out_nch * sizeof(int32_t));
 
-		/* Read two frames from all input channels */
-		for (i = 0; i < 2 * in_nch; i++) {
-			x = audio_stream_read_frag_s32(source, idx_in++);
-			cd->in[i] = *x;
-		}
+			/* Read two frames from all input channels */
+			for (i = 0; i < 2 * in_nch; i++) {
+				cd->in[i] = *x;
+				tdfb_direction_copy_emphasis(cd, in_nch, &emp_ch, *x);
+				x++;
+			}
 
-		/* Run and mix all filters to their output channel */
-		for (i = 0; i < cfg->num_filters; i++) {
-			is = cd->input_channel_select[i];
-			is2 = is + in_nch;
-			om = cd->output_channel_mix[i];
-			/* Prepare FIR */
-			f = &cd->fir[i];
-			fir_hifiep_setup_circular(f);
-			fir_get_lrshifts(f, &lshift, &rshift);
-			/* Process two samples */
-			fir_32x16_2x_hifiep(f, cd->in[is], cd->in[is2],
-					    &y0, &y1, lshift, rshift);
-			/* Mix as Q5.27 */
-			for (k = 0; k < out_nch; k++) {
-				if (om & 1) {
-					cd->out[k] += y0 >> 4;
-					cd->out[k + out_nch] += y1 >> 4;
-				}
-				om = om >> 1;
+			/* Process */
+			tdfb_core(cd, in_nch, out_nch);
+
+			/* Write two frames of output. In Q5.27 to Q1.31 conversion
+			 * rounding is not applicable so just shift left by 4.
+			 */
+			for (i = 0; i < 2 * out_nch; i++) {
+				*y = sat_int32((int64_t)cd->out[i] << 4);
+				y++;
 			}
 		}
-
-		/* Write two frames of output. In Q5.27 to Q1.31 conversion
-		 * rounding is not applicable so just shift left by 4.
-		 */
-		for (i = 0; i < 2 * out_nch; i++) {
-			y = audio_stream_write_frag_s32(sink, idx_out++);
-			*y = sat_int32((int64_t)cd->out[i] << 4);
-		}
+		remaining_frames -= f;
+		x = audio_stream_wrap(source, x);
+		y = audio_stream_wrap(sink, y);
 	}
 }
 #endif
