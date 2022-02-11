3ac8fda4b7e1ec466c64e86959f3e02d07965d23
Rander Wang <rander.wang@intel.com>
2022-02-11 15:37:27 +0800

    ipc4: add gain module support
    Gain works as peak volume except that Gain doesn't
    report peak volume to host
    
    Signed-off-by: Rander Wang <rander.wang@intel.com>

--- src/audio/volume/volume.c
+++ src/audio/volume/volume.c
@@ -55,6 +55,10 @@ DECLARE_SOF_RT_UUID("pga", volume_uuid, 0xb77e677e, 0x5ff4, 0x4188,
 /* 8a171323-94a3-4e1d-afe9-fe5dbaa4c393 */
 DECLARE_SOF_RT_UUID("pga", volume_uuid, 0x8a171323, 0x94a3, 0x4e1d,
 		    0xaf, 0xe9, 0xfe, 0x5d, 0xba, 0xa4, 0xc3, 0x93);
+
+/* 61bca9a8-18d0-4a18-8e7b-2639219804b7 */
+DECLARE_SOF_RT_UUID("gain", gain_uuid, 0x61bca9a8, 0x18d0, 0x4a18,
+		    0x8e, 0x7b, 0x26, 0x39, 0x21, 0x98, 0x04, 0xb7);
 #endif
 
 DECLARE_TR_CTX(volume_tr, SOF_UUID(volume_uuid), LOG_LEVEL_INFO);
@@ -1288,3 +1292,38 @@ UT_STATIC void sys_comp_volume_init(void)
 }
 
 DECLARE_MODULE(sys_comp_volume_init);
+
+#if CONFIG_COMP_GAIN
+/** \brief gain component definition. */
+static const struct comp_driver comp_gain = {
+	.type	= SOF_COMP_VOLUME,
+	.uid	= SOF_RT_UUID(gain_uuid),
+	.tctx	= &volume_tr,
+	.ops	= {
+		.create		= volume_new,
+		.free		= volume_free,
+		.cmd		= volume_cmd,
+		.trigger	= volume_trigger,
+		.copy		= volume_copy,
+		.prepare	= volume_prepare,
+		.reset		= volume_reset,
+		.set_large_config = volume_set_large_config,
+		.get_large_config = volume_get_large_config,
+	},
+};
+
+static SHARED_DATA struct comp_driver_info comp_gain_info = {
+	.drv = &comp_gain,
+};
+
+/**
+ * \brief Initializes gain component.
+ */
+UT_STATIC void sys_comp_gain_init(void)
+{
+	comp_register(platform_shared_get(&comp_gain_info,
+					  sizeof(comp_gain_info)));
+}
+
+DECLARE_MODULE(sys_comp_gain_init);
+#endif
