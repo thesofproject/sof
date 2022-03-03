eba5396499e5973c20e96285543823c6e0e284e3
Chunxu Li <chunxu.li@mediatek.com>
2022-03-03 15:23:09 +0800

    platform: mtk: add platform driver support for mt8186
    Add support for dai, dma platform driver.
    
    Signed-off-by: Chunxu Li <chunxu.li@mediatek.com>

--- src/platform/mt8186/lib/dma.c
+++ src/platform/mt8186/lib/dma.c
@@ -12,7 +12,11 @@
 #include <sof/sof.h>
 #include <sof/spinlock.h>
 
+#include <mt8186-afe-regs.h>
+#include <mt8186-afe-common.h>
+
 extern const struct dma_ops dummy_dma_ops;
+extern const struct dma_ops memif_ops;
 
 static SHARED_DATA struct dma dma[PLATFORM_NUM_DMACS] = {
 {
@@ -23,6 +27,16 @@ static SHARED_DATA struct dma dma[PLATFORM_NUM_DMACS] = {
 		.channels	= 16,
 	},
 	.ops	= &dummy_dma_ops,
+},
+{
+	.plat_data = {
+		.id		= DMA_ID_AFE_MEMIF,
+		.dir		= DMA_DIR_MEM_TO_DEV | DMA_DIR_DEV_TO_MEM,
+		.devs		= DMA_DEV_AFE_MEMIF,
+		.base		= AFE_BASE_ADDR,
+		.channels	= MT8186_MEMIF_NUM,
+	},
+	.ops	= &memif_ops,
 },
 };
 
