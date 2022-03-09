thesofproject/sof

commit f1e6e1fdd3b7ede771d1babd4106301639015091

xtensa-build-all.sh: Code style correction
Combined case label tgl and tgl-h similarly as in xtensa-build-zephyr.sh
Corrected code formatting.

Signed-off-by: Adrian Warecki <adrianx.warecki@intel.com>

scripts/xtensa-build-all.sh
@@ -275,23 +275,14 @@ do
 			XTENSA_TOOLS_VERSION="RG-2017.8-linux"
 			HAVE_ROM='yes'
 			;;
-		tgl)
+		tgl|tgl-h)
 			PLATFORM="tgplp"
 			XTENSA_CORE="cavs2x_LX6HiFi3_2017_8"
 			HOST="xtensa-cnl-elf"
 			XTENSA_TOOLS_VERSION="RG-2017.8-linux"
 			HAVE_ROM='yes'
 			# default key for TGL
-	PLATFORM_PRIVATE_KEY="-D${SIGNING_TOOL}_PRIVATE_KEY=$SOF_TOP/keys/otc_private_key_3k.pem"
-			;;
-		tgl-h)
-			PLATFORM="tgph"
-			XTENSA_CORE="cavs2x_LX6HiFi3_2017_8"
-			HOST="xtensa-cnl-elf"
-			XTENSA_TOOLS_VERSION="RG-2017.8-linux"
-			HAVE_ROM='yes'
-			# default key for TGL
-	PLATFORM_PRIVATE_KEY="-D${SIGNING_TOOL}_PRIVATE_KEY=$SOF_TOP/keys/otc_private_key_3k.pem"
+			PLATFORM_PRIVATE_KEY="-D${SIGNING_TOOL}_PRIVATE_KEY=$SOF_TOP/keys/otc_private_key_3k.pem"
 			;;
 		jsl)
 			PLATFORM="jasperlake"
@@ -330,12 +321,12 @@ do
 			HOST="xtensa-rn-elf"
 			XTENSA_TOOLS_VERSION="RF-2016.4-linux"
 			;;
-                mt8186)
-                        PLATFORM="mt8186"
-                        XTENSA_CORE="hifi5_7stg_I64D128"
-                        HOST="xtensa-mt8186-elf"
-                        XTENSA_TOOLS_VERSION="RI-2020.5-linux"
-                        ;;
+		mt8186)
+			PLATFORM="mt8186"
+			XTENSA_CORE="hifi5_7stg_I64D128"
+			HOST="xtensa-mt8186-elf"
+			XTENSA_TOOLS_VERSION="RI-2020.5-linux"
+			;;
 		mt8195)
 			PLATFORM="mt8195"
 			XTENSA_CORE="hifi4_8195_PROD"

commit 137039fdb70ffbe0ef755530eaa3f0608c7a1f67

xtensa-build-all.sh: Use appropriate defconfig for tgl-h platform
Recent combination of tgl and tgl-h building resulted in use of an invalid
defconfig file for tgl-h platform. This commit reverts that change.

Fixes: f1e6e1fdd3b7ede771d1babd4106301639015091

Signed-off-by: Adrian Warecki <adrianx.warecki@intel.com>

scripts/xtensa-build-all.sh
@@ -275,8 +275,17 @@ do
 			XTENSA_TOOLS_VERSION="RG-2017.8-linux"
 			HAVE_ROM='yes'
 			;;
-		tgl|tgl-h)
+		tgl)
 			PLATFORM="tgplp"
+			XTENSA_CORE="cavs2x_LX6HiFi3_2017_8"
+			HOST="xtensa-cnl-elf"
+			XTENSA_TOOLS_VERSION="RG-2017.8-linux"
+			HAVE_ROM='yes'
+			# default key for TGL
+			PLATFORM_PRIVATE_KEY="-D${SIGNING_TOOL}_PRIVATE_KEY=$SOF_TOP/keys/otc_private_key_3k.pem"
+			;;
+		tgl-h)
+			PLATFORM="tgph"
 			XTENSA_CORE="cavs2x_LX6HiFi3_2017_8"
 			HOST="xtensa-cnl-elf"
 			XTENSA_TOOLS_VERSION="RG-2017.8-linux"