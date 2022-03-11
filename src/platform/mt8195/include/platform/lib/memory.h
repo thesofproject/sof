1588fc7ca64604dc143fdddb408cdb2d142cd74f
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2022-03-11 16:21:12 +0100

    cache: add cache_to_uncache_init() to all platforms
    cache_to_uncache_init() is needed to convert addresses from cached to
    uncached aliases in a preprocessor-friendly way. Add it to all
    platforms.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/platform/mt8195/include/platform/lib/memory.h
+++ src/platform/mt8195/include/platform/lib/memory.h
@@ -185,6 +185,7 @@ static inline void *platform_shared_get(void *ptr, int bytes)
 
 #define uncache_to_cache(address) address
 #define cache_to_uncache(address) address
+#define cache_to_uncache_init(address) address
 #define is_uncached(address) 0
 
 /**
