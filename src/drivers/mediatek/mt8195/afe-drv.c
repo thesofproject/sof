thesofproject/sof

commit 1b7f8ed2a6fa0f0944b70a25c9bf5dcff64c6f9c

drivers: mtk: add afe driver for mtk mt8195
Add afe driver for memif/sinegen.
Add afe-dai.c/afe-drv.c/afe-memif.c for afe common driver

AFE : Audio Front End

The audio front-end essentially consists of voice and audio data paths.

frontend (memif) :
  memory interface,
  UL (uplink for capture),
  DL(downlink for playback)
backend:
  TDM In,
  TMD out,
  DMIC,
  GASRC, etc
interconn:
  inter-connection,
  connect frontends
  backends as DSP path

Note:
TEST_SGEN macro define is just for test

Signed-off-by: YC Hung <yc.hung@mediatek.com>
Signed-off-by: Allen-KH Cheng <allen-kh.cheng@mediatek.com>

src/drivers/mediatek/mt8195/afe-drv.c
@@ -1,0 +1,414 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2021 Mediatek
+//
+// Author: Bo Pan <bo.pan@mediatek.com>
+//         YC Hung <yc.hung@mediatek.com>
+
+#include <sof/common.h>
+#include <sof/lib/io.h>
+#include <sof/lib/alloc.h>
+#include <ipc/stream.h>
+#include <ipc/topology.h>
+#include <sof/lib/uuid.h>
+#include <errno.h>
+#include <sof/drivers/afe-drv.h>
+#include <sof/drivers/afe-memif.h>
+
+/*#define AFE_DRV_LOG*/
+static struct mtk_base_afe mtk_afe;
+
+#ifndef AFE_DRV_LOG
+#undef printf
+#define printf(format, ...)
+#endif
+
+DECLARE_SOF_UUID("afedrv", afedrv_uuid, 0x4e8f16d1, 0xe935, 0x41f4,
+		 0xb9, 0x9e, 0x42, 0xc5, 0x7e, 0x74, 0x57, 0x84);
+
+DECLARE_TR_CTX(afedrv_tr, SOF_UUID(afedrv_uuid), LOG_LEVEL_INFO);
+
+static inline void afe_reg_read(struct mtk_base_afe *afe, uint32_t reg, uint32_t *value)
+{
+	*value = io_reg_read((uint32_t)((char *)afe->base + reg));
+	tr_dbg(&afedrv_tr, "r_reg:0x%x, value:0x%x\n", reg, *value);
+}
+
+static inline void afe_reg_write(struct mtk_base_afe *afe, uint32_t reg, uint32_t value)
+{
+	io_reg_write((uint32_t)((char *)afe->base + reg), value);
+	tr_dbg(&afedrv_tr, "w_reg:0x%x, value:0x%x\n", reg, value);
+}
+
+static inline void afe_reg_update_bits(struct mtk_base_afe *afe, uint32_t reg, uint32_t mask,
+				       uint32_t value)
+{
+	io_reg_update_bits((uint32_t)((char *)afe->base + reg), mask, value);
+	tr_dbg(&afedrv_tr, "u_reg:0x%x, value:0x%x\n", reg, value);
+}
+
+static int afe_memif_set_channel(struct mtk_base_afe *afe, int id, unsigned int channel)
+{
+	struct mtk_base_afe_memif *memif = &afe->memif[id];
+	unsigned int mono;
+
+	if (memif->data->mono_shift < 0)
+		return 0;
+
+	if (memif->data->ch_num_reg >= 0) {
+		afe_reg_update_bits(afe, memif->data->ch_num_reg,
+				    memif->data->ch_num_maskbit << memif->data->ch_num_shift,
+				    channel << memif->data->ch_num_shift);
+	}
+
+	if (memif->data->quad_ch_mask) {
+		unsigned int quad_ch = channel == 4;
+
+		afe_reg_update_bits(afe, memif->data->quad_ch_reg,
+				    memif->data->quad_ch_mask << memif->data->quad_ch_shift,
+				    quad_ch << memif->data->quad_ch_shift);
+	}
+
+	mono = (bool)memif->data->mono_invert ^ (channel == 1);
+	afe_reg_update_bits(afe, memif->data->mono_reg, 1 << memif->data->mono_shift,
+			    mono << memif->data->mono_shift);
+	return 0;
+}
+
+static int afe_memif_set_rate(struct mtk_base_afe *afe, int id, unsigned int rate)
+{
+	struct mtk_base_afe_memif *memif = &afe->memif[id];
+	int fs;
+
+	fs = afe->afe_fs(rate, memif->data->id);
+	if (fs < 0)
+		return -EINVAL;
+
+	afe_reg_update_bits(afe, memif->data->fs_reg,
+			    memif->data->fs_maskbit << memif->data->fs_shift,
+			    fs << memif->data->fs_shift);
+
+	return 0;
+}
+
+static int afe_memif_set_format(struct mtk_base_afe *afe, int id, unsigned int format)
+{
+	struct mtk_base_afe_memif *memif = &afe->memif[id];
+	int hd_audio;
+	int memif_32bit_supported = afe->memif_32bit_supported;
+
+	/* set hd mode */
+	switch (format) {
+	case SOF_IPC_FRAME_S16_LE:
+		hd_audio = 0;
+		break;
+	case SOF_IPC_FRAME_S32_LE:
+	case SOF_IPC_FRAME_S24_4LE:
+		if (memif_32bit_supported)
+			hd_audio = 2;
+		else
+			hd_audio = 1;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	afe_reg_update_bits(afe, memif->data->hd_reg, 0x3 << memif->data->hd_shift,
+			    hd_audio << memif->data->hd_shift);
+	return 0;
+}
+
+int afe_memif_set_params(struct mtk_base_afe *afe, int id, unsigned int channel, unsigned int rate,
+			 unsigned int format)
+{
+	int ret;
+
+	ret = afe_memif_set_channel(afe, id, channel);
+	if (ret < 0)
+		return ret;
+	ret = afe_memif_set_rate(afe, id, rate);
+	if (ret < 0)
+		return ret;
+	ret = afe_memif_set_format(afe, id, format);
+	if (ret < 0)
+		return ret;
+	/* TODO IRQ direction, irq format setting ? */
+
+	return ret;
+}
+
+int afe_memif_set_addr(struct mtk_base_afe *afe, int id, unsigned int dma_addr,
+		       unsigned int dma_bytes)
+{
+	struct mtk_base_afe_memif *memif = &afe->memif[id];
+	int msb_at_bit33 = 0; /* for dsp side only support 32bit address */
+	unsigned int phys_buf_addr;
+	unsigned int phys_buf_addr_upper_32 = 0; /* for dsp side only support 32bit address */
+
+	memif->dma_addr = dma_addr;
+
+	/* convert adsp address to afe address */
+	if (afe->adsp2afe_addr)
+		dma_addr = afe->adsp2afe_addr(dma_addr);
+
+	phys_buf_addr = dma_addr;
+
+	memif->afe_addr = phys_buf_addr;
+	memif->buffer_size = dma_bytes;
+	tr_dbg(&afedrv_tr, "dma_addr:%u, size:%u\n", dma_addr, dma_bytes);
+	/* start */
+	afe_reg_write(afe, memif->data->reg_ofs_base, phys_buf_addr);
+	/* end */
+	if (memif->data->reg_ofs_end)
+		afe_reg_write(afe, memif->data->reg_ofs_end, phys_buf_addr + dma_bytes - 1);
+	else
+		afe_reg_write(afe, memif->data->reg_ofs_base + afe->base_end_offset,
+			      phys_buf_addr + dma_bytes - 1);
+
+	/* set start, end, upper 32 bits */
+	if (memif->data->reg_ofs_base_msb) {
+		afe_reg_write(afe, memif->data->reg_ofs_base_msb, phys_buf_addr_upper_32);
+		afe_reg_write(afe, memif->data->reg_ofs_end_msb, phys_buf_addr_upper_32);
+	}
+
+	/* set MSB to 33-bit */
+	if (memif->data->msb_reg >= 0)
+		afe_reg_update_bits(afe, memif->data->msb_reg, 1 << memif->data->msb_shift,
+				    msb_at_bit33 << memif->data->msb_shift);
+
+	/* set MSB to 33-bit, for memif end address */
+	if (memif->data->msb2_reg >= 0)
+		afe_reg_update_bits(afe, memif->data->msb2_reg, 1 << memif->data->msb2_shift,
+				    msb_at_bit33 << memif->data->msb2_shift);
+
+	return 0;
+}
+
+int afe_memif_set_enable(struct mtk_base_afe *afe, int id, int enable)
+{
+	const struct mtk_base_memif_data *memif_data = afe->memif[id].data;
+
+	if (memif_data->enable_shift < 0)
+		return 0;
+
+	/* enable agent */
+	/* TODO: enable/disable should in different sequence? */
+	if (memif_data->agent_disable_reg > 0) {
+		afe_reg_update_bits(afe, memif_data->agent_disable_reg,
+				    1 << memif_data->agent_disable_shift,
+				    (!enable) << memif_data->agent_disable_shift);
+	}
+
+	afe_reg_update_bits(afe, memif_data->enable_reg, 1 << memif_data->enable_shift,
+			    enable << memif_data->enable_shift);
+
+	return 0;
+}
+
+int afe_memif_get_direction(struct mtk_base_afe *afe, int id)
+{
+	const struct mtk_base_memif_data *memif_data = afe->memif[id].data;
+
+	if (memif_data->id >= 0 && memif_data->id < afe->memif_dl_num)
+		return MEM_DIR_PLAYBACK;
+	return MEM_DIR_CAPTURE;
+}
+
+unsigned int afe_memif_get_cur_position(struct mtk_base_afe *afe, int id)
+{
+	const struct mtk_base_memif_data *memif_data = afe->memif[id].data;
+	unsigned int hw_ptr;
+
+	if (memif_data->reg_ofs_cur < 0)
+		return 0;
+	afe_reg_read(afe, memif_data->reg_ofs_cur, &hw_ptr);
+
+	/* convert afe address to adsp address */
+	if (afe->afe2adsp_addr)
+		hw_ptr = afe->afe2adsp_addr(hw_ptr);
+	return hw_ptr;
+}
+
+int afe_dai_set_config(struct mtk_base_afe *afe, int id, unsigned int channel, unsigned int rate,
+		       unsigned int format)
+{
+	struct mtk_base_afe_dai *dai = &afe->dais[id];
+
+	/* TODO 1. if need use dai->id to search target dai */
+	/* TODO 1. if need a status to control the dai status */
+
+	if (id >= afe->dais_size)
+		return -EINVAL;
+
+	tr_info(&afedrv_tr, "afe_dai_set_config, id:%d\n", id);
+
+	dai = &afe->dais[id];
+	dai->channel = channel;
+	dai->format = format;
+	dai->rate = rate;
+
+	tr_info(&afedrv_tr, "dai:%d set: format:%d, rate:%d, channel:%d\n", id, format, rate,
+		channel);
+
+	return 0;
+}
+
+int afe_dai_get_config(struct mtk_base_afe *afe, int id, unsigned int *channel, unsigned int *rate,
+		       unsigned int *format)
+{
+	struct mtk_base_afe_dai *dai = &afe->dais[id];
+
+	/* TODO 1. if need use dai->id to search target dai */
+	/* TODO 1. if need a status to control the dai status */
+	tr_info(&afedrv_tr, "afe_dai_get_config, id:%d\n", id);
+
+	if (id >= afe->dais_size || id < 0) {
+		tr_err(&afedrv_tr, "afe_dai_get_config , invalid id:%d\n", id);
+		return -EINVAL;
+	}
+	dai = &afe->dais[id];
+
+	*channel = dai->channel;
+	*rate = dai->rate;
+	*format = dai->format;
+
+	tr_info(&afedrv_tr, "dai:%d get: format:%d, rate:%d, channel:%d\n", id, *format, *rate,
+		*channel);
+
+	return 0;
+}
+
+/* TODO, IRQ common register name need use config? */
+int afe_irq_get_status(struct mtk_base_afe *afe, int id)
+{
+	return 0;
+}
+
+int afe_irq_clear(struct mtk_base_afe *afe, int id)
+{
+	return 0;
+}
+
+int afe_irq_config(struct mtk_base_afe *afe, int id, unsigned int rate, unsigned int period)
+{
+	struct mtk_base_afe_irq *irq = &afe->irqs[id];
+	unsigned int fs;
+
+	afe_reg_update_bits(afe, irq->irq_data->irq_cnt_reg,
+			    irq->irq_data->irq_cnt_maskbit << irq->irq_data->irq_cnt_shift,
+			    period << irq->irq_data->irq_cnt_shift);
+
+	/* set irq fs */
+	fs = afe->irq_fs(rate);
+
+	if (fs < 0)
+		return -EINVAL;
+
+	afe_reg_update_bits(afe, irq->irq_data->irq_fs_reg,
+			    irq->irq_data->irq_fs_maskbit << irq->irq_data->irq_fs_shift,
+			    fs << irq->irq_data->irq_fs_shift);
+	return 0;
+}
+
+/* TODO, for dma based scheduler*/
+int afe_irq_enable(struct mtk_base_afe *afe, int id)
+{
+	return 0;
+}
+
+int afe_irq_disable(struct mtk_base_afe *afe, int id)
+{
+	return 0;
+}
+
+struct mtk_base_afe *afe_get(void)
+{
+	return &mtk_afe;
+}
+
+int afe_probe(struct mtk_base_afe *afe)
+{
+	int i;
+	struct mtk_base_afe_platform *platform = &mtk_afe_platform;
+
+	/* mtk afe already init done */
+	if (afe->ref_count > 0) {
+		afe->ref_count++;
+		return 0;
+	}
+
+	afe->platform_priv = platform;
+	afe->base = platform->base_addr;
+	afe->memif_32bit_supported = platform->memif_32bit_supported;
+	afe->memif_dl_num = platform->memif_dl_num;
+
+	afe->base_end_offset = platform->base_end_offset;
+	afe->adsp2afe_addr = platform->adsp2afe_addr;
+	afe->afe2adsp_addr = platform->afe2adsp_addr;
+	afe->afe_fs = platform->afe_fs; /* must be */
+	afe->irq_fs = platform->irq_fs;
+	if (!afe->afe_fs)
+		return -EINVAL;
+	tr_dbg(&afedrv_tr, "afe_base:0x%x\n", afe->base);
+	/* TODO how to get the memif number, how to sync with dmac lib */
+	afe->memifs_size = platform->memif_size;
+	afe->memif = rzalloc(SOF_MEM_ZONE_RUNTIME_SHARED, 0, SOF_MEM_CAPS_RAM,
+			     sizeof(struct mtk_base_afe_memif) * afe->memifs_size);
+	if (!afe->memif)
+		return -ENOMEM;
+
+	for (i = 0; i < afe->memifs_size; i++)
+		afe->memif[i].data = &platform->memif_datas[i];
+
+	/* TODO how to get the dai number, how to sync with dai lib*/
+	afe->dais_size = platform->dais_size;
+	afe->dais = rzalloc(SOF_MEM_ZONE_RUNTIME_SHARED, 0, SOF_MEM_CAPS_RAM,
+			    sizeof(struct mtk_base_afe_dai) * afe->dais_size);
+	if (!afe->dais)
+		goto err_alloc_memif;
+
+	/* TODO how to get the irq number */
+	afe->irqs_size = platform->irqs_size;
+	afe->irqs = rzalloc(SOF_MEM_ZONE_RUNTIME_SHARED, 0, SOF_MEM_CAPS_RAM,
+			    sizeof(struct mtk_base_afe_irq) * afe->irqs_size);
+	if (!afe->irqs)
+		goto err_alloc_dais;
+
+	for (i = 0; i < afe->irqs_size; i++)
+		afe->irqs[i].irq_data = &platform->irq_datas[i];
+
+	afe->ref_count++;
+
+	return 0;
+err_alloc_dais:
+	rfree(afe->dais);
+err_alloc_memif:
+	rfree(afe->memif);
+
+	return -ENOMEM;
+}
+
+int afe_remove(struct mtk_base_afe *afe)
+{
+	afe->ref_count--;
+
+	if (afe->ref_count > 0)
+		return 0;
+
+	if (afe->ref_count < 0) {
+		afe->ref_count = 0;
+		tr_dbg(&afedrv_tr, "afe ref_count < 0, :%d\n", afe->ref_count);
+		return 0;
+	}
+
+	rfree(afe->memif);
+	afe->memif = NULL;
+
+	rfree(afe->dais);
+	afe->dais = NULL;
+
+	rfree(afe->irqs);
+	afe->irqs = NULL;
+
+	return 0;
+}

src/drivers/mediatek/mt8195/afe-dai.c
@@ -1,0 +1,116 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2021 Mediatek
+//
+// Author: Bo Pan <bo.pan@mediatek.com>
+//         YC Hung <yc.hung@mediatek.com>
+
+#include <sof/audio/component.h>
+#include <sof/drivers/afe-drv.h>
+#include <sof/drivers/timer.h>
+#include <sof/lib/alloc.h>
+#include <sof/lib/dai.h>
+#include <sof/lib/dma.h>
+#include <sof/lib/io.h>
+#include <sof/lib/notifier.h>
+#include <sof/lib/uuid.h>
+#include <sof/math/numbers.h>
+#include <sof/platform.h>
+#include <ipc/dai.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* 30290c76-6a05-4784-8464-c21f09cee87e */
+DECLARE_SOF_UUID("afe-dai", afe_dai_uuid, 0x30290c76, 0x6a05, 0x4784,
+		 0x84, 0x64, 0xc2, 0x1f, 0x09, 0xce, 0xe8, 0x7e);
+
+DECLARE_TR_CTX(afe_dai_tr, SOF_UUID(afe_dai_uuid), LOG_LEVEL_INFO);
+
+static int afe_dai_drv_trigger(struct dai *dai, int cmd, int direction)
+{
+	return 0;
+}
+
+static int afe_dai_drv_set_config(struct dai *dai, struct ipc_config_dai *common_config,
+				  void *spec_config)
+{
+	struct sof_ipc_dai_config *config = spec_config;
+	struct mtk_base_afe *afe = dai_get_drvdata(dai);
+
+	afe_dai_set_config(afe,
+			   dai->index,
+			   config->afe.dai_channels,
+			   config->afe.dai_rate,
+			   config->afe.dai_format);
+	return 0;
+}
+
+/* get HDA hw params */
+static int afe_dai_drv_get_hw_params(struct dai *dai, struct sof_ipc_stream_params *params, int dir)
+{
+	struct mtk_base_afe *afe = dai_get_drvdata(dai);
+	unsigned int channel, rate, format;
+
+	afe_dai_get_config(afe, dai->index, &channel, &rate, &format);
+	params->rate = rate;
+	params->channels = channel;
+	params->buffer_fmt = format;
+	params->frame_fmt = format;
+
+	return 0;
+}
+
+static int afe_dai_drv_probe(struct dai *dai)
+{
+	struct mtk_base_afe *afe = afe_get();
+
+	dai_info(dai, "afe_dai_probe()");
+
+	if (dai_get_drvdata(dai))
+		return -EEXIST;
+
+	dai_set_drvdata(dai, afe);
+
+	return 0;
+}
+
+static int afe_dai_drv_remove(struct dai *dai)
+{
+	dai_info(dai, "afe_dai_remove()");
+
+	return 0;
+}
+
+static int afe_dai_drv_dummy(struct dai *dai)
+{
+	return 0;
+}
+
+static int afe_dai_drv_get_handshake(struct dai *dai, int direction, int stream_id)
+{
+	return (int)dai->plat_data.fifo[0].handshake;
+}
+
+static int afe_dai_drv_get_fifo(struct dai *dai, int direction, int stream_id)
+{
+	return 0;
+}
+
+const struct dai_driver afe_dai_driver = {
+	.type = SOF_DAI_MEDIATEK_AFE,
+	.uid = SOF_UUID(afe_dai_uuid),
+	.tctx = &afe_dai_tr,
+	.dma_dev = DMA_DEV_AFE_MEMIF,
+	.ops = {
+		.trigger		= afe_dai_drv_trigger,
+		.set_config		= afe_dai_drv_set_config,
+		.pm_context_store	= afe_dai_drv_dummy,
+		.pm_context_restore	= afe_dai_drv_dummy,
+		.get_hw_params		= afe_dai_drv_get_hw_params,
+		.get_handshake		= afe_dai_drv_get_handshake,
+		.get_fifo		= afe_dai_drv_get_fifo,
+		.probe			= afe_dai_drv_probe,
+		.remove			= afe_dai_drv_remove,
+	},
+};

src/include/ipc/dai-mediatek.h
@@ -1,0 +1,25 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2021 Mediatek
+//
+// Author: Bo Pan <bo.pan@mediatek.com>
+
+#ifndef __IPC_DAI_MEDIATEK_H__
+#define __IPC_DAI_MEDIATEK_H__
+
+#include <ipc/header.h>
+#include <stdint.h>
+
+/* AFE Configuration Request - SOF_DAI_MEDIATEK_AFE */
+struct sof_ipc_dai_afe_params {
+	uint32_t reserved0;
+
+	uint32_t dai_channels;
+	uint32_t dai_rate;
+	uint32_t dai_format;
+	uint32_t stream_id;
+	uint32_t reserved[4]; /* reserve for future */
+} __attribute__((packed));
+
+#endif /* __IPC_DAI_MEDIATEK_H__ */
+

src/include/ipc/dai.h
@@ -18,6 +18,7 @@
 
 #include <ipc/dai-intel.h>
 #include <ipc/dai-imx.h>
+#include <ipc/dai-mediatek.h>
 #include <ipc/header.h>
 #include <stdint.h>
 
@@ -70,7 +71,8 @@ enum sof_ipc_dai_type {
 	SOF_DAI_IMX_ESAI,               /**< i.MX ESAI */
 	SOF_DAI_AMD_BT,			/**< Amd BT */
 	SOF_DAI_AMD_SP,			/**< Amd SP */
-	SOF_DAI_AMD_DMIC		/**< Amd DMIC */
+	SOF_DAI_AMD_DMIC,		/**< Amd DMIC */
+	SOF_DAI_MEDIATEK_AFE            /**< Mtk AFE */
 };
 
 /* general purpose DAI configuration */
@@ -95,6 +97,7 @@ struct sof_ipc_dai_config {
 		struct sof_ipc_dai_alh_params alh;
 		struct sof_ipc_dai_esai_params esai;
 		struct sof_ipc_dai_sai_params sai;
+		struct sof_ipc_dai_afe_params afe;
 	};
 } __attribute__((packed, aligned(4)));
 

src/include/sof/drivers/afe-dai.h
@@ -1,0 +1,29 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2021 Mediatek
+//
+// Author: Bo Pan <bo.pan@mediatek.com>
+//         YC Hung <yc.hung@mediatek.com>
+
+#ifndef __SOF_DRIVERS_AFE_DAI_H__
+#define __SOF_DRIVERS_AFE_DAI_H__
+
+#include <sof/bit.h>
+#include <sof/common.h>
+#include <sof/lib/dma.h>
+#include <sof/trace/trace.h>
+#include <user/trace.h>
+#include <stdint.h>
+
+#define AFE_HS_GET_DAI(hs) (((hs) & MASK(7, 0)) >> 0)
+#define AFE_HS_SET_DAI(dai) SET_BITS(7, 0, dai)
+#define AFE_HS_GET_IRQ(hs) (((hs) & MASK(15, 8)) >> 8)
+#define AFE_HS_SET_IRQ(irq) SET_BITS(15, 8, irq)
+#define AFE_HS_GET_CHAN(hs) (((hs) & MASK(23, 16)) >> 16)
+#define AFE_HS_SET_CHAN(chan) SET_BITS(23, 16, chan)
+#define AFE_HANDSHAKE(dai, irq, channel)\
+	(AFE_HS_SET_DAI(dai) | AFE_HS_SET_CHAN(channel) | AFE_HS_SET_IRQ(irq))
+
+extern const struct dai_driver afe_dai_driver;
+#endif /* __SOF_DRIVERS_AFE_MEMIF_H__ */
+

src/include/sof/drivers/afe-drv.h
@@ -1,0 +1,169 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2021 Mediatek
+//
+// Author: Bo Pan <bo.pan@mediatek.com>
+//         YC Hung <yc.hung@mediatek.com>
+
+#ifndef __SOF_DRIVERS_AFE_DRV_H__
+#define __SOF_DRIVERS_AFE_DRV_H__
+
+struct mtk_base_memif_data {
+	int id;
+	const char *name;
+	int reg_ofs_base;
+	int reg_ofs_cur;
+	int reg_ofs_end;
+	int reg_ofs_base_msb;
+	int reg_ofs_cur_msb;
+	int reg_ofs_end_msb;
+	int fs_reg;
+	int fs_shift;
+	int fs_maskbit;
+	int mono_reg;
+	int mono_shift;
+	int mono_invert;
+	int quad_ch_reg;
+	int quad_ch_mask;
+	int quad_ch_shift;
+	int enable_reg;
+	int enable_shift;
+	int hd_reg;
+	int hd_shift;
+	int hd_align_reg;
+	int hd_align_mshift;
+	int msb_reg;
+	int msb_shift;
+	int msb2_reg;
+	int msb2_shift;
+	int agent_disable_reg;
+	int agent_disable_shift;
+	int ch_num_reg;
+	int ch_num_shift;
+	int ch_num_maskbit;
+	/* playback memif only */
+	int pbuf_reg;
+	int pbuf_mask;
+	int pbuf_shift;
+	int minlen_reg;
+	int minlen_mask;
+	int minlen_shift;
+};
+
+struct mtk_base_irq_data {
+	int id;
+	int irq_cnt_reg;
+	int irq_cnt_shift;
+	int irq_cnt_maskbit;
+	int irq_fs_reg;
+	int irq_fs_shift;
+	int irq_fs_maskbit;
+	int irq_en_reg;
+	int irq_en_shift;
+	int irq_clr_reg;
+	int irq_clr_shift;
+	int irq_ap_en_reg;
+	int irq_ap_en_shift;
+	int irq_scp_en_reg;
+	int irq_scp_en_shift;
+};
+
+struct mtk_base_afe_memif {
+	unsigned int dma_addr;
+	unsigned int afe_addr;
+	unsigned int buffer_size;
+
+	const struct mtk_base_memif_data *data;
+	int irq_usage;
+};
+
+struct mtk_base_afe_dai {
+	int id;
+	unsigned int channel;
+	unsigned int rate;
+	unsigned int format;
+	/* other? */
+};
+
+struct mtk_base_afe_irq {
+	const struct mtk_base_irq_data *irq_data;
+	int mask;
+	int irq_occupyed;
+};
+
+struct mtk_base_afe {
+	int ref_count;
+	unsigned int base;
+
+	struct mtk_base_afe_memif *memif;
+	int memifs_size;
+	int memif_32bit_supported;
+	int memif_dl_num;
+
+	struct mtk_base_afe_irq *irqs;
+	int irqs_size;
+
+	struct mtk_base_afe_dai *dais;
+	int dais_size;
+
+	unsigned int (*afe2adsp_addr)(unsigned int addr);
+	unsigned int (*adsp2afe_addr)(unsigned int addr);
+	unsigned int (*afe_fs)(unsigned int rate, int aud_blk);
+	unsigned int (*irq_fs)(unsigned int rate);
+
+	int base_end_offset;
+
+	void *platform_priv;
+};
+
+/* platform information */
+struct mtk_base_afe_platform {
+	unsigned int base_addr;
+	const struct mtk_base_memif_data *memif_datas;
+	int memif_size;
+	int memif_32bit_supported;
+	int memif_dl_num;
+
+	struct mtk_base_irq_data *irq_datas;
+	int irqs_size;
+	int dais_size;
+
+	int base_end_offset;
+
+	/* misc */
+	unsigned int (*afe2adsp_addr)(unsigned int addr);
+	unsigned int (*adsp2afe_addr)(unsigned int addr);
+	unsigned int (*afe_fs)(unsigned int rate, int aud_blk);
+	unsigned int (*irq_fs)(unsigned int rate);
+};
+
+extern struct mtk_base_afe_platform mtk_afe_platform;
+
+struct mtk_base_afe *afe_get(void);
+int afe_probe(struct mtk_base_afe *afe);
+int afe_remove(struct mtk_base_afe *afe);
+
+/* dai operation */
+int afe_dai_get_config(struct mtk_base_afe *afe, int id, unsigned int *channel, unsigned int *rate,
+		       unsigned int *format);
+int afe_dai_set_config(struct mtk_base_afe *afe, int id, unsigned int channel, unsigned int rate,
+		       unsigned int format);
+
+/* memif operation */
+int afe_memif_set_params(struct mtk_base_afe *afe, int id, unsigned int channel, unsigned int rate,
+			 unsigned int format);
+int afe_memif_set_addr(struct mtk_base_afe *afe, int id, unsigned int dma_addr,
+		       unsigned int dma_bytes);
+int afe_memif_set_enable(struct mtk_base_afe *afe, int id, int enable);
+unsigned int afe_memif_get_cur_position(struct mtk_base_afe *afe, int id);
+int afe_memif_get_direction(struct mtk_base_afe *afe, int id);
+
+/* irq opeartion */
+int afe_irq_get_status(struct mtk_base_afe *afe, int id);
+int afe_irq_clear(struct mtk_base_afe *afe, int id);
+int afe_irq_config(struct mtk_base_afe *afe, int id, unsigned int rate, unsigned int period);
+int afe_irq_enable(struct mtk_base_afe *afe, int id);
+int afe_irq_disable(struct mtk_base_afe *afe, int id);
+
+#endif /* __SOF_DRIVERS_AFE_DRV_H__ */
+

src/include/sof/drivers/afe-memif.h
@@ -1,0 +1,24 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2021 Mediatek
+//
+// Author: Bo Pan <bo.pan@mediatek.com>
+//         YC Hung <yc.hung@mediatek.com>
+
+#ifndef __SOF_DRIVERS_AFE_MEMIF_H__
+#define __SOF_DRIVERS_AFE_MEMIF_H__
+
+#include <sof/bit.h>
+#include <sof/common.h>
+#include <sof/lib/dma.h>
+#include <sof/trace/trace.h>
+#include <user/trace.h>
+#include <stdint.h>
+
+enum {
+	MEM_DIR_CAPTURE = 0,
+	MEM_DIR_PLAYBACK,
+};
+
+#endif /* __SOF_DRIVERS_AFE_MEMIF_H__ */
+

commit 915ecbdff230f43213b7c3e0a0f580bb3148ffbe

mediatek: mt8195: afe-drv: fix fs cppcheck warning
Define fs as signed integer in afe_irq_config to fix cppcheck
warning.

Fixes: 1b7f8ed2a6(drivers: mtk: add afe driver for mtk mt8195)
Signed-off-by: YC Hung <yc.hung@mediatek.com>

src/drivers/mediatek/mt8195/afe-drv.c
@@ -292,7 +292,7 @@ int afe_irq_clear(struct mtk_base_afe *afe, int id)
 int afe_irq_config(struct mtk_base_afe *afe, int id, unsigned int rate, unsigned int period)
 {
 	struct mtk_base_afe_irq *irq = &afe->irqs[id];
-	unsigned int fs;
+	int fs;
 
 	afe_reg_update_bits(afe, irq->irq_data->irq_cnt_reg,
 			    irq->irq_data->irq_cnt_maskbit << irq->irq_data->irq_cnt_shift,