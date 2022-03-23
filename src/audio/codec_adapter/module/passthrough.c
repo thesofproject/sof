5383b7d2253fe291ebee5653ecc6d8e3fd59aab3
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2022-03-23 16:15:13 -0700

    codec_adapter: rename the codec folder to module
    and fix all the includes accordingly. No functional changes.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- src/audio/codec_adapter/module/passthrough.c
+++ src/audio/codec_adapter/module/passthrough.c
@@ -6,7 +6,7 @@
 //
 // Passthrough codec implementation to demonstrate Codec Adapter API
 
-#include <sof/audio/codec_adapter/codec/generic.h>
+#include <sof/audio/codec_adapter/module/generic.h>
 
 /* 376b5e44-9c82-4ec2-bc83-10ea101afa8f */
 DECLARE_SOF_RT_UUID("passthrough_codec", passthrough_uuid, 0x376b5e44, 0x9c82, 0x4ec2,
