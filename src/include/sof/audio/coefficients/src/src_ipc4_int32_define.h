353244d146dc8fc4348858e7eccc54a885f8af30
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2022-03-30 18:56:46 +0300

    Audio: SRC: Add IPC4 full conversions matrix coefficients
    This patch contains the new coefficients. All were created with
    a script in tools/tune/src.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

---
+++ src/include/sof/audio/coefficients/src/src_ipc4_int32_define.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ *
+ */
+
+#ifndef __SOF_AUDIO_COEFFICIENTS_SRC_SRC_IPC4_INT32_DEFINE_H__
+#define __SOF_AUDIO_COEFFICIENTS_SRC_SRC_IPC4_INT32_DEFINE_H__
+
+/* SRC constants */
+#define MAX_FIR_DELAY_SIZE 706
+#define MAX_OUT_DELAY_SIZE 900
+#define MAX_BLK_IN 80
+#define MAX_BLK_OUT 40
+#define NUM_IN_FS 16
+#define NUM_OUT_FS 10
+#define STAGE1_TIMES_MAX 32
+#define STAGE2_TIMES_MAX 32
+#define STAGE_BUF_SIZE 672
+#define NUM_ALL_COEFFICIENTS 61680
+
+#endif /* __SOF_AUDIO_COEFFICIENTS_SRC_SRC_IPC4_INT32_DEFINE_H__ */
