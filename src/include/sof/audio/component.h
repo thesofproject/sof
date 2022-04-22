7bc232d24bdcd9275b5716686748ecae2ab43a11
Pin-chih Lin <johnylin@google.com>
2022-04-22 17:36:57 +0800

    component: fix comp_perf_info unsigned format
    Use %u instead of %d in printf for uint32_t variable.
    
    Signed-off-by: Pin-chih Lin <johnylin@google.com>

--- src/include/sof/audio/component.h
+++ src/include/sof/audio/component.h
@@ -231,7 +231,7 @@ enum {
 #endif /* #if defined(__ZEPHYR__) && defined(CONFIG_ZEPHYR_LOG) */
 
 #define comp_perf_info(pcd, comp_p)					\
-	comp_info(comp_p, "perf comp_copy peak plat %d cpu %d",		\
+	comp_info(comp_p, "perf comp_copy peak plat %u cpu %u",		\
 		  (uint32_t)((pcd)->plat_delta_peak),			\
 		  (uint32_t)((pcd)->cpu_delta_peak))
 
