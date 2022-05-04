a3ca1744aef759f87b76b2b561994a9a5ef7c998
Balakishorepati <balaKishore.pati@amd.com>
2022-05-04 12:05:15 +0530

    platform:amd:renoir: increase default frequency to 600 Mhz
    Increase clk from 200 Mhz to 600Mhz during boot up time.
    
    Signed-off-by: Balakishorepati <balaKishore.pati@amd.com>

--- src/platform/amd/renoir/lib/clk.c
+++ src/platform/amd/renoir/lib/clk.c
@@ -14,7 +14,7 @@
 #include <platform/chip_registers.h>
 
 static struct freq_table platform_cpu_freq[] = {
-	{200000000, 200000 },
+	{600000000, 600000 },
 };
 
 STATIC_ASSERT(NUM_CPU_FREQ == ARRAY_SIZE(platform_cpu_freq),
@@ -137,4 +137,5 @@ void platform_clock_init(struct sof *sof)
 		};
 		k_spinlock_init(&sof->clocks[i].lock);
 	}
+	acp_change_clock_notify(600000000);
 }
