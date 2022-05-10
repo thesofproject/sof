f64e6fbdbaa5d0b0022d412cc3de955a37f59297
Eddy Hsu <eddyhsu@chromium.org>
2022-05-10 17:10:10 +0000

    crossover: change to use model_handler to handle configs.
    Change to use the new model_handler to handle configs. In this way,
    `config_new` is no longer needed and the code looks much cleaner now.
    
    Signed-off-by: Eddy Hsu <eddyhsu@chromium.org>

--- src/include/sof/audio/crossover/crossover.h
+++ src/include/sof/audio/crossover/crossover.h
@@ -79,8 +79,8 @@ typedef void (*crossover_split)(int32_t in, int32_t out[],
 struct comp_data {
 	/**< filter state */
 	struct crossover_state state[PLATFORM_MAX_CHANNELS];
+	struct comp_data_blob_handler *model_handler;
 	struct sof_crossover_config *config;      /**< pointer to setup blob */
-	struct sof_crossover_config *config_new;  /**< pointer to new setup */
 	enum sof_ipc_frame source_format;         /**< source frame format */
 	crossover_process crossover_process;      /**< processing function */
 	crossover_split crossover_split;          /**< split function */
