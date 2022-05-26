3295eda53958ce3c9c7cd31b1d23d79f6287c796
Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
2022-05-26 14:32:07 +0200

    Connect intel_module_adapter with generic module adapter.
    Added glue logic to integrate with module adapter
    the loadable modules developed under Intel
    IADK Framework.
    
    Signed-off-by: Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>

--- src/include/sof/audio/module_adapter/module/generic.h
+++ src/include/sof/audio/module_adapter/module/generic.h
@@ -144,6 +144,8 @@ struct module_data {
 	struct module_interface *ops; /**< module specific operations */
 	struct module_memory memory; /**< memory allocated by module */
 	struct module_processing_data mpd; /**< shared data comp <-> module */
+	void *module_adapter; /**<loadable module interface handle */
+	uint32_t module_entry_point; /**<loadable module entry point address */
 };
 
 /* module_adapter private, runtime data */
