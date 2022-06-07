thesofproject/sof

commit 919e6463c652dd4ae9fe53c9e4991a519b59fceb

ipc: ipc4: Use Zephyr API k_sleep() instead of obsolete wait_delay()
Use Zephyr API k_sleep() instead of obsolete wait_delay(). In XTOS
build the k_sleep() is implemented in xtos-wrapper/include/kernel.h
and it actually calls the same old wait_delay().

Signed-off-by: Jyri Sarha <jyri.sarha@intel.com>

src/ipc/ipc4/handler.c
@@ -21,7 +21,6 @@
 #include <sof/ipc/driver.h>
 #include <sof/lib/mailbox.h>
 #include <sof/lib/pm_runtime.h>
-#include <sof/lib/wait.h>
 #include <sof/math/numbers.h>
 #include <sof/trace/trace.h>
 #include <ipc4/error_status.h>
@@ -31,6 +30,8 @@
 #include <ipc4/notification.h>
 #include <ipc/trace.h>
 #include <user/trace.h>
+
+#include <kernel.h>
 
 #include <errno.h>
 #include <stdbool.h>
@@ -372,8 +373,7 @@ static int ipc_wait_for_compound_msg(void)
 	int ret = 0;
 
 	while (msg_data.delayed_reply) {
-		/* TODO: update to yield(tmeout) for Zephyr */
-		wait_delay(10000);
+		k_sleep(Z_TIMEOUT_TICKS(10000));
 
 		if (!try_count--) {
 			ret = IPC4_FAILURE;

commit 4055cd29135c831031e2fcecab7097d9aba9e777

ipc: ipc4: correct the sleep time back to 250usec
When code was moved to k_sleep(), the amount of sleep time
was changed to Z_TIMEOUT_TICKS(10000). This resulted in 100x
longer sleeps. The original sleep was 10000 ticks of the 19.2/38.4Mhz
platform clock, so a minimum sleep of 260-520usec. With 50kHz
Zephyr ticks of the system clock, the sleep is 200ms.

Additionally this value now depends on Zephyr
CONFIG_SYS_CLOCK_TICKS_PER_SEC setting and this can vary between
platforms.The IPC processing should follow timing expected by the host,
so it is better to use absolute sleep times.

Building SOF with a Zephyr OS tick rate of less than 20kHz resulted in
host IPC timeouts (firmware minimum sleep in range of 500+ms). This
happens when ipc_wait_for_compound_msg() is called and delayed
response is not immediately available.

Change the sleep back to original ~250usec value.

Fixes: 919e6463c652 ("ipc: ipc4: Use Zephyr API k_sleep() instead of obsolete wait_delay()")
Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

src/arch/xtos-wrapper/include/kernel.h
@@ -24,6 +24,8 @@ typedef struct {
 
 #define Z_TIMEOUT_TICKS(t) ((k_timeout_t) { .ticks = (t) })
 
+#define Z_TIMEOUT_US(t) ((k_timeout_t) { .ticks = clock_us_to_ticks(PLATFORM_DEFAULT_CLOCK, t) })
+
 static inline void k_sleep(k_timeout_t timeout)
 {
 	wait_delay(timeout.ticks);

src/ipc/ipc4/handler.c
@@ -373,7 +373,7 @@ static int ipc_wait_for_compound_msg(void)
 	int ret = 0;
 
 	while (msg_data.delayed_reply) {
-		k_sleep(Z_TIMEOUT_TICKS(10000));
+		k_sleep(Z_TIMEOUT_US(250));
 
 		if (!try_count--) {
 			ret = IPC4_FAILURE;