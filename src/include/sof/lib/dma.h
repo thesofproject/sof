thesofproject/sof

commit 443b21de4b3fbe01394e9a4d5f72483a112f9580

dma-trace: Fix potential race issue by using blocking DMA copy
It is possible that that in trace_work() sof_ipc_dma_trace_posn ipc
message reaches host side sof driver before the dma_copy_to_host_nowait()
is finished. Change the dma_copy_to_host_nowait() implementation to
dma_copy_to_host() by adding DMA_COPY_BLOCKING flag to dma_copy()
command.

The flag is already there on the !defined(CONFIG_DMA_GW) version of
the function and the old name of the function was a bit misleading.

This commit changes also probe.c probe_task() so that it also uses the
new blocking DMA copy. I could not see any justification to use nowait
version there either.

Signed-off-by: Jyri Sarha <jyri.sarha@intel.com>

src/include/sof/lib/dma.h
@@ -541,8 +541,8 @@ int dma_copy_from_host_nowait(struct dma_copy *dc,
 			      int32_t size);
 
 /* DMA copy data from DSP to host */
-int dma_copy_to_host_nowait(struct dma_copy *dc, struct dma_sg_config *host_sg,
-	int32_t host_offset, void *local_ptr, int32_t size);
+int dma_copy_to_host(struct dma_copy *dc, struct dma_sg_config *host_sg,
+		     int32_t host_offset, void *local_ptr, int32_t size);
 
 int dma_copy_set_stream_tag(struct dma_copy *dc, uint32_t stream_tag);
 

src/ipc/dma-copy.c
@@ -59,13 +59,13 @@ static struct dma_sg_elem *sg_get_elem_at(struct dma_sg_config *host_sg,
  */
 #if CONFIG_DMA_GW
 
-int dma_copy_to_host_nowait(struct dma_copy *dc, struct dma_sg_config *host_sg,
-			    int32_t host_offset, void *local_ptr, int32_t size)
+int dma_copy_to_host(struct dma_copy *dc, struct dma_sg_config *host_sg,
+		     int32_t host_offset, void *local_ptr, int32_t size)
 {
 	int ret;
 
 	/* tell gateway to copy */
-	ret = dma_copy(dc->chan, size, 0);
+	ret = dma_copy(dc->chan, size, DMA_COPY_BLOCKING);
 	if (ret < 0)
 		return ret;
 
@@ -75,8 +75,8 @@ int dma_copy_to_host_nowait(struct dma_copy *dc, struct dma_sg_config *host_sg,
 
 #else /* CONFIG_DMA_GW */
 
-int dma_copy_to_host_nowait(struct dma_copy *dc, struct dma_sg_config *host_sg,
-			    int32_t host_offset, void *local_ptr, int32_t size)
+int dma_copy_to_host(struct dma_copy *dc, struct dma_sg_config *host_sg,
+		     int32_t host_offset, void *local_ptr, int32_t size)
 {
 	struct dma_sg_config config;
 	struct dma_sg_elem *host_sg_elem;

src/probe/probe.c
@@ -201,15 +201,15 @@ static enum task_state probe_task(void *data)
 	int err;
 
 	if (_probe->ext_dma.dmapb.avail > 0)
-		err = dma_copy_to_host_nowait(&_probe->ext_dma.dc,
-					      &_probe->ext_dma.config, 0,
-					       (void *)_probe->ext_dma.dmapb.r_ptr,
-					       _probe->ext_dma.dmapb.avail);
+		err = dma_copy_to_host(&_probe->ext_dma.dc,
+				       &_probe->ext_dma.config, 0,
+				       (void *)_probe->ext_dma.dmapb.r_ptr,
+				       _probe->ext_dma.dmapb.avail);
 	else
 		return SOF_TASK_STATE_RESCHEDULE;
 
 	if (err < 0) {
-		tr_err(&pr_tr, "probe_task(): dma_copy_to_host_nowait() failed.");
+		tr_err(&pr_tr, "probe_task(): dma_copy_to_host() failed.");
 		return err;
 	}
 

src/trace/dma-trace.c
@@ -83,10 +83,10 @@ static enum task_state trace_work(void *data)
 	d->copy_in_progress = 1;
 
 	/* copy this section to host */
-	size = dma_copy_to_host_nowait(&d->dc, config, d->posn.host_offset,
-				       buffer->r_ptr, size);
+	size = dma_copy_to_host(&d->dc, config, d->posn.host_offset,
+				buffer->r_ptr, size);
 	if (size < 0) {
-		tr_err(&dt_tr, "trace_work(): dma_copy_to_host_nowait() failed");
+		tr_err(&dt_tr, "trace_work(): dma_copy_to_host() failed");
 		goto out;
 	}
 

commit 66df9d54b5a45dcd1b9fea31606288d63cd94acd

dma: Resurrect dma_copy_to_host_nowait()
Resurrect dma_copy_to_host_nowait() since the probes do not appear to
work without it. This version implements also non blocking version for
non CONFIG_DMA_GW case. Earlier the function was called nowait, despite
the DMA_COPY_BLOCKING flag given to dma_copy_legacy().

Fixes: 443b21de4b3f ("dma-trace: Fix potential race issue by using blocking DMA copy")

Signed-off-by: Jyri Sarha <jyri.sarha@intel.com>

src/include/sof/lib/dma.h
@@ -552,6 +552,9 @@ int dma_copy_from_host_nowait(struct dma_copy *dc,
 /* DMA copy data from DSP to host */
 int dma_copy_to_host(struct dma_copy *dc, struct dma_sg_config *host_sg,
 		     int32_t host_offset, void *local_ptr, int32_t size);
+int dma_copy_to_host_nowait(struct dma_copy *dc, struct dma_sg_config *host_sg,
+			    int32_t host_offset, void *local_ptr, int32_t size);
+
 
 int dma_copy_set_stream_tag(struct dma_copy *dc, uint32_t stream_tag);
 

src/ipc/dma-copy.c
@@ -74,10 +74,26 @@ int dma_copy_to_host(struct dma_copy *dc, struct dma_sg_config *host_sg,
 	return size;
 }
 
+int dma_copy_to_host_nowait(struct dma_copy *dc, struct dma_sg_config *host_sg,
+			    int32_t host_offset, void *local_ptr, int32_t size)
+{
+	int ret;
+
+	/* tell gateway to copy */
+	ret = dma_copy_legacy(dc->chan, size, 0);
+	if (ret < 0)
+		return ret;
+
+	/* bytes copied */
+	return size;
+}
+
 #else /* CONFIG_DMA_GW */
 
-int dma_copy_to_host(struct dma_copy *dc, struct dma_sg_config *host_sg,
-		     int32_t host_offset, void *local_ptr, int32_t size)
+static int
+dma_copy_to_host_flags(struct dma_copy *dc, struct dma_sg_config *host_sg,
+		       int32_t host_offset, void *local_ptr, int32_t size,
+		       uint32_t flags)
 {
 	struct dma_sg_config config;
 	struct dma_sg_elem *host_sg_elem;
@@ -118,12 +134,26 @@ int dma_copy_to_host(struct dma_copy *dc, struct dma_sg_config *host_sg,
 		return err;
 
 	err = dma_copy_legacy(dc->chan, local_sg_elem.size,
-			      DMA_COPY_ONE_SHOT | DMA_COPY_BLOCKING);
+			      flags);
 	if (err < 0)
 		return err;
 
 	/* bytes copied */
 	return local_sg_elem.size;
+}
+
+int dma_copy_to_host(struct dma_copy *dc, struct dma_sg_config *host_sg,
+		     int32_t host_offset, void *local_ptr, int32_t size)
+{
+	return dma_copy_to_host_flags(dc, host_sg, host_offset, local_ptr, size,
+				      DMA_COPY_ONE_SHOT | DMA_COPY_BLOCKING);
+}
+
+int dma_copy_to_host_nowait(struct dma_copy *dc, struct dma_sg_config *host_sg,
+			    int32_t host_offset, void *local_ptr, int32_t size)
+{
+	return dma_copy_to_host_flags(dc, host_sg, host_offset, local_ptr, size,
+				      DMA_COPY_ONE_SHOT);
 }
 
 #endif /* CONFIG_DMA_GW */