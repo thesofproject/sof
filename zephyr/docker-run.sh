8d9aa96830c0f8bc22aab6feef550314a6859d42
Marc Herbert <marc.herbert@intel.com>
2022-09-01 17:28:57 +0000

    zephyr/docker-run.sh: add comments about image versions
    Explain how to use this script with a different image version.
    
    Docker image versions and tags tend to confuse new users.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- zephyr/docker-run.sh
+++ zephyr/docker-run.sh
@@ -4,6 +4,32 @@
 
 # set -x
 
+# Tip: if you're not really sure what image version you're using,
+# invoke this script with sample commands like these:
+#
+#  ./sof/zephyr/docker-run.sh  ls -l /opt/toolchains/
+#  ./sof/zephyr/docker-run.sh  /opt/sparse/bin/sparse --version
+#  ./sof/zephyr/docker-run.sh  /bin/bash
+#
+#   etc.
+# https://github.com/zephyrproject-rtos/docker-image/commits/master
+#
+# "latest" is just the default tag used when you don't specify any. To
+# use this script with a image other than the one officially tagged
+# "latest", simply overwrite the official tag. Example:
+#
+#   docker tag ghcr.io/zephyrproject-rtos/zephyr-build:v0.24.1
+#              ghcr.io/zephyrproject-rtos/zephyr-build:latest
+#
+# "latest" is just a regular tag like "v0.24.1", it may or many not name
+# the most recent image.
+#
+# To automatically restore the official "latest" tag, just delete it:
+#
+#   docker image rm ghcr.io/zephyrproject-rtos/zephyr-build:latest
+#
+# (The actual image stays in the docker cache, only the tag is deleted)
+
 set -e
 
 SOF_TOP="$(cd "$(dirname "$0")"/.. && /bin/pwd)"
