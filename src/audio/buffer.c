thesofproject/sof

commit 95485f05b5c0a0f686a34932cbb0f29e1a983b04

buffer: pass locked buffers to notification recipients
SOF probes use notifiers to be informed when buffer fill state
changes. These notifiers are always called synchronously on the same
core. We use that fact to pass locked buffers to probes.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/audio/buffer.c
@@ -195,10 +195,22 @@ void buffer_free(struct comp_buffer *buffer)
 	rfree(buffer);
 }
 
+/*
+ * comp_update_buffer_produce() and comp_update_buffer_consume() send
+ * NOTIFIER_ID_BUFFER_PRODUCE and NOTIFIER_ID_BUFFER_CONSUME notifier events
+ * respectively. The only recipient of those notifications is probes. The
+ * target for those notifications is always the current core, therefore notifier
+ * callbacks will be called synchronously from notifier_event() calls. Therefore
+ * we cannot pass unlocked buffer pointers to probes, because if they try to
+ * acquire the buffer, that can cause a deadlock. In general locked objects
+ * shouldn't be passed to potentially asynchronous contexts, but here we have no
+ * choice but to use our knowledge of the local notifier behaviour and pass
+ * locked buffers to notification recipients.
+ */
 void comp_update_buffer_produce(struct comp_buffer __sparse_cache *buffer, uint32_t bytes)
 {
 	struct buffer_cb_transact cb_data = {
-		.buffer = cache_to_uncache(buffer),
+		.buffer = buffer,
 		.transaction_amount = bytes,
 		.transaction_begin_address = buffer->stream.w_ptr,
 	};
@@ -216,7 +228,7 @@ void comp_update_buffer_produce(struct comp_buffer __sparse_cache *buffer, uint3
 	audio_stream_produce(&buffer->stream, bytes);
 
 	/* Notifier looks for the pointer value to match it against registration */
-	notifier_event(cache_to_uncache(buffer), NOTIFIER_ID_BUFFER_PRODUCE,
+	notifier_event(buffer, NOTIFIER_ID_BUFFER_PRODUCE,
 		       NOTIFIER_TARGET_CORE_LOCAL, &cb_data, sizeof(cb_data));
 
 	buf_dbg(buffer, "comp_update_buffer_produce(), ((buffer->avail << 16) | buffer->free) = %08x, ((buffer->id << 16) | buffer->size) = %08x",
@@ -231,7 +243,7 @@ void comp_update_buffer_produce(struct comp_buffer __sparse_cache *buffer, uint3
 void comp_update_buffer_consume(struct comp_buffer __sparse_cache *buffer, uint32_t bytes)
 {
 	struct buffer_cb_transact cb_data = {
-		.buffer = cache_to_uncache(buffer),
+		.buffer = buffer,
 		.transaction_amount = bytes,
 		.transaction_begin_address = buffer->stream.r_ptr,
 	};
@@ -248,7 +260,7 @@ void comp_update_buffer_consume(struct comp_buffer __sparse_cache *buffer, uint3
 
 	audio_stream_consume(&buffer->stream, bytes);
 
-	notifier_event(cache_to_uncache(buffer), NOTIFIER_ID_BUFFER_CONSUME,
+	notifier_event(buffer, NOTIFIER_ID_BUFFER_CONSUME,
 		       NOTIFIER_TARGET_CORE_LOCAL, &cb_data, sizeof(cb_data));
 
 	buf_dbg(buffer, "comp_update_buffer_consume(), (buffer->avail << 16) | buffer->free = %08x, (buffer->id << 16) | buffer->size = %08x, (buffer->r_ptr - buffer->addr) << 16 | (buffer->w_ptr - buffer->addr)) = %08x",

src/include/sof/audio/buffer.h
@@ -151,8 +151,9 @@ struct comp_buffer {
 	bool walking;		/**< indicates if the buffer is being walked */
 };
 
+/* Only to be used for synchronous same-core notifications! */
 struct buffer_cb_transact {
-	struct comp_buffer *buffer;
+	struct comp_buffer __sparse_cache *buffer;
 	uint32_t transaction_amount;
 	void *transaction_begin_address;
 };

src/probe/probe.c
@@ -751,7 +751,7 @@ static void probe_cb_produce(void *arg, enum notify_id type, void *data)
 {
 	struct probe_pdata *_probe = probe_get();
 	struct buffer_cb_transact *cb_data = data;
-	struct comp_buffer *buffer = cb_data->buffer;
+	struct comp_buffer __sparse_cache *buffer = cb_data->buffer;
 	struct probe_dma_ext *dma;
 	uint32_t buffer_id;
 	uint32_t head, tail;
@@ -778,7 +778,7 @@ static void probe_cb_produce(void *arg, enum notify_id type, void *data)
 		format = probe_gen_format(buffer->stream.frame_fmt,
 					  buffer->stream.rate,
 					  buffer->stream.channels);
-		ret = probe_gen_header(buffer->id,
+		ret = probe_gen_header(buffer_id,
 				       cb_data->transaction_amount,
 				       format);
 		if (ret < 0)
@@ -840,8 +840,8 @@ static void probe_cb_produce(void *arg, enum notify_id type, void *data)
 		/* check if transaction amount exceeds component buffer end addr */
 		/* if yes: divide copying into two stages, head and tail */
 		if ((char *)cb_data->transaction_begin_address +
-			cb_data->transaction_amount > (char *)cb_data->buffer->stream.end_addr) {
-			head = (char *)cb_data->buffer->stream.end_addr -
+			cb_data->transaction_amount > (char *)buffer->stream.end_addr) {
+			head = (char *)buffer->stream.end_addr -
 				(char *)cb_data->transaction_begin_address;
 			tail = cb_data->transaction_amount - head;
 
@@ -851,7 +851,7 @@ static void probe_cb_produce(void *arg, enum notify_id type, void *data)
 				goto err;
 
 			ret = copy_from_pbuffer(&dma->dmapb,
-						cb_data->buffer->stream.addr, tail);
+						buffer->stream.addr, tail);
 			if (ret < 0)
 				goto err;
 		} else {
@@ -899,7 +899,8 @@ err:
 static void probe_cb_free(void *arg, enum notify_id type, void *data)
 {
 	struct buffer_cb_free *cb_data = data;
-	uint32_t buffer_id = cb_data->buffer->id;
+	struct comp_buffer __sparse_cache *buffer = cb_data->buffer;
+	uint32_t buffer_id = buffer->id;
 	int ret;
 
 	tr_dbg(&pr_tr, "probe_cb_free() buffer_id = %u", buffer_id);

commit 1cf7543146d454a19d16acf9cfac20122f4c0801

buffer: fix notifier events
Buffer notifier events are registered with buffer's uncached address
as a key, so notifier events must use the same.

Fixes: 95485f05b5c0 ("buffer: pass locked buffers to notification
recipients")
Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/audio/buffer.c
@@ -228,7 +228,7 @@ void comp_update_buffer_produce(struct comp_buffer __sparse_cache *buffer, uint3
 	audio_stream_produce(&buffer->stream, bytes);
 
 	/* Notifier looks for the pointer value to match it against registration */
-	notifier_event(buffer, NOTIFIER_ID_BUFFER_PRODUCE,
+	notifier_event(cache_to_uncache(buffer), NOTIFIER_ID_BUFFER_PRODUCE,
 		       NOTIFIER_TARGET_CORE_LOCAL, &cb_data, sizeof(cb_data));
 
 	buf_dbg(buffer, "comp_update_buffer_produce(), ((buffer->avail << 16) | buffer->free) = %08x, ((buffer->id << 16) | buffer->size) = %08x",
@@ -260,7 +260,7 @@ void comp_update_buffer_consume(struct comp_buffer __sparse_cache *buffer, uint3
 
 	audio_stream_consume(&buffer->stream, bytes);
 
-	notifier_event(buffer, NOTIFIER_ID_BUFFER_CONSUME,
+	notifier_event(cache_to_uncache(buffer), NOTIFIER_ID_BUFFER_CONSUME,
 		       NOTIFIER_TARGET_CORE_LOCAL, &cb_data, sizeof(cb_data));
 
 	buf_dbg(buffer, "comp_update_buffer_consume(), (buffer->avail << 16) | buffer->free = %08x, (buffer->id << 16) | buffer->size = %08x, (buffer->r_ptr - buffer->addr) << 16 | (buffer->w_ptr - buffer->addr)) = %08x",