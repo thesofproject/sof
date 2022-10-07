91c7b6cab3e877bafa4236e76f13829171d60a7d
Tomasz Lissowski <tomasz.lissowski@intel.com>
2022-10-07 14:25:03 +0200

    ipc4: add mixing-based processing in channel selector
    Add mixing-based processing in channel selector to implement
    capabilities resulting from IPC4 configuration. Enable flexible
    channels count (from 1 to 8) independently for input and output.
    
    Signed-off-by: Tomasz Lissowski <tomasz.lissowski@intel.com>

--- src/include/sof/audio/selector.h
+++ src/include/sof/audio/selector.h
@@ -27,6 +27,7 @@
 struct comp_buffer;
 struct comp_dev;
 
+#if CONFIG_IPC_MAJOR_3
 /** \brief Supported channel count on input. */
 #define SEL_SOURCE_2CH 2
 #define SEL_SOURCE_4CH 4
@@ -35,6 +36,13 @@ struct comp_dev;
 #define SEL_SINK_1CH 1
 #define SEL_SINK_2CH 2
 #define SEL_SINK_4CH 4
+#else
+/** \brief Maximum supported channel count on input. */
+#define SEL_SOURCE_CHANNELS_MAX 8
+
+/** \brief Maximum supported channel count on output. */
+#define SEL_SINK_CHANNELS_MAX   8
+#endif
 
 #if CONFIG_IPC_MAJOR_4
 /** \brief selector processing function interface */
@@ -45,6 +53,20 @@ struct micsel_data {
 	struct ipc4_base_module_cfg base_cfg;
 	struct ipc4_audio_format output_format;
 };
+
+/** \brief IPC4 configuration IDs for selector. */
+enum ipc4_selector_config_id {
+	IPC4_SELECTOR_COEFFS_CONFIG_ID = 0,	/**< Mixing coefficients config ID */
+};
+
+/** \brief IPC4 mixing coefficients configuration. */
+struct ipc4_selector_coeffs_config {
+	uint16_t rsvd0;	/**< Unused field, keeps the structure aligned with common layout */
+	uint16_t rsvd1;	/**< Unused field, keeps the structure aligned with common layout */
+
+	/** Mixing coefficients in Q10 fixed point format */
+	int16_t coeffs[SEL_SINK_CHANNELS_MAX][SEL_SOURCE_CHANNELS_MAX];
+};
 #else
 typedef void (*sel_func)(struct comp_dev *dev, struct audio_stream __sparse_cache *sink,
 			 const struct audio_stream __sparse_cache *source, uint32_t frames);
@@ -53,7 +75,8 @@ typedef void (*sel_func)(struct comp_dev *dev, struct audio_stream __sparse_cach
 /** \brief Selector component private data. */
 struct comp_data {
 #if CONFIG_IPC_MAJOR_4
-		struct micsel_data md;
+	struct micsel_data md;
+	struct ipc4_selector_coeffs_config coeffs_config;
 #endif
 
 	uint32_t source_period_bytes;	/**< source number of period bytes */
