thesofproject/sof

commit c3c94fc5151886f198bf331af9f454c6089634db

header: rtos: use rtos specific version of wait.h
Code can now include <rtos/wait.h> and uses native Zephyr 64
cycle API instead of SOF version.

Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

zephyr/include/rtos/wait.h
@@ -1,0 +1,54 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022 Intel Corporation. All rights reserved.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ */
+
+#ifndef __ZEPHYR_RTOS_WAIT_H__
+#define __ZEPHYR_RTOS_WAIT_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <zephyr/kernel.h>
+
+/* TODO: use equivalent Zephyr */
+static inline void idelay(int n)
+{
+	while (n--)
+		asm volatile("nop");
+}
+
+/* DSP default delay in cycles - all platforms use this today */
+#define PLATFORM_DEFAULT_DELAY	12
+
+static inline void wait_delay(uint64_t number_of_clks)
+{
+	uint64_t timeout = k_cycle_get_64() + number_of_clks;
+
+	while (k_cycle_get_64() < timeout)
+		idelay(PLATFORM_DEFAULT_DELAY);
+}
+
+static inline void wait_delay_ms(uint64_t ms)
+{
+	wait_delay(k_ms_to_cyc_ceil64(ms));
+}
+
+static inline void wait_delay_us(uint64_t us)
+{
+	wait_delay(k_us_to_cyc_ceil64(us));
+}
+
+int poll_for_register_delay(uint32_t reg, uint32_t mask,
+			    uint32_t val, uint64_t us);
+
+/* TODO: this is tmp used by some IMX IPC drivers that have not transitioned
+ * to Zephyr. To be removed after drivers migrated to Zephyr. It's xtensa only too.
+ */
+static inline void wait_for_interrupt(int level)
+{
+	asm volatile("waiti 0");
+}
+
+#endif /* __ZEPHYR_RTOS_WAIT_H__ */

zephyr/wrapper.c
@@ -20,6 +20,7 @@
 #include <sof/audio/pipeline.h>
 #include <sof/audio/component_ext.h>
 #include <sof/trace/trace.h>
+#include <rtos/wait.h>
 
 /* Zephyr includes */
 #include <zephyr/device.h>
@@ -685,7 +686,7 @@ void platform_dai_timestamp(struct comp_dev *dai,
 		posn->flags |= SOF_TIME_DAI_VALID;
 
 	/* get SSP wallclock - DAI sets this to stream start value */
-	posn->wallclock = sof_cycle_get_64() - posn->wallclock;
+	posn->wallclock = k_cycle_get_64() - posn->wallclock;
 	posn->wallclock_hz = clock_get_freq(PLATFORM_DEFAULT_CLOCK);
 	posn->flags |= SOF_TIME_WALL_VALID;
 }
@@ -693,7 +694,7 @@ void platform_dai_timestamp(struct comp_dev *dai,
 /* get current wallclock for componnent */
 void platform_dai_wallclock(struct comp_dev *dai, uint64_t *wallclock)
 {
-	*wallclock = sof_cycle_get_64();
+	*wallclock = k_cycle_get_64();
 }
 
 /*
@@ -825,4 +826,34 @@ struct idc **idc_get(void)
 	return p_idc + cpu;
 }
 #endif
+
+#define DEFAULT_TRY_TIMES 8
+
+int poll_for_register_delay(uint32_t reg, uint32_t mask,
+			    uint32_t val, uint64_t us)
+{
+	uint64_t tick = k_us_to_cyc_ceil64(us);
+	uint32_t tries = DEFAULT_TRY_TIMES;
+	uint64_t delta = tick / tries;
+
+	if (!delta) {
+		/*
+		 * If we want to wait for less than DEFAULT_TRY_TIMES ticks then
+		 * delta has to be set to 1 and number of tries to that of number
+		 * of ticks.
+		 */
+		delta = 1;
+		tries = tick;
+	}
+
+	while ((io_reg_read(reg) & mask) != val) {
+		if (!tries--) {
+			LOG_DBG("poll timeout reg %u mask %u val %u us %u",
+			       reg, mask, val, (uint32_t)us);
+			return -EIO;
+		}
+		wait_delay(delta);
+	}
+	return 0;
+}
 

xtos/include/rtos/wait.h
@@ -9,17 +9,19 @@
  * Simple wait for event completion and signaling with timeouts.
  */
 
-#ifndef __SOF_LIB_WAIT_H__
-#define __SOF_LIB_WAIT_H__
+#ifndef __XTOS_RTOS_WAIT_H__
+#define __XTOS_RTOS_WAIT_H__
 
+#include <stddef.h>
+#include <stdint.h>
+
+#if !CONFIG_LIBRARY
 #include <arch/lib/wait.h>
+#include <sof/drivers/interrupt.h>
 #include <sof/drivers/timer.h>
 #include <rtos/spinlock.h>
 #include <sof/trace/trace.h>
 #include <user/trace.h>
-
-#include <stddef.h>
-#include <stdint.h>
 
 extern struct tr_ctx wait_tr;
 
@@ -36,7 +38,6 @@ static inline void wait_for_interrupt(int level)
 	tr_dbg(&wait_tr, "WFX");
 }
 
-#if !CONFIG_LIBRARY
 /**
  * \brief Waits at least passed number of clocks.
  * \param[in] number_of_clks Minimum number of clocks to wait.
@@ -63,4 +64,4 @@ static inline void wait_delay_us(uint64_t us) {}
 int poll_for_register_delay(uint32_t reg, uint32_t mask,
 			    uint32_t val, uint64_t us);
 
-#endif /* __SOF_LIB_WAIT_H__ */
+#endif /* __XTOS_RTOS_WAIT_H__ */

commit 7ab4b49f7d654e360babf5abf123359d81b0a502

zephyr: Fix cycle counting on i.MX
On i.MX8 k_cycle_get_64 always returns 0 so use sof_cycle_get_64
instead.

There is no better solution to support 64bit cycle counting for i.MX
and will have to stick with this implementation.

Fixes: c3c94fc515188 ("header: rtos: use rtos specific version of wait.h")
Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

zephyr/include/rtos/wait.h
@@ -24,9 +24,9 @@ static inline void idelay(int n)
 
 static inline void wait_delay(uint64_t number_of_clks)
 {
-	uint64_t timeout = k_cycle_get_64() + number_of_clks;
+	uint64_t timeout = sof_cycle_get_64() + number_of_clks;
 
-	while (k_cycle_get_64() < timeout)
+	while (sof_cycle_get_64() < timeout)
 		idelay(PLATFORM_DEFAULT_DELAY);
 }
 

zephyr/wrapper.c
@@ -664,7 +664,7 @@ void platform_dai_timestamp(struct comp_dev *dai,
 		posn->flags |= SOF_TIME_DAI_VALID;
 
 	/* get SSP wallclock - DAI sets this to stream start value */
-	posn->wallclock = k_cycle_get_64() - posn->wallclock;
+	posn->wallclock = sof_cycle_get_64() - posn->wallclock;
 	posn->wallclock_hz = clock_get_freq(PLATFORM_DEFAULT_CLOCK);
 	posn->flags |= SOF_TIME_WALL_VALID;
 }
@@ -672,7 +672,7 @@ void platform_dai_timestamp(struct comp_dev *dai,
 /* get current wallclock for componnent */
 void platform_dai_wallclock(struct comp_dev *dai, uint64_t *wallclock)
 {
-	*wallclock = k_cycle_get_64();
+	*wallclock = sof_cycle_get_64();
 }
 
 /*