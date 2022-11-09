e0d8078fa748a51959b2042e50c1a24be5fa7dc0
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2022-11-09 17:53:37 +0200

    Tools: Testbench: Avoid need linux kernel headers in build
    This patch adds simplified linux/types.h into testbench headers
    to enable include of ALSA asoc.h to build without other difficult
    kernel headers content.
    
    The CMakeLists.txt files are updated to make a build time copy of
    asoc.h to subdirectory include of the build directories. The new
    directory is added to headers path.
    
    This change avoids build fail with xcc toolchain that can't use the
    gcc toolchain headers from the system where the ALSA headers are
    located.
    
    Suggested-by: Marc Herbert <marc.herbert@intel.com>
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

---
+++ tools/tplg_parser/include/linux/types.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#ifndef __TPLG_PARSER_LINUX_TYPES_H__
+#define __TPLG_PARSER_LINUX_TYPES_H__
+
+#include <stdint.h>
+
+/*
+ * This header files allows to include asoc.h for topology parsing to
+ * non-gcc builds with other C library, e.g. the one that is used by
+ * xt-xcc compiler. The kernel linux/types.h cannot be used because
+ * the other definitions there are not compatible with the toolchain.
+ */
+
+/* There are minimum types needed for alsa/sound/uapi/asoc.h */
+
+typedef int64_t __le64;
+typedef int32_t __le32;
+typedef int16_t __le16;
+typedef uint8_t __u8;
+
+#endif /* __TPLG_PARSER_LINUX_TYPES_H__ */
