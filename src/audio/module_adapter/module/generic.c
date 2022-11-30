e1dcc6e1628d9ecec62b53a3a52f1c4a89864c1a
Pin-chih Lin <johnylin@google.com>
2022-11-30 19:22:20 +0800

    Reland "module_adapter: Modify reset API"
    This reverts commit af1e120d167e ("Revert "module_adapter: Modify
    reset API"").
    
    Module DTS has the latest library update adapted for the new reset API
    
    Signed-off-by: Pin-chih Lin <johnylin@google.com>

--- src/audio/module_adapter/module/generic.c
+++ src/audio/module_adapter/module/generic.c
@@ -281,10 +281,11 @@ int module_reset(struct processing_module *mod)
 	rfree(md->cfg.data);
 	md->cfg.data = NULL;
 
-	/* module resets itself to the initial condition after prepare()
-	 * so let's change its state to reflect that.
+	/*
+	 * reset the state to allow the module's prepare callback to be invoked again for the
+	 * subsequent triggers
 	 */
-	md->state = MODULE_IDLE;
+	md->state = MODULE_INITIALIZED;
 
 	return 0;
 }
