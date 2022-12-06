b26a52e6e9b5449861a6f68078a79ee8cd9c51b9
Per Åhgren <peah@chromium.org>
2022-12-06 23:29:50 +0100

    Updating of GoogleRtcAudioProcessing component to provide a more general behavior
    Changes in the GoogleRtcAudioProcessing component to
    --Allow sample rate and number of channels to explicitly be specified.
    --Update the memory management.
    --Allow parameters to be passed.
    --Allow more than mono to be processed
    --Allow parameters to be specified from Kconfig
    
    Signed-off-by: Per Åhgren <peah@google.com>
    (cherry picked from commit e616b4210b051d7652e7c60e87357aad13ff06b9)

--- third_party/include/google_rtc_audio_processing.h
+++ third_party/include/google_rtc_audio_processing.h
@@ -13,16 +13,51 @@ extern "C" {
 #endif
 
 // This define ensure that the linked library matches the header file.
+// LINT.IfChange(api_version)
 #define GoogleRtcAudioProcessingCreate GoogleRtcAudioProcessingCreate_v1
+// LINT.ThenChange()
 
 typedef struct GoogleRtcAudioProcessingState GoogleRtcAudioProcessingState;
 
+// LINT.IfChange()
+
+// Attaches `buffer` to use for memory allocations. The ownership of `buffer`
+// remains within the caller.
+void GoogleRtcAudioProcessingAttachMemoryBuffer(uint8_t *const buffer,
+						int buffer_size);
+
+// Detaches any attached memory buffer used for memory allocations. Returns 0 if
+// success and non zero if failure.
+void GoogleRtcAudioProcessingDetachMemoryBuffer(void);
+
 // Creates an instance of GoogleRtcAudioProcessing with the tuning embedded in
-// the library.
+// the library. If creation fails, NULL is returned.
 GoogleRtcAudioProcessingState *GoogleRtcAudioProcessingCreate(void);
+
+// Creates an instance of GoogleRtcAudioProcessing based on `config` and stream
+// formats, where the content of config overrides any embedded parameters and
+// the stream formats overrides any content in the config. Setting `config` to
+// NULL means that no config is specified. If creation fails, NULL is returned.
+GoogleRtcAudioProcessingState *GoogleRtcAudioProcessingCreateWithConfig(
+    int capture_sample_rate_hz, int num_capture_input_channels,
+    int num_capture_output_channels, int render_sample_rate_hz,
+    int num_render_channels, const uint8_t *const config, int config_size);
 
 // Frees all allocated resources in `state`.
 void GoogleRtcAudioProcessingFree(GoogleRtcAudioProcessingState *state);
+
+// Specifies the stream formats to use. Returns 0 if success and non zero if
+// failure.
+int GoogleRtcAudioProcessingSetStreamFormats(
+    GoogleRtcAudioProcessingState *const state, int capture_sample_rate_hz,
+    int num_capture_input_channels, int num_capture_output_channels,
+    int render_sample_rate_hz, int num_render_channels);
+
+// Specifies setup-specific parameters. Returns 0 if success and non zero if
+// failure. Parameters which are NULL are ignored.
+int GoogleRtcAudioProcessingParameters(
+    GoogleRtcAudioProcessingState *const state, float *capture_headroom_linear,
+    float *echo_path_delay_ms);
 
 // Returns the framesize used for processing.
 int GoogleRtcAudioProcessingGetFramesizeInMs(
@@ -62,8 +97,10 @@ int GoogleRtcAudioProcessingAnalyzeRender_float32(
 int GoogleRtcAudioProcessingAnalyzeRender_int16(
     GoogleRtcAudioProcessingState *const state, const int16_t *const src);
 
+// LINT.ThenChange(:api_version)
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // GOOGLE_RTC_AUDIO_PROCESSING_H
+#endif	// GOOGLE_RTC_AUDIO_PROCESSING_H
