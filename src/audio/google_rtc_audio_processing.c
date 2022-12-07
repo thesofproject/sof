8a0f10b22ae5e37f93cbd062811f85d9df3668f7
Per Åhgren <peah@google.com>
2022-12-08 00:47:02 +0100

    Correct build error in the GoogleRtcAudioProcessing component
    Correct a typo that causes a build error in the GoogleRtcAudioProcessing component.
    
    Signed-off-by: Per Åhgren <peah@google.com>
    (cherry picked from commit 0f1434504b952a2920ff054ff7b34d1b08e8bd98)

--- src/audio/google_rtc_audio_processing.c
+++ src/audio/google_rtc_audio_processing.c
@@ -325,7 +325,7 @@ static int google_rtc_audio_processing_prepare(struct comp_dev *dev)
 
 		if (source_c->source->pipeline->pipeline_id != dev->pipeline->pipeline_id) {
 			cd->aec_reference = source;
-			aec_channels = sourcs_c->stream.channels;
+			aec_channels = source_c->stream.channels;
 		} else {
 			cd->raw_microphone = source;
 		}
