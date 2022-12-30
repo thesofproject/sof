785ebb87c9cfecc25dfe6341d2fb70d07f830823
Trevor Wu <trevor.wu@mediatek.com>
2022-12-30 10:01:46 +0800

    drivers: mtk: separate sinegen control from memif
    Sinegen control part is platform dependent and optional for debug
    purporse. On the other hand, memif control driver which is platform
    independent could be reused. As a result, sinegen control driver is
    separated from afe-memif.c.
    
    Signed-off-by: Trevor Wu <trevor.wu@mediatek.com>

---
+++ src/include/sof/drivers/afe-sgen.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright(c) 2023 MediaTek. All rights reserved.
+ *
+ * Author: Trevor Wu <trevor.wu@mediatek.com>
+ */
+
+#ifndef __SOF_DRIVERS_AFE_SGEN_H__
+#define __SOF_DRIVERS_AFE_SGEN_H__
+
+void afe_sinegen_enable(void);
+void afe_sinegen_disable(void);
+
+#endif /* __SOF_DRIVERS_AFE_SGEN_H__ */
+
