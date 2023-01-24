04130ea58aa2c4b617f71bfb7ecc9c0e6dc4974f
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2023-01-24 15:44:09 +0100

    eq-fir: convert to Zephyr native initialisation
    Use SOF_MODULE_INIT() to register eq-fir with the Zephyr
    initialisation framework.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/audio/eq_fir/eq_fir.c
+++ src/audio/eq_fir/eq_fir.c
@@ -17,6 +17,7 @@
 #include <sof/debug/panic.h>
 #include <sof/ipc/msg.h>
 #include <rtos/alloc.h>
+#include <rtos/init.h>
 #include <sof/lib/memory.h>
 #include <sof/lib/uuid.h>
 #include <sof/list.h>
@@ -544,3 +545,4 @@ static struct module_interface eq_fir_interface = {
 };
 
 DECLARE_MODULE_ADAPTER(eq_fir_interface, eq_fir_uuid, eq_fir_tr);
+SOF_MODULE_INIT(eq_fir, sys_comp_module_eq_fir_interface_init);
--- zephyr/wrapper.c
+++ zephyr/wrapper.c
@@ -184,7 +184,6 @@ static void sys_module_init(void)
 
 void sys_comp_host_init(void);
 void sys_comp_mux_init(void);
-void sys_comp_module_eq_fir_interface_init(void);
 void sys_comp_keyword_init(void);
 void sys_comp_asrc_init(void);
 void sys_comp_dcblock_init(void);
@@ -251,9 +250,6 @@ int task_main_start(struct sof *sof)
 
 int start_complete(void)
 {
-	if (IS_ENABLED(CONFIG_COMP_FIR))
-		sys_comp_module_eq_fir_interface_init();
-
 	if (IS_ENABLED(CONFIG_COMP_IIR))
 		sys_comp_eq_iir_init();
 
