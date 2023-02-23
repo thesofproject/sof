6ca18d9689e23feb84bbc9c13405b17afd734e4d
Piotr Makaruk <piotr.makaruk@intel.com>
2023-02-23 15:16:14 +0100

    ipc4: add file for notifications initializations
    Add separate file for notifications initializations. All generic
    initializations could be stored in one place.
    
    Signed-off-by: Piotr Makaruk <piotr.makaruk@intel.com>

---
+++ src/ipc/ipc4/notification.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright(c) 2023 Intel Corporation. All rights reserved.
+ *
+ * Author: Piotr Makaruk <piotr.makaruk@intel.com>
+ */
+
+#include <sof/common.h>
+#include <stdbool.h>
+#include <ipc4/notification.h>
