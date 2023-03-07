9f8a3f2eba0ee12fd5da7cf5888ba873c8dab44b
Baofeng Tian <baofeng.tian@intel.com>
2023-03-07 15:45:10 +0800

    dai-zephyr: Expose common functions for DAI new/free
    Create and expose two functions for dai allocation and
    free along with struct dai_data,these functions will be shared by both
    dai and copier components.
    This is in preparation to remove the creation of dai component and
    endpoint buffer in init_dai().
    
    Below is the preparation work:
    Add dai data structure to copier data structure
    Currently, copier call dai interface need through ops driver.
    However, with dedicated work, all dai interface can be called
    through dai_data, this commit add dai data into copier data.
    
    Signed-off-by: Baofeng Tian <baofeng.tian@intel.com>

--- src/include/ipc4/copier.h
+++ src/include/ipc4/copier.h
@@ -263,6 +263,7 @@ struct copier_data {
 	uint64_t output_total_data_processed;
 	struct host_data *hd;
 	bool ipc_gtw;
+	struct dai_data *dd;
 };
 
 int apply_attenuation(struct comp_dev *dev, struct copier_data *cd,
