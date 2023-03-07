d20916066e1a8226ac3a0b91bb10781ac2219fad
Adrian Warecki <adrian.warecki@intel.com>
2023-03-07 15:28:12 +0100

    misc_utils: Added functions to print enum and flags value
    Added name_val structure representing name/value tuple. Added function
    print_enum which displays the name of the enum corresponding to a given
    value. Added print_flags function which display names of flags constituting
    a given value.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/include/rimage/misc_utils.h
+++ src/include/rimage/misc_utils.h
@@ -3,6 +3,9 @@
  * Copyright(c) 2023 Intel Corporation. All rights reserved.
  */
 
+#ifndef __MISC_UTILS_H__
+#define __MISC_UTILS_H__
+
 #include <stdint.h>
 
 /**
@@ -11,3 +14,16 @@
  * @param size of the array
  */
 void bytes_swap(uint8_t *ptr, uint32_t size);
+
+struct name_val {
+	const char *name;
+	unsigned long value;
+};
+
+#define NAME_VAL_ENTRY(x) { .name = #x, .value = x }
+#define NAME_VAL_END { .name = 0, .value = 0 }
+
+void print_enum(unsigned long value, const struct name_val *values);
+void print_flags(unsigned long value, const struct name_val *flags);
+
+#endif /* __MISC_UTILS_H__ */
