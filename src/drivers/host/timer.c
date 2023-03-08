faf323a758d6bad6f242b2e5707e04cc1c610757
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2023-03-08 11:30:24 +0200

    Drivers: Host: Timer: Add dummy platform_dai_wallclock() function
    This avoids tools/oss-fuzz build fail to issue
    
    dai-legacy.c:
    (.text.dai_comp_trigger_internal[dai_comp_trigger_internal]+0xaca):
    undefined reference to `platform_dai_wallclock'
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/drivers/host/timer.c
+++ src/drivers/host/timer.c
@@ -7,7 +7,9 @@
 //         Rander Wang <rander.wang@intel.com>
 //         Janusz Jankowski <janusz.jankowski@linux.intel.com>
 
+#include <sof/audio/component.h>
 #include <rtos/timer.h>
+#include <stdint.h>
 
 void platform_host_timestamp(struct comp_dev *host,
 			     struct sof_ipc_stream_posn *posn)
@@ -18,6 +20,12 @@ void platform_host_timestamp(struct comp_dev *host,
 void platform_dai_timestamp(struct comp_dev *dai,
 			    struct sof_ipc_stream_posn *posn)
 {
+}
+
+/* Add function to avoid tools/oss-fuzz build fail */
+void platform_dai_wallclock(struct comp_dev *dai, uint64_t *wallclock)
+{
+	*wallclock = 0;
 }
 
 #ifndef __ZEPHYR__
