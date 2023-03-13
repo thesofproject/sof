23504df4f9f068b0b7cf755e00d72547616dfd12
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2023-03-13 15:55:04 +0100

    ipc4: module: remove unused variable size structures
    struct ipc4_module_init_instance contains 4 structures at the end,
    that aren't currently used in the firmware. Some of them like
    struct ipc4_module_init_ext_data have a flexible array at the end
    but can have further structures following them. Remove these
    structures until any of them are needed, at which point they can be
    re-added with proper size accounting.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/include/ipc4/module.h
+++ src/include/ipc4/module.h
@@ -4,9 +4,6 @@
  */
 
 /*
- * This file contains structures that are exact copies of an existing ABI used
- * by IOT middleware. They are Intel specific and will be used by one middleware.
- *
  * Some of the structures may contain programming implementations that makes them
  * unsuitable for generic use and general usage.
  *
@@ -145,11 +142,15 @@ struct ipc4_module_init_instance {
 			uint32_t _hw_reserved_2     : 2;
 		} r;
 	} extension;
-
-	struct ipc4_module_init_ext_init ext_init;
-	struct ipc4_module_init_ext_data ext_data;
-	struct ipc4_module_init_gna_config gna_config;
-	struct ipc4_module_init_data init_data;
+/*
+ * The following objects are optional and follow this structure in this
+ * order provided the respective object bit is set in preceding object.
+ *
+ *	struct ipc4_module_init_ext_init ext_init;
+ *	struct ipc4_module_init_ext_data ext_data;
+ *	struct ipc4_module_init_gna_config gna_config;
+ *	struct ipc4_module_init_data init_data;
+ */
 } __attribute__((packed, aligned(4)));
 
 /*!
