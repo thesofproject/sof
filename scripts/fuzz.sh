f41ad4329d4ca1c17a52ad4f037a0fa12880d885
Marc Herbert <marc.herbert@intel.com>
2023-03-22 01:39:27 +0000

    scripts/fuzz.sh: add timeout feature and stdout redirection
    Also add getopts and a very crude help. Should be enough to get started
    in CI.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- scripts/fuzz.sh
+++ scripts/fuzz.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+print_help()
+{
+    cat <<EOFHELP
 # Simple wrapper around a libfuzzer test run, as much for
 # documentation as direct use.  The idea here is really simple: build
 # for the Zephyr "native_posix" board (which is a just a x86
@@ -21,13 +24,49 @@ set -e
 # You will need the kconfigs specified below for correct operation,
 # but can add more at the end of this script's command line to
 # duplicate configurations as needed.  Alternatively you can pass
-# overlay files in kconfig syntax via -DOVERLAY_CONFIG=..., etc...
+# overlay files in kconfig syntax via:
+#    fuzz.sh  -t 300 -- -DOVERLAY_CONFIG=..., etc...
+EOFHELP
+}
+
+
+# To test this test, make the following change locally:
+: <<EOF_TEST_PATCH
+--- a/src/ipc/ipc3/handler.c
++++ b/src/ipc/ipc3/handler.c
+@@ -1609,6 +1609,8 @@ void ipc_boot_complete_msg(struct ipc_cmd_hdr *header, uint32_t data)
+
+ void ipc_cmd(struct ipc_cmd_hdr *_hdr)
+ {
++       __ASSERT(false, "test the fuzzer test");
++
+        struct sof_ipc_cmd_hdr *hdr = ipc_from_hdr(_hdr);
+EOF_TEST_PATCH
+
 
 main()
 {
   setup
 
-  west build -p -d build-fuzz -b native_posix "$SOF_TOP"/app/ -- \
+  # Parse "$@". getopts stops after '--'
+  while getopts "ho:t:" opt; do
+      case "$opt" in
+          h) print_help; exit 0;;
+          o) FUZZER_STDOUT="$OPTARG";;
+          t) TEST_DURATION="$OPTARG";;
+          *) print_help; exit 1;;
+      esac
+  done
+
+  # Pass all "$@" arguments remaining after '--' to cmake.
+  # This also drops _leading_ '--'.
+  shift $((OPTIND-1))
+
+  # Default values
+  : "${TEST_DURATION:=3}"
+  : "${FUZZER_STDOUT:=/dev/stdout}" # bashism
+
+  west build -d build-fuzz -b native_posix "$SOF_TOP"/app/ -- \
     -DCONFIG_ASSERT=y \
     -DCONFIG_SYS_HEAP_BIG_ONLY=y \
     -DCONFIG_ZEPHYR_NATIVE_DRIVERS=y \
@@ -36,7 +75,23 @@ main()
     -DCONFIG_ASAN=y "$@"
 
   mkdir -p ./fuzz_corpus
-  build-fuzz/zephyr/zephyr.exe ./fuzz_corpus
+
+  timeoutret=0
+  date
+  timeout -k 10 "$TEST_DURATION" >"$FUZZER_STDOUT" build-fuzz/zephyr/zephyr.exe ./fuzz_corpus ||
+      timeoutret=$?
+  if [ $timeoutret -eq 124 ]; then
+      # The current ASAN seems overly sensitive: it frequently (but not always) complains
+      # loudly that it has been aborted with a so-called DEADLYSIGNAL when the timeout
+      # command above actually delivers just a gentle TERM by default. Counteracts the
+      # wrong impression this unpredictable whining can give with a proud "successfully"
+      # message.
+      printf '\n\nSuccessfully aborted fuzzer after %d seconds test duration with DEADLYSIGNAL\n' \
+             "$TEST_DURATION"
+  else
+      die 'zephyr.exe fuzzer stopped before timing out: %d\n' $timeoutret
+  fi
+  date
 }
 
 
@@ -54,6 +109,16 @@ setup()
     ZEP_DIR=$(2>/dev/null west list -f '{path}' zephyr || echo 'zephyr')
     export ZEPHYR_BASE="$WS_TOP/$ZEP_DIR"
 
+}
+
+die()
+{
+        >&2 printf '%s ERROR: ' "$0"
+        # We want die() to be usable exactly like printf
+        # shellcheck disable=SC2059
+        >&2 printf "$@"
+        >&2 printf '\n'
+        exit 1
 }
 
 main "$@"
