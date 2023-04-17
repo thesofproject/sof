d93b0a4c2e7df1831c0e8b30b50e07afbe3bf619
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-04-17 13:11:56 +0200

    pipeline2.0: introduce a source API
    this is a definition of API to source of audio data
    
    THE SOURCE is any component in the system that have data
    stored somehow and can give the data outside at request.
    The source API does not define who and how has produced
    the data
      The user - a module - sees this as a producer that
                 PROVIDES data for processing
      The IMPLEMENTATION - audio_stream, DP Queue - sees
                 this API as a destination it must send data to
    
    Examples of components that should expose source API:
      - DMIC. Data are coming from the outside world,
              stores in tmp buffer and can be presented
    	  to the rest of the system using source_api
      - a memory ring buffer
    	Data are coming from other module
            (usually using sink_api, but it does not matter in fact)
    
    Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

---
+++ src/audio/source_api_helper.c
@@ -0,0 +1,167 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2023 Intel Corporation. All rights reserved.
+//
+
+#include <sof/audio/source_api.h>
+#include <sof/audio/source_api_implementation.h>
+#include <sof/audio/audio_stream.h>
+
+void source_init(struct sof_source __sparse_cache *source, const struct source_ops *ops,
+		 struct sof_audio_stream_params *audio_stream_params)
+{
+	source->ops = ops;
+	source->requested_read_frag_size = 0;
+	source->audio_stream_params = audio_stream_params;
+}
+
+size_t source_get_data_available(struct sof_source __sparse_cache *source)
+{
+	return source->ops->get_data_available(source);
+}
+
+int source_get_data(struct sof_source __sparse_cache *source, size_t req_size,
+		    void **data_ptr, void **buffer_start, size_t *buffer_size)
+{
+	int ret;
+
+	if (source->requested_read_frag_size)
+		return -EBUSY;
+
+	ret = source->ops->get_data(source, req_size, data_ptr, buffer_start, buffer_size);
+
+	if (!ret)
+		source->requested_read_frag_size = req_size;
+	return ret;
+}
+
+int source_release_data(struct sof_source __sparse_cache *source, size_t free_size)
+{
+	int ret;
+
+	/* Check if anything was obtained before for reading by source_get_data */
+	if (!source->requested_read_frag_size)
+		return -ENODATA;
+
+	/* limit size of data to be freed to previously obtained size */
+	if (free_size > source->requested_read_frag_size)
+		free_size = source->requested_read_frag_size;
+
+	ret = source->ops->release_data(source, free_size);
+
+	if (!ret)
+		source->requested_read_frag_size = 0;
+
+	source->num_of_bytes_processed += free_size;
+	return ret;
+}
+
+size_t source_get_num_of_processed_bytes(struct sof_source __sparse_cache *source)
+{
+	return source->num_of_bytes_processed;
+}
+
+void source_reset_num_of_processed_bytes(struct sof_source __sparse_cache *source)
+{
+	source->num_of_bytes_processed = 0;
+}
+
+enum sof_ipc_frame source_get_frm_fmt(struct sof_source __sparse_cache *source)
+{
+	return source->audio_stream_params->frame_fmt;
+}
+
+enum sof_ipc_frame source_get_valid_fmt(struct sof_source __sparse_cache *source)
+{
+	return source->audio_stream_params->valid_sample_fmt;
+}
+
+unsigned int source_get_rate(struct sof_source __sparse_cache *source)
+{
+	return source->audio_stream_params->rate;
+}
+
+unsigned int source_get_channels(struct sof_source __sparse_cache *source)
+{
+	return source->audio_stream_params->channels;
+}
+
+uint32_t source_get_buffer_fmt(struct sof_source __sparse_cache *source)
+{
+	return source->audio_stream_params->buffer_fmt;
+}
+
+bool source_get_underrun(struct sof_source __sparse_cache *source)
+{
+	return source->audio_stream_params->underrun_permitted;
+}
+
+int source_set_valid_fmt(struct sof_source __sparse_cache *source,
+			 enum sof_ipc_frame valid_sample_fmt)
+{
+	source->audio_stream_params->valid_sample_fmt = valid_sample_fmt;
+	if (source->ops->on_audio_format_set)
+		return source->ops->on_audio_format_set(source);
+	return 0;
+}
+
+int source_set_rate(struct sof_source __sparse_cache *source, unsigned int rate)
+{
+	source->audio_stream_params->rate = rate;
+	if (source->ops->on_audio_format_set)
+		return source->ops->on_audio_format_set(source);
+	return 0;
+}
+
+int source_set_channels(struct sof_source __sparse_cache *source, unsigned int channels)
+{
+	source->audio_stream_params->channels = channels;
+	if (source->ops->on_audio_format_set)
+		return source->ops->on_audio_format_set(source);
+	return 0;
+}
+
+int source_set_buffer_fmt(struct sof_source __sparse_cache *source, uint32_t buffer_fmt)
+{
+	source->audio_stream_params->buffer_fmt = buffer_fmt;
+	if (source->ops->on_audio_format_set)
+		return source->ops->on_audio_format_set(source);
+	return 0;
+}
+
+int source_set_underrun(struct sof_source __sparse_cache *source, bool underrun_permitted)
+{
+	source->audio_stream_params->underrun_permitted = underrun_permitted;
+	if (source->ops->on_audio_format_set)
+		return source->ops->on_audio_format_set(source);
+	return 0;
+}
+
+size_t source_get_frame_bytes(struct sof_source __sparse_cache *source)
+{
+	return get_frame_bytes(source_get_frm_fmt(source),
+				source_get_channels(source));
+}
+
+size_t source_get_data_frames_available(struct sof_source __sparse_cache *source)
+{
+	return source_get_data_available(source) /
+			source_get_frame_bytes(source);
+}
+
+int source_set_params(struct sof_source __sparse_cache *source,
+		      struct sof_ipc_stream_params *params, bool force_update)
+{
+	if (source->ops->audio_set_ipc_params)
+		return source->ops->audio_set_ipc_params(source, params, force_update);
+	return 0;
+}
+
+int source_set_alignment_constants(struct sof_source __sparse_cache *source,
+				   const uint32_t byte_align,
+				   const uint32_t frame_align_req)
+{
+	if (source->ops->set_alignment_constants)
+		return source->ops->set_alignment_constants(source, byte_align, frame_align_req);
+	return 0;
+}
