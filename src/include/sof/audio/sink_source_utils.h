305f8a2088aa4bcd1a2d8db7d9b652155e0291a6
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-04-17 15:41:00 +0200

    pipeline2.0: introduce sink/source utilities
    There are many operations on sink/source that may be put into a
    common library. This is the place for it.
    
    Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

---
+++ src/include/sof/audio/sink_source_utils.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2023 Intel Corporation. All rights reserved.
+ *
+ */
+
+#ifndef SINK_SOURCE_UTILS_H
+#define SINK_SOURCE_UTILS_H
+
+#include <sof/audio/sink_api.h>
+#include <sof/audio/source_api.h>
+
+/**
+ * copy bytes from source to sink
+ *
+ * @param source the data source
+ * @param sink the data target
+ * @param free if true, data from source will be freed
+ *	       if false, data will remain in the source
+ * @param size number of bytes to be copied
+ */
+int source_to_sink_copy(struct sof_source __sparse_cache *source,
+			struct sof_sink __sparse_cache *sink, bool free, size_t size);
+
+#endif /* SINK_SOURCE_UTILS_H */
