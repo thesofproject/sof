b11045c2f1cca3b36b967b3f5905435c8744d0cc
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-04-17 13:28:54 +0200

    pipeline2.0: introduce a sink API
    this is a definition of API to sink of audio data
    
    THE SINK is any component that can store data somehow
    and provide a buffer to be filled with data at request.
    The sink API does not define how the data will be processed/used
    
     The user - a module - sees this API as a destination
                it must send data to
     The IMPLEMENTATION - audio_stream, DP Queue -
                sees this as a producer that PROVIDES data for processing
    
    Examples of components that should expose SINK api
     - /dev/null
         all the data stored in sink buffer are just simply discarded
     - I2S sender
         Data stored in sink buffer will be sent to the external world
     - a memory ring buffer
         data stored in the buffer will be sent to another module
         (usually using source API, but it does not matter in fact).
    
    Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

---
+++ src/include/sof/audio/sink_api_implementation.h
@@ -0,0 +1,91 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2023 Intel Corporation. All rights reserved.
+ *
+ */
+
+#ifndef __SOF_SINK_API_IMPLEMENTATION_H__
+#define __SOF_SINK_API_IMPLEMENTATION_H__
+
+#include <sof/common.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+/* forward def */
+struct sof_audio_stream_params;
+
+/**
+ * this is a definition of internals of sink API
+ *
+ * this file should be included by the implementations of sink API
+ *
+ * The clients of stream API should use functions provided in sink_api.h ONLY
+ *
+ */
+
+struct sink_ops {
+	/**
+	 * see comment of sink_get_free_size()
+	 */
+	size_t (*get_free_size)(struct sof_sink __sparse_cache *sink);
+
+	/**
+	 * see comment of sink_get_buffer()
+	 */
+	int (*get_buffer)(struct sof_sink __sparse_cache *sink, size_t req_size,
+			  void **data_ptr, void **buffer_start, size_t *buffer_size);
+
+	/**
+	 * see comment of sink_commit_buffer()
+	 */
+	int (*commit_buffer)(struct sof_sink __sparse_cache *sink, size_t commit_size);
+
+	/**
+	 * OPTIONAL: Notification to the sink implementation about changes in audio format
+	 *
+	 * Once any of *audio_stream_params elements changes, the implementation of
+	 * sink may need to perform some extra operations.
+	 * This callback will be called immediately after any change
+	 *
+	 * @retval 0 if success, negative if new parameters are not supported
+	 */
+	int (*on_audio_format_set)(struct sof_sink __sparse_cache *sink);
+
+	/**
+	 * OPTIONAL
+	 * see sink_set_params comments
+	 */
+	int (*audio_set_ipc_params)(struct sof_sink __sparse_cache *sink,
+				    struct sof_ipc_stream_params *params, bool force_update);
+
+	/**
+	 * OPTIONAL
+	 * see comment for sink_set_alignment_constants
+	 */
+	int (*set_alignment_constants)(struct sof_sink __sparse_cache *sink,
+				       const uint32_t byte_align,
+				       const uint32_t frame_align_req);
+};
+
+/** internals of sink API. NOT TO BE MODIFIED OUTSIDE OF sink_api_helper.h */
+struct sof_sink {
+	const struct sink_ops *ops;	/** operations interface */
+	size_t requested_write_frag_size; /** keeps number of bytes requested by get_buffer() */
+	size_t num_of_bytes_processed; /** processed bytes counter */
+	struct sof_audio_stream_params *audio_stream_params; /** pointer to audio params */
+};
+
+/**
+ * Init of the API, must be called before any operation
+ *
+ * @param sink pointer to the structure
+ * @param ops pointer to API operations
+ * @param audio_stream_params pointer to structure with audio parameters
+ *	  note that the audio_stream_params must be accessible by the caller core
+ *	  the implementation must ensure coherent access to the parameteres
+ *	  in case of i.e. cross core shared queue, it must be located in non-cached memory
+ */
+void sink_init(struct sof_sink __sparse_cache *sink, const struct sink_ops *ops,
+	       struct sof_audio_stream_params *audio_stream_params);
+
+#endif /* __SOF_SINK_API_IMPLEMENTATION_H__ */
