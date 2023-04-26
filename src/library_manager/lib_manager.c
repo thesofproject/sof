thesofproject/sof

commit 32e4f7e27a57af3f7202ea6d5267e53f2e4bf2ee

buffer: use an accessor to read struct audio_stream::end_ptr
This patch adds audio_stream_get_end_ptr() to read
struct audio_stream::end_addr and converts all users. Apart from
hiding internals of the structure, it also enforces the use of the
__sparse_cache annotation.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/library_manager/lib_manager.c
@@ -411,7 +411,7 @@ static int lib_manager_dma_buffer_init(struct lib_manager_dma_buf *buffer, uint3
 	dcache_writeback_region((__sparse_force void __sparse_cache *)buffer->addr, size);
 
 	tr_dbg(&lib_manager_tr, "lib_manager_dma_buffer_init(): %#lx, %#lx",
-	       buffer->addr, buffer->end_addr);
+	       buffer->addr, audio_stream_get_end_addr(buffer));
 
 	return 0;
 }

src/audio/asrc/asrc.c
@@ -147,14 +147,14 @@ static void src_copy_s32(struct comp_dev *dev,
 	buf = (int32_t *)cd->ibuf[0];
 	n = cd->source_frames * source->channels;
 	while (n > 0) {
-		n_wrap_src = (int32_t *)source->end_addr - src;
+		n_wrap_src = (int32_t *)audio_stream_get_end_addr(source) - src;
 		n_copy = (n < n_wrap_src) ? n : n_wrap_src;
 		for (i = 0; i < n_copy; i++)
 			*buf++ = (*src++) << cd->data_shift;
 
 		/* Update and check both source and destination for wrap */
 		n -= n_copy;
-		src_inc_wrap(&src, source->end_addr, source->size);
+		src_inc_wrap(&src, audio_stream_get_end_addr(source), source->size);
 	}
 
 	/* Run ASRC */
@@ -177,14 +177,14 @@ static void src_copy_s32(struct comp_dev *dev,
 	buf = (int32_t *)cd->obuf[0];
 	n = out_frames * sink->channels;
 	while (n > 0) {
-		n_wrap_snk = (int32_t *)sink->end_addr - snk;
+		n_wrap_snk = (int32_t *)audio_stream_get_end_addr(sink) - snk;
 		n_copy = (n < n_wrap_snk) ? n : n_wrap_snk;
 		for (i = 0; i < n_copy; i++)
 			*snk++ = (*buf++) >> cd->data_shift;
 
 		/* Update and check both source and destination for wrap */
 		n -= n_copy;
-		src_inc_wrap(&snk, sink->end_addr, sink->size);
+		src_inc_wrap(&snk, audio_stream_get_end_addr(sink), sink->size);
 	}
 
 	*n_read = in_frames;
@@ -216,7 +216,7 @@ static void src_copy_s16(struct comp_dev *dev,
 	buf = (int16_t *)cd->ibuf[0];
 	n = cd->source_frames * source->channels;
 	while (n > 0) {
-		n_wrap_src = (int16_t *)source->end_addr - src;
+		n_wrap_src = (int16_t *)audio_stream_get_end_addr(source) - src;
 		n_copy = (n < n_wrap_src) ? n : n_wrap_src;
 		s_copy = n_copy * sizeof(int16_t);
 		ret = memcpy_s(buf, s_copy, src, s_copy);
@@ -226,7 +226,7 @@ static void src_copy_s16(struct comp_dev *dev,
 		n -= n_copy;
 		src += n_copy;
 		buf += n_copy;
-		src_inc_wrap_s16(&src, source->end_addr, source->size);
+		src_inc_wrap_s16(&src, audio_stream_get_end_addr(source), source->size);
 	}
 
 	/* Run ASRC */
@@ -250,7 +250,7 @@ static void src_copy_s16(struct comp_dev *dev,
 	buf = (int16_t *)cd->obuf[0];
 	n = out_frames * sink->channels;
 	while (n > 0) {
-		n_wrap_snk = (int16_t *)sink->end_addr - snk;
+		n_wrap_snk = (int16_t *)audio_stream_get_end_addr(sink) - snk;
 		n_copy = (n < n_wrap_snk) ? n : n_wrap_snk;
 		s_copy = n_copy * sizeof(int16_t);
 		ret = memcpy_s(snk, s_copy, buf, s_copy);
@@ -260,7 +260,7 @@ static void src_copy_s16(struct comp_dev *dev,
 		n -= n_copy;
 		snk += n_copy;
 		buf += n_copy;
-		src_inc_wrap_s16(&snk, sink->end_addr, sink->size);
+		src_inc_wrap_s16(&snk, audio_stream_get_end_addr(sink), sink->size);
 	}
 
 	*n_read = in_frames;

src/audio/copier/copier.c
@@ -794,7 +794,7 @@ static void copy_single_channel_c16(struct audio_stream __sparse_cache *dst,
 
 		src_samples_without_wrap = audio_stream_samples_without_wrap_s16(src, r_ptr);
 		r_end_ptr = src_stream_sample_count < src_samples_without_wrap ?
-			r_ptr + src_stream_sample_count : (int16_t *)src->end_addr;
+			r_ptr + src_stream_sample_count : (int16_t *)audio_stream_get_end_addr(src);
 
 		r_ptr_before_loop = r_ptr;
 
@@ -802,7 +802,7 @@ static void copy_single_channel_c16(struct audio_stream __sparse_cache *dst,
 			*w_ptr = *r_ptr;
 			r_ptr += src->channels;
 			w_ptr += dst->channels;
-		} while (r_ptr < r_end_ptr && w_ptr < (int16_t *)dst->end_addr);
+		} while (r_ptr < r_end_ptr && w_ptr < (int16_t *)audio_stream_get_end_addr(dst));
 
 		src_stream_sample_count -= r_ptr - r_ptr_before_loop;
 	}
@@ -833,7 +833,7 @@ static void copy_single_channel_c32(struct audio_stream __sparse_cache *dst,
 
 		src_samples_without_wrap = audio_stream_samples_without_wrap_s32(src, r_ptr);
 		r_end_ptr = src_stream_sample_count < src_samples_without_wrap ?
-			r_ptr + src_stream_sample_count : (int32_t *)src->end_addr;
+			r_ptr + src_stream_sample_count : (int32_t *)audio_stream_get_end_addr(src);
 
 		r_ptr_before_loop = r_ptr;
 
@@ -841,7 +841,7 @@ static void copy_single_channel_c32(struct audio_stream __sparse_cache *dst,
 			*w_ptr = *r_ptr;
 			r_ptr += src->channels;
 			w_ptr += dst->channels;
-		} while (r_ptr < r_end_ptr && w_ptr < (int32_t *)dst->end_addr);
+		} while (r_ptr < r_end_ptr && w_ptr < (int32_t *)audio_stream_get_end_addr(dst));
 
 		src_stream_sample_count -= r_ptr - r_ptr_before_loop;
 	}

src/audio/dcblock/dcblock_hifi3.c
@@ -33,7 +33,7 @@ static inline void dcblock_set_circular(const struct audio_stream __sparse_cache
 {
 	/* Set source as circular buffer 0 */
 	AE_SETCBEGIN0(source->addr);
-	AE_SETCEND0(source->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(source));
 }
 
 #if CONFIG_FORMAT_S16LE

src/audio/dcblock/dcblock_hifi4.c
@@ -34,11 +34,11 @@ static inline void dcblock_set_circular(const struct audio_stream __sparse_cache
 {
 	/* Set source as circular buffer 0 */
 	AE_SETCBEGIN0(source->addr);
-	AE_SETCEND0(source->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(source));
 
 	/* Set sink as circular buffer 1 */
 	AE_SETCBEGIN1(sink->addr);
-	AE_SETCEND1(sink->end_addr);
+	AE_SETCEND1(audio_stream_get_end_addr(sink));
 }
 
 #if CONFIG_FORMAT_S16LE

src/audio/google/google_hotword_detect.c
@@ -408,12 +408,13 @@ static int ghd_copy(struct comp_dev *dev)
 	comp_dbg(dev, "buffer begin/r_ptr/end [0x%x 0x%x 0x%x]",
 		 (uint32_t)stream->addr,
 		 (uint32_t)audio_stream_get_rptr(stream),
-		 (uint32_t)stream->end_addr);
+		 (uint32_t)audio_stream_get_end_addr(stream));
 
 	/* copy and perform detection */
 	buffer_stream_invalidate(source_c, bytes);
 
-	tail_bytes = (char *)stream->end_addr - (char *)audio_stream_get_rptr(stream);
+	tail_bytes = (char *)audio_stream_get_end_addr(stream) -
+		(char *)audio_stream_get_rptr(stream);
 	if (bytes <= tail_bytes)
 		tail_bytes = bytes;
 	else

src/audio/kpb.c
@@ -941,7 +941,7 @@ static void kpb_micselect_copy16(struct comp_buffer __sparse_cache *sink,
 	size_t i;
 
 	AE_SETCBEGIN0(ostream->addr);
-	AE_SETCEND0(ostream->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(ostream));
 
 	buffer_stream_invalidate(source, size);
 	const ae_int16 *in_ptr = audio_stream_get_rptr(istream);
@@ -974,7 +974,7 @@ static void kpb_micselect_copy32(struct comp_buffer __sparse_cache *sink,
 	size_t i;
 
 	AE_SETCBEGIN0(ostream->addr);
-	AE_SETCEND0(ostream->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(ostream));
 
 	buffer_stream_invalidate(source, size);
 
@@ -1020,7 +1020,7 @@ static void kpb_micselect_copy16(struct comp_buffer __sparse_cache *sink,
 
 		for (size_t i = 0; i < samples_per_chan * in_channels; i += in_channels) {
 			if (&out_data[out_samples + ch]
-					>= (int16_t *)ostream->end_addr) {
+					>= (int16_t *)audio_stream_get_end_addr(ostream)) {
 				out_data = (int16_t *)ostream->addr;
 				out_samples = 0;
 			}
@@ -1051,7 +1051,7 @@ static void kpb_micselect_copy32(struct comp_buffer __sparse_cache *sink,
 
 		for (size_t i = 0; i < samples_per_chan * in_channels; i += in_channels) {
 			if (&out_data[out_samples + ch]
-					>= (int32_t *)ostream->end_addr) {
+					>= (int32_t *)audio_stream_get_end_addr(ostream)) {
 				out_data = (int32_t *)ostream->addr;
 				out_samples = 0;
 			}
@@ -1829,7 +1829,7 @@ static void kpb_convert_24b_to_32b(const void *linear_source, int ioffset,
 	ae_int32x2 *buf;
 
 	buf = (ae_int32x2 *)(sink->addr);
-	buf_end = (ae_int32x2 *)(sink->end_addr);
+	buf_end = audio_stream_get_end_addr(sink);
 	ae_int32x2 *out_ptr = (ae_int32x2 *)buf;
 
 	AE_SETCBEGIN0(buf);

src/audio/mixin_mixout/mixin_mixout_hifi3.c
@@ -136,7 +136,7 @@ static void remap_mix_channel_s16(struct audio_stream __sparse_cache *sink,
 
 	/* set source as circular buffer, hifi3 only have 1 circular buffer*/
 	AE_SETCBEGIN0(source->addr);
-	AE_SETCEND0(source->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(source));
 
 	for (left_frames = frames_to_mix; left_frames > 0; left_frames -= frames) {
 		/* audio_stream_wrap() is required and is done below in a loop */
@@ -198,7 +198,7 @@ static void mute_channel_s16(struct audio_stream __sparse_cache *stream, int32_t
 	frame_count -= skip_mixed_frames;
 
 	AE_SETCBEGIN0(stream->addr);
-	AE_SETCEND0(stream->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(stream));
 
 	/* audio_stream_wrap() is needed here and it is just below in a loop */
 	ptr = (ae_int16 *)audio_stream_get_wptr(stream) + mixed_frames * stream->channels +
@@ -328,7 +328,7 @@ static void remap_mix_channel_s24(struct audio_stream __sparse_cache *sink,
 	AE_L16_IP(gain_v, pgain, 0);
 	/* set source as circular buffer, hifi3 only have 1 circular buffer*/
 	AE_SETCBEGIN0(source->addr);
-	AE_SETCEND0(source->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(source));
 
 	for (left_frames = frames_to_mix; left_frames > 0; left_frames -= frames) {
 		dst = audio_stream_wrap(sink, dst);
@@ -500,7 +500,7 @@ static void remap_mix_channel_s32(struct audio_stream __sparse_cache *sink,
 	AE_L16_IP(gain_v, pgain, 0);
 	/* set source as circular buffer, hifi3 only have 1 circular buffer*/
 	AE_SETCBEGIN0(source->addr);
-	AE_SETCEND0(source->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(source));
 	src = audio_stream_wrap(source, src);
 
 	for (left_frames = frames_to_mix; left_frames > 0; left_frames -= frames) {
@@ -564,7 +564,7 @@ static void mute_channel_s32(struct audio_stream __sparse_cache *stream, int32_t
 	frame_count -= skip_mixed_frames;
 
 	AE_SETCBEGIN0(stream->addr);
-	AE_SETCEND0(stream->end_addr);
+	AE_SETCEND0(audio_stream_get_end_addr(stream));
 
 	/* audio_stream_wrap() is needed here and it is just below in a loop */
 	ptr = (ae_int32 *)audio_stream_get_wptr(stream) + mixed_frames * stream->channels +

commit 4ea0393ecd929ee68bb1ce3de46b20189e4e1fd0

lib-manager: fix compilation breakage
Fix breakage introduced by 32e4f7e27a57 ("buffer: use an accessor to
read struct audio_stream::end_ptr") which accidentally wrongly used
audio_stream_get_end_addr() to access the .end_addr member of
struct lib_manager_dma_buf.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/library_manager/lib_manager.c
@@ -411,7 +411,7 @@ static int lib_manager_dma_buffer_init(struct lib_manager_dma_buf *buffer, uint3
 	dcache_writeback_region((__sparse_force void __sparse_cache *)buffer->addr, size);
 
 	tr_dbg(&lib_manager_tr, "lib_manager_dma_buffer_init(): %#lx, %#lx",
-	       buffer->addr, audio_stream_get_end_addr(buffer));
+	       buffer->addr, buffer->end_addr);
 
 	return 0;
 }