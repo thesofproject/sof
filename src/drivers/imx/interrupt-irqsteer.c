480b24e8eae643beafc66b5957fe1140c29b80a8
Paul Olaru <paul.olaru@nxp.com>
2023-04-28 13:12:39 +0300

    drivers: imx: interrupt: Use Zephyr wrapper definitions
    For the functions platform_interrupt_clear and platform_interrupt_set,
    the existing definitions based on arch_interrupt_* do not compile with
    the xt-clang 2023 toolchain for imx. Use the Zephyr wrapper
    implementations for those for now.
    
    Signed-off-by: Paul Olaru <paul.olaru@nxp.com>

--- src/drivers/imx/interrupt-irqsteer.c
+++ src/drivers/imx/interrupt-irqsteer.c
@@ -451,6 +451,7 @@ void platform_interrupt_init(void)
 		interrupt_cascade_register(dsp_irq + i);
 }
 
+#ifndef __ZEPHYR__
 void platform_interrupt_set(uint32_t irq)
 {
 	if (interrupt_is_dsp_direct(irq))
@@ -462,6 +463,7 @@ void platform_interrupt_clear(uint32_t irq, uint32_t mask)
 	if (interrupt_is_dsp_direct(irq))
 		arch_interrupt_clear(irq);
 }
+#endif /* __ZEPHYR */
 
 uint32_t platform_interrupt_get_enabled(void)
 {
