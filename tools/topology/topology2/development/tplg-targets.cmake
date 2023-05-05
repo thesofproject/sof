8560c9ccbaef36a1ba47e09c2aa9cef44833dc9c
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2023-05-05 13:57:11 -0700

    topology2: cavs-nocodec: Build LNL nocodec topology
    Build LNL nocodec topology with simple copier-copier pipelines.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- tools/topology/topology2/development/tplg-targets.cmake
+++ tools/topology/topology2/development/tplg-targets.cmake
@@ -37,6 +37,11 @@ DEEPBUFFER_D0I3_COMPATIBLE=true"
 "cavs-nocodec-multicore\;sof-mtl-nocodec-multicore\;PLATFORM=mtl,SSP1_ENABLED=true,SSP0_CORE_ID=0,\
 SSP1_CORE_ID=1,SSP2_CORE_ID=2,PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-mtl-nocodec.bin"
 
+# SSP topology for LNL FPGA with lower DMIC IO clock of 19.2MHz
+"cavs-nocodec\;sof-lnl-nocodec-fpga\;PLATFORM=lnl,NUM_DMICS=4,PDM1_MIC_A_ENABLE=1,\
+PDM1_MIC_B_ENABLE=1,PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-lnl-nocodec-fpga.bin,\
+PASSTHROUGH=true,DMIC_IO_CLK=19200000"
+
 # CAVS HDA topology with mixer-based efx eq pipelines for HDA and passthrough pipelines for HDMI
 "sof-hda-generic\;sof-hda-efx-generic\;HDA_CONFIG=efx,USE_CHAIN_DMA=true,DEEPBUFFER_FW_DMA_MS=100,\
 EFX_FIR_PARAMS=passthrough,EFX_IIR_PARAMS=passthrough"
