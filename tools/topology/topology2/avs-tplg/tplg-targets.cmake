f654d27c7d2ec5180f07cb941c279088c932c9e2
Jaska Uimonen <jaska.uimonen@intel.com>
2023-05-16 10:40:47 +0300

    topology2: move bt nocodecs to development
    Move bluetooth nocodec topology to development and add target
    for mtl.
    
    Signed-off-by: Jaska Uimonen <jaska.uimonen@intel.com>
    (cherry picked from commit a2944ddc51600f210affb0852d571f77acdd27ba)

--- tools/topology/topology2/avs-tplg/tplg-targets.cmake
+++ tools/topology/topology2/avs-tplg/tplg-targets.cmake
@@ -38,11 +38,4 @@ PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-tgl-rt711-rt1308-4ch.bin"
 "cavs-sdw\;sof-adl-rt711-4ch\;DEEPBUFFER_FW_DMA_MS=100,NUM_DMICS=4,DMIC0_ID=2,\
 DMIC1_ID=3,PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,HDMI1_ID=4,HDMI2_ID=5,\
 HDMI3_ID=6,PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-adl-rt711-4ch.bin"
-
-# BT offload
-"cavs-nocodec-bt\;sof-nocodec-bt\;PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-nocodec-bt.bin,\
-PLATFORM=tgl"
-# BT offload loopback test topology (lbm)
-"cavs-nocodec-bt\;sof-nocodec-bt-lbm\;BT_LOOPBACK_MODE=true,PLATFORM=tgl,\
-PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-nocodec-bt-lbm.bin"
 )
