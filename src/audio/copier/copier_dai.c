63b1264a7471f2457b5b67dc3f0457b96bc41082
Baofeng Tian <baofeng.tian@intel.com>
2023-05-17 14:26:27 +0800

    copier-dai: create new file for dai specific code
    Move copier.c dai specific code to copier_dai.c
    
    Signed-off-by: Baofeng Tian <baofeng.tian@intel.com>

---
+++ src/audio/copier/copier_dai.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright 2023 Intel Corporation. All rights reserved.
+//
+// Author: Baofeng Tian <baofeng.tian@intel.com>
+
+#include <sof/trace/trace.h>
+
+LOG_MODULE_DECLARE(copier, CONFIG_SOF_LOG_LEVEL);
