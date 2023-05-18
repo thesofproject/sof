bf6b875bd022c19ddf5749855b09ffcdde317c99
Baofeng Tian <baofeng.tian@intel.com>
2023-05-18 14:25:09 +0800

    ipcgtw: merge ipcgtw.h to ipcgtw_copier.h header file
    No need to keep separate ipcgtw.h header file, merge it to
    ipcgtw_copier.h.
    
    Signed-off-by: Baofeng Tian <baofeng.tian@intel.com>

--- src/include/sof/audio/ipcgtw_copier.h
+++ src/include/sof/audio/ipcgtw_copier.h
@@ -18,6 +18,7 @@
 #include <ipc4/gateway.h>
 #include <sof/list.h>
 #include <ipc/stream.h>
+#include <ipc4/copier.h>
 
 /* Host communicates with IPC gateways via global IPC messages. To address a particular
  * IPC gateway, its node_id is sent in message payload. Hence we need to keep a list of existing
@@ -33,6 +34,66 @@ struct ipcgtw_data {
 	 */
 	uint32_t buf_size;
 };
+
+/**< IPC header format for IPC gateway messages */
+struct ipc4_ipcgtw_cmd {
+	union  {
+		uint32_t dat;
+
+		struct {
+			/**< Command, see below */
+			uint32_t cmd : 24;
+
+			/**< One of Global::Type */
+			uint32_t type : 5;
+
+			/**< Msg::MSG_REQUEST */
+			uint32_t rsp : 1;
+
+			/**< Msg::FW_GEN_MSG */
+			uint32_t msg_tgt : 1;
+
+			uint32_t _reserved_0 : 1;
+		} r;
+	} primary;
+	union  {
+		uint32_t dat;
+		struct {
+			uint32_t data_size : 30;
+			uint32_t _reserved_0 : 2;
+		} r;
+	} extension;
+} __packed __aligned(4);
+
+/**< Values of ipc4_ipcgtw_cmd::primary.r.cmd */
+enum {
+	IPC4_IPCGWCMD_GET_DATA = 1,
+	IPC4_IPCGWCMD_SET_DATA = 2,
+	IPC4_IPCGWCMD_FLUSH_DATA = 3
+};
+
+/* Incoming IPC gateway message */
+struct ipc4_ipc_gateway_cmd_data {
+	/* node_id of the target gateway */
+	union ipc4_connector_node_id node_id;
+	/* Payload (actual size is in the header extension.r.data_size) */
+	uint8_t payload[];
+};
+
+/* Reply to IPC gateway message */
+struct ipc4_ipc_gateway_cmd_data_reply {
+	union {
+		uint32_t size_avail;    /* Reply for IPC4_IPCGWCMD_GET_DATA */
+		uint32_t size_consumed; /* Reply for IPC4_IPCGWCMD_SET_DATA */
+	} u;
+	/* Total reply size is returned in reply header extension.r.data_size.
+	 * This payload size if 4 bytes smaller (size of the union above).
+	 */
+	uint8_t payload[];
+};
+
+int copier_ipcgtw_process(const struct ipc4_ipcgtw_cmd *cmd,
+			  void *reply_payload, uint32_t *reply_payload_size);
 
 int copier_ipcgtw_params(struct ipcgtw_data *ipcgtw_data, struct comp_dev *dev,
 			 struct sof_ipc_stream_params *params);
