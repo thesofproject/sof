fc5e117f68ecc13b6c2a8e8762120c67f13d26f8
Andrula Song <andrula.song@intel.com>
2023-06-07 09:52:03 +0800

    Audio: Volume: Add passthrough functions
    Add passthrough functions implementation. If the gain of all
    channels equal 0dB, then we use passthrough functions to process
    the volume component.
    
    Signed-off-by: Andrula Song <andrula.song@intel.com>

--- src/audio/module_adapter/module/volume/volume_generic.c
+++ src/audio/module_adapter/module/volume/volume_generic.c
@@ -93,6 +93,47 @@ static void vol_s24_to_s24(struct processing_module *mod, struct input_stream_bu
 		y = audio_stream_wrap(sink, y + n);
 	}
 }
+
+/**
+ * \brief Volume passthrough from 24/32 bit to 24/32 bit.
+ * \param[in,out] dev Volume base component device.
+ * \param[in,out] sink Destination buffer.
+ * \param[in,out] source Input buffer.
+ * \param[in] frames Number of frames to process.
+ * \param[in] attenuation factor for peakmeter adjustment (unused)
+ *
+ * Copy and scale volume from 24/32 bit source buffer
+ * to 24/32 bit destination buffer.
+ */
+static void vol_passthrough_s24_to_s24(struct processing_module *mod,
+				       struct input_stream_buffer *bsource,
+				       struct output_stream_buffer *bsink, uint32_t frames,
+				       uint32_t attenuation)
+{
+	struct audio_stream __sparse_cache *source = bsource->data;
+	struct audio_stream __sparse_cache *sink = bsink->data;
+	int32_t *x;
+	int32_t *y;
+	int nmax, n;
+	const int nch = audio_stream_get_channels(source);
+	int remaining_samples = frames * nch;
+
+	x = audio_stream_wrap(source, (char *)audio_stream_get_rptr(source) + bsource->consumed);
+	y = audio_stream_wrap(sink, (char *)audio_stream_get_wptr(sink) + bsink->size);
+
+	bsource->consumed += VOL_S32_SAMPLES_TO_BYTES(remaining_samples);
+	bsink->size += VOL_S32_SAMPLES_TO_BYTES(remaining_samples);
+	while (remaining_samples) {
+		nmax = audio_stream_samples_without_wrap_s24(source, x);
+		n = MIN(remaining_samples, nmax);
+		nmax = audio_stream_samples_without_wrap_s24(sink, y);
+		n = MIN(n, nmax);
+		memcpy_s(y, n * sizeof(int32_t), x, n * sizeof(int32_t));
+		remaining_samples -= n;
+		x = audio_stream_wrap(source, x + n);
+		y = audio_stream_wrap(sink, y + n);
+	}
+}
 #endif /* CONFIG_FORMAT_S24LE */
 
 #if CONFIG_FORMAT_S32LE
@@ -142,6 +183,46 @@ static void vol_s32_to_s32(struct processing_module *mod, struct input_stream_bu
 							   Q_SHIFT_BITS_64(31, VOL_QXY_Y, 31));
 			}
 		}
+		remaining_samples -= n;
+		x = audio_stream_wrap(source, x + n);
+		y = audio_stream_wrap(sink, y + n);
+	}
+}
+
+/**
+ * \brief Volume passthrough from 32 bit to 32 bit.
+ * \param[in,out] dev Volume base component device.
+ * \param[in,out] sink Destination buffer.
+ * \param[in,out] source Input buffer.
+ * \param[in] frames Number of frames to process.
+ * \param[in] attenuation factor for peakmeter adjustment (unused)
+ *
+ * Copy and scale volume from 32 bit source buffer
+ * to 32 bit destination buffer.
+ */
+static void vol_passthrough_s32_to_s32(struct processing_module *mod,
+				       struct input_stream_buffer *bsource,
+				       struct output_stream_buffer *bsink, uint32_t frames,
+				       uint32_t attenuation)
+{
+	struct audio_stream __sparse_cache *source = bsource->data;
+	struct audio_stream __sparse_cache *sink = bsink->data;
+	int32_t *x;
+	int32_t *y;
+	int nmax, n;
+	const int nch = audio_stream_get_channels(source);
+	int remaining_samples = frames * nch;
+
+	x = audio_stream_wrap(source, (char *)audio_stream_get_rptr(source) + bsource->consumed);
+	y = audio_stream_wrap(sink, (char *)audio_stream_get_wptr(sink) + bsink->size);
+	bsource->consumed += VOL_S32_SAMPLES_TO_BYTES(remaining_samples);
+	bsink->size += VOL_S32_SAMPLES_TO_BYTES(remaining_samples);
+	while (remaining_samples) {
+		nmax = audio_stream_samples_without_wrap_s32(source, x);
+		n = MIN(remaining_samples, nmax);
+		nmax = audio_stream_samples_without_wrap_s32(sink, y);
+		n = MIN(n, nmax);
+		memcpy_s(y, n * sizeof(int32_t), x, n * sizeof(int32_t));
 		remaining_samples -= n;
 		x = audio_stream_wrap(source, x + n);
 		y = audio_stream_wrap(sink, y + n);
@@ -198,18 +279,58 @@ static void vol_s16_to_s16(struct processing_module *mod, struct input_stream_bu
 		y = audio_stream_wrap(sink, y + n);
 	}
 }
+
+/**
+ * \brief Volume passthrough from 16 bit to 16 bit.
+ * \param[in,out] dev Volume base component device.
+ * \param[in,out] sink Destination buffer.
+ * \param[in,out] source Input buffer.
+ * \param[in] frames Number of frames to process.
+ * \param[in] attenuation factor for peakmeter adjustment (unused)
+ *
+ * Copy and scale volume from 16 bit source buffer
+ * to 16 bit destination buffer.
+ */
+static void vol_passthrough_s16_to_s16(struct processing_module *mod,
+				       struct input_stream_buffer *bsource,
+				       struct output_stream_buffer *bsink, uint32_t frames,
+				       uint32_t attenuation)
+{
+	struct audio_stream __sparse_cache *source = bsource->data;
+	struct audio_stream __sparse_cache *sink = bsink->data;
+	int16_t *x;
+	int16_t *y;
+	int nmax, n;
+	const int nch = audio_stream_get_channels(source);
+	int remaining_samples = frames * nch;
+
+	x = audio_stream_wrap(source, (char *)audio_stream_get_rptr(source) + bsource->consumed);
+	y = audio_stream_wrap(sink, (char *)audio_stream_get_wptr(sink) + bsink->size);
+	bsource->consumed += VOL_S16_SAMPLES_TO_BYTES(remaining_samples);
+	bsink->size += VOL_S16_SAMPLES_TO_BYTES(remaining_samples);
+	while (remaining_samples) {
+		nmax = audio_stream_samples_without_wrap_s16(source, x);
+		n = MIN(remaining_samples, nmax);
+		nmax = audio_stream_samples_without_wrap_s16(sink, y);
+		n = MIN(n, nmax);
+		memcpy_s(y, n * sizeof(int16_t), x, n * sizeof(int16_t));
+		remaining_samples -= n;
+		x = audio_stream_wrap(source, x + n);
+		y = audio_stream_wrap(sink, y + n);
+	}
+}
 #endif /* CONFIG_FORMAT_S16LE */
 
 const struct comp_func_map volume_func_map[] = {
 #if CONFIG_FORMAT_S16LE
-	{ SOF_IPC_FRAME_S16_LE, vol_s16_to_s16 },
-#endif /* CONFIG_FORMAT_S16LE */
+	{ SOF_IPC_FRAME_S16_LE, vol_s16_to_s16, vol_passthrough_s16_to_s16},
+#endif
 #if CONFIG_FORMAT_S24LE
-	{ SOF_IPC_FRAME_S24_4LE, vol_s24_to_s24 },
-#endif /* CONFIG_FORMAT_S24LE */
+	{ SOF_IPC_FRAME_S24_4LE, vol_s24_to_s24, vol_passthrough_s24_to_s24},
+#endif
 #if CONFIG_FORMAT_S32LE
-	{ SOF_IPC_FRAME_S32_LE, vol_s32_to_s32 },
-#endif /* CONFIG_FORMAT_S32LE */
+	{ SOF_IPC_FRAME_S32_LE, vol_s32_to_s32, vol_passthrough_s32_to_s32},
+#endif
 };
 
 const size_t volume_func_count = ARRAY_SIZE(volume_func_map);
