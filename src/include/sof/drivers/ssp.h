755a5127054e1156c68462efdad6e16ce07e039a
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2023-06-15 11:43:49 +0300

    intel: ssp: Handle the cases when SSP is not clock provider for FS and/or BCLK
    When SSP is consumer for FS and BCLK then it does not needs MCLK as the
    shifting and rate is derived form the I2S bus directly.
    When SSP provides FS only then the BCLK setup is not needed.
    When SSP provides BCLK only then both MCLK and BCLK setup is needed.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- src/include/sof/drivers/ssp.h
+++ src/include/sof/drivers/ssp.h
@@ -228,11 +228,13 @@ extern const struct dai_driver ssp_driver;
 #define ssp_irq(ssp) \
 	ssp->plat_data.irq
 
-#define SSP_CLK_MCLK_ES_REQ	BIT(0)
-#define SSP_CLK_MCLK_ACTIVE	BIT(1)
-#define SSP_CLK_BCLK_ES_REQ	BIT(2)
-#define SSP_CLK_BCLK_ACTIVE	BIT(3)
+#define SSP_CLK_MCLK_IS_NEEDED	BIT(1)
+#define SSP_CLK_MCLK_ES_REQ	BIT(2)
+#define SSP_CLK_MCLK_ACTIVE	BIT(3)
 #define SSP_CLK_MCLK_AON_REQ	BIT(4)
+#define SSP_CLK_BCLK_IS_NEEDED	BIT(5)
+#define SSP_CLK_BCLK_ES_REQ	BIT(6)
+#define SSP_CLK_BCLK_ACTIVE	BIT(7)
 
 /* SSP private data */
 struct ssp_pdata {
