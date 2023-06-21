thesofproject/sof

commit fa77edf9a47ed186fc30a9692f141a434e26369f

pipeline2.0: change module prepare API to use sink/src.c
Module prepare is an operation that needs to set up sink
and source according to needs.
Therefore it must have access to sink/source handlers
This commit adds handlers to API. In case the module uses
legacy audio stream sink/source pointers will be NULLs
and number of sinks/sources will be zero

Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

src/audio/module_adapter/module_adapter.c
@@ -165,6 +165,55 @@ err:
 	return NULL;
 }
 
+static int module_adapter_sink_src_prepare(struct comp_dev *dev)
+{
+	struct comp_buffer __sparse_cache *source_buffers_c[PLATFORM_MAX_STREAMS];
+	struct comp_buffer __sparse_cache *sinks_buffers_c[PLATFORM_MAX_STREAMS];
+	struct sof_sink __sparse_cache *audio_sink[PLATFORM_MAX_STREAMS];
+	struct sof_source __sparse_cache *audio_src[PLATFORM_MAX_STREAMS];
+	struct processing_module *mod = comp_get_drvdata(dev);
+	struct list_item *blist;
+	uint32_t num_of_sources = 0;
+	uint32_t num_of_sinks = 0;
+	int ret;
+
+	/* acquire all sink and source buffers, get handlers to sink/source API */
+	list_for_item(blist, &dev->bsink_list) {
+		struct comp_buffer *sink_buffer_uc;
+
+		sink_buffer_uc = container_of(blist, struct comp_buffer, source_list);
+		sinks_buffers_c[num_of_sinks] = buffer_acquire(sink_buffer_uc);
+		audio_sink[num_of_sinks] =
+				audio_stream_get_sink(&sinks_buffers_c[num_of_sinks]->stream);
+		sink_reset_num_of_processed_bytes(audio_sink[num_of_sinks]);
+		num_of_sinks++;
+	}
+
+	list_for_item(blist, &dev->bsource_list) {
+		struct comp_buffer *source_buffer_uc;
+
+		source_buffer_uc = container_of(blist, struct comp_buffer, sink_list);
+		source_buffers_c[num_of_sources] = buffer_acquire(source_buffer_uc);
+		audio_src[num_of_sources] =
+				audio_stream_get_source(&source_buffers_c[num_of_sources]->stream);
+		source_reset_num_of_processed_bytes(audio_src[num_of_sources]);
+		num_of_sources++;
+	}
+
+	/* Prepare module */
+	ret = module_prepare(mod, audio_src, num_of_sources, audio_sink, num_of_sinks);
+
+	/* release all source buffers in reverse order */
+	for (int i = num_of_sources - 1; i >= 0; i--)
+		buffer_release(source_buffers_c[i]);
+
+	/* release all sink buffers in reverse order */
+	for  (int i = num_of_sinks - 1; i >= 0 ; i--)
+		buffer_release(sinks_buffers_c[i]);
+
+	return ret;
+}
+
 /*
  * \brief Prepare the module
  * \param[in] dev - component device pointer.
@@ -189,7 +238,11 @@ int module_adapter_prepare(struct comp_dev *dev)
 	comp_dbg(dev, "module_adapter_prepare() start");
 
 	/* Prepare module */
-	ret = module_prepare(mod);
+	if (IS_PROCESSING_MODE_SINK_SOURCE(mod))
+		ret = module_adapter_sink_src_prepare(dev);
+	else
+		ret = module_prepare(mod, NULL, 0, NULL, 0);
+
 	if (ret) {
 		if (ret != PPL_STATUS_PATH_STOP)
 			comp_err(dev, "module_adapter_prepare() error %x: module prepare failed",

src/audio/eq_fir/eq_fir.c
@@ -564,7 +564,9 @@ static void eq_fir_set_alignment(struct audio_stream __sparse_cache *source,
 	audio_stream_init_alignment_constants(byte_align, frame_align_req, sink);
 }
 
-static int eq_fir_prepare(struct processing_module *mod)
+static int eq_fir_prepare(struct processing_module *mod,
+			  struct sof_source __sparse_cache **sources, int num_of_sources,
+			  struct sof_sink __sparse_cache **sinks, int num_of_sinks)
 {
 	struct comp_data *cd = module_get_private_data(mod);
 	struct comp_buffer *sourceb, *sinkb;

src/audio/eq_iir/eq_iir.c
@@ -879,7 +879,9 @@ static void eq_iir_set_passthrough_func(struct comp_data *cd,
 #endif
 }
 
-static int eq_iir_prepare(struct processing_module *mod)
+static int eq_iir_prepare(struct processing_module *mod,
+			  struct sof_source __sparse_cache **sources, int num_of_sources,
+			  struct sof_sink __sparse_cache **sinks, int num_of_sinks)
 {
 	struct comp_data *cd = module_get_private_data(mod);
 	struct comp_buffer *sourceb, *sinkb;

src/audio/mfcc/mfcc.c
@@ -186,7 +186,9 @@ static void mfcc_set_alignment(struct audio_stream *source, struct audio_stream 
 	audio_stream_init_alignment_constants(byte_align, frame_align_req, sink);
 }
 
-static int mfcc_prepare(struct processing_module *mod)
+static int mfcc_prepare(struct processing_module *mod,
+			struct sof_source __sparse_cache **sources, int num_of_sources,
+			struct sof_sink __sparse_cache **sinks, int num_of_sinks)
 {
 	struct mfcc_comp_data *cd = module_get_private_data(mod);
 	struct comp_buffer *sourceb;

src/audio/mixer/mixer.c
@@ -217,7 +217,9 @@ static inline void mixer_set_frame_alignment(struct audio_stream __sparse_cache 
 	audio_stream_init_alignment_constants(byte_align, frame_align_req, source);
 }
 
-static int mixer_prepare(struct processing_module *mod)
+static int mixer_prepare(struct processing_module *mod,
+			 struct sof_source __sparse_cache **sources, int num_of_sources,
+			 struct sof_sink __sparse_cache **sinks, int num_of_sinks)
 {
 	struct mixer_data *md = module_get_private_data(mod);
 	struct comp_buffer __sparse_cache *sink_c;

src/audio/mixin_mixout/mixin_mixout.c
@@ -686,7 +686,9 @@ static int mixin_params(struct processing_module *mod)
  * We should also make sure that we propagate the prepare call to downstream
  * if downstream is not currently active.
  */
-static int mixin_prepare(struct processing_module *mod)
+static int mixin_prepare(struct processing_module *mod,
+			 struct sof_source __sparse_cache **sources, int num_of_sources,
+			 struct sof_sink __sparse_cache **sinks, int num_of_sinks)
 {
 	struct mixin_data *md = module_get_private_data(mod);
 	struct comp_dev *dev = mod->dev;
@@ -805,7 +807,9 @@ static int mixout_params(struct processing_module *mod)
 	return 0;
 }
 
-static int mixout_prepare(struct processing_module *mod)
+static int mixout_prepare(struct processing_module *mod,
+			  struct sof_source __sparse_cache **sources, int num_of_sources,
+			  struct sof_sink __sparse_cache **sinks, int num_of_sinks)
 {
 	struct module_source_info __sparse_cache *mod_source_info;
 	struct comp_dev *dev = mod->dev;

src/audio/module_adapter/module/cadence.c
@@ -501,7 +501,9 @@ static int cadence_codec_init_process(struct processing_module *mod)
 	return 0;
 }
 
-static int cadence_codec_prepare(struct processing_module *mod)
+static int cadence_codec_prepare(struct processing_module *mod,
+				 struct sof_source __sparse_cache **sources, int num_of_sources,
+				 struct sof_sink __sparse_cache **sinks, int num_of_sinks)
 {
 	int ret = 0, mem_tabs_size;
 	struct comp_dev *dev = mod->dev;

src/audio/module_adapter/module/dts.c
@@ -183,7 +183,9 @@ static int dts_codec_init(struct processing_module *mod)
 	return ret;
 }
 
-static int dts_codec_prepare(struct processing_module *mod)
+static int dts_codec_prepare(struct processing_module *mod,
+			     struct sof_source __sparse_cache **sources, int num_of_sources,
+			     struct sof_sink __sparse_cache **sinks, int num_of_sinks)
 {
 	int ret;
 	struct comp_dev *dev = mod->dev;

commit f18e70010c914c86c8f6e3a5d00ead21a6347c84

module_adapter: Fix compilation error with Xtensa toolchain
We get the following error using Xtensa toolchain on i.MX:

src/audio/module_adapter/module_adapter.c:
In function ‘module_adapter_sink_src_prepare’:
src/audio/module_adapter/module_adapter.c:207: error: ‘for’ loop initial declaration used outside C99 mode
src/audio/module_adapter/module_adapter.c:211: error: redefinition of ‘i’
src/audio/module_adapter/module_adapter.c:207: error: previous definition of ‘i’ was here
src/audio/module_adapter/module_adapter.c:211: error: ‘for’ loop initial declaration used outside C99 mode

Fix this by declaring `i` at the top of the function.

Fixes: fa77edf9a47e ("pipeline2.0: change module prepare API to use sink/src.c")
Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

src/audio/module_adapter/module_adapter.c
@@ -175,7 +175,7 @@ static int module_adapter_sink_src_prepare(struct comp_dev *dev)
 	struct list_item *blist;
 	uint32_t num_of_sources = 0;
 	uint32_t num_of_sinks = 0;
-	int ret;
+	int ret, i;
 
 	/* acquire all sink and source buffers, get handlers to sink/source API */
 	list_for_item(blist, &dev->bsink_list) {
@@ -204,11 +204,11 @@ static int module_adapter_sink_src_prepare(struct comp_dev *dev)
 	ret = module_prepare(mod, audio_src, num_of_sources, audio_sink, num_of_sinks);
 
 	/* release all source buffers in reverse order */
-	for (int i = num_of_sources - 1; i >= 0; i--)
+	for (i = num_of_sources - 1; i >= 0; i--)
 		buffer_release(source_buffers_c[i]);
 
 	/* release all sink buffers in reverse order */
-	for  (int i = num_of_sinks - 1; i >= 0 ; i--)
+	for  (i = num_of_sinks - 1; i >= 0 ; i--)
 		buffer_release(sinks_buffers_c[i]);
 
 	return ret;
@@ -911,7 +911,7 @@ static int module_adapter_sink_source_copy(struct comp_dev *dev)
 	struct list_item *blist;
 	uint32_t num_of_sources = 0;
 	uint32_t num_of_sinks = 0;
-	int ret;
+	int ret, i;
 
 	comp_dbg(dev, "module_adapter_sink_source_copy(): start");
 
@@ -946,13 +946,13 @@ static int module_adapter_sink_source_copy(struct comp_dev *dev)
 	}
 
 	/* release all source buffers in reverse order */
-	for (int i = num_of_sources - 1; i >= 0; i--) {
+	for (i = num_of_sources - 1; i >= 0; i--) {
 		mod->total_data_consumed += source_get_num_of_processed_bytes(audio_src[i]);
 		buffer_release(source_buffers_c[i]);
 	}
 
 	/* release all sink buffers in reverse order */
-	for  (int i = num_of_sinks - 1; i >= 0 ; i--) {
+	for  (i = num_of_sinks - 1; i >= 0 ; i--) {
 		mod->total_data_produced += sink_get_num_of_processed_bytes(audio_sink[i]);
 		buffer_release(sinks_buffers_c[i]);
 	}