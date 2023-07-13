thesofproject/sof

commit e57883f3c2fda999d13b449b51f1e4d550441dcf

buffer: use an accessor to read struct audio_stream::channels
This patch adds audio_stream_get_channels() to read
struct audio_stream::channels and converts all users. Apart from
hiding internals of the structure, it also enforces the use of the
__sparse_cache annotation.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/audio/module_adapter/module/waves.c
@@ -243,9 +243,9 @@ static int waves_effect_check(struct comp_dev *dev)
 	}
 
 	/* upmix/downmix not supported */
-	if (src_fmt->channels != snk_fmt->channels) {
+	if (src_fmt->channels != audio_stream_get_channels(snk_fmt)) {
 		comp_err(dev, "waves_effect_check() source %d sink %d channels mismatch",
-			 src_fmt->channels, snk_fmt->channels);
+			 audio_stream_get_channels(src_fmt), audio_stream_get_channels(snk_fmt));
 		ret = -EINVAL;
 		goto out;
 	}
@@ -285,7 +285,8 @@ static int waves_effect_check(struct comp_dev *dev)
 	}
 
 	if (src_fmt->channels != 2) {
-		comp_err(dev, "waves_effect_check() channels %d not supported", src_fmt->channels);
+		comp_err(dev, "waves_effect_check() channels %d not supported",
+			 audio_stream_get_channels(src_fmt));
 		ret = -EINVAL;
 		goto out;
 	}
@@ -348,7 +349,7 @@ static int waves_effect_init(struct processing_module *mod)
 	waves_codec->o_buffer = 0;
 
 	waves_codec->i_format.sampleRate = audio_stream_get_rate(src_fmt);
-	waves_codec->i_format.numChannels = src_fmt->channels;
+	waves_codec->i_format.numChannels = audio_stream_get_channels(src_fmt);
 	waves_codec->i_format.samplesFormat = sample_format;
 	waves_codec->i_format.samplesLayout = buffer_format;
 	waves_codec->o_format = waves_codec->i_format;
@@ -359,8 +360,8 @@ static int waves_effect_init(struct processing_module *mod)
 	 */
 	waves_codec->buffer_samples = audio_stream_get_rate(src_fmt) * dev->pipeline->period /
 		1000000;
-	waves_codec->buffer_bytes = waves_codec->buffer_samples * src_fmt->channels *
-		waves_codec->sample_size_in_bytes;
+	waves_codec->buffer_bytes = waves_codec->buffer_samples *
+		audio_stream_get_channels(src_fmt) * waves_codec->sample_size_in_bytes;
 
 	// trace allows printing only up-to 4 words at a time
 	// logging all the information in two calls

src/audio/aria/aria.c
@@ -330,7 +330,7 @@ static int aria_copy(struct comp_dev *dev)
 	buffer_stream_invalidate(source_c, source_bytes);
 
 	for (i = 0; i < c.frames; i++) {
-		for (channel = 0; channel < sink_c->stream.channels; channel++) {
+		for (channel = 0; channel < audio_stream_get_channels(&sink_c->stream); channel++) {
 			cd->buf_in[frag] = *(int32_t *)audio_stream_read_frag_s32(&source_c->stream,
 										  frag);
 
@@ -338,16 +338,18 @@ static int aria_copy(struct comp_dev *dev)
 		}
 	}
 	dcache_writeback_region((__sparse_force void __sparse_cache *)cd->buf_in,
-				c.frames * sink_c->stream.channels * sizeof(int32_t));
+				c.frames * audio_stream_get_channels(&sink_c->stream) *
+				sizeof(int32_t));
 
 	aria_process_data(dev, cd->buf_out, sink_bytes / sizeof(uint32_t),
 			  cd->buf_in, source_bytes / sizeof(uint32_t));
 
 	dcache_writeback_region((__sparse_force void __sparse_cache *)cd->buf_out,
-				c.frames * sink_c->stream.channels * sizeof(int32_t));
+				c.frames * audio_stream_get_channels(&sink_c->stream) *
+				sizeof(int32_t));
 	frag = 0;
 	for (i = 0; i < c.frames; i++) {
-		for (channel = 0; channel < sink_c->stream.channels; channel++) {
+		for (channel = 0; channel < audio_stream_get_channels(&sink_c->stream); channel++) {
 			destp = audio_stream_write_frag_s32(&sink_c->stream, frag);
 			*destp = cd->buf_out[frag];
 

src/audio/asrc/asrc.c
@@ -145,7 +145,7 @@ static void src_copy_s32(struct comp_dev *dev,
 
 	/* Copy input data from source */
 	buf = (int32_t *)cd->ibuf[0];
-	n = cd->source_frames * source->channels;
+	n = cd->source_frames * audio_stream_get_channels(source);
 	while (n > 0) {
 		n_wrap_src = (int32_t *)audio_stream_get_end_addr(source) - src;
 		n_copy = (n < n_wrap_src) ? n : n_wrap_src;
@@ -176,7 +176,7 @@ static void src_copy_s32(struct comp_dev *dev,
 		comp_err(dev, "src_copy_s32(), error %d", ret);
 
 	buf = (int32_t *)cd->obuf[0];
-	n = out_frames * sink->channels;
+	n = out_frames * audio_stream_get_channels(sink);
 	while (n > 0) {
 		n_wrap_snk = (int32_t *)audio_stream_get_end_addr(sink) - snk;
 		n_copy = (n < n_wrap_snk) ? n : n_wrap_snk;
@@ -216,7 +216,7 @@ static void src_copy_s16(struct comp_dev *dev,
 
 	/* Copy input data from source */
 	buf = (int16_t *)cd->ibuf[0];
-	n = cd->source_frames * source->channels;
+	n = cd->source_frames * audio_stream_get_channels(source);
 	while (n > 0) {
 		n_wrap_src = (int16_t *)audio_stream_get_end_addr(source) - src;
 		n_copy = (n < n_wrap_src) ? n : n_wrap_src;
@@ -251,7 +251,7 @@ static void src_copy_s16(struct comp_dev *dev,
 		comp_err(dev, "src_copy_s16(), error %d", ret);
 
 	buf = (int16_t *)cd->obuf[0];
-	n = out_frames * sink->channels;
+	n = out_frames * audio_stream_get_channels(sink);
 	while (n > 0) {
 		n_wrap_snk = (int16_t *)audio_stream_get_end_addr(sink) - snk;
 		n_copy = (n < n_wrap_snk) ? n : n_wrap_snk;
@@ -817,8 +817,8 @@ static int asrc_prepare(struct comp_dev *dev)
 		goto err;
 	}
 
-	sample_bytes = frame_bytes / source_c->stream.channels;
-	for (i = 0; i < sourceb->stream.channels; i++) {
+	sample_bytes = frame_bytes / audio_stream_get_channels(&source_c->stream);
+	for (i = 0; i < audio_stream_get_channels(&source_c->stream); i++) {
 		cd->ibuf[i] = cd->buf + i * sample_bytes;
 		cd->obuf[i] = cd->ibuf[i] + cd->source_frames_max * frame_bytes;
 	}
@@ -826,7 +826,7 @@ static int asrc_prepare(struct comp_dev *dev)
 	/* Get required size and allocate memory for ASRC */
 	sample_bits = sample_bytes * 8;
 	ret = asrc_get_required_size(dev, &cd->asrc_size,
-				     source_c->stream.channels,
+				     audio_stream_get_channels(&source_c->stream),
 				     sample_bits);
 	if (ret) {
 		comp_err(dev, "asrc_prepare(), get_required_size_bytes failed");
@@ -852,7 +852,7 @@ static int asrc_prepare(struct comp_dev *dev)
 		fs_sec = cd->source_rate;
 	}
 
-	ret = asrc_initialise(dev, cd->asrc_obj, source_c->stream.channels,
+	ret = asrc_initialise(dev, cd->asrc_obj, audio_stream_get_channels(&source_c->stream),
 			      fs_prim, fs_sec,
 			      ASRC_IOF_INTERLEAVED, ASRC_IOF_INTERLEAVED,
 			      ASRC_BM_LINEAR, cd->frames, sample_bits,

src/audio/copier/copier.c
@@ -779,7 +779,7 @@ static void copy_single_channel_c16(struct audio_stream __sparse_cache *dst,
 	 * is NOT number of samples we need to copy but total samples for all channels. We just
 	 * track them to know when to stop.
 	 */
-	int src_stream_sample_count = frame_count * src->channels;
+	int src_stream_sample_count = frame_count * audio_stream_get_channels(src);
 
 	while (src_stream_sample_count) {
 		int src_samples_without_wrap;
@@ -796,8 +796,8 @@ static void copy_single_channel_c16(struct audio_stream __sparse_cache *dst,
 
 		do {
 			*w_ptr = *r_ptr;
-			r_ptr += src->channels;
-			w_ptr += dst->channels;
+			r_ptr += audio_stream_get_channels(src);
+			w_ptr += audio_stream_get_channels(dst);
 		} while (r_ptr < r_end_ptr && w_ptr < (int16_t *)audio_stream_get_end_addr(dst));
 
 		src_stream_sample_count -= r_ptr - r_ptr_before_loop;
@@ -818,7 +818,7 @@ static void copy_single_channel_c32(struct audio_stream __sparse_cache *dst,
 	 * is NOT number of samples we need to copy but total samples for all channels. We just
 	 * track them to know when to stop.
 	 */
-	int src_stream_sample_count = frame_count * src->channels;
+	int src_stream_sample_count = frame_count * audio_stream_get_channels(src);
 
 	while (src_stream_sample_count) {
 		int src_samples_without_wrap;
@@ -835,8 +835,8 @@ static void copy_single_channel_c32(struct audio_stream __sparse_cache *dst,
 
 		do {
 			*w_ptr = *r_ptr;
-			r_ptr += src->channels;
-			w_ptr += dst->channels;
+			r_ptr += audio_stream_get_channels(src);
+			w_ptr += audio_stream_get_channels(dst);
 		} while (r_ptr < r_end_ptr && w_ptr < (int32_t *)audio_stream_get_end_addr(dst));
 
 		src_stream_sample_count -= r_ptr - r_ptr_before_loop;
@@ -1120,8 +1120,8 @@ static int demux_from_multi_endpoint_buffer(struct copier_data *cd)
 
 		endp_buf_c = buffer_acquire(cd->endpoint_buffer[endp_idx]);
 
-		for (endp_channel = 0; endp_channel < endp_buf_c->stream.channels;
-				endp_channel++) {
+		for (endp_channel = 0; endp_channel <
+		     audio_stream_get_channels(&endp_buf_c->stream); endp_channel++) {
 			int multi_buf_channel = endp_buf_c->chmap[endp_channel];
 
 			cd->copy_single_channel(&endp_buf_c->stream, endp_channel,
@@ -1166,8 +1166,8 @@ static int mux_into_multi_endpoint_buffer(struct copier_data *cd)
 		endp_buf_byte_count = frame_count * audio_stream_frame_bytes(&endp_buf_c->stream);
 		buffer_stream_invalidate(endp_buf_c, endp_buf_byte_count);
 
-		for (endp_channel = 0; endp_channel < endp_buf_c->stream.channels;
-				endp_channel++) {
+		for (endp_channel = 0; endp_channel <
+		     audio_stream_get_channels(&endp_buf_c->stream); endp_channel++) {
 			int multi_buf_channel = endp_buf_c->chmap[endp_channel];
 
 			cd->copy_single_channel(&multi_buf_c->stream, multi_buf_channel,
@@ -1242,7 +1242,7 @@ static int do_conversion_copy(struct comp_dev *dev,
 	buffer_stream_invalidate(src, processed_data->source_bytes);
 
 	cd->converter[i](&src->stream, 0, &sink->stream, 0,
-			 processed_data->frames * sink->stream.channels);
+			 processed_data->frames * audio_stream_get_channels(&sink->stream));
 
 	if (cd->attenuation) {
 		ret = apply_attenuation(dev, cd, sink, processed_data->frames);
@@ -1411,7 +1411,7 @@ static void copier_dma_cb(void *arg, enum notify_id type, void *data)
 			sink = buffer_acquire(cd->hd->dma_buffer);
 
 		frames = bytes / get_sample_bytes(audio_stream_get_frm_fmt(&sink->stream));
-		frames = frames / sink->stream.channels;
+		frames = frames / audio_stream_get_channels(&sink->stream);
 
 		ret = apply_attenuation(dev, cd, sink, frames);
 		if (ret < 0)
@@ -1490,7 +1490,7 @@ static int copier_params(struct comp_dev *dev, struct sof_ipc_stream_params *par
 			struct sof_ipc_stream_params demuxed_params = *params;
 
 			buf_c = buffer_acquire(cd->endpoint_buffer[i]);
-			demuxed_params.channels = buf_c->stream.channels;
+			demuxed_params.channels = audio_stream_get_channels(&buf_c->stream);
 			buffer_release(buf_c);
 
 			ret = cd->endpoint[i]->drv->ops.params(cd->endpoint[i],

src/audio/copier/copier_generic.c
@@ -26,7 +26,7 @@ int apply_attenuation(struct comp_dev *dev, struct copier_data *cd,
 	int i;
 	int n;
 	int nmax;
-	int remaining_samples = frame * sink->stream.channels;
+	int remaining_samples = frame * audio_stream_get_channels(&sink->stream);
 	int32_t *dst = audio_stream_get_rptr(&sink->stream);
 
 	/* only support attenuation in format of 32bit */

src/audio/copier/copier_hifi.c
@@ -30,7 +30,7 @@ int apply_attenuation(struct comp_dev *dev, struct copier_data *cd,
 	ae_int32x2 sample;
 	ae_valign uu = AE_ZALIGN64();
 	ae_valign su = AE_ZALIGN64();
-	int remaining_samples = frame * sink->stream.channels;
+	int remaining_samples = frame * audio_stream_get_channels(&sink->stream);
 	uint32_t *dst = audio_stream_get_rptr(&sink->stream);
 	ae_int32x2 *in = (ae_int32x2 *)dst;
 	ae_int32x2 *out = (ae_int32x2 *)dst;

src/audio/crossover/crossover.c
@@ -610,7 +610,7 @@ static int crossover_copy(struct comp_dev *dev)
 	/* Check for changed configuration */
 	if (comp_is_new_data_blob_available(cd->model_handler)) {
 		cd->config = comp_get_data_blob(cd->model_handler, NULL, NULL);
-		ret = crossover_setup(cd, source_c->stream.channels);
+		ret = crossover_setup(cd, audio_stream_get_channels(&source_c->stream));
 		if (ret < 0) {
 			comp_err(dev, "crossover_copy(), failed Crossover setup");
 			goto out;
@@ -747,7 +747,7 @@ static int crossover_prepare(struct comp_dev *dev)
 
 	comp_info(dev, "crossover_prepare(), source_format=%d, sink_formats=%d, nch=%d",
 		  cd->source_format, cd->source_format,
-		  source_c->stream.channels);
+		  audio_stream_get_channels(&source_c->stream));
 
 	cd->config = comp_get_data_blob(cd->model_handler, NULL, NULL);
 
@@ -759,7 +759,7 @@ static int crossover_prepare(struct comp_dev *dev)
 	}
 
 	if (cd->config) {
-		ret = crossover_setup(cd, source_c->stream.channels);
+		ret = crossover_setup(cd, audio_stream_get_channels(&source_c->stream));
 		if (ret < 0) {
 			comp_err(dev, "crossover_prepare(), setup failed");
 			goto out;

src/audio/crossover/crossover_generic.c
@@ -96,7 +96,7 @@ static void crossover_s16_default_pass(const struct comp_dev *dev,
 	int16_t *x;
 	int32_t *y;
 	int i, j;
-	int n = source_stream->channels * frames;
+	int n = audio_stream_get_channels(source_stream) * frames;
 
 	for (i = 0; i < n; i++) {
 		x = audio_stream_read_frag_s16(source_stream, i);
@@ -120,7 +120,7 @@ static void crossover_s32_default_pass(const struct comp_dev *dev,
 	const struct audio_stream __sparse_cache *source_stream = &source->stream;
 	int32_t *x, *y;
 	int i, j;
-	int n = source_stream->channels * frames;
+	int n = audio_stream_get_channels(source_stream) * frames;
 
 	for (i = 0; i < n; i++) {
 		x = audio_stream_read_frag_s32(source_stream, i);
@@ -148,7 +148,7 @@ static void crossover_s16_default(const struct comp_dev *dev,
 	int16_t *x, *y;
 	int ch, i, j;
 	int idx;
-	int nch = source_stream->channels;
+	int nch = audio_stream_get_channels(source_stream);
 	int32_t out[num_sinks];
 
 	for (ch = 0; ch < nch; ch++) {
@@ -187,7 +187,7 @@ static void crossover_s24_default(const struct comp_dev *dev,
 	int32_t *x, *y;
 	int ch, i, j;
 	int idx;
-	int nch = source_stream->channels;
+	int nch = audio_stream_get_channels(source_stream);
 	int32_t out[num_sinks];
 
 	for (ch = 0; ch < nch; ch++) {
@@ -226,7 +226,7 @@ static void crossover_s32_default(const struct comp_dev *dev,
 	int32_t *x, *y;
 	int ch, i, j;
 	int idx;
-	int nch = source_stream->channels;
+	int nch = audio_stream_get_channels(source_stream);
 	int32_t out[num_sinks];
 
 	for (ch = 0; ch < nch; ch++) {

commit 50d5e5b4408e6d57ba555d482688dd31d4545492

audio: waves: Fix buffer accessors
Since 3P code can't be compiled without the library (for now) we rely on
reviews for API conversions which unfortunately has proven to not be
perfect. Fixup the accessors for the waves module.

Fixes: e57883f3c2fd ("buffer: use an accessor to read struct audio_stream::channels")
Fixes: f8e4d693b26f ("buffer: use an accessor to read struct audio_stream::rate")
Fixes: 017b8ebcde57 ("buffer: use an accessor to read struct audio_stream::frame_fmt")
Fixes: 90828e230590 ("buffer: external modules use API to access buffer_fmt")
Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>

src/audio/module_adapter/module/waves.c
@@ -232,7 +232,7 @@ static int waves_effect_check(struct comp_dev *dev)
 	/* todo use fallback to comp_verify_params when ready */
 
 	/* resampling not supported */
-	if (src_fmt->rate != audio_stream_get_rate(snk_fmt)) {
+	if (audio_stream_get_rate(src_fmt) != audio_stream_get_rate(snk_fmt)) {
 		comp_err(dev, "waves_effect_check() source %d sink %d rate mismatch",
 			 audio_stream_get_rate(src_fmt), audio_stream_get_rate(snk_fmt));
 		ret = -EINVAL;
@@ -240,7 +240,7 @@ static int waves_effect_check(struct comp_dev *dev)
 	}
 
 	/* upmix/downmix not supported */
-	if (src_fmt->channels != audio_stream_get_channels(snk_fmt)) {
+	if (audio_stream_get_channels(src_fmt) != audio_stream_get_channels(snk_fmt)) {
 		comp_err(dev, "waves_effect_check() source %d sink %d channels mismatch",
 			 audio_stream_get_channels(src_fmt), audio_stream_get_channels(snk_fmt));
 		ret = -EINVAL;
@@ -248,7 +248,7 @@ static int waves_effect_check(struct comp_dev *dev)
 	}
 
 	/* different frame format not supported */
-	if (src_fmt->frame_fmt != audio_stream_get_frm_fmt(snk_fmt)) {
+	if (audio_stream_get_frm_fmt(src_fmt) != audio_stream_get_frm_fmt(snk_fmt)) {
 		comp_err(dev, "waves_effect_check() source %d sink %d sample format mismatch",
 			 audio_stream_get_frm_fmt(src_fmt), audio_stream_get_frm_fmt(snk_fmt));
 		ret = -EINVAL;
@@ -256,7 +256,7 @@ static int waves_effect_check(struct comp_dev *dev)
 	}
 
 	/* different interleaving is not supported */
-	if (source_c->buffer_fmt != sink_c->buffer_fmt) {
+	if (audio_stream_get_buffer_fmt(src_fmt) != audio_stream_get_buffer_fmt(snk_fmt)) {
 		comp_err(dev, "waves_effect_check() source %d sink %d buffer format mismatch");
 		ret = -EINVAL;
 		goto out;
@@ -268,7 +268,7 @@ static int waves_effect_check(struct comp_dev *dev)
 		goto out;
 	}
 
-	if (!layout_is_supported(source_c->buffer_fmt)) {
+	if (!layout_is_supported(audio_stream_get_buffer_fmt(src_fmt))) {
 		comp_err(dev, "waves_effect_check() non interleaved format not supported");
 		ret = -EINVAL;
 		goto out;
@@ -281,7 +281,7 @@ static int waves_effect_check(struct comp_dev *dev)
 		goto out;
 	}
 
-	if (src_fmt->channels != 2) {
+	if (audio_stream_get_channels(src_fmt) != 2) {
 		comp_err(dev, "waves_effect_check() channels %d not supported",
 			 audio_stream_get_channels(src_fmt));
 		ret = -EINVAL;
@@ -325,10 +325,10 @@ static int waves_effect_init(struct processing_module *mod)
 		goto out;
 	}
 
-	buffer_format = layout_convert_sof_to_me(source_c->buffer_fmt);
+	buffer_format = layout_convert_sof_to_me(audio_stream_get_buffer_fmt(src_fmt));
 	if (buffer_format < 0) {
 		comp_err(dev, "waves_effect_init() sof buffer format %d not supported",
-			 source_c->buffer_fmt);
+			 audio_stream_get_buffer_fmt(src_fmt));
 		ret = -EINVAL;
 		goto out;
 	}