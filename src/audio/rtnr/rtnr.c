thesofproject/sof

commit 10f6dd0d7bdf785e2a0cb462e94577d6cbfc994a

module-adapter: add a constant data pointer
module_config contains a .data pointer that is used both for initial
and run-time configuration. The initial configuration should be
const. We add a new .init_data pointer for it.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/audio/rtnr/rtnr.c
@@ -219,10 +219,10 @@ static int32_t rtnr_check_config_validity(struct comp_dev *dev,
 }
 
 static struct comp_dev *rtnr_new(const struct comp_driver *drv,
-								struct comp_ipc_config *config,
-								void *spec)
+				 const struct comp_ipc_config *config,
+				 const void *spec)
 {
-	struct ipc_config_process *ipc_rtnr = spec;
+	const struct ipc_config_process *ipc_rtnr = spec;
 	struct comp_dev *dev;
 	struct comp_data *cd;
 	size_t bs = ipc_rtnr->size;

src/audio/aria/aria.c
@@ -122,10 +122,10 @@ int aria_process_data(struct comp_dev *dev,
 	return aria_algo_buffer_data(dev, src, src_size);
 }
 
-static int init_aria(struct comp_dev *dev, struct comp_ipc_config *config,
-		     void *spec)
+static int init_aria(struct comp_dev *dev, const struct comp_ipc_config *config,
+		     const void *spec)
 {
-	struct ipc4_aria_module_cfg *aria = spec;
+	const struct ipc4_aria_module_cfg *aria = spec;
 	struct aria_data *cd;
 	size_t ibs, chc, sgs, sgc, req_mem, att;
 	void *buf, *buf_in, *buf_out;
@@ -181,7 +181,8 @@ static int init_aria(struct comp_dev *dev, struct comp_ipc_config *config,
 }
 
 static struct comp_dev *aria_new(const struct comp_driver *drv,
-				 struct comp_ipc_config *config, void *spec)
+				 const struct comp_ipc_config *config,
+				 const void *spec)
 {
 	struct comp_dev *dev;
 	int ret;

src/audio/asrc/asrc.c
@@ -324,14 +324,14 @@ static int asrc_get_attribute(struct comp_dev *dev, uint32_t type, void *value)
 #endif
 
 static struct comp_dev *asrc_new(const struct comp_driver *drv,
-				 struct comp_ipc_config *config,
-				 void *spec)
+				 const struct comp_ipc_config *config,
+				 const void *spec)
 {
 	struct comp_dev *dev;
 #ifndef CONFIG_IPC_MAJOR_4
-	struct ipc_config_asrc *ipc_asrc = spec;
+	const struct ipc_config_asrc *ipc_asrc = spec;
 #else
-	struct ipc4_asrc_module_cfg *ipc_asrc = spec;
+	const struct ipc4_asrc_module_cfg *ipc_asrc = spec;
 #endif
 	struct comp_data *cd;
 

src/audio/base_fw.c
@@ -239,14 +239,14 @@ static uint32_t basefw_set_system_time(uint32_t param_id,
 				       bool first_block,
 				       bool last_block,
 				       uint32_t data_offset,
-				       char *data)
+				       const char *data)
 {
 	/* TODO: configurate time to logging subsystem */
 	if (!(first_block && last_block))
 		return IPC4_INVALID_REQUEST;
 
-	global_system_time_info.host_time.val_l = ((struct ipc4_system_time *)data)->val_l;
-	global_system_time_info.host_time.val_u = ((struct ipc4_system_time *)data)->val_u;
+	global_system_time_info.host_time.val_l = ((const struct ipc4_system_time *)data)->val_l;
+	global_system_time_info.host_time.val_u = ((const struct ipc4_system_time *)data)->val_u;
 
 	uint64_t current_dsp_time = sof_cycle_get_64();
 

src/audio/copier/copier.c
@@ -55,7 +55,7 @@ static pcm_converter_func get_converter_func(const struct ipc4_audio_format *in_
 static int create_endpoint_buffer(struct comp_dev *parent_dev,
 				  struct copier_data *cd,
 				  struct comp_ipc_config *config,
-				  struct ipc4_copier_module_cfg *copier_cfg,
+				  const struct ipc4_copier_module_cfg *copier_cfg,
 				  enum ipc4_gateway_type type,
 				  int index)
 {
@@ -166,7 +166,7 @@ static int create_endpoint_buffer(struct comp_dev *parent_dev,
  */
 static int create_host(struct comp_dev *parent_dev, struct copier_data *cd,
 		       struct comp_ipc_config *config,
-		       struct ipc4_copier_module_cfg *copier_cfg,
+		       const struct ipc4_copier_module_cfg *copier_cfg,
 		       int dir)
 {
 	struct sof_uuid host = {0x8b9d100c, 0x6d78, 0x418f, {0x90, 0xa3, 0xe0,
@@ -231,7 +231,7 @@ e_buf:
 static int init_dai(struct comp_dev *parent_dev,
 		    const struct comp_driver *drv,
 		    struct comp_ipc_config *config,
-		    struct ipc4_copier_module_cfg *copier,
+		    const struct ipc4_copier_module_cfg *copier,
 		    struct pipeline *pipeline,
 		    struct ipc_config_dai *dai,
 		    enum ipc4_gateway_type type,
@@ -300,7 +300,7 @@ e_buf:
  */
 static int create_dai(struct comp_dev *parent_dev, struct copier_data *cd,
 		      struct comp_ipc_config *config,
-		      struct ipc4_copier_module_cfg *copier,
+		      const struct ipc4_copier_module_cfg *copier,
 		      struct pipeline *pipeline)
 {
 	struct sof_uuid id = {0xc2b00d27, 0xffbc, 0x4150, {0xa5, 0x1a, 0x24,
@@ -429,10 +429,10 @@ static int init_pipeline_reg(struct comp_dev *dev)
 }
 
 static struct comp_dev *copier_new(const struct comp_driver *drv,
-				   struct comp_ipc_config *config,
-				   void *spec)
+				   const struct comp_ipc_config *config,
+				   const void *spec)
 {
-	struct ipc4_copier_module_cfg *copier = spec;
+	const struct ipc4_copier_module_cfg *copier = spec;
 	union ipc4_connector_node_id node_id;
 	struct ipc_comp_dev *ipc_pipe;
 	struct ipc *ipc = ipc_get();
@@ -484,7 +484,7 @@ static struct comp_dev *copier_new(const struct comp_driver *drv,
 		switch (node_id.f.dma_type) {
 		case ipc4_hda_host_output_class:
 		case ipc4_hda_host_input_class:
-			if (create_host(dev, cd, config, copier, cd->direction)) {
+			if (create_host(dev, cd, &dev->ipc_config, copier, cd->direction)) {
 				comp_cl_err(&comp_copier, "unenable to create host");
 				goto error_cd;
 			}
@@ -505,7 +505,7 @@ static struct comp_dev *copier_new(const struct comp_driver *drv,
 		case ipc4_i2s_link_input_class:
 		case ipc4_alh_link_output_class:
 		case ipc4_alh_link_input_class:
-			if (create_dai(dev, cd, config, copier, ipc_pipe->pipeline)) {
+			if (create_dai(dev, cd, &dev->ipc_config, copier, ipc_pipe->pipeline)) {
 				comp_cl_err(&comp_copier, "unenable to create dai");
 				goto error_cd;
 			}

src/audio/crossover/crossover.c
@@ -321,12 +321,12 @@ static int crossover_setup(struct comp_data *cd, int nch)
  * \return Pointer to Crossover Filter component device.
  */
 static struct comp_dev *crossover_new(const struct comp_driver *drv,
-				      struct comp_ipc_config *config,
-				      void *spec)
+				      const struct comp_ipc_config *config,
+				      const void *spec)
 {
 	struct comp_dev *dev;
 	struct comp_data *cd;
-	struct ipc_config_process *ipc_crossover = spec;;
+	const struct ipc_config_process *ipc_crossover = spec;
 	size_t bs = ipc_crossover->size;
 	int ret;
 

src/audio/dai-legacy.c
@@ -160,11 +160,11 @@ static void dai_dma_cb(void *arg, enum notify_id type, void *data)
 }
 
 static struct comp_dev *dai_new(const struct comp_driver *drv,
-				struct comp_ipc_config *config,
-				void *spec)
+				const struct comp_ipc_config *config,
+				const void *spec)
 {
 	struct comp_dev *dev;
-	struct ipc_config_dai *dai = spec;
+	const struct ipc_config_dai *dai = spec;
 	struct dai_data *dd;
 	uint32_t dir, caps, dma_dev;
 

src/audio/dai-zephyr.c
@@ -291,11 +291,11 @@ static void dai_dma_cb(void *arg, enum notify_id type, void *data)
 }
 
 static struct comp_dev *dai_new(const struct comp_driver *drv,
-				struct comp_ipc_config *config,
-				void *spec)
+				const struct comp_ipc_config *config,
+				const void *spec)
 {
 	struct comp_dev *dev;
-	struct ipc_config_dai *dai_cfg = spec;
+	const struct ipc_config_dai *dai_cfg = spec;
 	struct dai_data *dd;
 	uint32_t dir;
 

commit bf13304bdbb57e63a0fa75e973731da664b093ba

audio: rtnr: fix function arg attributes
constify RO params

Fixes: 10f6dd0d7b ("module-adapter: add a constant data pointer")
Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>

src/audio/rtnr/rtnr.c
@@ -67,7 +67,7 @@ DECLARE_TR_CTX(rtnr_tr, SOF_UUID(rtnr_uuid), LOG_LEVEL_INFO);
 
 /* Static functions */
 static int rtnr_set_config_bytes(struct comp_dev *dev,
-				 unsigned char *data, uint32_t size);
+				 const unsigned char *data, uint32_t size);
 
 /* Called by the processing library for debugging purpose */
 void rtnr_printf(int a, int b, int c, int d, int e)
@@ -530,7 +530,7 @@ static int rtnr_reconfigure(struct comp_dev *dev)
 }
 
 static int rtnr_set_config_bytes(struct comp_dev *dev,
-				 unsigned char *data, uint32_t size)
+				 const unsigned char *data, uint32_t size)
 {
 	struct comp_data *cd = comp_get_drvdata(dev);
 	int ret;