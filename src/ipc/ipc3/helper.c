thesofproject/sof

commit 21ed10abf8a2673f13dea3fd39a063c561083054

ipc3: helper: Do not silently accept unrecognized component type in IPC
Return an error code from comp_specific_builder() if the component
type is not recognized, instead of just silently ignoring the error.

Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

src/ipc/ipc3/helper.c
@@ -191,8 +191,8 @@ union ipc_config_specific {
 } __attribute__((packed, aligned(4)));
 
 /* build component specific data */
-static void comp_specific_builder(struct sof_ipc_comp *comp,
-				  union ipc_config_specific *config)
+static int comp_specific_builder(struct sof_ipc_comp *comp,
+				 union ipc_config_specific *config)
 {
 #if CONFIG_LIBRARY
 	struct sof_ipc_comp_file *file = (struct sof_ipc_comp_file *)comp;
@@ -285,8 +285,9 @@ static void comp_specific_builder(struct sof_ipc_comp *comp,
 #endif
 		break;
 	default:
-		break;
+		return -EINVAL;
 	}
+	return 0;
 }
 
 struct ipc_comp_dev *ipc_get_comp_by_ppl_id(struct ipc *ipc, uint16_t type, uint32_t ppl_id)
@@ -328,8 +329,11 @@ struct comp_dev *comp_new(struct sof_ipc_comp *comp)
 		drv->tctx->uuid_p, comp->type, comp->pipeline_id, comp->id);
 
 	/* build the component */
+	if (comp_specific_builder(comp, &spec) < 0) {
+		comp_cl_err(drv, "comp_new(): component type not recognized");
+		return NULL;
+	}
 	comp_common_builder(comp, &config);
-	comp_specific_builder(comp, &spec);
 	cdev = drv->ops.create(drv, &config, &spec);
 	if (!cdev) {
 		comp_cl_err(drv, "comp_new(): unable to create the new component");

commit 2d3fb2ca4c97098d0301f2924f540e56d1379fca

ipc3: helper: Fix mixer scenario
With mixer after commit 21ed10abf8a2 ("ipc3: helper: Do not silently
accept unrecognized component type in IPC") we get the following error

dma-trace  src/ipc/ipc3/helper.c:333  ERROR comp_new(): component type not recognized

This happens because now we return an error from comp_specific_builder() with the default
case.

Fix this by adding a case for mixer.

Fixes: 21ed10abf8a2 ("ipc3: helper: Do not silently accept unrecognized component type in IPC")
Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

src/ipc/ipc3/helper.c
@@ -284,6 +284,8 @@ static int comp_specific_builder(struct sof_ipc_comp *comp,
 		config->process.data = proc->data;
 #endif
 		break;
+	case SOF_COMP_MIXER:
+		break;
 	default:
 		return -EINVAL;
 	}