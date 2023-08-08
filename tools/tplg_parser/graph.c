34518568f72f4d80a633493295e5a9fed80a149f
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2023-08-08 12:24:58 -0700

    tplg_parser: Add support for parsing and saving route
    Add an option to parse and save the routes from topology so that the
    IPCs can be sent at a later time.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- tools/tplg_parser/graph.c
+++ tools/tplg_parser/graph.c
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <ipc/topology.h>
 #include <sof/lib/uuid.h>
+#include <sof/list.h>
 #include <sof/ipc/topology.h>
 #include <kernel/header.h>
 #include <tplg_parser/topology.h>
@@ -70,6 +71,45 @@ int tplg_create_graph(struct tplg_context *ctx, int count, int pipeline_id,
 		strcat(pipeline_string, graph_elem->sink);
 		strcat(pipeline_string, "\n");
 	}
+
+	return 0;
+}
+
+/* parse and save the route information for IPC4 */
+int tplg_parse_graph(struct tplg_context *ctx, struct list_item *widget_list,
+		     struct list_item *route_list)
+{
+	struct snd_soc_tplg_dapm_graph_elem *graph_elem;
+	struct tplg_route_info *route;
+	struct list_item *item;
+
+	route = calloc(sizeof(struct tplg_route_info), 1);
+	if (!route)
+		return -ENOMEM;
+
+	graph_elem = tplg_get_graph(ctx);
+
+	/* look up from the widget list and populate the route info */
+	list_for_item(item, widget_list) {
+		struct tplg_comp_info *comp_info = container_of(item, struct tplg_comp_info, item);
+
+		if (!strcmp(comp_info->name, graph_elem->source))
+			route->source = comp_info;
+
+		if (!strcmp(comp_info->name, graph_elem->sink))
+			route->sink = comp_info;
+	}
+
+	if (!route->source || !route->sink) {
+		fprintf(stderr, "%s() error loading route: source=%s, sink=%s\n",
+			__func__, graph_elem->source, graph_elem->sink);
+		free(route);
+		return -EINVAL;
+	}
+
+	list_item_append(&route->item, route_list);
+
+	printf("loading route %s -> %s\n", route->source->name, route->sink->name);
 
 	return 0;
 }
