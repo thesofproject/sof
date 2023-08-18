thesofproject/sof

commit 11b9ce23f7700cd238b18d650f28aa934d545fc8

lib-manager: Free up memory for the library in case of a store error
If lib_manager_store_data() fails to read the rest of the library we need
to free up the allocated memory.

Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

src/library_manager/lib_manager.c
@@ -541,8 +541,10 @@ static int lib_manager_store_library(struct lib_manager_dma_ext *dma_ext,
 	/* Copy remaining library part into storage buffer */
 	ret = lib_manager_store_data(dma_ext, (uint8_t __sparse_cache *)library_base_address +
 				     MAN_MAX_SIZE_V1_8, preload_size - MAN_MAX_SIZE_V1_8);
-	if (ret < 0)
+	if (ret < 0) {
+		rfree(library_base_address);
 		return ret;
+	}
 
 	/* Now update sof context with new library */
 	lib_manager_update_sof_ctx((__sparse_force void *)library_base_address, lib_id);

commit 19c378f224af96a03d96e61b9b2ea66da8c5d137

lib_manager: Add missing _sparse_force in lib_manager_store_library() error case
The fix for the memory leak missed the _sparse_force, causing sparse
warning.

Fixes: 11b9ce23f770 ("lib-manager: Free up memory for the library in case of a store error")
Reported-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

src/library_manager/lib_manager.c
@@ -561,7 +561,7 @@ static int lib_manager_store_library(struct lib_manager_dma_ext *dma_ext,
 	ret = lib_manager_store_data(dma_ext, (uint8_t __sparse_cache *)library_base_address +
 				     MAN_MAX_SIZE_V1_8, preload_size - MAN_MAX_SIZE_V1_8);
 	if (ret < 0) {
-		rfree(library_base_address);
+		rfree((__sparse_force void *)library_base_address);
 		return ret;
 	}
 