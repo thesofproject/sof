e24e46b07b3ccebf133657e487992398c2b51b65
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2023-08-29 18:36:23 +0300

    Tools: Tune: Add tplg2 support to dcblock setup tool
    This patch adds generate of blob for tplg2 and ipc4 in
    formats .conf, text, and binary formats.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/tune/dcblock/example_dcblock.m
+++ tools/tune/dcblock/example_dcblock.m
@@ -1,22 +1,30 @@
 function example_dcblock();
 
 % Set the parameters here
-tplg_fn = "../../topology/topology1/m4/dcblock_coef_default.m4" % Control Bytes File
+tplg1_fn = "../../topology/topology1/m4/dcblock_coef_default.m4" % Control Bytes File
+tplg2_fn = "../../topology/topology2/include/components/dcblock/default.conf"
 % Use those files with sof-ctl to update the component's configuration
-blob_fn = "../../ctl/dcblock_coef.blob" % Blob binary file
-alsa_fn = "../../ctl/dcblock_coef.txt" % ALSA CSV format file
+blob3_fn = "../../ctl/ipc3/dcblock_coef.blob" % Blob binary file
+alsa3_fn = "../../ctl/ipc3/dcblock_coef.txt" % ALSA CSV format file
+blob4_fn = "../../ctl/ipc4/dcblock_coef.blob" % Blob binary file
+alsa4_fn = "../../ctl/ipc4/dcblock_coef.txt" % ALSA CSV format file
 
 endian = "little";
 R_coeffs = [0.98, 0.98, 0.98, 0.98, 0.98, 0.98, 0.98, 0.98];
 
-blob8 = dcblock_build_blob(R_coeffs, endian);
-
-% Generate output files
 addpath ./../common
 
-tplg_write(tplg_fn, blob8, "DCBLOCK");
-blob_write(blob_fn, blob8);
-alsactl_write(alsa_fn, blob8);
+blob8 = dcblock_build_blob(R_coeffs, endian);
+blob8_ipc4 = dcblock_build_blob(R_coeffs, endian, 4);
+
+% Generate output files
+tplg_write(tplg1_fn, blob8, "DCBLOCK");
+blob_write(blob3_fn, blob8);
+alsactl_write(alsa3_fn, blob8);
+
+tplg2_write(tplg2_fn, blob8_ipc4, "dcblock_config", "Exported with script example_dcblock.m");
+blob_write(blob4_fn, blob8_ipc4);
+alsactl_write(alsa4_fn, blob8_ipc4);
 
 % Plot Filter's Transfer Function and Step Response
 % As an example, plot the graphs of the first coefficient
