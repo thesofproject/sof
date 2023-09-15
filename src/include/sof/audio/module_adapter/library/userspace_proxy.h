84b3a04bc8933e24aa533d813338a8d945326222
Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
2023-09-15 14:49:23 +0200

    userspace: Introduce userspace proxy
    Introduce initial support for userspace proxy logic. This includes
    configuring the module's memory domain to ensure proper access rights and
    creating intermediary functions that forward calls to the module's
    interface.
    
    Signed-off-by: Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/include/sof/audio/module_adapter/library/userspace_proxy.h
+++ src/include/sof/audio/module_adapter/library/userspace_proxy.h
@@ -14,12 +14,52 @@
 #include <stdbool.h>
 
 #include <zephyr/kernel.h>
+#include <utilities/array.h>
+
+#include <native_system_agent.h>
+#include <module/module/interface.h>
+
+struct module_interface;
+struct comp_driver;
+struct sof_man_module;
+struct system_agent_params;
 
 /* Processing module structure fields needed for user mode */
 struct userspace_context {
 	struct k_mem_domain *comp_dom;			/* Module specific memory domain	*/
+	const struct module_interface *interface;	/* Userspace module interface		*/
 };
-
 #endif /* CONFIG_USERSPACE */
+
+#if CONFIG_SOF_USERSPACE_PROXY
+/**
+ * Creates userspace module proxy
+ *
+ * @param user_ctx - pointer to pointer of userspace module context
+ * @param drv - pointer to component driver
+ * @param manifest - pointer to module manifest
+ * @param start_fn - pointer to system agent start function
+ * @param agent_params - pointer to system_agent_params
+ * @param agent_interface - pointer to variable to store module interface created by agent
+ * @param ops - Pointer to a variable that will hold the address of the module interface
+ *		structure. The function stores a pointer to its own userspace proxy
+ *		interface structure in this variable.
+ *
+ * @return 0 for success, error otherwise.
+ */
+int userspace_proxy_create(struct userspace_context **user_ctx, const struct comp_driver *drv,
+			   const struct sof_man_module *manifest, system_agent_start_fn start_fn,
+			   const struct system_agent_params *agent_params,
+			   const void **agent_interface, const struct module_interface **ops);
+
+/**
+ * Destroy userspace module proxy
+ *
+ * @param drv - pointer to component driver
+ * @param user_ctx - pointer to userspace module context
+ */
+void userspace_proxy_destroy(const struct comp_driver *drv, struct userspace_context *user_ctx);
+
+#endif /* CONFIG_SOF_USERSPACE_PROXY */
 
 #endif /* __SOF_AUDIO_USERSPACE_PROXY_H__ */
