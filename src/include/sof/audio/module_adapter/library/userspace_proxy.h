c72675f31be61b6191696f06d1e012323b9faba3
Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
2023-09-15 14:49:23 +0200

    userspace: Introduce userspace proxy and user worker to service IPC
    The non-privileged modules code should be executed in separate thread
    executed in userspace mode. This way the code and data owned by such
    module will be isolated from other non-privileged modules in the system.
    
    The implementation creates user_work_queue threads that will service all
    IPC's for all such modules. There are security domains defined that will
    combines service of IPC's for modules that could exist without isolation
    (eg. several instances of the same module or modules originating from single
    vendor).
    
    Signed-off-by: Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/include/sof/audio/module_adapter/library/userspace_proxy.h
+++ src/include/sof/audio/module_adapter/library/userspace_proxy.h
@@ -14,12 +14,59 @@
 #include <stdbool.h>
 
 #include <zephyr/kernel.h>
+#include <zephyr/app_memory/app_memdomain.h>
+#include <utilities/array.h>
+
+#define APP_USER_DATA	K_APP_DMEM(ipc_partition)
+#define APP_USER_BSS	K_APP_BMEM(ipc_partition)
+
+#include <native_system_agent.h>
+#include <module/module/interface.h>
+
+struct module_interface;
+struct comp_driver;
+struct sof_man_module;
 
 /* Processing module structure fields needed for user mode */
 struct userspace_context {
 	struct k_mem_domain *comp_dom;			/* Module specific memory domain	*/
+	uint32_t security_domain_id;			/* Determine security domain id number	*/
+	void *wrk_ctx;					/* Work queue context			*/
+	const struct module_interface *interface;	/* Userspace module interface		*/
 };
+/**
+* Creates userspace module proxy
+*
+* @param user_ctx - pointer to pointer of userspace module context
+* @param drv - pointer to component driver
+* @param manifest - pointer to module manifest
+* @param agent - pointer to system agent start function
+* @param entry_point - loadable module entry point
+* @param module_id - module id
+* @param instance_id - instance id
+* @param core_id - core id - unused
+* @param log_handle - log handle
+* @param mod_cfg - ipc4 module configuration
+* @param interface - pointer to variable to store created module interface
+* @return 0 for success, error otherwise.
+*/
+int userspace_proxy_create(struct userspace_context **user_ctx, const struct comp_driver *drv,
+			   const struct sof_man_module *manifest, system_agent_start_fn start_fn,
+			   uintptr_t entry_point, uint32_t module_id, uint32_t instance_id,
+			   uint32_t core_id, uint32_t log_handle, byte_array_t *mod_cfg,
+			   const void **adapter);
+
+/**
+* Creates userspace module proxy
+* 
+* @param drv - pointer to component driver
+* @param user_ctx - pointer to userspace module context
+*/
+void userspace_proxy_destroy(const struct comp_driver *drv, struct userspace_context *user_ctx);
+
+#else
+
+#define APP_USER_DATA
 
 #endif /* CONFIG_USERSPACE */
-
 #endif /* __SOF_AUDIO_USERSPACE_PROXY_H__ */
