d93e32fb3949fe1ee8fcfe9075ab3534b552ea49
Adrian Warecki <adrian.warecki@intel.com>
2023-09-18 15:11:09 +0200

    smex: elf: elf_find_section: Check function input data
    String terminator was added to the buffer with a list of section names
    in the elf file. Added check to the section name index to make sure it
    doesn't go beyond the buffer size.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>
    (cherry picked from commit 5b837af2237ec609421ddeb1d5f3032e660a9a9a)

--- smex/elf.c
+++ smex/elf.c
@@ -408,10 +408,17 @@ int elf_find_section(const struct elf_module *module, const char *name)
 		ret = -errno;
 		goto out;
 	}
+	buffer[section->size - 1] = '\0';
 
 	/* find section with name */
 	for (i = 0; i < hdr->shnum; i++) {
 		s = &module->section[i];
+		if (s->name >= section->size) {
+			fprintf(stderr, "error: invalid section name string index %d\n", s->name);
+			ret = -EINVAL;
+			goto out;
+		}
+
 		if (!strcmp(name, buffer + s->name)) {
 			ret = i;
 			goto out;
