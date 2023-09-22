b98e912fff563a24774135698d626fc40ee5999b
Tomasz Leman <tomasz.m.leman@intel.com>
2023-09-22 10:10:36 +0200

    platform: posix: clk: add lowest freq index
    This patch adds CPU_LOWEST_FREQ_IDX definition to keep compliance with
    changes in base_fw.
    
    Signed-off-by: Tomasz Leman <tomasz.m.leman@intel.com>

--- src/platform/posix/include/platform/lib/clk.h
+++ src/platform/posix/include/platform/lib/clk.h
@@ -5,6 +5,7 @@
 
 #define CLK_MAX_CPU_HZ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
 #define CPU_LPRO_FREQ_IDX 1
+#define CPU_LOWEST_FREQ_IDX CPU_LPRO_FREQ_IDX
 
 /* This is not a platform function, it's defined in src/lib/clk.c.
  * But the declaration has historically been in the platform layer, so
