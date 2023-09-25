3d040c72430c30333dc4823547906dd33671bdae
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2023-09-25 18:42:51 +0300

    Tools: Testbench: Add Google RTC with mockup to testbench
    This patch allows to load and run it in testbench.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/testbench/common_test.c
+++ tools/testbench/common_test.c
@@ -54,6 +54,7 @@ int tb_setup(struct sof *sof, struct testbench_prm *tp)
 	sys_comp_module_drc_interface_init();
 	sys_comp_module_eq_fir_interface_init();
 	sys_comp_module_eq_iir_interface_init();
+	sys_comp_module_google_rtc_audio_processing_interface_init();
 	sys_comp_module_multiband_drc_interface_init();
 	sys_comp_module_mux_interface_init();
 	sys_comp_module_src_interface_init();
