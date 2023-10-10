2d70bb50a7ab561a14f59f9fecc60d011a79d479
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2023-10-10 19:15:46 +0300

    Audio: IGO_NR: Set in stub handle size to prevent init error
    Function igo_nr_init() expects a nonzero value for rballoc()
    size so this can't be left zero. The value is just something
    for stub usage that felt suitable for an unknown.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/igo_nr/igo_nr_stub.c
+++ src/audio/igo_nr/igo_nr_stub.c
@@ -6,8 +6,15 @@
 
 #include <sof/audio/igo_nr/igo_nr_comp.h>
 
+/* Set handle_size to Just something instead of 0 from rzalloc() of
+ * component data to avoid rballoc() in igo_nr_init() to fail.
+ */
+#define IGO_NR_STUB_HANDLE_SIZE		42
+
 enum IgoRet IgoLibGetInfo(struct IgoLibInfo *info)
 {
+	info->handle_size = IGO_NR_STUB_HANDLE_SIZE;
+
 	return IGO_RET_OK;
 }
 
