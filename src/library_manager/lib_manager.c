a41e6073de21966ba80e111394d5f6e989163fc7
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2023-10-24 17:56:31 +0200

    module: fix cache synchronisation
    Buffers, where modules are stored originally, don't need their caches
    to be synchronised - they're only used as a data storage and only
    accessed via cached aliases. But when final buffers are allocated,
    from which modules are actually run, those buffers do indeed need
    cache synchronisation.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/library_manager/lib_manager.c
+++ src/library_manager/lib_manager.c
@@ -614,9 +614,6 @@ static void __sparse_cache *lib_manager_allocate_store_mem(uint32_t size,
 		return NULL;
 	}
 
-	dcache_invalidate_region(local_add, size);
-	icache_invalidate_region(local_add, size);
-
 	return local_add;
 }
 
