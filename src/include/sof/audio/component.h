04991becea3d24ecabed60519979b4c4d2b12e04
Joe Cheng <joe.cheng@xperi.com>
2023-11-01 17:42:16 +0800

    audio : dts : add DTS support for MTL
    Add DTS init function
    
    Signed-off-by: Joe Cheng <joe.cheng@xperi.com>

--- src/include/sof/audio/component.h
+++ src/include/sof/audio/component.h
@@ -718,6 +718,7 @@ void sys_comp_module_crossover_interface_init(void);
 void sys_comp_module_dcblock_interface_init(void);
 void sys_comp_module_demux_interface_init(void);
 void sys_comp_module_drc_interface_init(void);
+void sys_comp_module_dts_interface_init(void);
 void sys_comp_module_eq_fir_interface_init(void);
 void sys_comp_module_eq_iir_interface_init(void);
 void sys_comp_module_mfcc_interface_init(void);
