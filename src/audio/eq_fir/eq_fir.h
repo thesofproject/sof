537b485aebb89d06581209114e01d13ddb29f808
Baofeng Tian <baofeng.tian@intel.com>
2023-11-03 14:46:10 +0800

    Audio: eq_fir: move out eq_fir ipc3 and ipc4 specific code
    Move out ipc3 and ipc4 specific code to corresponding source file.
    Also, move some common functions to header file.
    
    Signed-off-by: Baofeng Tian <baofeng.tian@intel.com>

--- src/audio/eq_fir/eq_fir.h
+++ src/audio/eq_fir/eq_fir.h
@@ -28,6 +28,20 @@
 #define EQ_FIR_BYTES_TO_S16_SAMPLES(b)	((b) >> 1)
 #define EQ_FIR_BYTES_TO_S32_SAMPLES(b)	((b) >> 2)
 
+/* fir component private data */
+struct comp_data {
+	struct fir_state_32x16 fir[PLATFORM_MAX_CHANNELS]; /**< filters state */
+	struct comp_data_blob_handler *model_handler;
+	struct sof_eq_fir_config *config;
+	int32_t *fir_delay;			/**< pointer to allocated RAM */
+	size_t fir_delay_size;			/**< allocated size */
+	void (*eq_fir_func)(struct fir_state_32x16 fir[],
+			    struct input_stream_buffer *bsource,
+			    struct output_stream_buffer *bsink,
+			    int frames);
+	int nch;
+};
+
 #if CONFIG_FORMAT_S16LE
 void eq_fir_s16(struct fir_state_32x16 *fir, struct input_stream_buffer *bsource,
 		struct output_stream_buffer *bsink, int frames);
@@ -51,6 +65,57 @@ void eq_fir_s32(struct fir_state_32x16 *fir, struct input_stream_buffer *bsource
 void eq_fir_2x_s32(struct fir_state_32x16 *fir, struct input_stream_buffer *bsource,
 		   struct output_stream_buffer *bsink, int frames);
 #endif /* CONFIG_FORMAT_S32LE */
+
+int set_fir_func(struct processing_module *mod, enum sof_ipc_frame fmt);
+
+int eq_fir_params(struct processing_module *mod);
+
+/*
+ * The optimized FIR functions variants need to be updated into function
+ * set_fir_func.
+ */
+
+#if FIR_HIFI3 || FIR_HIFIEP
+#if CONFIG_FORMAT_S16LE
+static inline void set_s16_fir(struct comp_data *cd)
+{
+	cd->eq_fir_func = eq_fir_2x_s16;
+}
+#endif /* CONFIG_FORMAT_S16LE */
+#if CONFIG_FORMAT_S24LE
+static inline void set_s24_fir(struct comp_data *cd)
+{
+	cd->eq_fir_func = eq_fir_2x_s24;
+}
+#endif /* CONFIG_FORMAT_S24LE */
+#if CONFIG_FORMAT_S32LE
+static inline void set_s32_fir(struct comp_data *cd)
+{
+	cd->eq_fir_func = eq_fir_2x_s32;
+}
+#endif /* CONFIG_FORMAT_S32LE */
+
+#else
+/* FIR_GENERIC */
+#if CONFIG_FORMAT_S16LE
+static inline void set_s16_fir(struct comp_data *cd)
+{
+	cd->eq_fir_func = eq_fir_s16;
+}
+#endif /* CONFIG_FORMAT_S16LE */
+#if CONFIG_FORMAT_S24LE
+static inline void set_s24_fir(struct comp_data *cd)
+{
+	cd->eq_fir_func = eq_fir_s24;
+}
+#endif /* CONFIG_FORMAT_S24LE */
+#if CONFIG_FORMAT_S32LE
+static inline void set_s32_fir(struct comp_data *cd)
+{
+	cd->eq_fir_func = eq_fir_s32;
+}
+#endif /* CONFIG_FORMAT_S32LE */
+#endif
 
 #ifdef UNIT_TEST
 void sys_comp_module_eq_fir_interface_init(void);
---
+++ src/audio/eq_fir/eq_fir_ipc3.c
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2017 Intel Corporation. All rights reserved.
+//
+// Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
+//         Liam Girdwood <liam.r.girdwood@linux.intel.com>
+//         Keyon Jie <yang.jie@linux.intel.com>
+
+#include <sof/audio/module_adapter/module/generic.h>
+#include <ipc/stream.h>
+#include <sof/audio/component.h>
+#include <sof/trace/trace.h>
+#include <errno.h>
+
+#include "eq_fir.h"
+
+LOG_MODULE_DECLARE(eq_fir, CONFIG_SOF_LOG_LEVEL);
+
+int set_fir_func(struct processing_module *mod, enum sof_ipc_frame fmt)
+{
+	struct comp_data *cd = module_get_private_data(mod);
+
+	switch (fmt) {
+#if CONFIG_FORMAT_S16LE
+	case SOF_IPC_FRAME_S16_LE:
+		comp_dbg(mod->dev, "set_fir_func(), SOF_IPC_FRAME_S16_LE");
+		set_s16_fir(cd);
+		break;
+#endif /* CONFIG_FORMAT_S16LE */
+#if CONFIG_FORMAT_S24LE
+	case SOF_IPC_FRAME_S24_4LE:
+		comp_dbg(mod->dev, "set_fir_func(), SOF_IPC_FRAME_S24_4LE");
+		set_s24_fir(cd);
+		break;
+#endif /* CONFIG_FORMAT_S24LE */
+#if CONFIG_FORMAT_S32LE
+	case SOF_IPC_FRAME_S32_LE:
+		comp_dbg(mod->dev, "set_fir_func(), SOF_IPC_FRAME_S32_LE");
+		set_s32_fir(cd);
+		break;
+#endif /* CONFIG_FORMAT_S32LE */
+	default:
+		comp_err(mod->dev, "set_fir_func(), invalid frame_fmt");
+		return -EINVAL;
+	}
+	return 0;
+}
+
+int eq_fir_params(struct processing_module *mod)
+{
+	return 0;
+}
+
