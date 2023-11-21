f4cfc7a791a41d3f867e2d3d33dcae7d270107d3
Serhiy Katsyuba <serhiy.katsyuba@intel.com>
2023-11-21 16:41:09 +0100

    ipc4: mixin/mixout: Remove redundant mute channel impl
    Mute channel functionality was previously used as part of channel
    remapping feature. Since that feature was removed some time ago mute
    channel functionality is no longer used and is just a leftover from not
    fully completed cleanup.
    
    Signed-off-by: Serhiy Katsyuba <serhiy.katsyuba@intel.com>

--- src/audio/mixin_mixout/mixin_mixout.c
+++ src/audio/mixin_mixout/mixin_mixout.c
@@ -76,7 +76,6 @@ struct mixin_sink_config {
 /* mixin component private data */
 struct mixin_data {
 	normal_mix_func normal_mix_channel;
-	mute_func mute_channel;
 	struct mixin_sink_config sink_config[MIXIN_MAX_SINKS];
 };
 
@@ -510,7 +509,6 @@ static int mixin_reset(struct processing_module *mod)
 	comp_dbg(dev, "mixin_reset()");
 
 	mixin_data->normal_mix_channel = NULL;
-	mixin_data->mute_channel = NULL;
 
 	return 0;
 }
@@ -636,14 +634,13 @@ static int mixin_prepare(struct processing_module *mod,
 	case SOF_IPC_FRAME_S24_4LE:
 	case SOF_IPC_FRAME_S32_LE:
 		md->normal_mix_channel = normal_mix_get_processing_function(fmt);
-		md->mute_channel = mute_mix_get_processing_function(fmt);
 		break;
 	default:
 		comp_err(dev, "unsupported data format %d", fmt);
 		return -EINVAL;
 	}
 
-	if (!md->normal_mix_channel || !md->mute_channel) {
+	if (!md->normal_mix_channel) {
 		comp_err(dev, "have not found the suitable processing function");
 		return -EINVAL;
 	}
