thesofproject/sof

commit 16d126a36fd82113a7f09c2c9d490309a62d0ca4

audio: Header files cleanup
Removed unused includes from a header files.

Signed-off-by: Pawel Dobrowolski <pawelx.dobrowolski@intel.com>

src/include/sof/common.h
@@ -27,7 +27,6 @@
 #if !defined(__ASSEMBLER__) && defined(__XTENSA__)
 
 #include <ipc/trace.h>
-#include <rtos/panic.h>
 #define VERIFY_ALIGN
 
 #endif

src/include/ipc4/up_down_mixer.h
@@ -7,7 +7,6 @@
 #ifndef __SOF_IPC4_UP_DOWN_MIXER_H__
 #define __SOF_IPC4_UP_DOWN_MIXER_H__
 
-#include <sof/compiler_attributes.h>
 #include "base-config.h"
 
 /**

src/include/sof/audio/coefficients/up_down_mixer/up_down_mixer.h
@@ -6,7 +6,6 @@
 // Author: Adrian Bonislawski <adrian.bonislawski@intel.com>
 
 #include <ipc4/up_down_mixer.h>
-#include <sof/audio/format.h>
 #include <stdint.h>
 
 #if CONFIG_COMP_UP_DOWN_MIXER

src/include/sof/audio/component.h
@@ -19,29 +19,13 @@
 #include <sof/audio/buffer.h>
 #include <sof/audio/format.h>
 #include <sof/audio/pipeline.h>
-#include <rtos/panic.h>
 #include <rtos/idc.h>
-#include <sof/common.h>
-#include <sof/list.h>
-#include <rtos/alloc.h>
-#include <sof/lib/cpu.h>
 #include <sof/lib/dai.h>
-#include <sof/lib/memory.h>
-#include <sof/lib/perf_cnt.h>
-#include <sof/math/numbers.h>
 #include <sof/schedule/schedule.h>
-#include <rtos/sof.h>
-#include <sof/trace/trace.h>
 #include <ipc/control.h>
-#include <ipc/stream.h>
 #include <sof/ipc/topology.h>
 #include <kernel/abi.h>
-#include <user/trace.h>
 
-#include <errno.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <limits.h>
 
 struct comp_dev;

src/include/sof/audio/component_ext.h
@@ -17,11 +17,7 @@
 #define __SOF_AUDIO_COMPONENT_INT_H__
 
 #include <sof/audio/component.h>
-#include <rtos/idc.h>
-#include <sof/list.h>
 #include <ipc/topology.h>
-#include <kernel/abi.h>
-#include <stdbool.h>
 
 /** \addtogroup component_api_helpers Component Mgmt API
  *  @{

src/include/sof/audio/up_down_mixer/up_down_mixer.h
@@ -10,14 +10,11 @@
 
 #include <sof/audio/component_ext.h>
 #include <sof/audio/ipc-config.h>
-#include <rtos/bit.h>
 #include <sof/common.h>
-#include <sof/trace/trace.h>
 #include <ipc/stream.h>
 #include <ipc4/up_down_mixer.h>
 #include <ipc4/module.h>
 #include <ipc4/base-config.h>
-#include <user/trace.h>
 #include <stddef.h>
 #include <stdint.h>
 

commit f2b2f80ccfc9e9a6d687d3257e62bc4c0ffb94a3

include: sof: common.h Fix compilation error
Commit 16d126a36fd821 ("audio: Header files cleanup") removed
rtos/panic.h include but this is still needed.

Otherwise, we get the following compilation error:

/work/repos/sof/src/lib/lib.c: In function ‘memset’:
/work/repos/sof/src/lib/lib.c:31: warning: implicit declaration of function ‘sof_panic’

Fixes: 16d126a36fd821 ("audio: Header files cleanup")
Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

src/include/sof/common.h
@@ -27,6 +27,7 @@
 #if !defined(__ASSEMBLER__) && defined(__XTENSA__)
 
 #include <ipc/trace.h>
+#include <rtos/panic.h>
 #define VERIFY_ALIGN
 
 #endif