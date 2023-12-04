c4d478556ecfa8d71687dd950efe3065e8c14203
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2023-12-04 10:27:19 +0200

    topology2: Rename USE_CHAIN_DMA to HDMI_USE_CHAIN_DMA
    In practice all HDA links (on LNL SDW/SSP/DMIC also) can use ChainDMA, but
    the flag is used to enable this only for display links.
    
    Rename the flag to be precise about this.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
    (cherry picked from commit cf473b5aa738d813e1b5143638b6036fd9cf0617)
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- tools/topology/topology2/avs-tplg/tplg-targets.cmake
+++ tools/topology/topology2/avs-tplg/tplg-targets.cmake
@@ -3,17 +3,18 @@
 # Array of "input-file-name;output-file-name;comma separated pre-processor variables"
 set(TPLGS
 # CAVS HDMI only topology with passthrough pipelines
-"sof-hda-generic\;sof-hda-generic-idisp\;USE_CHAIN_DMA=true,DEEPBUFFER_FW_DMA_MS=100"
+"sof-hda-generic\;sof-hda-generic-idisp\;HDMI_USE_CHAIN_DMA=true,DEEPBUFFER_FW_DMA_MS=100"
 # CAVS HDA topology with mixer-based pipelines for HDA and passthrough pipelines for HDMI
-"sof-hda-generic\;sof-hda-generic\;HDA_CONFIG=mix,USE_CHAIN_DMA=true,DEEPBUFFER_FW_DMA_MS=100"
+"sof-hda-generic\;sof-hda-generic\;HDA_CONFIG=mix,HDMI_USE_CHAIN_DMA=true,DEEPBUFFER_FW_DMA_MS=100"
 # If the alsatplg plugins for NHLT are not available, the NHLT blobs will not be added to the
 # topologies below.
 "sof-hda-generic\;sof-hda-generic-2ch\;\
-HDA_CONFIG=mix,NUM_DMICS=2,PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-hda-generic-2ch.bin,USE_CHAIN_DMA=true,\
-DEEPBUFFER_FW_DMA_MS=100"
+HDA_CONFIG=mix,NUM_DMICS=2,PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-hda-generic-2ch.bin,\
+HDMI_USE_CHAIN_DMA=true,DEEPBUFFER_FW_DMA_MS=100"
 "sof-hda-generic\;sof-hda-generic-4ch\;\
-HDA_CONFIG=mix,NUM_DMICS=4,PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,\
-PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-hda-generic-4ch.bin,USE_CHAIN_DMA=true,DEEPBUFFER_FW_DMA_MS=100"
+HDA_CONFIG=mix,NUM_DMICS=4,PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-hda-generic-4ch.bin,\
+HDMI_USE_CHAIN_DMA=true,DEEPBUFFER_FW_DMA_MS=100,\
+PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1"
 
 # IPC4 topology for TGL rt711 Headset + rt1316 Amplifier + rt714 DMIC
 "cavs-sdw\;sof-tgl-rt711-rt1316-rt714\;NUM_SDW_AMP_LINKS=2,SDW_DMIC=1,\
