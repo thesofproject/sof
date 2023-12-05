7b8068963cbdaee5f722eaec5d4ac8df189782f2
Marc Herbert <marc.herbert@intel.com>
2023-12-05 05:24:17 +0000

    scripts: rename XTENSA_TOOLS_VERSION to TOOLCHAIN_VER as in Zephyr
    Rename the variable name used by SOF script with the one expected by the
    Zephyr build system for simplicity and consistency.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- scripts/set_xtensa_params.sh
+++ scripts/set_xtensa_params.sh
@@ -32,7 +32,7 @@ case "$platform" in
 	PLATFORM="tgplp"
 	XTENSA_CORE="cavs2x_LX6HiFi3_2017_8"
 	HOST="xtensa-cnl-elf"
-	XTENSA_TOOLS_VERSION="RG-2017.8-linux"
+	TOOLCHAIN_VER="RG-2017.8-linux"
 	HAVE_ROM='yes'
 	IPC4_CONFIG_OVERLAY="tigerlake_ipc4"
 	# default key for TGL
@@ -42,7 +42,7 @@ case "$platform" in
 	PLATFORM="tgph"
 	XTENSA_CORE="cavs2x_LX6HiFi3_2017_8"
 	HOST="xtensa-cnl-elf"
-	XTENSA_TOOLS_VERSION="RG-2017.8-linux"
+	TOOLCHAIN_VER="RG-2017.8-linux"
 	HAVE_ROM='yes'
 	# default key for TGL
 	PLATFORM_PRIVATE_KEY="-D${SIGNING_TOOL}_PRIVATE_KEY=$SOF_TOP/keys/otc_private_key_3k.pem"
@@ -50,7 +50,7 @@ case "$platform" in
     mtl|lnl)
 	PLATFORM="$platform"
 	XTENSA_CORE="ace10_LX7HiFi4_2022_10"
-	XTENSA_TOOLS_VERSION="RI-2022.10-linux"
+	TOOLCHAIN_VER="RI-2022.10-linux"
 	# TODO: to reduce duplication, make rebuild-testbench "smarter"
 	# and able to decode ZEPHYR_TOOLCHAIN_VARIANT (even when it does
 	# not care about Zephyr)
@@ -62,25 +62,25 @@ case "$platform" in
 	PLATFORM="imx8"
 	XTENSA_CORE="hifi4_nxp_v3_3_1_2_2017"
 	HOST="xtensa-imx-elf"
-	XTENSA_TOOLS_VERSION="RG-2017.8-linux"
+	TOOLCHAIN_VER="RG-2017.8-linux"
 	;;
     imx8x)
 	PLATFORM="imx8x"
 	XTENSA_CORE="hifi4_nxp_v3_3_1_2_2017"
 	HOST="xtensa-imx-elf"
-	XTENSA_TOOLS_VERSION="RG-2017.8-linux"
+	TOOLCHAIN_VER="RG-2017.8-linux"
 	;;
     imx8m)
 	PLATFORM="imx8m"
 	XTENSA_CORE="hifi4_mscale_v0_0_2_2017"
 	HOST="xtensa-imx8m-elf"
-	XTENSA_TOOLS_VERSION="RG-2017.8-linux"
+	TOOLCHAIN_VER="RG-2017.8-linux"
 	;;
     imx8ulp)
 	PLATFORM="imx8ulp"
 	XTENSA_CORE="hifi4_nxp2_ulp_prod"
 	HOST="xtensa-imx8ulp-elf"
-	XTENSA_TOOLS_VERSION="RG-2017.8-linux"
+	TOOLCHAIN_VER="RG-2017.8-linux"
 	;;
 
     # AMD
@@ -88,28 +88,28 @@ case "$platform" in
 	PLATFORM="renoir"
 	XTENSA_CORE="ACP_3_1_001_PROD_2019_1"
 	HOST="xtensa-rn-elf"
-	XTENSA_TOOLS_VERSION="RI-2019.1-linux"
+	TOOLCHAIN_VER="RI-2019.1-linux"
 	;;
     rmb)
 	PLATFORM="rembrandt"
 	ARCH="xtensa"
 	XTENSA_CORE="LX7_HiFi5_PROD"
 	HOST="xtensa-rmb-elf"
-	XTENSA_TOOLS_VERSION="RI-2019.1-linux"
+	TOOLCHAIN_VER="RI-2019.1-linux"
 	;;
     vangogh)
 	PLATFORM="vangogh"
 	ARCH="xtensa"
 	XTENSA_CORE="ACP_5_0_001_PROD"
 	HOST="xtensa-vangogh-elf"
-	XTENSA_TOOLS_VERSION="RI-2019.1-linux"
+	TOOLCHAIN_VER="RI-2019.1-linux"
 	;;
     acp_6_3)
 	PLATFORM="acp_6_3"
 	ARCH="xtensa"
 	XTENSA_CORE="ACP_6_3_HiFi5_PROD_Linux"
 	HOST="xtensa-acp_6_3-elf"
-	XTENSA_TOOLS_VERSION="RI-2021.6-linux"
+	TOOLCHAIN_VER="RI-2021.6-linux"
 	;;
 
     # Mediatek
@@ -117,19 +117,19 @@ case "$platform" in
 	PLATFORM="mt8186"
 	XTENSA_CORE="hifi5_7stg_I64D128"
 	HOST="xtensa-mt8186-elf"
-	XTENSA_TOOLS_VERSION="RI-2020.5-linux"
+	TOOLCHAIN_VER="RI-2020.5-linux"
 	;;
     mt8188)
 	PLATFORM="mt8188"
 	XTENSA_CORE="hifi5_7stg_I64D128"
 	HOST="xtensa-mt8188-elf"
-	XTENSA_TOOLS_VERSION="RI-2020.5-linux"
+	TOOLCHAIN_VER="RI-2020.5-linux"
 	;;
     mt8195)
 	PLATFORM="mt8195"
 	XTENSA_CORE="hifi4_8195_PROD"
 	HOST="xtensa-mt8195-elf"
-	XTENSA_TOOLS_VERSION="RI-2019.1-linux"
+	TOOLCHAIN_VER="RI-2019.1-linux"
 	;;
 
     *)
