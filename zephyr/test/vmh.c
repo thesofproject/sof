3e2f073d40d7397ae9c000d6dc7803f85fb3c762
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2023-12-06 17:47:25 +0100

    test: add a simple VMH test
    This adds an initial Virtual Memory Heap test.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

---
+++ zephyr/test/vmh.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright(c) 2023 Intel Corporation. All rights reserved.
+ *
+ * Author: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
+ */
+
+#include <errno.h>
+#include <stdbool.h>
+
+#include <adsp_memory_regions.h>
+#include <sof/boot_test.h>
+#include <sof/lib/regions_mm.h>
+
+#include <zephyr/logging/log.h>
+#include <zephyr/ztest.h>
+
+LOG_MODULE_DECLARE(sof_boot_test, CONFIG_SOF_LOG_LEVEL);
+
+static int vmh_test(void)
+{
+	struct vmh_heap *h = vmh_init_heap(NULL, MEM_REG_ATTR_CORE_HEAP, 0, false);
+
+	if (!h)
+		return -ENOMEM;
+
+	return vmh_free_heap(h);
+}
+
+ZTEST(sof_boot, virtual_memory_heap)
+{
+	int ret = vmh_test();
+
+	TEST_CHECK_RET(ret, "virtual_memory_heap");
+}
