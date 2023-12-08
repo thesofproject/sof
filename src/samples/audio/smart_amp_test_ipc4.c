6d3061c76f23f5e92ec89578476006f6fe3b4cd4
Rander Wang <rander.wang@intel.com>
2023-12-08 13:38:29 +0800

    lmdk: add dummy loadable smart amp support
    It will be built will __SOF_MODULE_SERVICE_BUILD__ enabled
    
    Signed-off-by: Rander Wang <rander.wang@intel.com>

--- src/samples/audio/smart_amp_test_ipc4.c
+++ src/samples/audio/smart_amp_test_ipc4.c
@@ -3,8 +3,9 @@
 // Copyright(c) 2020 Intel Corporation. All rights reserved.
 //
 // Author: Bartosz Kokoszko <bartoszx.kokoszko@linux.intel.com>
+
+#ifndef __SOF_MODULE_SERVICE_BUILD__
 #include <sof/compiler_attributes.h>
-#include <sof/samples/audio/smart_amp_test.h>
 #include <sof/audio/module_adapter/module/generic.h>
 #include <sof/audio/ipc-config.h>
 #include <sof/trace/trace.h>
@@ -18,6 +19,7 @@
 #include <ipc4/module.h>
 
 LOG_MODULE_REGISTER(smart_amp_test, CONFIG_SOF_LOG_LEVEL);
+#include <zephyr/logging/log.h>
 
 /* 167a961e-8ae4-11ea-89f1-000c29ce1635 */
 DECLARE_SOF_RT_UUID("smart_amp-test", smart_amp_test_comp_uuid, 0x167a961e, 0x8ae4,
@@ -25,6 +27,22 @@ DECLARE_SOF_RT_UUID("smart_amp-test", smart_amp_test_comp_uuid, 0x167a961e, 0x8a
 
 DECLARE_TR_CTX(smart_amp_test_comp_tr, SOF_UUID(smart_amp_test_comp_uuid),
 	       LOG_LEVEL_INFO);
+#else
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include <module/base.h>
+#include <module/api_ver.h>
+#include <module/interface.h>
+#include <iadk/adsp_error_code.h>
+#include <rimage/sof/user/manifest.h>
+#include <audio/source_api.h>
+#include <audio/sink_api.h>
+#include <ipc4/module.h>
+#include <sof/math/numbers.h>
+#endif
+#include <sof/samples/audio/smart_amp_test.h>
 
 typedef void (*smart_amp_proc)(int8_t const *src_ptr,
 			       int8_t const *src_begin,
@@ -37,57 +55,53 @@ typedef void (*smart_amp_proc)(int8_t const *src_ptr,
 struct smart_amp_data {
 	struct sof_smart_amp_ipc4_config ipc4_cfg;
 	struct sof_smart_amp_config config;
-	struct comp_data_blob_handler *model_handler;
-	void *data_blob;
-	size_t data_blob_size;
 	smart_amp_proc process;
 	uint32_t out_channels;
 };
 
+#ifdef __SOF_MODULE_SERVICE_BUILD__
+static struct smart_amp_data smart_amp_priv;
+#endif
+
 static int smart_amp_init(struct processing_module *mod)
 {
 	struct smart_amp_data *sad;
-	struct comp_dev *dev = mod->dev;
 	struct module_data *mod_data = &mod->priv;
-	const size_t in_size = sizeof(struct ipc4_input_pin_format) * SMART_AMP_NUM_IN_PINS;
-	const size_t out_size = sizeof(struct ipc4_output_pin_format) * SMART_AMP_NUM_OUT_PINS;
 	int ret;
 	const struct ipc4_base_module_extended_cfg *base_cfg = mod_data->cfg.init_data;
 
-	comp_dbg(dev, "smart_amp_init()");
+	LOG_DBG("smart_amp_init()");
+
+#ifndef __SOF_MODULE_SERVICE_BUILD__
 	sad = rzalloc(SOF_MEM_ZONE_RUNTIME, 0, SOF_MEM_CAPS_RAM, sizeof(*sad));
 	if (!sad)
 		return -ENOMEM;
-
+#else
+	sad = &smart_amp_priv;
+#endif
 	mod_data->private = sad;
-
-	/* component model data handler */
-	sad->model_handler = comp_data_blob_handler_new(dev);
-	if (!sad->model_handler) {
-		ret = -ENOMEM;
-		goto sad_fail;
-	}
 
 	if (base_cfg->base_cfg_ext.nb_input_pins != SMART_AMP_NUM_IN_PINS ||
 	    base_cfg->base_cfg_ext.nb_output_pins != SMART_AMP_NUM_OUT_PINS) {
-		comp_err(dev, "smart_amp_init(): Invalid pin configuration");
 		ret = -EINVAL;
 		goto sad_fail;
 	}
 
 	/* Copy the pin formats */
-	memcpy_s(sad->ipc4_cfg.input_pins, in_size,
-		 base_cfg->base_cfg_ext.pin_formats, in_size);
-	memcpy_s(&sad->ipc4_cfg.output_pin, out_size,
-		 &base_cfg->base_cfg_ext.pin_formats[in_size], out_size);
-
-	mod->max_sources = SMART_AMP_NUM_IN_PINS;
+	sad->ipc4_cfg.input_pins[0] =
+		*((struct ipc4_input_pin_format *)base_cfg->base_cfg_ext.pin_formats);
+	sad->ipc4_cfg.input_pins[1] =
+		*((struct ipc4_input_pin_format *)base_cfg->base_cfg_ext.pin_formats + 1);
+	sad->ipc4_cfg.output_pin =
+		*(struct ipc4_output_pin_format *)(base_cfg->base_cfg_ext.pin_formats
+			+ sizeof(sad->ipc4_cfg.input_pins));
 
 	return 0;
 
 sad_fail:
-	comp_data_blob_handler_free(sad->model_handler);
+#ifndef __SOF_MODULE_SERVICE_BUILD__
 	rfree(sad);
+#endif
 	return ret;
 }
 
@@ -96,23 +110,21 @@ static int smart_amp_set_config(struct processing_module *mod, uint32_t config_i
 				const uint8_t *fragment, size_t fragment_size, uint8_t *response,
 				size_t response_size)
 {
-	struct comp_dev *dev = mod->dev;
 	struct smart_amp_data *sad = module_get_private_data(mod);
 
-	comp_dbg(dev, "smart_amp_set_config()");
+	LOG_DBG("smart_amp_set_config()");
 
 	switch (config_id) {
 	case SMART_AMP_SET_MODEL:
-		return comp_data_blob_set(sad->model_handler, pos,
-					 data_offset_size, fragment, fragment_size);
+		return 0;
 	case SMART_AMP_SET_CONFIG:
 		if (fragment_size != sizeof(sad->config)) {
-			comp_err(dev, "smart_amp_set_config(): invalid config size %u, expect %u",
-				 fragment_size, sizeof(struct sof_smart_amp_config));
+			LOG_ERR("smart_amp_set_config(): invalid config size %u, expect %u",
+				fragment_size, sizeof(struct sof_smart_amp_config));
 			return -EINVAL;
 		}
-		comp_dbg(dev, "smart_amp_set_config(): config size = %u", fragment_size);
-		memcpy_s(&sad->config, sizeof(sad->config), fragment, fragment_size);
+		LOG_DBG("smart_amp_set_config(): config size = %u", fragment_size);
+		sad->config = *(struct sof_smart_amp_config *)fragment;
 		return 0;
 	default:
 		return -EINVAL;
@@ -124,19 +136,12 @@ static inline int smart_amp_get_config(struct processing_module *mod,
 				       uint8_t *fragment, size_t fragment_size)
 {
 	struct smart_amp_data *sad = module_get_private_data(mod);
-	struct comp_dev *dev = mod->dev;
-	int ret;
 
-	comp_dbg(dev, "smart_amp_get_config()");
+	LOG_DBG("smart_amp_get_config()");
 
 	switch (config_id) {
 	case SMART_AMP_GET_CONFIG:
-		ret = memcpy_s(fragment, fragment_size, &sad->config, sizeof(sad->config));
-		if (ret) {
-			comp_err(dev, "smart_amp_get_config(): wrong config size %d",
-				 fragment_size);
-			return ret;
-		}
+		 *(struct sof_smart_amp_config *)fragment = sad->config;
 		*data_offset_size = sizeof(sad->config);
 		return 0;
 	default:
@@ -146,12 +151,14 @@ static inline int smart_amp_get_config(struct processing_module *mod,
 
 static int smart_amp_free(struct processing_module *mod)
 {
-	struct smart_amp_data *sad = module_get_private_data(mod);
-	struct comp_dev *dev = mod->dev;
+	LOG_DBG("smart_amp_free()");
 
-	comp_dbg(dev, "smart_amp_free()");
-	comp_data_blob_handler_free(sad->model_handler);
+#ifndef __SOF_MODULE_SERVICE_BUILD__
+	struct smart_amp_data *sad = module_get_private_data(mod);
+
 	rfree(sad);
+#endif
+
 	return 0;
 }
 
@@ -270,8 +277,6 @@ static int smart_amp_process(struct processing_module *mod,
 			     struct sof_source **sources, int num_of_sources,
 			     struct sof_sink **sinks, int num_of_sinks)
 {
-	struct smart_amp_data *sad = module_get_private_data(mod);
-	struct comp_dev *dev = mod->dev;
 	struct sof_source *fb_input = NULL;
 	/* if there is only one input stream, it should be the source input */
 	struct sof_source *src_input = sources[0];
@@ -309,9 +314,7 @@ static int smart_amp_process(struct processing_module *mod,
 
 static int smart_amp_reset(struct processing_module *mod)
 {
-	struct comp_dev *dev = mod->dev;
-
-	comp_dbg(dev, "smart_amp_reset()");
+	LOG_DBG("smart_amp_reset()");
 
 	return 0;
 }
@@ -325,6 +328,7 @@ static smart_amp_proc get_smart_amp_process(struct sof_sink *sink)
 	case SOF_IPC_FRAME_S32_LE:
 		return process_s32;
 	default:
+		LOG_ERR("smart_amp_process() error: not supported frame format");
 		return NULL;
 	}
 }
@@ -334,19 +338,15 @@ static int smart_amp_prepare(struct processing_module *mod,
 			     struct sof_sink **sinks, int num_of_sinks)
 {
 	struct smart_amp_data *sad = module_get_private_data(mod);
-	struct comp_dev *dev = mod->dev;
-	struct comp_buffer *source_buffer;
-	struct comp_buffer *sink_buffer;
-	struct list_item *blist;
-	int ret;
-	int i;
+
+	LOG_DBG("smart_amp_prepare()");
 
 	sad->process = get_smart_amp_process(sinks[0]);
-
 	if (!sad->process) {
-		comp_err(dev, "smart_amp_prepare(): get_smart_amp_process failed");
+		LOG_ERR("smart_amp_prepare(): get_smart_amp_process failed");
 		return -EINVAL;
 	}
+
 	return 0;
 }
 
@@ -360,8 +360,32 @@ static const struct module_interface smart_amp_test_interface = {
 	.free = smart_amp_free
 };
 
+#ifndef __SOF_MODULE_SERVICE_BUILD__
 DECLARE_MODULE_ADAPTER(smart_amp_test_interface, smart_amp_test_comp_uuid, smart_amp_test_comp_tr);
 /* DECLARE_MODULE_ADAPTER() creates
  * "sys_comp_module_<smart_amp_test_interface>_init()" (and a lot more)
  */
 SOF_MODULE_INIT(smart_amp_test, sys_comp_module_smart_amp_test_interface_init);
+#else
+static const struct module_interface *loadable_module_main(void *mod_cfg,
+							   void *parent_ppl,
+							   void **mod_ptr)
+{
+	return &smart_amp_test_interface;
+}
+
+DECLARE_LOADABLE_MODULE_API_VERSION(smart_amp_test);
+
+__attribute__((section(".module")))
+const struct sof_man_module_manifest main_manifest = {
+	.module = {
+		.name = "SMATEST",
+		.uuid = {0x1E, 0x96, 0x7A, 0x16, 0xE4, 0x8A, 0xEA, 0x11,
+			 0x89, 0xF1, 0x00, 0x0C, 0x29, 0xCE, 0x16, 0x35},
+		.entry_point = (uint32_t)loadable_module_main,
+		.type = { .load_type = SOF_MAN_MOD_TYPE_MODULE,
+		.domain_ll = 1 },
+		.affinity_mask = 1,
+	}
+};
+#endif
