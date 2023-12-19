c17bc039a6d670bba97f3f63e44c8de4914aca4a
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2023-12-19 14:51:48 +0200

    audio: dai-zephyr: put no-data checks behind a build option
    Put the data availability checks behind a build option (new option
    CONFIG_DAI_VERBOSE_GLITCH_WARNINGS). In normal conditions, the DAI copy
    should never be called in a condition when there is no data to be
    copied. The audio interface is running and needs a constant stream of
    audio samples, so there is no remedy for not having data. In practise,
    most of the DAIs have a small buffer and can survive transient small
    gaps in data flow.
    
    The existing code has warning level logs for this condition. This is
    very useful for debug, as many common problems in firmware (and DSP
    topologies) show up as delays observed in the DAI copy logic. For
    product use, printing the warnings may make the situation worse by
    increasing the DSP load at a time when it has just missed a deadline.
    
    To allow for both debug and to avoid these checks in product
    builds, move the related checks behind a separate build option.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- src/audio/dai-zephyr.c
+++ src/audio/dai-zephyr.c
@@ -1286,7 +1286,9 @@ int dai_zephyr_multi_endpoint_copy(struct dai_data **dd, struct comp_dev *dev,
 
 	/* return if nothing to copy */
 	if (!frames) {
+#if CONFIG_DAI_VERBOSE_GLITCH_WARNINGS
 		comp_warn(dev, "dai_zephyr_multi_endpoint_copy(): nothing to copy");
+#endif
 
 		for (i = 0; i < num_endpoints; i++) {
 			ret = dma_reload(dd[i]->chan->dma->z_dev, dd[i]->chan->index, 0, 0, 0);
@@ -1460,6 +1462,7 @@ int dai_common_copy(struct dai_data *dd, struct comp_dev *dev, pcm_converter_fun
 	comp_dbg(dev, "dai_common_copy(), dir: %d copy_bytes= 0x%x",
 		 dev->direction, copy_bytes);
 
+#if CONFIG_DAI_VERBOSE_GLITCH_WARNINGS
 	/* Check possibility of glitch occurrence */
 	if (dev->direction == SOF_IPC_STREAM_PLAYBACK &&
 	    copy_bytes + avail_bytes < dd->period_bytes)
@@ -1469,10 +1472,13 @@ int dai_common_copy(struct dai_data *dd, struct comp_dev *dev, pcm_converter_fun
 		 copy_bytes + free_bytes < dd->period_bytes)
 		comp_warn(dev, "dai_common_copy(): Copy_bytes %d + free bytes %d < period bytes %d, possible glitch",
 			  copy_bytes, free_bytes, dd->period_bytes);
+#endif
 
 	/* return if nothing to copy */
 	if (!copy_bytes) {
+#if CONFIG_DAI_VERBOSE_GLITCH_WARNINGS
 		comp_warn(dev, "dai_zephyr_copy(): nothing to copy");
+#endif
 		dma_reload(dd->chan->dma->z_dev, dd->chan->index, 0, 0, 0);
 		return 0;
 	}
