da55a5ccf4e6ed3ca7dbc1f4f3c348ed70823b8f
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2023-12-21 14:36:13 +0100

    dp: look at all sinks and sources in default "is ready"
    The default "is ready to process" implementation used to look
    at a first sink and first source only.
    
    This change makes it to look at all sinks and sources.
    
    Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

--- src/include/sof/audio/module_adapter/module/generic.h
+++ src/include/sof/audio/module_adapter/module/generic.h
@@ -187,8 +187,15 @@ bool module_is_ready_to_process(struct processing_module *mod,
 	/* default action - the module is ready if there's enough data for processing and enough
 	 * space to store result. IBS/OBS as declared in init_instance
 	 */
-	return (source_get_data_available(sources[0]) >= source_get_min_available(sources[0]) &&
-		sink_get_free_size(sinks[0]) >= sink_get_min_free_space(sinks[0]));
+	for (int i = 0; i < num_of_sources; i++)
+		if (source_get_data_available(sources[i]) < source_get_min_available(sources[i]))
+			return false;
+
+	for (int i = 0; i < num_of_sinks; i++)
+		if (sink_get_free_size(sinks[i]) < sink_get_min_free_space(sinks[i]))
+			return false;
+
+	return true;
 }
 
 int module_process_sink_src(struct processing_module *mod,
