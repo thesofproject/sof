16d788efed76fc1cd0a0be1d973be0b9e4e3e42c
Chao Song <chao.song@linux.intel.com>
2024-01-11 12:54:00 +0800

    Topology2: Add sof-lnl-rt722-l0
    This patch adds the topology for LNL RVP with rt722
    multiple function codec.
    
    Signed-off-by: Chao Song <chao.song@linux.intel.com>

--- tools/topology/topology2/sof-ace-tplg/tplg-targets.cmake
+++ tools/topology/topology2/sof-ace-tplg/tplg-targets.cmake
@@ -102,4 +102,8 @@ NUM_HDMIS=0,SDW_SPK_STREAM=SDW2-Playback,SDW_SPK_IN_STREAM=SDW2-Capture,SDW_DMIC
 "cavs-sdw\;sof-lnl-rt712-l2-rt1712-l3\;PLATFORM=lnl,SDW_DMIC=1,NUM_HDMIS=0,NUM_SDW_AMP_LINKS=1,\
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
 SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
+
+"cavs-sdw\;sof-lnl-rt722-l0\;PLATFORM=lnl,SDW_DMIC=1,NUM_HDMIS=0,NUM_SDW_AMP_LINKS=1,\
+SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
+SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
 )
