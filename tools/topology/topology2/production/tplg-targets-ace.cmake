95c488c1165f9d6d06882c7dde9da28842f5cd86
Marc Herbert <marc.herbert@intel.com>
2024-02-14 01:29:54 +0000

    topology2: move HDA generic to new -hda-generic.cmake file
    Zero functional change.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- tools/topology/topology2/production/tplg-targets-ace.cmake
+++ tools/topology/topology2/production/tplg-targets-ace.cmake
@@ -2,12 +2,6 @@
 
 # Array of "input-file-name;output-file-name;comma separated pre-processor variables"
 list(APPEND TPLGS
-# HDMI only topology with passthrough pipelines
-"sof-hda-generic\;sof-hda-generic-idisp\;DEEPBUFFER_FW_DMA_MS=100,\
-DEEPBUFFER_D0I3_COMPATIBLE=true"
-# HDA topology with mixer-based pipelines for HDA and passthrough pipelines for HDMI
-"sof-hda-generic\;sof-hda-generic\;HDA_CONFIG=mix,DEEPBUFFER_FW_DMA_MS=100,\
-DEEPBUFFER_D0I3_COMPATIBLE=true"
 # If the alsatplg plugins for NHLT are not available, the NHLT blobs will not be added to the
 # topologies below.
 "sof-hda-generic\;sof-hda-generic-ace1-4ch\;PLATFORM=mtl,\
