ea28f10af7d2cda12c3d7ec2685162db2320de80
Marc Herbert <marc.herbert@intel.com>
2024-02-14 01:38:08 +0000

    topology2: add NHTL-free, 2 DMIC and 4 DMIC HDA generic topologies
    Addition modelled after main branch mega
    commit 65e4c1f8b519 ("topology2: Merge avs-tplg and sof-ace-tplg under
    production directory")
    
    As the "Deep Buffer cleanups" commit 80f283de0cf4 ("topology2: Mark
    DeepBuffer D0I3 compatible by default") and
    commit 4bb48758ad27 ("topology2: Remove redundant
    DEEPBUFFER_FW_DMA_MS=100 settings") are not in this stable-v2.8 branch,
    the following parameters are present here:
    
      DEEPBUFFER_FW_DMA_MS=100, DEEPBUFFER_D0I3_COMPATIBLE=true
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- tools/topology/topology2/production/tplg-targets-hda-generic.cmake
+++ tools/topology/topology2/production/tplg-targets-hda-generic.cmake
@@ -6,4 +6,13 @@ DEEPBUFFER_D0I3_COMPATIBLE=true"
 # HDA topology with mixer-based pipelines for HDA and passthrough pipelines for HDMI
 "sof-hda-generic\;sof-hda-generic\;HDA_CONFIG=mix,DEEPBUFFER_FW_DMA_MS=100,\
 DEEPBUFFER_D0I3_COMPATIBLE=true"
+
+# HDA topology with mixer-based pipelines for HDA and
+# passthrough pipelines for HDMI and
+# 2 or 4 DMIC, no NHLT blob included in topology
+"sof-hda-generic\;sof-hda-generic-2ch\;HDA_CONFIG=mix,NUM_DMICS=2,\
+DEEPBUFFER_FW_DMA_MS=100,DEEPBUFFER_D0I3_COMPATIBLE=true"
+"sof-hda-generic\;sof-hda-generic-4ch\;HDA_CONFIG=mix,NUM_DMICS=4,\
+DEEPBUFFER_FW_DMA_MS=100,DEEPBUFFER_D0I3_COMPATIBLE=true,\
+PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1"
 )
