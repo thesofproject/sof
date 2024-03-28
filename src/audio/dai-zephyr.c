thesofproject/sof

commit 9831a9ded7701416f5eab1ab745383041bdb42dc

audio: dai-zephyr: reset DMA buffer cursors on TRIGGER_RELEASE
On NXP platforms, stopping the DMAC and then re-configuring it
leads to the internal DMA buffer cursors being reset. Leaving the
SOF DMA buffer cursors in the same state means the DMA driver
and SOF may potentially end up with two different views of the
same buffer (from the cursors' point of view). This in, turn,
may lead to the DAI consuming stale data because of the values of
"free" and "pending" queried from dma_reload() stopping
dai_common_copy() from copying new data. To fix this, reset the
read and write cursors of the DMA buffer from SOF side. This
way, both SOF and the DMA driver will have the same initial
view of the DMA buffer cursors.

Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

src/audio/dai-zephyr.c
@@ -1069,6 +1069,16 @@ static int dai_comp_trigger_internal(struct dai_data *dd, struct comp_dev *dev, 
 			buffer_zero(dd->dma_buffer);
 		}
 
+		/* DMA driver and SOF's view of the DMA buffer's
+		 * read and write cursors must be the same to
+		 * avoid scenarios in which the DMA driver
+		 * reports false "free" and "pending" values, thus
+		 * leading to the DMA copying stale data due to
+		 * dma_status() stopping dai_common_copy() from
+		 * updating the data.
+		 */
+		audio_stream_reset(&dd->dma_buffer->stream);
+
 		/* only start the DAI if we are not XRUN handling */
 		if (dd->xrun == 0) {
 			/* recover valid start position */

commit dc9316d5e5f287965693c9f2c78e809f86894ef3

audio: dai-zephyr: Do not reset the DMA buffer cursor for HD-DMA on TRIGGER_RELEASE
During DMA stop/config/start the read/write pointer of HD-DMA is not
reset unlike other DMAs (GPDMA, DMAC).

Only call the audio_stream_reset() if the link is not serviced by HD-DMA.

Link: https://github.com/thesofproject/sof/issues/8986
Fixes: 9831a9ded770 ("audio: dai-zephyr: reset DMA buffer cursors on TRIGGER_RELEASE")
Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

src/audio/dai-zephyr.c
@@ -1141,8 +1141,12 @@ static int dai_comp_trigger_internal(struct dai_data *dd, struct comp_dev *dev, 
 		 * leading to the DMA copying stale data due to
 		 * dma_status() stopping dai_common_copy() from
 		 * updating the data.
+		 *
+		 * Only applies to non HD-DMA links as HD-DMA read/write pointer
+		 * is not reset during stop/config/start
 		 */
-		audio_stream_reset(&dd->dma_buffer->stream);
+		if (!(dd->dai->dma_caps & DMA_CAP_HDA))
+			audio_stream_reset(&dd->dma_buffer->stream);
 
 		/* only start the DAI if we are not XRUN handling */
 		if (dd->xrun == 0) {