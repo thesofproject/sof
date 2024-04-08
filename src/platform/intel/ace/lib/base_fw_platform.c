14c4e867573096a00634e46975479591ec0257a9
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2024-04-08 16:39:11 +0300

    audio: base_fw: add platform layer to IPC4 hw_config data
    Some of the IPC4 HW_CONFIG fields are platform specific and cannot be
    filled with generic code.
    
    Handle this functionality by separating platform specific parts of
    base_fw to a new base_fw_platform.h interface. Move out existing code
    for Intel cAVS and ACE platforms. Add a new stub implementation for
    posix platform. This posix stub can be also used as a starting point
    when adding IPC4 support to new platforms.
    
    This platform construct can be later used to move out other
    vendor and platform data out from base_fw.c.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

---
+++ src/platform/intel/ace/lib/base_fw_platform.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2024 Intel Corporation.
+//
+// Author: Kai Vehmanen <kai.vehmanen@linux.intel.com>
+
+#include <rtos/string.h>
+#include <sof/tlv.h>
+#include <sof/lib/dai.h>
+#include <ipc4/base_fw.h>
+
+int platform_basefw_hw_config(uint32_t *data_offset, char *data)
+{
+	struct sof_tlv *tuple = (struct sof_tlv *)data;
+	uint32_t value;
+
+	tlv_value_uint32_set(tuple, IPC4_HP_EBB_COUNT_HW_CFG, PLATFORM_HPSRAM_EBB_COUNT);
+
+	tuple = tlv_next(tuple);
+	/* 2 DMIC dais */
+	value =  DAI_NUM_SSP_BASE + DAI_NUM_HDA_IN + DAI_NUM_HDA_OUT +
+			DAI_NUM_ALH_BI_DIR_LINKS + 2;
+	tlv_value_uint32_set(tuple, IPC4_GATEWAY_COUNT_HW_CFG, value);
+
+	tuple = tlv_next(tuple);
+	tlv_value_uint32_set(tuple, IPC4_LP_EBB_COUNT_HW_CFG, PLATFORM_LPSRAM_EBB_COUNT);
+
+	tuple = tlv_next(tuple);
+	*data_offset = (int)((char *)tuple - data);
+
+	return 0;
+}
