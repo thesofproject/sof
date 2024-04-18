c75c17e80069df88750619bf788e0636ab5a9006
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2024-04-18 14:37:00 +0300

    ipc4: base_fw: add support for get/set large config extensions
    Add support to handle platform specific extensions with
    platform_basefw_get_large_config() and
    platform_basefw_set_large_config(). These functions are called when
    get/set handled is called with a parameter type not handled
    in generic code.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- src/platform/posix/base_fw_platform.c
+++ src/platform/posix/base_fw_platform.c
@@ -6,6 +6,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <ipc4/error_status.h>
 #include <ipc4/base_fw_platform.h>
 
 int platform_basefw_fw_config(uint32_t *data_offset, char *data)
@@ -27,4 +28,24 @@ struct sof_man_fw_desc *platform_base_fw_get_manifest(void)
 	struct sof_man_fw_desc *desc = NULL;
 
 	return desc;
+}
+
+int platform_basefw_get_large_config(struct comp_dev *dev,
+				     uint32_t param_id,
+				     bool first_block,
+				     bool last_block,
+				     uint32_t *data_offset,
+				     char *data)
+{
+	return -EINVAL;
+}
+
+int platform_basefw_set_large_config(struct comp_dev *dev,
+				     uint32_t param_id,
+				     bool first_block,
+				     bool last_block,
+				     uint32_t data_offset,
+				     const char *data)
+{
+	return IPC4_UNKNOWN_MESSAGE_TYPE;
 }
