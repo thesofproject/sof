ecc69e4dcd6318729f1b6b55795d9633f82a01db
Tobiasz Dryjanski <tobiaszx.dryjanski@intel.com>
2024-05-14 13:05:59 +0200

    performance measurements: add global perf data get ipc
    Implement global performance data get ipc which extracts performance data
    from MW3
    
    Signed-off-by: Tobiasz Dryjanski <tobiaszx.dryjanski@intel.com>

--- src/audio/base_fw.c
+++ src/audio/base_fw.c
@@ -22,6 +22,7 @@
 #include <sof/audio/module_adapter/module/generic.h>
 #include <sof/schedule/dp_schedule.h>
 #include <sof/schedule/ll_schedule.h>
+#include <sof/debug/telemetry/performance_monitor.h>
 #include <sof/debug/telemetry/telemetry.h>
 #include <sof/debug/telemetry/performance_monitor.h>
 /* FIXME:
@@ -429,6 +430,24 @@ int set_perf_meas_state(const char *data)
 	return IPC4_SUCCESS;
 }
 
+static int global_perf_data_get(uint32_t *data_off_size, char *data)
+{
+#ifdef CONFIG_SOF_TELEMETRY_PERFORMANCE_MEASUREMENTS
+	int ret;
+	struct global_perf_data *perf_data = (struct global_perf_data *)data;
+
+	ret = get_performance_data(perf_data);
+	if (ret < 0)
+		return IPC4_ERROR_INVALID_PARAM;
+	*data_off_size = sizeof(*perf_data)
+			+ perf_data->perf_item_count * sizeof(*perf_data->perf_items);
+
+	return IPC4_SUCCESS;
+#else
+	return IPC4_UNAVAILABLE;
+#endif
+}
+
 static int basefw_get_large_config(struct comp_dev *dev,
 				   uint32_t param_id,
 				   bool first_block,
@@ -468,13 +487,14 @@ static int basefw_get_large_config(struct comp_dev *dev,
 		return basefw_modules_info_get(data_offset, data);
 	case IPC4_LIBRARIES_INFO_GET:
 		return basefw_libraries_info_get(data_offset, data);
+	case IPC4_GLOBAL_PERF_DATA:
+		return global_perf_data_get(data_offset, data);
 	/* TODO: add more support */
 	case IPC4_DSP_RESOURCE_STATE:
 	case IPC4_NOTIFICATION_MASK:
 	case IPC4_PIPELINE_PROPS_GET:
 	case IPC4_GATEWAYS_INFO_GET:
 	case IPC4_PERF_MEASUREMENTS_STATE:
-	case IPC4_GLOBAL_PERF_DATA:
 		COMPILER_FALLTHROUGH;
 	default:
 		break;
