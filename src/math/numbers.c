thesofproject/sof

commit a306792a680495d7589e7457029cd36f7f493554

llext: add missing symbol exports
Export additional symbols, needed for modules like eq_iir and
mixin-mixout.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/math/numbers.c
@@ -175,3 +175,6 @@ uint32_t crc32(uint32_t base, const void *data, uint32_t bytes)
 
 	return ~crc;
 }
+
+uint64_t __udivdi3(uint64_t a, uint64_t b);
+EXPORT_SYMBOL(__udivdi3);

src/audio/audio_stream.c
@@ -3,6 +3,8 @@
 // Copyright(c) 2023 Intel Corporation. All rights reserved.
 //
 
+#include <rtos/symbol.h>
+
 #include <sof/audio/audio_stream.h>
 #include <sof/audio/buffer.h>
 #include <sof/audio/dp_queue.h>
@@ -102,7 +104,7 @@ void audio_stream_set_align(const uint32_t byte_align,
 	stream->frame_align_req = frame_align_req;
 	audio_stream_recalc_align(stream);
 }
-
+EXPORT_SYMBOL(audio_stream_set_align);
 
 static int audio_stream_release_data(struct sof_source *source, size_t free_size)
 {

src/audio/buffer.c
@@ -19,6 +19,7 @@
 #include <sof/lib/notifier.h>
 #include <sof/list.h>
 #include <rtos/spinlock.h>
+#include <rtos/symbol.h>
 #include <ipc/topology.h>
 #include <errno.h>
 #include <stddef.h>
@@ -227,6 +228,7 @@ int buffer_set_params(struct comp_buffer *buffer,
 
 	return 0;
 }
+EXPORT_SYMBOL(buffer_set_params);
 
 bool buffer_params_match(struct comp_buffer *buffer,
 			 struct sof_ipc_stream_params *params, uint32_t flag)

src/audio/component.c
@@ -153,6 +153,7 @@ int comp_set_state(struct comp_dev *dev, int cmd)
 
 	return 0;
 }
+EXPORT_SYMBOL(comp_set_state);
 
 void sys_comp_init(struct sof *sof)
 {
@@ -419,6 +420,7 @@ void cir_buf_copy(void *src, void *src_addr, void *src_end, void *dst,
 }
 
 #endif
+EXPORT_SYMBOL(audio_stream_copy);
 
 void audio_stream_copy_from_linear(const void *linear_source, int ioffset,
 				   struct audio_stream *sink, int ooffset,

src/audio/data_blob.c
@@ -102,6 +102,7 @@ void *comp_get_data_blob(struct comp_data_blob_handler *blob_handler,
 
 	return blob_handler->data;
 }
+EXPORT_SYMBOL(comp_get_data_blob);
 
 bool comp_is_new_data_blob_available(struct comp_data_blob_handler
 					*blob_handler)
@@ -119,6 +120,7 @@ bool comp_is_new_data_blob_available(struct comp_data_blob_handler
 
 	return false;
 }
+EXPORT_SYMBOL(comp_is_new_data_blob_available);
 
 bool comp_is_current_data_blob_valid(struct comp_data_blob_handler
 				     *blob_handler)
@@ -162,6 +164,7 @@ int comp_init_data_blob(struct comp_data_blob_handler *blob_handler,
 
 	return 0;
 }
+EXPORT_SYMBOL(comp_init_data_blob);
 
 int comp_data_blob_set(struct comp_data_blob_handler *blob_handler,
 		       enum module_cfg_fragment_position pos, uint32_t data_offset_size,
@@ -615,6 +618,7 @@ int comp_data_blob_get_cmd(struct comp_data_blob_handler *blob_handler,
 
 	return ret;
 }
+EXPORT_SYMBOL(comp_data_blob_get_cmd);
 
 static void *default_alloc(size_t size)
 {

src/audio/module_adapter/module/generic.c
@@ -11,6 +11,8 @@
  *
  */
 
+#include <rtos/symbol.h>
+
 #include <sof/audio/module_adapter/module/generic.h>
 
 LOG_MODULE_DECLARE(module_adapter, CONFIG_SOF_LOG_LEVEL);
@@ -537,3 +539,4 @@ void module_update_buffer_position(struct input_stream_buffer *input_buffers,
 	input_buffers->consumed += audio_stream_frame_bytes(source) * frames;
 	output_buffers->size += audio_stream_frame_bytes(sink) * frames;
 }
+EXPORT_SYMBOL(module_update_buffer_position);

src/ipc/ipc4/helper.c
@@ -1038,6 +1038,7 @@ struct comp_dev *ipc4_get_comp_dev(uint32_t comp_id)
 
 	return icd ? icd->cd : NULL;
 }
+EXPORT_SYMBOL(ipc4_get_comp_dev);
 
 int ipc4_add_comp_dev(struct comp_dev *dev)
 {

src/module/audio/sink_api.c
@@ -67,16 +67,19 @@ int sink_set_frm_fmt(struct sof_sink *sink, enum sof_ipc_frame frame_fmt)
 		return sink->ops->on_audio_format_set(sink);
 	return 0;
 }
+EXPORT_SYMBOL(sink_set_frm_fmt);
 
 size_t sink_get_frame_bytes(struct sof_sink *sink)
 {
 	return get_frame_bytes(sink_get_frm_fmt(sink), sink_get_channels(sink));
 }
+EXPORT_SYMBOL(sink_get_frame_bytes);
 
 size_t sink_get_free_frames(struct sof_sink *sink)
 {
 	return sink_get_free_size(sink) / sink_get_frame_bytes(sink);
 }
+EXPORT_SYMBOL(sink_get_free_frames);
 
 int sink_set_valid_fmt(struct sof_sink *sink, enum sof_ipc_frame valid_sample_fmt)
 {
@@ -85,6 +88,7 @@ int sink_set_valid_fmt(struct sof_sink *sink, enum sof_ipc_frame valid_sample_fm
 		return sink->ops->on_audio_format_set(sink);
 	return 0;
 }
+EXPORT_SYMBOL(sink_set_valid_fmt);
 
 int sink_set_rate(struct sof_sink *sink, unsigned int rate)
 {
@@ -101,6 +105,7 @@ int sink_set_channels(struct sof_sink *sink, unsigned int channels)
 		return sink->ops->on_audio_format_set(sink);
 	return 0;
 }
+EXPORT_SYMBOL(sink_set_channels);
 
 int sink_set_buffer_fmt(struct sof_sink *sink, uint32_t buffer_fmt)
 {

commit 80b8e6aeee6b6fe88e96abdc24f7224fad322025

lp64: fix undefined reference compilation breakage
__udivdi3 isn't defined on LP64, don't export it.

Fixes: a306792a6804 ("llext: add missing symbol exports")
Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

src/math/numbers.c
@@ -176,5 +176,7 @@ uint32_t crc32(uint32_t base, const void *data, uint32_t bytes)
 	return ~crc;
 }
 
+#if !CONFIG_SOC_MIMX9352_A55
 uint64_t __udivdi3(uint64_t a, uint64_t b);
 EXPORT_SYMBOL(__udivdi3);
+#endif