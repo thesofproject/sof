b19e0411f9bf5f93b6768522f14ad35faf133e2b
Bard Liao <yung-chuan.liao@linux.intel.com>
2024-05-17 09:23:01 +0000

    topology2: add sof-lnl-cs42l43-l0.tplg
    This is for cs42l43 bridge/amp configuration.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- tools/topology/topology2/production/tplg-targets-ace2.cmake
+++ tools/topology/topology2/production/tplg-targets-ace2.cmake
@@ -17,6 +17,11 @@ SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
 SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
 
+"cavs-sdw\;sof-lnl-cs42l43-l0\;PLATFORM=lnl,SDW_DMIC=1,NUM_SDW_AMP_LINKS=1,\
+SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
+SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack,\
+SDW_AMP_FMT_24=true,SDW_JACK_FMT_24=true"
+
 # No SDW Jack. SDW DMIC+SPK
 "cavs-sdw\;sof-lnl-rt1318-l12-rt714-l0\;PLATFORM=lnl,SDW_JACK=false,SDW_DMIC=1,\
 NUM_SDW_AMP_LINKS=2,SDW_DMIC_STREAM=SDW0-Capture"
