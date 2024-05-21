30a778c563b282e7acd9eb7bc957a9b3a5629ba7
Tobiasz Dryjanski <tobiaszx.dryjanski@intel.com>
2024-05-21 16:24:32 +0200

    performance measurement: add bitmap functionality based on bitarray
    Implement wrapper to extend Zephyr's bitarray by adding a counter for
    allocated bits. This bitmap will be used to allocate performance data
    entries in memory window 3.
    
    Signed-off-by: Tobiasz Dryjanski <tobiaszx.dryjanski@intel.com>

--- src/debug/telemetry/telemetry.c
+++ src/debug/telemetry/telemetry.c
@@ -5,6 +5,7 @@
 // Author: Tobiasz Dryjanski <tobiaszx.dryjanski@intel.com>
 
 #include <zephyr/debug/sparse.h>
+#include <zephyr/sys/bitarray.h>
 
 #include <sof/audio/module_adapter/module/generic.h>
 #include <sof/debug/telemetry/telemetry.h>
@@ -27,6 +28,82 @@ static int telemetry_prev_ccount[CONFIG_MAX_CORE_COUNT];
 static int telemetry_perf_period_sum[CONFIG_MAX_CORE_COUNT];
 static int telemetry_perf_period_cnt[CONFIG_MAX_CORE_COUNT];
 static struct telemetry_perf_queue telemetry_perf_queue[CONFIG_MAX_CORE_COUNT];
+
+struct perf_bitmap {
+	sys_bitarray_t *array;
+	uint16_t occupied;
+	size_t size;
+};
+
+static int perf_bitmap_init(struct perf_bitmap * const bitmap, sys_bitarray_t *array, size_t size)
+{
+	k_spinlock_key_t key = k_spin_lock(&array->lock);
+
+	bitmap->array = array;
+	bitmap->size = size;
+	bitmap->occupied = 0;
+
+	k_spin_unlock(&array->lock, key);
+	return 0;
+}
+
+static int perf_bitmap_alloc(struct perf_bitmap * const bitmap, size_t *offset)
+{
+	k_spinlock_key_t key;
+	int ret = sys_bitarray_alloc(bitmap->array, 1, offset);
+
+	if (!ret) {
+		key = k_spin_lock(&bitmap->array->lock);
+		bitmap->occupied++;
+		k_spin_unlock(&bitmap->array->lock, key);
+	}
+
+	return ret;
+}
+
+static int perf_bitmap_free(struct perf_bitmap * const bitmap, size_t offset)
+{
+	k_spinlock_key_t key;
+	int ret =  sys_bitarray_free(bitmap->array, 1, offset);
+
+	if (!ret) {
+		key = k_spin_lock(&bitmap->array->lock);
+		bitmap->occupied--;
+		k_spin_unlock(&bitmap->array->lock, key);
+	}
+
+	return ret;
+}
+
+static int perf_bitmap_setbit(struct perf_bitmap * const bitmap, size_t bit)
+{
+	return sys_bitarray_set_bit(bitmap->array, bit);
+}
+
+static int perf_bitmap_clearbit(struct perf_bitmap * const bitmap, size_t bit)
+{
+	return sys_bitarray_clear_bit(bitmap->array, bit);
+}
+
+static inline uint16_t perf_bitmap_get_occupied(struct perf_bitmap * const bitmap)
+{
+	return bitmap->occupied;
+}
+
+static inline uint16_t perf_bitmap_get_size(struct perf_bitmap * const bitmap)
+{
+	return bitmap->size;
+}
+
+static bool perf_bitmap_is_bit_clear(struct perf_bitmap * const bitmap, size_t bit)
+{
+	int val;
+	int ret = sys_bitarray_test_bit(bitmap->array, bit, &val);
+
+	if (ret < 0)
+		return false;
+	return !val;
+}
 
 static void telemetry_perf_queue_append(struct telemetry_perf_queue *q, size_t element)
 {
