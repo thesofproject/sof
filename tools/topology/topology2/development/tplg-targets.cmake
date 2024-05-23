95a0ffc0d4c41bb692c3d9656b5f7fd224252e60
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2024-05-23 15:37:26 +0300

    Tools: Topology2: Add beamformer and DRC to HDA analog capture
    This patch adds to analog microphone capture pipeline TDFB
    beamformer and DRC for dynamic range control. The name of updated
    topology is sof-hda-efx-generic.tplg. Similar capture enhance is
    added to multi-band DRC version sof-hda-efx-mbdrc-generic.tplg.
    
    The pipeline is enabled if HDA_MIC_ENHANCED_CAPTURE is set to true
    in topologies build. They keys EFX_HDA_MIC_TDFB_PARAMS and
    EFX_HDA_MIC_DRC_PARAMS control the configurations blobs use.
    
    There is no processing applied to capture audio since the topology
    is built with passthrough blobs. E.g. UCM should set up processing
    with blobs those have effect.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/topology/topology2/development/tplg-targets.cmake
+++ tools/topology/topology2/development/tplg-targets.cmake
@@ -56,8 +56,9 @@ NHLT_BIN=nhlt-sof-lnl-nocodec-fpga-4ch.bin,PASSTHROUGH=true,DMIC_IO_CLK=19200000
 "cavs-sdw\;sof-lnl-fpga-rt711-l0\;PLATFORM=lnl,NUM_HDMIS=0,PASSTHROUGH=true"
 
 # CAVS HDA topology with mixer-based efx eq pipelines for HDA and passthrough pipelines for HDMI
-"sof-hda-generic\;sof-hda-efx-generic\;HDA_CONFIG=efx,\
-EFX_FIR_PARAMS=passthrough,EFX_IIR_PARAMS=passthrough,EFX_DRC_PARAMS=passthrough"
+"sof-hda-generic\;sof-hda-efx-generic\;HDA_CONFIG=efx,HDA_MIC_ENHANCED_CAPTURE=true,\
+EFX_FIR_PARAMS=passthrough,EFX_IIR_PARAMS=passthrough,EFX_DRC_PARAMS=passthrough,\
+EFX_HDA_MIC_TDFB_PARAMS=line2_pass,EFX_HDA_MIC_DRC_PARAMS=passthrough"
 
 "sof-hda-generic\;sof-hda-efx-generic-2ch\;\
 HDA_CONFIG=efx,NUM_DMICS=2,\
@@ -69,9 +70,9 @@ HDA_CONFIG=efx,NUM_DMICS=4,PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,\
 EFX_FIR_PARAMS=passthrough,EFX_IIR_PARAMS=passthrough,\
 EFX_DRC_PARAMS=passthrough"
 
-"sof-hda-generic\;sof-hda-efx-mbdrc-generic\;HDA_CONFIG=efx,\
-EFX_FIR_PARAMS=passthrough,EFX_IIR_PARAMS=passthrough,\
-EFX_DRC_COMPONENT=multiband,EFX_MBDRC_PARAMS=passthrough"
+"sof-hda-generic\;sof-hda-efx-mbdrc-generic\;HDA_CONFIG=efx,HDA_MIC_ENHANCED_CAPTURE=true,\
+EFX_FIR_PARAMS=passthrough,EFX_IIR_PARAMS=passthrough,EFX_DRC_COMPONENT=multiband,\
+EFX_MBDRC_PARAMS=passthrough,EFX_HDA_MIC_TDFB_PARAMS=line2_pass,EFX_HDA_MIC_DRC_PARAMS=passthrough"
 
 "sof-hda-generic\;sof-hda-efx-mbdrc-generic-2ch\;\
 HDA_CONFIG=efx,NUM_DMICS=2,\
