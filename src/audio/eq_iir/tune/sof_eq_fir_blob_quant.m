b55cb64ac1391844f3a1d5bb84ffa66873251056
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2024-06-03 15:33:07 +0300

    Audio: EQIIR: Tune: Prefix top level functions with sof_
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

---
+++ src/audio/eq_iir/tune/sof_eq_fir_blob_quant.m
@@ -0,0 +1,94 @@
+function fbr = sof_eq_fir_blob_quant(b, bits, strip_trailing_zeros)
+
+%% Quantize FIR coefficients and return vector with length,
+%  out shift, and coefficients to be used in the setup blob.
+%
+%  fbr = sof_eq_fir_blob_resp(b, bits)
+%  b - FIR coefficients
+%  bits - optional number of bits, defaults to 16
+%
+%  fbr - vector with length, in shift, out shift, and quantized coefficients
+%
+
+% SPDX-License-Identifier: BSD-3-Clause
+%
+% Copyright (c) 2016, Intel Corporation. All rights reserved.
+%
+% Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
+
+b = (b(:))';
+
+if nargin < 3
+	strip_trailing_zeros = 1;
+end
+
+if nargin < 2
+	bits = 16;
+end
+
+%% Quantize
+[bq, shift] = sof_eq_fir_quantize(b, bits);
+
+%% Check trailing zeros
+nf = length(bq);
+nz = nf;
+while bq(nz) == 0
+	nz = nz - 1;
+end
+if nz < nf && strip_trailing_zeros
+	nb = nz + 1;
+	fprintf(1, 'Note: Filter length was reduced ');
+	fprintf(1, 'to %d -> %d due to trailing zeros.\n', nf, nb);
+	bq = bq(1:nb);
+else
+	nb = nf;
+end
+
+%% Make length multiple of four (optimized FIR core)
+mod4 = mod(nb, 4);
+if mod4 > 0
+	pad = zeros(1,4-mod4);
+	bqp = [bq pad];
+	nnew = length(bqp);
+	fprintf(1,'Note: Filter length was %d, padded length into %d.\n', ...
+		nb, nnew);
+else
+	fprintf(1,'Note: Filter length is %d\n', nb);
+	nnew = nb;
+	bqp = bq;
+end
+
+%% Pack data into FIR coefficient format
+%	int16_t length
+%	int16_t out_shift
+%	uint32_t reserved[4]
+%	int16_t coef[]
+fbr = [nnew shift 0 0 0 0 0 0 0 0 bqp];
+
+end
+
+function [bq, shift] = sof_eq_fir_quantize(b, bits)
+
+% [bq, shift] = eq_fir_quantize(b, bits)
+%
+% Inputs
+% b - FIR coefficients
+% bits - number bits for 2s complement coefficient
+%
+% Outputs
+% bq - quantized coefficients
+% shift - shift right parameter to apply after FIR computation to
+% compensate for coefficients scaling
+%
+
+scale = 2^(bits-1);
+
+%% Output shift for coefficients
+m = max(abs(b));
+shift = -ceil(log(m+1/scale)/log(2));
+bsr = b*2^shift;
+
+%% Quantize to Q1.bits-1 format, e.g. Q1.15 for 16 bits
+bq = sof_eq_coef_quant(bsr, bits, bits-1);
+
+end
---
+++ src/audio/eq_iir/tune/sof_export_c_eq_uint32t.m
@@ -0,0 +1,91 @@
+function sof_export_c_eq_uint32t(fn, blob8, vn, justeq)
+
+% sof_export_c_eq_uint32t(fn, blob8, vn)
+%
+% Export 8-bit blob data as C int32_t vector in hexadecimal
+%
+% fn - filename for export
+% blob8 - blob data from EQ design
+% vn - variable name
+% justeq - export just the EQ wihtout ABI headers for direct use in FW
+
+% SPDX-License-Identifier: BSD-3-Clause
+%
+% Copyright (c) 2021, Intel Corporation. All rights reserved.
+
+	% Write blob
+	fh = fopen(fn, 'w');
+	if fh < 0
+		fprintf(1, 'Error: Could not open file: %s\n', fn);
+		error("Failed.");
+	end
+
+	year = datestr(now, 'yyyy');
+	fprintf(fh, '/* SPDX-License-Identifier: BSD-3-Clause\n');
+	fprintf(fh, ' *\n');
+	fprintf(fh, ' * Copyright(c) %s Intel Corporation. All rights reserved.\n', year);
+	fprintf(fh, ' */\n');
+	fprintf(fh, '\n');
+
+	% Pad blob length to multiple of four bytes
+	n_orig = length(blob8);
+	n_new = ceil(n_orig/4);
+	blob8_new = zeros(1, n_new*4);
+	blob8_new(1:n_orig) = blob8;
+
+	%% Convert to 32 bit
+	blob32 = zeros(1, n_new, 'uint32');
+	k = 2.^[0 8 16 24];
+	for i=1:n_new
+		j = (i-1)*4;
+		blob32(i) = blob8_new(j+1)*k(1) + blob8_new(j+2)*k(2) ...
+			    +  blob8_new(j+3)*k(3) + blob8_new(j+4)*k(4);
+	end
+
+	% Omit all headers if requested
+	if justeq
+		blob32 = blob32(18:end);
+		n_new = length(blob32);
+	end
+
+	numbers_in_line = 4;
+	full_lines = floor(n_new/numbers_in_line);
+	numbers_remain = n_new - numbers_in_line * full_lines;
+
+	n = 1;
+	fprintf(fh, 'uint32_t %s[%d] = {\n', vn, n_new);
+	for i = 1:full_lines
+		fprintf(fh, '\t');
+		for j = 1:numbers_in_line
+			fprintf(fh, '0x%08x', blob32(n));
+			if n < n_new
+				fprintf(fh, ',');
+			end
+			if j < numbers_in_line
+				fprintf(fh, ' ');
+			else
+				fprintf(fh, '\n');
+			end
+			n = n + 1;
+		end
+	end
+
+	if numbers_remain
+		fprintf(fh, '\t');
+		for j = 1:numbers_remain
+			fprintf(fh, '0x%08x', blob32(n));
+			if n < n_new
+				fprintf(fh, ',');
+			end
+			if j < numbers_remain
+				fprintf(fh, ' ');
+			else
+				fprintf(fh, '\n');
+			end
+			n = n + 1;
+		end
+	end
+
+	fprintf(fh, '};\n');
+	fclose(fh);
+end
