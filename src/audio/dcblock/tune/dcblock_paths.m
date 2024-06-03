980a1ea7d7e1f632ac873ce33f7c4145915b3512
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2024-06-03 12:08:53 +0300

    Audio: DCblock: Fix search path to common functions
    A function dcblock_paths.m is added to handle add and remove
    of path.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

---
+++ src/audio/dcblock/tune/dcblock_paths.m
@@ -0,0 +1,18 @@
+function dcblock_paths(enable)
+
+% dcblock_paths(enable)
+% enable - set to true to enable needed search path
+%          set to false to disable the search paths
+%
+
+% SPDX-License-Identifier: BSD-3-Clause
+%
+% Copyright (c) 2024, Intel Corporation. All rights reserved.
+
+	common = '../../../../tools/tune/common';
+	if enable
+		addpath(common);
+	else
+		rmpath(common);
+	end
+end
