bfa9580f73447ea1978ab25ee037bf80e2ee0d60
Dobrowolski, PawelX <pawelx.dobrowolski@intel.com>
2024-06-14 13:28:13 +0200

    platform: version: add hardware configuration version
    Due to introducing new platform to sof we need to recognize which
    iteration of ace board we are dealing with.
    
    Signed-off-by: Dobrowolski, PawelX <pawelx.dobrowolski@intel.com>

--- src/platform/intel/ace/include/ace/version.h
+++ src/platform/intel/ace/include/ace/version.h
@@ -8,6 +8,19 @@
 #ifndef __ACE_VERSION_H__
 #define __ACE_VERSION_H__
 
-#define HW_CFG_VERSION		0
+#define ACE_VERSION_1_5 0x10500 /* MTL */
+#define ACE_VERSION_2_0 0x20000 /* LNL */
+#define ACE_VERSION_3_0 0x30000 /* PTL */
+
+/* ACE version defined by CONFIG_ACE_VER_*/
+#if defined(CONFIG_ACE_VERSION_1_5)
+#define ACE_VERSION ACE_VERSION_1_5
+#elif defined(CONFIG_ACE_VERSION_2_0)
+#define ACE_VERSION ACE_VERSION_2_0
+#elif defined(CONFIG_ACE_VERSION_3_0)
+#define ACE_VERSION ACE_VERSION_3_0
+#endif
+
+#define HW_CFG_VERSION		ACE_VERSION
 
 #endif /* __ACE_VERSION_H__ */
