55429c674961ae92d9ef863cd240849323578d1e
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2024-06-17 17:24:35 +0200

    llext: remove logging during module freeing
    When pipelines are destroyed, component drivers' .reset() and .free()
    are called. If those drivers were loaded dynamically their memory is
    then unmapped. But logging takes place in a low priority task, so it
    is important that no logging is done from those methods.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/audio/mixin_mixout/mixin_mixout.c
+++ src/audio/mixin_mixout/mixin_mixout.c
@@ -173,9 +173,7 @@ static int mixout_init(struct processing_module *mod)
 static int mixin_free(struct processing_module *mod)
 {
 	struct mixin_data *md = module_get_private_data(mod);
-	struct comp_dev *dev = mod->dev;
 
-	comp_dbg(dev, "mixin_free()");
 	rfree(md);
 
 	return 0;
@@ -183,7 +181,6 @@ static int mixin_free(struct processing_module *mod)
 
 static int mixout_free(struct processing_module *mod)
 {
-	comp_dbg(mod->dev, "mixout_free()");
 	rfree(module_get_private_data(mod));
 
 	return 0;
@@ -554,9 +551,6 @@ static int mixout_process(struct processing_module *mod,
 static int mixin_reset(struct processing_module *mod)
 {
 	struct mixin_data *mixin_data = module_get_private_data(mod);
-	struct comp_dev *dev = mod->dev;
-
-	comp_dbg(dev, "mixin_reset()");
 
 	mixin_data->mix = NULL;
 	mixin_data->gain_mix = NULL;
@@ -567,8 +561,6 @@ static int mixin_reset(struct processing_module *mod)
 static int mixout_reset(struct processing_module *mod)
 {
 	struct comp_dev *dev = mod->dev;
-
-	comp_dbg(dev, "mixout_reset()");
 
 	/* FIXME: move this to module_adapter_reset() */
 	if (dev->pipeline->source_comp->direction == SOF_IPC_STREAM_PLAYBACK) {
