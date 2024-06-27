430d877cbd348d67e61420db9605aab1a9c9c94f
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2024-06-27 09:00:11 +0200

    topology2: enable HDMI for all SoundWire production topologies
    For some reason we still have NUM_HDMIS=0 in production topologies,
    that's a miss. We have no reason to keep those initial developer
    settings.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- tools/topology/topology2/production/tplg-targets-cavs25.cmake
+++ tools/topology/topology2/production/tplg-targets-cavs25.cmake
@@ -25,7 +25,7 @@ DMIC1_ID=4,PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,SDW_AMP_FEEDBACK=false,\
 PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-tgl-rt711-rt1308-4ch.bin"
 
 "cavs-sdw\;sof-tgl-cs42l43-l3-cs35l56-l01\;PLATFORM=tgl,NUM_SDW_AMP_LINKS=2,SDW_DMIC=1,\
-NUM_HDMIS=0,SDW_SPK_STREAM=Playback-SmartAmp,SDW_SPK_IN_STREAM=Capture-SmartAmp,\
+SDW_SPK_STREAM=Playback-SmartAmp,SDW_SPK_IN_STREAM=Capture-SmartAmp,\
 SDW_DMIC_STREAM=Capture-SmartMic,SDW_JACK_OUT_STREAM=Playback-SimpleJack,\
 SDW_JACK_IN_STREAM=Capture-SimpleJack,SDW_AMP_FMT_24=true,SDW_JACK_FMT_24=true"
 
