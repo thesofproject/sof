73538628af7c983a8f139d01c76f4c3e9e6c8697
Tobiasz Dryjanski <tobiaszx.dryjanski@intel.com>
2024-06-28 10:36:00 +0200

    performance measurement: add bitmap functionality based on bitarray
    Implement wrapper to extend Zephyr's bitarray by adding a counter for
    allocated bits. This bitmap will be used to allocate performance data
    entries in memory window 3. Also adds new performance_monitor files.
    
    Signed-off-by: Tobiasz Dryjanski <tobiaszx.dryjanski@intel.com>

---
+++ src/include/sof/debug/telemetry/performance_monitor.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2024 Intel Corporation. All rights reserved.
+ *
+ * Author: Tobiasz Dryjanski <tobiaszx.dryjanski@intel.com>
+ */
+
+#ifndef __SOF_PERFORMANCE_MONITOR_H__
+#define __SOF_PERFORMANCE_MONITOR_H__
+
+#endif
