2688f6345ad4ca8a9ad0f9c045adfe1f01b7fb3c
Andy Ross <andyross@google.com>
2024-07-03 11:56:25 -0700

    platform/posix: Port fuzzer to upstream "native_sim" board
    The older native_posix board is being deprecated, use native_sim,
    which is the future-proof API.  Mostly just swap the board target and
    change the C API names.
    
    Note the NATIVE_SIMULATOR_IF decoaration on LLVMFuzzerTestOneInput():
    that forces the function to be included in the first-stage zephyr.elf
    link (otherwise it would be dropped as Zephyr/SOF itself doesn't
    reference the entry point) and to be visible as a global symbol to the
    libfuzzer instrumentation layer.
    
    Signed-off-by: Andy Ross <andyross@google.com>

--- src/platform/posix/fuzz.c
+++ src/platform/posix/fuzz.c
@@ -8,10 +8,8 @@
 
 #include <irq_ctrl.h>
 #include <zephyr/sys/time_units.h>
-
-/* Zephyr arch APIs, not in a header (native_sim has them though) */
-void posix_init(int argc, char *argv[]);
-void posix_exec_for(uint64_t us);
+#include <nsi_cpu_if.h>
+#include <nsi_main_semipublic.h>
 
 const uint8_t *posix_fuzz_buf;
 size_t posix_fuzz_sz;
@@ -23,12 +21,13 @@ size_t posix_fuzz_sz;
  * "long enough" to handle the event and reach a quiescent state
  * again)
  */
+NATIVE_SIMULATOR_IF
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t sz)
 {
 	static bool runner_initialized;
 
 	if (!runner_initialized) {
-		posix_init(0, NULL);
+		nsi_init(0, NULL);
 		runner_initialized = true;
 	}
 
@@ -42,6 +41,6 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t sz)
 	/* Give the OS time to process whatever happened in that
 	 * interrupt and reach an idle state.
 	 */
-	posix_exec_for(k_ticks_to_us_ceil64(CONFIG_ZEPHYR_POSIX_FUZZ_TICKS));
+	nsi_exec_for(k_ticks_to_us_ceil64(CONFIG_ZEPHYR_POSIX_FUZZ_TICKS));
 	return 0;
 }
