9aa87e54a1a076283bf23eeb7221898fae1cc381
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2024-07-19 10:48:40 +0200

    Revert "ipc4: copier: Add IPC4 channel map handler"
    This reverts commit d629e521020c7d2375ed4696d0937c5cdd444e62.
    First of the 3 commits, that need to be reverted.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/audio/copier/copier_dai.c
+++ src/audio/copier/copier_dai.c
@@ -164,7 +164,6 @@ static int copier_dai_init(struct comp_dev *dev,
 {
 	struct processing_module *mod = comp_mod(dev);
 	struct copier_data *cd = module_get_private_data(mod);
-	uint32_t chmap;
 	struct dai_data *dd;
 	int ret;
 
@@ -179,7 +178,6 @@ static int copier_dai_init(struct comp_dev *dev,
 		config->frame_fmt = out_frame_fmt;
 		pipeline->sink_comp = dev;
 		cd->bsource_buffer = true;
-		chmap = copier->base.audio_fmt.ch_map;
 	} else {
 		enum sof_ipc_frame in_frame_fmt, in_valid_fmt;
 
@@ -189,7 +187,6 @@ static int copier_dai_init(struct comp_dev *dev,
 					    copier->base.audio_fmt.s_type);
 		config->frame_fmt = in_frame_fmt;
 		pipeline->source_comp = dev;
-		chmap = copier->out_fmt.ch_map;
 	}
 
 	/* save the channel map and count for ALH multi-gateway */
@@ -207,8 +204,6 @@ static int copier_dai_init(struct comp_dev *dev,
 	ret = dai_common_new(dd, dev, dai);
 	if (ret < 0)
 		goto free_dd;
-
-	dd->chmap = chmap;
 
 	pipeline->sched_id = config->id;
 
@@ -454,9 +449,9 @@ static int copy_single_channel_c32(const struct audio_stream *src,
 	return 0;
 }
 
-void copier_dai_adjust_params(const struct copier_data *cd,
-			      struct ipc4_audio_format *in_fmt,
-			      struct ipc4_audio_format *out_fmt)
+static void copier_dai_adjust_params(const struct copier_data *cd,
+				     struct ipc4_audio_format *in_fmt,
+				     struct ipc4_audio_format *out_fmt)
 {
 	struct comp_buffer *dma_buf;
 	int dma_buf_channels;
@@ -524,7 +519,7 @@ int copier_dai_params(struct copier_data *cd, struct comp_dev *dev,
 			ipc4_playback : ipc4_capture;
 
 		cd->dd[0]->process =
-			get_converter_func(&in_fmt, &out_fmt, cd->gtw_type, dir, cd->dd[0]->chmap);
+			get_converter_func(&in_fmt, &out_fmt, cd->gtw_type, dir, DUMMY_CHMAP);
 
 		return ret;
 	}
