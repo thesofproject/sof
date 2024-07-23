dbc17cb3b413ccc2cad72f52869912dd8712fc4a
Adrian Warecki <adrian.warecki@intel.com>
2024-07-23 18:01:00 +0200

    module: Move module log priority definition to module directory
    Move logging priority definitions used by iadk modules to the modules
    directory so other types of loadable modules can use them. Propose
    a function to convert log priority used by loadable modules to log level
    used by sof.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/include/sof/audio/module_adapter/iadk/adsp_stddef.h
+++ src/include/sof/audio/module_adapter/iadk/adsp_stddef.h
@@ -8,12 +8,13 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include <user/trace.h>
 #include <rtos/string.h>
 
 #ifdef __ZEPHYR__
 #include <zephyr/sys/util.h>
 #endif /* __ZEPHYR__ */
+
+#include <module/module/logger.h>
 
 #ifdef __XTENSA__
   #define RESTRICT __restrict
@@ -21,30 +22,10 @@
   #define RESTRICT
 #endif
 
+
 /*! Log level priority enumeration. */
-typedef enum log_priority {
-	/*! Critical message. */
-	L_CRITICAL = LOG_LEVEL_CRITICAL,
-	/*! Error message. */
-	L_ERROR = LOG_LEVEL_ERROR,
-	/*! High importance log level. */
-	L_HIGH = LOG_LEVEL_ERROR,
-	/*! Warning message. */
-	L_WARNING = LOG_LEVEL_WARNING,
-	/*! Medium importance log level. */
-	L_MEDIUM = LOG_LEVEL_WARNING,
-	/*! Low importance log level. */
-	L_LOW = LOG_LEVEL_INFO,
-	/*! Information. */
-	L_INFO = LOG_LEVEL_INFO,
-	/*! Verbose message. */
-	L_VERBOSE = LOG_LEVEL_VERBOSE,
-	L_DEBUG   = LOG_LEVEL_DEBUG,
-	L_MAX     = LOG_LEVEL_DEBUG,
-} AdspLogPriority,
-	log_priority_e;
-struct AdspLogHandle;
-typedef struct AdspLogHandle AdspLogHandle;
+typedef enum log_priority AdspLogPriority, log_priority_e;
+typedef struct log_handle AdspLogHandle;
 
 #ifdef __cplusplus
 namespace intel_adsp
