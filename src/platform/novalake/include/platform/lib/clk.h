5d2f74108c13feba0486c63a19fbbe5d84754823
Serhiy Katsyuba <serhiy.katsyuba@intel.com>
2024-08-28 16:43:09 +0200

    intel: nvl: add NVL platform
    Initial commit to add Nova Lake platform.
    
    Signed-off-by: Serhiy Katsyuba <serhiy.katsyuba@intel.com>
    Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

---
+++ src/platform/novalake/include/platform/lib/clk.h
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2022-2024 Intel Corporation.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ *         Keyon Jie <yang.jie@linux.intel.com>
+ *         Rander Wang <rander.wang@intel.com>
+ */
+
+#ifdef __SOF_LIB_CLK_H__
+
+#ifndef __PLATFORM_LIB_CLK_H__
+#define __PLATFORM_LIB_CLK_H__
+
+#include <ace/lib/clk.h>
+
+#define CLK_MAX_CPU_HZ		CONFIG_XTENSA_CCOUNT_HZ
+
+#define CPU_WOVCRO_FREQ_IDX	0
+
+#define CPU_LPRO_FREQ_IDX	1
+
+#define CPU_HPRO_FREQ_IDX	2
+
+#define CPU_LOWEST_FREQ_IDX	CPU_WOVCRO_FREQ_IDX
+
+#define CPU_DEFAULT_IDX		CPU_HPRO_FREQ_IDX
+
+#define NUM_CPU_FREQ		3
+
+#endif /* __PLATFORM_LIB_CLK_H__ */
+
+#else
+
+#error "This file shouldn't be included from outside of sof/lib/clk.h"
+
+#endif /* __SOF_LIB_CLK_H__ */
