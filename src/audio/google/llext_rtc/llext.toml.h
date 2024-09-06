99a35a1798144968f27372080c71ab536368e55b
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2024-09-06 17:03:32 +0200

    google_rtc_aec: enable building as an llext module
    Add support for LLEXT building to google_rtc_audio_processing. Only
    building in a stub configuration is supported, linking with a
    prebuilt library should be implemented separately.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

---
+++ src/audio/google/llext_rtc/llext.toml.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2021 Intel Corporation. All rights reserved.
+ */
+#include <tools/rimage/config/platform.toml>
+#define LOAD_TYPE "2"
+#include "../google_rtc_audio_processing.toml"
+
+[module]
+count = __COUNTER__
