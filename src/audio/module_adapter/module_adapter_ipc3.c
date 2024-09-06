thesofproject/sof

commit 0f4a246df10d6d7f0a1d0a2a1b80b09c7405fb54

module_adapter: fix switch case for spec parsing
This switch case is non exhaustive and has not guards on config types
which means assuming all types that are default are process types is
false. The fuzzer found a way to utilize a DAI to bypass IPC layer
checks but break the module adapter.

Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>

src/audio/module_adapter/module_adapter_ipc3.c
@@ -68,7 +68,17 @@ int module_adapter_init_data(struct comp_dev *dev,
 		data = spec;
 		break;
 	}
-	default:
+	case SOF_COMP_EQ_IIR:
+	case SOF_COMP_EQ_FIR:
+	case SOF_COMP_KEYWORD_DETECT:
+	case SOF_COMP_KPB:
+	case SOF_COMP_SELECTOR:
+	case SOF_COMP_DEMUX:
+	case SOF_COMP_MUX:
+	case SOF_COMP_DCBLOCK:
+	case SOF_COMP_SMART_AMP:
+	case SOF_COMP_MODULE_ADAPTER:
+	case SOF_COMP_NONE:
 	{
 		const struct ipc_config_process *ipc_module_adapter = spec;
 
@@ -76,6 +86,9 @@ int module_adapter_init_data(struct comp_dev *dev,
 		data = ipc_module_adapter->data;
 		break;
 	}
+	default:
+		comp_err(dev, "module_adapter_init_data() unsupported comp type %d", config->type);
+		return -EINVAL;
 	}
 
 	/* Copy initial config */

commit cc3765b25ff5953063498c68175a43dcc02cbc2c

module_adapter: add mixer component for initialization
Add mixer component in module_adapter initialization function
otherwise, for IPC3, we get the following error:
"module_adapter_init_data() unsupported comp type 6".

Fixes: 0f4a246df10d ("module_adapter: fix switch case for spec parsing")

Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

src/audio/module_adapter/module_adapter_ipc3.c
@@ -40,8 +40,8 @@ int module_adapter_init_data(struct comp_dev *dev,
 {
 	int ret;
 
-	const unsigned char *data;
-	uint32_t size;
+	const unsigned char *data = NULL;
+	uint32_t size = 0;
 
 	switch (config->type) {
 	case SOF_COMP_VOLUME:
@@ -68,6 +68,8 @@ int module_adapter_init_data(struct comp_dev *dev,
 		data = spec;
 		break;
 	}
+	case SOF_COMP_MIXER:
+		break;
 	case SOF_COMP_EQ_IIR:
 	case SOF_COMP_EQ_FIR:
 	case SOF_COMP_KEYWORD_DETECT: