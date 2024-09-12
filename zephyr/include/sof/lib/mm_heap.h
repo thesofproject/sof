b643d1005b3d5a4d339309856c0c9b4f034b3ed5
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2024-09-12 14:09:08 +0300

    zephyr: sof/lib/mm_heap.h added Zephyr version
    Definitions of mm_heap.h interface are not needed in Zephyr builds. To
    avoid pulling in XTOS definitions when building with
    CONFIG_SOF_ZEPHYR_STRICT_HEADERS=n, add a no-op version for Zephyr.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

---
+++ zephyr/include/sof/lib/mm_heap.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2024 Intel Corporation.
+ */
+
+#ifndef __SOF_LIB_MM_HEAP_H__
+#define __SOF_LIB_MM_HEAP_H__
+
+/* no-op on Zephyr */
+
+#endif /* __SOF_LIB_MM_HEAP_H__ */
