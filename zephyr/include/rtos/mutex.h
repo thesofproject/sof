3d18332bb136a4b6da3d733f3ee770d633d15126
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2024-09-18 14:51:14 +0300

    zephyr: add rtos/mutex.h
    The RTOS layer should provide the same interface for all OS'es.
    Currently rtos/mutex.h is only defined for XTOS. Add rtos/mutex.h
    also for Zephyr to allow generic SOF code to use this interface.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

---
+++ zephyr/include/rtos/mutex.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2024 Intel Corporation.
+//
+
+#ifndef __ZEPHYR_RTOS_MUTEX_H__
+#define __ZEPHYR_RTOS_MUTEX_H__
+
+#include <zephyr/kernel.h> /* k_mutex_*() */
+
+#endif /* __ZEPHYR_RTOS_MUTEX_H__ */
