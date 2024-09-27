04252f6981b72577841eb57846a46381855c9e28
Naveen Manohar <naveen.m@intel.com>
2024-09-27 14:31:54 +0530

    topology2: add support for rt721-sdw topology
    Adding topology support for rt721-sdw monolithic codec
    verified on ptl-rvp
    
    Signed-off-by: Naveen Manohar <naveen.m@intel.com>

--- tools/topology/topology2/production/tplg-targets-ace3.cmake
+++ tools/topology/topology2/production/tplg-targets-ace3.cmake
@@ -3,6 +3,11 @@
 # Array of "input-file-name;output-file-name;comma separated pre-processor variables"
 list(APPEND TPLGS
 # SDW topology for PTL RVP
+"cavs-sdw\;sof-ptl-rt721\;PLATFORM=ptl,SDW_DMIC=1,NUM_SDW_AMP_LINKS=1,\
+SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
+SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack,\
+HDMI1_ID=7,HDMI2_ID=8,HDMI3_ID=9"
+
 "cavs-sdw\;sof-ptl-rt722\;PLATFORM=ptl,SDW_DMIC=1,NUM_SDW_AMP_LINKS=1,\
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
 SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
