ace858c96a840de595df649a3374b7ab31ca0443
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2024-10-08 16:10:45 +0300

    rtos: add Zephyr implementation of sof/lib/memory.h
    Implement sof/lib/memory.h for Zephyr build and do not rely on
    the xtos version for Zephyr builds.
    
    Link: https://github.com/thesofproject/sof/issues/9015
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

---
+++ zephyr/include/sof/lib/memory.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2024 Intel Corporation.
+ */
+
+#ifndef __SOF_LIB_MEMORY_H__
+#define __SOF_LIB_MEMORY_H__
+
+#include <platform/lib/memory.h>
+
+#endif /* __SOF_LIB_MEMORY_H__ */
