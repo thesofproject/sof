aff0ac6b090c2aa523f0717cc094bebe02c51566
Marcin Szkudlinski <marcin.szkudlinski@intel.com>
2024-10-16 15:31:58 +0200

    unify API - the API

--- src/include/sof/audio/audio_buffer.h
+++ src/include/sof/audio/audio_buffer.h
@@ -32,6 +32,33 @@ struct audio_buffer_ops {
 	 *	  OPTIONAL
 	 */
 	void (*clean)(struct sof_audio_buffer *buffer);
+
+
+	/**
+	 * OPTIONAL: Notification to the sink implementation about changes in audio format
+	 *
+	 * Once any of *audio_stream_params elements changes, the implementation of
+	 * sink may need to perform some extra operations.
+	 * This callback will be called immediately after any change
+	 *
+	 * @retval 0 if success, negative if new parameters are not supported
+	 */
+	int (*on_audio_format_set)(struct sof_audio_buffer *buffer);
+
+	/**
+	 * OPTIONAL
+	 * see sink_set_params comments
+	 */
+	int (*audio_set_ipc_params)(struct sof_audio_buffer *buffer,
+				    struct sof_ipc_stream_params *params, bool force_update);
+
+	/**
+	 * OPTIONAL
+	 * see comment for sink_set_alignment_constants
+	 */
+	int (*set_alignment_constants)(struct sof_audio_buffer *buffer,
+				       const uint32_t byte_align,
+				       const uint32_t frame_align_req);
 };
 
 /* base class for all buffers, all buffers must inherit from it */
@@ -278,22 +305,10 @@ static inline struct sof_audio_buffer *sof_audio_buffer_from_source(struct sof_s
  * @param audio_buffer_ops pointer to required buffer virtual methods implementation
  * @param audio_stream_params pointer to audio stream (currently kept in buffer implementation)
  */
-static inline
 void audio_buffer_init(struct sof_audio_buffer *buffer, uint32_t buffer_type, bool is_shared,
-		       const struct source_ops *source_ops, const struct sink_ops *sink_ops,
+		       struct source_ops *source_ops, struct sink_ops *sink_ops,
 		       const struct audio_buffer_ops *audio_buffer_ops,
-		       struct sof_audio_stream_params *audio_stream_params)
-{
-	CORE_CHECK_STRUCT_INIT(&buffer, is_shared);
-	buffer->buffer_type = buffer_type;
-	buffer->ops = audio_buffer_ops;
-	buffer->audio_stream_params = audio_stream_params;
-	buffer->is_shared = is_shared;
-	source_init(audio_buffer_get_source(buffer), source_ops,
-		    audio_buffer_get_stream_params(buffer));
-	sink_init(audio_buffer_get_sink(buffer), sink_ops,
-		  audio_buffer_get_stream_params(buffer));
-}
+		       struct sof_audio_stream_params *audio_stream_params);
 
 /**
  * @brief free buffer and all allocated resources
