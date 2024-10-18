09fedf76311568b3635961c3b4fcdce43ea44af9
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2024-10-18 14:42:26 +0300

    Audio: Multiband-DRC: Tune: Expose all band parameters to top
    This change allows to tune all bands parameters from script
    top level. It prepares for a better testable default blob
    for the component. The actual setup blobs data is not impacted
    by this same since the parameters are kept the same.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/multiband_drc/tune/sof_example_multiband_drc.m
+++ src/audio/multiband_drc/tune/sof_example_multiband_drc.m
@@ -1,11 +1,24 @@
 function sof_example_multiband_drc()
 
+rz1 = [0.09 0.16 0.42 0.98];
+
 prm.name = "default";
 prm.sample_rate = 48000;
 prm.enable_emp_deemp = 1;
+prm.stage_gain = 4.4;
+prm.stage_ratio = 2;
 prm.num_bands = 3;
-prm.enable_bands = [1 2 3 0];
-prm.band_lower_freq = [0 200 2000 10000];
+prm.enable_bands     = [     1      2      3      0 ];
+prm.band_lower_freq  = [     0    200   2000  10000 ];
+prm.threshold        = [   -24    -24    -24    -24 ];
+prm.knee             = [    30     30     30     30 ];
+prm.ratio            = [    12     12     12     12 ];
+prm.attack           = [ 0.003  0.003  0.003  0.003 ];
+prm.release          = [   0.2    0.2    0.2    0.2 ];
+prm.pre_delay        = [ 0.006  0.006  0.006  0.006 ];
+prm.release_spacing  = [     5      5      5      5 ];
+prm.post_gain        = [     0      0      0      0 ];
+prm.release_zone     = [   rz1'   rz1'   rz1'   rz1'];
 export_multiband_drc(prm)
 
 prm.name = "passthrough";
@@ -44,10 +57,10 @@ enable_emp_deemp = prm.enable_emp_deemp;
 
 % The parameters of Emphasis IIR filter
 % stage_gain: The gain of each emphasis filter stage
-iir_params.stage_gain = 4.4;
+iir_params.stage_gain = prm.stage_gain;
 % stage_ratio: The frequency ratio for each emphasis filter stage to the
 %              previous stage
-iir_params.stage_ratio = 2;
+iir_params.stage_ratio = prm.stage_ratio;
 % anchor: The frequency of the first emphasis filter, in normalized frequency
 %         (in [0, 1], relative to half of the sample rate)
 iir_params.anchor = 15000 / nyquist;
@@ -66,60 +79,22 @@ iir_params.anchor = 15000 / nyquist;
 %   band_lower_freq: The lower frequency of the band, in normalized frequency
 %                    (in [0, 1], relative to half of the sample rate)
 
-% Band 1 DRC parameter
-drc_params(1).enabled = prm.enable_bands(1);
-drc_params(1).threshold = -24;
-drc_params(1).knee = 30;
-drc_params(1).ratio = 12;
-drc_params(1).attack = 0.003;
-drc_params(1).release = 0.2;
-drc_params(1).pre_delay = 0.006;
-drc_params(1).release_zone = [0.09 0.16 0.42 0.98];
-drc_params(1).release_spacing = 5;
-drc_params(1).post_gain = 0;
-drc_params(1).band_lower_freq = prm.band_lower_freq(1) / nyquist;
-
-% Band 2 DRC parameter (only valid if num_bands > 1)
-drc_params(2).enabled = prm.enable_bands(2);
-drc_params(2).threshold = -24;
-drc_params(2).knee = 30;
-drc_params(2).ratio = 12;
-drc_params(2).attack = 0.003;
-drc_params(2).release = 0.2;
-drc_params(2).pre_delay = 0.006;
-drc_params(2).release_zone = [0.09 0.16 0.42 0.98];
-drc_params(2).release_spacing = 5;
-drc_params(2).post_gain = 0;
-drc_params(2).band_lower_freq = prm.band_lower_freq(2) / nyquist;
-
-% Band 3 DRC parameter (only valid if num_bands > 2)
-drc_params(3).enabled = prm.enable_bands(3);
-drc_params(3).threshold = -24;
-drc_params(3).knee = 30;
-drc_params(3).ratio = 12;
-drc_params(3).attack = 0.003;
-drc_params(3).release = 0.2;
-drc_params(3).pre_delay = 0.006;
-drc_params(3).release_zone = [0.09 0.16 0.42 0.98];
-drc_params(3).release_spacing = 5;
-drc_params(3).post_gain = 0;
-drc_params(3).band_lower_freq = prm.band_lower_freq(3) / nyquist;
-
-% Band 4 DRC parameter (only valid if num_bands > 3)
-drc_params(4).enabled = prm.enable_bands(4);
-drc_params(4).threshold = -24;
-drc_params(4).knee = 30;
-drc_params(4).ratio = 12;
-drc_params(4).attack = 0.003;
-drc_params(4).release = 0.2;
-drc_params(4).pre_delay = 0.006;
-drc_params(4).release_zone = [0.09 0.16 0.42 0.98];
-drc_params(4).release_spacing = 5;
-drc_params(4).post_gain = 0;
-drc_params(4).band_lower_freq = prm.band_lower_freq(4) / nyquist;
+for i = 1:4
+	% Band n DRC parameter, (only valid if num_bands >= i)
+	drc_params(i).enabled = prm.enable_bands(i);
+	drc_params(i).threshold = prm.threshold(i);
+	drc_params(i).knee = prm.knee(i);
+	drc_params(i).ratio = prm.ratio(i);
+	drc_params(i).attack = prm.attack(i);
+	drc_params(i).release = prm.release(i);
+	drc_params(i).pre_delay = prm.pre_delay(i);
+	drc_params(i).release_zone = prm.release_zone(:, i)';
+	drc_params(i).release_spacing = prm.release_spacing(i);
+	drc_params(i).post_gain = prm.post_gain(i);
+	drc_params(i).band_lower_freq = prm.band_lower_freq(i) / nyquist;
+end
 
 % Generate Emphasis/Deemphasis IIR filter quantized coefs struct from parameters
-
 
 [emp_coefs, deemp_coefs] = sof_iir_gen_quant_coefs(iir_params);
 
