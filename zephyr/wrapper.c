eac3c4470a13bf99698f6be2a3155dd46adb0de7
Daniel Baluta <daniel.baluta@nxp.com>
2024-10-23 20:10:48 +0300

    imx8mp: Switch to native drivers and timer domain
    This includes:
       - switching all topologies from DMA domain to timer domain.
       - interrupt related fixes done via CONFIG_IMX8M.
       - remove SOF standalone drivers from compilation
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

--- zephyr/wrapper.c
+++ zephyr/wrapper.c
@@ -53,7 +53,7 @@ const char irq_name_level2[] = "level2";
 const char irq_name_level5[] = "level5";
 
 /* imx currently has no IRQ driver in Zephyr so we force to xtos IRQ */
-#if defined(CONFIG_IMX8M) || defined(CONFIG_AMD)
+#if defined(CONFIG_AMD)
 int interrupt_register(uint32_t irq, void(*handler)(void *arg), void *arg)
 {
 #ifdef CONFIG_DYNAMIC_INTERRUPTS
