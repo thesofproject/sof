7718e435822745263c33b35def564438feb77157
Jyri Sarha <jyri.sarha@linux.intel.com>
2024-11-04 12:17:41 +0200

    probe: llext: Enable building probe as loadable llext module
    To build PROBE as module just set CONFIG_PROBE=m in Kconfig.
    
    Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

---
+++ src/probe/llext/llext.toml.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2024 Intel Corporation. All rights reserved.
+ */
+#include <tools/rimage/config/platform.toml>
+#define LOAD_TYPE "2"
+#include "../probe.toml"
+
+[module]
+count = __COUNTER__
