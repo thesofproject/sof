7ce4e07775999c402a65b68859dddf182e58f01a
Bard Liao <yung-chuan.liao@linux.intel.com>
2024-11-20 16:33:08 +0800

    topology2: add topologies for each sdca functions
    Add topologies for each sdca function. Linux kernel will load the
    corresponding topology per sdca function. The naming rule is
    sof-<platform>-<function>-<BE id>
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- tools/topology/topology2/production/tplg-targets-ace3.cmake
+++ tools/topology/topology2/production/tplg-targets-ace3.cmake
@@ -57,4 +57,13 @@ SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
 "cavs-sdw\;sof-ptl-cs42l43-l2-cs35l56x6-l13\;NUM_SDW_AMP_LINKS=3,SDW_DMIC=1,\
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
 SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
+
+# Split topologies
+"cavs-sdw\;sof-ptl-dmic-2ch-id5\;PLATFORM=mtl,SDW_JACK=false,NUM_HDMIS=0,NUM_DMICS=2,\
+PDM1_MIC_A_ENABLE=0,PDM1_MIC_B_ENABLE=0,DMIC0_ID=5,DMIC1_ID=6,PREPROCESS_PLUGINS=nhlt,\
+NHLT_BIN=nhlt-sof-ptl-dmic-2ch-id5.bin"
+
+"cavs-sdw\;sof-ptl-dmic-4ch-id5\;PLATFORM=mtl,SDW_JACK=false,NUM_HDMIS=0,NUM_DMICS=4,\
+PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,DMIC0_ID=5,DMIC1_ID=6,PREPROCESS_PLUGINS=nhlt,\
+NHLT_BIN=nhlt-sof-ptl-dmic-4ch-id5.bin"
 )
