fb969f22b458ab4dfc0c53c227fe14e4fd1a80f5
Darren.Ye <darren.ye@mediatek.com>
2025-01-20 15:04:11 +0800

    platform: mtk: add platform driver support for mt8196
    Add support for dai, dma platform driver.
    
    Signed-off-by: Darren.Ye <darren.ye@mediatek.com>

--- src/platform/mt8196/lib/dma.c
+++ src/platform/mt8196/lib/dma.c
@@ -3,14 +3,18 @@
  * Copyright(c) 2024 MediaTek. All rights reserved.
  *
  * Author: Hailong Fan <hailong.fan@mediatek.com>
+ *         Darren Ye <darren.ye@mediatek.com>
  */
 
 #include <rtos/interrupt.h>
 #include <rtos/spinlock.h>
 #include <sof/common.h>
+#include <sof/drivers/afe-memif.h>
 #include <sof/lib/dma.h>
 #include <sof/lib/memory.h>
 #include <rtos/sof.h>
+#include <mt8196-afe-reg.h>
+#include <mt8196-afe-common.h>
 
 extern const struct dma_ops dummy_dma_ops;
 
@@ -24,6 +28,17 @@ static SHARED_DATA struct dma dma[PLATFORM_NUM_DMACS] = {
 	},
 	.ops	= &dummy_dma_ops,
 },
+{
+	.plat_data = {
+		.id		= DMA_ID_AFE_MEMIF,
+		.dir		= DMA_DIR_MEM_TO_DEV | DMA_DIR_DEV_TO_MEM,
+		.devs		= DMA_DEV_AFE_MEMIF,
+		.base		= AFE_BASE_ADDR,
+		.channels	= MT8196_MEMIF_NUM,
+	},
+	.ops	= &memif_ops,
+},
+
 };
 
 static const struct dma_info lib_dma = {
