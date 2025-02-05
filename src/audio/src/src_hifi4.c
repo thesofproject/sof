f44f61b31f4d52cb2ddd4c395665424fc238673e
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-02-05 16:54:33 +0200

    Audio: SRC: Remove unnecessary initialization from fir_filter()
    The intrinsics "functions" set these in their first use despite
    it doesn't appear so if looking the code like normal C-functions.
    E.g. this code line loads data from address coepf into coef2 and
    advances coefp.
    
    	AE_L32X2F24_IP(coef2, coefp, sizeof(ae_f24x2));
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/src/src_hifi4.c
+++ src/audio/src/src_hifi4.c
@@ -180,10 +180,10 @@ static inline void fir_filter(ae_f32 *rp, const void *cp, ae_f32 *wp0,
 	 */
 	ae_f64 a0;
 	ae_f64 a1;
-	ae_f24x2 data2 = AE_ZERO24();
-	ae_f24x2 coef2 = AE_ZERO24();
-	ae_f24x2 d0 = AE_ZERO24();
-	ae_f24x2 d1 = AE_ZERO24();
+	ae_f24x2 data2;
+	ae_f24x2 coef2;
+	ae_f24x2 d0;
+	ae_f24x2 d1;
 	ae_f24x2 *coefp;
 	ae_f24x2 *dp;
 	ae_f24 *dp1;
