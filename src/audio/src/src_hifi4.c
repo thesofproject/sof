0401893c5c4776626f0218f1260b14ffef27b82e
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-02-10 16:21:45 +0200

    Audio: SRC: Optimize HiFi4 version code
    This patch applies to HiF4 part of the optimizations done for HiFi5
    version.
    
    - As a fix for code the coefficients read align register is primed
      for 16 bit coefficients version. Without priming the load works
      correctly only for aligned addresses.
    - The FIR filter functions are separated for stereo and any channels
      count versions. It avoids channels count check for every
      intermediate and final sample value.
    - The 32 bit coefficients version is similarly separated for stereo
      and any channels version.
    - The data loads are changed to more efficient 32 bits without
      s24 conversion. The dual-MAC is changed to 32x32 with Q17.47
      format.
    - The data store is changed in stereo version to 64 bit store of
      stereo frame.
    - In src_polyphase_stage_cir() function the filters process loops
      are separated for stereo and for any channels count.
    
    The MCPS is improved from 18.03 to 16.00 MCPS in stereo 32 bit
    44.1 kHz to 48 kHz conversion.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/src/src_hifi4.c
+++ src/audio/src/src_hifi4.c
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
-// Copyright(c) 2022 Intel Corporation. All rights reserved.
+// Copyright(c) 2022-2025 Intel Corporation.
 //
 // Author: Krzysztof Frydryk <krzysztofx.frydryk@intel.com>
+//         Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
 
-/* HiFi4 optimized code parts for SRC */
+/* HiFi5 optimized code parts for SRC */
 
 #include "src_config.h"
 
@@ -23,20 +24,21 @@
 #include <zephyr/sys/util.h>
 #endif /* __ZEPHYR__ */
 
-/* HiFi4 has
- * 16x 64 bit registers in register file AE_DR
- */
+#if SRC_SHORT
+#define SRC_COEF_SIZE	sizeof(int16_t)
+#else
+#define SRC_COEF_SIZE	sizeof(int32_t)
+#endif
 
 #if SRC_SHORT /* 16 bit coefficients version */
 
-static inline void fir_filter(ae_f32 *rp, const void *cp, ae_f32 *wp0,
-			      const int taps_div_4, const int shift,
-			      const int nch)
+static inline void fir_filter_2ch(ae_f32 *rp, const void *cp, ae_f32 *wp0,
+				  const int taps_div_4, const int shift)
 {
 	/* This function uses
-	 * 6x 64 bit registers
-	 * 3x integers
-	 * 5x address pointers,
+	 * 7x 64 bit registers
+	 * 2x integers
+	 * 3x address pointers,
 	 */
 	ae_f64 a0;
 	ae_f64 a1;
@@ -47,73 +49,89 @@ static inline void fir_filter(ae_f32 *rp, const void *cp, ae_f32 *wp0,
 	ae_f32x2 data2;
 	ae_f16x4 *coefp;
 	ae_f32x2 *dp;
+	int i;
+	ae_f32 *wp = wp0;
+	const int inc = 2 * sizeof(int32_t);
+
+	/* Move data pointer back by one sample to start from right
+	 * channel sample. Discard read value p0.
+	 */
+	dp = (ae_f32x2 *)rp;
+	AE_L32_XC(d0, (ae_f32 *)dp, -sizeof(ae_f32));
+
+	/* Reset coefficient pointer and clear accumulator */
+	coefp = (ae_f16x4 *)cp;
+	a0 = AE_ZERO64();
+	a1 = AE_ZERO64();
+	u = AE_LA64_PP(coefp);
+
+	/* Compute FIR filter for current channel with four
+	 * taps per every loop iteration.  Four coefficients
+	 * are loaded simultaneously. Data is read
+	 * from interleaved buffer with stride of channels
+	 * count.
+	 */
+	for (i = 0; i < taps_div_4; i++) {
+		/* Load four coefficients */
+		AE_LA16X4_IP(coef4, u, coefp);
+
+		/* Load two data samples from two channels */
+		AE_L32X2_XC(d0, dp, inc); /* r0, l0 */
+		AE_L32X2_XC(d1, dp, inc); /* r1, l1 */
+
+		/* Select to data2 sequential samples from a channel
+		 * and then accumulate to a0 and a1
+		 * data2_h * coef4_3 + data2_l * coef4_2.
+		 * The data is 32 bits Q1.31 and coefficient 16 bits
+		 * Q1.15. The accumulators are Q17.47.
+		 */
+		data2 = AE_SEL32_LL(d0, d1); /* l0, l1 */
+		AE_MULAAFD32X16_H3_L2(a0, data2, coef4);
+		data2 = AE_SEL32_HH(d0, d1); /* r0, r1 */
+		AE_MULAAFD32X16_H3_L2(a1, data2, coef4);
+
+		/* Load two data samples from two channels */
+		AE_L32X2_XC(d0, dp, inc); /* r2, l2 */
+		AE_L32X2_XC(d1, dp, inc); /* r3, l3 */
+
+		/* Accumulate
+		 * data2_h * coef4_1 + data2_l * coef4_0.
+		 */
+		data2 = AE_SEL32_LL(d0, d1); /* l2, l3 */
+		AE_MULAAFD32X16_H1_L0(a0, data2, coef4);
+		data2 = AE_SEL32_HH(d0, d1); /* r2, r3 */
+		AE_MULAAFD32X16_H1_L0(a1, data2, coef4);
+	}
+
+	/* Scale FIR output with right shifts, round/saturate
+	 * to Q1.31, and store 32 bit output.
+	 */
+	AE_S32_L_XP(AE_ROUND32F48SSYM(AE_SRAA64(a0, shift)), wp, sizeof(int32_t));
+	AE_S32_L_XP(AE_ROUND32F48SSYM(AE_SRAA64(a1, shift)), wp, sizeof(int32_t));
+}
+
+static inline void fir_filter(ae_f32 *rp, const void *cp, ae_f32 *wp0,
+			      const int taps_div_4, const int shift,
+			      const int nch)
+{
+	/* This function uses
+	 * 6x 64 bit registers
+	 * 3x integers
+	 * 3x address pointers,
+	 */
+	ae_f64 a0;
+	ae_valign u;
+	ae_f16x4 coef4;
+	ae_f32x2 d0;
+	ae_f32x2 d1;
+	ae_f32x2 data2;
+	ae_f16x4 *coefp;
 	ae_f32 *dp0;
 	ae_f32 *dp1;
 	int i;
 	int j;
 	ae_f32 *wp = wp0;
 	const int inc = nch * sizeof(int32_t);
-
-	if (nch == 2) {
-		/* Move data pointer back by one sample to start from right
-		 * channel sample. Discard read value p0.
-		 */
-		dp = (ae_f32x2 *)rp;
-		AE_L32_XC(d0, (ae_f32 *)dp, -sizeof(ae_f32));
-
-		/* Reset coefficient pointer and clear accumulator */
-		coefp = (ae_f16x4 *)cp;
-		a0 = AE_ZERO64();
-		a1 = AE_ZERO64();
-
-		/* Compute FIR filter for current channel with four
-		 * taps per every loop iteration.  Four coefficients
-		 * are loaded simultaneously. Data is read
-		 * from interleaved buffer with stride of channels
-		 * count.
-		 */
-		for (i = 0; i < taps_div_4; i++) {
-			/* Load four coefficients */
-			AE_LA16X4_IP(coef4, u, coefp);
-
-			/* Load two data samples from two channels */
-			AE_L64_XC(d0, dp, inc); /* r0, l0 */
-			AE_L64_XC(d1, dp, inc); /* r1, l1 */
-
-			/* Select to data2 sequential samples from a channel
-			 * and then accumulate to a0 and a1
-			 * data2_h * coef4_3 + data2_l * coef4_2.
-			 * The data is 32 bits Q1.31 and coefficient 16 bits
-			 * Q1.15. The accumulators are Q17.47.
-			 */
-			data2 = AE_SEL32_LL(d0, d1); /* l0, l1 */
-			AE_MULAAFD32X16_H3_L2(a0, data2, coef4);
-			data2 = AE_SEL32_HH(d0, d1); /* r0, r1 */
-			AE_MULAAFD32X16_H3_L2(a1, data2, coef4);
-
-			/* Load two data samples from two channels */
-			AE_L64_XC(d0, dp, inc); /* r2, l2 */
-			AE_L64_XC(d1, dp, inc); /* r3, l3 */
-
-			/* Accumulate
-			 * data2_h * coef4_1 + data2_l * coef4_0.
-			 */
-			data2 = AE_SEL32_LL(d0, d1); /* l2, l3 */
-			AE_MULAAFD32X16_H1_L0(a0, data2, coef4);
-			data2 = AE_SEL32_HH(d0, d1); /* r2, r3 */
-			AE_MULAAFD32X16_H1_L0(a1, data2, coef4);
-		}
-
-		/* Scale FIR output with right shifts, round/saturate
-		 * to Q1.31, and store 32 bit output.
-		 */
-		AE_S32_L_XP(AE_ROUND32F48SSYM(AE_SRAA64(a0, shift)), wp,
-			    sizeof(int32_t));
-		AE_S32_L_XP(AE_ROUND32F48SSYM(AE_SRAA64(a1, shift)), wp,
-			    sizeof(int32_t));
-
-		return;
-	}
 
 	dp1 = (ae_f32 *)rp;
 	for (j = 0; j < nch; j++) {
@@ -124,6 +142,7 @@ static inline void fir_filter(ae_f32 *rp, const void *cp, ae_f32 *wp0,
 		/* Reset coefficient pointer and clear accumulator */
 		coefp = (ae_f16x4 *)cp;
 		a0 = AE_ZERO64();
+		u = AE_LA64_PP(coefp);
 
 		/* Compute FIR filter for current channel with four
 		 * taps per every loop iteration. Data is read from
@@ -169,139 +188,144 @@ static inline void fir_filter(ae_f32 *rp, const void *cp, ae_f32 *wp0,
 
 #else /* 32bit coefficients version */
 
+static inline void fir_filter_2ch(ae_f32 *rp, const void *cp, ae_f32 *wp0,
+				  const int taps_div_4, const int shift)
+{
+	ae_f64 a0 = AE_ZERO64();
+	ae_f64 a1 = AE_ZERO64();
+	ae_valign coef_align;
+	ae_f32x2 coef32;
+	ae_f32x2 coef10;
+	ae_f32x2 sample10;
+	ae_f32x2 f0;
+	ae_f32x2 f1;
+	ae_int32x2 *coefp;
+	ae_f32x2 *dp;
+	ae_f32 *wp = wp0;
+	const int inc = 2 * sizeof(int32_t);
+	int i;
+
+	/* Move data pointer back by one sample to start from right
+	 * channel sample. Discard read value p0.
+	 */
+	dp = (ae_f32x2 *)rp;
+	AE_L32_XC(f0, (ae_f32 *)dp, -sizeof(ae_f32));
+
+	/* Reset coefficient pointer and clear accumulator */
+	coefp = (ae_int32x2 *)cp;
+	coef_align = AE_LA64_PP(coefp);
+
+	/* Compute FIR filter for current channel with four
+	 * taps per every loop iteration.  Two coefficients
+	 * are loaded simultaneously. Data is read
+	 * from interleaved buffer with stride of channels
+	 * count.
+	 */
+	for (i = 0; i < taps_div_4; i++) {
+		/* Load four coefficients */
+		AE_LA32X2_IP(coef10, coef_align, coefp);
+		AE_LA32X2_IP(coef32, coef_align, coefp);
+
+		/* Load two data samples from two channels. Note: Due to
+		 * polyphase array data start shift for sub-filters can't
+		 * use 128 bits load due to align requirement.
+		 */
+		AE_L32X2_XC(f0, dp, inc); /* f0.h is left0, f0.l is right0 */
+		AE_L32X2_XC(f1, dp, inc); /* f1.h is left1, f1.l is right1 */
+
+		/* a0 (left)  += left10.h  (left1)  * coef10.h (coef2)
+		 *            += left10.l  (left0)  * coef10.l (coef1)
+		 * a1 (right) += right10.h (right1) * coef10.h (coef2)
+		 *            += right10.l (right0) * coef10.l (coef1)
+		 */
+		sample10 = AE_SEL32_HH(f0, f1);
+		AE_MULAAFD32RA_HH_LL(a0, sample10, coef10);
+		sample10 = AE_SEL32_LL(f0, f1);
+		AE_MULAAFD32RA_HH_LL(a1, sample10, coef10);
+
+		/* Repeat for next two taps */
+		AE_L32X2_XC(f0, dp, inc); /* f0.h is left2, f0.l is right2 */
+		AE_L32X2_XC(f1, dp, inc); /* f1.h is left3, f1.l is right3 */
+		sample10 = AE_SEL32_HH(f0, f1);
+		AE_MULAAFD32RA_HH_LL(a0, sample10, coef32);
+		sample10 = AE_SEL32_LL(f0, f1);
+		AE_MULAAFD32RA_HH_LL(a1, sample10, coef32);
+	}
+
+	/* Scale FIR output with right shifts, round/saturate
+	 * to Q1.31, and store 32 bit output.
+	 */
+	f0 = AE_ROUND32X2F48SASYM(AE_SRAA64(a1, shift), AE_SRAA64(a0, shift));
+	AE_S32X2_I(f0, (ae_int32x2 *)wp, 0);
+}
+
 static inline void fir_filter(ae_f32 *rp, const void *cp, ae_f32 *wp0,
 			      const int taps_div_4, const int shift,
 			      const int nch)
 {
-	/* This function uses
-	 * 6x 64 bit registers
-	 * 3x integers
-	 * 5x address pointers,
-	 */
-	ae_f64 a0;
-	ae_f64 a1;
-	ae_f24x2 data2;
-	ae_f24x2 coef2;
-	ae_f24x2 d0;
-	ae_f24x2 d1;
-	ae_f24x2 *coefp;
-	ae_f24x2 *dp;
-	ae_f24 *dp1;
-	ae_f24 *dp0;
-	int i;
-	int j;
+	ae_f64 a0 = AE_ZERO64();
+	ae_valign coef_align;
+	ae_f32x2 coef32;
+	ae_f32x2 coef10;
+	ae_f32x2 sample10;
+	ae_f32x2 f0;
+	ae_f32x2 f1;
+	ae_int32x2 *coefp;
+	ae_f32 *dp;
+	ae_f32 *dp1 = rp;
 	ae_f32 *wp = wp0;
 	const int inc = nch * sizeof(int32_t);
+	int i;
+	int j;
 
-	if (nch == 2) {
-		/* Move data pointer back by one sample to start from right
-		 * channel sample. Discard read value p0.
-		 */
-		dp = (ae_f24x2 *)rp;
-		AE_L32F24_XC(d0, (ae_f24 *)dp, -sizeof(ae_f24));
-
-		/* Reset coefficient pointer and clear accumulator */
-		coefp = (ae_f24x2 *)cp;
-		a0 = AE_ZERO64();
-		a1 = AE_ZERO64();
-
-		/* Compute FIR filter for current channel with four
-		 * taps per every loop iteration.  Two coefficients
-		 * are loaded simultaneously. Data is read
-		 * from interleaved buffer with stride of channels
-		 * count.
-		 */
-		for (i = 0; i < taps_div_4; i++) {
-			/* Load two coefficients. Coef2_h contains tap *coefp
-			 * and coef2_l contains the next tap.
-			 */
-			/* TODO: Ensure coefficients are 64 bits aligned */
-			AE_L32X2F24_IP(coef2, coefp, sizeof(ae_f24x2));
-
-			/* Load two data samples from two channels */
-			AE_L32X2F24_XC(d0, dp, inc); /* r0, l0 */
-			AE_L32X2F24_XC(d1, dp, inc); /* r1, l1 */
-
-			/* Select to data2 two successive left channel samples
-			 * from d0 and d1, multiply-add and accumulate to a0.
-			 * Select to data2 two successive right channel samples
-			 * from d0 and d1, multiply-add and accumulate to a1.
-			 * data2_h * coef2_h + data2_l * coef2_l. The Q1.31
-			 * data and Q1.15 coefficients are used as 24 bits as
-			 * Q1.23 values.
-			 */
-			data2 = AE_SELP24_LL(d0, d1);
-			AE_MULAAFP24S_HH_LL(a0, data2, coef2);
-			data2 = AE_SELP24_HH(d0, d1);
-			AE_MULAAFP24S_HH_LL(a1, data2, coef2);
-
-			/* Repeat for next two taps */
-			AE_L32X2F24_IP(coef2, coefp, sizeof(ae_f24x2));
-			AE_L32X2F24_XC(d0, dp, inc); /* r2, l2 */
-			AE_L32X2F24_XC(d1, dp, inc); /* r3, l3 */
-			data2 = AE_SELP24_LL(d0, d1);
-			AE_MULAAFP24S_HH_LL(a0, data2, coef2);
-			data2 = AE_SELP24_HH(d0, d1);
-			AE_MULAAFP24S_HH_LL(a1, data2, coef2);
-		}
-
-		/* Scale FIR output with right shifts, round/saturate
-		 * to Q1.31, and store 32 bit output.
-		 */
-		AE_S32_L_XP(AE_ROUND32F48SSYM(AE_SRAA64(a0, shift)), wp,
-			    sizeof(int32_t));
-		AE_S32_L_XP(AE_ROUND32F48SSYM(AE_SRAA64(a1, shift)), wp,
-			    sizeof(int32_t));
-
-		return;
-	}
-
-	dp1 = (ae_f24 *)rp;
 	for (j = 0; j < nch; j++) {
 		/* Copy pointer and advance to next ch with dummy load */
-		dp0 = dp1;
-		AE_L32F24_XC(data2, dp1, -sizeof(ae_f24));
+		dp = dp1;
+		AE_L32_XC(f0, dp1, -sizeof(ae_f32));
 
 		/* Reset coefficient pointer and clear accumulator */
-		coefp = (ae_f24x2 *)cp;
+		coefp = (ae_int32x2 *)cp;
 		a0 = AE_ZERO64();
+		coef_align = AE_LA64_PP(coefp);
 
 		/* Compute FIR filter for current channel with four
 		 * taps per every loop iteration. Data is read from
 		 * interleaved buffer with stride of channels count.
 		 */
 		for (i = 0; i < taps_div_4; i++) {
-			/* Load two coefficients */
-			coef2 = *coefp++;
+			/* Load four coefficients
+			 * TODO: Ensure coefficients are 128 bits aligned
+			 */
+			AE_LA32X2_IP(coef10, coef_align, coefp);
+			AE_LA32X2_IP(coef32, coef_align, coefp);
 
 			/* Load two data samples, place to high and
 			 * low of data2.
 			 */
-			AE_L32F24_XC(d0, dp0, inc);
-			AE_L32F24_XC(d1, dp0, inc);
-			data2 = AE_SELP24_LL(d0, d1);
+			AE_L32_XC(f0, dp, inc);
+			AE_L32_XC(f1, dp, inc);
+			sample10 = AE_SEL32_LL(f0, f1);
 
 			/* Accumulate to data2_h * coef2_h +
 			 * data2_l*coef2_l. The Q1.31 bit data is used
 			 * as Q1.23 from MSB side bits of the 32 bit
 			 * word. The accumulator m is Q17.47.
 			 */
-			AE_MULAAFD24_HH_LL(a0, data2, coef2);
+			AE_MULAAFD32RA_HH_LL(a0, sample10, coef10);
 
 			/* Repeat the same for next two filter taps */
-			coef2 = *coefp++;
-			AE_L32F24_XC(d0, dp0, inc);
-			AE_L32F24_XC(d1, dp0, inc);
-			data2 = AE_SELP24_LL(d0, d1);
-			AE_MULAAFD24_HH_LL(a0, data2, coef2);
+			AE_L32_XC(f0, dp, inc);
+			AE_L32_XC(f1, dp, inc);
+			sample10 = AE_SEL32_LL(f0, f1);
+			AE_MULAAFD32RA_HH_LL(a0, sample10, coef32);
 		}
 
 		/* Scale FIR output with right shifts, round/saturate Q17.47
 		 * to Q1.31, and store 32 bit output. Advance write
 		 * pointer to next sample.
 		 */
-		AE_S32_L_XP(AE_ROUND32F48SSYM(AE_SRAA64(a0, shift)), wp,
-			    sizeof(int32_t));
+		f0 = AE_ROUND32F48SASYM(AE_SRAA64(a0, shift));
+		AE_S32_L_XP(f0, wp, sizeof(int32_t));
 	}
 }
 
@@ -315,7 +339,7 @@ void src_polyphase_stage_cir(struct src_stage_prm *s)
 	 * 16x integers
 	 * 11x address pointers,
 	 */
-	ae_int32x2 q = AE_ZERO32();
+	ae_int32x2 q;
 	ae_f32 *rp;
 	ae_f32 *wp;
 	int i;
@@ -342,12 +366,7 @@ void src_polyphase_stage_cir(struct src_stage_prm *s)
 	int32_t *y_wptr = (int32_t *)s->y_wptr;
 	int32_t *x_end_addr = (int32_t *)s->x_end_addr;
 	int32_t *y_end_addr = (int32_t *)s->y_end_addr;
-
-#if SRC_SHORT
-	const size_t subfilter_size = cfg->subfilter_length * sizeof(int16_t);
-#else
-	const size_t subfilter_size = cfg->subfilter_length * sizeof(int32_t);
-#endif
+	const size_t subfilter_size = cfg->subfilter_length * SRC_COEF_SIZE;
 
 	for (n = 0; n < s->times; n++) {
 		/* Input data to filter */
@@ -388,17 +407,25 @@ void src_polyphase_stage_cir(struct src_stage_prm *s)
 		 * sub-filters.
 		 */
 		wp = (ae_f32 *)fir->out_rp;
-		for (i = 0; i < cfg->num_of_subfilters; i++) {
-			fir_filter(rp, cp, wp, taps_div_4, cfg->shift, nch);
-			wp += nch_x_odm;
-			cp = (uint8_t *)cp + subfilter_size;
-			src_inc_wrap((int32_t **)&wp, out_delay_end, out_size);
-
-			/* Circular advance pointer rp by number of
-			 * channels x input delay multiplier. Loaded value q
-			 * is discarded.
-			 */
-			AE_L32_XC(q, rp, nch_x_idm_sz);
+		if (nch == 2) {
+			for (i = 0; i < cfg->num_of_subfilters; i++) {
+				fir_filter_2ch(rp, cp, wp, taps_div_4, cfg->shift);
+				wp += nch_x_odm;
+				cp = (uint8_t *)cp + subfilter_size;
+				src_inc_wrap((int32_t **)&wp, out_delay_end, out_size);
+				/* Circular advance pointer rp by number of channels x input
+				 * delay multiplier. Loaded value q is discarded.
+				 */
+				AE_L32_XC(q, rp, nch_x_idm_sz);
+			}
+		} else {
+			for (i = 0; i < cfg->num_of_subfilters; i++) {
+				fir_filter(rp, cp, wp, taps_div_4, cfg->shift, nch);
+				wp += nch_x_odm;
+				cp = (uint8_t *)cp + subfilter_size;
+				src_inc_wrap((int32_t **)&wp, out_delay_end, out_size);
+				AE_L32_XC(q, rp, nch_x_idm_sz);
+			}
 		}
 
 		/* Output */
@@ -464,12 +491,7 @@ void src_polyphase_stage_cir_s16(struct src_stage_prm *s)
 	int16_t *y_wptr = (int16_t *)s->y_wptr;
 	int16_t *x_end_addr = (int16_t *)s->x_end_addr;
 	int16_t *y_end_addr = (int16_t *)s->y_end_addr;
-
-#if SRC_SHORT
-	const size_t subfilter_size = cfg->subfilter_length * sizeof(int16_t);
-#else
-	const size_t subfilter_size = cfg->subfilter_length * sizeof(int32_t);
-#endif
+	const size_t subfilter_size = cfg->subfilter_length * SRC_COEF_SIZE;
 
 	for (n = 0; n < s->times; n++) {
 		/* Input data */
@@ -509,17 +531,25 @@ void src_polyphase_stage_cir_s16(struct src_stage_prm *s)
 		 * sub-filters.
 		 */
 		wp = (ae_f32 *)fir->out_rp;
-		for (i = 0; i < cfg->num_of_subfilters; i++) {
-			fir_filter(rp, cp, wp, taps_div_4, cfg->shift, nch);
-			wp += nch_x_odm;
-			cp = (uint8_t *)cp + subfilter_size;
-			src_inc_wrap((int32_t **)&wp, out_delay_end, out_size);
-
-			/* Circular advance pointer rp by number of
-			 * channels x input delay multiplier. Loaded value q
-			 * is discarded.
-			 */
-			AE_L32_XC(q, rp, nch_x_idm_sz);
+		if (nch == 2) {
+			for (i = 0; i < cfg->num_of_subfilters; i++) {
+				fir_filter_2ch(rp, cp, wp, taps_div_4, cfg->shift);
+				wp += nch_x_odm;
+				cp = (uint8_t *)cp + subfilter_size;
+				src_inc_wrap((int32_t **)&wp, out_delay_end, out_size);
+				/* Circular advance pointer rp by number of channels x input delay
+				 * multiplier. Loaded value q is discarded.
+				 */
+				AE_L32_XC(q, rp, nch_x_idm_sz);
+			}
+		} else {
+			for (i = 0; i < cfg->num_of_subfilters; i++) {
+				fir_filter(rp, cp, wp, taps_div_4, cfg->shift, nch);
+				wp += nch_x_odm;
+				cp = (uint8_t *)cp + subfilter_size;
+				src_inc_wrap((int32_t **)&wp, out_delay_end, out_size);
+				AE_L32_XC(q, rp, nch_x_idm_sz);
+			}
 		}
 
 		/* Output */
