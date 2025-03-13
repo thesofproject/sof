cc2f354716d326b979817f785eb65ae24dc45da7
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-03-13 20:51:31 +0200

    Audio: Volume: Remove unnecessary clear of HiFi registers
    These registers are set later in the code in the load
    instructions. This change saves a bit of code size and
    a tiny amount of processor cycles.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/volume/volume_hifi4_with_peakvol.c
+++ src/audio/volume/volume_hifi4_with_peakvol.c
@@ -56,9 +56,9 @@ static void vol_s24_to_s24_s32(struct processing_module *mod, struct input_strea
 	struct vol_data *cd = module_get_private_data(mod);
 	struct audio_stream *source = bsource->data;
 	struct audio_stream *sink = bsink->data;
-	ae_f32x2 in_sample = AE_ZERO32();
-	ae_f32x2 out_sample = AE_ZERO32();
-	ae_f32x2 volume = AE_ZERO32();
+	ae_f32x2 in_sample;
+	ae_f32x2 out_sample;
+	ae_f32x2 volume;
 	int i, n, m;
 	ae_f32x2 *vol;
 	ae_valign inu;
@@ -152,7 +152,7 @@ static void vol_passthrough_s24_to_s24_s32(struct processing_module *mod,
 	struct vol_data *cd = module_get_private_data(mod);
 	struct audio_stream *source = bsource->data;
 	struct audio_stream *sink = bsink->data;
-	ae_f32x2 in_sample = AE_ZERO32();
+	ae_f32x2 in_sample;
 
 	int i, n, m;
 	ae_valign inu;
@@ -219,9 +219,9 @@ static void vol_s32_to_s24_s32(struct processing_module *mod, struct input_strea
 	struct vol_data *cd = module_get_private_data(mod);
 	struct audio_stream *source = bsource->data;
 	struct audio_stream *sink = bsink->data;
-	ae_f32x2 in_sample = AE_ZERO32();
-	ae_f32x2 out_sample = AE_ZERO32();
-	ae_f32x2 volume = AE_ZERO32();
+	ae_f32x2 in_sample;
+	ae_f32x2 out_sample;
+	ae_f32x2 volume;
 	int i, n, m;
 	ae_f64 mult0;
 	ae_f64 mult1;
@@ -323,7 +323,7 @@ static void vol_passthrough_s32_to_s24_s32(struct processing_module *mod,
 	struct vol_data *cd = module_get_private_data(mod);
 	struct audio_stream *source = bsource->data;
 	struct audio_stream *sink = bsink->data;
-	ae_f32x2 in_sample = AE_ZERO32();
+	ae_f32x2 in_sample;
 	int i, n, m;
 	ae_valign inu;
 	ae_valign outu = AE_ZALIGN64();
@@ -387,12 +387,12 @@ static void vol_s16_to_s16(struct processing_module *mod, struct input_stream_bu
 	struct vol_data *cd = module_get_private_data(mod);
 	struct audio_stream *source = bsource->data;
 	struct audio_stream *sink = bsink->data;
-	ae_f32x2 volume0 = AE_ZERO32();
-	ae_f32x2 volume1 = AE_ZERO32();
-	ae_f32x2 out_sample0 = AE_ZERO32();
-	ae_f32x2 out_sample1 = AE_ZERO32();
-	ae_f16x4 in_sample = AE_ZERO16();
-	ae_f16x4 out_sample = AE_ZERO16();
+	ae_f32x2 volume0;
+	ae_f32x2 volume1;
+	ae_f32x2 out_sample0;
+	ae_f32x2 out_sample1;
+	ae_f16x4 in_sample;
+	ae_f16x4 out_sample;
 	int i, n, m, left;
 	ae_f32x2 *buf;
 	ae_f32x2 *buf_end;
@@ -529,7 +529,7 @@ static void vol_passthrough_s16_to_s16(struct processing_module *mod,
 	struct vol_data *cd = module_get_private_data(mod);
 	struct audio_stream *source = bsource->data;
 	struct audio_stream *sink = bsink->data;
-	ae_f16x4 in_sample = AE_ZERO16();
+	ae_f16x4 in_sample;
 	int i, n, m, left;
 	ae_valign inu;
 	ae_valign outu = AE_ZALIGN64();
