e6f4e59075de739cccf248d5092d0e1ba29d63d5
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-03-17 19:55:35 +0200

    Tools: Testbench: Add support for ALSA controls script
    This patch adds to testbench command line option -s <controls file>.
    The controls file can contain comments with line starting with #,
    empty lines, amixer cset name= commands and sleep n commands.
    
    The script format is similar to what can be applied to a real
    SOF device running Linux OS. E.g.
    
    amixer -c0 cset name='Post Mixer Analog Playback DRC switch' off
    amixer -c0 cset name='Post Mixer Analog Playback Volume' 42,43
    sleep 0.5
    amixer -c0 cset name='Post Mixer Analog Playback Volume' 45
    
    The result simulated waveform would show in the beginning left
    channel set to gain value 42, and right channel to gain value 43.
    After processing 0.5 seconds of audio both left and right
    channel gains are set to 45.
    
    The supported controls types are mixer, switch and enum.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/testbench/utils.c
+++ tools/testbench/utils.c
@@ -8,6 +8,7 @@
 #include <sof/ipc/topology.h>
 #include <sof/lib/notifier.h>
 
+#include <ctype.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -255,4 +256,180 @@ void tb_getcycles(uint64_t *cycles)
 #else
 	*cycles = 0;
 #endif
+}
+
+static void tb_trim_line(char *new, char *line)
+{
+	int i = 0;
+	int j = 0;
+	int n = strlen(line);
+	bool in_quotes = false;
+
+	/* Trim begin */
+	while (isspace((int)line[i]) && i < n)
+		i++;
+
+	/* Copy line with multiple blanks removed, change single quotes to
+	 * double quotes, don't remove blanks from inside quotes.
+	 */
+	while (i < n) {
+		if (!isspace((int)line[i])) {
+			if (line[i] == '\'')
+				new[j] = '\"';
+			else
+				new[j] = line[i];
+			if (new[j] == '\"')
+				in_quotes = !in_quotes;
+			j++;
+			i++;
+		} else if (in_quotes || ((i + 1) < n && !isspace((int)line[i + 1]))) {
+			new[j] = ' ';
+			j++;
+			i++;
+		} else {
+			i++;
+		}
+	}
+	new[j] = 0;
+}
+
+static int tb_parse_sleep(char *line, int64_t *sleep_ns)
+{
+	char *token = strtok(line, "sleep ");
+
+	*sleep_ns = (int64_t)(atof(token) * 1e9);
+	printf("Info: Next control will be applied after %lld ns.\n", (long long)*sleep_ns);
+	return 0;
+}
+
+int tb_decode_enum(struct snd_soc_tplg_enum_control *enum_ctl, char *token)
+{
+	int enum_items = enum_ctl->items;
+	int i;
+
+	for (i = 0; i < enum_items; i++) {
+		if (strcmp(enum_ctl->texts[i], token) == 0)
+			return i;
+	}
+
+	return -EINVAL;
+}
+
+static struct tb_ctl *tb_find_control_by_name(struct testbench_prm *tp, char *name)
+{
+	struct tb_glb_state *glb = &tp->glb_ctx;
+	struct tb_ctl *ctl;
+	int i;
+
+	for (i = 0; i <  glb->num_ctls; i++) {
+		ctl = &glb->ctl[i];
+		if (strcmp(ctl->name, name) == 0)
+			return ctl;
+	}
+
+	return NULL;
+}
+
+static int tb_parse_amixer(struct testbench_prm *tp, char *line)
+{
+	char *line_end;
+	char *name_str;
+	char *end_str;
+	char control_name[TB_MAX_CTL_NAME_CHARS] = {0};
+	char control_params[TB_MAX_CTL_NAME_CHARS] = {0};
+	char *find_cset_name_str = "cset name=\"";
+	char *find_end_str = "\" ";
+	int find_len = strlen(find_cset_name_str);
+	int find_end_len = strlen(find_end_str);
+	int len;
+	struct tb_ctl *ctl;
+	int ret;
+
+	name_str = strstr(line, find_cset_name_str);
+	if (!name_str) {
+		fprintf(stderr, "error: no control name in script line: %s\n", line);
+		return -EINVAL;
+	}
+
+	end_str = strstr(&name_str[find_len], find_end_str);
+	if (!end_str) {
+		fprintf(stderr, "error: no control name end quote in script line: %s\n", line);
+		return -EINVAL;
+	}
+
+	len = end_str - name_str - find_len;
+	memcpy(control_name, name_str + find_len, len);
+
+	line_end = line + strlen(line);
+	len = line_end - end_str - find_end_len;
+	memcpy(control_params, &end_str[find_end_len], len);
+
+	printf("Info: Setting control name '%s' to value (%s)\n", control_name, control_params);
+
+	ctl = tb_find_control_by_name(tp, control_name);
+	if (!ctl) {
+		fprintf(stderr, "error: control %s not found in topology.\n", control_name);
+		return -EINVAL;
+	}
+
+	switch (ctl->type) {
+	case SND_SOC_TPLG_TYPE_MIXER:
+		ret = tb_set_mixer_control(tp, ctl, control_params);
+		break;
+	case SND_SOC_TPLG_TYPE_ENUM:
+		ret = tb_set_enum_control(tp, ctl, control_params);
+		break;
+	default:
+		fprintf(stderr, "error: control %s type %d is not supported.\n",
+			control_name, ctl->type);
+		ret = -EINVAL;
+	}
+
+	return ret;
+}
+
+int tb_read_controls(struct testbench_prm *tp, int64_t *sleep_ns)
+{
+	char *sleep_cmd = "sleep ";
+	char *amixer_cmd = "amixer ";
+	char *raw_line;
+	char *line;
+	int ret = 0;
+
+	*sleep_ns = 0;
+	if (!tp->control_fh)
+		return 0;
+
+	raw_line = malloc(TB_MAX_CMD_CHARS);
+	assert(raw_line);
+
+	line = malloc(TB_MAX_CMD_CHARS);
+	assert(line);
+
+	while (fgets(raw_line, TB_MAX_CMD_CHARS, tp->control_fh)) {
+		tb_trim_line(line, raw_line);
+		if (line[0] == '#' || strlen(line) == 0)
+			continue;
+
+		if (strncmp(line, sleep_cmd, sizeof(*sleep_cmd)) == 0) {
+			ret = tb_parse_sleep(line, sleep_ns);
+			if (ret) {
+				fprintf(stderr, "error: failed parse of sleep command.\n");
+				break;
+			}
+			break;
+		}
+
+		if (strncmp(line, amixer_cmd, sizeof(*amixer_cmd)) == 0) {
+			ret = tb_parse_amixer(tp, line);
+			if (ret) {
+				fprintf(stderr, "error: failed parse of amixer command.\n");
+				break;
+			}
+		}
+	}
+
+	free(line);
+	free(raw_line);
+	return ret;
 }
