3a2868b99467987544743bac803af3b549fe51c9
Adrian Warecki <adrian.warecki@intel.com>
2025-03-18 15:32:12 +0100

    ipc4: notification: Separate resource_notif_header_init function
    Separate common resource event notification initialization code to a new
    resource_notif_header_init function.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/ipc/ipc4/notification.c
+++ src/ipc/ipc4/notification.c
@@ -3,27 +3,35 @@
  * Copyright(c) 2023 Intel Corporation. All rights reserved.
  *
  * Author: Piotr Makaruk <piotr.makaruk@intel.com>
+ *	   Adrian Warecki <adrian.warecki@intel.com>
  */
 
 #include <sof/common.h>
+#include <sof/ipc/msg.h>
 #include <stdbool.h>
 #include <ipc4/notification.h>
 
 #if CONFIG_XRUN_NOTIFICATIONS_ENABLE
-void xrun_notif_msg_init(struct ipc_msg *msg_xrun, uint32_t resource_id, uint32_t event_type)
+static void resource_notif_header_init(struct ipc_msg *msg)
 {
-	struct ipc4_resource_event_data_notification *notif_data = msg_xrun->tx_data;
+	struct ipc4_resource_event_data_notification *notif_data = msg->tx_data;
 	union ipc4_notification_header header;
 
 	header.r.notif_type = SOF_IPC4_NOTIFY_RESOURCE_EVENT;
 	header.r.type = SOF_IPC4_GLB_NOTIFICATION;
 	header.r.rsp = SOF_IPC4_MESSAGE_DIR_MSG_REQUEST;
 	header.r.msg_tgt = SOF_IPC4_MESSAGE_TARGET_FW_GEN_MSG;
-	msg_xrun->header = header.dat;
+	msg->header = header.dat;
+	memset(&notif_data->event_data, 0, sizeof(notif_data->event_data));
+}
 
+void xrun_notif_msg_init(struct ipc_msg *msg_xrun, uint32_t resource_id, uint32_t event_type)
+{
+	struct ipc4_resource_event_data_notification *notif_data = msg_xrun->tx_data;
+
+	resource_notif_header_init(msg_xrun);
 	notif_data->resource_id = resource_id;
 	notif_data->event_type = event_type;
 	notif_data->resource_type = SOF_IPC4_GATEWAY;
-	memset(&notif_data->event_data, 0, sizeof(notif_data->event_data));
 }
 #endif
