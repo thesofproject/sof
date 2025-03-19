35e3e4f193022fda483d816f100c37c885f0e6e5
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2025-03-19 17:14:01 +0200

    tools: testbench: fix load of component src_lite
    The register to testbench needs the init function prototype
    into sof/audio/component.h. Add src_lite to the prototype list,
    so it can be enabled in testbench builds.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- src/include/sof/audio/component.h
+++ src/include/sof/audio/component.h
@@ -869,6 +869,7 @@ void sys_comp_module_asrc_interface_init(void);
 void sys_comp_module_rtnr_interface_init(void);
 void sys_comp_module_selector_interface_init(void);
 void sys_comp_module_src_interface_init(void);
+void sys_comp_module_src_lite_interface_init(void);
 void sys_comp_module_tdfb_interface_init(void);
 void sys_comp_module_volume_interface_init(void);
 void sys_comp_module_tester_interface_init(void);
