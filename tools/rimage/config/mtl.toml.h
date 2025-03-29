c3cfacb969a88d0e4fdbf3cdea73e8abe9f526d2
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2025-03-29 19:50:47 +0000

    modules: tensorflow: Add initial tensorflow micro module
    This is WIP and adds a TFLM module based on micro_speech example to
    do audio classification.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- tools/rimage/config/mtl.toml.h
+++ tools/rimage/config/mtl.toml.h
@@ -154,5 +154,9 @@
 #include <audio/mfcc/mfcc.toml>
 #endif
 
+#if defined(CONFIG_COMP_TENSORFLOW) || defined(LLEXT_FORCE_ALL_MODULAR)
+#include <audio/tensorflow/tflmcly.toml>
+#endif
+
 [module]
 count = __COUNTER__
