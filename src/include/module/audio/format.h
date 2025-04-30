16ff082c0f5c78cfef79b285fd545a753993fb9e
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-04-30 18:26:16 +0300

    Audio: PCM converter: Add support for A-law sample type
    This patch adds to the PCM converter functions
    pcm_convert_alaw_to_s32() and pcm_convert_s32_to_alaw().
    
    The function audio_stream_fmt_conversion() is also updated
    to check the sample type and depth to set up correctly
    conversion for A-law.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/include/module/audio/format.h
+++ src/include/module/audio/format.h
@@ -22,6 +22,7 @@ static inline uint32_t get_sample_bytes(enum sof_ipc_frame fmt)
 	case SOF_IPC_FRAME_S24_3LE:
 		return 3;
 	case SOF_IPC_FRAME_U8:
+	case SOF_IPC_FRAME_A_LAW:
 		return 1;
 	default:
 		return 4;
@@ -37,6 +38,7 @@ static inline uint32_t get_sample_bitdepth(enum sof_ipc_frame fmt)
 	case SOF_IPC_FRAME_S24_3LE:
 		return 24;
 	case SOF_IPC_FRAME_U8:
+	case SOF_IPC_FRAME_A_LAW:
 		return 8;
 	default:
 		return 32;
