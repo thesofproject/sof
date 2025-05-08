4d1e65600533a2e5d3671bb01ae55f114dae4ffd
Daniel Baluta <daniel.baluta@nxp.com>
2025-05-08 10:35:22 +0300

    comp: Add initial NXP Essential Audio Processing component
    NXP’s Essential Audio Processing (EAP) library is a bundle of audio
    processing blocks for enhancing the tonal and spatial perception
    of sound in audio applications
    
    The initial support adds some presets parameters that can be set
    using the standard alsamixer interface.
        * AllEffectOff
        * VoiceEnhancer
        * MusicEnhancer
        * AutoVolumeLeveler
        * ConcertSound
        * LoudnessMaximiser
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

---
+++ src/include/sof/audio/nxp/eap/EAP_Parameter_presets.h
@@ -0,0 +1,13 @@
+#ifndef EAP_PARAMETER_PRESETS_H_
+#define EAP_PARAMETER_PRESETS_H_
+
+#include <nxp/eap/EAP_Includes/EAP16.h>
+
+#include <Example_Application/CONFIG_HEADER/EAP_Parameter_AllEffectOff.h>
+#include <Example_Application/CONFIG_HEADER/EAP_Parameter_AutoVolumeLeveler.h>
+#include <Example_Application/CONFIG_HEADER/EAP_Parameter_ConcertSound.h>
+#include <Example_Application/CONFIG_HEADER/EAP_Parameter_LoudnessMaximiser.h>
+#include <Example_Application/CONFIG_HEADER/EAP_Parameter_MusicEnhancerRMSLimiter.h>
+#include <Example_Application/CONFIG_HEADER/EAP_Parameter_VoiceEnhancer.h>
+
+#endif /* EAP_PARAMETER_PRESETS_H_ */
