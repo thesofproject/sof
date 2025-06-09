28ea88be40195570b8f38504098a3efa31034ba7
Bard Liao <yung-chuan.liao@linux.intel.com>
2025-06-09 21:13:29 +0800

    Topology2: add fragment sdca amp feedback topologies
    Add fragment sdca amp feedback topologies.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- tools/topology/topology2/production/tplg-targets-sdca-generic.cmake
+++ tools/topology/topology2/production/tplg-targets-sdca-generic.cmake
@@ -10,8 +10,16 @@ SDW_JACK_IN_STREAM=Capture-SimpleJack,NUM_HDMIS=0"
 "cavs-sdw\;sof-sdca-1amp-id2\;NUM_SDW_AMP_LINKS=1,SDW_JACK=false,\
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,NUM_HDMIS=0"
 
+"cavs-sdw\;sof-sdca-1amp-feedback-id3\;NUM_SDW_AMP_LINKS=1,SDW_JACK=false,\
+SDW_SPK_STREAM=Playback-SmartAmp,SDW_SPK_IN_STREAM=Capture-SmartAmp,\
+SDW_AMP_FEEDBACK=true,NUM_HDMIS=0"
+
 "cavs-sdw\;sof-sdca-2amp-id2\;NUM_SDW_AMP_LINKS=2,SDW_JACK=false,\
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,NUM_HDMIS=0"
+
+"cavs-sdw\;sof-sdca-2amp-feedback-id3\;NUM_SDW_AMP_LINKS=2,SDW_JACK=false,\
+SDW_SPK_STREAM=Playback-SmartAmp,SDW_SPK_IN_STREAM=Capture-SmartAmp,\
+SDW_AMP_FEEDBACK=true,NUM_HDMIS=0"
 
 "cavs-sdw\;sof-sdca-mic-id4\;SDW_JACK=false,SDW_DMIC=1,NUM_HDMIS=0,\
 SDW_DMIC_STREAM=Capture-SmartMic"
