6280a703635cb887157ea2809f39d70b84bf6940
Adrian Warecki <adrian.warecki@intel.com>
2025-07-07 20:36:54 +0200

    ptl: mmu: Introduce driver heap for non-privileged modules
    Non-privileged modules should be isolated each other. It means that
    module related data should be located in memory available for kernel
    and the owner module only. To manage access all such data should be
    allocated from single, separate region.
    
    This patch introduces private module system heap. It is allocated from
    regular heap, assigned to module thread and then used to allocate all
    infrastructure data associated with the module.
    Other non-privileged modules will not have access to this region.
    Currently it is arbitrary defined to use 5 pages, what could change.
    
    Signed-off-by: Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/audio/module_adapter/module_adapter.c
+++ src/audio/module_adapter/module_adapter.c
@@ -90,7 +90,7 @@ struct comp_dev *module_adapter_new_ext(const struct comp_driver *drv,
 	int flags = config->proc_domain == COMP_PROCESSING_DOMAIN_DP ?
 			     SOF_MEM_FLAG_USER | SOF_MEM_FLAG_COHERENT : SOF_MEM_FLAG_USER;
 
-	mod = rzalloc(flags, sizeof(*mod));
+	mod = drv_heap_rzalloc(drv->drv_heap, flags, sizeof(*mod));
 	if (!mod) {
 		comp_err(dev, "module_adapter_new(), failed to allocate memory for module");
 		goto err;
