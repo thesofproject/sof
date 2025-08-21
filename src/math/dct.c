54fe0ecfe5f3785e9d3f590f76989f50dd9b389d
Jyri Sarha <jyri.sarha@linux.intel.com>
2025-08-21 22:32:55 +0300

    Math: Make mod_alloc() versions of all heap using functions
    Make mod_alloc() versions of all heap using functions. The new functions
    are simple copy-paste versions of the original, just using mod_alloc()
    and friend instead of rmalloc() friends.
    
    The idea is to get rid off the original versions once all the modules
    are converted to module API heap usage and the cmocka math tests have
    been fixed to work with mod_alloc() versions.
    
    Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

--- src/math/dct.c
+++ src/math/dct.c
@@ -4,6 +4,7 @@
 //
 // Author: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
 
+#include <sof/audio/module_adapter/module/generic.h>
 #include <sof/audio/format.h>
 #include <sof/math/matrix.h>
 #include <sof/math/dct.h>
@@ -52,6 +53,53 @@ int dct_initialize_16(struct dct_plan_16 *dct)
 		return -EINVAL;
 
 	dct->matrix = mat_matrix_alloc_16b(dct->num_in, dct->num_out, 15);
+	if (!dct->matrix)
+		return -ENOMEM;
+
+	c1 = PI_Q29 / dct->num_in;
+	arg = Q_SHIFT_RND(TWO_Q29 / dct->num_in, 29, 12);
+	c2 = sqrt_int16(arg); /* Q4.12 */
+	for (n = 0; n < dct->num_in; n++) {
+		for (k = 0; k < dct->num_out; k++) {
+			/* Note: Current int16_t nk works up to DCT_MATRIX_SIZE_MAX = 91 */
+			nk = (Q_SHIFT_LEFT(n, 0, 1) + HALF_Q1) * Q_SHIFT_LEFT(k, 0, 1); /*Q14.2 */
+			arg = Q_MULTSR_32X32((int64_t)c1, nk, 29, 2, 24); /* Q8.24 */
+			/* Note: Q8.24 works up to DCT_MATRIX_SIZE_MAX = 42 */
+			arg %= TWO_PI_Q24;
+			cos = cos_fixed_32b(Q_SHIFT_LEFT(arg, 24, 28)); /* Q1.31 */
+			dct_val = Q_MULTSR_32X32((int64_t)cos, c2, 31, 12, 15); /* Q1.15 */
+			if (k == 0)
+				dct_val = Q_MULTSR_32X32((int64_t)dct_val,
+							 ONE_OVER_SQRT_TWO, 15, 31, 15);
+
+			mat_set_scalar_16b(dct->matrix, n, k, dct_val);
+		}
+	}
+
+	return 0;
+}
+
+int mod_dct_initialize_16(struct processing_module *mod, struct dct_plan_16 *dct)
+{
+	int16_t dct_val;
+	int32_t arg;
+	int32_t cos;
+	int32_t c1;
+	int16_t c2;
+	int16_t nk;
+	int n;
+	int k;
+
+	if (dct->type != DCT_II || dct->ortho != true)
+		return -EINVAL;
+
+	if (dct->num_in < 1 || dct->num_out < 1)
+		return -EINVAL;
+
+	if (dct->num_in > DCT_MATRIX_SIZE_MAX || dct->num_out > DCT_MATRIX_SIZE_MAX)
+		return -EINVAL;
+
+	dct->matrix = mod_mat_matrix_alloc_16b(mod, dct->num_in, dct->num_out, 15);
 	if (!dct->matrix)
 		return -ENOMEM;
 
--- src/math/fft/fft_common.c
+++ src/math/fft/fft_common.c
@@ -5,6 +5,7 @@
 // Author: Amery Song <chao.song@intel.com>
 //	   Keyon Jie <yang.jie@linux.intel.com>
 
+#include <sof/audio/module_adapter/module/generic.h>
 #include <sof/audio/buffer.h>
 #include <sof/audio/format.h>
 #include <sof/common.h>
@@ -63,6 +64,55 @@ struct fft_plan *fft_plan_new(void *inb, void *outb, uint32_t size, int bits)
 	return plan;
 }
 
+struct fft_plan *mod_fft_plan_new(struct processing_module *mod, void *inb,
+				  void *outb, uint32_t size, int bits)
+{
+	struct fft_plan *plan;
+	int lim = 1;
+	int len = 0;
+	int i;
+
+	if (!inb || !outb)
+		return NULL;
+
+	plan = mod_zalloc(mod, sizeof(struct fft_plan));
+	if (!plan)
+		return NULL;
+
+	switch (bits) {
+	case 16:
+		plan->inb16 = inb;
+		plan->outb16 = outb;
+		break;
+	case 32:
+		plan->inb32 = inb;
+		plan->outb32 = outb;
+		break;
+	default:
+		return NULL;
+	}
+
+	/* calculate the exponent of 2 */
+	while (lim < size) {
+		lim <<= 1;
+		len++;
+	}
+
+	plan->size = lim;
+	plan->len = len;
+
+	plan->bit_reverse_idx = mod_zalloc(mod,	plan->size * sizeof(uint16_t));
+	if (!plan->bit_reverse_idx)
+		return NULL;
+
+	/* set up the bit reverse index */
+	for (i = 1; i < plan->size; ++i)
+		plan->bit_reverse_idx[i] = (plan->bit_reverse_idx[i >> 1] >> 1) |
+					   ((i & 1) << (len - 1));
+
+	return plan;
+}
+
 void fft_plan_free(struct fft_plan *plan)
 {
 	if (!plan)
@@ -70,4 +120,13 @@ void fft_plan_free(struct fft_plan *plan)
 
 	rfree(plan->bit_reverse_idx);
 	rfree(plan);
+}
+
+void mod_fft_plan_free(struct processing_module *mod, struct fft_plan *plan)
+{
+	if (!plan)
+		return;
+
+	mod_free(mod, plan->bit_reverse_idx);
+	mod_free(mod, plan);
 }
