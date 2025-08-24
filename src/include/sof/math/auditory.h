028a1e139407b5a27894bef8c22ede82300a0b22
Jyri Sarha <jyri.sarha@linux.intel.com>
2025-08-24 21:10:07 +0300

    Math: Remove directly heap using math functions
    Both unit tests and modules should now use the module API heap
    allocation functions, so we can now remove the directly heap using
    versions.
    
    Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

--- src/include/sof/math/auditory.h
+++ src/include/sof/math/auditory.h
@@ -76,7 +76,6 @@ int16_t psy_mel_to_hz(int16_t mel);
  *			   filter coefficients.
  * \return                 Zero when success, otherwise error code.
  */
-int psy_get_mel_filterbank(struct psy_mel_filterbank *mel_fb);
 struct processing_module;
 int mod_psy_get_mel_filterbank(struct processing_module *mod, struct psy_mel_filterbank *mel_fb);
 
