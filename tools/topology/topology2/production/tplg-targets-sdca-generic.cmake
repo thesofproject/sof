b0ab82b3361f18125967617ffe1e47e157eb6c51
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-08-27 11:22:37 +0300

    Tools: Topology: Build topologies with 2-way speaker crossover
    This patch modifies cavs-sdw.conf to use sdw-amp-xover.conf
    when crossover feature is enabled with SDW_AMP_XOVER set
    to true.
    
    The built SDCA function topologies are
    sof-sdca-2amp-id2-xover.tplg and
    sof-sdca-2amp-feedback-id3-xover.tplg.
    
    The currently built topology duplicates a stereo L,R stream
    into L,R,L,R stream and applies 4th order Linkwitz-Riley
    low-pass for the first pair and high-pass for the second pair.
    
    The crossover frequency is set to 2 kHz as example. The
    crossover parameters should be changed to match the acoustical
    design of speaker acoustics of the device. Also there could be
    different channels mappings and high/low-pass apply need.
    
    Other variants can be created with the Octave scripts in the
    next patch.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/topology/topology2/production/tplg-targets-sdca-generic.cmake
+++ tools/topology/topology2/production/tplg-targets-sdca-generic.cmake
@@ -25,4 +25,18 @@ SDW_DMIC_STREAM=Capture-SmartMic"
 "cavs-sdw\;sof-hdmi-pcm5-id6\;SDW_JACK=false,HDMI1_ID=6,HDMI2_ID=7,HDMI3_ID=8"
 "cavs-sdw\;sof-hdmi-pcm5-id7\;SDW_JACK=false,HDMI1_ID=7,HDMI2_ID=8,HDMI3_ID=9"
 
+# Topology for speaker with 2-way crossover filter in SOF
+# with channels order L-low, R-low, L-high, R-high
+"cavs-sdw\;sof-sdca-2amp-id2-xover\;NUM_SDW_AMP_LINKS=2,SDW_JACK=false,\
+SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,NUM_HDMIS=0,\
+SDW_AMP_NUM_CHANNELS=4,SDW_AMP_XOVER=true,\
+SDW_AMP_XOVER_SELECTOR_PARAMS=xover_selector_lr_to_lrlr,\
+SDW_AMP_XOVER_EQIIR_PARAMS=xover_lr4_2000hz_llhh_48khz"
+
+"cavs-sdw\;sof-sdca-2amp-feedback-id3-xover\;NUM_SDW_AMP_LINKS=2,\
+SDW_JACK=false,SDW_AMP_FEEDBACK=true,SDW_SPK_STREAM=Playback-SmartAmp,\
+SDW_SPK_IN_STREAM=Capture-SmartAmp,NUM_HDMIS=0,\
+SDW_AMP_NUM_CHANNELS=4,SDW_AMP_XOVER=true,\
+SDW_AMP_XOVER_SELECTOR_PARAMS=xover_selector_lr_to_lrlr,\
+SDW_AMP_XOVER_EQIIR_PARAMS=xover_lr4_2000hz_llhh_48khz"
 )
