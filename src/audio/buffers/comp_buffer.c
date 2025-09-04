2b71b0cc3b0338ce6247700fd546b602cf5e8d53
Adrian Warecki <adrian.warecki@intel.com>
2025-09-04 19:20:39 +0200

    userspace: Add userspace_context and common_partition memory domain
    Introduce the userspace_context structure to store data required by modules
    operating in userspace.
    
    Add the common_partition memory domain to share SOF components that need
    to be accessible from userspace.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/audio/buffers/comp_buffer.c
+++ src/audio/buffers/comp_buffer.c
@@ -11,6 +11,7 @@
 #include <sof/audio/sink_api.h>
 #include <sof/audio/source_api.h>
 #include <sof/audio/sink_source_utils.h>
+#include <rtos/userspace_helper.h>
 #include <sof/common.h>
 #include <rtos/interrupt.h>
 #include <rtos/alloc.h>
@@ -161,7 +162,7 @@ static void comp_buffer_free(struct sof_audio_buffer *audio_buffer)
 	rfree(buffer);
 }
 
-static const struct source_ops comp_buffer_source_ops = {
+APP_TASK_DATA static const struct source_ops comp_buffer_source_ops = {
 	.get_data_available = comp_buffer_get_data_available,
 	.get_data = comp_buffer_get_data,
 	.release_data = comp_buffer_release_data,
@@ -170,7 +171,7 @@ static const struct source_ops comp_buffer_source_ops = {
 	.set_alignment_constants = audio_buffer_source_set_alignment_constants,
 };
 
-static const struct sink_ops comp_buffer_sink_ops = {
+APP_TASK_DATA static const struct sink_ops comp_buffer_sink_ops = {
 	.get_free_size = comp_buffer_get_free_size,
 	.get_buffer = comp_buffer_get_buffer,
 	.commit_buffer = comp_buffer_commit_buffer,
