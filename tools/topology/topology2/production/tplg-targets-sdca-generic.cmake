c6916c65384df795e28718f365850badc826bb1e
Bard Liao <yung-chuan.liao@linux.intel.com>
2025-09-11 17:46:57 +0800

    Topology2: add missing function topologies
    Add different DMIC BE ID function topologies for ACE3 platforms.
    Also, the HDMI BE id could be 2 or 3 if there is no PCH DMIC is used.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- tools/topology/topology2/production/tplg-targets-sdca-generic.cmake
+++ tools/topology/topology2/production/tplg-targets-sdca-generic.cmake
@@ -20,6 +20,8 @@ SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,NUM_HDMIS=0"
 "cavs-sdw\;sof-sdca-mic-id4\;SDW_JACK=false,SDW_DMIC=1,NUM_HDMIS=0,\
 SDW_DMIC_STREAM=Capture-SmartMic"
 
+"cavs-sdw\;sof-hdmi-pcm5-id2\;SDW_JACK=false,HDMI1_ID=2,HDMI2_ID=3,HDMI3_ID=4"
+"cavs-sdw\;sof-hdmi-pcm5-id3\;SDW_JACK=false,HDMI1_ID=3,HDMI2_ID=4,HDMI3_ID=5"
 "cavs-sdw\;sof-hdmi-pcm5-id4\;SDW_JACK=false,HDMI1_ID=4,HDMI2_ID=5,HDMI3_ID=6"
 "cavs-sdw\;sof-hdmi-pcm5-id5\;SDW_JACK=false"
 "cavs-sdw\;sof-hdmi-pcm5-id6\;SDW_JACK=false,HDMI1_ID=6,HDMI2_ID=7,HDMI3_ID=8"
