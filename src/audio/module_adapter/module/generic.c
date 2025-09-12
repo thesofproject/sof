389e7e15b3c5d0ff9c9cec8d142f1295a9d486fa
Jyri Sarha <jyri.sarha@linux.intel.com>
2025-09-12 16:10:13 +0300

    modules: mod_alloc: mod_alloc size and alignment parameter to size_t
    Change mod_alloc_align and friends size and alignment parameters type
    to size_t.
    
    Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

--- src/audio/module_adapter/module/generic.c
+++ src/audio/module_adapter/module/generic.c
@@ -168,7 +168,7 @@ static void container_put(struct processing_module *mod, struct module_resource 
  *
  * The allocated memory is automatically freed when the module is unloaded.
  */
-void *mod_alloc_align(struct processing_module *mod, uint32_t size, uint32_t alignment)
+void *mod_alloc_align(struct processing_module *mod, size_t size, size_t alignment)
 {
 	struct module_resource *container = container_get(mod);
 	struct module_resources *res = &mod->priv.resources;
@@ -191,7 +191,7 @@ void *mod_alloc_align(struct processing_module *mod, uint32_t size, uint32_t ali
 	ptr = rmalloc_align(SOF_MEM_FLAG_USER, size, alignment);
 #endif
 	if (!ptr) {
-		comp_err(mod->dev, "Failed to alloc %d bytes %d alignment for comp %x.",
+		comp_err(mod->dev, "Failed to alloc %zu bytes %zu alignment for comp %#x.",
 			 size, alignment, dev_comp_id(mod->dev));
 		container_put(mod, container);
 		return NULL;
@@ -219,7 +219,7 @@ EXPORT_SYMBOL(mod_alloc_align);
  * Like mod_alloc_align() but the alignment can not be specified. However,
  * rballoc() will always aligns the memory to PLATFORM_DCACHE_ALIGN.
  */
-void *mod_alloc(struct processing_module *mod, uint32_t size)
+void *mod_alloc(struct processing_module *mod, size_t size)
 {
 	return mod_alloc_align(mod, size, 0);
 }
@@ -233,7 +233,7 @@ EXPORT_SYMBOL(mod_alloc);
  *
  * Like mod_alloc() but the allocated memory is initialized to zero.
  */
-void *mod_zalloc(struct processing_module *mod, uint32_t size)
+void *mod_zalloc(struct processing_module *mod, size_t size)
 {
 	void *ret = mod_alloc(mod, size);
 
