cc9eb68d1d40c6a0721d839dcbc140b23752016d
Adrian Bonislawski <adrian.bonislawski@intel.com>
2025-10-02 11:46:59 +0200

    rimage: config: add FORCE_ALL_MODULES macro to nvl.toml.h
    When this header is then
    preprocessed with that macro defined, all module TOML files will be
    included.
    
    Signed-off-by: Adrian Bonislawski <adrian.bonislawski@intel.com>

--- tools/rimage/config/nvl.toml.h
+++ tools/rimage/config/nvl.toml.h
@@ -24,35 +24,35 @@ auto_start = "0"
 
 index = __COUNTER__
 
-#if defined(CONFIG_COMP_TESTER)
+#if defined(CONFIG_COMP_TESTER) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <debug/tester/tester.toml>
 #endif
 
-#ifdef CONFIG_COMP_MIXIN_MIXOUT
+#if defined(CONFIG_COMP_MIXIN_MIXOUT) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/mixin_mixout/mixin_mixout.toml>
 #endif
 
-#ifdef CONFIG_COMP_COPIER
+#if defined(CONFIG_COMP_COPIER) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/copier/copier.toml>
 #endif
 
-#ifdef CONFIG_COMP_VOLUME
+#if defined(CONFIG_COMP_VOLUME) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/volume/volume.toml>
 #endif
 
-#ifdef CONFIG_COMP_ASRC
+#if defined(CONFIG_COMP_ASRC) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/asrc/asrc.toml>
 #endif
 
-#ifdef CONFIG_COMP_SRC
+#if defined(CONFIG_COMP_SRC) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/src/src.toml>
 #endif
 
-#ifdef CONFIG_COMP_SEL
+#if defined(CONFIG_COMP_SEL) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/selector/selector.toml>
 #endif
 
-#ifdef CONFIG_COMP_UP_DOWN_MIXER
+#if defined(CONFIG_COMP_UP_DOWN_MIXER) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/up_down_mixer/up_down_mixer.toml>
 #endif
 
@@ -60,7 +60,7 @@ index = __COUNTER__
 #include <probe/probe.toml>
 #endif
 
-#ifdef CONFIG_COMP_MUX
+#if defined(CONFIG_COMP_MUX) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/mux/mux.toml>
 #endif
 
@@ -68,63 +68,63 @@ index = __COUNTER__
 #include <samples/audio/detect_test.toml>
 #endif
 
-#ifdef CONFIG_COMP_KPB
+#if defined(CONFIG_COMP_KPB) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/kpb.toml>
 #endif
 
-#ifdef CONFIG_SAMPLE_SMART_AMP
+#if defined(CONFIG_SAMPLE_SMART_AMP) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <samples/audio/smart_amp_test.toml>
 #endif
 
-#ifdef CONFIG_COMP_IIR
+#if defined(CONFIG_COMP_IIR) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/eq_iir/eq_iir.toml>
 #endif
 
-#ifdef CONFIG_COMP_FIR
+#if defined(CONFIG_COMP_FIR) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/eq_fir/eq_fir.toml>
 #endif
 
-#ifdef CONFIG_COMP_ARIA
+#if defined(CONFIG_COMP_ARIA) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/aria/aria.toml>
 #endif
 
-#ifdef CONFIG_COMP_DRC
+#if defined(CONFIG_COMP_DRC) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/drc/drc.toml>
 #endif
 
-#ifdef CONFIG_COMP_CROSSOVER
+#if defined(CONFIG_COMP_CROSSOVER) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/crossover/crossover.toml>
 #endif
 
-#ifdef CONFIG_COMP_MULTIBAND_DRC
+#if defined(CONFIG_COMP_MULTIBAND_DRC) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/multiband_drc/multiband_drc.toml>
 #endif
 
-#ifdef CONFIG_COMP_DCBLOCK
+#if defined(CONFIG_COMP_DCBLOCK) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/dcblock/dcblock.toml>
 #endif
 
-#ifdef CONFIG_COMP_GOOGLE_RTC_AUDIO_PROCESSING
+#if defined(CONFIG_COMP_GOOGLE_RTC_AUDIO_PROCESSING) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/google/google_rtc_audio_processing.toml>
 #endif
 
-#ifdef CONFIG_COMP_TDFB
+#if defined(CONFIG_COMP_TDFB) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/tdfb/tdfb.toml>
 #endif
 
-#ifdef CONFIG_COMP_RTNR
+#if defined(CONFIG_COMP_RTNR) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/rtnr/rtnr.toml>
 #endif
 
-#ifdef CONFIG_COMP_IGO_NR
+#if defined(CONFIG_COMP_IGO_NR) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/igo_nr/igo_nr.toml>
 #endif
 
-#ifdef CONFIG_COMP_MFCC
+#if defined(CONFIG_COMP_MFCC) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/mfcc/mfcc.toml>
 #endif
 
-#if defined(CONFIG_COMP_SOUND_DOSE)
+#if defined(CONFIG_COMP_SOUND_DOSE) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/sound_dose/sound_dose.toml>
 #endif
 
