521c2a0a78057545a0c4795f19b38ea7338bbca5
Jyri Sarha <jyri.sarha@linux.intel.com>
2025-10-02 20:04:05 +0300

    ipc4: helper: Do not invalidate cache of whole mailbox for module init
    Invalidate cache for only the module init payload size not the whole
    mailbox.
    
    Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

--- src/ipc/ipc4/helper.c
+++ src/ipc/ipc4/helper.c
@@ -139,10 +139,10 @@ __cold struct comp_dev *comp_new_ipc4(struct ipc4_module_init_instance *module_i
 	ipc_config.ipc_config_size = module_init->extension.r.param_block_size * sizeof(uint32_t);
 	ipc_config.ipc_extended_init = module_init->extension.r.extended_init;
 
-	dcache_invalidate_region((__sparse_force void __sparse_cache *)MAILBOX_HOSTBOX_BASE,
-				 MAILBOX_HOSTBOX_SIZE);
-
 	data = ipc4_get_comp_new_data();
+	if (!IS_ENABLED(CONFIG_LIBRARY))
+		sys_cache_data_invd_range((__sparse_force void __sparse_cache *)data,
+					  ipc_config.ipc_config_size);
 
 #if CONFIG_LIBRARY
 	ipc_config.ipc_config_size -= sizeof(struct sof_uuid);
