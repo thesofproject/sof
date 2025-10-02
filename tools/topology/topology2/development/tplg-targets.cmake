7bcdf6f5ac20930ba5a65c1abdf1275a868d7c37
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-10-02 13:59:27 +0300

    Tools: Topology: Add development tplg sof-nvl-nocodec.tplg
    This patch adds the nocodec topology for NVL platform. Since
    the topologies are identical to PTL, the special topologies
    for other rates and channels counts can be copied from the
    PTL builds.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/topology/topology2/development/tplg-targets.cmake
+++ tools/topology/topology2/development/tplg-targets.cmake
@@ -97,6 +97,10 @@ NHLT_BIN=nhlt-sof-ptl-nocodec-fpga-dmic-4ch-96k.bin,PASSTHROUGH=true,DMIC_IO_CLK
 PDM1_MIC_B_ENABLE=1,DMIC0_RATE=96000,SSP0_RATE=96000,PREPROCESS_PLUGINS=nhlt,\
 NHLT_BIN=nhlt-sof-ptl-nocodec-fpga-dmic-4ch-96k-ssp0-96k.bin,PASSTHROUGH=true,DMIC_IO_CLK=19200000"
 
+# SSP topology for NVL
+"cavs-nocodec\;sof-nvl-nocodec\;PLATFORM=nvl,NUM_DMICS=4,PDM1_MIC_A_ENABLE=1,PDM1_MIC_B_ENABLE=1,\
+PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-nvl-nocodec.bin"
+
 # Topology for PTL with max98357a and rt5682
 "cavs-rt5682\;sof-ptl-max98357a-rt5682-ssp2-ssp0\;PLATFORM=ptl,NUM_DMICS=4,PDM1_MIC_A_ENABLE=1,\
 PDM1_MIC_B_ENABLE=1,DMIC0_PCM_ID=99,PREPROCESS_PLUGINS=nhlt,\
