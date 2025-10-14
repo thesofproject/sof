08ba06b1cdc18feb57e15daa9d70520885334224
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-10-14 13:16:09 +0300

    Math: FFT: Remove fft_plan_new() and fft_plan_free()
    The module versions of the functions mod_fft_plan_new()
    and mod_fft_plan_free() should be used instead. Since all
    previous usage of the functions has been updated these are
    safe to remove.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/math/fft/fft_common.c
+++ src/math/fft/fft_common.c
@@ -12,57 +12,6 @@
 #include <rtos/alloc.h>
 #include <sof/math/fft.h>
 
-struct fft_plan *fft_plan_new(void *inb, void *outb, uint32_t size, int bits)
-{
-	struct fft_plan *plan;
-	int lim = 1;
-	int len = 0;
-	int i;
-
-	if (!inb || !outb)
-		return NULL;
-
-	plan = rzalloc(SOF_MEM_FLAG_USER, sizeof(struct fft_plan));
-	if (!plan)
-		return NULL;
-
-	switch (bits) {
-	case 16:
-		plan->inb16 = inb;
-		plan->outb16 = outb;
-		break;
-	case 32:
-		plan->inb32 = inb;
-		plan->outb32 = outb;
-		break;
-	default:
-		rfree(plan);
-		return NULL;
-	}
-
-	/* calculate the exponent of 2 */
-	while (lim < size) {
-		lim <<= 1;
-		len++;
-	}
-
-	plan->size = lim;
-	plan->len = len;
-
-	plan->bit_reverse_idx = rzalloc(SOF_MEM_FLAG_USER,
-					plan->size * sizeof(uint16_t));
-	if (!plan->bit_reverse_idx) {
-		rfree(plan);
-		return NULL;
-	}
-
-	/* set up the bit reverse index */
-	for (i = 1; i < plan->size; ++i)
-		plan->bit_reverse_idx[i] = (plan->bit_reverse_idx[i >> 1] >> 1) |
-					   ((i & 1) << (len - 1));
-
-	return plan;
-}
 
 struct fft_plan *mod_fft_plan_new(struct processing_module *mod, void *inb,
 				  void *outb, uint32_t size, int bits)
@@ -111,15 +60,6 @@ struct fft_plan *mod_fft_plan_new(struct processing_module *mod, void *inb,
 					   ((i & 1) << (len - 1));
 
 	return plan;
-}
-
-void fft_plan_free(struct fft_plan *plan)
-{
-	if (!plan)
-		return;
-
-	rfree(plan->bit_reverse_idx);
-	rfree(plan);
 }
 
 void mod_fft_plan_free(struct processing_module *mod, struct fft_plan *plan)
