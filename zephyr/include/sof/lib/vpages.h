c4d353ebe3fcdb05cb0e04578039b5282a5dee08
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2025-10-23 12:42:00 +0100

    vpages: Add a simple virtual continuous page allocator
    Add a simple virtual page allocator that uses the Zephyr memory mapping
    infrastructure to allocate pages from a virtual region and map them
    to physical pages.
    
    Due to simplicity, the number of allocations is limited to
    CONFIG_SOF_VPAGE_MAX_ALLOCS
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

---
+++ zephyr/include/sof/lib/vpages.h
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright(c) 2025 Intel Corporation.
+
+/* Virtual Page Allocator API */
+#ifndef __SOF_LIB_VPAGE_H__
+#define __SOF_LIB_VPAGE_H__
+
+#include <zephyr/kernel.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Allocate virtual pages
+ * Allocates a specified number of contiguous virtual memory pages by mapping
+ * physical pages.
+ *
+ * @param[in] pages Number of 4kB pages to allocate.
+ *
+ * @return Pointer to the allocated virtual memory region, or NULL on failure.
+ */
+void *alloc_vpages(uint32_t pages);
+
+/**
+ * @brief Free virtual pages
+ * Frees previously allocated virtual memory pages and unmaps them.
+ *
+ * @param[in] ptr Pointer to the memory pages to free.
+ */
+void free_vpages(void *ptr);
+
+/**
+ * @brief Initialize virtual page allocator
+ *
+ * Initializes a virtual page allocator that manages a virtual memory region
+ * using a page table and block structures.
+ *
+ * @retval 0 if successful.
+ * @retval -ENOMEM on creation failure.
+ */
+int init_vpages(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SOF_LIB_VPAGE_H__ */
