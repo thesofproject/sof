b3d0c47ae15c27948f9b8460da744c428b7b9166
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2025-10-23 13:39:49 +0100

    vregion: Add support for per pipeline/module virtual regions
    Add support for per pipeline and per module virtual memory regions.
    
    The intention is to provide a single virtual memory region per pipeline or
    per DP module that can simplify module/pipeline memory management.
    
    The region takes advantage of the way pipeline and modules are constructed,
    destroyed and used during their lifetimes.
    
    1) memory tracking - 1 pointer/size per pipeline or DP module.
    2) memory read/write/execute permissions
    3) cache utilization.
    
    Modules and pipelines will allocate from their region only and this
    will be abstracted via the allocation APIs.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

---
+++ zephyr/include/sof/lib/vregion.h
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright(c) 2025 Intel Corporation.
+
+/* Pre Allocated Contiguous Virtual Region */
+#ifndef __SOF_LIB_VREGION_H__
+#define __SOF_LIB_VREGION_H__
+
+#include <zephyr/kernel.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct vregion;
+
+/**
+ * @brief Create a new virtual region instance.
+ *
+ * Create a new virtual region instance with specified static and dynamic partitions.
+ * Total size is the sum of static and dynamic sizes.
+ *
+ * @param[in] lifetime_size Size of the virtual region lifetime partition.
+ * @param[in] interim_size Size of the virtual region interim partition.
+ * @return struct vregion* Pointer to the new virtual region instance, or NULL on failure.
+ */
+struct vregion *vregion_create(size_t lifetime_size, size_t interim_size);
+
+/**
+ * @brief Destroy a virtual region instance.
+ *
+ * Free all associated resources and deallocate the virtual region instance.
+ *
+ * @param[in] vr Pointer to the virtual region instance to destroy.
+ */
+void vregion_destroy(struct vregion *vr);
+
+/**
+ * @brief Memory types for virtual region allocations.
+ * Used to specify the type of memory allocation within a virtual region.
+ *
+ * @note
+ * - interim: allocation that can be freed i.e. get/set large config, kcontrols.
+ * - lifetime: allocation that cannot be freed i.e. init data, pipeline data.
+ */
+enum vregion_mem_type {
+	VREGION_MEM_TYPE_INTERIM,		/* interim allocation that can be freed */
+	VREGION_MEM_TYPE_LIFETIME,		/* lifetime allocation */
+};
+
+/**
+ * @brief Allocate memory from the specified virtual region.
+ *
+ * @param[in] vr Pointer to the virtual region instance.
+ * @param[in] type Type of memory to allocate (lifetime or interim).
+ * @param[in] size Size of memory to allocate in bytes.
+ * @return void* Pointer to the allocated memory, or NULL on failure.
+ */
+void *vregion_alloc(struct vregion *vr, enum vregion_mem_type type, size_t size);
+
+/**
+ * @brief Allocate aligned memory from the specified virtual region.
+ *
+ * Allocate aligned memory from the specified virtual region based on the memory type.
+ *
+ * @param[in] vr Pointer to the virtual region instance.
+ * @param[in] type Type of memory to allocate (lifetime or interim).
+ * @param[in] size Size of memory to allocate in bytes.
+ * @param[in] alignment Alignment of memory to allocate in bytes.
+ * @return void* Pointer to the allocated memory, or NULL on failure.
+ */
+void *vregion_alloc_align(struct vregion *vr, enum vregion_mem_type type,
+			  size_t size, size_t alignment);
+
+/**
+ * @brief Free memory allocated from the specified virtual region.
+ *
+ * Free memory previously allocated from the specified virtual region.
+ *
+ * @param[in] vr Pointer to the virtual region instance.
+ * @param[in] ptr Pointer to the memory to free.
+ */
+void vregion_free(struct vregion *vr, void *ptr);
+
+/**
+ * @brief Log virtual region memory usage.
+ *
+ * @param[in] vr Pointer to the virtual region instance.
+ */
+void vregion_info(struct vregion *vr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SOF_LIB_VREGION_H__ */
