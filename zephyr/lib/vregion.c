7887cfb825786f3c4e58fbf30987d37689b7726b
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2025-10-23 13:39:49 +0100

    vregion: Add support for per pipeline/module virtual regions
    Add support for per pipeline and per module virtual memory regions.
    
    The intention is to provide a single virtual memory region per pipeline or
    per DP module that can simplify module/pipeline memory management.
    
    The region takes advantage of the way pipeline and modules are constructed,
    destroyed and used during their lifetimes.
    
    1) memory tracking - 1 pointer/size per pipeline or DP module.
    2) memory read/write/execute permissions
    3) cache utilization.
    
    Modules and pipelines will allocate from their region only and this
    will be abstracted via the allocation APIs.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

---
+++ zephyr/lib/vregion.c
@@ -0,0 +1,355 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright(c) 2025 Intel Corporation.
+ *
+ * Author: Liam Girdwood <liam.r.girdwood@linux.intel.com>
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <sof/lib/vpage.h>
+#include <sof/lib/vregion.h>
+#include <rtos/alloc.h>
+#include <sof/common.h>
+
+LOG_MODULE_REGISTER(vregion, CONFIG_SOF_LOG_LEVEL);
+
+/*
+ * Pre Allocated Contiguous Virtual Memory Region Allocator
+ *
+ * This allocator manages a pre-allocated virtual memory region that uses the
+ * virtual page allocator to allocate and free memory pages.
+ *
+ * It is designed for use cases where a contiguous virtual memory region
+ * is required, such as for batched allocation of audio pipelines and modules.
+ *
+ * New pipelines will create a new virtual region and will specify the size of the region
+ * which can be divided into multiple areas for different allocation lifetimes, permissions
+ * and sharing requirements.
+ *
+ * Advantages:
+ *
+ * 1) Contiguous virtual memory region for easier management and tracking of
+ *    pipeline & DP module memory. i.e. we just need to track the vregion pointer.
+ * 2) Reduced fragmentation and better cache utilization by using a simple linear
+ *    allocator for lifetime objects.
+ *
+ * Note: Software must pass in the size of the region areas at pipeline creation time.
+ */
+
+/**
+ * @brief virtual region memory structure.
+ *
+ * This structure represents a virtual memory region, which includes
+ * information about the base address, size, and allocation status
+ * of the region.
+ *
+ * Currently the virtual region memory can be partitioned into two areas on
+ * page-aligned boundaries:
+ *
+ * 1. Interim Heap: An interim memory area used for multiple temporary
+ *    allocations and frees over the lifetime of the audio processing pipeline.
+ *    E.g. for module kcontrol derived allocations.
+ *
+ * 2. Lifetime Allocator: A simple incrementing allocator used for long-term
+ *    static allocations that persist for the lifetime of the audio processing
+ *    pipeline. This allocator compresses allocations for better cache
+ *    utilization.
+ *
+ * More types can be added in the future.
+ *
+ *  * TODO: Pipeline/module reset() could reset the dynamic heap.
+ */
+
+ /* linear heap used for lifetime allocations */
+struct vlinear_heap {
+	uint8_t *base;		/* base address of linear allocator */
+	uint8_t *ptr;		/* current alloc pointer */
+	size_t size;		/* size of linear allocator in bytes */
+	size_t used;		/* used bytes in linear allocator */
+	int free_count;		/* number of frees - tuning only */
+};
+
+/* zephyr k_heap for interim allocations. TODO: make lockless for improved performance */
+struct interim_heap {
+	struct k_heap heap;
+};
+
+/* Main vregion context, see above intro for more details.
+ * TODO: Add support to flag which heaps should have their contexts saved and restored.
+ */
+struct vregion {
+	/* region context */
+	uint8_t *base;			/* base address of entire region */
+	size_t size;			/* size of whole region in bytes */
+	unsigned int pages;		/* size of whole region in pages */
+
+	/* interim heap */
+	struct interim_heap interim;	/* interim heap */
+
+	/* lifetime heap */
+	struct vlinear_heap lifetime;	/* lifetime linear heap */
+};
+
+/**
+ * @brief Create a new virtual region instance.
+ *
+ * Create a new VIRTUAL REGION instance with specified static and dynamic
+ * sizes. Total size is their sum.
+ *
+ * @param[in] lifetime_size Size of the virtual region lifetime partition.
+ * @param[in] interim_size Size of the virtual region interim partition.
+ * @return struct vregion* Pointer to the new virtual region instance, or NULL on failure.
+ */
+struct vregion *vregion_create(size_t lifetime_size, size_t interim_size)
+{
+	struct vregion *vr;
+	unsigned int pages;
+	size_t total_size;
+	uint8_t *vregion_base;
+
+	if (!lifetime_size || !interim_size) {
+		LOG_ERR("error: invalid vregion lifetime size %zu or interim size %zu",
+			lifetime_size, interim_size);
+		return NULL;
+	}
+
+	/*
+	 * Align up lifetime sizes and interim sizes to nearest page, the
+	 * vregion structure is stored in lifetime area so account for its size too.
+	 */
+	lifetime_size += sizeof(*vr);
+	lifetime_size = ALIGN_UP(lifetime_size, CONFIG_MM_DRV_PAGE_SIZE);
+	interim_size = ALIGN_UP(interim_size, CONFIG_MM_DRV_PAGE_SIZE);
+	total_size = lifetime_size + interim_size;
+
+	/* allocate pages for vregion */
+	pages = total_size / CONFIG_MM_DRV_PAGE_SIZE;
+	vregion_base = vpage_alloc(pages);
+	if (!vregion_base)
+		return NULL;
+
+	/* init vregion - place it at the start of the lifetime region */
+	vr = (struct vregion *)(vregion_base + interim_size);
+	vr->base = vregion_base;
+	vr->size = total_size;
+	vr->pages = pages;
+
+	/* set partition sizes */
+	vr->interim.heap.heap.init_bytes = interim_size;
+	vr->lifetime.size = lifetime_size;
+
+	/* set base addresses for partitions */
+	vr->interim.heap.heap.init_mem = vr->base;
+	vr->lifetime.base = vr->base + interim_size;
+
+	/* set alloc ptr addresses for lifetime linear partitions */
+	vr->lifetime.ptr = vr->lifetime.base + sizeof(*vr); /* skip vregion struct */
+	vr->lifetime.used = sizeof(*vr);
+
+	/* init interim heaps */
+	k_heap_init(&vr->interim.heap, vr->interim.heap.heap.init_mem, interim_size);
+
+	/* log the new vregion */
+	LOG_INF("new at base %p size %#zx pages %u struct embedded at %p",
+		(void *)vr->base, total_size, pages, (void *)vr);
+	LOG_DBG(" interim size %#zx at %p", interim_size, (void *)vr->interim.heap.heap.init_mem);
+	LOG_DBG(" lifetime size %#zx at %p", lifetime_size, (void *)vr->lifetime.base);
+
+	return vr;
+}
+
+/**
+ * @brief Destroy a virtual region instance.
+ *
+ * @param[in] vr Pointer to the virtual region instance to destroy.
+ */
+void vregion_destroy(struct vregion *vr)
+{
+	if (!vr)
+		return;
+
+	/* log the vregion being destroyed */
+	LOG_DBG("destroy %p size %#zx pages %u", (void *)vr->base, vr->size, vr->pages);
+	LOG_DBG(" lifetime used %zu free count %d", vr->lifetime.used, vr->lifetime.free_count);
+	vpage_free(vr->base);
+}
+
+/**
+ * @brief Allocate memory with alignment from the virtual region dynamic heap.
+ *
+ * @param[in] heap Pointer to the heap to use.
+ * @param[in] size Size of the allocation.
+ * @param[in] align Alignment of the allocation.
+ * @return void* Pointer to the allocated memory, or NULL on failure.
+ */
+static void *interim_alloc(struct interim_heap *heap,
+			   size_t size, size_t align)
+{
+	void *ptr;
+
+	ptr = k_heap_aligned_alloc(&heap->heap, align, size, K_NO_WAIT);
+	if (!ptr)
+		LOG_ERR("error: interim alloc failed for %d bytes align %d",
+			size, align);
+
+	return ptr;
+}
+
+/**
+ * @brief Free memory from the virtual region interim heap.
+ *
+ * @param[in] heap Pointer to the heap to use.
+ * @param[in] ptr Pointer to the memory to free.
+ */
+static void interim_free(struct interim_heap *heap, void *ptr)
+{
+	k_heap_free(&heap->heap, ptr);
+}
+
+/**
+ * @brief Allocate memory from the virtual region lifetime allocator.
+ *
+ * @param[in] heap Pointer to the linear heap to use.
+ * @param[in] size Size of the allocation.
+ * @param[in] align Alignment of the allocation.
+ *
+ * @return void* Pointer to the allocated memory, or NULL on failure.
+ */
+static void *lifetime_alloc(struct vlinear_heap *heap,
+			    size_t size, size_t align)
+{
+	void *ptr;
+	uint8_t *aligned_ptr;
+	size_t heap_obj_size;
+
+	/* align heap pointer to alignment requested */
+	aligned_ptr = UINT_TO_POINTER(ALIGN_UP(POINTER_TO_UINT(heap->ptr), align));
+
+	/* also align up size to D$ bytes if asked - allocation head and tail aligned */
+	if (align == CONFIG_DCACHE_LINE_SIZE)
+		size = ALIGN_UP(size, CONFIG_DCACHE_LINE_SIZE);
+
+	/* calculate new heap object size for object and alignment */
+	heap_obj_size = aligned_ptr - heap->ptr + size;
+
+	/* check we have enough lifetime space left */
+	if (heap_obj_size + heap->used > heap->size) {
+		LOG_ERR("error: lifetime alloc failed for object %d heap %d bytes free %d",
+			size, heap_obj_size, heap->size - heap->used);
+		return NULL;
+	}
+
+	/* allocate memory */
+	ptr = aligned_ptr;
+	heap->ptr += heap_obj_size;
+	heap->used += heap_obj_size;
+
+	return ptr;
+}
+
+/**
+ * @brief Free memory from the virtual region lifetime allocator.
+ *
+ * @param[in] heap Pointer to the linear heap to use.
+ * @param[in] ptr Pointer to the memory to free.
+ */
+static void lifetime_free(struct vlinear_heap *heap, void *ptr)
+{
+	/* simple free, just increment free count, this is for tuning only */
+	heap->free_count++;
+
+	LOG_DBG("lifetime free %p count %d", ptr, heap->free_count);
+}
+
+/**
+ * @brief Free memory from the virtual region.
+ *
+ * @param vr Pointer to the virtual region instance.
+ * @param ptr Pointer to the memory to free.
+ */
+void vregion_free(struct vregion *vr, void *ptr)
+{
+	if (!vr || !ptr)
+		return;
+
+	/* check if pointer is in interim heap */
+	if (ptr >= (void *)vr->interim.heap.heap.init_mem &&
+	    ptr < (void *)((uint8_t *)vr->interim.heap.heap.init_mem +
+			   vr->interim.heap.heap.init_bytes)) {
+		interim_free(&vr->interim, ptr);
+		return;
+	}
+
+	/* check if pointer is in lifetime heap */
+	if (ptr >= (void *)vr->lifetime.base &&
+	    ptr < (void *)(vr->lifetime.base + vr->lifetime.size)) {
+		lifetime_free(&vr->lifetime, ptr);
+		return;
+	}
+
+	LOG_ERR("error: vregion free invalid pointer %p", ptr);
+}
+EXPORT_SYMBOL(vregion_free);
+
+/**
+ * @brief Allocate memory type from the virtual region.
+ *
+ * @param[in] vr Pointer to the virtual region instance.
+ * @param[in] type Memory type to allocate.
+ * @param[in] size Size of the allocation.
+ * @param[in] alignment Alignment of the allocation.
+ *
+ * @return void* Pointer to the allocated memory, or NULL on failure.
+ */
+void *vregion_alloc_align(struct vregion *vr, enum vregion_mem_type type,
+			  size_t size, size_t alignment)
+{
+	if (!vr || !size)
+		return NULL;
+
+	if (!alignment)
+		alignment = 4; /* default align 4 bytes */
+
+	switch (type) {
+	case VREGION_MEM_TYPE_INTERIM:
+		return interim_alloc(&vr->interim, size, alignment);
+	case VREGION_MEM_TYPE_LIFETIME:
+		return lifetime_alloc(&vr->lifetime, size, alignment);
+	default:
+		LOG_ERR("error: invalid memory type %d", type);
+		return NULL;
+	}
+}
+EXPORT_SYMBOL(vregion_alloc_align);
+
+/**
+ * @brief Allocate memory from the virtual region.
+ * @param[in] vr Pointer to the virtual region instance.
+ * @param[in] type Memory type to allocate.
+ * @param[in] size Size of the allocation.
+ * @return void* Pointer to the allocated memory, or NULL on failure.
+ */
+void *vregion_alloc(struct vregion *vr, enum vregion_mem_type type, size_t size)
+{
+	return vregion_alloc_align(vr, type, size, 0);
+}
+EXPORT_SYMBOL(vregion_alloc);
+
+/**
+ * @brief Log virtual region memory usage.
+ *
+ * @param[in] vr Pointer to the virtual region instance.
+ */
+void vregion_info(struct vregion *vr)
+{
+	if (!vr)
+		return;
+
+	LOG_INF("base %p size %#zx pages %u",
+		(void *)vr->base, vr->size, vr->pages);
+	LOG_INF("lifetime used %#zx free count %d",
+		vr->lifetime.used, vr->lifetime.free_count);
+}
+EXPORT_SYMBOL(vregion_info);
