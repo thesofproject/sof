b3b130e6ae4ad68f2bf8d570c4c05e6e72f06ddc
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2025-11-05 19:08:04 +0200

    Math: FFT: Add support for some non-power-of-two FFT sizes
    This patch adds function mod_fft_multi_plan_new() and
    fft_multi_execute_32() to execute other than a power of
    two FFT size. The FFT sizes such 1536 that is 3*512
    or 3072 that is 3*1024 are supported by this change.
    
    The procedure to compute this consists of doing three power
    of two FFTs, multiply the outputs with twiddle factors and
    doing DFT of size for the outputs. The produce is wrapped
    by the new functions those are used similarly as previous
    FFT functions. The fft-multi function can be also used for
    power of two FFTs.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/math/fft/tune/sof_export_twiddle.m
+++ src/math/fft/tune/sof_export_twiddle.m
@@ -1,15 +1,18 @@
-% sof_export_twiddle(bits, fn, fft_size_max)
+% sof_export_twiddle(bits, fn, fft_size_max, denom, str_size_max, str_var)
 %
 % Input
 %   bits - Number of bits for data, 16 or 32
 %   fn - File name, defaults to twiddle.h
 %   fft_size_max - Number of twiddle factors, defaults to 1024 if omitted
+%   denom - divide index * 2 * pi by denom instead of fft_size_max, same if omitted
+%   str_size_max - macro name for values array size, FFT_SIZE_MAX if omitted
+%   str_var - variable name prefix, twiddle if omitted
 
 % SPDX-License-Identifier: BSD-3-Clause
 %
 % Copyright (c) 2022, Intel Corporation. All rights reserved.
 
-function sof_export_twiddle(bits, fn, fft_size_max)
+function sof_export_twiddle(bits, fn, fft_size_max, denom, str_size_max, str_var)
 
 if nargin < 2
 	fn = 'twiddle.h';
@@ -17,6 +20,18 @@ end
 
 if nargin < 3
 	fft_size_max = 1024;
+end
+
+if nargin < 4
+	denom = fft_size_max;
+end
+
+if nargin < 5
+	str_size_max = 'FFT_SIZE_MAX';
+end
+
+if nargin < 6
+	str_var = 'twiddle';
 end
 
 switch bits
@@ -34,8 +49,8 @@ end
 hcaps = upper(hname);
 
 i = 0:(fft_size_max  - 1);
-twiddle_real = cos(i * 2 * pi / fft_size_max);
-twiddle_imag = -sin(i * 2 * pi / fft_size_max);
+twiddle_real = cos(i * 2 * pi / denom);
+twiddle_imag = -sin(i * 2 * pi / denom);
 
 year = datestr(now(), 'yyyy');
 fh = fopen(fn, 'w');
@@ -48,12 +63,14 @@ fprintf(fh, '/* Twiddle factors in Q%d.%d format */\n\n', qx, qy);
 fprintf(fh, '#ifndef __INCLUDE_%s_H__\n', hcaps);
 fprintf(fh, '#define __INCLUDE_%s_H__\n\n', hcaps);
 fprintf(fh, '#include <stdint.h>\n\n');
-fprintf(fh, '#define FFT_SIZE_MAX	%d\n\n', fft_size_max);
+fprintf(fh, '#define %s\t%d\n\n', str_size_max, fft_size_max);
 fprintf(fh, '/* in Q1.%d, generated from cos(i * 2 * pi / FFT_SIZE_MAX) */\n', qy);
-c_export_int(fh, 'twiddle_real', 'FFT_SIZE_MAX', twiddle_real, qx, qy);
+str_real = sprintf('%s_real', str_var);
+c_export_int(fh, str_real, str_size_max, twiddle_real, qx, qy);
 
 fprintf(fh, '/* in Q1.%d, generated from sin(i * 2 * pi / FFT_SIZE_MAX) */\n', qy);
-c_export_int(fh, 'twiddle_imag', 'FFT_SIZE_MAX', twiddle_imag, qx, qy);
+str_imag = sprintf('%s_imag', str_var);
+c_export_int(fh, str_imag, str_size_max, twiddle_imag, qx, qy);
 
 fprintf(fh, '#endif\n');
 fclose(fh);
