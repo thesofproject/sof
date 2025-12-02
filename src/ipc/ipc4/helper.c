thesofproject/sof

commit c30f5bcb815da2a0e31fb7bba1ee48fafdf23fe8

ipc4: helper: Fix error handling in ipc_comp_connect
Handle ring buffer allocation failure by jumping to the new free_unlocked
label that skips ll_unblock. Do not release the lock at this point because
it is acquired later.

Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

src/ipc/ipc4/helper.c
@@ -613,7 +613,7 @@ __cold int ipc_comp_connect(struct ipc *ipc, ipc_pipe_comp_connect *_connect)
 						 audio_buffer_is_shared(&buffer->audio_buffer),
 						 buf_get_id(buffer));
 		if (!ring_buffer)
-			goto free;
+			goto free_unlocked;
 
 		/* data destination module needs to use ring_buffer */
 		audio_buffer_attach_secondary_buffer(&buffer->audio_buffer, dp_on_source,
@@ -698,6 +698,7 @@ e_sink_connect:
 	pipeline_disconnect(source, buffer, PPL_CONN_DIR_COMP_TO_BUFFER);
 free:
 	ll_unblock(cross_core_bind, flags);
+free_unlocked:
 	buffer_free(buffer);
 	return IPC4_INVALID_RESOURCE_STATE;
 }

commit fce9bcd1c427d2521c0d4cecd5c33b5f42ab0341

ipc4: helper: fix build when CONFIG_ZEPHYR_DP_SCHEDULER=n
A goto label that is not used when built with
CONFIG_ZEPHYR_DP_SCHEDULER=n causes build errors. Rework
the code to handle the error without a conditional jump
label.

Fixes: c30f5bcb815d ("ipc4: helper: Fix error handling in ipc_comp_connect")
Suggested-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

src/ipc/ipc4/helper.c
@@ -612,8 +612,10 @@ __cold int ipc_comp_connect(struct ipc *ipc, ipc_pipe_comp_connect *_connect)
 						 sink_get_min_free_space(snk),
 						 audio_buffer_is_shared(&buffer->audio_buffer),
 						 buf_get_id(buffer));
-		if (!ring_buffer)
-			goto free_unlocked;
+		if (!ring_buffer) {
+			buffer_free(buffer);
+			return IPC4_OUT_OF_MEMORY;
+		}
 
 		/* data destination module needs to use ring_buffer */
 		audio_buffer_attach_secondary_buffer(&buffer->audio_buffer, dp_on_source,
@@ -698,7 +700,6 @@ e_sink_connect:
 	pipeline_disconnect(source, buffer, PPL_CONN_DIR_COMP_TO_BUFFER);
 free:
 	ll_unblock(cross_core_bind, flags);
-free_unlocked:
 	buffer_free(buffer);
 	return IPC4_INVALID_RESOURCE_STATE;
 }