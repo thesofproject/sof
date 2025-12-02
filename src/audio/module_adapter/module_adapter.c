thesofproject/sof

commit 9ff0a7af8cfd1975b84c14a8f503be3d36aec9bd

alloc: sof_heap: Add missing support for shared buffers in sof_heap_alloc
Add missing support for the SOF_MEM_FLAG_USER_SHARED_BUFFER flag in the
sof_heap_alloc function.

Add a generic function is_heap_pointer that allows checking whether
a given pointer belongs to a given heap. It is used in the sof_heap_free
function to ensure that the freed pointer belongs to the specified heap,
and if not, to free it using rfree.

Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

src/audio/module_adapter/module_adapter.c
@@ -70,6 +70,8 @@ static struct k_heap *module_adapter_dp_heap_new(const struct comp_ipc_config *c
 	void *mod_heap_buf = mod_heap_mem + heap_prefix_size;
 
 	k_heap_init(mod_heap, mod_heap_buf, heap_size - heap_prefix_size);
+	mod_heap->heap.init_mem = mod_heap_buf;
+	mod_heap->heap.init_bytes = heap_size - heap_prefix_size;
 
 	return mod_heap;
 }

zephyr/lib/alloc.c
@@ -155,6 +155,25 @@ extern char _end[], _heap_sentry[];
 
 static struct k_heap sof_heap;
 
+/**
+ * Checks whether pointer is from a given heap memory.
+ * @param heap Pointer to a heap.
+ * @param ptr Pointer to memory being checked.
+ * @return True if pointer falls into heap memory region, false otherwise.
+ */
+static bool is_heap_pointer(const struct k_heap *heap, void *ptr)
+{
+	uintptr_t heap_start =
+		POINTER_TO_UINT(sys_cache_cached_ptr_get(heap->heap.init_mem));
+	uintptr_t heap_end = heap_start + heap->heap.init_bytes;
+
+	if (!is_cached(ptr))
+		ptr = (__sparse_force void *)sys_cache_cached_ptr_get(ptr);
+
+	return ((POINTER_TO_UINT(ptr) >= heap_start) &&
+		(POINTER_TO_UINT(ptr) < heap_end));
+}
+
 #if CONFIG_SOF_USERSPACE_USE_SHARED_HEAP
 static struct k_heap shared_buffer_heap;
 
@@ -628,7 +647,7 @@ EXPORT_SYMBOL(rfree);
 void *sof_heap_alloc(struct k_heap *heap, uint32_t flags, size_t bytes,
 		     size_t alignment)
 {
-	if (flags & SOF_MEM_FLAG_LARGE_BUFFER)
+	if (flags & (SOF_MEM_FLAG_LARGE_BUFFER | SOF_MEM_FLAG_USER_SHARED_BUFFER))
 		return rballoc_align(flags, bytes, alignment);
 
 	if (!heap)
@@ -642,7 +661,7 @@ void *sof_heap_alloc(struct k_heap *heap, uint32_t flags, size_t bytes,
 
 void sof_heap_free(struct k_heap *heap, void *addr)
 {
-	if (heap && addr)
+	if (heap && addr && is_heap_pointer(heap, addr))
 		heap_free(heap, addr);
 	else
 		rfree(addr);

commit 82ea77801a27eec87c9c0386ee95178c9404b420

audio: module_adapter: fix non-Zephyr builds
Only use Zephyr heap definitions when building with Zephyr.

Fixes: 9ff0a7af8cfd ("alloc: sof_heap: Add missing support for shared buffers in sof_heap_alloc")
Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

src/audio/module_adapter/module_adapter.c
@@ -70,8 +70,10 @@ static struct k_heap *module_adapter_dp_heap_new(const struct comp_ipc_config *c
 	void *mod_heap_buf = mod_heap_mem + heap_prefix_size;
 
 	k_heap_init(mod_heap, mod_heap_buf, heap_size - heap_prefix_size);
+#ifdef __ZEPHYR__
 	mod_heap->heap.init_mem = mod_heap_buf;
 	mod_heap->heap.init_bytes = heap_size - heap_prefix_size;
+#endif
 
 	return mod_heap;
 }