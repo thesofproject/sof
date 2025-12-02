thesofproject/sof

commit 9ff0a7af8cfd1975b84c14a8f503be3d36aec9bd

alloc: sof_heap: Add missing support for shared buffers in sof_heap_alloc
Add missing support for the SOF_MEM_FLAG_USER_SHARED_BUFFER flag in the
sof_heap_alloc function.

Add a generic function is_heap_pointer that allows checking whether
a given pointer belongs to a given heap. It is used in the sof_heap_free
function to ensure that the freed pointer belongs to the specified heap,
and if not, to free it using rfree.

Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

zephyr/lib/alloc.c
@@ -155,6 +155,25 @@ extern char _end[], _heap_sentry[];
 
 static struct k_heap sof_heap;
 
+/**
+ * Checks whether pointer is from a given heap memory.
+ * @param heap Pointer to a heap.
+ * @param ptr Pointer to memory being checked.
+ * @return True if pointer falls into heap memory region, false otherwise.
+ */
+static bool is_heap_pointer(const struct k_heap *heap, void *ptr)
+{
+	uintptr_t heap_start =
+		POINTER_TO_UINT(sys_cache_cached_ptr_get(heap->heap.init_mem));
+	uintptr_t heap_end = heap_start + heap->heap.init_bytes;
+
+	if (!is_cached(ptr))
+		ptr = (__sparse_force void *)sys_cache_cached_ptr_get(ptr);
+
+	return ((POINTER_TO_UINT(ptr) >= heap_start) &&
+		(POINTER_TO_UINT(ptr) < heap_end));
+}
+
 #if CONFIG_SOF_USERSPACE_USE_SHARED_HEAP
 static struct k_heap shared_buffer_heap;
 
@@ -628,7 +647,7 @@ EXPORT_SYMBOL(rfree);
 void *sof_heap_alloc(struct k_heap *heap, uint32_t flags, size_t bytes,
 		     size_t alignment)
 {
-	if (flags & SOF_MEM_FLAG_LARGE_BUFFER)
+	if (flags & (SOF_MEM_FLAG_LARGE_BUFFER | SOF_MEM_FLAG_USER_SHARED_BUFFER))
 		return rballoc_align(flags, bytes, alignment);
 
 	if (!heap)
@@ -642,7 +661,7 @@ void *sof_heap_alloc(struct k_heap *heap, uint32_t flags, size_t bytes,
 
 void sof_heap_free(struct k_heap *heap, void *addr)
 {
-	if (heap && addr)
+	if (heap && addr && is_heap_pointer(heap, addr))
 		heap_free(heap, addr);
 	else
 		rfree(addr);

src/audio/module_adapter/module_adapter.c
@@ -70,6 +70,8 @@ static struct k_heap *module_adapter_dp_heap_new(const struct comp_ipc_config *c
 	void *mod_heap_buf = mod_heap_mem + heap_prefix_size;
 
 	k_heap_init(mod_heap, mod_heap_buf, heap_size - heap_prefix_size);
+	mod_heap->heap.init_mem = mod_heap_buf;
+	mod_heap->heap.init_bytes = heap_size - heap_prefix_size;
 
 	return mod_heap;
 }

commit 31bb4821816f6f1a0b3fc67b4cc4617d402ba808

zephyr: lib: alloc: use of is_cached() breaks non-xtensa builds
is_cached() is not available in posix Zephyr builds (like fuzzer).
Use standard Zephyr interface for cache primitives instead.

Fixes: 9ff0a7af8cfd ("alloc: sof_heap: Add missing support for shared buffers in sof_heap_alloc")
Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

zephyr/lib/alloc.c
@@ -167,7 +167,7 @@ static bool is_heap_pointer(const struct k_heap *heap, void *ptr)
 		POINTER_TO_UINT(sys_cache_cached_ptr_get(heap->heap.init_mem));
 	uintptr_t heap_end = heap_start + heap->heap.init_bytes;
 
-	if (!is_cached(ptr))
+	if (!sys_cache_is_ptr_cached(ptr))
 		ptr = (__sparse_force void *)sys_cache_cached_ptr_get(ptr);
 
 	return ((POINTER_TO_UINT(ptr) >= heap_start) &&
@@ -182,7 +182,7 @@ static bool is_shared_buffer_heap_pointer(void *ptr)
 	uintptr_t shd_heap_start = POINTER_TO_UINT(shared_heapmem);
 	uintptr_t shd_heap_end = POINTER_TO_UINT(shared_heapmem + SHARED_BUFFER_HEAP_MEM_SIZE);
 
-	if (is_cached(ptr))
+	if (sys_cache_is_ptr_cached(ptr))
 		ptr = sys_cache_uncached_ptr_get((__sparse_force void __sparse_cache *)ptr);
 
 	return (POINTER_TO_UINT(ptr) >= shd_heap_start) && (POINTER_TO_UINT(ptr) < shd_heap_end);
@@ -341,7 +341,7 @@ static bool is_virtual_heap_pointer(void *ptr)
 		POINTER_TO_UINT(sys_cache_cached_ptr_get(&_unused_ram_start_marker));
 	uintptr_t virtual_heap_end = CONFIG_KERNEL_VM_BASE + CONFIG_KERNEL_VM_SIZE;
 
-	if (!is_cached(ptr))
+	if (!sys_cache_is_ptr_cached(ptr))
 		ptr = (__sparse_force void *)sys_cache_cached_ptr_get(ptr);
 
 	return ((POINTER_TO_UINT(ptr) >= virtual_heap_start) &&
@@ -463,7 +463,7 @@ static void heap_free(struct k_heap *h, void *mem)
 #ifdef CONFIG_SOF_ZEPHYR_HEAP_CACHED
 	void *mem_uncached;
 
-	if (is_cached(mem)) {
+	if (sys_cache_is_ptr_cached(mem)) {
 		mem_uncached = sys_cache_uncached_ptr_get((__sparse_force void __sparse_cache *)mem);
 		sys_cache_data_flush_and_invd_range(mem,
 				sys_heap_usable_size(&h->heap, mem_uncached));