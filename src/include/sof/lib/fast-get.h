1fe8c485bae23711f89d7cb3749b379450726d2e
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2026-01-23 11:07:43 +0100

    audio: fast-get: fast_get() and fast_put() should not be syscalls
    Userspace modules should only call mod_fast_get() and mod_fast_put(),
    which already can cross into the kernel space on their own, so
    fast_get() and fast_put() themselves don't need to be syscalls.
    Remove their syscall implementations.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/include/sof/lib/fast-get.h
+++ src/include/sof/lib/fast-get.h
@@ -12,17 +12,7 @@
 
 struct k_heap;
 
-#if defined(__ZEPHYR__) && defined(CONFIG_SOF_FULL_ZEPHYR_APPLICATION)
-#include <zephyr/toolchain.h>
-
-__syscall const void *fast_get(struct k_heap *heap, const void * const dram_ptr, size_t size);
-__syscall void fast_put(struct k_heap *heap, const void *sram_ptr);
-#include <zephyr/syscalls/fast-get.h>
-#else
-const void *z_impl_fast_get(struct k_heap *heap, const void * const dram_ptr, size_t size);
-void z_impl_fast_put(struct k_heap *heap, const void *sram_ptr);
-#define fast_get z_impl_fast_get
-#define fast_put z_impl_fast_put
-#endif /* __ZEPHYR__ */
+const void *fast_get(struct k_heap *heap, const void * const dram_ptr, size_t size);
+void fast_put(struct k_heap *heap, const void *sram_ptr);
 
 #endif /* __SOF_LIB_FAST_GET_H__ */
