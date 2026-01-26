be9c9f0747d6312880f7446859dc9589e0f3cde1
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2026-01-26 12:32:08 +0200

    Math: Add 32-bit square root function sofm_sqrt_int32()
    This patch adds a higher precision 32-bit fractional integer
    square root function to SOF math library. The algorithm uses
    a lookup table for initial value and two iterations with
    Newton-Raphson method to improve the accuracy. Both input and
    output format is Q2.30. The format was chosen to match complex
    to polar conversions numbers range for Q1.31 complex values.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/include/sof/math/sqrt.h
+++ src/include/sof/math/sqrt.h
@@ -17,4 +17,16 @@
  * @return Calculated square root of n in Q4.12 format, from 0 to 4.0.
  */
 uint16_t sofm_sqrt_int16(uint16_t u);
+
+/**
+ * sofm_sqrt_int32() - Calculate 32-bit fractional square root function.
+ * @param n Input value in Q2.30 format, from 0 to 2.0.
+ * @return Calculated square root of n in Q2.30 format.
+ *
+ * The input range of square root function is matched with Q1.31
+ * complex numbers range where the magnitude squared can be to 2.0.
+ * The function returns zero for non-positive input values.
+ */
+int32_t sofm_sqrt_int32(int32_t n);
+
 #endif /* __SOF_MATH_SQRT_H__ */
