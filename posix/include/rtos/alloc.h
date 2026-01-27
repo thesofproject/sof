6a403458d4bcbee8d20ffc5f3d52accdd359327e
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2026-01-27 14:24:39 +0100

    Reapply "audio: module-adapter: use objpool for allocation containers"
    This reverts commit 6d2d581472e5a4b15e506ba60fb70d31b09176eb. Using
    an object pool for module allocation containers should now work
    again.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- posix/include/rtos/alloc.h
+++ posix/include/rtos/alloc.h
@@ -140,10 +140,7 @@ static inline void k_heap_init(struct k_heap *heap, void *mem, size_t bytes)
 void *sof_heap_alloc(struct k_heap *heap, uint32_t flags, size_t bytes,
 		     size_t alignment);
 void sof_heap_free(struct k_heap *heap, void *addr);
-static inline struct k_heap *sof_sys_heap_get(void)
-{
-	return NULL;
-}
+struct k_heap *sof_sys_heap_get(void);
 
 /**
  * Calculates length of the null-terminated string.
