128e258e4fe9f70665e9cf491688aa85698b86b8
Jyri Sarha <jyri.sarha@linux.intel.com>
2026-02-02 23:06:19 +0200

    debug_stream: text_msg: Set exception dump hooks if available
    Set exception dump hooks if CONFIG_EXCEPTION_DUMP_HOOK=y. This enables
    sending a simple text report of fatal exceptions. To get this working
    one needs these config options:
    
    CONFIG_EXCEPTION_DUMP_HOOK=y
    CONFIG_EXCEPTION_DUMP_HOOK_ONLY=y
    CONFIG_SOF_DEBUG_STREAM_SLOT=y
    CONFIG_SOF_DEBUG_STREAM_TEXT_MSG=y
    CONFIG_SOF_DEBUG_STREAM_SLOT_NUMBER=2
    CONFIG_SOF_TELEMETRY=n
    CONFIG_SOF_TELEMETRY_PERFORMANCE_MEASUREMENTS=n
    CONFIG_SOF_TELEMETRY_IO_PERFORMANCE_MEASUREMENTS=n
    
    If system hangs and an the exception is reported successfully the
    report can be seen with debug_stream.py (which should be installed in
    the same directory with cavstool.py). It does matter if the too was
    not running at the time. The report should be available there in the
    debug slot window for debug_stream.py to decode as long as system
    remains up. The report should looks something like this:
    
    CPU 2: CPU 2 EXCCAUSE 13 (load/store PIF data error)
     PC 0xa06b24ba VADDR 0xa0031020
     PS 0x60820
       (INTLEVEL:0 EXCM: 0 UM:1 RING:0 WOE:1 OWB:8 CALLINC:2)
     A0 0xa06b10dd  SP 0xa00f8b00  A2 0xa  A3 0xa01a9354
     A4 0xa01a922c  A5 0x3c1  A6 0xa01a7ee4  A7 0xa01a7ec4
     A8 0xa006e572  A9 0xa00f8ab0 A10 0x4018d8b0 A11 0xa
    A12 0x14 A13 0x1 A14 0xa A15 (nil)
    LBEG 0xa0044323 LEND 0xa0044330 LCOUNT 0xa006de66
    SAR 0x5
     THREADPTR (nil)
    BT 0xa06b24b7:0xa00f8b00 CORRUPTED
    
    Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

--- src/debug/debug_stream/debug_stream_text_msg.c
+++ src/debug/debug_stream/debug_stream_text_msg.c
@@ -8,8 +8,12 @@
 #include <soc.h>
 #include <adsp_debug_window.h>
 #include <sof/common.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/arch/exception.h>
 
 #include <user/debug_stream_text_msg.h>
+
+LOG_MODULE_REGISTER(debug_stream_text_msg);
 
 void ds_msg(const char *format, ...)
 {
@@ -33,3 +37,59 @@ void ds_msg(const char *format, ...)
 						  sizeof(buf.msg.hdr.data[0]));
 	debug_stream_slot_send_record(&buf.msg.hdr);
 }
+
+#if defined(CONFIG_EXCEPTION_DUMP_HOOK)
+static struct {
+	struct debug_stream_text_msg msg;
+	char text[512];
+} __packed ds_buf;
+static int reports_sent_cpu[CONFIG_MP_MAX_NUM_CPUS];
+static size_t ds_pos;
+
+static void ds_exception_drain(bool flush)
+{
+	if (flush) {
+		ds_pos = 0;
+		return;
+	}
+
+	if (reports_sent_cpu[arch_proc_id()]++ > 0)
+		return;
+
+	ds_buf.msg.hdr.id = DEBUG_STREAM_RECORD_ID_TEXT_MSG;
+	ds_buf.msg.hdr.size_words = SOF_DIV_ROUND_UP(sizeof(ds_buf.msg) + ds_pos,
+						     sizeof(ds_buf.msg.hdr.data[0]));
+	/* Make sure the possible upto 3 extra bytes at end of msg are '\0' */
+	memset(ds_buf.text + ds_pos, 0, ds_buf.msg.hdr.size_words *
+	       sizeof(ds_buf.msg.hdr.data[0]) - ds_pos);
+	debug_stream_slot_send_record(&ds_buf.msg.hdr);
+	ds_pos = 0;
+}
+
+static void ds_exception_dump(const char *format, va_list args)
+{
+	ssize_t len;
+
+	if (reports_sent_cpu[arch_proc_id()] > 0)
+		return;
+
+	len = vsnprintf(ds_buf.text + ds_pos, sizeof(ds_buf.text) - ds_pos, format, args);
+	if (len < 0) {
+		ds_pos = 0;
+		return;
+	}
+	ds_pos += MIN(len, sizeof(ds_buf.text) - ds_pos);
+
+	if (ds_pos >= sizeof(ds_buf.text))
+		ds_exception_drain(false);
+}
+
+static int init_exception_dump_hook(void)
+{
+	set_exception_dump_hook(ds_exception_dump, ds_exception_drain);
+	LOG_INF("exception_dump_hook set");
+	return 0;
+}
+
+SYS_INIT(init_exception_dump_hook, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+#endif
