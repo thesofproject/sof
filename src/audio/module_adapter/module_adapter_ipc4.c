thesofproject/sof

commit d26364912305f457a1f86c3b7f6d1e75ad569711

module: Add support for module-specific init data
Add a global ID for module specific init data in the extended init data
section and update the decode function to save the data in the module's
ext_data struct along with the size.

Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

src/audio/module_adapter/module_adapter_ipc4.c
@@ -80,6 +80,18 @@ int module_ext_init_decode(const struct comp_driver *drv, struct module_ext_init
 				     dp_data->shared_bytes);
 			break;
 		}
+		case IPC4_MOD_INIT_DATA_ID_MODULE_DATA:
+		{
+			/*
+			 * set the module init_data. Modules must copy/save this in their init
+			 * callbacks if they need this to be persistent
+			 */
+			ext_data->module_data = (const void *)(obj + 1);
+			ext_data->module_data_size = obj->object_words * sizeof(uint32_t);
+			comp_cl_info(drv, "module init data size %u bytes",
+				     ext_data->module_data_size);
+			break;
+		}
 		default:
 			comp_cl_info(drv, "Unknown ext init object id %u of %u words",
 				     obj->object_id, obj->object_words);
@@ -147,8 +159,11 @@ int module_adapter_init_data(struct comp_dev *dev,
 		}
 	}
 
-	dst->init_data = cfg; /* legacy API */
-	dst->avail = true;
+	if (!config->ipc_extended_init) {
+		dst->init_data = cfg; /* legacy API */
+		dst->avail = true;
+	}
+
 	return 0;
 }
 

src/include/ipc4/module.h
@@ -77,7 +77,8 @@ struct ipc4_vendor_error {
 enum ipc4_mod_init_data_glb_id {
 	IPC4_MOD_INIT_DATA_ID_INVALID = 0,
 	IPC4_MOD_INIT_DATA_ID_DP_DATA = 1,
-	IPC4_MOD_INIT_DATA_ID_MAX = IPC4_MOD_INIT_DATA_ID_DP_DATA,
+	IPC4_MOD_INIT_DATA_ID_MODULE_DATA = 2,
+	IPC4_MOD_INIT_DATA_ID_MAX = IPC4_MOD_INIT_DATA_ID_MODULE_DATA,
 };
 
 /* data object for vendor bespoke data with ABI growth and backwards compat */

src/include/module/module/base.h
@@ -33,6 +33,8 @@
 struct ipc4_module_init_ext_obj_dp_data;
 struct module_ext_init_data {
 	const struct ipc4_module_init_ext_obj_dp_data *dp_data;
+	const void *module_data;
+	size_t module_data_size;
 };
 
 /**

commit 4841dc609dffbba7b6a3b5fbcff85e3b479b8a86

audio: module-adapter: Fix SRC regression when ext_init dp_data present
Fix SRC DP regression when dp_data is present in the ext_init
payload.

The only situation where we can assume that there is no module
specific init after the ext_init payload is when it was offered as
part of ext_init payload.

Completes fix abf1aa954311 ("audio: module-adapter: fix SRC DP regression")

Fixes: d26364912305 ("module: Add support for module-specific init data")

Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

src/audio/module_adapter/module_adapter_ipc4.c
@@ -159,8 +159,8 @@ int module_adapter_init_data(struct comp_dev *dev,
 		}
 	}
 
-	if (!config->ipc_extended_init ||
-	    (!dst->ext_data->dp_data && !dst->ext_data->module_data)) {
+	/* Assume legacy API if module data was not found in ext_init payload */
+	if (!dst->ext_data || !dst->ext_data->module_data) {
 		dst->init_data = cfg; /* legacy API */
 		dst->avail = true;
 	}