fb04ed0a6f0e9551feda8bd3170b5a523816e804
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2026-02-13 15:50:38 +0100

    fast-get: limit scope
    fast-get is only needed when DRAM is used, i.e. when
    CONFIG_COLD_STORE_EXECUTE_DRAM is selected. It's also unneeded in
    modules when CONFIG_LLEXT_TYPE_ELF_SHAREDLIB is used, i.e. in gcc
    builds, because also in those builds no .cold and .coldrodata
    sections are created.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/include/sof/lib/fast-get.h
+++ src/include/sof/lib/fast-get.h
@@ -12,7 +12,26 @@
 
 struct k_heap;
 
+/*
+ * When built for SOF, fast_get() and fast_put() are only needed when DRAM
+ * storage and execution is enabled (CONFIG_COLD_STORE_EXECUTE_DRAM=y), but not
+ * when building LLEXT extensions (!defined(LL_EXTENSION_BUILD)), using Zephyr
+ * SDK (CONFIG_LLEXT_TYPE_ELF_RELOCATABLE=n while
+ * CONFIG_LLEXT_TYPE_ELF_SHAREDLIB=y).
+ * For unit-testing full versions of fast_get() and fast_put() are checked by
+ * test/ztest/unit/fast-get/ and test/cmocka/src/lib/fast-get/
+ */
+#if (CONFIG_COLD_STORE_EXECUTE_DRAM &&					\
+	(CONFIG_LLEXT_TYPE_ELF_RELOCATABLE || !defined(LL_EXTENSION_BUILD))) || \
+	!CONFIG_SOF_FULL_ZEPHYR_APPLICATION
 const void *fast_get(struct k_heap *heap, const void * const dram_ptr, size_t size);
 void fast_put(struct k_heap *heap, const void *sram_ptr);
+#else
+static inline const void *fast_get(struct k_heap *heap, const void * const dram_ptr, size_t size)
+{
+	return dram_ptr;
+}
+static inline void fast_put(struct k_heap *heap, const void *sram_ptr) {}
+#endif
 
 #endif /* __SOF_LIB_FAST_GET_H__ */
