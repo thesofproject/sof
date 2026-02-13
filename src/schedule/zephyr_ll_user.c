2684660ffc30dfec9902cc31dcdffe52d13b208d
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2026-02-13 17:44:14 +0200

    schedule: zephyr_ll_user: make the heap accessible from user-space
    Rework the heap allocation. Instead of reusing module_driver_heap_init(),
    allocate the heap in zephyr_ll_user_resources_init(). A copy of
    heap pointer is stored into user-space accessible memory and
    zephyr_ll_user_heap_verify() is added to allow kernel code verify the heap
    object.
    
    CONFIG_SOF_ZEPHYR_SYS_USER_HEAP_SIZE is added to Kconfig to control
    size of the LL user heap. This will initially cover all user-space
    audio pipeline allocations.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- src/schedule/zephyr_ll_user.c
+++ src/schedule/zephyr_ll_user.c
@@ -17,25 +17,51 @@ LOG_MODULE_DECLARE(ll_schedule, CONFIG_SOF_LOG_LEVEL);
  *
  * This structure encapsulates the memory management resources required for the
  * low-latency (LL) scheduler in userspace mode. It provides memory isolation
- * and heap management for LL scheduler threads.
+ * and heap management for LL scheduler threads. Only kernel accessible.
  */
 struct zephyr_ll_mem_resources {
 	struct k_mem_domain mem_domain; /**< Memory domain for LL thread isolation */
-	struct k_heap *heap; /**< Heap allocator for LL scheduler memory */
+	struct k_heap heap; /**< Heap allocator for LL scheduler memory */
 };
 
 static struct zephyr_ll_mem_resources ll_mem_resources;
 
-static struct k_heap *zephyr_ll_heap_init(void)
+/**
+ * Heap allocator for LL scheduler memory (user accessible pointer)
+ *
+ * Note: this is also user-writable, so kernel must not rely on this to
+ * be correct and must always validate it separately.
+ */
+APP_SYSUSER_DATA static struct k_heap *zephyr_ll_heap;
+
+static struct k_heap *ll_heap_alloc(void)
 {
-	struct k_heap *heap = module_driver_heap_init();
-	struct k_mem_partition mem_partition;
-	int ret;
+	const size_t alloc_size = CONFIG_SOF_ZEPHYR_SYS_USER_HEAP_SIZE;
+
+	BUILD_ASSERT(CONFIG_SOF_ZEPHYR_SYS_USER_HEAP_SIZE % CONFIG_MM_DRV_PAGE_SIZE == 0);
+
+	void *mem = rballoc_align(SOF_MEM_FLAG_USER | SOF_MEM_FLAG_COHERENT, alloc_size,
+				  CONFIG_MM_DRV_PAGE_SIZE);
+	if (!mem)
+		return NULL;
+
+	k_heap_init(&ll_mem_resources.heap, mem, alloc_size);
 
 	/*
-	 * TODO: the size of LL heap should be independently configurable and
-	 *       not tied to CONFIG_SOF_ZEPHYR_USERSPACE_MODULE_HEAP_SIZE
+	 * k_heap_init() does not set these, so set the values
+	 * manually here
 	 */
+	ll_mem_resources.heap.heap.init_mem = mem;
+	ll_mem_resources.heap.heap.init_bytes = alloc_size;
+
+	return &ll_mem_resources.heap;
+}
+
+static void ll_heap_init(void)
+{
+	struct k_heap *heap = ll_heap_alloc();
+	struct k_mem_partition mem_partition;
+	int ret;
 
 	if (!heap) {
 		tr_err(&ll_tr, "heap alloc fail");
@@ -60,25 +86,58 @@ static struct k_heap *zephyr_ll_heap_init(void)
 	       (void *)mem_partition.start, heap->heap.init_bytes, ret);
 	if (ret)
 		k_panic();
-
-	return heap;
 }
 
 void zephyr_ll_user_resources_init(void)
 {
+	int ret;
+
 	k_mem_domain_init(&ll_mem_resources.mem_domain, 0, NULL);
 
-	ll_mem_resources.heap = zephyr_ll_heap_init();
+	ll_heap_init();
+
+	/* store a user-accessible pointer */
+	zephyr_ll_heap = &ll_mem_resources.heap;
 
 	/* attach common partition to LL domain */
 	user_memory_attach_common_partition(zephyr_ll_mem_domain());
+
+	ret = user_memory_attach_system_user_partition(zephyr_ll_mem_domain());
+	if (ret)
+		k_panic();
 }
 
+/**
+ * Check if 'heap' is a valid heap pointer.
+ *
+ * Available only in kernel mode.
+ *
+ * @return true if valid
+ */
+bool zephyr_ll_user_heap_verify(struct k_heap *heap)
+{
+	return heap == &ll_mem_resources.heap;
+}
+
+/**
+ * Returns heap object to use in user-space LL code.
+ *
+ * Can be called from user-space.
+ *
+ * @return heap pointer that can be passed to sof_heap_alloc()
+ */
 struct k_heap *zephyr_ll_user_heap(void)
 {
-	return ll_mem_resources.heap;
+	return zephyr_ll_heap;
 }
 
+/**
+ * Returns pointer to LL user-space memory domain.
+ *
+ * Available only in kernel mode.
+ *
+ * @return pointer to memory domain
+ */
 struct k_mem_domain *zephyr_ll_mem_domain(void)
 {
 	return &ll_mem_resources.mem_domain;
