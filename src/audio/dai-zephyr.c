a1c65ef455c536f5694e039cb745c68f89e2f51a
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2026-03-26 15:39:48 +0100

    dai: (cosmetic) fix an open-coded notifier call
    Use NOTIFIER_TARGET_CORE_LOCAL instead of open coding it.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- src/audio/dai-zephyr.c
+++ src/audio/dai-zephyr.c
@@ -1455,7 +1455,7 @@ int dai_common_trigger(struct dai_data *dd, struct comp_dev *dev, int cmd)
 
 			irq_local_disable(irq_flags);
 			notifier_event(group, NOTIFIER_ID_DAI_TRIGGER,
-				       BIT(cpu_get_id()), NULL, 0);
+				       NOTIFIER_TARGET_CORE_LOCAL, NULL, 0);
 			irq_local_enable(irq_flags);
 
 			/* return error of last trigger */
