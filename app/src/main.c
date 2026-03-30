a6e0a25902e4be00bf1c7143400192e54c1d6a6d
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2026-03-30 18:15:45 +0100

    qemu: native_sim: run the ztests and quit after completion
    Run the boot tests and quit when done.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- app/src/main.c
+++ app/src/main.c
@@ -89,9 +89,11 @@ void sys_arch_reboot(int type)
 void test_main(void)
 {
 	sof_app_main();
-#if CONFIG_SOF_BOOT_TEST && defined(QEMU_BOOT_TESTS)
+#if CONFIG_SOF_BOOT_TEST
 	sof_run_boot_tests();
+#if defined(QEMU_BOOT_TESTS)
 	qemu_xtensa_exit(0);
+#endif
 #endif
 }
 #else
