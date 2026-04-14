b759c46408776cbea01af39f9a43091272a70816
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2026-04-14 11:20:04 +0300

    Audio: SRC: Fix return code for failed delay lines allocate
    The proper return code is -ENOMEM. The -EINVAL makes this
    look like at higher logs level a component internal failure
    that it isn't.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- src/audio/src/src_common.c
+++ src/audio/src/src_common.c
@@ -537,7 +537,7 @@ int src_params_general(struct processing_module *mod,
 	if (!cd->delay_lines) {
 		comp_err(dev, "failed to alloc cd->delay_lines, delay_lines_size = %zu",
 			 delay_lines_size);
-		return  -EINVAL;
+		return  -ENOMEM;
 	}
 
 	/* Clear all delay lines here */
