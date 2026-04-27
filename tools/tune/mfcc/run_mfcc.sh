3e602072891ed5f84213b32af3a92797b539ad0c
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2026-04-27 15:44:09 +0300

    Audio: MFCC: Use sof-testbench4 and topology2 to run the module
    This patch updates the run script run_mfcc.sh to use topology2
    and the current sof-testbech4.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- tools/tune/mfcc/run_mfcc.sh
+++ tools/tune/mfcc/run_mfcc.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright(c) 2022 Intel Corporation. All rights reserved.
+# Copyright(c) 2022-2026 Intel Corporation. All rights reserved.
 
 set -e
 
 RAW_INPUT=in.raw
 RAW_OUTPUT=mfcc.raw
 
-export LD_LIBRARY_PATH=../../testbench/build_testbench/sof_ep/install/lib:../../testbench/build_testbench/sof_parser/install/lib
-
-TESTBENCH=../../testbench/build_testbench/install/bin/testbench
-OPT="-q -r 16000 -R 16000 -c 1 -n 1 -b S16_LE -t ../../build_tools/test/topology/test-playback-ssp5-mclk-0-I2S-mfcc-s16le-s16le-48k-24576k-codec.tplg"
+TESTBENCH=$SOF_WORKSPACE/sof/tools/testbench/build_testbench/install/bin/sof-testbench4
+TOPOLOGY=$SOF_WORKSPACE/sof/tools/build_tools/topology/topology2/development/sof-hda-benchmark-mfcc16.tplg
+OPT="-r 16000 -c 2 -b S16_LE -p 3,4 -t $TOPOLOGY -i $RAW_INPUT -o $RAW_OUTPUT"
 
 # Convert input audio file raw 16 kHz 1 channel 16 bit
 sox --encoding signed-integer "$1" -L -r 16000 -c 1 -b 16 "$RAW_INPUT"
