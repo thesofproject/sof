thesofproject/sof

commit 57d78f647665c6808bebc4d5d1175c950eab481d

ipc: make IPC message allocation userspace-safe
Add an optional heap parameter to ipc_msg_w_ext_init() and
ipc_msg_init() so callers can direct allocations to a specific
heap. When the heap argument is NULL the existing rzalloc()
path is used; when non-NULL, sof_heap_alloc()/sof_heap_free()
are used instead. This allows IPC messages to be allocated from
userspace-accessible heaps.

For audio module contexts, introduce mod_ipc_msg_w_ext_init()
and mod_ipc_msg_init() in generic.h. These use mod_zalloc()/
mod_free() for allocations that are automatically tracked and
freed with the module lifecycle.

ipc_msg_w_ext_init() is moved from a static inline in msg.h to
a non-inline function in ipc-common.c due to the additional
sof_heap_alloc dependency.

Update all existing callers:
- Module context callers (cadence, tdfb, sound_dose) use the
  new mod_ipc_msg_*() variants.
- host-zephyr.c uses hd->heap, pipeline-graph.c uses the heap
  parameter from pipeline_new().
- Remaining kernel-context callers pass NULL for the default
  heap.

Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>
(cherry picked from commit 11a17d542f135524d4c2d348a5ea60756d1859ff)

src/audio/module_adapter/module/cadence_ipc4.c
@@ -484,8 +484,8 @@ static int cadence_codec_process(struct processing_module *mod, struct sof_sourc
 		primary->r.type = SOF_IPC4_GLB_NOTIFICATION;
 		primary->r.rsp = SOF_IPC4_MESSAGE_DIR_MSG_REQUEST;
 		primary->r.msg_tgt = SOF_IPC4_MESSAGE_TARGET_FW_GEN_MSG;
-		msg = ipc_msg_w_ext_init(msg_proto.header, msg_proto.extension,
-					 sizeof(*msg_module_data));
+		msg = mod_ipc_msg_w_ext_init(mod, msg_proto.header, msg_proto.extension,
+					     sizeof(*msg_module_data));
 		if (msg) {
 			msg_module_data = (struct sof_ipc4_notify_module_data *)msg->tx_data;
 			msg_module_data->instance_id = IPC4_INST_ID(ipc_config->id);

src/audio/google/google_hotword_detect.c
@@ -112,7 +112,7 @@ static struct comp_dev *ghd_create(const struct comp_driver *drv,
 	cd->event.event_type = SOF_CTRL_EVENT_KD;
 	cd->event.num_elems = 0;
 
-	cd->msg = ipc_msg_init(cd->event.rhdr.hdr.cmd, cd->event.rhdr.hdr.size);
+	cd->msg = ipc_msg_init(NULL, cd->event.rhdr.hdr.cmd, cd->event.rhdr.hdr.size);
 	if (!cd->msg) {
 		comp_err(dev, "ipc_msg_init failed");
 		goto cd_fail;

src/audio/host-legacy.c
@@ -553,7 +553,7 @@ int host_common_new(struct host_data *hd, struct comp_dev *dev,
 
 	ipc_build_stream_posn(&hd->posn, SOF_IPC_STREAM_POSITION, config_id);
 
-	hd->msg = ipc_msg_init(hd->posn.rhdr.hdr.cmd, hd->posn.rhdr.hdr.size);
+	hd->msg = ipc_msg_init(NULL, hd->posn.rhdr.hdr.cmd, hd->posn.rhdr.hdr.size);
 	if (!hd->msg) {
 		comp_err(dev, "ipc_msg_init failed");
 		dma_put(hd->dma);

src/audio/host-zephyr.c
@@ -728,7 +728,7 @@ __cold int host_common_new(struct host_data *hd, struct comp_dev *dev,
 	ipc_build_stream_posn(&hd->posn, SOF_IPC_STREAM_POSITION, config_id);
 
 #if CONFIG_HOST_DMA_IPC_POSITION_UPDATES
-	hd->msg = ipc_msg_init(hd->posn.rhdr.hdr.cmd, sizeof(hd->posn));
+	hd->msg = ipc_msg_init(hd->heap, hd->posn.rhdr.hdr.cmd, sizeof(hd->posn));
 	if (!hd->msg) {
 		comp_err(dev, "ipc_msg_init failed");
 		sof_dma_put(hd->dma);

src/audio/pipeline/pipeline-graph.c
@@ -156,7 +156,7 @@ struct pipeline *pipeline_new(struct k_heap *heap, uint32_t pipeline_id, uint32_
 	ipc_build_stream_posn(&posn, SOF_IPC_STREAM_TRIG_XRUN, p->comp_id);
 
 	if (posn.rhdr.hdr.size) {
-		p->msg = ipc_msg_init(posn.rhdr.hdr.cmd, posn.rhdr.hdr.size);
+		p->msg = ipc_msg_init(heap, posn.rhdr.hdr.cmd, posn.rhdr.hdr.size);
 		if (!p->msg) {
 			pipe_err(p, "ipc_msg_init failed");
 			goto free;

src/audio/sound_dose/sound_dose-ipc4.c
@@ -32,9 +32,9 @@ static struct ipc_msg *sound_dose_notification_init(struct processing_module *mo
 	primary->r.type = SOF_IPC4_GLB_NOTIFICATION;
 	primary->r.rsp = SOF_IPC4_MESSAGE_DIR_MSG_REQUEST;
 	primary->r.msg_tgt = SOF_IPC4_MESSAGE_TARGET_FW_GEN_MSG;
-	msg = ipc_msg_w_ext_init(msg_proto.header, msg_proto.extension,
-				 sizeof(struct sof_ipc4_notify_module_data) +
-				 sizeof(struct sof_ipc4_control_msg_payload));
+	msg = mod_ipc_msg_w_ext_init(mod, msg_proto.header, msg_proto.extension,
+				     sizeof(struct sof_ipc4_notify_module_data) +
+				     sizeof(struct sof_ipc4_control_msg_payload));
 	if (!msg)
 		return NULL;
 

src/audio/tdfb/tdfb_ipc3.c
@@ -36,7 +36,7 @@ static int init_get_ctl_ipc(struct processing_module *mod)
 
 	cd->ctrl_data->rhdr.hdr.cmd = SOF_IPC_GLB_COMP_MSG | SOF_IPC_COMP_GET_VALUE | comp_id;
 	cd->ctrl_data->rhdr.hdr.size = TDFB_GET_CTRL_DATA_SIZE;
-	cd->msg = ipc_msg_init(cd->ctrl_data->rhdr.hdr.cmd, cd->ctrl_data->rhdr.hdr.size);
+	cd->msg = mod_ipc_msg_init(mod, cd->ctrl_data->rhdr.hdr.cmd, cd->ctrl_data->rhdr.hdr.size);
 
 	cd->ctrl_data->comp_id = comp_id;
 	cd->ctrl_data->type = SOF_CTRL_TYPE_VALUE_CHAN_GET;

src/audio/tdfb/tdfb_ipc4.c
@@ -48,8 +48,8 @@ static struct ipc_msg *tdfb_notification_init(struct processing_module *mod,
 	primary->r.type = SOF_IPC4_GLB_NOTIFICATION;
 	primary->r.rsp = SOF_IPC4_MESSAGE_DIR_MSG_REQUEST;
 	primary->r.msg_tgt = SOF_IPC4_MESSAGE_TARGET_FW_GEN_MSG;
-	msg = ipc_msg_w_ext_init(msg_proto.header, msg_proto.extension,
-				 sizeof(struct sof_ipc4_notify_module_data) +
+	msg = mod_ipc_msg_w_ext_init(mod, msg_proto.header, msg_proto.extension,
+				     sizeof(struct sof_ipc4_notify_module_data) +
 				 sizeof(struct sof_ipc4_control_msg_payload) +
 				 sizeof(struct sof_ipc4_ctrl_value_chan));
 	if (!msg)

commit a39901dfaa1baa2604b79a998cd8eadcd5ab3306

audio: cadence: Revert earlier incomplete mod_ipc_msg_w_ext_init() change
The earlier version of taking mod_ipc_msg_w_ext_init() into use in
cadence module was incomplete. Revert the incomplete change. The
change was part of:

Fixes: 57d78f647665 ("ipc: make IPC message allocation userspace-safe")

Signed-off-by: Jyri Sarha <jyri.sarha@linux.intel.com>

src/audio/module_adapter/module/cadence_ipc4.c
@@ -484,8 +484,8 @@ static int cadence_codec_process(struct processing_module *mod, struct sof_sourc
 		primary->r.type = SOF_IPC4_GLB_NOTIFICATION;
 		primary->r.rsp = SOF_IPC4_MESSAGE_DIR_MSG_REQUEST;
 		primary->r.msg_tgt = SOF_IPC4_MESSAGE_TARGET_FW_GEN_MSG;
-		msg = mod_ipc_msg_w_ext_init(mod, msg_proto.header, msg_proto.extension,
-					     sizeof(*msg_module_data));
+		msg = ipc_msg_w_ext_init(msg_proto.header, msg_proto.extension,
+					 sizeof(*msg_module_data));
 		if (msg) {
 			msg_module_data = (struct sof_ipc4_notify_module_data *)msg->tx_data;
 			msg_module_data->instance_id = IPC4_INST_ID(ipc_config->id);