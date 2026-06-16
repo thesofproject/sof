109529caa54645ffaf8bc6c78cf572ec85212ffe
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2026-06-16 20:45:41 +0300

    schedule: zephyr_dp_sched_app: fix build with no thread names
    Build fails when building with CONFIG_THREAD_NAME disabled. Fix
    the issue by conditional compilation of code using
    CONFIG_THREAD_MAX_NAME_LEN.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- src/schedule/zephyr_dp_schedule_application.c
+++ src/schedule/zephyr_dp_schedule_application.c
@@ -414,11 +414,13 @@ void scheduler_dp_internal_free(struct task *task)
 #ifdef CONFIG_THREAD_NAME
 static void scheduler_dp_thread_name_set(k_tid_t thread_id, struct processing_module *mod)
 {
+#ifdef CONFIG_THREAD_NAME
 	char name[CONFIG_THREAD_MAX_NAME_LEN];
 
 	snprintf(name, sizeof(name), "DP:%#x", mod->dev->ipc_config.id);
 
 	k_thread_name_set(thread_id, name);
+#endif
 }
 #else
 /* k_thread_name_set() is a no-op so skip constructing a thread name */
