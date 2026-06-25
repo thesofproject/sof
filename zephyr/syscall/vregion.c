e19e04bfeb7c28860bc6b4499ff2dbefa4992faf
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2026-06-25 14:12:12 +0300

    zephyr: lib: make vregion_alloc/free system calls
    Make vregion_alloc(), vregion_alloc_coherent(),
    vregion_alloc_align(), vregion_alloc_coherent_align(),
    and vregion_free() available as Zephyr system calls for
    user-space threads.
    
    Add K_SYSCALL_MEMORY_WRITE verification to all syscall
    handlers to validate the calling thread has access to
    the vregion's managed memory area.
    
    Add CONFIG_SOF_USERSPACE_INTERFACE_VREGION Kconfig option
    to control the feature. It is auto-selected by
    SOF_USERSPACE_LL when SOF_VREGIONS is enabled.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

---
+++ zephyr/syscall/vregion.c
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright(c) 2026 Intel Corporation.
+
+#include <sof/lib/vregion.h>
+#include <zephyr/kernel.h>
+#include <zephyr/internal/syscall_handler.h>
+
+static inline void *z_vrfy_vregion_alloc(struct vregion *vr, size_t size)
+{
+	size_t vr_size = 0;
+	uintptr_t vr_start;
+
+	vregion_mem_info(vr, &vr_size, &vr_start);
+	if (vr_size)
+		K_OOPS(K_SYSCALL_MEMORY_WRITE((void *)vr_start, vr_size));
+
+	return z_impl_vregion_alloc(vr, size);
+}
+#include <zephyr/syscalls/vregion_alloc_mrsh.c>
+
+static inline void *z_vrfy_vregion_alloc_coherent(struct vregion *vr, size_t size)
+{
+	size_t vr_size = 0;
+	uintptr_t vr_start;
+
+	vregion_mem_info(vr, &vr_size, &vr_start);
+	if (vr_size)
+		K_OOPS(K_SYSCALL_MEMORY_WRITE((void *)vr_start, vr_size));
+
+	return z_impl_vregion_alloc_coherent(vr, size);
+}
+#include <zephyr/syscalls/vregion_alloc_coherent_mrsh.c>
+
+static inline void *z_vrfy_vregion_alloc_align(struct vregion *vr, size_t size, size_t alignment)
+{
+	size_t vr_size = 0;
+	uintptr_t vr_start;
+
+	vregion_mem_info(vr, &vr_size, &vr_start);
+	if (vr_size)
+		K_OOPS(K_SYSCALL_MEMORY_WRITE((void *)vr_start, vr_size));
+
+	return z_impl_vregion_alloc_align(vr, size, alignment);
+}
+#include <zephyr/syscalls/vregion_alloc_align_mrsh.c>
+
+static inline void *z_vrfy_vregion_alloc_coherent_align(struct vregion *vr,
+							size_t size, size_t alignment)
+{
+	size_t vr_size = 0;
+	uintptr_t vr_start;
+
+	vregion_mem_info(vr, &vr_size, &vr_start);
+	if (vr_size)
+		K_OOPS(K_SYSCALL_MEMORY_WRITE((void *)vr_start, vr_size));
+
+	return z_impl_vregion_alloc_coherent_align(vr, size, alignment);
+}
+#include <zephyr/syscalls/vregion_alloc_coherent_align_mrsh.c>
+
+static inline void z_vrfy_vregion_free(struct vregion *vr, void *ptr)
+{
+	size_t vr_size = 0;
+	uintptr_t vr_start;
+
+	vregion_mem_info(vr, &vr_size, &vr_start);
+	if (vr_size)
+		K_OOPS(K_SYSCALL_MEMORY_WRITE((void *)vr_start, vr_size));
+
+	z_impl_vregion_free(vr, ptr);
+}
+#include <zephyr/syscalls/vregion_free_mrsh.c>
