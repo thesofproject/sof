5f6f9633ee884d52b1946dacd3d76e2002d755b5
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2026-07-02 18:31:55 +0300

    Revert "module: generic: remove MEM_API_CHECK_THREAD debug mechanism"
    This reverts commit 768faad56199b52585c42f181011013102b1e164.

--- src/include/sof/audio/module_adapter/module/generic.h
+++ src/include/sof/audio/module_adapter/module/generic.h
@@ -21,6 +21,10 @@
 #include <sof/audio/source_api.h>
 #include "module_interface.h"
 
+/* The __ZEPHYR__ condition is to keep cmocka tests working */
+#if CONFIG_MODULE_MEMORY_API_DEBUG && defined(__ZEPHYR__)
+#include <zephyr/kernel/thread.h>
+#endif
 #include <sof/compiler_attributes.h>
 
 /*
@@ -130,6 +134,9 @@ struct module_resources {
 	size_t heap_usage;
 	size_t heap_high_water_mark;
 	struct mod_alloc_ctx *alloc;
+#if CONFIG_MODULE_MEMORY_API_DEBUG && defined(__ZEPHYR__)
+	k_tid_t rsrc_mngr;
+#endif
 };
 
 enum mod_resource_type {
