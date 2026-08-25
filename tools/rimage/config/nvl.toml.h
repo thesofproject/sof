196722a75fbb81b6cad0bb8b999925876095f1a3
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2026-08-25 11:46:42 +0300

    rimage: config: nvl: include phase template module if enabled
    Follow mtl/lnl/ptl/wcl and allow the template to be built.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

--- tools/rimage/config/nvl.toml.h
+++ tools/rimage/config/nvl.toml.h
@@ -142,6 +142,10 @@ index = __COUNTER__
 #include <audio/mfcc/mfcc.toml>
 #endif
 
+#if defined(CONFIG_COMP_TEMPLATE) || defined(LLEXT_FORCE_ALL_MODULAR)
+#include <audio/template/template.toml>
+#endif
+
 #if defined(CONFIG_COMP_SOUND_DOSE) || defined(LLEXT_FORCE_ALL_MODULAR)
 #include <audio/sound_dose/sound_dose.toml>
 #endif
