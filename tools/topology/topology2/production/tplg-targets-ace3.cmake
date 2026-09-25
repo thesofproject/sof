thesofproject/sof

commit 0dad33d68c7a73152c64bde01c879943737f34a1

topology2: rename sof-ptl-rt713-l3-rt1320-l1-2ch.tplg
The existing sof-ptl-rt713-l3-rt1320-l1.tplg includes 2ch PCH DMIC. And
the '-2ch' suffix will be added in the Linux driver. Rename
sof-ptl-rt713-l3-rt1320-l1 to sof-ptl-rt713-l3-rt1320-l1-2ch.
It is fine because the sof-ptl-rt713-l3-rt1320-l1.tplg was not released
to sof-bin yet.

Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

tools/topology/topology2/production/tplg-targets-ace3.cmake
@@ -168,11 +168,11 @@ SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-
 SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
 
 # LG Gram Pro 2026 (16Z90U-KU7BK) with RT713 on link 3 and single stereo RT1320 on link 1
-"cavs-sdw\;sof-ptl-rt713-l3-rt1320-l1\;PLATFORM=ptl,SDW_DMIC=0,NUM_SDW_AMP_LINKS=1,\
+"cavs-sdw\;sof-ptl-rt713-l3-rt1320-l1-2ch\;PLATFORM=ptl,SDW_DMIC=0,NUM_SDW_AMP_LINKS=1,\
 NUM_DMICS=2,PDM1_MIC_A_ENABLE=0,PDM1_MIC_B_ENABLE=0,DMIC0_ID=3,DMIC1_ID=4,\
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=SDW1-Playback,\
 SDW_JACK_OUT_STREAM=SDW3-Playback,SDW_JACK_IN_STREAM=SDW3-Capture,\
-PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-ptl-rt713-l3-rt1320-l1.bin"
+PREPROCESS_PLUGINS=nhlt,NHLT_BIN=nhlt-sof-ptl-rt713-l3-rt1320-l1-2ch.bin"
 
 "cavs-sdw\;sof-ptl-cs42l43-l2-cs35l56x6-l13\;NUM_SDW_AMP_LINKS=3,SDW_DMIC=1,\
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\

commit 0708fd532ad88d028cf2859f5655fed295b0f6f6

topology2: ace3: Add back sof-ptl-rt713-l3-rt1320-l1 topology
The original topology got renamed to sof-ptl-rt713-l3-rt1320-l1-2ch
and no replacement was left for the variant without PCH-DMIC.
It is true, thet before the rename the non DMIC topology included
the pch-dmic, which was incorrect.

Fixes: 0dad33d68c7a ("topology2: rename sof-ptl-rt713-l3-rt1320-l1-2ch.tplg")
Link: https://github.com/thesofproject/linux/issues/5946
Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>

tools/topology/topology2/production/tplg-targets-ace3.cmake
@@ -171,6 +171,11 @@ SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
 SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
 SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
 
+# MSI Prestige 13 AI+ A3MG for example
+"cavs-sdw\;sof-ptl-rt713-l3-rt1320-l1\;PLATFORM=ptl,SDW_DMIC=1,NUM_SDW_AMP_LINKS=1,\
+SDW_AMP_FEEDBACK=false,SDW_SPK_STREAM=Playback-SmartAmp,SDW_DMIC_STREAM=Capture-SmartMic,\
+SDW_JACK_OUT_STREAM=Playback-SimpleJack,SDW_JACK_IN_STREAM=Capture-SimpleJack"
+
 # LG Gram Pro 2026 (16Z90U-KU7BK) with RT713 on link 3 and single stereo RT1320 on link 1
 "cavs-sdw\;sof-ptl-rt713-l3-rt1320-l1-2ch\;PLATFORM=ptl,SDW_DMIC=0,NUM_SDW_AMP_LINKS=1,\
 NUM_DMICS=2,PDM1_MIC_A_ENABLE=0,PDM1_MIC_B_ENABLE=0,DMIC0_ID=3,DMIC1_ID=4,\